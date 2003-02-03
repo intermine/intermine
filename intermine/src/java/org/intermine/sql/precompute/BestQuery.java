2d7751181ee0e4a5f5e063e6317bd4fe8395729b
andy <andy>
2003-02-03 15:11:11 +0000

    Throw SQLException from add() if error occurs in underlying database

--- intermine/src/java/org/intermine/sql/precompute/BestQuery.java
+++ intermine/src/java/org/intermine/sql/precompute/BestQuery.java
@@ -1,17 +1,23 @@
 package org.flymine.sql.precompute;
 
+import org.flymine.sql.query.Query;
+import java.sql.SQLException;
+
 /**
  * This object is an abstract superclass for a Best Query tracker. Queries can be added to these
  * objects, and they will keep track of them.
  *
  * @author Matthew Wakeling
+ * @author Andrew Varley
  */
 public abstract class BestQuery
 {
     /**
      * Allows a Query to be added to this tracker.
      *
-     * @param obj a Query to be added to the tracker
+     * @param q a Query to be added to the tracker
+     * @throws BestQueryException when adding should stop
+     * @throws SQLException if error occurs in the underlying database
      */
-    public abstract void add(Query obj);
+    public abstract void add(Query q) throws BestQueryException, SQLException;
 }
