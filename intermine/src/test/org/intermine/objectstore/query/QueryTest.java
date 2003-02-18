fad1065c0a22d27c993199769a9675b12ed41a2c
mark <mark>
2003-02-18 17:25:00 +0000

    Added tests for aliased QueryNodes

--- intermine/src/test/org/intermine/objectstore/query/QueryTest.java
+++ intermine/src/test/org/intermine/objectstore/query/QueryTest.java
@@ -20,4 +20,25 @@ public class QueryTest extends TestCase
     public  void testAddClass() {
         query.addClass(new QueryClass(Department.class));
     }
+
+    public  void testAddNull() {
+        try {
+            query.addClass(null);
+            fail("Expected: NullPointerException");
+        }
+        catch (NullPointerException e) {
+        }
+    }
+
+    public void testAddToSelect() throws Exception {
+        QueryClass qn1 = new QueryClass(Department.class);
+        QueryClass qn2 = new QueryClass(Department.class);
+        QueryField qn3 = new QueryField(qn1, "name");
+        query.addToSelect(qn1);
+        assertEquals("a1", (String) query.getAliases().get(qn1));
+        query.addToSelect(qn2);
+        assertEquals("a2", (String) query.getAliases().get(qn2));
+        query.addToSelect(qn3);
+        assertEquals("a3", (String) query.getAliases().get(qn3));
+    }
 }
