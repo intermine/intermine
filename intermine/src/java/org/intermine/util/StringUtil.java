3b317a747ceb246b6b29eca2e24d8efe04704421
mark <mark>
2003-02-20 18:48:44 +0000

    Added toSameInitialCase

--- intermine/src/java/org/intermine/util/StringUtil.java
+++ intermine/src/java/org/intermine/util/StringUtil.java
@@ -13,7 +13,7 @@ public class StringUtil
     /**
      * Returns the number of occurances of str in target
      *
-     * @param str the sting to count
+     * @param str the String to count
      * @param target the String to look in
      * @return the number of occurances of str in target
      * @throws NullPointerException if either str or target are null
@@ -48,4 +48,33 @@ public class StringUtil
         return str.substring(0, 1).toUpperCase() + str.substring(1, str.length());
     }
 
+    /**
+     * Returns a string with the same initial letter case as the template string
+     *
+     * @param n the String to convert
+     * @param template the String to base the conversion on
+     * @return the new String, capitalised like template
+     */
+    public static String toSameInitialCase(String n, String template) {
+        if (n == null) {
+            throw new NullPointerException("String to convert cannot be null");
+        }
+        if (template == null) {
+            return n;
+        }
+        Character first = new Character(template.charAt(0));
+        StringBuffer sb = new StringBuffer();
+        
+        if (Character.isUpperCase(template.charAt(0))) {
+            sb.append(Character.toUpperCase(n.charAt(0)));
+        }
+        if (Character.isLowerCase(template.charAt(0))) {
+            sb.append(Character.toLowerCase(n.charAt(0)));
+        }
+        if (n.length() > 1) {
+            sb.append(n.substring(1, n.length()));
+        }
+        
+        return sb.toString();
+    }
 }
