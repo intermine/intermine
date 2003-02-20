6d4677e2f4a857f04b4ef655e8f429935e2996be
andy <andy>
2003-02-20 11:07:08 +0000

    Added constructor

--- intermine/src/java/org/intermine/objectstore/query/Results.java
+++ intermine/src/java/org/intermine/objectstore/query/Results.java
@@ -5,6 +5,7 @@ import java.util.AbstractList;
 import java.util.ArrayList;
 
 import org.flymine.objectstore.ObjectStore;
+import org.flymine.objectstore.ObjectStoreException;
 
 /**
  * @author Mark Woodbridge
@@ -19,32 +20,59 @@ public class Results extends AbstractList
     protected List resultsRows = new ArrayList(); // @element-type ResultRow
 
     /**
+     * No argument constructor for testing purposes
+     *
+     */
+    protected Results() {
+    }
+
+    /**
+     * Constructor for a Results object
+     *
+     * @param q the Query that produces this Results
+     * @param os the ObjectStore that can be used to get results rows from
+     */
+     public Results(Query q, ObjectStore os) {
+         if ((q == null) || (os == null)) {
+             throw new NullPointerException("Arguments must not be null");
+         }
+         this.query = q;
+         this.os = os;
+     }
+
+    /**
+     * Returns a range of rows of results
+     *
      * @param start the start index
      * @param end the end index
-     * @return the relevant objects
-     */    
+     * @return the relevant ResultRows as a List
+     */
     public List range(int start, int end) {
         return null;
     }
 
     /**
+     * Fetch rows from the underlying ObjectStore
+     *
      * @param start the start index
      * @param end the end index
-     */    
-    public void fetchRows(int start, int end) {
+     * @throws ObjectStoreException if an error occurs in the underlying ObjectStore
+     */
+    protected void fetchRows(int start, int end) throws ObjectStoreException {
+        resultsRows.add(os.execute(query, start, end));
     }
-    
+
     /**
      * @param index of the object required
      * @return the relevant object
-     */    
+     */
     public Object get(int index) {
         return null;
     }
-    
+
     /**
      * @return the number of rows in this results item
-     */    
+     */
     public int size() {
         return 0;
     }
