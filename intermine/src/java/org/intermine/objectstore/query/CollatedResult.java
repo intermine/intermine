3fe85764ea87a6078a7dd0ccc352650b9c8d6988
andy <andy>
2003-02-26 16:37:57 +0000

    Changed to extend Results, thus enabling the storage of a Query that is executed to retrieve the matches

--- intermine/src/java/org/intermine/objectstore/query/CollatedResult.java
+++ intermine/src/java/org/intermine/objectstore/query/CollatedResult.java
@@ -1,31 +1,48 @@
 package org.flymine.objectstore.query;
 
-import java.util.List;
-import java.util.ArrayList;
+import org.flymine.objectstore.*;
 
 /**
- * A CollatedResult wraps the result of a Query with its associated objects.
- * In the case of an aggregate this would be (e.g.) the count, along with the objects counted.
+ * A CollatedResult wraps the result of a Query along with another
+ * Query that can be used by the ObjectStore to retrieve all the
+ * objects that this result refers to.
+ *
+ * In the case of an aggregate this would be (e.g.) the count, along
+ * with a query to retrieve the objects counted.
+ *
+ * CollatedResult is a Results object, which itself is a List, so we have all the
+ * usual methods of interacting with the Results.
  *
  * @author Mark Woodbridge
  * @author Richard Smith
+ * @author Andrew Varley
  */
-public class CollatedResult
+public class CollatedResult extends Results
 {
     private Object result;
-    private List matches = new ArrayList(); // @element-type SingletonResult
 
     /**
+     * Construct a collated result
+     *
+     * @param result the result
+     * @param q the Query to use to retrieve matches
+     * @param os the ObjectStore to use to run the Query
+     */
+    public CollatedResult(Object result, Query q, ObjectStore os) {
+        super(q, os);
+        if (result == null) {
+            throw new NullPointerException("result must not be null");
+        }
+        this.result = result;
+    }
+
+    /**
+     * Get the result
+     *
      * @return the result
-     */    
+     */
     public Object getResult() {
         return result;
     }
-    
-    /**
-     * @return the objects that "match" the result
-     */    
-    public List getMatches() {
-        return matches;
-    }
+
 }
