c7c2136b3966a5eba50182ee9d10c43edd55807e
mark <mark>
2003-03-04 15:13:33 +0000

    now extends PersistenceBrokerFlyMineImplTest

--- intermine/src/test/org/intermine/objectstore/ojb/ObjectStoreOjbImplTest.java
+++ intermine/src/test/org/intermine/objectstore/ojb/ObjectStoreOjbImplTest.java
@@ -5,20 +5,18 @@ import junit.framework.TestCase;
 import org.flymine.objectstore.*;
 import org.flymine.sql.DatabaseFactory;
 import org.flymine.sql.Database;
+import org.flymine.objectstore.query.Query;
+import org.flymine.objectstore.query.QueryClass;
 
-public class ObjectStoreOjbImplTest extends TestCase
+import org.flymine.model.testmodel.*;
+
+public class ObjectStoreOjbImplTest extends PersistenceBrokerFlyMineImplTest
 {
     public ObjectStoreOjbImplTest(String arg) {
         super(arg);
     }
 
-    private Database db;
-
-    public void setUp() throws Exception {
-        db = DatabaseFactory.getDatabase("db.unittest");
-    }
-
-
+    private ObjectStore os;
 
     public void testNullConstructor() throws Exception {
         try {
@@ -29,10 +27,18 @@ public class ObjectStoreOjbImplTest extends TestCase
     }
 
     public void testGetInstance() throws Exception {
-        ObjectStore os1 = ObjectStoreOjbImpl.getInstance(db);
+        os = ObjectStoreOjbImpl.getInstance(db);
         ObjectStore os2 = ObjectStoreOjbImpl.getInstance(db);
-        assertSame(os1, os2);
+        assertSame(os, os2);
     }
 
-
+    public void testQuery() throws Exception {
+        Query query = new Query();
+        QueryClass company = new QueryClass(Company.class);
+        query.addFrom(company);
+        query.addToSelect(company);
+        //assertEquals(2, os.execute(query).size());
+        //assertEquals(companys.get(0), os.execute(query).get(0));
+        assertTrue(true);
+    }
 }
