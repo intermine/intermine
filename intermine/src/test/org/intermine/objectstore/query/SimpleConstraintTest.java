9148a019887f0a1ebfda393ad2d9850f59172100
rnsmith <rns>
2003-03-11 10:42:29 +0000

    more tests for type-checking, test for getRealType()

--- intermine/src/test/org/intermine/objectstore/query/SimpleConstraintTest.java
+++ intermine/src/test/org/intermine/objectstore/query/SimpleConstraintTest.java
@@ -34,19 +34,15 @@ public class SimpleConstraintTest extends TestCase {
         try {
             constraint = new SimpleConstraint(qeNum1, SimpleConstraint.EQUALS, null);
             fail("Expected: NullPointerException");
+        } catch (NullPointerException e) {
         }
-        catch (NullPointerException e) {
-        }
-
     }
-
 
     public void testNullSingleConstrctor() throws Exception {
         try {
             constraint = new SimpleConstraint(null, SimpleConstraint.IS_NULL);
             fail("Expected: NullPointerException");
-        }
-        catch (NullPointerException e) {
+        } catch (NullPointerException e) {
         }
     }
 
@@ -88,5 +84,69 @@ public class SimpleConstraintTest extends TestCase {
     }
 
 
+    public void testDualWrongTypeString() throws Exception {
+        try {
+            constraint = new SimpleConstraint(qeStr1, SimpleConstraint.GREATER_THAN, qeStr1);
+            fail ("Expected: InvalidArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            constraint = new SimpleConstraint(qeStr1, SimpleConstraint.GREATER_THAN_EQUALS, qeStr1);
+            fail ("Expected: InvalidArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+                try {
+            constraint = new SimpleConstraint(qeStr1, SimpleConstraint.LESS_THAN, qeStr1);
+            fail ("Expected: InvalidArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            constraint = new SimpleConstraint(qeStr1, SimpleConstraint.LESS_THAN_EQUALS, qeStr1);
+            fail ("Expected: InvalidArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+
+    }
+
+    public void testDualWrongTypeNumber() throws Exception {
+        try {
+            constraint = new SimpleConstraint(qeNum1, SimpleConstraint.MATCHES, qeNum1);
+            fail ("Expected: InvalidArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            constraint = new SimpleConstraint(qeNum1, SimpleConstraint.DOES_NOT_MATCH, qeNum1);
+            fail ("Expected: InvalidArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+    // create negated SimpleConstraints and check getRealType gives correct answer
+    public void testGetRealType() throws Exception {
+        try {
+            SimpleConstraint c1 = new SimpleConstraint(qeNum1, SimpleConstraint.EQUALS, qeNum1, true);
+            assertEquals(c1.getRealType(), SimpleConstraint.NOT_EQUALS);
+            SimpleConstraint c2 = new SimpleConstraint(qeNum1, SimpleConstraint.NOT_EQUALS, qeNum1, true);
+            assertEquals(c2.getRealType(), SimpleConstraint.EQUALS);
+            SimpleConstraint c3 = new SimpleConstraint(qeNum1, SimpleConstraint.LESS_THAN, qeNum1, true);
+            assertEquals(c3.getRealType(), SimpleConstraint.GREATER_THAN_EQUALS);
+            SimpleConstraint c4 = new SimpleConstraint(qeNum1, SimpleConstraint.GREATER_THAN_EQUALS, qeNum1, true);
+            assertEquals(c4.getRealType(), SimpleConstraint.LESS_THAN);
+            SimpleConstraint c5 = new SimpleConstraint(qeNum1, SimpleConstraint.GREATER_THAN, qeNum1, true);
+            assertEquals(c5.getRealType(), SimpleConstraint.LESS_THAN_EQUALS);
+            SimpleConstraint c6 = new SimpleConstraint(qeNum1, SimpleConstraint.LESS_THAN_EQUALS, qeNum1, true);
+            assertEquals(c6.getRealType(), SimpleConstraint.GREATER_THAN);
+            SimpleConstraint c7 = new SimpleConstraint(qeStr1, SimpleConstraint.MATCHES, qeStr1, true);
+            assertEquals(c7.getRealType(), SimpleConstraint.DOES_NOT_MATCH);
+            SimpleConstraint c8 = new SimpleConstraint(qeStr1, SimpleConstraint.DOES_NOT_MATCH, qeStr1, true);
+            assertEquals(c8.getRealType(), SimpleConstraint.MATCHES);
+            SimpleConstraint c9 = new SimpleConstraint(qeNum1, SimpleConstraint.IS_NULL, true);
+            assertEquals(c9.getRealType(), SimpleConstraint.IS_NOT_NULL);
+            SimpleConstraint c10 = new SimpleConstraint(qeNum1, SimpleConstraint.IS_NOT_NULL, true);
+            assertEquals(c10.getRealType(), SimpleConstraint.IS_NULL);
+        } finally {
+        }
+
+    }
 
 }
