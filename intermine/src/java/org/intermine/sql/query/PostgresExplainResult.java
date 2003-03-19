351926036e4a5be808811b5d3a8e233b1d543c4d
rnsmith <rns>
2003-03-19 13:49:22 +0000

    added constructor that takes a PreparedStatement

--- intermine/src/java/org/intermine/sql/query/PostgresExplainResult.java
+++ intermine/src/java/org/intermine/sql/query/PostgresExplainResult.java
@@ -51,6 +51,38 @@ public class PostgresExplainResult extends ExplainResult
     }
 
     /**
+     * Constructs an instance of ExplainResult given a PreparedStatement
+     * object.  Assumes that sql string already has EXPLAIN at beginning
+     *
+     * @param stmt the PreparedStatement to be explained
+     * @throws SQLException if the query cannot be explained by that database
+     */
+    public PostgresExplainResult(PreparedStatement stmt) throws SQLException {
+        if (stmt == null) {
+            throw (new NullPointerException("PreparedStatement argument cannot be null"));
+        }
+
+        Connection database = stmt.getConnection();
+        if (database == null) {
+            throw (new NullPointerException("Failed to retrive Connection from PreparedStatement"));
+        }
+
+        stmt.execute();
+
+        Exception warning = database.getWarnings();
+        if (warning == null) {
+            throw (new SQLException("Failed to get a valid warning string from database"));
+        }
+        database.clearWarnings();
+        String text = warning.toString();
+        try {
+            parseWarningString(text);
+        } catch (RuntimeException e) {
+            throw (new SQLException("Error parsing EXPLAIN string: " + e));
+        }
+    }
+
+    /**
      * Parses the warning returned by the database into statistics for the
      * ExplainResult object.
      *
