de008472c610fd5be618cefc9b0f424478901900
mark <mark>
2003-03-19 17:15:30 +0000

    perform configuration using Database object

--- intermine/src/java/org/intermine/objectstore/ojb/PersistenceBrokerFactoryFlyMineImpl.java
+++ intermine/src/java/org/intermine/objectstore/ojb/PersistenceBrokerFactoryFlyMineImpl.java
@@ -1,9 +1,18 @@
 package org.flymine.objectstore.ojb;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import org.apache.ojb.broker.PBKey;
-import org.apache.ojb.broker.ta.PersistenceBrokerFactoryDefaultImpl;
 import org.apache.ojb.broker.PersistenceBroker;
 import org.apache.ojb.broker.PBFactoryException;
+import org.apache.ojb.broker.ta.PersistenceBrokerFactoryDefaultImpl;
+import org.apache.ojb.broker.metadata.MetadataManager;
+import org.apache.ojb.broker.metadata.ConnectionRepository;
+import org.apache.ojb.broker.metadata.JdbcConnectionDescriptor;
+//import org.apache.ojb.broker.metadata.DescriptorRepository;
+
+import org.flymine.sql.Database;
 
 /**
  * Extension of PersistenceBrokerFactoryDefaultImpl to return the right PersistenceBroker
@@ -12,10 +21,64 @@ import org.apache.ojb.broker.PBFactoryException;
  */
 public class PersistenceBrokerFactoryFlyMineImpl extends PersistenceBrokerFactoryDefaultImpl
 {
+    private Map pbKeys = new HashMap();
+
     /**
+     * This version of createPersistenceBroker forces use of OJB.properties
      * @see PersistenceBrokerFactoryIF#createPersistenceBroker
      */
     public PersistenceBroker createPersistenceBroker(PBKey pbKey) throws PBFactoryException {
         return createNewBrokerInstance(pbKey);
+    }
+
+    /**
+     * Create a FlyMine persistence broker from a database object
+     *
+     * @param db the database object
+     * @return a FlyMine PersistenceBroker
+     * @throws PBFactoryException if there is a problem creating the broker
+     */
+    public PersistenceBrokerFlyMineImpl createPersistenceBroker(Database db)
+        throws PBFactoryException {
+        if (db == null) {
+            throw new PBFactoryException("Database is null");
+        }
+
+        PBKey key = (PBKey) pbKeys.get(db);
+        if (key == null) {
+            MetadataManager mdm = MetadataManager.getInstance();
+            ConnectionRepository cr = mdm.connectionRepository();
+            JdbcConnectionDescriptor jcd = new JdbcConnectionDescriptor();
+            
+            String alias = "jcdAlias";
+            
+            jcd.setJcdAlias(alias);
+            //jcd.setDefaultConnection(true);
+            
+            jcd.setDriver(db.getDriver());
+            jcd.setDbms(db.getPlatform());
+            
+            String[] s = db.getURL().split(":");
+            jcd.setProtocol(s[0]);
+            jcd.setSubProtocol(s[1]);
+            jcd.setDbAlias(s[2]);
+            
+            cr.addDescriptor(jcd);
+
+//         String repositoryFile = "repository_" + db.getModel() + ".xml";
+//         DescriptorRepository dr = mdm.readDescriptorRepository(repositoryFile);
+//         mdm.mergeDescriptorRepository(dr);
+
+            key = new PBKey(alias, db.getUser(), db.getPassword());
+            pbKeys.put(db, key);
+        }
+
+        PersistenceBroker pb = createPersistenceBroker(key);
+        
+        if (!(pb instanceof PersistenceBrokerFlyMineImpl)) {
+            throw new IllegalArgumentException("PersistenceBrokerClass is not set to "
+                                               + "PersistenceBrokerFlyMineImpl in OJB.properties");
+        }
+        return (PersistenceBrokerFlyMineImpl) pb;
     }
 }
