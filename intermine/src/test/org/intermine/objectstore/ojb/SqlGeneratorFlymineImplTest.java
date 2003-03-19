e5da0be9cb8fc1bc5a32a2a678d1702d2099beff
rnsmith <rns>
2003-03-19 14:19:21 +0000

    test that LIMIT and OFFSET are appended correctly

--- intermine/src/test/org/intermine/objectstore/ojb/SqlGeneratorFlymineImplTest.java
+++ intermine/src/test/org/intermine/objectstore/ojb/SqlGeneratorFlymineImplTest.java
@@ -12,9 +12,23 @@ import org.flymine.model.testmodel.Company;
 
 public class SqlGeneratorFlymineImplTest extends TestCase
 {
+    Database db;
+    ObjectStoreOjbImpl os;
+    PersistenceBroker broker;
+    DescriptorRepository dr;
+    SqlGeneratorFlymineImpl gen;
+
 
     public SqlGeneratorFlymineImplTest(String arg1) {
         super(arg1);
+    }
+
+    public void setUp() throws Exception {
+        db = DatabaseFactory.getDatabase("db.unittest");
+        os = ObjectStoreOjbImpl.getInstance(db);
+        broker = os.getPersistenceBroker();
+        dr = broker.getDescriptorRepository();
+        gen = (SqlGeneratorFlymineImpl) broker.serviceSqlGenerator();
     }
 
     public void testPreparedSelectStatement() throws Exception {
@@ -28,9 +42,21 @@ public class SqlGeneratorFlymineImplTest extends TestCase
         QueryClass qc1 = new QueryClass(Company.class);
         q.addToSelect(qc1);
         q.addFrom(qc1);
-        FlymineSqlSelectStatement s1 = new FlymineSqlSelectStatement(q, dr, 0, 10000);
-        assertEquals(s1.getStatement(), gen.getPreparedSelectStatement(q, dr, 0, 10000));
+        FlymineSqlSelectStatement s1 = new FlymineSqlSelectStatement(q, dr);
+        assertEquals(s1.getStatement(), gen.getPreparedSelectStatement(q, dr, 0, 0));
     }
 
+
+    public void testLimitAndOffset() throws Exception {
+        Query q = new Query();
+        QueryClass qc1 = new QueryClass(Company.class);
+        q.addToSelect(qc1);
+        q.addFrom(qc1);
+        FlymineSqlSelectStatement s1 = new FlymineSqlSelectStatement(q, dr);
+        String string1 = s1.getStatement();
+        string1 += " LIMIT 10300 OFFSET 47";
+
+        assertEquals(string1, gen.getPreparedSelectStatement(q, dr, 47, 10300));
+    }
 
 }
