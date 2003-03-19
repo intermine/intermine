97dc4a528318b032e46b3aea05512292e0d7d606
mark <mark>
2003-03-19 17:15:57 +0000

    initial revision

---
+++ intermine/src/java/org/intermine/objectstore/ObjectStoreWriter.java
@@ -0,0 +1,54 @@
+package org.flymine.objectstore;
+
+/**
+ * Store, update, and delete objects
+ *
+ * @author Mark Woodbridge
+ */
+public interface ObjectStoreWriter
+{
+    /**
+     * Store an object in this ObjectStore
+     *
+     * @param o the object to store
+     * @throws ObjectStoreException if an error occurs during storage of the object
+     */
+    public void store(Object o) throws ObjectStoreException;
+    
+    /**
+     * Delete an object from this ObjectStore
+     *
+     * @param o the object to delete
+     * @throws ObjectStoreException if an error occurs during deletion of the object
+     */
+    public void delete(Object o) throws ObjectStoreException;
+
+    /**
+     * Check whether the ObjectStore is performing a transaction
+     *
+     * @return true if in a transaction, false otherwise
+     * @throws ObjectStoreException if an error occurs the check
+     */
+    public boolean isInTransaction() throws ObjectStoreException;
+
+    /**
+     * Request that the ObjectStore begins a transaction
+     *
+     * @throws ObjectStoreException if a transaction is in progress, or is aborted
+     */
+    public void beginTransaction() throws ObjectStoreException;
+
+    /**
+     * Request that the ObjectStore commits and closes the transaction
+     *
+     * @throws ObjectStoreException if a transaction is not in progress, or is aborted
+     */
+    public void commitTransaction() throws ObjectStoreException;
+
+    /**
+     * Request that the ObjectStore aborts and closes the transaction
+     *
+     * @throws ObjectStoreException if a transaction is not in progress
+     */
+    public void abortTransaction() throws ObjectStoreException;
+}
---
+++ intermine/src/java/org/intermine/objectstore/ojb/ObjectStoreWriterOjbImpl.java
@@ -0,0 +1,116 @@
+package org.flymine.objectstore.ojb;
+
+import org.apache.ojb.broker.PersistenceBroker;
+import org.apache.ojb.broker.PersistenceBrokerException;
+import org.apache.ojb.broker.TransactionInProgressException;
+import org.apache.ojb.broker.TransactionNotInProgressException;
+import org.apache.ojb.broker.TransactionAbortedException;
+import org.apache.ojb.broker.ta.PersistenceBrokerFactoryFactory;
+
+import org.flymine.sql.Database;
+import org.flymine.objectstore.ObjectStoreWriter;
+import org.flymine.objectstore.ObjectStoreException;
+
+/**
+ * Implementation of ObjectStoreWriter that uses OJB as its underlying store
+ *
+ * @author Mark Woodbridge
+ */
+public class ObjectStoreWriterOjbImpl implements ObjectStoreWriter
+{
+    protected PersistenceBroker pb = null;
+
+    /**
+     * No argument constructor for testing purposes
+     */
+    protected ObjectStoreWriterOjbImpl() {
+    }
+
+    /**
+     * Constructs an ObjectStoreWriterOjbImpl interfacing with an OJB instance
+     * NB There can be multiple ObjectStoreWriters per Database, each holding a PersistenceBroker
+     *
+     * @param db the database in which the model resides
+     * @throws ObjectStoreException if there is any problem with the underlying OJB instance
+     */
+    public ObjectStoreWriterOjbImpl(Database db) throws ObjectStoreException {
+        pb = ((PersistenceBrokerFactoryFlyMineImpl) PersistenceBrokerFactoryFactory.instance())
+            .createPersistenceBroker(db);
+    }
+
+    /**
+     * @see ObjectStoreWriter#store
+     */
+    public void store(Object o) throws ObjectStoreException {
+        try {
+            pb.store(o);
+        } catch (Exception e) {
+            throw new ObjectStoreException(e);
+        }
+    }
+    
+    /**
+     * @see ObjectStoreWriter#delete
+     */
+    public void delete(Object o) throws ObjectStoreException {
+        try {
+            pb.delete(o);
+        } catch (Exception e) {
+            throw new ObjectStoreException(e);
+        }
+    }
+
+    /**
+     * @see ObjectStoreWriter#isInTransaction
+     */
+    public boolean isInTransaction() throws ObjectStoreException {
+        boolean result;
+        try {
+            return pb.isInTransaction();
+        } catch (PersistenceBrokerException e) {
+            throw new ObjectStoreException(e);
+        }
+    }
+
+    /**
+     * @see ObjectStoreWriter#beginTransaction
+     */
+    public void beginTransaction() throws ObjectStoreException {
+        try {
+            pb.beginTransaction();
+        } catch (TransactionInProgressException e) {
+            throw new ObjectStoreException("Cannot begin transaction: "
+                                           + "a transaction is already in progress");
+        } catch (TransactionAbortedException e) {
+            throw new ObjectStoreException("Cannot begin transaction: "
+                                           + "transaction aborted");
+        }
+    }
+
+    /**
+     * @see ObjectStoreWriter#commitTransaction
+     */
+    public void commitTransaction() throws ObjectStoreException {
+        try {
+            pb.commitTransaction();
+        } catch (TransactionNotInProgressException e) {
+            throw new ObjectStoreException("Cannot commit transaction: "
+                                           + "there is no transaction is in progress");
+        } catch (TransactionAbortedException e) {
+            throw new ObjectStoreException("Cannot abort transaction: "
+                                           + "transaction aborted");
+        }
+    }
+    
+    /**
+     * @see ObjectStoreWriter#abortTransaction
+     */
+    public void abortTransaction() throws ObjectStoreException {
+        try {
+            pb.abortTransaction();
+        } catch (TransactionNotInProgressException e) {
+            throw new ObjectStoreException("Cannot commit transaction: "
+                                           + "there is no transaction is in progress");
+        }
+    }
+}
