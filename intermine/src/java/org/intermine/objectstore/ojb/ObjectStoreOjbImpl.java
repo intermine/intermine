91eb1b20cb98478c02ce8290ee5b8eeb72dd9e91
rnsmith <rns>
2003-03-19 14:27:54 +0000

    executeQuery() now runs EXPLAIN and runs checks before executing a query. Added estimate() methods

--- intermine/src/java/org/intermine/objectstore/ojb/ObjectStoreOjbImpl.java
+++ intermine/src/java/org/intermine/objectstore/ojb/ObjectStoreOjbImpl.java
@@ -4,15 +4,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Arrays;
+import java.util.Properties;
 
 import org.apache.ojb.broker.*;
 import org.apache.ojb.broker.metadata.*;
 
 import org.flymine.sql.Database;
+import org.flymine.sql.query.ExplainResult;
 import org.flymine.objectstore.*;
 import org.flymine.objectstore.query.Query;
 import org.flymine.objectstore.query.Results;
 import org.flymine.objectstore.query.ResultsRow;
+import org.flymine.util.PropertiesUtil;
 
 /**
  * Implementation of ObjectStore that uses OJB as its underlying store.
@@ -23,6 +26,10 @@ public class ObjectStoreOjbImpl implements ObjectStore
 {
     protected static Map instances = new HashMap();
     protected PersistenceBrokerFlyMineImpl pb = null;
+    protected long maxTime;
+    protected int maxRows;
+    protected int maxLimit;
+    protected int maxOffset;
 
     /**
      * No argument constructor for testing purposes
@@ -70,6 +77,13 @@ public class ObjectStoreOjbImpl implements ObjectStore
                                                + "PersistenceBrokerFlyMineImpl to be "
                                                + "returned from PersistenceBrokerFactory");
         }
+
+        Properties props = PropertiesUtil.getPropertiesStartingWith("os.query");
+        props = PropertiesUtil.stripStart("os.query", props);
+        maxRows = Integer.parseInt((String) props.get("max-rows"));
+        maxTime = Long.parseLong((String) props.get("max-time"));
+        maxLimit = Integer.parseInt((String) props.get("max-limit"));
+        maxOffset = Integer.parseInt((String) props.get("max-offset"));
     }
 
     /**
@@ -96,6 +110,7 @@ public class ObjectStoreOjbImpl implements ObjectStore
                 instances.put(db, new ObjectStoreOjbImpl(db));
             }
         }
+
         return (ObjectStoreOjbImpl) instances.get(db);
     }
 
@@ -122,11 +137,63 @@ public class ObjectStoreOjbImpl implements ObjectStore
      * @throws ObjectStoreException if an error occurs during the running of the Query
      */
     public List execute(Query q, int start, int end) throws ObjectStoreException {
-        List res = pb.execute(q, start, end - start + 1);
+        // check limit and offset are valid
+        int limit = (end - start) + 1;
+        if (start > maxOffset) {
+            throw (new ObjectStoreException("start parameter (" + start
+                                            + ") is greater than permitted maximum ("
+                                            + maxOffset + ")"));
+        }
+        if (limit > maxLimit) {
+            throw (new ObjectStoreException("number of rows required (" + limit
+                                            + ") is greater than permitted maximum ("
+                                            + maxLimit + ")"));
+        }
+
+        ExplainResult explain = pb.explain(q, start, end);
+
+        if (explain.getTime() > maxTime) {
+            throw (new ObjectStoreException("Estimated time to run query(" + explain.getTime()
+                                            + ") greater than permitted maximum ("
+                                            + maxTime + ")"));
+        }
+        if (explain.getRows() > maxRows) {
+            throw (new ObjectStoreException("Estimated number of rows (" + explain.getRows()
+                                            + ") greater than permitted maximum ("
+                                            + maxRows + ")"));
+        }
+
+        List res = pb.execute(q, start, limit);
         for (int i = 0; i < res.size(); i++) {
             res.set(i, new ResultsRow(Arrays.asList((Object[]) res.get(i))));
         }
         return res;
     }
 
+    /**
+     * Runs an EXPLAIN on the query without ant LIMIT or OFFSET.
+     *
+     * @param q the query to estimate rows for
+     * @return parsed results of EXPLAIN
+     * @throws ObjectStoreException if an error occurs explining the query
+     */
+    public ExplainResult estimate(Query q) throws ObjectStoreException {
+        return pb.explain(q, 0, 0);
+    }
+
+    /**
+     * Runs an EXPLAIN for the given query with specified start and end parameters.  This
+     * gives estimated time for a single 'page' of the query.
+     *
+     * @param q the query to explain
+     * @param start first row required, numbered from zero
+     * @param end the number of the last row required, numbered from zero
+     * @return parsed results of EXPLAIN
+     * @throws ObjectStoreException if an error occurs explining the query
+     */
+    public ExplainResult estimate(Query q, int start, int end) throws ObjectStoreException {
+        return pb.explain(q, start, end);
+    }
+
 }
+
