0c062bc9edbde6bee2e5c10f47826ad13b77dc66
rnsmith <rns>
2003-03-24 09:39:40 +0000

    added tests for equals() and hashCode() methods

--- intermine/src/test/org/intermine/objectstore/query/ConstraintSetTest.java
+++ intermine/src/test/org/intermine/objectstore/query/ConstraintSetTest.java
@@ -58,4 +58,68 @@ public class ConstraintSetTest extends TestCase {
         }
     }
 
+
+    public void testEqual() throws Exception {
+        ConstraintSet cs1 = new ConstraintSet(ConstraintSet.AND);
+        ConstraintSet cs2 = new ConstraintSet(ConstraintSet.AND);
+        ConstraintSet cs3 = new ConstraintSet(ConstraintSet.AND);
+        ConstraintSet cs4 = new ConstraintSet(ConstraintSet.AND);
+        ConstraintSet cs5 = new ConstraintSet(ConstraintSet.OR);
+        ConstraintSet cs6 = new ConstraintSet(ConstraintSet.AND, true);
+
+        cs1.addConstraint(sc1).addConstraint(sc2);
+        cs2.addConstraint(sc1).addConstraint(sc2);
+        assertEquals(cs1, cs1);
+        assertEquals(cs1, cs2);
+
+        // cs3 has same two constraints but in different oreder, should still be equal
+        cs3.addConstraint(sc2).addConstraint(sc1);
+        assertEquals(cs1, cs3);
+
+        cs4.addConstraint(sc1);
+        assertEquals(cs4, cs4);
+        assertTrue("Expected cs1 and cs4 to not be equal", !cs1.equals(cs4));
+
+        // cs5 is AND, cs4 is OR
+        cs5.addConstraint(sc1);
+        assertTrue("Expected cs4 and cs5 to not be equal", !cs4.equals(cs5));
+
+        // cs6 is negated, cs4 is not
+        cs6.addConstraint(sc1);
+        assertTrue("Expected cs4 and cs6 to not be equal", !cs4.equals(cs6));
+
+    }
+
+    public void testHashCode() throws Exception {
+        ConstraintSet cs1 = new ConstraintSet(ConstraintSet.AND);
+        ConstraintSet cs2 = new ConstraintSet(ConstraintSet.AND);
+        ConstraintSet cs3 = new ConstraintSet(ConstraintSet.AND);
+        ConstraintSet cs4 = new ConstraintSet(ConstraintSet.AND);
+        ConstraintSet cs5 = new ConstraintSet(ConstraintSet.OR);
+        ConstraintSet cs6 = new ConstraintSet(ConstraintSet.AND, true);
+
+        cs1.addConstraint(sc1).addConstraint(sc2);
+        cs2.addConstraint(sc1).addConstraint(sc2);
+        assertEquals(cs1.hashCode(), cs1.hashCode());
+        assertEquals(cs1.hashCode(), cs2.hashCode());
+
+        // cs3 has same two constraints but in different oreder, should still be equal
+        cs3.addConstraint(sc2).addConstraint(sc1);
+        assertEquals(cs1.hashCode(), cs3.hashCode());
+
+        cs4.addConstraint(sc1);
+        assertEquals(cs4.hashCode(), cs4.hashCode());
+        assertTrue("Expected cs1.hashCode() and cs4.hashCode() to not be equal", cs1.hashCode() != cs4.hashCode());
+
+        // cs5 is AND, cs4 is OR
+        cs5.addConstraint(sc1);
+        assertTrue("Expected cs4.hashCode() and cs5.hashCode() to not be equal", cs4.hashCode() != cs5.hashCode());
+
+        // cs6 is negated, cs4 is not
+        cs6.addConstraint(sc1);
+        assertTrue("Expected cs4.hashCode() and cs6.hashCode() to not be equal", cs4.hashCode() != cs6.hashCode());
+
+    }
+
+
 }
