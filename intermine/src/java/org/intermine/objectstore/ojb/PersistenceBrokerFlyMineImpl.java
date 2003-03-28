28bcff1feaf45cd457373fabebc20fe63ff280e2
mark <mark>
2003-03-28 16:46:25 +0000

    overrides getReferencedObject to return proxies if required

--- intermine/src/java/org/intermine/objectstore/ojb/PersistenceBrokerFlyMineImpl.java
+++ intermine/src/java/org/intermine/objectstore/ojb/PersistenceBrokerFlyMineImpl.java
@@ -1,16 +1,23 @@
 package org.flymine.objectstore.ojb;
 
+import org.apache.ojb.broker.PersistenceBrokerException;
+import org.apache.ojb.broker.Identity;
 import org.apache.ojb.broker.PBKey;
 import org.apache.ojb.broker.ta.PersistenceBrokerFactoryIF;
 import org.apache.ojb.broker.singlevm.PersistenceBrokerImpl;
+import org.apache.ojb.broker.metadata.ObjectReferenceDescriptor;
+import org.apache.ojb.broker.metadata.ClassDescriptor;
 
 import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 
-import org.flymine.objectstore.query.Query;
+import org.flymine.objectstore.proxy.LazyInitializer;
+import org.flymine.objectstore.proxy.LazyReference;
 import org.flymine.sql.query.ExplainResult;
 import org.flymine.sql.Database;
+
+import org.flymine.objectstore.query.*;
 
 /**
  * Extension of PersistenceBrokerImpl to allow execution of ObjectStore queries
@@ -19,6 +26,9 @@ import org.flymine.sql.Database;
  */
 public class PersistenceBrokerFlyMineImpl extends PersistenceBrokerImpl
 {
+    protected static final org.apache.log4j.Logger LOG = 
+        org.apache.log4j.Logger.getLogger(LazyInitializer.class);
+
     private Database database;
     
     /**
@@ -32,7 +42,7 @@ public class PersistenceBrokerFlyMineImpl extends PersistenceBrokerImpl
      * @see PersistenceBrokerImpl#PersistenceBrokerImpl
      */
     public PersistenceBrokerFlyMineImpl(PBKey key, PersistenceBrokerFactoryIF pbf)
-    {
+    {        
         super(key, pbf);
     }
 
@@ -63,6 +73,76 @@ public class PersistenceBrokerFlyMineImpl extends PersistenceBrokerImpl
      */
     public ExplainResult explain(Query query, int start, int limit) {
         return ((JdbcAccessFlymineImpl) serviceJdbcAccess()).explainQuery(query, start, limit);
+    }
+
+    /**
+     * Override getReferencedObject to use dynamically generated proxies
+     * 
+     * @param obj the object containing the reference
+     * @param rds the descriptor of the reference
+     * @param cld the descriptor of the referenced object
+     * @return the referenced object
+     */
+    protected Object getReferencedObject(Object obj, ObjectReferenceDescriptor rds, 
+                                         ClassDescriptor cld) {
+        Class referencedClass = rds.getItemClass();
+        Object[] pkVals = rds.getForeignKeyValues(obj, cld);
+        boolean allPkNull = true;
+        
+        for (int i = 0; i < pkVals.length; i++) {
+            if (pkVals[i] != null) {
+                allPkNull = false;
+                break;
+            }
+        }
+        
+        if (allPkNull) {
+            return null;
+        }
+        
+        if (rds.isLazy()) {
+            Object o = null;
+            try {
+                o = referencedClass.getDeclaredConstructor(new Class[] {})
+                    .newInstance(new Object[] {});
+            } catch (Exception e) {
+            }
+            Query query = new Query();
+            QueryClass company = new QueryClass(referencedClass);
+            QueryClass department = new QueryClass(obj.getClass());
+            query.addToSelect(company);
+            query.addFrom(company);
+            query.addFrom(department);
+            ClassConstraint cc1 = new ClassConstraint(department, ClassConstraint.EQUALS, obj);
+            String[] s = obj.getClass().getName().split("[.]");
+            QueryReference qr = null;
+            try {
+                qr = new QueryObjectReference(department, rds.getAttributeName());
+            } catch (Exception e) {
+                throw new PersistenceBrokerException(e);
+            }
+            ContainsConstraint cc2 = 
+                new ContainsConstraint(qr, ContainsConstraint.CONTAINS, company);
+            ConstraintSet cs = new ConstraintSet(ConstraintSet.AND);            
+            cs.addConstraint(cc1);
+            cs.addConstraint(cc2);
+            query.setConstraint(cs);
+            //TODO this pkVals stuff is a temporary measure until .equals is sensible
+            return (LazyReference) 
+                LazyInitializer.getDynamicProxy(referencedClass, query, (Integer) pkVals[0]);
+        }
+        
+        Class referencedProxy = rds.getItemProxyClass();
+        if (referencedProxy != null) {
+            try {
+                return referencedProxy.getDeclaredConstructor(new Class[] {})
+                    .newInstance(new Object[] {});
+            } catch (Exception e) {
+                throw new PersistenceBrokerException(e);
+            }
+        } else {
+            return getObjectByIdentity(new Identity(referencedClass, pkVals));
+        }
     }
 
     /**
