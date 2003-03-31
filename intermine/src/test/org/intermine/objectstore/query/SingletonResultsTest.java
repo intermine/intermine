7b4fb6451cdf5a4527c5779ad6d5e5cbb01ef877
andy <andy>
2003-03-31 12:09:47 +0000

    Test class for SingletonResults

---
+++ intermine/src/test/org/intermine/objectstore/query/SingletonResultsTest.java
@@ -0,0 +1,133 @@
+package org.flymine.objectstore.query;
+
+import junit.framework.TestCase;
+
+import java.util.List;
+
+import org.flymine.objectstore.dummy.ObjectStoreDummyImpl;
+import org.flymine.model.testmodel.Department;
+import org.flymine.model.testmodel.Employee;
+
+public class SingletonResultsTest extends TestCase
+{
+    public SingletonResultsTest(String arg1) {
+        super(arg1);
+    }
+
+    private ObjectStoreDummyImpl os;
+    private Query q;
+
+    public void setUp() throws Exception {
+
+        // Set up a dummy ObjectStore with 10 rows to return
+        os = new ObjectStoreDummyImpl();
+        os.setResultsSize(10);
+
+        for (int i = 0; i < 10; i++) {
+            ResultsRow row = new ResultsRow();
+            Employee e = new Employee();
+            e.setName("" + i);
+            row.add(e);
+            os.addRow(row);
+        }
+
+        q = new Query();
+        QueryClass qc1 = new QueryClass(Department.class);
+
+        q.addToSelect(qc1);
+        q.addFrom(qc1);
+
+    }
+
+    public void testConstructMultiColumnQuery() throws Exception {
+        Query q = new Query();
+        QueryClass qc1 = new QueryClass(Department.class);
+        QueryClass qc2 = new QueryClass(Employee.class);
+
+        q.addToSelect(qc1);
+        q.addToSelect(qc2);
+        q.addFrom(qc1);
+
+        try {
+            Results res = new SingletonResults(q, os);
+            fail("Expected: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+
+    }
+
+    public void testConstructNonQueryClassQuery() throws Exception {
+        Query q = new Query();
+        QueryClass qc1 = new QueryClass(Department.class);
+        QueryField qf1 = new QueryField(qc1, "name");
+
+        q.addToSelect(qf1);
+        q.addFrom(qc1);
+
+        try {
+            Results res = new SingletonResults(q, os);
+            fail("Expected: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+
+    }
+
+    public void testConstructNullQuery() throws Exception {
+        try {
+            Results res = new SingletonResults(null, new ObjectStoreDummyImpl());
+            fail("Expected: NullPointerException");
+        }
+        catch (NullPointerException e) {
+        }
+    }
+
+    public void testConstructNullObjectStore() throws Exception {
+        try {
+            Results res = new SingletonResults(new Query(), null);
+            fail("Expected: NullPointerException");
+        }
+        catch (NullPointerException e) {
+        }
+    }
+
+    public void testGet() throws Exception {
+        Results res = new SingletonResults(q, os);
+
+        assertTrue(res.get(0) instanceof Employee);
+        assertEquals("0", ((Employee) res.get(0)).getName());
+
+    }
+
+    public void testRange() throws Exception {
+        Results res = new SingletonResults(q, os);
+        List objs = res.range(0,5);
+
+        for (int i = 0; i <= 5; i++) {
+            assertTrue(objs.get(i) instanceof Employee);
+            assertEquals("" + i, ((Employee) objs.get(i)).getName());
+        }
+    }
+
+
+    public void testGetOutOfBounds() throws Exception {
+        Results res = new SingletonResults(q, os);
+        try {
+            res.get(10);
+            fail("Expected IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException e) {
+        }
+    }
+
+    public void testRangeOutOfBounds() throws Exception {
+        Results res = new SingletonResults(q, os);
+        res.setBatchSize(5);
+        try {
+            res.range(6,11);
+            fail("Expected IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException e) {
+        }
+    }
+
+}
