941868ecca9b0850b43c962380a19d9d8f95f58d
rnsmith <rns>
2003-04-01 13:18:22 +0000

    tests for LazyCollection

---
+++ intermine/src/test/org/intermine/objectstore/proxy/LazyCollectionTest.java
@@ -0,0 +1,62 @@
+package org.flymine.objectstore.proxy;
+
+import junit.framework.TestCase;
+
+import org.flymine.objectstore.query.Query;
+import org.flymine.objectstore.query.QueryClass;
+import org.flymine.objectstore.query.QueryValue;
+import org.flymine.model.testmodel.Department;
+import org.flymine.model.testmodel.Manager;
+
+public class LazyCollectionTest extends TestCase {
+
+    private LazyCollection col;
+
+
+    public LazyCollectionTest(String arg1) {
+        super(arg1);
+    }
+
+    public void setUp() {
+    }
+
+    public void testNullArg() throws Exception {
+        try {
+            col = new LazyCollection(null);
+            fail("Expected: NullPointerException");
+        } catch (NullPointerException e) {
+        }
+    }
+
+
+    public void testNotOneSelectItem() throws Exception {
+        Query q1 = new Query();
+        try {
+            col = new LazyCollection(q1);
+            fail("Expected: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+
+        QueryClass qc1 = new QueryClass(Department.class);
+        QueryClass qc2 = new QueryClass(Manager.class);
+        q1.addToSelect(qc1).addToSelect(qc2);
+        try {
+            col = new LazyCollection(q1);
+            fail("Expected: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+
+    public void testSelectNotAQueryClass() throws Exception {
+        Query q1 = new Query();
+        QueryValue qv1 = new QueryValue(new Integer(1234));
+        q1.addToSelect(qv1);
+        try {
+            col = new LazyCollection(q1);
+            fail("Expected: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+}
