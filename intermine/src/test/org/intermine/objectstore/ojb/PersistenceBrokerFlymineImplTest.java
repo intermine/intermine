5dfeb542302640eac1e4f770a0d9b6577b4f80d8
rnsmith <rns>
2003-04-01 13:31:32 +0000

    tests for LazyCollection creation in PersistenceBrokerFlymineImpl

---
+++ intermine/src/test/org/intermine/objectstore/ojb/PersistenceBrokerFlymineImplTest.java
@@ -0,0 +1,143 @@
+package org.flymine.objectstore.ojb;
+
+import java.util.List;
+import java.util.Collection;
+import java.lang.reflect.Field;
+
+import junit.framework.TestCase;
+
+import org.apache.ojb.broker.*;
+import org.apache.ojb.broker.metadata.*;
+
+import org.flymine.sql.Database;
+import org.flymine.sql.DatabaseFactory;
+import org.flymine.objectstore.query.*;
+import org.flymine.objectstore.ojb.FlymineSqlSelectStatement;
+import org.flymine.objectstore.ojb.PersistenceBrokerFlyMineImpl;
+import org.flymine.objectstore.query.Query;
+import org.flymine.objectstore.proxy.LazyCollection;
+import org.flymine.model.testmodel.Department;
+import org.flymine.model.testmodel.Company;
+import org.flymine.model.testmodel.Employee;
+
+
+
+public class PersistenceBrokerFlymineImplTest extends TestCase {
+
+    Database db;
+    ObjectStoreOjbImpl os;
+    PersistenceBrokerFlyMineImpl broker;
+    DescriptorRepository dr;
+
+    public PersistenceBrokerFlymineImplTest(String arg1) {
+        super(arg1);
+    }
+
+    public void setUp() throws Exception {
+        db = DatabaseFactory.getDatabase("db.unittest");
+        os = ObjectStoreOjbImpl.getInstance(db);
+        dr = ObjectStoreOjbImpl.getInstance(db).getPersistenceBroker().getDescriptorRepository();
+        broker = (PersistenceBrokerFlyMineImpl) os.getPersistenceBroker();
+    }
+
+
+    // check that query to retrieve collection is correctly formed
+    public void testGetCollectionQuery() throws Exception {
+        broker.clearCache();
+        // need an object, its ClassDescriptor and CollectionDescriptor for one of its collections
+        Department dept = getDeptExampleObject();
+        ClassDescriptor cldDept = dr.getDescriptorFor(Department.class);
+        CollectionDescriptor codEmployees = cldDept.getCollectionDescriptorByName("employees");
+
+        Query q = broker.getCollectionQuery(dept, cldDept, codEmployees);
+        FlymineSqlSelectStatement stmt = new FlymineSqlSelectStatement(q, dr);
+
+        FieldDescriptor fld = cldDept.getFieldDescriptorByName("id");
+        Integer id = (Integer) fld.getPersistentField().get(dept);
+
+        assertEquals(stmt.getStatement(), "SELECT DISTINCT a1_.CLASS AS a1_CLASS, a1_.ID AS a1_ID, a1_.addressId AS a1_addressId, a1_.age AS a1_age, a1_.departmentId AS a1_departmentId, a1_.departmentThatRejectedMeId AS a1_departmentThatRejectedMeId, a1_.fullTime AS a1_fullTime, a1_.name AS a1_name, a1_.salary AS a1_salary, a1_.title AS a1_title FROM Employee AS a1_, Department AS a2_ WHERE ((a2_.ID = " + id.intValue() + ") AND (a2_.ID = a1_.departmentId)) ORDER BY a1_.ID");
+
+    }
+
+    // check that query to retrieve collection is correctly formed for many to many relationship
+    public void testGetCollectionQueryMtoN() throws Exception {
+        broker.clearCache();
+        // Many Contractors per Company, check query still formed correctly
+        Company comp = getCompExampleObject();
+        ClassDescriptor cldComp = dr.getDescriptorFor(Company.class);
+        CollectionDescriptor codContractors = cldComp.getCollectionDescriptorByName("contractors");
+
+        Query q = broker.getCollectionQuery(comp, cldComp, codContractors);
+        FlymineSqlSelectStatement stmt = new FlymineSqlSelectStatement(q, dr);
+
+        FieldDescriptor fld = cldComp.getFieldDescriptorByName("id");
+        Integer id = (Integer) fld.getPersistentField().get(comp);
+
+        assertEquals(stmt.getStatement(), "SELECT DISTINCT a1_.ID AS a1_ID, a1_.businessAddressId AS a1_businessAddressId, a1_.name AS a1_name, a1_.personalAddressId AS a1_personalAddressId FROM Contractor AS a1_, Company AS a2_, CompanyContractor AS ind_a2_a1_CompanyContractor_ WHERE ((a2_.ID = " + id.intValue() + ") AND (a2_.ID = ind_a2_a1_CompanyContractor_.companyId AND a1_.ID = ind_a2_a1_CompanyContractor_.contractorId)) ORDER BY a1_.ID");
+
+    }
+
+
+    // test that field of materialized object is set to a LazyCollection
+    public void testLazyCollectionField() throws Exception {
+        broker.clearCache();
+        Department dept = getDeptExampleObject();
+        ClassDescriptor cldDept = dr.getDescriptorFor(Department.class);
+        CollectionDescriptor codEmployees = cldDept.getCollectionDescriptorByName("employees");
+
+        // override anything in mapping file
+        codEmployees.setLazy(true);
+
+        broker.retrieveCollection(dept, cldDept, codEmployees, true);
+
+        Collection col = dept.getEmployees();
+        if (!(col instanceof LazyCollection))
+            fail("Expected Department.employees to be a LazyCollection");
+    }
+
+
+    // test that entire collection of employees has been materialized
+    public void testNotLazyCollectionField() throws Exception {
+        broker.clearCache();
+        Department dept = getDeptExampleObject();
+        ClassDescriptor cldDept = dr.getDescriptorFor(Department.class);
+        CollectionDescriptor codEmployees = cldDept.getCollectionDescriptorByName("employees");
+
+        // override anything in mapping file
+        codEmployees.setLazy(false);
+
+        broker.retrieveCollection(dept, cldDept, codEmployees, true);
+
+        Collection col = dept.getEmployees();
+        if (col instanceof LazyCollection)
+            fail("Expected Department.employees to be materialized but was a LazyCollection");
+
+    }
+
+
+    // set up a Department object with an id
+    private Department getDeptExampleObject() throws Exception {
+        Department dept = new Department();
+        Class deptClass = dept.getClass();
+        Field f = deptClass.getDeclaredField("id");
+        f.setAccessible(true);
+        f.set(dept, new Integer(1234));
+        dept.setName("DepartmentA1");
+
+        return dept;
+    }
+
+
+    // set up a Company object with an id
+    private Company getCompExampleObject() throws Exception {
+        Company comp = new Company();
+        Class compClass = comp.getClass();
+        Field f = compClass.getDeclaredField("id");
+        f.setAccessible(true);
+        f.set(comp, new Integer(1234));
+        comp.setName("DepartmentA1");
+
+        return comp;
+    }
+
+}
