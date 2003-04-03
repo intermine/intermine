5cf6cac937898e7cac97c3c34e08d0c235740f05
rnsmith <rns>
2003-04-03 14:09:11 +0000

    added comment about equals() method.

--- intermine/src/java/org/intermine/objectstore/query/QueryClass.java
+++ intermine/src/java/org/intermine/objectstore/query/QueryClass.java
@@ -2,6 +2,9 @@ package org.flymine.objectstore.query;
 
 /**
  * Represents the database extent of a Java class
+ * NOTE - No equals() method is defined for this class and none should be.
+ * org.flymine.objectstore.query.Query.equals relies on QueryClass using
+ * Object.equals() to prevent ambiguity.
  *
  * @author Mark Woodbridge
  * @author Richard Smith
@@ -9,22 +12,23 @@ package org.flymine.objectstore.query;
 public class QueryClass implements QueryNode, FromElement
 {
     private Class type;
-    
+
     /**
      * Constructs a QueryClass representing the specified Java class
      *
      * @param type the Java class
-     */    
+     */
     public QueryClass(Class type) {
         this.type = type;
     }
-    
+
     /**
      * Gets the Java class represented by this QueryClass
      *
      * @return the class name
-     */    
+     */
     public Class getType() {
         return type;
     }
+
 }
