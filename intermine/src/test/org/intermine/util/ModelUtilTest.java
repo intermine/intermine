f55e766640f399ab91ce16bf94c956cf7bc6d56e
andy <andy>
2003-04-11 16:08:04 +0000

    Added tests for checkKey() method

--- intermine/src/test/org/intermine/util/ModelUtilTest.java
+++ intermine/src/test/org/intermine/util/ModelUtilTest.java
@@ -21,4 +21,56 @@ public class ModelUtilTest extends TestCase
     public void testGetFieldTypeReference() {
         assertEquals(ModelUtil.REFERENCE, ModelUtil.getFieldType(Company.class, "address"));
     }
+
+
+    public void testCheckKeyValidObject() throws Exception {
+        Address address = new Address();
+        address.setAddress("Employee Street, BVille");
+
+        CEO employee = new CEO();
+        employee.setName("EmployeeB1");
+        employee.setFullTime(true);
+        employee.setAddress(address);
+        employee.setAge(40);
+        employee.setTitle("Mr.");
+        employee.setSalary(45000);
+
+        assertTrue(ModelUtil.checkKey(employee));
+    }
+
+    public void testCheckKeyInvalidObjectNoReference() throws Exception {
+
+        CEO employee = new CEO();
+        employee.setName("EmployeeB1");
+        employee.setFullTime(true);
+        employee.setAge(40);
+        employee.setTitle("Mr.");
+        employee.setSalary(45000);
+
+        assertFalse(ModelUtil.checkKey(employee));
+    }
+
+    public void testCheckKeyInvalidObjectNoAttribute() throws Exception {
+        Address address = new Address();
+        address.setAddress("Employee Street, BVille");
+
+        CEO employee = new CEO();
+        employee.setFullTime(true);
+        employee.setAddress(address);
+        employee.setAge(40);
+        employee.setTitle("Mr.");
+        employee.setSalary(45000);
+
+        assertFalse(ModelUtil.checkKey(employee));
+                    }
+
+    public void testCheckKeyNullObject() throws Exception {
+        try {
+            ModelUtil.checkKey(null);
+            fail ("Expected: NullPointerException");
+        } catch (NullPointerException e) {
+        }
+    }
+
+
 }
