33c0de22c9e1da7afccebdd6f3892cbe267c9314
rnsmith <rns>
2003-04-14 10:25:11 +0000

    interface for integration writers, extends ObjectStoreWriter

---
+++ intermine/src/java/org/intermine/dataloader/IntegrationWriter.java
@@ -0,0 +1,27 @@
+package org.flymine.dataloader;
+
+import org.flymine.objectstore.ObjectStoreWriter;
+import org.flymine.objectstore.ObjectStoreException;
+
+/**
+ * Define methods needed to deal with integration when writing to an ObjectStore.  To retain
+ * O/R mapping independence implementations of this interface should delegate writing to
+ * a mapping tool specific implementation of ObjectStoreWriter.
+ *
+ * @author Richard Smith
+ */
+
+public interface IntegrationWriter extends ObjectStoreWriter
+{
+
+    /**
+     * Given a new object from a data source find whether corresponding object exists in ObjectStore
+     * and if so which fields the current data source has permission to write to.
+     *
+     * @param obj the example object
+     * @return details of database object and which fields can be overwrittern
+     * @throws ObjectStoreException if anything goes wrong
+     */
+    public IntegrationDescriptor getByExample(Object obj) throws ObjectStoreException;
+
+}
