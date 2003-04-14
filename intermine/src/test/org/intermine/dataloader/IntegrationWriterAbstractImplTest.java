c454d31bce5af3878bb3fba57773badf6533e93e
rnsmith <rns>
2003-04-14 12:45:51 +0000

    added test for wrong properties

--- intermine/src/test/org/intermine/dataloader/IntegrationWriterAbstractImplTest.java
+++ intermine/src/test/org/intermine/dataloader/IntegrationWriterAbstractImplTest.java
@@ -4,26 +4,34 @@ import junit.framework.TestCase;
 
 import org.flymine.objectstore.ObjectStoreAbstractImpl;
 import org.flymine.objectstore.ObjectStoreFactory;
+import org.flymine.objectstore.ObjectStoreException;
 
 public class IntegrationWriterAbstractImplTest extends TestCase
 {
+    protected ObjectStoreAbstractImpl os;
+    protected IntegrationWriterAbstractImpl iWriter;
+
     public IntegrationWriterAbstractImplTest(String args) {
         super(args);
     }
 
     public void setUp() throws Exception {
-
+        os = (ObjectStoreAbstractImpl) ObjectStoreFactory.getObjectStore("os.unittest");
     }
 
     public void testWorks() throws Exception {
-        ObjectStoreAbstractImpl os = (ObjectStoreAbstractImpl) ObjectStoreFactory.getObjectStore("os.unittest");
-        IntegrationWriterAbstractImpl iWriter;
         iWriter = IntegrationWriterAbstractImpl.getInstance("source1", os, "dataloader.unittest");
         assertNotNull(iWriter);
 
     }
 
-
+    public void testWrongProps() throws Exception {
+        try {
+            iWriter = IntegrationWriterAbstractImpl.getInstance("source1", os, "dataloader.wrong");
+            fail("Expected ObjectStoreException");
+        } catch (ObjectStoreException e) {
+        }
+    }
 
 
 }
