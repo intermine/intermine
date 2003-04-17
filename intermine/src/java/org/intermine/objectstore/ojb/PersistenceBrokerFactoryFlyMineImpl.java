1281346c6428efd150c568755f51f443ceded753
mark <mark>
2003-04-17 10:20:16 +0000

    defers decision over whether to provide a pooled broker to the ojb factory

--- intermine/src/java/org/intermine/objectstore/ojb/PersistenceBrokerFactoryFlyMineImpl.java
+++ intermine/src/java/org/intermine/objectstore/ojb/PersistenceBrokerFactoryFlyMineImpl.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 
 import org.apache.ojb.broker.PBKey;
 import org.apache.ojb.broker.PersistenceBroker;
-import org.apache.ojb.broker.PBFactoryException;
 import org.apache.ojb.broker.ta.PersistenceBrokerFactoryDefaultImpl;
 import org.apache.ojb.broker.metadata.MetadataManager;
 import org.apache.ojb.broker.metadata.ConnectionRepository;
@@ -24,21 +23,13 @@ public class PersistenceBrokerFactoryFlyMineImpl extends PersistenceBrokerFactor
     private Map pbKeys = new HashMap();
 
     /**
-     * This version of createPersistenceBroker forces use of OJB.properties
-     * @see PersistenceBrokerFactoryIF#createPersistenceBroker
-     */
-    public PersistenceBroker createPersistenceBroker(PBKey pbKey) throws PBFactoryException {
-        return createNewBrokerInstance(pbKey);
-    }
-
-    /**
      * Create a FlyMine persistence broker from a database object
      *
      * @param model the name of the model
      * @param db the database object
      * @return a FlyMine PersistenceBroker
      */
-    public PersistenceBrokerFlyMineImpl createPersistenceBroker(Database db, String model) {
+    public PersistenceBrokerFlyMine createPersistenceBroker(Database db, String model) {
         PBKey key = (PBKey) pbKeys.get(db);
         if (key == null) {
             MetadataManager mdm = MetadataManager.getInstance();
@@ -69,11 +60,11 @@ public class PersistenceBrokerFactoryFlyMineImpl extends PersistenceBrokerFactor
 
         PersistenceBroker pb = createPersistenceBroker(key);
         
-        if (!(pb instanceof PersistenceBrokerFlyMineImpl)) {
+        if (!(pb instanceof PersistenceBrokerFlyMine)) {
             throw new IllegalArgumentException("PersistenceBrokerClass is not set to "
                                                + "PersistenceBrokerFlyMineImpl in OJB.properties");
         }
-        ((PersistenceBrokerFlyMineImpl) pb).setDatabase(db);
-        return (PersistenceBrokerFlyMineImpl) pb;
+        ((PersistenceBrokerFlyMine) pb).setDatabase(db);
+        return (PersistenceBrokerFlyMine) pb;
     }
 }
