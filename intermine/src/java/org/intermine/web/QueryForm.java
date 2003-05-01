cd669bf2f47254226d5e0d4bd97370ac05ee1e18
andy <andy>
2003-05-01 08:53:47 +0000

    Added javadoc comments

--- intermine/src/java/org/intermine/web/QueryForm.java
+++ intermine/src/java/org/intermine/web/QueryForm.java
@@ -1,19 +1,31 @@
 package org.flymine.web;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionMapping;
-import org.apache.struts.upload.FormFile;
 
-public class QueryForm extends ActionForm {
+/**
+ * Form bean to represent the inputs to a text-based query
+ *
+ * @author Andrew Varley
+ */
+public class QueryForm extends ActionForm
+{
 
     protected String querystring;
 
+    /**
+     * Set the query string
+     *
+     * @param querystring the query string
+     */
     public void setQuerystring(String querystring) {
         this.querystring = querystring;
     }
 
+    /**
+     * Get the query string
+     *
+     * @return the query string
+     */
     public String getQuerystring() {
         return querystring;
     }
