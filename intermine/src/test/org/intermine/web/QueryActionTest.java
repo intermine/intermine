dafae94daa91dec2400bde4b66b366e699ad516b
andy <andy>
2003-05-02 09:00:48 +0000

    Test for QueryAction

---
+++ intermine/src/test/org/intermine/web/QueryActionTest.java
@@ -0,0 +1,45 @@
+package org.flymine.web;
+
+import java.util.List;
+
+import servletunit.struts.MockStrutsTestCase;
+
+public class QueryActionTest extends MockStrutsTestCase {
+
+    public QueryActionTest(String testName) {
+        super(testName);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testSuccessfulQuery() {
+       setRequestPathInfo("/query");
+       addRequestParameter("querystring","select * from OJB_HL_SEQ");
+       actionPerform();
+       verifyForward("results");
+       assertNotNull((List) getRequest().getAttribute("results"));
+       verifyNoActionErrors();
+    }
+
+    public void testEmptyQuery() {
+        setRequestPathInfo("/query");
+        addRequestParameter("querystring","");
+        actionPerform();
+        verifyForward("error");
+        assertNull((String) getRequest().getAttribute("results"));
+    }
+
+    public void testRubbishQuery() {
+        setRequestPathInfo("/query");
+        addRequestParameter("querystring","some rubbish");
+        actionPerform();
+        verifyForward("error");
+        assertNull((String) getRequest().getAttribute("results"));
+    }
+}
