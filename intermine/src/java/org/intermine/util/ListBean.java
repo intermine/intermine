6424ec68b43d761725225b0e529bbf0a66ba4426
rnsmith <rns>
2003-05-02 12:24:11 +0000

    ArrayList with getters and setters so it can be marshalled/unmarshalled by castor as a bean

---
+++ intermine/src/java/org/intermine/util/ListBean.java
@@ -0,0 +1,40 @@
+package org.flymine.util;
+
+import java.util.List;
+import java.util.Collection;
+import java.util.ArrayList;
+
+/**
+ * A wrapper for java.util.ArrayList that has getter and setter methods for the
+ * the list contents so it appears bean-like.  Used for marshalling/unmarsahlling
+ * data with Castor so it appears that a FlyMine xml simply contains a List of
+ * business objects.  Castor can only deal with bean-like objects
+ *
+ * @author Richard Smith
+ */
+
+
+
+public class ListBean extends ArrayList
+{
+
+    /**
+     * Set fictional items field to be a given List, will clear any existing elements
+     *
+     * @param c collection of objects to become list
+     */
+    public void setItems(Collection c) {
+        this.clear();
+        this.addAll(c);
+    }
+
+    /**
+     * Retrieve items from list, simply returns this.
+     *
+     * @return this list
+     */
+    public List getItems() {
+        return this;
+    }
+
+}
