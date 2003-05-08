e76fb738341156402962c55d8d20c8a23c68d152
mnw21 <mnw21>
2003-05-08 16:16:15 +0000

    Tests equals method

--- intermine/src/test/org/intermine/objectstore/query/QueryValueTest.java
+++ intermine/src/test/org/intermine/objectstore/query/QueryValueTest.java
@@ -13,12 +13,12 @@ public class QueryValueTest extends TestCase
     public void setUp() {
     }
 
-    public void testGetType() {
+    public void testGetType() throws Exception {
         QueryValue value = new QueryValue("string");
         assertEquals(String.class, value.getType());
     }
 
-    public void testInvalidType() {
+    public void testInvalidType() throws Exception {
         try {
             QueryValue value = new QueryValue(new Object());
             fail("Expected IllegalArgumentException");
@@ -26,4 +26,12 @@ public class QueryValueTest extends TestCase
         }
     }
 
+    public void testDifferentNumbersEqual() throws Exception {
+        assertEquals(new QueryValue(new Integer(5)), new QueryValue(new Long(5)));
+        assertEquals(new QueryValue(new Integer(5)), new QueryValue(new Double(5.0)));
+    }
+
+    public void testDifferentNumbersNotEqual() throws Exception {
+        assertTrue("Expected 5 to not equal 5.00001", !(new QueryValue(new Integer(5))).equals(new QueryValue(new Double(5.00001))));
+    }
 }
