84027cae4ccf76a990bddafccb1744af12ae3c1f
mnw21 <mnw21>
2003-05-08 16:14:56 +0000

    All the regular tests

--- intermine/src/test/org/intermine/objectstore/query/QueryParserTest.java
+++ intermine/src/test/org/intermine/objectstore/query/QueryParserTest.java
@@ -1,13 +1,14 @@
 package org.flymine.objectstore.query;
 
 import junit.framework.TestCase;
+import org.flymine.objectstore.ObjectStoreQueriesTestCase;
 
 /**
  * Test for testing the parser on the flymine query object.
  *
  * @author Matthew Wakeling
  */
-public class QueryParserTest extends TestCase
+public class QueryParserTest extends ObjectStoreQueriesTestCase
 {
     public QueryParserTest(String arg) {
         super(arg);
@@ -17,6 +18,42 @@ public class QueryParserTest extends TestCase
      * Set up all the results expected for a given subset of queries
      */
     public void setUpResults() {
+        results.put("SelectSimpleObject", "select a1_ from Company as a1_");
+        results.put("SubQuery", "select a1_.a1_.name AS a2_, a1_.a2_ AS a3_ from (select a1_, 5 as a2_ from Company as a1_) as a1_");
+        results.put("WhereSimpleEquals", "select a1_.name as a2_ from Company as a1_ where a1_.vatNumber = 1234");
+        results.put("WhereSimpleNotEquals", "select a1_.name as a2_ from Company as a1_ where a1_.vatNumber != 1234");
+        results.put("WhereSimpleLike", "select a1_.name as a2_ from Company as a1_ where a1_.name like 'Company%'");
+        results.put("WhereEqualsString", "select a1_.name as a2_ from Company as a1_ where a1_.name = 'CompanyA'");
+        results.put("WhereAndSet", "select a1_.name as a2_ from Company as a1_ where a1_.name like 'Company%' and a1_.vatNumber > 2000");
+        results.put("WhereOrSet", "select a1_.name as a2_ from Company as a1_ where a1_.name like 'CompanyA%' or a1_.vatNumber > 2000");
+        results.put("WhereNotSet", "select a1_.name as a2_ from Company as a1_ where not (a1_.name like 'Company%' and a1_.vatNumber > 2000)");
+        results.put("WhereSubQueryField", "select a1_ from Department as a1_ where a1_.name in (select a1_.name as a2_ from Department as a1_) order by a1_.name");
+        results.put("WhereSubQueryClass", "select a1_ from Company as a1_ where a1_ in (select a1_ from Company as a1_ where a1_.name = 'CompanyA')");
+        results.put("WhereNotSubQueryClass", "select a1_ from Company as a1_ where a1_ not in (select a1_ from Company as a1_ where a1_.name = 'CompanyA')");
+        results.put("WhereNegSubQueryClass", "select a1_ from Company as a1_ where not (a1_ in (select a1_ from Company as a1_ where a1_.name = 'CompanyA'))");
+        results.put("WhereClassClass", "select a1_, a2_ from Company as a1_, Company as a2_ where a1_ = a2_");
+        results.put("WhereNotClassClass", "select a1_, a2_ from Company as a1_, Company as a2_ where a1_ != a2_");
+        results.put("WhereNegClassClass", "select a1_, a2_ from Company as a1_, Company as a2_ where not a1_ = a2_");
+        results.put("Contains11", "select a1_, a2_ from Department as a1_, Manager as a2_ where a1_.manager contains a2_ and a1_.name = 'DepartmentA1'");
+        results.put("ContainsNot11", "select a1_, a2_ from Department as a1_, Manager as a2_ where a1_.manager does not contain a2_ and a1_.name = 'DepartmentA1'");
+        results.put("ContainsNeg11", "select a1_, a2_ from Department as a1_, Manager as a2_ where not a1_.manager contains a2_ and a1_.name = 'DepartmentA1'");
+        results.put("Contains1N", "select a1_, a2_ from Company as a1_, Department as a2_ where a1_.departments contains a2_ and a1_.name = 'CompanyA'");
+        results.put("ContainsN1", "select a1_, a2_ from Department as a1_, Company as a2_ where a1_.company contains a2_ and a2_.name = 'CompanyA'");
+        results.put("ContainsMN", "select a1_, a2_ from Contractor as a1_, Company as a2_ where a1_.companys contains a2_ and a1_.name = 'ContractorA'");
+        results.put("ContainsDuplicatesMN", "select a1_, a2_ from Contractor as a1_, Company as a2_ where a1_.oldComs contains a2_");
+        results.put("SimpleGroupBy", "select a1_, count(*) as a2_ from Company as a1_, Department as a3_ where a1_.departments contains a3_ group by a1_");
+        results.put("MultiJoin", "select a1_, a2_, a3_, a4_ from Company as a1_, Department as a2_, Manager as a3_, Address as a4_ where a1_.departments contains a2_ and a2_.manager contains a3_ and a3_.address contains a4_ and a3_.name = 'EmployeeA1'");
+        results.put("SelectComplex", "select avg(a1_.vatNumber) + 20 as a3_, a2_.name as a4_, a2_ from Company as a1_, Department as a2_ group by a2_");
+        results.put("SelectClassAndSubClasses", "select a1_ from Employee as a1_ order by a1_.name");
+        results.put("SelectInterfaceAndSubClasses", "select a1_ from Employable as a1_");
+        results.put("SelectInterfaceAndSubClasses2", "select a1_ from RandomInterface as a1_");
+        results.put("SelectInterfaceAndSubClasses3", "select a1_ from ImportantPerson as a1_");
+    }
+
+    public void executeTest(String type) throws Exception {
+        String fql = (String) results.get(type);
+        Query parsed = new Query(fql, "org.flymine.model.testmodel");
+        assertEquals(type + " has failed", (Query) queries.get(type), parsed);
     }
 
     public void testConstants() throws Exception {
@@ -193,7 +230,7 @@ public class QueryParserTest extends TestCase
 
     public void testValidConstraints() throws Exception {
         Query q = new Query("select c_, d_, e_ from Company as c_, Department as d_, CEO as e_ where c_.departments does not contain d_ and c_.CEO contains e_ and (c_.vatNumber < 5 or c_.name like 'fish%') and e_.salary is not null and c_.vatNumber > e_.age and c_.name in (select Company.name as name from Company)", "org.flymine.model.testmodel");
-        assertEquals("SELECT c_, d_, e_ FROM org.flymine.model.testmodel.Company AS c_, org.flymine.model.testmodel.Department AS d_, org.flymine.model.testmodel.CEO AS e_ WHERE (c_.CEO CONTAINS e_ AND c_.departments DOES NOT CONTAIN d_ AND (c_.name LIKE fish% OR c_.vatNumber < 5) AND e_.salary IS NOT NULL AND c_.vatNumber > e_.age AND c_.name IN (SELECT Company.name AS name FROM org.flymine.model.testmodel.Company AS Company))", q.toString());
+        assertEquals("SELECT c_, d_, e_ FROM org.flymine.model.testmodel.Company AS c_, org.flymine.model.testmodel.Department AS d_, org.flymine.model.testmodel.CEO AS e_ WHERE (c_.departments DOES NOT CONTAIN d_ AND c_.CEO CONTAINS e_ AND (c_.vatNumber < 5 OR c_.name LIKE fish%) AND e_.salary IS NOT NULL AND c_.vatNumber > e_.age AND c_.name IN (SELECT Company.name AS name FROM org.flymine.model.testmodel.Company AS Company))", q.toString());
     }
 
     public void testInvalidConstraint() throws Exception {
