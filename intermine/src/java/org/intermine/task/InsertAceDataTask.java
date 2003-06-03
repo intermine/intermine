d4ac0c88893ecd13995fe422d5d6db6e4e526ebf
andy <andy>
2003-06-03 10:24:16 +0000

    Task to insert data from an AceDB server into an ObjectStore

---
+++ intermine/src/java/org/intermine/task/InsertAceDataTask.java
@@ -0,0 +1,118 @@
+package org.flymine.task;
+
+import java.lang.reflect.Method;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+
+/**
+ * Uses an ObjectStoreWriter to insert data from an Ace dataase server
+ *
+ * @author Andrew Varley
+ */
+public class InsertAceDataTask extends ClassPathTask
+{
+
+    protected String store;
+    protected String user;
+    protected String password;
+    protected String host;
+    protected int port;
+
+    /**
+     * Set the ObjectStore
+     *
+     * @param store the name of the ObjectStore
+     */
+    public void setObjectStore(String store) {
+        this.store = store;
+    }
+
+    /**
+     * Set the username for the AceDB server
+     *
+     * @param user the name of the user
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    /**
+     * Set the password for the AceDB server
+     *
+     * @param password the password of the user
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Set the host running the AceDB server
+     *
+     * @param host the name of the host
+     */
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    /**
+     * Set the port for the AceDB server
+     *
+     * @param port the port number
+     */
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    /**
+     * @see Task#execute
+     * @throws BuildException
+     */
+    public void execute() throws BuildException {
+        if (this.store == null) {
+            throw new BuildException("objectstore attribute is not set");
+        }
+        if (user == null) {
+            throw new BuildException("user attribute is not set");
+        }
+        if (password == null) {
+            throw new BuildException("password attribute is not set");
+        }
+        if (host == null) {
+            throw new BuildException("host attribute is not set");
+        }
+        if (port <= 0) {
+            throw new BuildException("port attribute is not set");
+        }
+
+        try {
+            Object driver = loadClass("org.flymine.task.AceDataLoaderDriver");
+
+            // Have to execute the loadData method by reflection as
+            // cannot cast to something that this class (which may use
+            // a different ClassLoader) can see
+
+            Method method = driver.getClass().getMethod("loadData", new Class[] {String.class,
+                                                                                 String.class,
+                                                                                 String.class,
+                                                                                 String.class,
+                                                                                 int.class });
+
+            log("found loadData method", Project.MSG_INFO);
+
+
+            method.invoke(driver, new Object [] {store,
+                                                 user,
+                                                 password,
+                                                 host,
+                                                 new Integer(port) });
+        } catch (Exception e) {
+            if (e.getMessage() != null) {
+                log(e.getMessage(), Project.MSG_INFO);
+            }
+            e.printStackTrace();
+            throw new BuildException(e);
+        }
+    }
+
+}
