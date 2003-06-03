d3c45a834eb7d34bfee0ff69ab47630f87ae2e14
andy <andy>
2003-06-03 16:37:03 +0000

    Dummy drivers used by ClassPathTaskTest and nowhere else

---
+++ intermine/src/test/org/intermine/task/DummyDriver2.java
@@ -0,0 +1,11 @@
+package org.flymine.task;
+
+/**
+ * Class that ClassPathTaskTest will try and load, that is not used anywhere else.
+ *
+ * @author Andrew Varley
+ */
+public class DummyDriver2
+{
+
+}
