3d8dc12b2c98eb15e975f736c7409e7c5e5cbf9e
mark <mark>
2003-06-05 12:51:02 +0000

    remove unimplemented abstract methods

--- intermine/src/java/org/intermine/objectstore/ObjectStoreAbstractImpl.java
+++ intermine/src/java/org/intermine/objectstore/ObjectStoreAbstractImpl.java
@@ -1,12 +1,5 @@
 package org.flymine.objectstore;
 
-import java.util.List;
-
-import org.flymine.objectstore.query.Query;
-import org.flymine.objectstore.query.Results;
-
-import org.flymine.sql.query.ExplainResult;
-
 /**
  * Abstract implementation of the ObjectStore interface. Used to provide uniformity
  * between different ObjectStore implementations.
@@ -24,60 +17,6 @@ public abstract class ObjectStoreAbstractImpl implements ObjectStore
      */
     protected ObjectStoreAbstractImpl() {
     }
-
-    /**
-     * Execute a Query on this ObjectStore
-     *
-     * @param q the Query to execute
-     * @return the results of the Query
-     * @throws ObjectStoreException if an error occurs during the running of the Query
-     */
-    public abstract Results execute(Query q) throws ObjectStoreException;
-
-    /**
-     * Execute a Query on this ObjectStore, asking for a certain range of rows to be returned.
-     * This will usually only be called by the Results object returned from
-     * <code>execute(Query q)</code>.
-     *
-     * @param q the Query to execute
-     * @param start the start row
-     * @param limit the maximum number of rows to return
-     * @return a List of ResultRows
-     * @throws ObjectStoreException if an error occurs during the running of the Query
-     */
-    public abstract List execute(Query q, int start, int limit) throws ObjectStoreException;
-
-
-    /**
-     * Runs an EXPLAIN on the query without a LIMIT or OFFSET.
-     *
-     * @param q the query to estimate rows for
-     * @return parsed results of EXPLAIN
-     * @throws ObjectStoreException if an error occurs explining the query
-     */
-    public abstract ExplainResult estimate(Query q) throws ObjectStoreException;
-
-    /**
-     * Runs an EXPLAIN for the given query with specified start and limit parameters.  This
-     * gives estimated time for a single 'page' of the query.
-     *
-     * @param q the query to explain
-     * @param start first row required, numbered from zero
-     * @param limit maximum number of rows to retun
-     * @return parsed results of EXPLAIN
-     * @throws ObjectStoreException if an error occurs explining the query
-     */
-    public abstract ExplainResult estimate(Query q, int start, int limit)
-        throws ObjectStoreException;
-
-    /**
-     * Execute a COUNT(*) on a query, returns the number of row the query will produce
-     *
-     * @param q Flymine Query on which to run COUNT(*)
-     * @return the number of rows to be produced by query
-     */
-    public abstract int count(Query q);
-
 
     /**
      * Checks the start and limit to see whether they are inside the
