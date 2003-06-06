8ef8c42e04eab94c64eb7e1f6aec6dfd9cef7655
mark <mark>
2003-06-06 12:51:06 +0000

    update to use "integration.unittest" properties

--- intermine/src/test/org/intermine/dataloader/IntegrationWriterFactoryTest.java
+++ intermine/src/test/org/intermine/dataloader/IntegrationWriterFactoryTest.java
@@ -2,32 +2,24 @@ package org.flymine.dataloader;
 
 import junit.framework.TestCase;
 
-import org.flymine.objectstore.ObjectStoreAbstractImpl;
-import org.flymine.objectstore.ObjectStoreFactory;
 import org.flymine.objectstore.ObjectStoreException;
 
 public class IntegrationWriterFactoryTest extends TestCase
 {
-    protected ObjectStoreAbstractImpl os;
     protected IntegrationWriter iw;
 
     public IntegrationWriterFactoryTest(String args) {
         super(args);
     }
 
-    public void setUp() throws Exception {
-        os = (ObjectStoreAbstractImpl) ObjectStoreFactory.getObjectStore("os.unittest");
+    public void testValidAlias() throws Exception {
+        IntegrationWriter iw = IntegrationWriterFactory.getIntegrationWriter("integration.unittest", "source1");
+        assertNotNull(iw);        
     }
 
-    public void testWorks() throws Exception {
-        iw = IntegrationWriterFactory.getIntegrationWriter("integration.unittest", "source1");
-        assertNotNull(iw);
-
-    }
-
-    public void testWrongProps() throws Exception {
+    public void testInvalidAlias() throws Exception {
         try {
-            iw = IntegrationWriterFactory.getIntegrationWriter("integration.wrong", "source1");
+            IntegrationWriterFactory.getIntegrationWriter("integration.wrong", "source1");
             fail("Expected ObjectStoreException");
         } catch (ObjectStoreException e) {
         }
