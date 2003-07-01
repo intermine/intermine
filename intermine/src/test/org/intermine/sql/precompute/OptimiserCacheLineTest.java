317aae33f667dfe6e9d232e352681937f903958f
mnw21 <mnw21>
2003-07-01 16:53:01 +0000

    Fixed tests

--- intermine/src/test/org/intermine/sql/precompute/OptimiserCacheLineTest.java
+++ intermine/src/test/org/intermine/sql/precompute/OptimiserCacheLineTest.java
@@ -24,18 +24,18 @@ public class OptimiserCacheLineTest extends TestCase
         assertTrue(!l1.isExpired());
         l1.expires = new Date();
         assertTrue("Score is " + l1.score(1000,0), 0.0 == l1.score(1000, 0));
-        assertTrue(0.0 == l1.score(1000, 100));
+        assertTrue(0.0 == l1.score(1000, 1000));
         assertTrue("Score is " + l1.score(250, 0), Math.abs(1.0 - l1.score(250, 0)) < 0.0001);
         assertTrue("Score is " + l1.score(4000, 0), Math.abs(1.0 - l1.score(4000, 0)) < 0.0001);
-        assertTrue(1.0 == l1.score(1000, 225));
+        assertTrue(1.0 == l1.score(1000, 1125));
         Thread.sleep(100);
         assertTrue(l1.isExpired());
     }
 
     public void test2() throws Exception {
-        OptimiserCacheLine l1 = new OptimiserCacheLine("optimised query", 1000, 500, 1000);
-        assertTrue("Score is " + l1.score(1000,0), 3.2 == l1.score(1000, 0));
-        assertTrue(2.4 == l1.score(1000, 100));
+        OptimiserCacheLine l1 = new OptimiserCacheLine("optimised query", 10000, 5000, 10000);
+        assertTrue("Score is " + l1.score(10000,0), 3.2 == l1.score(10000, 0));
+        assertTrue(2.4 == l1.score(10000, 1000));
     }
 
 }
--- intermine/src/test/org/intermine/sql/precompute/OptimiserCacheTest.java
+++ intermine/src/test/org/intermine/sql/precompute/OptimiserCacheTest.java
@@ -20,19 +20,19 @@ public class OptimiserCacheTest extends TestCase
 
     public void test1() throws Exception {
         OptimiserCache cache = new OptimiserCache();
-        cache.addCacheLine("original1", "optimised1_1", 1000, 0, 100000);
-        cache.addCacheLine("original1", "optimised1_2", 1000, 300, 100000);
-        cache.addCacheLine("original1", "optimised1_3", 1000, 20000, 100000);
-        cache.addCacheLine("original1", "optimised1_4", 16000, 0, 100000);
-        cache.addCacheLine("original1", "optimised1_5", 1000, 90000, 100000);
+        cache.addCacheLine("original1", "optimised1_1", 1000, 0, 1000000);
+        cache.addCacheLine("original1", "optimised1_2", 1000, 3000, 1000000);
+        cache.addCacheLine("original1", "optimised1_3", 1000, 200000, 1000000);
+        cache.addCacheLine("original1", "optimised1_4", 16000, 0, 1000000);
+        cache.addCacheLine("original1", "optimised1_5", 1000, 900000, 1000000);
         assertEquals("optimised1_1", cache.lookup("original1", 1000, 0));
-        assertEquals("optimised1_1", cache.lookup("original1", 1000, 149));
-        assertEquals("optimised1_2", cache.lookup("original1", 1000, 151));
+        assertEquals("optimised1_1", cache.lookup("original1", 1000, 1490));
+        assertEquals("optimised1_2", cache.lookup("original1", 1000, 1510));
         assertEquals("optimised1_1", cache.lookup("original1", 3999, 0));
         assertEquals("optimised1_4", cache.lookup("original1", 4001, 0));
-        assertEquals("optimised1_3", cache.lookup("original1", 1000, 15000));
-        assertEquals("optimised1_5", cache.lookup("original1", 1000, 100000));
-        assertNull(cache.lookup("original1", 1000, 60000));
+        assertEquals("optimised1_3", cache.lookup("original1", 1000, 150000));
+        assertEquals("optimised1_5", cache.lookup("original1", 1000, 1000000));
+        assertNull(cache.lookup("original1", 1000, 600000));
         assertNull(cache.lookup("somethingelse", 1000, 0));
     }
 
