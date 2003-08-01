bc6f6b3e7b93f2639142fef7d9cf640b439fb862
andy <andy>
2003-08-01 13:17:05 +0000

    Demonstration of data loading with getObjectByExample() lookup

---
+++ intermine/src/java/org/intermine/tutorial/SimpleDataLoader.java
@@ -0,0 +1,112 @@
+package org.flymine.tutorial;
+
+/*
+ * Copyright (C) 2002-2003 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.flymine.objectstore.ObjectStoreWriter;
+import org.flymine.objectstore.ObjectStoreWriterFactory;
+
+import org.flymine.model.tutorial.Company;
+import org.flymine.model.tutorial.Address;
+import org.flymine.model.tutorial.Department;
+
+/**
+ * Simple demonstration of loading data, where data may already exist
+ * in the ObjectStore. This is demonstrating using the
+ * ObjectStoreWriter.store() method. In real life, lots of this work
+ * can done by using one of the FlyMine IntegrationWriter or
+ * DataLoader classes.
+ *
+ * @author Andrew Varley
+ */
+public class SimpleDataLoader
+{
+
+    /**
+     * main method
+     * @param args command line parameters
+     * @throws Exception if an error occurs
+     */
+    public static void main(String[] args) throws Exception {
+        SimpleDataLoader sdl = new SimpleDataLoader();
+        sdl.storeCompany();
+    }
+
+    /**
+     * Store a Company object in the database
+     *
+     * @throws Exception if anything goes wrong
+     */
+    public void storeCompany() throws Exception {
+
+        // Get an ObjectStoreWriter to use for storing the object. The
+        // alias "osw.tutorial" refers to one set up in the
+        // flymine.properties file
+        ObjectStoreWriter osw = ObjectStoreWriterFactory.getObjectStoreWriter("osw.tutorial");
+
+        // Set up a Company object to store
+        Company company = new Company();
+
+        // Set the primary keys of the company object
+        Address address = new Address();
+        address.setAddress("1 The Street, A Town");
+        company.setName("CompanyA");
+        company.setAddress(address);
+
+        // Try and get the Company object from the database, if it exists
+        // Failure to do this lookup stage will mean that the objectstore
+        // will perform an insert rather than an update
+        Company companyFromDb = (Company) (osw.getObjectStore().getObjectByExample(company));
+        if (companyFromDb != null) {
+            // It did exist. Any attributes we alter must be done on
+            // this object, then the ObjectStore will know which to
+            // change in the store.
+            company = companyFromDb;
+        }
+
+        // Set some non-primary key attributes
+        company.setVatNumber(12345678);
+
+        // Set up a collection of departments
+        List departments = new ArrayList(company.getDepartments());
+
+        // Add some new departments. Here we are assuming they are
+        // completely new. If we weren't sure, we would have to set
+        // primary keys and do getObjectByExample() etc, exactly as
+        // for the company
+        Department department1 = new Department();
+        department1.setName("Department1");
+        department1.setCompany(company);
+        departments.add(department1);
+
+        Department department2 = new Department();
+        department2.setName("Department2");
+        department2.setCompany(company);
+        departments.add(department2);
+
+        // Set company's departments to this new list
+        company.setDepartments(departments);
+
+        // Store the company in the database
+        osw.store(company);
+
+        // Store the address object
+        osw.store(address);
+
+        // Store the new departments
+        osw.store(department1);
+        osw.store(department2);
+
+    }
+
+}
