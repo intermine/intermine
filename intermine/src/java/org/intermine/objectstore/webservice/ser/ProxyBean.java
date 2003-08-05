890fb7c39603ea7f75525f3217b1527d174916a7
mark <mark>
2003-08-05 15:00:08 +0000

    Initial revision

---
+++ intermine/src/java/org/intermine/objectstore/webservice/ser/ProxyBean.java
@@ -0,0 +1,96 @@
+package org.flymine.objectstore.webservice.ser;
+
+/*
+ * Copyright (C) 2002-2003 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import org.flymine.objectstore.query.fql.FqlQuery;
+
+/**
+ * Bean that represents a proxy (reference or collection), used in serialization
+*
+ * @author Mark Woodbridge
+ */
+public class ProxyBean
+{
+    String type;
+    FqlQuery fqlQuery;
+    Integer id;
+
+    /**
+     * No-arg constructor (for deserialization)
+     */
+    public ProxyBean() {
+    }
+
+    /**
+     * Constructor
+     * @param type the type of the underlying object
+     * @param fqlQuery the query to retrieve the object
+     * @param id the internal id of the underlying object
+     */
+    public ProxyBean(String type, FqlQuery fqlQuery, Integer id) {
+        this.type = type;
+        this.fqlQuery = fqlQuery;
+        this.id = id;
+    }
+
+    /**
+     * Returns the type of the underlying object
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Returns the query to retrieve the object
+     * @return the query
+     */
+    public FqlQuery getFqlQuery() {
+        return fqlQuery;
+    }
+
+    /**
+     * Returns the internal id of the underlying object
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @see Object#toString
+     */
+    public String toString() {
+        return "ProxyBean: type=" + type + " fqlQuery='" + fqlQuery + "' id=" + id;
+    }
+
+    /**
+     * @see Object#equals
+     */
+    public boolean equals(Object o) {
+        if (!(o instanceof ProxyBean)) {
+            return false;
+        }
+        ProxyBean p = (ProxyBean) o;
+        return p.type.equals(type)
+            && p.fqlQuery.equals(fqlQuery)
+            && p.id.equals(id);
+    }
+
+    /**
+     * @see Object#hashCode
+     */
+    public int hashCode() {
+        return 2 * type.hashCode()
+            + 3 * fqlQuery.hashCode()
+            + 5 * id.hashCode();
+    }
+}
---
+++ intermine/src/test/org/intermine/objectstore/webservice/ser/SerializationFunctionalTest.java
@@ -0,0 +1,135 @@
+package org.flymine.objectstore.webservice.ser;
+
+/*
+ * Copyright (C) 2002-2003 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import org.apache.axis.Constants;
+import org.apache.axis.Message;
+import org.apache.axis.MessageContext;
+import org.apache.axis.encoding.DeserializationContext;
+import org.apache.axis.encoding.DeserializationContextImpl;
+import org.apache.axis.encoding.SerializationContext;
+import org.apache.axis.encoding.SerializationContextImpl;
+import org.apache.axis.encoding.TypeMapping;
+import org.apache.axis.encoding.TypeMappingRegistry;
+import org.apache.axis.message.RPCElement;
+import org.apache.axis.message.RPCParam;
+import org.apache.axis.message.SOAPEnvelope;
+import org.apache.axis.server.AxisServer;
+import org.xml.sax.InputSource;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+import org.flymine.util.TypeUtil;
+
+import org.flymine.objectstore.query.fql.FqlQuery;
+import org.flymine.metadata.Model;
+import org.flymine.sql.query.ExplainResult;
+import org.flymine.model.testmodel.Address;
+import org.flymine.model.testmodel.Company;
+import org.flymine.model.testmodel.Department;
+
+import junit.framework.TestCase;
+
+public class SerializationFunctionalTest extends TestCase
+{
+    public void testEncoding() throws Exception {
+        MessageContext msgContext = new MessageContext(new AxisServer());
+        SOAPEnvelope msg = new SOAPEnvelope();
+
+        List args = args();
+        List params = new ArrayList();
+        for (int i = 0; i < args.size(); i++) {
+            params.add(new RPCParam("", "arg" + i, args.get(i)));
+        }
+
+        RPCElement body = new RPCElement("urn:myNamespace", "method1", params.toArray());
+        msg.addBodyElement(body);
+        
+        Writer stringWriter = new StringWriter();
+        SerializationContext context = new SerializationContextImpl(stringWriter, msgContext);
+            
+        TypeMappingRegistry reg = context.getTypeMappingRegistry();
+        TypeMapping tm = (TypeMapping) reg.getTypeMapping(Constants.URI_SOAP11_ENC);
+        if (tm == null) {
+            tm = (TypeMapping) reg.createTypeMapping();
+            reg.register(Constants.URI_DEFAULT_SOAP_ENC, tm);
+        }
+        SerializationUtil.registerMappings(tm);
+        SerializationUtil.registerDefaultMapping(tm, Address.class);
+        SerializationUtil.registerDefaultMapping(tm, Company.class);
+        SerializationUtil.registerDefaultMapping(tm, Department.class);
+        msg.output(context);
+            
+        String msgString = stringWriter.toString();
+        System.out.println(msgString);
+        Reader reader = new StringReader(msgString);
+            
+        DeserializationContext dser = new DeserializationContextImpl(
+                                                                     new InputSource(reader),
+                                                                     msgContext, Message.REQUEST);
+        dser.parse();
+        SOAPEnvelope env = dser.getEnvelope();
+            
+        RPCElement rpcElem = (RPCElement) env.getFirstBody();
+        for (int i = 0; i < args.size(); i++) {
+            assertEquals(args.get(i), rpcElem.getParam("arg" + i).getValue());
+        }
+    }
+
+    static List args() throws Exception {
+        List args = new ArrayList();
+
+        List l = new ArrayList();
+        l.add("one");
+        l.add("two");
+        //args.add(l);
+
+        ExplainResult e = new ExplainResult();
+        TypeUtil.setFieldValue(e, "rows", new Long(1));
+        TypeUtil.setFieldValue(e, "start", new Long(2));
+        TypeUtil.setFieldValue(e, "complete", new Long(3));
+        TypeUtil.setFieldValue(e, "width", new Long(4));
+        args.add(e);
+
+        FqlQuery q = new FqlQuery("select c from Company as c", null);
+        q.setParameters(l);
+        //args.add(q);
+
+        ProxyBean b = new ProxyBean("Company", q, new Integer(42));
+        args.add(b);
+        
+        Model m = Model.getInstanceByName("testmodel");
+        args.add(m);
+
+        Address a1 = new Address();
+        a1.setAddress("a1");
+        Company c1 = new Company();
+        c1.setName("c1");
+        c1.setVatNumber(101);
+        c1.setAddress(a1);
+        Department d1 = new Department();
+        d1.setName("d1");
+        d1.setCompany(c1);
+        Department d2 = new Department();
+        d2.setName("d2");
+        d2.setCompany(c1);
+         c1.setDepartments(Arrays.asList(new Object[] {d1, d2}));
+         //args.add(c1);
+
+        return args;
+    }
+}
