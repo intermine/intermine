594dd641ce9ebe9067b45d05c3648ca2d69565ee
andy <andy>
2003-08-19 15:18:11 +0000

    Added test for buttons and do all tests with a results object that knows its size, one that has an estimated size that is too small and another that has an estimated size too high

--- intermine/src/test/org/intermine/web/results/DisplayableResultsTest.java
+++ intermine/src/test/org/intermine/web/results/DisplayableResultsTest.java
@@ -22,26 +22,165 @@ public class DisplayableResultsTest extends TestCase
         super(arg);
     }
 
-    private Results results;
+    private ObjectStoreDummyImpl os;
+    private FqlQuery fq;
 
     public void setUp() throws Exception {
-        ObjectStoreDummyImpl os = new ObjectStoreDummyImpl();
+        os = new ObjectStoreDummyImpl();
         os.setResultsSize(15);
-        results = os.execute(new FqlQuery("select c, d from Company as c, Department as d", "org.flymine.model.testmodel").toQuery());
+        fq = new FqlQuery("select c, d from Company as c, Department as d", "org.flymine.model.testmodel");
     }
 
-    public void testConstructor() {
-        DisplayableResults dr = new DisplayableResults(results);
+    private DisplayableResults getExactResults() throws Exception {
+        Results results = os.execute(fq.toQuery());
+        // Make sure we definitely know the end
+        results.setBatchSize(20);
+        results.get(0);
+        return new DisplayableResults(results);
+    }
+
+    private DisplayableResults getEstimateTooHighResults() throws Exception {
+        os.setEstimatedResultsSize(25);
+        Results results = os.execute(fq.toQuery());
+        results.setBatchSize(1);
+        return new DisplayableResults(results);
+    }
+
+    private DisplayableResults getEstimateTooLowResults() throws Exception {
+        os.setEstimatedResultsSize(10);
+        Results results = os.execute(fq.toQuery());
+        results.setBatchSize(1);
+        return new DisplayableResults(results);
+    }
+
+    public void testConstructor() throws Exception {
+        DisplayableResults dr = getExactResults();
         assertEquals(2, dr.getColumns().size());
     }
 
+    public void testSizeExact() throws Exception {
+        DisplayableResults dr = getExactResults();
+        assertFalse(dr.isSizeEstimate());
+        assertEquals(15, dr.getSize());
+    }
 
-    public void testEnd() throws Exception {
-        DisplayableResults dr = new DisplayableResults(results);
-        dr.setEnd(10);
-        assertEquals(10, dr.getEnd());
-        dr.setEnd(20);
+    public void testSizeHigh() throws Exception {
+        DisplayableResults dr = getEstimateTooHighResults();
+        assertTrue(dr.isSizeEstimate());
+        assertEquals(25, dr.getSize());
+    }
+
+    public void testSizeLow() throws Exception {
+        DisplayableResults dr = getEstimateTooLowResults();
+        assertTrue(dr.isSizeEstimate());
+        assertEquals(10, dr.getSize());
+    }
+
+    public void testEndExact() throws Exception {
+        // At the beginning
+        DisplayableResults dr = getExactResults();
+        dr.setPageSize(10);
+        dr.setStart(0);
+        assertEquals(9, dr.getEnd());
+        // Abutting the end
+        dr = getExactResults();
+        dr.setPageSize(10);
+        dr.setStart(5);
+        assertEquals(14, dr.getEnd());
+        // Overlapping the end
+        dr = getExactResults();
+        dr.setPageSize(10);
+        dr.setStart(10);
         assertEquals(14, dr.getEnd());
     }
+
+    public void testEndHigh() throws Exception {
+        // At the beginning
+        DisplayableResults dr = getEstimateTooHighResults();
+        dr.setPageSize(10);
+        dr.setStart(0);
+        assertEquals(9, dr.getEnd());
+        // Abutting the end
+        dr = getEstimateTooHighResults();
+        dr.setPageSize(10);
+        dr.setStart(5);
+        assertEquals(14, dr.getEnd());
+        // Overlapping the end
+        dr = getEstimateTooHighResults();
+        dr.setPageSize(10);
+        dr.setStart(10);
+        assertEquals(14, dr.getEnd());
+    }
+
+    public void testEndLow() throws Exception {
+        // At the beginning
+        DisplayableResults dr = getEstimateTooLowResults();
+        dr.setPageSize(10);
+        dr.setStart(0);
+        assertEquals(9, dr.getEnd());
+        // Abutting the end
+        dr = getEstimateTooLowResults();
+        dr.setPageSize(10);
+        dr.setStart(5);
+        assertEquals(14, dr.getEnd());
+        // Overlapping the end
+        dr = getEstimateTooLowResults();
+        dr.setPageSize(10);
+        dr.setStart(10);
+        assertEquals(14, dr.getEnd());
+    }
+
+    public void testButtonsExact() throws Exception {
+        DisplayableResults dr = getExactResults();
+        dr.setPageSize(10);
+        // At the beginning
+        dr.setStart(0);
+        assertFalse(dr.isPreviousButton());
+        assertTrue(dr.isNextButton());
+        // Abutting the end
+        dr.setStart(5);
+        assertTrue(dr.isPreviousButton());
+        assertFalse(dr.isNextButton());
+        // Overlapping the end
+        dr.setStart(10);
+        assertTrue(dr.isPreviousButton());
+        assertFalse(dr.isNextButton());
+    }
+
+    public void testButtonsHigh() throws Exception {
+        DisplayableResults dr = getEstimateTooHighResults();
+        dr.setPageSize(10);
+        // At the beginning
+        dr.setStart(0);
+        assertFalse(dr.isPreviousButton());
+        assertTrue(dr.isNextButton());
+        // Abutting the end
+        dr.setStart(5);
+        assertTrue(dr.isPreviousButton());
+        assertFalse(dr.isNextButton());
+        // Overlapping the end
+        dr.setStart(10);
+        assertTrue(dr.isPreviousButton());
+        assertFalse(dr.isNextButton());
+    }
+
+    public void testButtonsLow() throws Exception {
+        DisplayableResults dr = getEstimateTooLowResults();
+        dr.setPageSize(10);
+        // At the beginning (this abuts the estimated end)
+        dr.setStart(0);
+        assertFalse(dr.isPreviousButton());
+        assertTrue(dr.isNextButton());
+        // Abutting the end
+        dr.setStart(5);
+        assertTrue(dr.isPreviousButton());
+        assertFalse(dr.isNextButton());
+        // Overlapping the end
+        dr.setStart(10);
+        assertTrue(dr.isPreviousButton());
+        assertFalse(dr.isNextButton());
+
+    }
+
 
 }
