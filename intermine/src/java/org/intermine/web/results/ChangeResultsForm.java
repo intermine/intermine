f22df0725eb28c51b09e0e7a80ca7da7ccab3c39
andy <andy>
2003-08-21 12:43:11 +0000

    Bean to hold parameters for changing properties of DisplayableResults

---
+++ intermine/src/java/org/intermine/web/results/ChangeResultsForm.java
@@ -0,0 +1,43 @@
+package org.flymine.web.results;
+
+/*
+ * Copyright (C) 2002-2003 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import org.apache.struts.action.ActionForm;
+
+/**
+ * Form bean to represent the inputs to a text-based query
+ *
+ * @author Andrew Varley
+ */
+public class ChangeResultsForm extends ActionForm
+{
+
+    protected String pageSize = "10";
+
+    /**
+     * Set the page size
+     *
+     * @param pageSize the page size to display
+     */
+    public void setPageSize(String pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    /**
+     * Get the page size
+     *
+     * @return the page size
+     */
+    public String getPageSize() {
+        return pageSize;
+    }
+
+}
