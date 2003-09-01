24aaa4b5aba73cd41bf58413fffd618a18b77200
andy <andy>
2003-09-01 10:05:55 +0000

    Added saveCollection() method (currently does nothing)

--- intermine/src/java/org/intermine/web/results/ChangeResultsSizeAction.java
+++ intermine/src/java/org/intermine/web/results/ChangeResultsSizeAction.java
@@ -63,6 +63,26 @@ public class ChangeResultsSizeAction extends LookupDispatchAction
     }
 
     /**
+     * Move a column nearer to the bottom of the list of columns. Must
+     * pass in a parameter "columnAlias" to indicate the column being
+     * moved.
+     *
+     * @param mapping The ActionMapping used to select this instance
+     * @param form The optional ActionForm bean for this request (if any)
+     * @param request The HTTP request we are processing
+     * @param response The HTTP response we are creating
+     * @return an ActionForward object defining where control goes next
+     *
+     * @exception ServletException if a servlet error occurs
+     */
+    public ActionForward saveCollection(ActionMapping mapping, ActionForm form,
+                                        HttpServletRequest request, HttpServletResponse response)
+        throws ServletException {
+        return mapping.findForward("results");
+    }
+
+
+    /**
      * Distributes the actions to the necessary methods, by providing a Map from action to
      * the name of a method.
      *
@@ -71,6 +91,7 @@ public class ChangeResultsSizeAction extends LookupDispatchAction
     protected Map getKeyMethodMap() {
         Map map = new HashMap();
         map.put("button.change", "changePageSize");
+        map.put("button.save", "saveCollection");
         return map;
     }
 
