bcda611ac3bedf11edc148a1f0072d04cde02a32
andy <andy>
2003-09-22 14:47:12 +0000

    Representation of a web page or tile

---
+++ intermine/src/java/org/intermine/web/config/Displayer.java
@@ -0,0 +1,64 @@
+package org.flymine.web.config;
+
+/*
+ * Copyright (C) 2002-2003 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+/**
+ * Configuration object describing details of a displayer
+ *
+ * @author Andrew Varley
+ */
+public class Displayer
+{
+    private String src;
+
+    /**
+     * Set the source of this displayer
+     *
+     * @param src the source
+     */
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    /**
+     * Get the source of this displayer
+     *
+     * @return the source
+     */
+    public String getSrc() {
+        return this.src;
+    }
+
+    /**
+     * @see Object#equals
+     *
+     * @param obj the Object to compare with
+     * @return true if this is equal to obj
+     */
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Displayer)) {
+            return false;
+        }
+
+        return src.equals(((Displayer) obj).src);
+    }
+
+    /**
+     * @see Object#hashCode
+     *
+     * @return the hashCode for this Displayer object
+     */
+    public int hashCode() {
+        return src.hashCode();
+    }
+
+}
+
