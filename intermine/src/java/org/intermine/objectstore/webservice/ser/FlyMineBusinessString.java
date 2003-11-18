f135481f9495359b8e81a2725bbf10830d5b0cec
mark <mark>
2003-11-18 13:48:56 +0000

    Initial revision

---
+++ intermine/src/java/org/intermine/objectstore/webservice/ser/FlyMineBusinessString.java
@@ -0,0 +1,58 @@
+package org.flymine.objectstore.webservice.ser;
+
+/*
+ * Copyright (C) 2002-2003 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+/**
+ * This class is a wrapper for the XML string used to represent
+ * FlyMineBusinessObjects when they are serialized
+ * @author Mark Woodbridge
+ */
+public class FlyMineBusinessString
+{
+    protected String string;
+
+    /**
+     * No-arg constructor
+     */
+    public FlyMineBusinessString() {
+    }
+
+    /**
+     * Construct a FlyMineBusinessString using a string
+     * @param string a LiteRendered XML string
+     */
+    public FlyMineBusinessString(String string) {
+        setString(string);
+    }
+
+    /**
+     * Set the internal string
+     * @param string a LiteRendered XML string
+     */
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    /**
+     * Get the internal string
+     * @return the LiteRendered XML string
+     */
+    public String getString() {
+        return string;
+    }
+
+    /**
+       * @see Object#toString
+       */
+    public String toString() {
+        return string;
+    }
+}
