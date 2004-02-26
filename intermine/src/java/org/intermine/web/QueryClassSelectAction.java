fa45ac406543fa28d57475044c9270f717c80250
mark <mark>
2004-02-26 12:49:35 +0000

    Add an action to allow class selection from a tree

--- intermine/src/java/org/intermine/web/QueryClassSelectAction.java
+++ intermine/src/java/org/intermine/web/QueryClassSelectAction.java
@@ -10,15 +10,17 @@ package org.flymine.web;
  *
  */
 
+import java.util.Map;
+import java.util.HashMap;
+
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.struts.action.Action;
+import org.apache.struts.actions.LookupDispatchAction;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.util.MessageResources;
 
 import org.flymine.objectstore.query.QueryClass;
 
@@ -30,7 +32,7 @@ import org.flymine.objectstore.query.QueryClass;
  * @author Mark Woodbridge
  */
 
-public class QueryClassSelectAction extends Action
+public class QueryClassSelectAction extends LookupDispatchAction
 {
     /**
      * Process the specified HTTP request, and create the corresponding HTTP
@@ -48,14 +50,13 @@ public class QueryClassSelectAction extends Action
      * @exception Exception if the application business logic throws
      *  an exception
      */
-    public ActionForward execute(ActionMapping mapping,
+    public ActionForward select(ActionMapping mapping,
                                  ActionForm form,
                                  HttpServletRequest request,
                                  HttpServletResponse response)
         throws Exception {
 
         // Extract attributes we will need
-        MessageResources messages = getResources(request);
         HttpSession session = request.getSession();
 
         QueryClassSelectForm queryClassSelectForm = (QueryClassSelectForm) form;
@@ -68,6 +69,44 @@ public class QueryClassSelectAction extends Action
         }
 
         return (mapping.findForward("buildquery"));
+    }
+
+    /**
+     * Process the specified HTTP request, and create the corresponding HTTP
+     * response (or forward to another web component that will create it).
+     * Return an <code>ActionForward</code> instance describing where and how
+     * control should be forwarded, or <code>null</code> if the response has
+     * already been completed.
+     *
+     * @param mapping The ActionMapping used to select this instance
+     * @param form The optional ActionForm bean for this request (if any)
+     * @param request The HTTP request we are processing
+     * @param response The HTTP response we are creating
+     * @return an ActionForward object defining where control goes next
+     *
+     * @exception Exception if the application business logic throws
+     *  an exception
+     */
+    public ActionForward browse(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+        throws Exception {
+
+        return mapping.findForward("tree");
+    }
+
+    /**
+     * Distributes the actions to the necessary methods, by providing a Map from action to
+     * the name of a method.
+     *
+     * @return a Map
+     */
+    protected Map getKeyMethodMap() {
+        Map map = new HashMap();
+        map.put("button.select", "select");
+        map.put("button.browse", "browse");
+        return map;
     }
 }
 
