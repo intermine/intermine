0c7b6bdb41be9d7aa73253648852e78b2c5546fd
kmr <kmr>
2004-03-11 13:54:47 +0000

    Added EDITING_ALIAS, QUERY_CLASSES, SAVED_BAGS, SAVED_BAGS_INVERSE, SAVED_QUERIES, SAVED_QUERIES_INVERSE, QUERY and RESULTS_TABLE.

--- intermine/src/java/org/intermine/web/Constants.java
+++ intermine/src/java/org/intermine/web/Constants.java
@@ -11,13 +11,62 @@ package org.flymine.web;
  */
 
 /**
- * Constants used as keys for Objects stored in the webapp contexts
- * @author Mark Woodbridge
+ * Container for global constants used by the webapp.
+ *
+ * @author Kim Rutherford
  */
+
 public interface Constants
 {
     /**
-     * Key for the DisplayModel
+     * The attribute name to use when storing the Model in the ServletContext.
      */
     public static final String MODEL = "MODEL";
+
+    /**
+     * The attribute name to use when storing the editingAlias in the ServletContext.  This is the
+     * alias of the QueryClass that is currently being editing.  It is used as a key to look up the
+     * QueryClass in the queryClasses Map.
+     */
+    public static final String EDITING_ALIAS = "EDITING_ALIAS";
+
+    /**
+     * The attribute name to use when storing the queryClasses Map in the ServletContext.
+     * queryClasses is a Map from aliases to DisplayQueryClass objects.
+     */
+    public static final String QUERY_CLASSES = "QUERY_CLASSES";
+
+    /**
+     * The attribute name to use when storing the Map of saved bags in the ServletContext.  The Map
+     * is from bag names to Collections of objects.
+     */
+    public static final String SAVED_BAGS = "SAVED_BAGS";
+
+    /**
+     * The Session attribute name to use when storing the Map of the saved collections to saved
+     * saved collection names.
+     */
+    public static final String SAVED_BAGS_INVERSE = "SAVED_BAGS_INVERSE";
+
+    /**
+     * The attribute name to use when storing the Map of saved queries in the ServletContext.  The
+     * Map is from query names to queries.
+     */
+    public static final String SAVED_QUERIES = "SAVED_QUERIES";
+
+    /**
+     * The Session attribute name to use when storing the Map of the saved queries to saved
+     * saved query names.
+     */
+    public static final String SAVED_QUERIES_INVERSE = "SAVED_QUERIES_INVERSE";
+
+    /**
+     * The attribute name to use when storing the Query in the ServletContext.
+     */
+    public static final String QUERY = "QUERY";
+
+    /**
+     * The attribute name to use when storing the results of a query in the ServletContext.
+     */
+    public static final String RESULTS_TABLE = "RESULTS_TABLE";
 }
