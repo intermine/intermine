3b7c0f870abfeb65248160b7c1832178ffbe4de0
andy <andy>
2004-03-29 10:20:05 +0000

    Initial version

---
+++ flymine/model/chado/src/java/org/flymine/dataconversion/ChadoDataTranslator.java
@@ -0,0 +1,113 @@
+package org.flymine.dataconversion;
+
+/*
+ * Copyright (C) 2002-2003 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.io.FileReader;
+import java.io.File;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+import org.flymine.FlyMineException;
+import org.flymine.xml.full.Attribute;
+import org.flymine.xml.full.Item;
+import org.flymine.xml.full.Reference;
+import org.flymine.ontology.OntologyUtil;
+import org.flymine.objectstore.ObjectStoreException;
+import org.flymine.objectstore.ObjectStore;
+import org.flymine.objectstore.ObjectStoreFactory;
+import org.flymine.objectstore.ObjectStoreWriter;
+import org.flymine.objectstore.ObjectStoreWriterFactory;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Convert Chado data in fulldata Item format conforming to a source OWL definition
+ * to fulldata Item format conforming to FlyMine OWL definition.
+ *
+ * @author Andrew Varley
+ * @author Mark Woodbridge
+ */
+public class ChadoDataTranslator extends DataTranslator
+{
+    protected static final Logger LOG = Logger.getLogger(ChadoDataTranslator.class);
+
+    /**
+     * @see DataTranslator#DataTranslator
+     */
+    public ChadoDataTranslator(ItemReader srcItemReader, OntModel model, String ns) {
+        super(srcItemReader, model, ns);
+    }
+
+
+    /**
+     * @see DataTranslator#translateItem
+     */
+    protected Collection translateItem(Item srcItem) throws ObjectStoreException, FlyMineException {
+        Collection result = new HashSet();
+        String className = OntologyUtil.getFragmentFromURI(srcItem.getClassName());
+        Collection translated = super.translateItem(srcItem);
+        if (translated != null) {
+            for (Iterator i = translated.iterator(); i.hasNext();) {
+                boolean storeTgtItem = true;
+                Item tgtItem = (Item) i.next();
+                if ("author".equals(className)) {
+                    tgtItem.addAttribute(new Attribute("name",
+                                         srcItem.getAttribute("givennames").getValue()
+                                         + " " + srcItem.getAttribute("surname").getValue()));
+                } else if ("feature_synonym".equals(className)) {
+                    promoteField(tgtItem, srcItem, "synonym", "synonym", "name");
+                } else if ("feature_dbxref".equals(className)) {
+                    promoteField(tgtItem, srcItem, "synonym", "dbxref", "accession");
+                    promoteField(tgtItem, srcItem, "source", "dbxref", "db");
+                }
+
+                if (storeTgtItem) {
+                    result.add(tgtItem);
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Main method
+     * @param args command line arguments
+     * @throws Exception if something goes wrong
+     */
+    public static void main (String[] args) throws Exception {
+        String srcOsName = args[0];
+        String tgtOswName = args[1];
+        String modelName = args[2];
+        String format = args[3];
+        String namespace = args[4];
+
+        Map paths = new HashMap();
+
+        ObjectStore osSrc = ObjectStoreFactory.getObjectStore(srcOsName);
+        ItemReader srcItemReader = new ObjectStoreItemReader(osSrc, paths);
+        ObjectStoreWriter oswTgt = ObjectStoreWriterFactory.getObjectStoreWriter(tgtOswName);
+        ItemWriter tgtItemWriter = new BufferedItemWriter(new ObjectStoreItemWriter(oswTgt));
+
+        OntModel model = ModelFactory.createOntologyModel();
+        model.read(new FileReader(new File(modelName)), null, format);
+        DataTranslator dt = new ChadoDataTranslator(srcItemReader, model, namespace);
+        model = null;
+        dt.translate(tgtItemWriter);
+        tgtItemWriter.close();
+    }
+}
