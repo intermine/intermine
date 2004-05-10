7be45a478be801699cc28ceff657f80a06881755
kmr <kmr>
2004-05-10 11:02:10 +0000

    Added a buttons Map attribute.

--- intermine/src/java/org/intermine/web/results/ChangeResultsForm.java
+++ intermine/src/java/org/intermine/web/results/ChangeResultsForm.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import java.util.Map;
+import java.util.HashMap;
 
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionError;
@@ -34,7 +35,8 @@ public class ChangeResultsForm extends ActionForm
     protected String[] selectedObjects = {};
     protected String bagName;
     protected String newBagName;
-
+    // map from "name" of last button pressed to text of that button
+    protected Map buttons = new HashMap();
 
     /**
      * Set the page size
@@ -110,23 +112,80 @@ public class ChangeResultsForm extends ActionForm
     }
 
     /**
+     * Set the buttons map
+     *
+     * @param buttons the map
+     */
+    public void setButtons(Map buttons) {
+        this.buttons = buttons;
+    }
+
+    /**
+     * Get the buttons map
+     *
+     * @return the map
+     */
+    public Map getButtons() {
+        return this.buttons;
+    }
+
+    /**
+     * Set a value for the given field
+     *
+     * @param key the field name
+     * @param value value to set
+     */
+    public void setButton(String key, Object value) {
+        buttons.put(key, value);
+    }
+
+    /**
+     * Get the value for the given field
+     *
+     * @param key the field name
+     * @return the field value
+     */
+    public Object getButton(String key) {
+        return buttons.get(key);
+    }
+
+    /**
+     * Return the name of the last button pressed
+     * @return the button name
+     */
+    public String getButton() {
+        if (buttons.size() == 0) {
+            return "";
+        } else {
+            return (String) buttons.keySet().iterator().next();
+        }
+    }
+
+    /**
      * @see ActionForm#validate
      */
     public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
         HttpSession session = request.getSession();
         Map savedBags = (Map) session.getAttribute(Constants.SAVED_BAGS);
 
-        ActionErrors errors = null;
-        if (newBagName.equals("")) {
-            errors = new ActionErrors();
-            errors.add(ActionErrors.GLOBAL_ERROR,
-                       new ActionError("errors.savebag.blank", newBagName));
-        } else if (savedBags != null && savedBags.containsKey(newBagName)) {
-            errors = new ActionErrors();
-            errors.add(ActionErrors.GLOBAL_ERROR,
-                       new ActionError("errors.savebag.existing", newBagName));
+        org.intermine.web.LogMe.log("i", "ChangeResultsForm.validate() getButton(): " +
+                                    getButton());
+
+        if ("saveNewBag".equals(getButton())) {
+            ActionErrors errors = null;
+            if (newBagName.equals("")) {
+                errors = new ActionErrors();
+                errors.add(ActionErrors.GLOBAL_ERROR,
+                           new ActionError("errors.savebag.blank", newBagName));
+            } else if (savedBags != null && savedBags.containsKey(newBagName)) {
+                errors = new ActionErrors();
+                errors.add(ActionErrors.GLOBAL_ERROR,
+                           new ActionError("errors.savebag.existing", newBagName));
+            }
+            return errors;
+        } else {
+            return null;
         }
-        return errors;
     }
 
     /**
@@ -138,5 +197,8 @@ public class ChangeResultsForm extends ActionForm
     public void reset(ActionMapping mapping, HttpServletRequest request) {
         selectedObjects = new String[] {};
         newBagName = null;
+        buttons = new HashMap();
+
+        org.intermine.web.LogMe.log("i", "ChangeResultsForm.reset()");
     }
 }
