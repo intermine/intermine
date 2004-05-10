0b75db9e92007dd52313bef4cf2ccfe7b1549e05
kmr <kmr>
2004-05-10 11:27:17 +0000

    Implementation of Action that views a bag.

---
+++ intermine/src/java/org/intermine/web/ViewBagAction.java
@@ -0,0 +1,45 @@
+package org.intermine.web;
+
+/*
+ * Copyright (C) 2002-2004 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.Map;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * Implementation of Action that views a bag.
+ * @author Kim Rutherford
+ */
+
+public class ViewBagAction extends Action
+{
+    /**
+     * @see Action#execute
+     */
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+        throws Exception {
+        HttpSession session = request.getSession();
+
+        session.removeAttribute("changeResultsForm"); 
+        
+        return mapping.findForward("details");
+    }
+}
