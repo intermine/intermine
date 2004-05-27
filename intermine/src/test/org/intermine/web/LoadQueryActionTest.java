86db6fb2af214fa82333e45a0557d4ab125338a7
kmr <kmr>
2004-05-27 13:04:42 +0000

    Tests for the LoadQueryAction class

---
+++ intermine/src/test/org/intermine/web/LoadQueryActionTest.java
@@ -0,0 +1,45 @@
+package org.intermine.web;
+
+/*
+ * Copyright (C) 2002-2004 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.io.*;
+import java.util.*;
+
+import servletunit.struts.MockStrutsTestCase;
+
+/**
+ * Tests for the LoadQueryAction class
+ *
+ * @author Kim Rutherford
+ */
+public class LoadQueryActionTest extends MockStrutsTestCase
+{
+    public LoadQueryActionTest(String arg) {
+        super(arg);
+    }
+
+    public void testLoad() throws Exception {
+        addRequestParameter("method", "load");
+        addRequestParameter("name", "employeesWithOldManagers");
+
+        setRequestPathInfo("/loadExampleQuery");
+
+        actionPerform();
+        verifyNoActionErrors();
+        verifyForward("query");
+
+        assertNotNull(getSession().getAttribute(Constants.QUERY));
+        assertNotNull(getSession().getAttribute(Constants.VIEW));
+        assertEquals("[Employee.name, Employee.age, Employee.department.name, Employee.department.manager.age]",
+                     "" + getSession().getAttribute(Constants.VIEW));
+        assertEquals("{Employee=Employee:Employee [], Employee.department=Employee.department:Department [], Employee.department.manager=Employee.department.manager:Manager [], Employee.department.manager.age=Employee.department.manager.age:int [> 10]}", "" + getSession().getAttribute(Constants.QUERY));
+    }
+}
