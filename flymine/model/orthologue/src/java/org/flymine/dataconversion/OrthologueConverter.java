8fb9578542559d0539340fad15a31250bc54e2ab
kmr <kmr>
2004-06-03 15:01:50 +0000

    Fixed checkstyle problem.

--- flymine/model/orthologue/src/java/org/flymine/dataconversion/OrthologueConverter.java
+++ flymine/model/orthologue/src/java/org/flymine/dataconversion/OrthologueConverter.java
@@ -15,7 +15,6 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Map;
