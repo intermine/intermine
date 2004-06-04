52416cd76fe85a25147d76dfb529465d1d3baa1d
kmr <kmr>
2004-06-04 15:22:30 +0000

    An implementation of the BioJava Sequence interface that uses FlyMine objects underneath.

---
+++ flymine/src/java/org/flymine/biojava/FlyMineSequence.java
@@ -0,0 +1,55 @@
+package org.flymine.biojava;
+
+/*
+ * Copyright (C) 2002-2004 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import org.biojava.bio.seq.impl.SimpleSequence;
+import org.biojava.bio.symbol.SymbolList;
+
+import org.flymine.model.genomic.Protein;
+import org.flymine.model.genomic.LocatedSequenceFeature;
+
+/**
+ * An implementation of the BioJava Sequence interface that uses FlyMine objects underneath.
+ *
+ * @author Kim Rutherford
+ */
+public class FlyMineSequence extends SimpleSequence
+{
+    /**
+     * The LocatedSequenceFeature that was passed to the constructor.
+     */
+    private LocatedSequenceFeature feature = null;
+    
+    /**
+     * The Protein that was passed to the constructor.
+     */
+    private Protein protein = null;
+    
+    /**
+     * Create a new FlyMineSequence from a LocatedSequenceFeature
+     * @param symbols a DNA SymbolList created from the LocatedSequenceFeature
+     * @param feature the LocatedSequenceFeature
+     */
+    FlyMineSequence (SymbolList symbols, LocatedSequenceFeature feature) {
+        super(symbols, null, null, null);
+        this.feature = feature;
+    }
+
+    /**
+     * Create a new FlyMineSequence from a Protein
+     * @param symbols a amino acid SymbolList created from the Protein
+     * @param protein the Protein
+     */
+    FlyMineSequence (SymbolList symbols, Protein protein) {
+        super(symbols, null, null, null);
+        this.protein = protein;
+    }
+}
