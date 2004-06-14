0bbeddb0648d716323baaa2b80b9557624842323
rnsmith <rns>
2004-06-14 16:57:57 +0000

    Added PSI data to model and data translation

---
+++ flymine/model/psi/src/java/org/flymine/dataconversion/PsiDataTranslator.java
@@ -0,0 +1,195 @@
+package org.flymine.dataconversion;
+
+/*
+ * Copyright (C) 2002-2004 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.HashSet;
+
+import com.hp.hpl.jena.ontology.OntModel;
+
+import org.intermine.InterMineException;
+import org.intermine.xml.full.Attribute;
+import org.intermine.xml.full.Item;
+import org.intermine.xml.full.Reference;
+import org.intermine.xml.full.ItemHelper;
+import org.intermine.ontology.OntologyUtil;
+import org.intermine.objectstore.ObjectStoreException;
+import org.intermine.dataconversion.ItemReader;
+import org.intermine.dataconversion.ItemWriter;
+import org.intermine.dataconversion.DataTranslator;
+
+/**
+ * DataTranslator specific to Protein Interacion data in PSI XML format.
+ *
+ * @author Richard Smith
+ * @author Andrew Varley
+ */
+
+public class PsiDataTranslator extends DataTranslator
+{
+     protected static final Logger LOG = Logger.getLogger(PsiDataTranslator.class);
+
+    private Item db = null;
+    private Item swissProt = null;
+
+    /**
+     * @see DataTranslator#DataTranslator
+     */
+    public PsiDataTranslator(ItemReader srcItemReader, OntModel model, String ns) {
+        super(srcItemReader, model, ns);
+    }
+
+
+    /**
+     * @see DataTranslator#translate
+     */
+    public void translate(ItemWriter tgtItemWriter)
+        throws ObjectStoreException, InterMineException {
+
+        tgtItemWriter.store(ItemHelper.convert(getSwissProt()));
+        super.translate(tgtItemWriter);
+    }
+
+   /**
+     * @see DataTranslator#translateItem
+     */
+    protected Collection translateItem(Item srcItem)
+        throws ObjectStoreException, InterMineException {
+
+        Collection result = new HashSet();
+        String srcNs = OntologyUtil.getNamespaceFromURI(srcItem.getClassName());
+        String className = OntologyUtil.getFragmentFromURI(srcItem.getClassName());
+        Collection translated = super.translateItem(srcItem);
+        if (translated != null) {
+            for (Iterator i = translated.iterator(); i.hasNext();) {
+                boolean storeTgtItem = true;
+                Item tgtItem = (Item) i.next();
+                if ("ExperimentType".equals(className)) {
+                    Item pub = createPublication(srcItem);
+                    if (pub != null) {
+                        tgtItem.addReference(new Reference("publication", pub.getIdentifier()));
+                    }
+                    result.add(pub);
+
+                    // no confidence values in current data set
+                } else if ("InteractionElementType".equals(className)) {
+                    // create ProteinInteraction relation
+                    Item interaction = createProteinInteraction(srcItem, tgtItem);
+                    addReferencedItem(tgtItem, interaction, "relations", true, "evidence", true);
+
+                    // reference source database
+                    addReferencedItem(tgtItem, getDb(), "source", false, "", false);
+
+                    // ExperimentalResult need to reference ProteinInteractionExperiment
+                    promoteField(tgtItem, srcItem, "analysis", "experimentList", "experimentRef");
+
+                    // set confidence from attributeList
+                    Iterator iter = ItemHelper.convert(srcItemReader
+                                 .getItemById(srcItem.getReference("attributeList").getRefId()))
+                        .getCollection("attributes").getRefIds().iterator();
+                    while (iter.hasNext()) {
+                        Item attribute = ItemHelper.convert(srcItemReader
+                                                            .getItemById((String) iter.next()));
+                        String value = attribute.getAttribute("attribute").getValue().trim();
+                        String name = attribute.getAttribute("name").getValue().trim();
+                        if (Character.isDigit(value.charAt(0))
+                            && name.equals("author-confidence")) {
+                            tgtItem.addAttribute(new Attribute("confidence", value));
+                        }
+                    }
+                    result.add(interaction);
+                } else if ("ProteinInteractorType".equals(className)) {
+                    // protein needs swissport id set and synonym to swissprot
+                    Item xref = ItemHelper.convert(srcItemReader
+                                  .getItemById(srcItem.getReference("xref").getRefId()));
+                    Item dbXref = ItemHelper.convert(srcItemReader
+                                    .getItemById(xref.getReference("primaryRef").getRefId()));
+                    if (dbXref.getAttribute("db").getValue().equals("uniprot")) {
+                        String value = dbXref.getAttribute("id").getValue();
+                        tgtItem.addAttribute(new Attribute("swissProtId", value));
+                        //tgtItem.addAttribute(new Attribute("identifer", value));
+                        Item synonym = createItem(tgtNs + "Synonym", "");
+                        addReferencedItem(synonym, getSwissProt(), "source", false, "", false);
+                        synonym.addAttribute(new Attribute("synonym", value));
+                        addReferencedItem(tgtItem, synonym, "synonyms", true, "subject", false);
+                        result.add(synonym);
+                    }
+
+
+                } else if ("Source_Entry_EntrySet".equals(className)) {
+                    tgtItem.setIdentifier(getDb().getIdentifier());
+                    tgtItem.addAttribute(new Attribute("title",
+                            ItemHelper.convert(srcItemReader
+                                 .getItemById(srcItem.getReference("names").getRefId()))
+                                                 .getAttribute("shortLabel").getValue()));
+                }
+                result.add(tgtItem);
+            }
+        }
+        return result;
+    }
+
+    // create a publication given an ExperimentType item
+    private Item createPublication(Item exptType) throws ObjectStoreException {
+        Item bibrefType = ItemHelper.convert(srcItemReader
+                            .getItemById(exptType.getReference("bibref").getRefId()));
+        Item xrefType = ItemHelper.convert(srcItemReader
+                          .getItemById(bibrefType.getReference("xref").getRefId()));
+        Item dbrefType = ItemHelper.convert(srcItemReader
+                           .getItemById(xrefType.getReference("primaryRef").getRefId()));
+
+        Item pub = null;
+        if (dbrefType != null && dbrefType.getAttribute("db").getValue().equals("pubmed")) {
+            pub = createItem(tgtNs + "Publication", "");
+            pub.addAttribute(new Attribute("pubMedId", dbrefType.getAttribute("id").getValue()));
+        }
+        return pub;
+    }
+
+    private Item createProteinInteraction(Item intElType, Item tgtItem)
+        throws ObjectStoreException {
+        Item interaction = createItem(tgtNs + "ProteinInteraction", "");
+
+        Item participants = ItemHelper.convert(srcItemReader
+                              .getItemById(intElType.getReference("participantList").getRefId()));
+        Iterator iter = participants.getCollection("proteinParticipants").getRefIds().iterator();
+        while (iter.hasNext()) {
+            // protein has role attribute which is either prey or bait
+            Item protein = ItemHelper.convert(srcItemReader.getItemById((String) iter.next()));
+            interaction.addReference(new Reference(protein.getAttribute("role").getValue(),
+                                     protein.getReference("proteinInteractorRef").getRefId()));
+        }
+        // object = prey, subject = bait
+        interaction.addReference(new Reference("object",
+                                               interaction.getReference("prey").getRefId()));
+        interaction.addReference(new Reference("subject",
+                                               interaction.getReference("bait").getRefId()));
+
+        addReferencedItem(interaction, getDb(), "evidence", true, "", false);
+        return interaction;
+    }
+
+    private Item getDb() {
+        if (this.db == null) {
+            db = createItem(tgtNs + "Database", "");
+        }
+        return db;
+    }
+
+    private Item getSwissProt() {
+        if (this.swissProt == null) {
+            swissProt = createItem(tgtNs + "Database", "");
+            swissProt.addAttribute(new Attribute("title", "Swiss-Prot"));
+        }
+        return swissProt;
+    }
+}
---
+++ flymine/model/psi/src/test/org/flymine/dataconversion/PsiDataTranslatorTest.java
@@ -0,0 +1,90 @@
+package org.flymine.dataconversion;
+
+/*
+ * Copyright (C) 2002-2004 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import junit.framework.TestCase;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.HashSet;
+import java.util.Set;
+import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.io.FileReader;
+
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+import org.intermine.xml.full.FullParser;
+import org.intermine.dataconversion.DataTranslator;
+import org.intermine.dataconversion.DataConversionTestCase;
+import org.intermine.dataconversion.MockItemReader;
+import org.intermine.dataconversion.MockItemWriter;
+import org.intermine.dataconversion.XmlConverter;
+import org.intermine.metadata.Model;
+import org.intermine.xml.full.FullRenderer;
+
+public class PsiDataTranslatorTest extends DataConversionTestCase {
+    private String tgtNs = "http://www.flymine.org/model/genomic#";
+
+    public void setUp() throws Exception {
+        super.setUp();
+        expectedItems = getExpectedItems();
+        modelName = "genomic";
+    }
+
+
+    public void testTranslate() throws Exception {
+        Collection srcItems = getSrcItems();
+        Map itemMap = writeItems(srcItems);
+        // print out source items XML - result of running XmlConverter on PSI XML
+        // FileWriter writer = new FileWriter(new File("src.xml"));
+        // writer.write(FullRenderer.render(srcItems));
+        // writer.flush(); writer.close();
+        DataTranslator translator = new PsiDataTranslator(new MockItemReader(itemMap),
+                                                              getOwlModel(), tgtNs);
+        MockItemWriter tgtIw = new MockItemWriter(new LinkedHashMap());
+        translator.translate(tgtIw);
+
+        assertEquals(new HashSet(expectedItems), tgtIw.getItems());
+    }
+
+    private Collection getSrcItems() throws Exception {
+        Model psiModel = Model.getInstanceByName("psi");
+        Reader srcReader = (new InputStreamReader(getClass().getClassLoader().getResourceAsStream("test/PsiDataTranslatorTest_src.xml")));
+        MockItemWriter mockIw = new MockItemWriter(new HashMap());
+        Reader xsdReader = new InputStreamReader(getClass().getClassLoader().getResourceAsStream("psi.xsd"));
+
+        XmlConverter converter = new XmlConverter(psiModel, srcReader, xsdReader, mockIw);
+        converter.process();
+
+        return mockIw.getItems();
+    }
+
+    protected Collection getExpectedItems() throws Exception {
+        return FullParser.parse(getClass().getClassLoader().getResourceAsStream("test/PsiDataTranslatorTest_tgt.xml"));
+    }
+
+    protected OntModel getOwlModel() {
+        InputStreamReader reader = new InputStreamReader(getClass().getClassLoader().getResourceAsStream("genomic.n3"));
+
+        OntModel ont = ModelFactory.createOntologyModel();
+        ont.read(reader, null, "N3");
+        return ont;
+    }
+
+}
