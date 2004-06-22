0d5085b7a788d22c6f58335ffa2271a8de19b4c3
kmr <kmr>
2004-06-22 12:51:48 +0000

    The range of all locations is now a multiple of 25.

--- flymine/model/genomic/src/test/org/flymine/postprocess/CalculateLocationsTest.java
+++ flymine/model/genomic/src/test/org/flymine/postprocess/CalculateLocationsTest.java
@@ -86,7 +86,7 @@ public class CalculateLocationsTest extends TestCase {
         Set toStore = new HashSet(Arrays.asList(new Object[] {getChromosome(), getChromosomeBand(), getBandOnChr()}));
         Supercontig sc = (Supercontig) DynamicUtil.createObject(Collections.singleton(Supercontig.class));
         sc.setId(new Integer(104));
-        Location loc = createLocation(getChromosome(), sc, 0, 1200, 1600);
+        Location loc = createLocation(getChromosome(), sc, 0, 1201, 1600);
         toStore.add(sc);
         toStore.add(loc);
 
@@ -99,7 +99,7 @@ public class CalculateLocationsTest extends TestCase {
         CalculateLocations cl = new CalculateLocations(osw);
         cl.createLocations();
 
-        Location expected = createLocation(getChromosomeBand(), sc, 0, 200, 600);
+        Location expected = createLocation(getChromosomeBand(), sc, 0, 201, 600);
         expected.setId(new Integer(0));
         Item expItem = FullRenderer.toItem(expected, model);
         Location result = (Location) ((ResultsRow) cl.findLocations(ChromosomeBand.class, Supercontig.class).next()).get(2);
@@ -114,8 +114,8 @@ public class CalculateLocationsTest extends TestCase {
         Contig c = (Contig) DynamicUtil.createObject(Collections.singleton(Contig.class));
         sc.setId(new Integer(104));
         c.setId(new Integer(105));
-        Location scOnChr = createLocation(getChromosome(), sc, 0, 1200, 1600);
-        Location contigOnSc = createLocation(sc, c, 0, 100, 350);
+        Location scOnChr = createLocation(getChromosome(), sc, 0, 1201, 1600);
+        Location contigOnSc = createLocation(sc, c, 0, 101, 350);
         toStore.add(sc);
         toStore.add(c);
         toStore.add(scOnChr);
@@ -131,7 +131,7 @@ public class CalculateLocationsTest extends TestCase {
         cl.createLocations();
 
         // test contig location on chromosome
-        Location expected = createLocation(getChromosome(), c, 0, 1300, 1550);
+        Location expected = createLocation(getChromosome(), c, 0, 1301, 1550);
         expected.setId(new Integer(0));
         Item expItem = FullRenderer.toItem(expected, model);
         Location result = (Location) ((ResultsRow) cl.findLocations(Chromosome.class, Contig.class).next()).get(2);
@@ -140,7 +140,7 @@ public class CalculateLocationsTest extends TestCase {
         assertEquals(expItem, resItem);
 
         // test contig location on ChromosomeBand
-        expected = createLocation(getChromosomeBand(), c, 0, 300, 550);
+        expected = createLocation(getChromosomeBand(), c, 0, 301, 550);
         expected.setId(new Integer(0));
         expItem = FullRenderer.toItem(expected, model);
         result = (Location) ((ResultsRow) cl.findLocations(ChromosomeBand.class, Contig.class).next()).get(2);
@@ -157,9 +157,9 @@ public class CalculateLocationsTest extends TestCase {
         c.setId(new Integer(105));
         Exon e = (Exon) DynamicUtil.createObject(Collections.singleton(Exon.class));
         e.setId(new Integer(106));
-        Location scOnChr = createLocation(getChromosome(), sc, 0, 1200, 1600);
-        Location contigOnSc = createLocation(sc, c, 0, 100, 350);
-        Location exonOnContig = createLocation(c, e, 0, 50, 150);
+        Location scOnChr = createLocation(getChromosome(), sc, 0, 1201, 1600);
+        Location contigOnSc = createLocation(sc, c, 0, 101, 350);
+        Location exonOnContig = createLocation(c, e, 0, 51, 150);
         toStore.add(sc);
         toStore.add(c);
         toStore.add(e);
@@ -175,7 +175,7 @@ public class CalculateLocationsTest extends TestCase {
         cl.createLocations();
 
         // test Exon location on Chromosome
-        Location expected = createLocation(getChromosome(), e, 0, 1350, 1450);
+        Location expected = createLocation(getChromosome(), e, 0, 1351, 1450);
         expected.setId(new Integer(0));
         Item expItem = FullRenderer.toItem(expected, model);
         Location result = (Location) ((ResultsRow) cl.findLocations(Chromosome.class, Exon.class).next()).get(2);
@@ -184,7 +184,7 @@ public class CalculateLocationsTest extends TestCase {
         assertEquals(expItem, resItem);
 
         // test Exon location on Supercontig
-        expected = createLocation(sc, e, 0, 150, 250);
+        expected = createLocation(sc, e, 0, 151, 250);
         expected.setId(new Integer(0));
         expItem = FullRenderer.toItem(expected, model);
         result = (Location) ((ResultsRow) cl.findLocations(Supercontig.class, Exon.class).next()).get(2);
@@ -193,7 +193,7 @@ public class CalculateLocationsTest extends TestCase {
         assertEquals(expItem, resItem);
 
         // test Exon location on ChromosomeBand
-        expected = createLocation(getChromosomeBand(), e, 0, 350, 450);
+        expected = createLocation(getChromosomeBand(), e, 0, 351, 450);
         expected.setId(new Integer(0));
         expItem = FullRenderer.toItem(expected, model);
         result = (Location) ((ResultsRow) cl.findLocations(ChromosomeBand.class, Exon.class).next()).get(2);
@@ -214,47 +214,47 @@ public class CalculateLocationsTest extends TestCase {
 
     public void testOverlap() throws Exception {
         CalculateLocations cl = new CalculateLocations(osw);
-        CalculateLocations.SimpleLoc parent = cl.new SimpleLoc(101, 102, 100, 200, 0);
+        CalculateLocations.SimpleLoc parent = cl.new SimpleLoc(101, 102, 101, 200, 0);
 
-        //   ------------       parent
+        //   ------------>       parent
         //          |
-        //          ---------   child
-        CalculateLocations.SimpleLoc s1 = cl.new SimpleLoc(101, 103, 150, 250, 0);
+        //          --------->   child
+        CalculateLocations.SimpleLoc s1 = cl.new SimpleLoc(101, 103, 151, 250, 0);
         assertTrue(cl.overlap(s1, parent));
         assertTrue(cl.overlap(parent, s1));
 
-        //       --------------  parent
+        //       -------------->   parent
         //            |
-        //   ----------          child
-        CalculateLocations.SimpleLoc s2 = cl.new SimpleLoc(101, 103, 50, 150, 0);
+        //   ---------->           child
+        CalculateLocations.SimpleLoc s2 = cl.new SimpleLoc(101, 103, 51, 150, 0);
         assertTrue(cl.overlap(s2, parent));
         assertTrue(cl.overlap(parent, s2));
 
-        //  ------------------  parent
+        //  ------------------>   parent
         //      |        |
-        //      ----------      child
-        CalculateLocations.SimpleLoc s3 = cl.new SimpleLoc(101, 103, 125, 175, 0);
+        //      ---------->       child
+        CalculateLocations.SimpleLoc s3 = cl.new SimpleLoc(101, 103, 126, 175, 0);
         assertTrue(cl.overlap(s3, parent));
         assertTrue(cl.overlap(parent, s3));
 
-        //      --------       parent
+        //      -------->        parent
         //      |      |
-        //   --------------    child
-        CalculateLocations.SimpleLoc s4 = cl.new SimpleLoc(101, 103, 50, 250, 0);
+        //   -------------->     child
+        CalculateLocations.SimpleLoc s4 = cl.new SimpleLoc(101, 103, 51, 250, 0);
         assertTrue(cl.overlap(s4, parent));
         assertTrue(cl.overlap(parent, s4));
 
-        // -------             parent
+        // ------->               parent
         //
-        //          -------    child
-        CalculateLocations.SimpleLoc s5 = cl.new SimpleLoc(101, 103, 250, 350, 0);
+        //           ------->     child
+        CalculateLocations.SimpleLoc s5 = cl.new SimpleLoc(101, 103, 251, 350, 0);
         assertFalse(cl.overlap(s5, parent));
         assertFalse(cl.overlap(parent, s5));
 
-        //          -------    parent
+        //           ------->     parent
         //
-        // -------             child
-        CalculateLocations.SimpleLoc s6 = cl.new SimpleLoc(101, 103, 25, 75, 0);
+        // ------->               child
+        CalculateLocations.SimpleLoc s6 = cl.new SimpleLoc(101, 103, 26, 75, 0);
         assertFalse(cl.overlap(s6, parent));
         assertFalse(cl.overlap(parent, s6));
     }
@@ -269,29 +269,29 @@ public class CalculateLocationsTest extends TestCase {
         int childId = 102;
         child.setId(new Integer(childId));
 
-        //   ------------       parent
+        //   ------------>        parent
         //          |
-        //          ---------   child
+        //          --------->    child
         CalculateLocations cl = new CalculateLocations(osw);
-        CalculateLocations.SimpleLoc parentOnChr = cl.new SimpleLoc(parentId, childId, 100, 300, 0);
-        CalculateLocations.SimpleLoc childOnChr = cl.new SimpleLoc(parentId, childId, 250, 450, 0);
+        CalculateLocations.SimpleLoc parentOnChr = cl.new SimpleLoc(parentId, childId, 101, 300, 0);
+        CalculateLocations.SimpleLoc childOnChr = cl.new SimpleLoc(parentId, childId, 251, 450, 0);
         Location res = cl.createLocation(parent, parentOnChr, child, childOnChr);
 
         PartialLocation exp1 = (PartialLocation) DynamicUtil.createObject(Collections.singleton(PartialLocation.class));
-        exp1.setStart(new Integer(150));
+        exp1.setStart(new Integer(151));
         exp1.setEnd(new Integer(200));
         exp1.setStartIsPartial(Boolean.FALSE);
         exp1.setEndIsPartial(Boolean.TRUE);
-        exp1.setSubjectStart(new Integer(0));
+        exp1.setSubjectStart(new Integer(1));
         exp1.setSubjectEnd(new Integer(50));
         exp1.setStrand(new Integer(0));
         exp1.setObject(parent);
         exp1.setSubject(child);
         assertEquals(toItem(exp1), toItem(res));
 
-        //       --------------  parent
+        //       -------------->   parent
         //            |
-        //   ----------          child
+        //   ---------->           child
         cl = new CalculateLocations(osw);
         parentOnChr = cl.new SimpleLoc(parentId, childId, 101, 300, 0);
         childOnChr = cl.new SimpleLoc(parentId, childId, 51, 150, 1);
@@ -302,23 +302,23 @@ public class CalculateLocationsTest extends TestCase {
         exp2.setEnd(new Integer(50));
         exp2.setStartIsPartial(Boolean.TRUE);
         exp2.setEndIsPartial(Boolean.FALSE);
-        exp2.setSubjectStart(new Integer(50));
+        exp2.setSubjectStart(new Integer(51));
         exp2.setSubjectEnd(new Integer(100));
         exp2.setStrand(new Integer(1));
         exp2.setObject(parent);
         exp2.setSubject(child);
         assertEquals(toItem(exp2), toItem(res));
 
-        //  ------------------  parent
+        //  ------------------>   parent
         //      |        |
-        //      ----------      child
+        //      ---------->       child
         cl = new CalculateLocations(osw);
-        parentOnChr = cl.new SimpleLoc(parentId, childId, 100, 300, 1);
-        childOnChr = cl.new SimpleLoc(parentId, childId, 150, 250, -1);
+        parentOnChr = cl.new SimpleLoc(parentId, childId, 101, 300, 1);
+        childOnChr = cl.new SimpleLoc(parentId, childId, 151, 250, -1);
         res = cl.createLocation(parent, parentOnChr, child, childOnChr);
 
         Location exp3 = (Location) DynamicUtil.createObject(Collections.singleton(Location.class));
-        exp3.setStart(new Integer(50));
+        exp3.setStart(new Integer(51));
         exp3.setEnd(new Integer(150));
         exp3.setStartIsPartial(Boolean.FALSE);
         exp3.setEndIsPartial(Boolean.FALSE);
@@ -327,28 +327,26 @@ public class CalculateLocationsTest extends TestCase {
         exp3.setSubject(child);
         assertEquals(toItem(exp3), toItem(res));
 
-        //      --------       parent
+        //      -------->        parent
         //      |      |
-        //   --------------    child
+        //   -------------->     child
         cl = new CalculateLocations(osw);
-        parentOnChr = cl.new SimpleLoc(parentId, childId, 100, 300, 1);
-        childOnChr = cl.new SimpleLoc(parentId, childId, 50, 350, 0);
+        parentOnChr = cl.new SimpleLoc(parentId, childId, 101, 300, 1);
+        childOnChr = cl.new SimpleLoc(parentId, childId, 51, 350, 0);
         res = cl.createLocation(parent, parentOnChr, child, childOnChr);
 
         PartialLocation exp4 = (PartialLocation) DynamicUtil.createObject(Collections.singleton(PartialLocation.class));
-        exp4.setStart(new Integer(0));
+        exp4.setStart(new Integer(1));
         exp4.setEnd(new Integer(200));
         exp4.setStartIsPartial(Boolean.TRUE);
         exp4.setEndIsPartial(Boolean.TRUE);
-        exp4.setSubjectStart(new Integer(50));
+        exp4.setSubjectStart(new Integer(51));
         exp4.setSubjectEnd(new Integer(250));
         exp4.setStrand(new Integer(0));
         exp4.setObject(parent);
         exp4.setSubject(child);
         assertEquals(toItem(exp4), toItem(res));
     }
-
-
 
     private Location createLocation(BioEntity object, BioEntity subject, int strand, int start, int end) {
         Location loc = (Location) DynamicUtil.createObject(Collections.singleton(Location.class));
@@ -359,6 +357,7 @@ public class CalculateLocationsTest extends TestCase {
         loc.setEnd(new Integer(end));
         loc.setStartIsPartial(Boolean.FALSE);
         loc.setEndIsPartial(Boolean.FALSE);
+
         return loc;
     }
 
@@ -419,7 +418,7 @@ public class CalculateLocationsTest extends TestCase {
             bandOnChr.setObject(getChromosome());
             bandOnChr.setSubject(getChromosomeBand());
             bandOnChr.setStrand(new Integer(0));
-            bandOnChr.setStart(new Integer(1000));
+            bandOnChr.setStart(new Integer(1001));
             bandOnChr.setEnd(new Integer(2000));
             bandOnChr.setId(new Integer(103));
         }
