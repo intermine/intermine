8aa148e4b1eabb7be5581f01e6ecdf8d4cf96147
kmr <kmr>
2004-07-02 15:04:31 +0000

    Added testInsertGeneReferences().
    Former-commit-id: f236ebb361ffa3bfe9269b863983aa2c0eaf5745

--- flymine/model/genomic/src/test/org/flymine/postprocess/CreateReferencesTest.java
+++ flymine/model/genomic/src/test/org/flymine/postprocess/CreateReferencesTest.java
@@ -46,18 +46,242 @@ public class CreateReferencesTest extends TestCase {
     private Model model;
     private Gene storedGene = null;
     private Transcript storedTranscript = null;
-    private SimpleRelation storedRelation = null;
+    private Exon storedExon = null;
+    private SimpleRelation storedTranscriptRelation = null;
+    private RankedRelation storedExonRelation = null;
 
     private static final Logger LOG = Logger.getLogger(CreateReferencesTest.class);
 
     public void setUp() throws Exception {
         osw = ObjectStoreWriterFactory.getObjectStoreWriter("osw.genomic-test");
         model = Model.getInstanceByName("genomic");
-        createData();
     }
 
-    public void tearDown() throws Exception {
-        LOG.error("in tear down");
+    public void testInsertReferences() throws Exception {
+        createData();
+        CreateReferences cr = new CreateReferences(osw);
+        cr.insertReferences(Gene.class, Transcript.class, SimpleRelation.class, "transcripts");
+        cr.insertReferences(Transcript.class, Exon.class, RankedRelation.class, "exons");
+
+        Gene expectedGene = (Gene) DynamicUtil.createObject(Collections.singleton(Gene.class));
+        expectedGene.setName("gene1");
+        expectedGene.setId(storedGene.getId());
+  
+        Transcript expectedTranscript =
+            (Transcript) DynamicUtil.createObject(Collections.singleton(Transcript.class));
+        expectedTranscript.setName("trans1");
+        expectedTranscript.setId(storedTranscript.getId());
+        expectedTranscript.setGene(expectedGene);
+        expectedGene.setTranscripts(Arrays.asList(new Object[] { expectedTranscript }));
+  
+        SimpleRelation expectedTranscriptRelation =
+            (SimpleRelation) DynamicUtil.createObject(Collections.singleton(SimpleRelation.class));
+        expectedTranscriptRelation.setId(storedTranscriptRelation.getId());
+        expectedTranscriptRelation.setObject(expectedGene);
+        expectedTranscriptRelation.setSubject(expectedTranscript);
+        expectedTranscript.setObjects(Arrays.asList(new Object[] { expectedTranscriptRelation }));
+        expectedGene.setSubjects(Arrays.asList(new Object[] { expectedTranscriptRelation }));
+  
+        Exon expectedExon = (Exon) DynamicUtil.createObject(Collections.singleton(Exon.class));
+        expectedExon.setName("exon1");
+        expectedExon.setId(storedExon.getId());
+        expectedTranscript.setExons(Arrays.asList(new Object[] {expectedExon}));
+        expectedExon.setTranscripts(Arrays.asList(new Object[] {expectedTranscript}));
+  
+        RankedRelation expectedExonRelation =
+            (RankedRelation) DynamicUtil.createObject(Collections.singleton(RankedRelation.class));
+        expectedExonRelation.setId(storedExonRelation.getId());
+        expectedExonRelation.setRank(new Integer(1));
+        expectedExonRelation.setObject(expectedTranscript);
+        expectedExonRelation.setSubject(expectedExon);
+        expectedTranscript.setSubjects(Arrays.asList(new Object[] { expectedExonRelation }));
+        expectedExon.setObjects(Arrays.asList(new Object[] { expectedExonRelation }));
+  
+        Item expGeneItem = toItem(expectedGene);
+        Item expTranscriptItem = toItem(expectedTranscript);
+        Item expExonItem = toItem(expectedExon);
+  
+        ObjectStore os = osw.getObjectStore();
+  
+        Query q = new Query();
+        QueryClass qcGene = new QueryClass(Gene.class);
+        q.addFrom(qcGene);
+        q.addToSelect(qcGene);
+        Results res = new Results(q, os, os.getSequence());
+        ResultsRow row = (ResultsRow) res.iterator().next();
+  
+        Gene resGene = (Gene) row.get(0);
+        Item resGeneItem = toItem(resGene);
+  
+        org.intermine.web.LogMe.log("i", "ID: " + resGene.getId() + " - " + resGene);
+        org.intermine.web.LogMe.log("i", "Gene transcripts: " + resGene.getTranscripts());
+        org.intermine.web.LogMe.log("i", "Gene exons: " + resGene.getExons());
+
+        assertEquals(expGeneItem, resGeneItem);
+  
+        q = new Query();
+        QueryClass qcTranscript = new QueryClass(Transcript.class);
+        q.addFrom(qcTranscript);
+        q.addToSelect(qcTranscript);
+  
+        res = new Results(q, os, os.getSequence());
+        row = (ResultsRow) res.iterator().next();
+  
+        Transcript resTranscript = (Transcript) row.get(0);
+        Item resTranscriptItem = toItem(resTranscript);
+        org.intermine.web.LogMe.log("i", "" + resTranscript);
+        assertEquals(expTranscriptItem, resTranscriptItem);
+  
+        q = new Query();
+        QueryClass qcExon = new QueryClass(Exon.class);
+        q.addFrom(qcExon);
+        q.addToSelect(qcExon);
+  
+        res = new Results(q, os, os.getSequence());
+        row = (ResultsRow) res.iterator().next();
+  
+        Exon resExon = (Exon) row.get(0);
+        Item resExonItem = toItem(resExon);
+        org.intermine.web.LogMe.log("i", "" + resExon);
+        assertEquals(expExonItem, resExonItem);
+        removeData();
+    }
+
+// waiting for a fix to the ObjectStore to stop multiple identical entries in the indirection
+// tables
+    public void testInsertGeneReferences() throws Exception {
+//         createData();
+//         CreateReferences cr = new CreateReferences(osw);
+//         cr.insertReferences(Gene.class, Transcript.class, SimpleRelation.class, "transcripts");
+//         cr.insertReferences(Transcript.class, Exon.class, RankedRelation.class, "exons");
+//         cr.insertGeneExonReferences("exons");
+
+//         Gene expectedGene = (Gene) DynamicUtil.createObject(Collections.singleton(Gene.class));
+//         expectedGene.setName("gene1");
+//         expectedGene.setId(storedGene.getId());
+  
+//         Transcript expectedTranscript =
+//             (Transcript) DynamicUtil.createObject(Collections.singleton(Transcript.class));
+//         expectedTranscript.setName("trans1");
+//         expectedTranscript.setId(storedTranscript.getId());
+//         expectedTranscript.setGene(expectedGene);
+//         expectedGene.setTranscripts(Arrays.asList(new Object[] { expectedTranscript }));
+  
+//         SimpleRelation expectedTranscriptRelation =
+//             (SimpleRelation) DynamicUtil.createObject(Collections.singleton(SimpleRelation.class));
+//         expectedTranscriptRelation.setId(storedTranscriptRelation.getId());
+//         expectedTranscriptRelation.setObject(expectedGene);
+//         expectedTranscriptRelation.setSubject(expectedTranscript);
+//         expectedTranscript.setObjects(Arrays.asList(new Object[] { expectedTranscriptRelation }));
+//         expectedGene.setSubjects(Arrays.asList(new Object[] { expectedTranscriptRelation }));
+  
+//         Exon expectedExon = (Exon) DynamicUtil.createObject(Collections.singleton(Exon.class));
+//         expectedExon.setName("exon1");
+//         expectedExon.setId(storedExon.getId());
+//         expectedTranscript.setExons(Arrays.asList(new Object[] {expectedExon}));
+//         expectedExon.setTranscripts(Arrays.asList(new Object[] {expectedTranscript}));
+//         expectedGene.setExons(Arrays.asList(new Object[] {expectedExon}));
+//         expectedExon.setGene(expectedGene);
+  
+//         RankedRelation expectedExonRelation =
+//             (RankedRelation) DynamicUtil.createObject(Collections.singleton(RankedRelation.class));
+//         expectedExonRelation.setId(storedExonRelation.getId());
+//         expectedExonRelation.setRank(new Integer(1));
+//         expectedExonRelation.setObject(expectedTranscript);
+//         expectedExonRelation.setSubject(expectedExon);
+//         expectedTranscript.setSubjects(Arrays.asList(new Object[] { expectedExonRelation }));
+//         expectedExon.setObjects(Arrays.asList(new Object[] { expectedExonRelation }));
+  
+//         Item expGeneItem = toItem(expectedGene);
+//         Item expTranscriptItem = toItem(expectedTranscript);
+//         Item expExonItem = toItem(expectedExon);
+  
+//         ObjectStore os = osw.getObjectStore();
+  
+//         Query q = new Query();
+//         QueryClass qcGene = new QueryClass(Gene.class);
+//         q.addFrom(qcGene);
+//         q.addToSelect(qcGene);
+//         Results res = new Results(q, os, os.getSequence());
+//         ResultsRow row = (ResultsRow) res.iterator().next();
+  
+//         Gene resGene = (Gene) row.get(0);
+//         Item resGeneItem = toItem(resGene);
+  
+//         org.intermine.web.LogMe.log("i", "------------------------------------------------------");
+//         org.intermine.web.LogMe.log("i", "gene from ObjectStore in test: "
+//                                     + resGene.getId() + " - " + resGene);
+//         org.intermine.web.LogMe.log("i", "Gene transcripts: " + resGene.getTranscripts());
+//         org.intermine.web.LogMe.log("i", "Gene exons: " + resGene.getExons());
+
+//         assertEquals(expGeneItem, resGeneItem);
+  
+//         q = new Query();
+//         QueryClass qcTranscript = new QueryClass(Transcript.class);
+//         q.addFrom(qcTranscript);
+//         q.addToSelect(qcTranscript);
+  
+//         res = new Results(q, os, os.getSequence());
+//         row = (ResultsRow) res.iterator().next();
+  
+//         Transcript resTranscript = (Transcript) row.get(0);
+//         Item resTranscriptItem = toItem(resTranscript);
+//         org.intermine.web.LogMe.log("i", "" + resTranscript);
+//         assertEquals(expTranscriptItem, resTranscriptItem);
+  
+//         q = new Query();
+//         QueryClass qcExon = new QueryClass(Exon.class);
+//         q.addFrom(qcExon);
+//         q.addToSelect(qcExon);
+  
+//         res = new Results(q, os, os.getSequence());
+//         row = (ResultsRow) res.iterator().next();
+  
+//         Exon resExon = (Exon) row.get(0);
+//         Item resExonItem = toItem(resExon);
+//         org.intermine.web.LogMe.log("i", "" + resExon);
+//         assertEquals(expExonItem, resExonItem);
+//         removeData();
+    }
+    
+    private void createData() throws Exception {
+        osw.flushObjectById();
+
+        storedGene = (Gene) DynamicUtil.createObject(Collections.singleton(Gene.class));
+        storedGene.setName("gene1");
+        
+        storedTranscript =
+            (Transcript) DynamicUtil.createObject(Collections.singleton(Transcript.class));
+        storedTranscript.setName("trans1");
+        
+        storedExon = (Exon) DynamicUtil.createObject(Collections.singleton(Exon.class));
+        storedExon.setName("exon1");
+        
+        storedTranscriptRelation =
+            (SimpleRelation) DynamicUtil.createObject(Collections.singleton(SimpleRelation.class));
+        storedTranscriptRelation.setObject(storedGene);
+        storedTranscriptRelation.setSubject(storedTranscript);
+        
+        storedExonRelation =
+            (RankedRelation) DynamicUtil.createObject(Collections.singleton(RankedRelation.class));
+        storedExonRelation.setObject(storedTranscript);
+        storedExonRelation.setSubject(storedExon);
+        storedExonRelation.setRank(new Integer(1));
+        
+        Set toStore = new HashSet(Arrays.asList(new Object[] {
+                                                    storedGene, storedTranscriptRelation,
+                                                    storedExonRelation, storedTranscript,
+                                                    storedExon
+                                                }));
+        Iterator i = toStore.iterator();
+        osw.beginTransaction();
+        while (i.hasNext()) {
+            osw.store((InterMineObject) i.next());
+        }
+        osw.commitTransaction();
+    }
+    
+    private void removeData() throws Exception {
         Query q = new Query();
         QueryClass qc = new QueryClass(InterMineObject.class);
         q.addFrom(qc);
@@ -73,68 +297,6 @@ public class CreateReferencesTest extends TestCase {
             osw.delete(o);
         }
         osw.close();
-        LOG.error("closed objectstore");
-    }
-
-
-    public void testCreatesReferencesToGene() throws Exception {
-        CreateReferences cr = new CreateReferences(osw);
-        cr.insertReferences();
-        
-        Gene expectedGene = (Gene) DynamicUtil.createObject(Collections.singleton(Gene.class));
-        expectedGene.setName("adh");
-        expectedGene.setId(storedGene.getId());
-
-        Transcript expectedTranscript =
-            (Transcript) DynamicUtil.createObject(Collections.singleton(Transcript.class));
-        expectedTranscript.setName("trans1");
-        expectedTranscript.setId(storedTranscript.getId());
-        
-        SimpleRelation expectedRelation =
-            (SimpleRelation) DynamicUtil.createObject(Collections.singleton(SimpleRelation.class));
-        expectedRelation.setId(storedRelation.getId());
-
-        List expectedTranscriptsList = Arrays.asList(new Object[] { expectedTranscript });
-        expectedGene.setTranscripts(expectedTranscriptsList);
-
-        List expectedRelationsList = Arrays.asList(new Object[] { expectedRelation });
-        expectedGene.setSubjects(expectedRelationsList);
-
-        Item expGeneItem = toItem(expectedGene);
-
-        Query q = new Query();
-        QueryClass qcObj = new QueryClass(Gene.class);
-        q.addFrom(qcObj);
-        q.addToSelect(qcObj);
-        ObjectStore os = osw.getObjectStore();
-        Results res = new Results(q, os, os.getSequence());
-        ResultsRow row = (ResultsRow) res.iterator().next();
-        Gene resGene = (Gene) row.get(0);
-
-        Item resGeneItem = toItem(resGene);
-        assertEquals(expGeneItem, resGeneItem);
-    }
-
-    private void createData() throws Exception {
-        storedGene = (Gene) DynamicUtil.createObject(Collections.singleton(Gene.class));
-        storedGene.setName("adh");
-
-        storedTranscript =
-            (Transcript) DynamicUtil.createObject(Collections.singleton(Transcript.class));
-        storedTranscript.setName("tran1");
-
-        storedRelation =
-            (SimpleRelation) DynamicUtil.createObject(Collections.singleton(SimpleRelation.class));
-        storedRelation.setObject(storedGene);
-        storedRelation.setSubject(storedTranscript);
-
-        Set toStore = new HashSet(Arrays.asList(new Object[] {
-                                                    storedGene, storedRelation, storedTranscript
-                                                }));
-        Iterator i = toStore.iterator();
-        while (i.hasNext()) {
-            osw.store((InterMineObject) i.next());
-        }
     }
 
     private Item toItem(InterMineObject o) {
