9cb0b7ce535fd5eaf1470124bdfeb63ef40e870a
kmr <kmr>
2004-07-07 14:06:51 +0000

    compareResultsToExpected() now performs a more complete test. Changed to test the new CreateReferences.insertReferences() method.

--- flymine/model/genomic/src/test/org/flymine/postprocess/CreateReferencesTest.java
+++ flymine/model/genomic/src/test/org/flymine/postprocess/CreateReferencesTest.java
@@ -44,9 +44,11 @@ public class CreateReferencesTest extends TestCase {
 
     private ObjectStoreWriter osw;
     private Model model;
+    private Chromosome storedChromosome = null;
     private Gene storedGene = null;
     private Transcript storedTranscript = null;
     private Exon storedExon = null;
+    private Location storedChromosomeRelation = null;
     private SimpleRelation storedTranscriptRelation = null;
     private RankedRelation storedExonRelation = null;
 
@@ -57,23 +59,125 @@ public class CreateReferencesTest extends TestCase {
         model = Model.getInstanceByName("genomic");
     }
 
-    public void testInsertReferences() throws Exception {
+    public void testInsertGeneTranscriptReferences() throws Exception {
         createData();
+        CalculateLocations cl = new CalculateLocations(osw);
+        cl.fixPartials();
+        cl.createLocations();
         CreateReferences cr = new CreateReferences(osw);
         cr.insertReferences(Gene.class, Transcript.class, SimpleRelation.class, "transcripts");
-        cr.insertReferences(Transcript.class, Exon.class, RankedRelation.class, "exons");
+  
+        compareGeneTranscriptResultsToExpected();
+        removeData();
+    }
+
+    public void testInsertChromosomeExonReferences() throws Exception {
+        createData();
+        CalculateLocations cl = new CalculateLocations(osw);
+        cl.fixPartials();
+        cl.createLocations();
+        CreateReferences cr = new CreateReferences(osw);
+        cr.insertReferences(Chromosome.class, "subjects", Relation.class, "subject",
+                            Exon.class, "chromosome");
+
+        compareChromosomeExonResultsToExpected();
+        removeData();
+    }
+
+    public void testInsertReferences() throws Exception {
+        createData();
+        CalculateLocations cl = new CalculateLocations(osw);
+        cl.fixPartials();
+        cl.createLocations();
+        CreateReferences cr = new CreateReferences(osw);
+        cr.insertReferences();
+
+        compareResultsToExpected();
+        removeData();
+    }
+
+    private void compareChromosomeExonResultsToExpected() throws Exception {
+        osw.flushObjectById();
+        Exon expectedExon = (Exon) DynamicUtil.createObject(Collections.singleton(Exon.class));
+        expectedExon.setName("exon1");
+        expectedExon.setId(storedExon.getId());
+
+        Chromosome expectedChromosome =
+            (Chromosome) DynamicUtil.createObject(Collections.singleton(Chromosome.class));
+        expectedChromosome.setName("chr1");
+        expectedChromosome.setId(storedChromosome.getId());
+
+        Relation expectedChromosomeRelation =
+            (Relation) DynamicUtil.createObject(Collections.singleton(Relation.class));
+        expectedChromosomeRelation.setId(storedChromosomeRelation.getId());
+        expectedChromosomeRelation.setObject(expectedChromosome);
+        expectedChromosomeRelation.setSubject(expectedExon);
+        expectedChromosome.setSubjects(Arrays.asList(new Object[] { expectedChromosomeRelation }));
+        expectedChromosome.setExons(Arrays.asList(new Object[] { expectedExon }));
+        expectedExon.setChromosome(expectedChromosome);
+
+        RankedRelation expectedExonRelation =
+            (RankedRelation) DynamicUtil.createObject(Collections.singleton(RankedRelation.class));
+        expectedExonRelation.setId(storedExonRelation.getId());
+        expectedExonRelation.setRank(new Integer(1));
+        expectedExonRelation.setSubject(expectedExon);
+        expectedExon.setObjects(Arrays.asList(new Object[] {
+                                                  expectedChromosomeRelation,
+                                                  expectedExonRelation
+                                              }));
+
+        Item expExonItem = toItem(expectedExon);
+        Item expChromosomeItem = toItem(expectedChromosome);
+
+        ObjectStore os = osw.getObjectStore();
+
+        os.flushObjectById();
+
+        Query q;
+        Results res;
+        ResultsRow row;
+
+
+        q = new Query();
+        QueryClass qcChromosome = new QueryClass(Chromosome.class);
+        q.addFrom(qcChromosome);
+        q.addToSelect(qcChromosome);
+
+        res = new Results(q, os, os.getSequence());
+        row = (ResultsRow) res.iterator().next();
+
+        Chromosome resChromosome = (Chromosome) row.get(0);
+        Item resChromosomeItem = toItem(resChromosome);
+        assertEquals(expChromosomeItem, resChromosomeItem);
+
+
+        q = new Query();
+        QueryClass qcExon = new QueryClass(Exon.class);
+        q.addFrom(qcExon);
+        q.addToSelect(qcExon);
+
+        res = new Results(q, os, os.getSequence());
+        row = (ResultsRow) res.iterator().next();
+
+        Exon resExon = (Exon) row.get(0);
+        Item resExonItem = toItem(resExon);
+        assertEquals(expExonItem, resExonItem);
+    }
+
+    private void compareGeneTranscriptResultsToExpected() throws Exception {
+        osw.flushObjectById();
 
         Gene expectedGene = (Gene) DynamicUtil.createObject(Collections.singleton(Gene.class));
         expectedGene.setName("gene1");
         expectedGene.setId(storedGene.getId());
-  
+
         Transcript expectedTranscript =
             (Transcript) DynamicUtil.createObject(Collections.singleton(Transcript.class));
         expectedTranscript.setName("trans1");
         expectedTranscript.setId(storedTranscript.getId());
         expectedTranscript.setGene(expectedGene);
         expectedGene.setTranscripts(Arrays.asList(new Object[] { expectedTranscript }));
-  
+
         SimpleRelation expectedTranscriptRelation =
             (SimpleRelation) DynamicUtil.createObject(Collections.singleton(SimpleRelation.class));
         expectedTranscriptRelation.setId(storedTranscriptRelation.getId());
@@ -81,72 +185,56 @@ public class CreateReferencesTest extends TestCase {
         expectedTranscriptRelation.setSubject(expectedTranscript);
         expectedTranscript.setObjects(Arrays.asList(new Object[] { expectedTranscriptRelation }));
         expectedGene.setSubjects(Arrays.asList(new Object[] { expectedTranscriptRelation }));
-  
-        Exon expectedExon = (Exon) DynamicUtil.createObject(Collections.singleton(Exon.class));
-        expectedExon.setName("exon1");
-        expectedExon.setId(storedExon.getId());
-        expectedTranscript.setExons(Arrays.asList(new Object[] {expectedExon}));
-        expectedExon.setTranscripts(Arrays.asList(new Object[] {expectedTranscript}));
-  
+
         RankedRelation expectedExonRelation =
             (RankedRelation) DynamicUtil.createObject(Collections.singleton(RankedRelation.class));
         expectedExonRelation.setId(storedExonRelation.getId());
         expectedExonRelation.setRank(new Integer(1));
         expectedExonRelation.setObject(expectedTranscript);
-        expectedExonRelation.setSubject(expectedExon);
         expectedTranscript.setSubjects(Arrays.asList(new Object[] { expectedExonRelation }));
-        expectedExon.setObjects(Arrays.asList(new Object[] { expectedExonRelation }));
-  
+
         Item expGeneItem = toItem(expectedGene);
         Item expTranscriptItem = toItem(expectedTranscript);
-        Item expExonItem = toItem(expectedExon);
-  
+
         ObjectStore os = osw.getObjectStore();
-  
-        Query q = new Query();
-        QueryClass qcGene = new QueryClass(Gene.class);
-        q.addFrom(qcGene);
-        q.addToSelect(qcGene);
-        Results res = new Results(q, os, os.getSequence());
-        ResultsRow row = (ResultsRow) res.iterator().next();
-  
-        Gene resGene = (Gene) row.get(0);
-        Item resGeneItem = toItem(resGene);
-  
-        assertEquals(expGeneItem, resGeneItem);
-  
+
+        os.flushObjectById();
+
+        Query q;
+        Results res;
+        ResultsRow row;
+
+
         q = new Query();
         QueryClass qcTranscript = new QueryClass(Transcript.class);
         q.addFrom(qcTranscript);
         q.addToSelect(qcTranscript);
-  
+
         res = new Results(q, os, os.getSequence());
         row = (ResultsRow) res.iterator().next();
-  
+
         Transcript resTranscript = (Transcript) row.get(0);
         Item resTranscriptItem = toItem(resTranscript);
         assertEquals(expTranscriptItem, resTranscriptItem);
-  
+
+
+
         q = new Query();
-        QueryClass qcExon = new QueryClass(Exon.class);
-        q.addFrom(qcExon);
-        q.addToSelect(qcExon);
-  
+        QueryClass qcGene = new QueryClass(Gene.class);
+        q.addFrom(qcGene);
+        q.addToSelect(qcGene);
         res = new Results(q, os, os.getSequence());
         row = (ResultsRow) res.iterator().next();
-  
-        Exon resExon = (Exon) row.get(0);
-        Item resExonItem = toItem(resExon);
-        assertEquals(expExonItem, resExonItem);
-        removeData();
+
+        Gene resGene = (Gene) row.get(0);
+        Item resGeneItem = toItem(resGene);
+        assertEquals(expGeneItem, resGeneItem);
+
+
     }
 
-    public void testInsertGeneReferences() throws Exception {
-        createData();
-        CreateReferences cr = new CreateReferences(osw);
-        cr.insertReferences(Gene.class, Transcript.class, SimpleRelation.class, "transcripts");
-        cr.insertReferences(Transcript.class, Exon.class, RankedRelation.class, "exons");
-        cr.insertGeneExonReferences("exons");
+    private void compareResultsToExpected() throws Exception {
+        osw.flushObjectById();
 
         Gene expectedGene = (Gene) DynamicUtil.createObject(Collections.singleton(Gene.class));
         expectedGene.setName("gene1");
@@ -182,25 +270,41 @@ public class CreateReferencesTest extends TestCase {
         expectedExonRelation.setObject(expectedTranscript);
         expectedExonRelation.setSubject(expectedExon);
         expectedTranscript.setSubjects(Arrays.asList(new Object[] { expectedExonRelation }));
-        expectedExon.setObjects(Arrays.asList(new Object[] { expectedExonRelation }));
+
+        Chromosome expectedChromosome =
+            (Chromosome) DynamicUtil.createObject(Collections.singleton(Chromosome.class));
+        expectedChromosome.setName("chr1");
+        expectedChromosome.setId(storedChromosome.getId());
+
+        Relation expectedChromosomeRelation =
+            (Relation) DynamicUtil.createObject(Collections.singleton(Relation.class));
+        expectedChromosomeRelation.setId(storedChromosomeRelation.getId());
+        expectedChromosomeRelation.setObject(expectedChromosome);
+        expectedChromosomeRelation.setSubject(expectedExon);
+        expectedChromosome.setSubjects(Arrays.asList(new Object[] { expectedChromosomeRelation }));
+        expectedChromosome.setExons(Arrays.asList(new Object[] { expectedExon }));
+        expectedChromosome.setGenes(Arrays.asList(new Object[] { expectedGene }));
+        expectedChromosome.setTranscripts(Arrays.asList(new Object[] { expectedTranscript }));
+        expectedGene.setChromosome(expectedChromosome);
+        expectedTranscript.setChromosome(expectedChromosome);
+        expectedExon.setChromosome(expectedChromosome);
+        expectedExon.setObjects(Arrays.asList(new Object[] {
+                                                  expectedChromosomeRelation, expectedExonRelation
+                                              }));
 
         Item expGeneItem = toItem(expectedGene);
         Item expTranscriptItem = toItem(expectedTranscript);
         Item expExonItem = toItem(expectedExon);
+        Item expChromosomeItem = toItem(expectedChromosome);
 
         ObjectStore os = osw.getObjectStore();
 
-        Query q = new Query();
-        QueryClass qcGene = new QueryClass(Gene.class);
-        q.addFrom(qcGene);
-        q.addToSelect(qcGene);
-        Results res = new Results(q, os, os.getSequence());
-        ResultsRow row = (ResultsRow) res.iterator().next();
+        os.flushObjectById();
 
-        Gene resGene = (Gene) row.get(0);
-        Item resGeneItem = toItem(resGene);
+        Query q;
+        Results res;
+        ResultsRow row;
 
-        assertEquals(expGeneItem, resGeneItem);
 
         q = new Query();
         QueryClass qcTranscript = new QueryClass(Transcript.class);
@@ -214,6 +318,32 @@ public class CreateReferencesTest extends TestCase {
         Item resTranscriptItem = toItem(resTranscript);
         assertEquals(expTranscriptItem, resTranscriptItem);
 
+
+
+        q = new Query();
+        QueryClass qcGene = new QueryClass(Gene.class);
+        q.addFrom(qcGene);
+        q.addToSelect(qcGene);
+        res = new Results(q, os, os.getSequence());
+        row = (ResultsRow) res.iterator().next();
+
+        Gene resGene = (Gene) row.get(0);
+        Item resGeneItem = toItem(resGene);
+        assertEquals(expGeneItem, resGeneItem);
+
+        q = new Query();
+        QueryClass qcChromosome = new QueryClass(Chromosome.class);
+        q.addFrom(qcChromosome);
+        q.addToSelect(qcChromosome);
+
+        res = new Results(q, os, os.getSequence());
+        row = (ResultsRow) res.iterator().next();
+
+        Chromosome resChromosome = (Chromosome) row.get(0);
+        Item resChromosomeItem = toItem(resChromosome);
+        assertEquals(expChromosomeItem, resChromosomeItem);
+
+
         q = new Query();
         QueryClass qcExon = new QueryClass(Exon.class);
         q.addFrom(qcExon);
@@ -225,37 +355,49 @@ public class CreateReferencesTest extends TestCase {
         Exon resExon = (Exon) row.get(0);
         Item resExonItem = toItem(resExon);
         assertEquals(expExonItem, resExonItem);
-        removeData();
+
     }
-    
+
     private void createData() throws Exception {
         osw.flushObjectById();
 
+        storedChromosome = (Chromosome) DynamicUtil.createObject(Collections.singleton(Chromosome.class));
+        storedChromosome.setName("chr1");
+
         storedGene = (Gene) DynamicUtil.createObject(Collections.singleton(Gene.class));
         storedGene.setName("gene1");
-        
+
         storedTranscript =
             (Transcript) DynamicUtil.createObject(Collections.singleton(Transcript.class));
         storedTranscript.setName("trans1");
-        
+        // currently the gene reference in Transcript is set before post-processing but the
+        // transcripts reference in Gene isn't set
+        storedTranscript.setGene(storedGene);
+
         storedExon = (Exon) DynamicUtil.createObject(Collections.singleton(Exon.class));
         storedExon.setName("exon1");
-        
+
         storedTranscriptRelation =
             (SimpleRelation) DynamicUtil.createObject(Collections.singleton(SimpleRelation.class));
         storedTranscriptRelation.setObject(storedGene);
         storedTranscriptRelation.setSubject(storedTranscript);
-        
+
         storedExonRelation =
             (RankedRelation) DynamicUtil.createObject(Collections.singleton(RankedRelation.class));
         storedExonRelation.setObject(storedTranscript);
         storedExonRelation.setSubject(storedExon);
         storedExonRelation.setRank(new Integer(1));
-        
+
+        storedChromosomeRelation =
+            (Location) DynamicUtil.createObject(Collections.singleton(Location.class));
+        storedChromosomeRelation.setObject(storedChromosome);
+        storedChromosomeRelation.setSubject(storedExon);
+
         Set toStore = new HashSet(Arrays.asList(new Object[] {
                                                     storedGene, storedTranscriptRelation,
                                                     storedExonRelation, storedTranscript,
-                                                    storedExon
+                                                    storedExon, storedChromosomeRelation,
+                                                    storedChromosome
                                                 }));
         Iterator i = toStore.iterator();
         osw.beginTransaction();
@@ -263,8 +405,8 @@ public class CreateReferencesTest extends TestCase {
             osw.store((InterMineObject) i.next());
         }
         osw.commitTransaction();
-    }
-    
+   }
+
     private void removeData() throws Exception {
         Query q = new Query();
         QueryClass qc = new QueryClass(InterMineObject.class);
@@ -290,4 +432,5 @@ public class CreateReferencesTest extends TestCase {
         Item item = FullRenderer.toItem(o, model);
         return item;
     }
+
 }
