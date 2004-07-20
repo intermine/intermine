8db402bf8eca43a074afd3687458cfe7728d2a17
mark <mark>
2004-07-20 09:24:37 +0000

    Fix a silly bug when ignoring the "id" field

--- intermine/src/java/org/intermine/web/MainHelper.java
+++ intermine/src/java/org/intermine/web/MainHelper.java
@@ -79,7 +79,7 @@ public class MainHelper
             String fieldName = fd.getName();
 
             if (fieldName.equals("id")) {
-                return;
+                continue;
             }
 
             String head, tail;
