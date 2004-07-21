ce27015dc1f2b421ca0e386151a1f24b2b50974f
rnsmith <rns>
2004-07-21 18:44:51 +0000

    ensembl orgnismdbid always set to a unique value, _flymine_x if not unique
    Former-commit-id: 87d31d18b6b8c71ec41f8786240d846a261cbcf8

--- flymine/model/ensembl/src/test/org/flymine/dataconversion/EnsemblDataTranslatorTest.java
+++ flymine/model/ensembl/src/test/org/flymine/dataconversion/EnsemblDataTranslatorTest.java
@@ -20,6 +20,7 @@ import java.util.LinkedHashMap;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.io.InputStreamReader;
 
 import com.hp.hpl.jena.ontology.OntModel;
@@ -29,6 +30,7 @@ import org.intermine.xml.full.FullParser;
 import org.intermine.xml.full.Item;
 import org.intermine.xml.full.Attribute;
 import org.intermine.xml.full.Reference;
+import org.intermine.xml.full.ReferenceList;
 import org.intermine.dataconversion.DataTranslator;
 import org.intermine.dataconversion.DataTranslatorTestCase;
 import org.intermine.dataconversion.MockItemReader;
@@ -83,6 +85,63 @@ public class EnsemblDataTranslatorTest extends DataTranslatorTestCase {
         Item tgtItem = createItem(tgtNs + "Gene", "1_1", "");
         assertEquals(expected, translator.setGeneSynonyms(gene, tgtItem, srcNs));
     }
+
+    // two genes with same flybase_gene (FBgn***) - expect one to get
+    // _flymine_1 on the end of organismDbId
+    public void testDuplicatedFlyBaseIds() throws Exception {
+        String srcNs = "http://www.flymine.org/model/ensembl#";
+        Item gene = createItem(srcNs + "gene", "1_1", "");
+        Item transcript = createItem(srcNs + "transcript", "1_2", "");
+        Item translation = createItem(srcNs + "translation", "1_3", "");
+        transcript.addReference(new Reference("gene", "1_1"));
+        transcript.addReference(new Reference("translation", "1_3"));
+
+        Item objectXref = createItem(srcNs + "object_xref", "1_4", "");
+        Item xref = createItem(srcNs + "xref", "1_5", "");
+        Item externalDb = createItem(srcNs + "external_db", "1_6", "");
+        objectXref.addReference(new Reference("ensembl", "1_3"));
+        objectXref.addReference(new Reference("xref", "1_5"));
+        xref.addAttribute(new Attribute("dbprimary_acc", "FBgn1001"));
+        xref.addReference(new Reference("external_db", "1_6"));
+        externalDb.addAttribute(new Attribute("db_name", "flybase_gene"));
+
+        Item gene2 = createItem(srcNs + "gene", "2_1", "");
+        Item transcript2 = createItem(srcNs + "transcript", "2_2", "");
+        Item translation2 = createItem(srcNs + "translation", "2_3", "");
+        transcript2.addReference(new Reference("gene", "2_1"));
+        transcript2.addReference(new Reference("translation", "2_3"));
+
+        Item objectXref2 = createItem(srcNs + "object_xref", "2_4", "");
+        Item xref2 = createItem(srcNs + "xref", "2_5", "");
+        Item externalDb2 = createItem(srcNs + "external_db", "2_6", "");
+        objectXref2.addReference(new Reference("ensembl", "2_3"));
+        objectXref2.addReference(new Reference("xref", "2_5"));
+        xref2.addAttribute(new Attribute("dbprimary_acc", "FBgn1001"));
+        xref2.addReference(new Reference("external_db", "2_6"));
+        externalDb2.addAttribute(new Attribute("db_name", "flybase_gene"));
+
+        Map itemMap = writeItems(new HashSet(Arrays.asList(new Object[] {gene, transcript, translation, objectXref, xref, externalDb, gene2, transcript2, translation2, objectXref2, xref2, externalDb2})));
+        EnsemblDataTranslator translator = new EnsemblDataTranslator(new MockItemReader(itemMap),
+                                                                      getOwlModel(), tgtNs, "WB");
+
+
+        Item exp1 = createItem(tgtNs + "Gene", "1_1", "");
+        exp1.addAttribute(new Attribute("organismDbId", "FBgn1001"));
+        ReferenceList rl1 = new ReferenceList("synonyms", new ArrayList(Collections.singleton("-1_1")));
+        exp1.addCollection(rl1);
+        Item tgtItem1 = createItem(tgtNs + "Gene", "1_1", "");
+        translator.setGeneSynonyms(gene, tgtItem1, srcNs);
+        assertEquals(exp1, tgtItem1);
+
+        Item exp2 = createItem(tgtNs + "Gene", "2_1", "");
+        exp2.addAttribute(new Attribute("organismDbId", "FBgn1001_flymine_1"));
+        ReferenceList rl2 = new ReferenceList("synonyms", new ArrayList(Collections.singleton("-1_3")));
+        exp2.addCollection(rl2);
+        Item tgtItem2 = createItem(tgtNs + "Gene", "2_1", "");
+        translator.setGeneSynonyms(gene2, tgtItem2, srcNs);
+        assertEquals(exp2, tgtItem2);
+    }
+
 
     protected Collection getExpectedItems() throws Exception {
         return FullParser.parse(getClass().getClassLoader().getResourceAsStream("test/EnsemblDataTranslatorFunctionalTest_tgt.xml"));
