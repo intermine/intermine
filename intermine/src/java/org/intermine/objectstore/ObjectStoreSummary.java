e3007f7b5f2ffb31d455b34e159a03251def5a0d
kmr <kmr>
2004-09-21 11:33:15 +0000

    ObjectStoreSummary now gets it's information from a previously generated properties file.

--- intermine/src/java/org/intermine/objectstore/ObjectStoreSummary.java
+++ intermine/src/java/org/intermine/objectstore/ObjectStoreSummary.java
@@ -12,10 +12,8 @@ package org.intermine.objectstore;
 
 import java.util.Map;
 import java.util.HashMap;
-
-import org.intermine.objectstore.query.Query;
-import org.intermine.objectstore.query.QueryField;
-import org.intermine.objectstore.query.QueryClass;
+import java.util.Properties;
+import java.util.Iterator;
 
 /**
  * A summary of an ObjectStore.
@@ -25,37 +23,37 @@ import org.intermine.objectstore.query.QueryClass;
 
 public class ObjectStoreSummary
 {
-    private final ObjectStore os;
-    private final Map counts = new HashMap();
+    private final Map classCounts = new HashMap();
+
+    static final String CLASS_COUNTS_SUFFIX = ".classCount";
 
     /**
      * Create a new ObjectStoreSummary object.
-     * @param os the ObjectStore to summarise.
+     * @param properties the Properties object to retrieve the summary from
      */
-    public ObjectStoreSummary (ObjectStore os) {
-        this.os = os;
+    public ObjectStoreSummary(Properties properties) {
+        Iterator keyIterator = properties.keySet().iterator();
+        while (keyIterator.hasNext()) {
+            String key = (String) keyIterator.next();
+
+            if (key.endsWith(CLASS_COUNTS_SUFFIX)) {
+                String className = key.substring(0, key.length() - CLASS_COUNTS_SUFFIX.length());
+                Integer count = Integer.valueOf((String) properties.get(key));
+                classCounts.put(className, count);
+            }
+        }
     }
 
     /**
      * Get the number of instances of a particular class in the ObjectStore.
      * @param className the class name to look up
-     * @throws ObjectStoreException if an error occurs during the database query
-     * @throws ClassNotFoundException if the className doesn't refer to a known class
      * @return the count of the instances of the class
+     * @throws ObjectStoreException if there is a problem with the ObjectStore
+     * @throws ClassNotFoundException if className is unknown
      */
     public int getClassCount(String className)
         throws ObjectStoreException, ClassNotFoundException {
-        Query q = new Query();
-        QueryClass qc = new QueryClass(Class.forName(className));
-        q.addToSelect(new QueryField(qc, "id"));
-        q.addFrom(qc);
 
-        if (counts.get(className) != null) {
-            return ((Integer) counts.get(className)).intValue();
-        } else {
-            int count = os.count(q, os.getSequence());
-            counts.put(className, new Integer(count));
-            return count;
-        }
+        return ((Integer) classCounts.get(className)).intValue();
     }
 }
