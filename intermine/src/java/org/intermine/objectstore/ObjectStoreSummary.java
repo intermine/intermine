1806ee8ae793240fd2cfa1856f6b2101d339de06
kmr <kmr>
2004-09-23 08:43:31 +0000

    Implemented getFieldValues().

--- intermine/src/java/org/intermine/objectstore/ObjectStoreSummary.java
+++ intermine/src/java/org/intermine/objectstore/ObjectStoreSummary.java
@@ -11,9 +11,13 @@ package org.intermine.objectstore;
  */
 
 import java.util.Map;
+import java.util.List;
 import java.util.HashMap;
 import java.util.Properties;
 import java.util.Iterator;
+import java.util.Arrays;
+
+import org.intermine.util.StringUtil;
 
 /**
  * A summary of an ObjectStore.
@@ -23,9 +27,11 @@ import java.util.Iterator;
 
 public class ObjectStoreSummary
 {
-    private final Map classCounts = new HashMap();
+    private final Map classCountsMap = new HashMap();
+    private final Map fieldValuesMap = new HashMap();
 
     static final String CLASS_COUNTS_SUFFIX = ".classCount";
+    static final String FIELDS_SUFFIX = ".fieldValues";
 
     /**
      * Create a new ObjectStoreSummary object.
@@ -39,21 +45,44 @@ public class ObjectStoreSummary
             if (key.endsWith(CLASS_COUNTS_SUFFIX)) {
                 String className = key.substring(0, key.length() - CLASS_COUNTS_SUFFIX.length());
                 Integer count = Integer.valueOf((String) properties.get(key));
-                classCounts.put(className, count);
+                classCountsMap.put(className, count);
+            } else if (key.endsWith(FIELDS_SUFFIX)) {
+                String classAndFieldName = key.substring(0, key.length() - FIELDS_SUFFIX.length());
+                String className =
+                    classAndFieldName.substring(0, classAndFieldName.lastIndexOf("."));
+                String fieldName =
+                    classAndFieldName.substring(classAndFieldName.lastIndexOf(".") + 1);
+                String fieldValuesString = (String) properties.get(key);
+                String[] fieldValues =
+                    StringUtil.split(fieldValuesString, ObjectStoreSummaryGenerator.FIELD_DELIM);
+                List fieldValuesList = Arrays.asList(fieldValues);
+                
+                fieldValuesMap.put(getFieldValuesKey(className, fieldName), fieldValuesList);
             }
         }
     }
 
+    private String getFieldValuesKey(String className, String fieldName) {
+        return className + "." + fieldName;
+    }
+    
     /**
      * Get the number of instances of a particular class in the ObjectStore.
      * @param className the class name to look up
      * @return the count of the instances of the class
-     * @throws ObjectStoreException if there is a problem with the ObjectStore
-     * @throws ClassNotFoundException if className is unknown
      */
-    public int getClassCount(String className)
-        throws ObjectStoreException, ClassNotFoundException {
+    public int getClassCount(String className) {
+        return ((Integer) classCountsMap.get(className)).intValue();
+    }
 
-        return ((Integer) classCounts.get(className)).intValue();
+    /**
+     * Get a list of the possible values (as Strings) for a given field in a given class.
+     * @param className the class to search for
+     * @param fieldName the field name to search for
+     * @return a list of the possible values for the class and field, or null if the summary isn't
+     * available (because, for example, there are too many possible values)
+     */
+    public List getFieldValues(String className, String fieldName) {
+        return (List) fieldValuesMap.get(getFieldValuesKey(className, fieldName));
     }
 }
