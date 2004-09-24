4e405e53a031c2ee8b701fd06b3a354f4473d4bb
kmr <kmr>
2004-09-24 10:30:31 +0000

    Model change: "residues" attribute is now a "sequence" reference. transferToChromosome() now uses less memory by saving holding a reference to one Chromosome sequence at a time.
    Former-commit-id: 4327d8e672561dc658f87548decb88e1afe18061

--- flymine/model/genomic/src/java/org/flymine/postprocess/TransferSequences.java
+++ flymine/model/genomic/src/java/org/flymine/postprocess/TransferSequences.java
@@ -20,12 +20,13 @@ import org.biojava.bio.symbol.IllegalSymbolException;
 import org.biojava.bio.symbol.IllegalAlphabetException;
 import org.biojava.bio.seq.DNATools;
 
+import org.intermine.objectstore.query.*;
 import org.intermine.objectstore.ObjectStore;
 import org.intermine.objectstore.ObjectStoreWriter;
 import org.intermine.objectstore.ObjectStoreException;
+import org.intermine.util.DynamicUtil;
 
 import org.intermine.objectstore.query.ResultsRow;
-import org.intermine.util.DynamicUtil;
 import org.flymine.model.genomic.*;
 
 import org.apache.log4j.Logger;
@@ -59,12 +60,18 @@ public class TransferSequences
     public void transferSequences()
         throws Exception {
         osw.beginTransaction();
-        Map seqArrayMap = transferToChromosome();
-        transferToLocatedSequenceFeatures(seqArrayMap);
+        transferToChromosome();
+        transferToLocatedSequenceFeatures();
+        transferToTranscripts();
         osw.commitTransaction();
     }
 
-    private Map transferToChromosome()
+    /**
+     * Copy the contig sequences to the appropriate place in the chromosome sequence and store a
+     * Sequence object for each Chromosome.
+     * Returns a Map from chromosome ID to a char array of the chromosome sequence.
+     */
+    private void transferToChromosome()
         throws IllegalSymbolException, IllegalAlphabetException, ObjectStoreException {
         ObjectStore os = osw.getObjectStore();
         Iterator resIter =
@@ -74,6 +81,9 @@ public class TransferSequences
         // this is a Map from Chromosome to char[]
         Map seqArrayMap = new HashMap();
 
+        Integer currentChrId = null;
+        char[] currentChrBases = null;
+
         while (resIter.hasNext()) {
             ResultsRow rr = (ResultsRow) resIter.next();
             Integer chrId = (Integer) rr.get(0);
@@ -81,38 +91,40 @@ public class TransferSequences
             Contig contig = (Contig) rr.get(1);
             Location contigOnChrLocation = (Location) rr.get(2);
 
-            char[] chrSequence;
-
-            if (seqArrayMap.get(chrId) == null) {
-                chrSequence = new char[chr.getLength().intValue()];
+            if (currentChrId == null || !chrId.equals(currentChrId)) {
+                if (currentChrId != null) {
+                    storeChromosome(chr, currentChrBases);
+                }
+                currentChrBases = new char[chr.getLength().intValue()];
                 // fill with '.' so we can see the parts of the Chromosome sequence that haven't
                 // been set
-                for (int i = 0; i < chrSequence.length; i++) {
-                    chrSequence[i] = '.';
+                for (int i = 0; i < currentChrBases.length; i++) {
+                    currentChrBases[i] = '.';
                 }
-                seqArrayMap.put(chrId, chrSequence);
-            } else {
-                chrSequence = (char[]) seqArrayMap.get(chrId);
+                currentChrId = chrId;
             }
 
-            copySeqArray(chrSequence, contig.getSequence().getSequence(),
-                         contigOnChrLocation);
+            copySeqArray(currentChrBases, contig.getSequence().getSequence(), contigOnChrLocation);
         }
-
-// XXX - Chromosome needs a residues field
-//
-//         // set the Chromosome residue fields
-//         Iterator iter = seqArrayMap.keySet().iterator();
-//         while (iter.hasNext()) {
-//             Chromosome chr = (Chromosome) os.getObjectById((Integer) iter.next());
-//             chr.setResidues(new String((char[]) seqArrayMap(chr)));
-//             osw.store(chr);
-//         }
-
-        return seqArrayMap;
+        Chromosome chr = (Chromosome) os.getObjectById(currentChrId);
+        storeChromosome(chr, currentChrBases);
     }
 
-    private void transferToLocatedSequenceFeatures(Map seqArrayMap)
+    private void storeChromosome(Chromosome chr, char [] chrBases) throws ObjectStoreException {
+        Sequence sequence =
+            (Sequence) DynamicUtil.createObject(Collections.singleton(Sequence.class));
+        sequence.setSequence(new String(chrBases));
+        chr.setSequence(sequence);
+        osw.store(chr);
+        osw.store(sequence);
+    }
+
+    /**
+     * Use the Location relations to copy the sequence from the Chromosomes to every
+     * LocatedSequenceFeature that is located on a Chromosome and which doesn't already have a
+     * sequence (ie. don't copy to Contig).
+     */
+    private void transferToLocatedSequenceFeatures()
         throws IllegalSymbolException, IllegalAlphabetException, ObjectStoreException {
         ObjectStore os = osw.getObjectStore();
         Iterator resIter =
@@ -126,23 +138,33 @@ public class TransferSequences
             LocatedSequenceFeature feature = (LocatedSequenceFeature) rr.get(1);
             Location locationOnChr = (Location) rr.get(2);
 
-            char[] chrSequence = (char[]) seqArrayMap.get(chrId);
-            String featureSeq = getSubSequence(chrSequence, locationOnChr);
-            Sequence seq =
-                    (Sequence) DynamicUtil.createObject(Collections.singleton(Sequence.class));
-            seq.setSequence(featureSeq);
-            feature.setSequence(seq);
-            osw.store(seq);
+            if (feature.getSequence() != null) {
+                // probably a contig
+                continue;
+            }
+
+            Chromosome chr = (Chromosome) os.getObjectById(chrId);
+            Sequence chromosomeSequence = chr.getSequence();
+
+            String featureSeq = getSubSequence(chromosomeSequence, locationOnChr);
+
+            Sequence sequence =
+                (Sequence) DynamicUtil.createObject(Collections.singleton(Sequence.class));
+            sequence.setSequence(featureSeq);
+            feature.setSequence(sequence);
             osw.store(feature);
+            osw.store(sequence);
         }
     }
 
-    private String getSubSequence(char[] chrSequence, Location locationOnChr)
+    private String getSubSequence(Sequence chromosomeSequence, Location locationOnChr)
         throws IllegalSymbolException, IllegalAlphabetException {
         int charsToCopy =
             locationOnChr.getEnd().intValue() - locationOnChr.getStart().intValue() + 1;
-        String subSeqString =
-            new String(chrSequence, locationOnChr.getStart().intValue() - 1, charsToCopy);
+        String chromosomeSequenceString = chromosomeSequence.getSequence();
+        int startPos = locationOnChr.getStart().intValue() - 1;
+        int endPos = startPos + charsToCopy;
+        String subSeqString = new String(chromosomeSequenceString.substring(startPos, endPos));
 
         if (locationOnChr.getStrand().intValue() == -1) {
             SymbolList symbolList = DNATools.createDNA(subSeqString);
@@ -174,5 +196,45 @@ public class TransferSequences
 
         System.arraycopy(sourceArray, 0,
                          destArray, contigOnChrLocation.getStart().intValue() - 1, charsToCopy);
+    }
+
+    /**
+     * For each Transcript, join and transfer the sequences to the parent Transcript.
+     */
+    private void transferToTranscripts() {
+        ObjectStore os = osw.getObjectStore();
+        Query q = new Query();
+        q.setDistinct(false);
+        QueryClass qcTranscript = new QueryClass(Transcript.class);
+        q.addFrom(qcTranscript);
+        q.addToSelect(qcTranscript);
+        QueryClass qcExon = new QueryClass(Exon.class);
+        q.addFrom(qcExon);
+        q.addToSelect(qcExon);
+        // add to query so the Exon.getSequence() is (mostly) fast
+        QueryClass qcExonSequence = new QueryClass(Sequence.class);
+        q.addFrom(qcExonSequence);
+        q.addToSelect(qcExonSequence);
+        q.addToOrderBy(qcTranscript);
+
+        ConstraintSet cs = new ConstraintSet(ConstraintOp.AND);
+        QueryCollectionReference exonRef = new QueryCollectionReference(qcTranscript, "exons");
+        ContainsConstraint cc1 = new ContainsConstraint(exonRef, ConstraintOp.CONTAINS, qcExon);
+        cs.addConstraint(cc1);
+        QueryObjectReference sequenceRef = new QueryObjectReference(qcExon, "sequence");
+        ContainsConstraint cc2 =
+            new ContainsConstraint(sequenceRef, ConstraintOp.CONTAINS, qcExonSequence);
+        cs.addConstraint(cc2);
+        q.setConstraint(cs);
+
+        Results res = new Results(q, os, os.getSequence());
+        res.setBatchSize(10000);
+
+
+        Iterator resIter = res.iterator();
+
+        while (resIter.hasNext()) {
+//            org.intermine.web.LogMe.log("i", "next: " + resIter.next());
+        }
     }
 }
