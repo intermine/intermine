a886dba58a454a1b98d38903348eb9fd84f6536c
tom <tom>
2004-09-27 10:57:11 +0000

    changed from lookup dispatch action to normal Action

--- intermine/src/java/org/intermine/web/QueryClassSelectAction.java
+++ intermine/src/java/org/intermine/web/QueryClassSelectAction.java
@@ -10,15 +10,12 @@ package org.intermine.web;
  *
  */
 
-import java.util.Map;
-import java.util.HashMap;
-
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.struts.actions.LookupDispatchAction;
+import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -36,7 +33,7 @@ import org.intermine.util.TypeUtil;
  * @author Mark Woodbridge
  */
 
-public class QueryClassSelectAction extends LookupDispatchAction
+public class QueryClassSelectAction extends Action
 {
     /**
      * Add a QueryClass of a specified type to the current query.
@@ -53,10 +50,10 @@ public class QueryClassSelectAction extends LookupDispatchAction
      * @exception Exception if the application business logic throws
      *  an exception
      */
-    public ActionForward selectClass(ActionMapping mapping,
-                                     ActionForm form,
-                                     HttpServletRequest request,
-                                     HttpServletResponse response)
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
         throws Exception {
         HttpSession session = request.getSession();
         String className = ((QueryClassSelectForm) form).getClassName();
@@ -87,44 +84,6 @@ public class QueryClassSelectAction extends LookupDispatchAction
         session.setAttribute(Constants.QUERY, query);
         session.setAttribute("path", TypeUtil.unqualifiedName(className));
         session.removeAttribute("prefix");
-    }
-    
-    /**
-     * Browse the full class hierarchy and allow the user to choose a type to add
-     * to the current query.
-     * Return an <code>ActionForward</code> instance describing where and how
-     * control should be forwarded, or <code>null</code> if the response has
-     * already been completed.
-     *
-     * @param mapping The ActionMapping used to select this instance
-     * @param form The optional ActionForm bean for this request (if any)
-     * @param request The HTTP request we are processing
-     * @param response The HTTP response we are creating
-     * @return an ActionForward object defining where control goes next
-     *
-     * @exception Exception if the application business logic throws
-     *  an exception
-     */
-    public ActionForward browse(ActionMapping mapping,
-                                 ActionForm form,
-                                 HttpServletRequest request,
-                                 HttpServletResponse response)
-        throws Exception {
-
-        return mapping.findForward("tree");
-    }
-
-    /**
-     * Distributes the actions to the necessary methods, by providing a Map from action to
-     * the name of a method.
-     *
-     * @return a Map
-     */
-    protected Map getKeyMethodMap() {
-        Map map = new HashMap();
-        map.put("button.selectClass", "selectClass");
-        map.put("button.browse", "browse");
-        return map;
     }
 }
 
