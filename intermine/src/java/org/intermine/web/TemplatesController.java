dd074a3d319da9fd52e578ff99ed1a9505be4d0d
tom <tom>
2004-09-28 11:54:57 +0000

    controller for templates.tile, filters templates based on category.
    Former-commit-id: e8c090761796a6a9cedb7e221ba108c9036a1c38

---
+++ intermine/src/java/org/intermine/web/TemplatesController.java
@@ -0,0 +1,63 @@
+package org.intermine.web;
+
+/*
+ * Copyright (C) 2002-2004 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Iterator;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+
+/**
+ * Controller for templates.page
+ *
+ * @author Thomas Riley
+ */
+public class TemplatesController extends Action
+{
+    /**
+     * @see TilesAction#execute
+     */
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+        throws Exception {
+        HttpSession session = request.getSession();
+        ServletContext servletContext = session.getServletContext();
+        Map templateQueries = (Map) servletContext.getAttribute(Constants.TEMPLATE_QUERIES);
+        String category = request.getParameter("category");
+        
+        List templates = new ArrayList();
+        Iterator iter = templateQueries.values().iterator();
+        
+        while (iter.hasNext()) {
+            TemplateQuery template = (TemplateQuery)iter.next();
+            if (category == null || category.equals(template.getCategory ())) {
+                templates.add(template);
+            }
+        }
+        
+        request.setAttribute("templates", templates);
+
+        return null;
+    }
+}
