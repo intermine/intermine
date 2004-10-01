d0e765cba98843053e487b62e97e0e8329347476
mark <mark>
2004-10-01 10:17:39 +0000

    Protein structure tests

---
+++ flymine/model/protein_structure/src/test/org/flymine/dataconversion/ProteinStructureDataTranslatorTest.java
@@ -0,0 +1,83 @@
+package org.flymine.dataconversion;
+
+/*
+ * Copyright (C) 2002-2004 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import junit.framework.TestCase;
+
+import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.LinkedHashSet;
+import java.util.LinkedHashMap;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.io.InputStreamReader;
+
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+import org.intermine.xml.full.FullParser;
+import org.intermine.xml.full.Item;
+import org.intermine.xml.full.Attribute;
+import org.intermine.xml.full.Reference;
+import org.intermine.xml.full.ReferenceList;
+import org.intermine.dataconversion.DataTranslator;
+import org.intermine.dataconversion.DataTranslatorTestCase;
+import org.intermine.dataconversion.MockItemReader;
+import org.intermine.dataconversion.MockItemWriter;
+
+public class ProteinStructureDataTranslatorTest extends DataTranslatorTestCase {
+    private String tgtNs = "http://www.flymine.org/model/genomic#";
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testTranslate() throws Exception {
+        Map itemMap = writeItems(getSrcItems());
+        DataTranslator translator = new ProteinStructureDataTranslator(new MockItemReader(itemMap),
+                                                                       getOwlModel(), tgtNs, "");
+        MockItemWriter tgtIw = new MockItemWriter(new LinkedHashMap());
+        translator.translate(tgtIw);
+        assertEquals(new HashSet(getExpectedItems()), tgtIw.getItems());
+    }
+
+    protected Collection getExpectedItems() throws Exception {
+        return FullParser.parse(getClass().getClassLoader().getResourceAsStream("test/ProteinStructureDataTranslatorFunctionalTest_tgt.xml"));
+    }
+
+    protected Collection getSrcItems() throws Exception {
+        return FullParser.parse(getClass().getClassLoader().getResourceAsStream("test/ProteinStructureDataTranslatorFunctionalTest_src.xml"));
+    }
+
+    protected OntModel getOwlModel() {
+        InputStreamReader reader = new InputStreamReader(getClass().getClassLoader().getResourceAsStream("genomic.n3"));
+
+        OntModel ont = ModelFactory.createOntologyModel();
+        ont.read(reader, null, "N3");
+        return ont;
+    }
+
+    protected String getModelName() {
+        return "genomic";
+    }
+
+    private Item createItem(String clsName, String identifier, String imps) {
+        Item item = new Item();
+        item.setClassName(clsName);
+        item.setIdentifier(identifier);
+        item.setImplementations(imps);
+        return item;
+    }
+}
