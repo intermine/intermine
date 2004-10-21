c58b3d8f33e2c30e0c8e29a18393dc820a6013e4
mnw21 <mnw21>
2004-10-21 10:58:10 +0000

    Fixed bug in optimiser, where a distinct query that utilises the orderby_field of a precomputed table would not produce an optimised query that is valid SQL
    Former-commit-id: f7fc59547ddf33664d49b88f8910d220f576ca66

--- intermine/src/test/org/intermine/sql/precompute/QueryOptimiserTest.java
+++ intermine/src/test/org/intermine/sql/precompute/QueryOptimiserTest.java
@@ -772,7 +772,7 @@ public class QueryOptimiserTest extends TestCase
         BestQueryStorer bestQuery;
 
         q = new Query("SELECT ta.id AS a, tb.id AS b, tc.id AS c FROM Company AS ta, Department AS tb, Employee AS tc WHERE ta.id > 25 ORDER BY ta.id, tb.id, tc.id");
-        eq = new Query("SELECT P42.a AS a, P42.b AS b, P42.c AS c FROM precomp1 AS P42 WHERE P42.orderby_field > 259999999999999999999999999999999999999999 ORDER BY orderby_field");
+        eq = new Query("SELECT P42.a AS a, P42.b AS b, P42.c AS c FROM precomp1 AS P42 WHERE P42.orderby_field > 259999999999999999999999999999999999999999 ORDER BY P42.orderby_field");
         eSet = new ConsistentSet();
         eSet.add(eq);
         StringUtil.setNextUniqueNumber(42);
@@ -800,6 +800,15 @@ public class QueryOptimiserTest extends TestCase
 
         q = new Query("SELECT ta.id AS a, tb.id AS b, tc.id AS c FROM Company AS ta, Department AS tb, Employee AS tc WHERE ta.id > 25 ORDER BY ta.id, tc.id, tb.id");
         eq = new Query("SELECT P42.a AS a, P42.b AS b, P42.c AS c FROM precomp1 AS P42 WHERE P42.orderby_field > 259999999999999999999999999999999999999999 ORDER BY P42.orderby_field, P42.c, P42.b");
+        eSet = new ConsistentSet();
+        eSet.add(eq);
+        StringUtil.setNextUniqueNumber(42);
+        bestQuery = new BestQueryStorer();
+        QueryOptimiser.recursiveOptimise(precomps, q, bestQuery, q);
+        assertEquals(eSet, bestQuery.getQueries());
+
+        q = new Query("SELECT DISTINCT ta.id AS a, tb.id AS b, tc.id AS c FROM Company AS ta, Department AS tb, Employee AS tc ORDER BY ta.id, tb.id, tc.id");
+        eq = new Query("SELECT DISTINCT P42.a AS a, P42.b AS b, P42.c AS c, P42.orderby_field FROM precomp1 AS P42 ORDER BY P42.orderby_field");
         eSet = new ConsistentSet();
         eSet.add(eq);
         StringUtil.setNextUniqueNumber(42);
