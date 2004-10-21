438e399cf447065be17fd452245229f7b52b54f9
tom <tom>
2004-10-21 10:46:10 +0000

    link action to edit a template
    Former-commit-id: 64b09132e908bb95f865cf43913cec437c0dbdf3

---
+++ intermine/src/java/org/intermine/web/EditTemplateAction.java
@@ -0,0 +1,70 @@
+package org.intermine.web;
+
+/*
+ * Copyright (C) 2002-2004 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.Iterator;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
+
+import org.intermine.objectstore.ObjectStore;
+import org.intermine.objectstore.ObjectStoreException;
+
+/**
+ * Action to edit a user template query. The action expect a <code>name</code>
+ * parameter identifying the template to edit. 
+ *
+ * @author Thomas Riley
+ */
+public class EditTemplateAction extends Action
+{
+    /**
+     * Process the specified HTTP request, and create the corresponding HTTP
+     * response (or forward to another web component that will create it).
+     * Return an <code>ActionForward</code> instance describing where and how
+     * control should be forwarded, or <code>null</code> if the response has
+     * already been completed.
+     *
+     * @param mapping The ActionMapping used to select this instance
+     * @param form The optional ActionForm bean for this request (if any)
+     * @param request The HTTP request we are processing
+     * @param response The HTTP response we are creating
+     * @return an ActionForward object defining where control goes next
+     *
+     * @exception Exception if the application business logic throws
+     *  an exception
+     */
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+        throws Exception {
+        HttpSession session = request.getSession();
+        ServletContext servletContext = session.getServletContext();
+        Profile profile = (Profile) session.getAttribute(Constants.PROFILE);
+        TemplateQuery template = (TemplateQuery) profile.getSavedTemplates().get(request.getParameter("name"));
+        
+        
+        PathQuery queryClone = (PathQuery) template.query.clone();
+        session.setAttribute(Constants.TEMPLATE_PATHQUERY, queryClone);
+        session.setAttribute(Constants.EDITING_TEMPLATE, template);
+        return mapping.findForward("templateBuilder");
+    }
+}
