eb5998520800c23caa66ca4b9efcb4f0c7dcc462
tom <tom>
2004-10-22 10:56:54 +0000

    template import form bean and action

---
+++ intermine/src/java/org/intermine/web/TemplatesImportAction.java
@@ -0,0 +1,71 @@
+package org.intermine.web;
+
+/*
+ * Copyright (C) 2002-2004 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * Imports templates in XML format.
+ *
+ * @author Thomas Riley
+ */
+public class TemplatesImportAction extends Action
+{
+    /**
+     * @see Action#execute
+     */
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+        throws Exception {
+        HttpSession session = request.getSession();
+        Profile profile = (Profile) session.getAttribute(Constants.PROFILE);
+        TemplatesImportForm tif = (TemplatesImportForm) form;
+        Map templates = null;
+        int deleted = 0, imported = 0;
+        
+        templates = TemplateHelper.xmlToTemplateMap(tif.getXml());
+        
+        if (tif.isOverwriting()
+            && profile.getSavedTemplates().size() > 0) {
+            Iterator iter = new HashSet(profile.getSavedTemplates().keySet()).iterator();
+            while (iter.hasNext()) {
+                profile.deleteTemplate((String) iter.next());
+                deleted++;
+            }
+        }
+        
+        Iterator iter = templates.values().iterator();
+        while (iter.hasNext()) {
+            TemplateQuery template = (TemplateQuery) iter.next();
+            profile.saveTemplate(template.getName(), template);
+            imported++;
+        }
+        
+        String msg = getResources(request).getMessage("importTemplates.done",
+                                                    new Integer(deleted), new Integer(imported));
+        session.setAttribute(Constants.MESSAGE, msg);
+        
+        return mapping.findForward("begin");
+    }
+}
