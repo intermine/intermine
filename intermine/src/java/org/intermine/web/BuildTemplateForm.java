66e87e8649a3f42d82e4c064cbd32408cada285f
tom <tom>
2004-11-01 18:06:28 +0000

    Default category property to empty string rather than null.

--- intermine/src/java/org/intermine/web/BuildTemplateForm.java
+++ intermine/src/java/org/intermine/web/BuildTemplateForm.java
@@ -213,6 +213,7 @@ public class BuildTemplateForm extends ValidatorForm
         constraintIds = new HashMap();
         templateDescription = "";
         shortName = "";
+        category = "";
     }
     
     /**
