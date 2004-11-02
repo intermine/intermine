0ef02f056424ff24388d79f6e9a14c5d58a1d68a
rnsmith <rns>
2004-11-02 18:26:53 +0000

    changed package specification for generated models, can now always pass in package name
    Former-commit-id: d2b873d83fee4aa669fe70cd410e1d9c4acfb51e

--- intermine/src/test/org/intermine/modelproduction/acedb/AceModelParserFunctionalTest.java
+++ intermine/src/test/org/intermine/modelproduction/acedb/AceModelParserFunctionalTest.java
@@ -28,9 +28,9 @@ import junit.framework.TestCase;
 public class AceModelParserFunctionalTest extends TestCase
 {
     private static final String MODEL = "acedbtest";
-    private static final String PKG = "org.intermine.model." + MODEL + ".";
-    private static final String nameSpace = "http://www.intermine.org/model";
-    private static final String uri = nameSpace + "/testmodel";
+    private static final String PKG = "org.intermine.model." + MODEL;
+    private static final String NS = "http://www.intermine.org/model";
+    private static final String uri = NS + "/testmodel";
 
     private ModelParser parser;
     private Reader reader;
@@ -42,7 +42,7 @@ public class AceModelParserFunctionalTest extends TestCase
 
     public void setUp() throws Exception {
         super.setUp();
-        parser = new AceModelParser(nameSpace, MODEL);
+        parser = new AceModelParser(MODEL, PKG, NS);
         reader = new InputStreamReader(getClass().getClassLoader().getResourceAsStream(MODEL + ".wrm"));
         model = createModel();
     }
@@ -60,15 +60,15 @@ public class AceModelParserFunctionalTest extends TestCase
         atts.add(new AttributeDescriptor("onOrOff", "boolean"));
         atts.add(new AttributeDescriptor("dateValue", "java.util.Date"));
         Set refs = new LinkedHashSet();
-        refs.add(new ReferenceDescriptor("reference", PKG + "AceTestObject", null));
-        refs.add(new ReferenceDescriptor("hashValue", PKG + "AceTestObject", null));
+        refs.add(new ReferenceDescriptor("reference", PKG + ".AceTestObject", null));
+        refs.add(new ReferenceDescriptor("hashValue", PKG + ".AceTestObject", null));
         Set cols = new LinkedHashSet();
-        cols.add(new CollectionDescriptor("stringValues", PKG + "Text", null, false));
-        cols.add(new CollectionDescriptor("references", PKG + "AceTestObject", null, false));
-        cols.add(new CollectionDescriptor("hashValues", PKG + "AceTestObject", null, false));
+        cols.add(new CollectionDescriptor("stringValues", PKG + ".Text", null, false));
+        cols.add(new CollectionDescriptor("references", PKG + ".AceTestObject", null, false));
+        cols.add(new CollectionDescriptor("hashValues", PKG + ".AceTestObject", null, false));
         Set clds = new LinkedHashSet();
         ((AceModelParser) parser).addBuiltinClasses(clds);
-        clds.add(new ClassDescriptor(PKG + "AceTestObject", null, false, atts, refs, cols));
+        clds.add(new ClassDescriptor(PKG + ".AceTestObject", null, false, atts, refs, cols));
         return new Model(MODEL, uri, clds);
     }
 }
