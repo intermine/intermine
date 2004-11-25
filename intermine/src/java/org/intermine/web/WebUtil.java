b52cf1907bfc8f270326d1649221bc73133ca27f
kmr <kmr>
2004-11-25 09:53:07 +0000

    Utilities for the web package.
    Former-commit-id: ac6aba6dfc6b2108528da358263ebab749e72a4e

---
+++ intermine/src/java/org/intermine/web/WebUtil.java
@@ -0,0 +1,52 @@
+package org.intermine.web;
+
+/*
+ * Copyright (C) 2002-2004 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Utility methods for the web package.
+ *
+ * @author Kim Rutherford
+ */
+
+public abstract class WebUtil
+{
+    protected static final Logger LOG = Logger.getLogger(WebUtil.class);
+
+    /**
+     * Lookup an Integer property from the SessionContext and return it.
+     * @param session the current session
+     * @param propertyName the property to find
+     * @param defaultValue the value to return if the property isn't present
+     * @returns the int value of the property
+     */
+    public static int getIntSessionProperty(HttpSession session, String propertyName,
+                                            int defaultValue) {
+        Map webProperties =
+            (Map) session.getServletContext().getAttribute(Constants.WEB_PROPERTIES);
+        String maxBagSizeString = (String) webProperties.get(propertyName);
+
+        int intVal = defaultValue;
+
+        try {
+            intVal = Integer.parseInt(maxBagSizeString);
+        } catch (NumberFormatException e) {
+            LOG.warn("Failed to parse " + propertyName + " property: " + maxBagSizeString);
+        }
+
+        return intVal;
+    }
+}
