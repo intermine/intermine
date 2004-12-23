2a8bd6a287051370efcce81eb64bd19a691bb119
mnw21 <mnw21>
2004-12-23 14:30:18 +0000

    New weak reference hash map, and the potential for lots of others
    Former-commit-id: 80bc46fdbe7b16a1d11f307d166a7a39ac13593a

--- intermine/src/java/org/intermine/util/WeakReferenceHashMap.java
+++ intermine/src/java/org/intermine/util/WeakReferenceHashMap.java
@@ -21,75 +21,75 @@ import org.apache.log4j.Logger;
 
 /**
  * This is a Map implementation designed specifically for people intending to create a cache.
- * The keys are held strongly, but the values are held softly, so the values can be
+ * The keys are held strongly, but the values are held weakly, so the values can be
  * garbage-collected. When an entry is garbage-collected, its key is removed from the Map on
  * the next Map activity.
  * <p>
  * The entrySet() and values() methods of this class do not work.
  * 
- * @see java.lang.ref.SoftReference
+ * @see java.lang.ref.WeakReference
  * @author Matthew Wakeling
  */
-public class CacheMap extends SoftReferenceMap
+public class WeakReferenceHashMap extends WeakReferenceMap
 {
-    private static final Logger LOG = Logger.getLogger(CacheMap.class);
+    private static final Logger LOG = Logger.getLogger(WeakReferenceHashMap.class);
 
     /**
-     * Constructs a new, empty <tt>CacheMap</tt> with the given initial
+     * Constructs a new, empty <tt>WeakReferenceHashMap</tt> with the given initial
      * capacity and the given load factor.
      *
-     * @param  initialCapacity The initial capacity of the <tt>CacheMap</tt>
-     * @param  loadFactor      The load factor of the <tt>CacheMap</tt>
+     * @param  initialCapacity The initial capacity of the <tt>WeakReferenceHashMap</tt>
+     * @param  loadFactor      The load factor of the <tt>WeakReferenceHashMap</tt>
      * @throws IllegalArgumentException  If the initial capacity is negative,
      *         or if the load factor is nonpositive.
      */
-    public CacheMap(int initialCapacity, float loadFactor) {
+    public WeakReferenceHashMap(int initialCapacity, float loadFactor) {
         subMap = new HashMap(initialCapacity, loadFactor);
         this.name = "unknown";
     }
 
     /**
-     * Constructs a new, empty <tt>CacheMap</tt> with the given initial
+     * Constructs a new, empty <tt>WeakReferenceHashMap</tt> with the given initial
      * capacity and the default load factor, which is <tt>0.75</tt>.
      *
-     * @param  initialCapacity The initial capacity of the <tt>CacheMap</tt>
+     * @param  initialCapacity The initial capacity of the <tt>WeakReferenceHashMap</tt>
      * @throws IllegalArgumentException  If the initial capacity is negative.
      */
-    public CacheMap(int initialCapacity) {
+    public WeakReferenceHashMap(int initialCapacity) {
         subMap = new HashMap(initialCapacity);
         this.name = "unknown";
     }
 
     /**
-     * Constructs a new, empty <tt>CacheMap</tt> with the default initial
+     * Constructs a new, empty <tt>WeakReferenceHashMap</tt> with the default initial
      * capacity (16) and the default load factor (0.75).
      */
-    public CacheMap() {
+    public WeakReferenceHashMap() {
         subMap = new HashMap();
         this.name = "unknown";
     }
 
     /**
-     * Constructs a new, empty <tt>CacheMap</tt> with the default initial
+     * Constructs a new, empty <tt>WeakReferenceHashMap</tt> with the default initial
      * capacity (16) and the default load factor (0.75), and a name.
      *
-     * @param name the name of the CacheMap - printed out in log messages
+     * @param name the name of the WeakReferenceHashMap - printed out in log messages
      */
-    public CacheMap(String name) {
+    public WeakReferenceHashMap(String name) {
         subMap = new HashMap();
         this.name = name;
     }
 
     /**
-     * Constructs a new <tt>CacheMap</tt> with the same mappings as the
-     * specified <tt>Map</tt>.  The <tt>CacheMap</tt> is created with 
+     * Constructs a new <tt>WeakReferenceHashMap</tt> with the same mappings as the
+     * specified <tt>Map</tt>.  The <tt>WeakReferenceHashMap</tt> is created with 
      * default load factor, which is <tt>0.75</tt> and an initial capacity
      * sufficient to hold the mappings in the specified <tt>Map</tt>.
      *
      * @param   t the map whose mappings are to be placed in this map.
      * @throws  NullPointerException if the specified map is null.
      */
-    public CacheMap(Map t) {
+    public WeakReferenceHashMap(Map t) {
         subMap = new HashMap();
         this.name = "unknown";
         putAll(t);
---
+++ intermine/src/java/org/intermine/util/WeakReferenceMap.java
@@ -0,0 +1,56 @@
+package org.intermine.util;
+
+/*
+ * Copyright (C) 2002-2004 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This is a Map implementation designed specifically for people intending to create a cache.
+ * The keys are held strongly, but the values are held weakly, so the values can be
+ * garbage-collected. When an entry is garbage-collected, its key is removed from the Map on
+ * the next Map activity. Subclasses of this map are required to provide the actual Map
+ * functionality.
+ * <p>
+ * The entrySet() and values() methods of this class do not work.
+ * 
+ * @see java.lang.ref.WeakReference
+ * @author Matthew Wakeling
+ */
+public abstract class WeakReferenceMap extends ReferenceMap
+{
+    private static final Logger LOG = Logger.getLogger(WeakReferenceMap.class);
+
+    public ReferenceWithKey newRef(Object value, ReferenceQueue queue, Object key) {
+        return new WeakReferenceWithKey(value, queue, key);
+    }
+
+    private static class WeakReferenceWithKey extends WeakReference implements ReferenceWithKey
+    {
+        private Object key;
+
+        WeakReferenceWithKey(Object value, ReferenceQueue queue, Object key) {
+            super(value, queue);
+            this.key = key;
+        }
+
+        public Object getKey() {
+            return key;
+        }
+    }
+}
