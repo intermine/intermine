45eb42af9381b871d8afbf650c2b73bf8aab00bd
kmr <kmr>
2005-01-05 10:11:27 +0000

    Don't call ObjectStore.flushObjectById() in init() - it drops all precomputed tables.

--- intermine/src/java/org/intermine/web/InitialiserPlugin.java
+++ intermine/src/java/org/intermine/web/InitialiserPlugin.java
@@ -79,7 +79,6 @@ public class InitialiserPlugin implements PlugIn
         ObjectStore os = null;
         try {
             os = ObjectStoreFactory.getObjectStore();
-            os.flushObjectById();
         } catch (Exception e) {
             throw new ServletException("Unable to instantiate ObjectStore", e);
         }
