25425760c5df20537cd78901e69754f41c5eac2e
tom <tom>
2005-02-15 16:46:48 +0000

    Test for ModelMergerTask and test for merging references and collections without reverse reference field.
    Former-commit-id: 8476ec6e6adaff62cf1811b9d28dd62a4c03fb82

---
+++ intermine/src/test/org/intermine/task/ModelMergerTaskTest.java
@@ -0,0 +1,111 @@
+package org.intermine.task;
+
+/*
+ * Copyright (C) 2002-2005 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.intermine.metadata.Model;
+import org.intermine.modelproduction.xml.InterMineModelParser;
+
+import junit.framework.TestCase;
+
+/**
+ * Test for ModelMergerTask.
+ * 
+ * @author Thomas Riley
+ */
+public class ModelMergerTaskTest extends TestCase
+{
+    String inputModel =
+        "<model name=\"testmodel\" namespace=\"testmodel#\">"
+            + "<class name=\"A\" extends=\"C\" is-interface=\"false\">"
+                + "<reference name=\"ref1\" referenced-type=\"C\"/>"
+                + "<collection name=\"col1\" referenced-type=\"C\"/>"
+                + "<attribute name=\"attrib1\" type=\"java.lang.Integer\"/>"
+            + "</class>"
+            + "<class name=\"C\" is-interface=\"false\"></class>"
+            + "<class name=\"D\" is-interface=\"true\"></class>"
+        + "</model>";
+    String additions =
+        "<model name=\"testmodel\" namespace=\"testmodel#\">"
+        + "<class name=\"A\" extends=\"B\" is-interface=\"false\">"
+            + "<reference name=\"ref1\" referenced-type=\"C\"/>"
+            + "<collection name=\"col2\" referenced-type=\"C\"/>"
+            + "<attribute name=\"attrib2\" type=\"java.lang.String\"/>"
+        + "</class>"
+        + "<class name=\"B\" is-interface=\"false\"></class>"
+    + "</model>";
+    String expected =
+        "<model name=\"testmodel\" namespace=\"testmodel#\">"
+        + "<class name=\"A\" extends=\"B\" is-interface=\"false\">"
+            + "<reference name=\"ref1\" referenced-type=\"C\"/>"
+            + "<collection name=\"col1\" referenced-type=\"C\"/>"
+            + "<collection name=\"col2\" referenced-type=\"C\"/>"
+            + "<attribute name=\"attrib1\" type=\"java.lang.Integer\"/>"
+            + "<attribute name=\"attrib2\" type=\"java.lang.String\"/>"
+        + "</class>"
+        + "<class name=\"C\" is-interface=\"false\"></class>"
+        + "<class name=\"D\" is-interface=\"true\"></class>"
+        + "<class name=\"B\" is-interface=\"false\"></class>"
+    + "</model>";
+    
+    File input, addition, output;
+    
+    /*
+     * @see TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        // write to temp files
+        input = File.createTempFile("input", ".xml");
+        addition = File.createTempFile("additions", ".xml");
+        output = File.createTempFile("output", ".xml");
+        
+        writeToFile(input, inputModel);
+        writeToFile(addition, additions);
+    }
+    
+    private void writeToFile(File file, String data) throws IOException {
+        FileWriter writer = new FileWriter(file);
+        writer.write(data);
+        writer.close();
+    }
+
+    /*
+     * @see TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        input.delete();
+        addition.delete();
+        output.delete();
+        super.tearDown();
+    }
+
+    public void testExecute() throws Exception {
+        ModelMergerTask task = new ModelMergerTask();
+        task.setAdditionsFile(addition.getAbsolutePath());
+        task.setInputModelFile(input.getAbsolutePath());
+        task.setOutputFile(output.getAbsolutePath());
+        
+        long startTime = System.currentTimeMillis();
+        task.execute();
+        System.out.println("" + (System.currentTimeMillis() - startTime));
+        
+        InterMineModelParser parser = new InterMineModelParser();
+        Model result = parser.process(new FileReader(output));
+        Model exp = parser.process(new StringReader(expected));
+        assertEquals(exp, result);
+    }
+}
