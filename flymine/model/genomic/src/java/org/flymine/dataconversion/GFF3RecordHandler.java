7ab8df362bcc0c5a66c4c8dfdaa81b0790102b0a
kmr <kmr>
2005-03-10 18:36:33 +0000

    Extra getters and setters. process() now doesn't need a idMap argument. Added itemFactory and identifierMap attributes.
    Former-commit-id: 480f95327072b21f2991b6d83a3455b48fcb7f1e

--- flymine/model/genomic/src/java/org/flymine/dataconversion/GFF3RecordHandler.java
+++ flymine/model/genomic/src/java/org/flymine/dataconversion/GFF3RecordHandler.java
@@ -16,6 +16,7 @@ import java.util.HashMap;
 import java.util.Collection;
 
 import org.intermine.xml.full.Item;
+import org.intermine.xml.full.ItemFactory;
 import org.intermine.metadata.Model;
 
 import org.flymine.io.gff3.GFF3Record;
@@ -33,6 +34,8 @@ public class GFF3RecordHandler
     private Item sequence;
     private Item analysis;
     private Model tgtModel;
+    private ItemFactory itemFactory;
+    private Map identifierMap;
 
     /**
      * Construct with the model to create items in (for type checking).
@@ -44,14 +47,31 @@ public class GFF3RecordHandler
 
     /**
      * Method to perform additional operations for a GFF3Record.  Access to core items
-     * is possible via getters() (sequence and coumputatinal analysis items cannot be
-     * altered but are avalable read-only).  Additional items to store can be placed
-     * in the items map, keys can be anything except a string starting with '_'.  idMap
-     * should be used to get/add any item identifiers for features used.
+     * is possible via getters() (sequence and computational analysis items cannot be
+     * altered but are available read-only).  Additional items to store can be placed
+     * in the items map, keys can be anything except a string starting with '_'.
      * @param record the GFF line being processed
-     * @param idMap map from GFF ID to item identifier for all features
      */
-    public void process(GFF3Record record, Map idMap) {
+    public void process(GFF3Record record) {
+        
+    }
+
+    /**
+     * Set the Map of GFF identifiers to Item identifier.  The Map should be used to get/add any
+     * item identifiers for features used so that multiple Items aren't created for the same
+     * feature.
+     * @param identifierMap map from GFF ID to item identifier for all features
+     */
+    public void setIdentifierMap(Map identifierMap) {
+        this.identifierMap = identifierMap;
+    }
+
+    /**
+     * Return the Model that was passed to the constructor.
+     * @return the Model
+     */
+    public Model getTargetModel() {
+        return tgtModel;
     }
 
     /**
@@ -63,7 +83,15 @@ public class GFF3RecordHandler
     }
 
     /**
-     * Set location item for this record.
+     * Return the sequence Item set by setSequence()
+     * @return the sequence Item
+     */
+    protected Item getSequence() {
+        return sequence;
+    }
+
+    /**
+     * Set the location item for this record.
      * @param location the location item
      */
     public void setLocation(Item location) {
@@ -71,7 +99,15 @@ public class GFF3RecordHandler
     }
 
     /**
-     * Set feature item for this record.
+     * Return the location Item set by setLocation()
+     * @return the location Item
+     */
+    protected Item getLocation() {
+        return (Item) items.get("_location");
+    }
+
+    /**
+     * Set hte feature item for this record.
      * @param feature the feature item
      */
     public void setFeature(Item feature) {
@@ -79,7 +115,15 @@ public class GFF3RecordHandler
     }
 
     /**
-     * Set ComputationalAnalysis item created for this record, should not be edited in handler.
+     * Return the feature Item set by setFeature()
+     * @return the feature Item
+     */
+    protected Item getFeature() {
+        return (Item) items.get("_feature");
+    }
+
+    /**
+     * Set the ComputationalAnalysis item created for this record, should not be edited in handler.
      * @param analysis the ComputationalAnalysis item
      */
     public void setAnalysis(final Item analysis) {
@@ -87,7 +131,7 @@ public class GFF3RecordHandler
     }
 
     /**
-     * Set ComputationalResult item for this record.
+     * Set the ComputationalResult item for this record.
      * @param result the ComputationalResult item
      */
     public void setResult(Item result) {
@@ -95,11 +139,43 @@ public class GFF3RecordHandler
     }
 
     /**
-     * Set SimpleRelation item from feature to parent feature for this record.
+     * Return the result Item set by setResult()
+     * @return the result Item
+     */
+    protected Item getResult() {
+        return (Item) items.get("_result");
+    }
+
+    /**
+     * Get the SimpleRelation item from feature to parent feature for this record.
      * @param relation the relation item
      */
     public void setParentRelation(Item relation) {
         items.put("_relation", relation);
+    }
+
+    /**
+     * Return the SimpleRelation Item set by setParentRelation()
+     * @return the location Item
+     */
+    protected Item getParentRelation() {
+        return (Item) items.get("_relation");
+    }
+
+    /**
+     * Set the ItemFactory to use in this handler.
+     * @param itemFactory the ItemFactory
+     */
+    public void setItemFactory(ItemFactory itemFactory) {
+        this.itemFactory = itemFactory;
+    }
+
+    /**
+     * Get the ItemFactory for this handler.
+     * @return the ItemFactory
+     */
+    protected ItemFactory getItemFactory() {
+        return itemFactory;
     }
 
     /**
@@ -118,5 +194,13 @@ public class GFF3RecordHandler
      */
     public Collection getItems() {
         return items.values();
+    }
+
+    /**
+     * Add a new Item to the Collection returned by getItems().
+     * @param item the Item to add
+     */
+    public void addItem(Item item) {
+        items.put(item.getIdentifier(), item);
     }
 }
