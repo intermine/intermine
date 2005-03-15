088d4cd9f14a519b61b473b4461c88e1ae0acfeb
wenyan <wenyan>
2005-03-15 15:35:33 +0000

    create ucsc model for  data imported from UCSC database

---
+++ flymine/model/ucsc/src/java/org/flymine/dataconversion/TfbsGFF3RecordHandler.java
@@ -0,0 +1,41 @@
+package org.flymine.dataconversion;
+
+/*
+ * Copyright (C) 2002-2005 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import org.intermine.metadata.Model;
+
+import org.flymine.io.gff3.GFF3Record;
+
+/**
+ * A converter/retriever for Tfbs GFF3 files.
+ *
+ * @author Wenyan Ji
+ */
+
+public class TfbsGFF3RecordHandler extends GFF3RecordHandler
+{
+
+
+    /**
+     * Create a new TfbsGFF3RecordHandler for the given target model.
+     * @param tgtModel the model for which items will be created
+     */
+    public TfbsGFF3RecordHandler(Model tgtModel) {
+        super(tgtModel);
+    }
+
+    /**
+     * @see GFF3RecordHandler#process()
+     */
+    public void process(GFF3Record record) {
+
+    }
+}
