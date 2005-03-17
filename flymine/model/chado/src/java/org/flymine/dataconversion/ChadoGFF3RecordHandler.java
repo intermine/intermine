9dafd38f07960404aa023533e28cd41a54617f0e
rnsmith <rns>
2005-03-17 16:51:50 +0000

    updates to get handle broken FlyBase gff. still need to get additional synonyms
    Former-commit-id: 6b05f541e8f0b4bec869b54dc46be9977d3babc8

--- flymine/model/chado/src/java/org/flymine/dataconversion/ChadoGFF3RecordHandler.java
+++ flymine/model/chado/src/java/org/flymine/dataconversion/ChadoGFF3RecordHandler.java
@@ -14,9 +14,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import org.intermine.metadata.Model;
 import org.intermine.xml.full.Item;
+import org.intermine.xml.full.ReferenceList;
 import org.intermine.util.XmlUtil;
 
 import org.flymine.io.gff3.GFF3Record;
@@ -33,6 +36,9 @@ public class ChadoGFF3RecordHandler extends GFF3RecordHandler
 {
     private Map references;
     private static final Logger LOG = Logger.getLogger(ChadoGFF3RecordHandler.class);
+    private String tgtNs;
+    private Map sources = new HashMap();
+
     /**
      * Create a new FlyBaseGFF3RecordHandler for the given target model.
      * @param tgtModel the model for which items will be created
@@ -46,27 +52,30 @@ public class ChadoGFF3RecordHandler extends GFF3RecordHandler
         references = new HashMap();
         references.put("Enhancer", "gene");
         references.put("Exon", "transcripts");
-        references.put("FivePrimeUTR", "MRNAs");
-        references.put("InsertionSite", "gene");
+        references.put("InsertionSite", "genes");
         references.put("Intron", "transcripts");
-        references.put("MRNA", "gene");
+        //references.put("MRNA", "gene");
         references.put("NcRNA", "gene");
-        references.put("SnRNA", "gene");
+        //references.put("SnRNA", "gene");
         references.put("SnoRNA", "gene");
         references.put("TRNA", "gene");
         references.put("PointMutation", "gene");
         references.put("PolyASite", "processedTranscripts");
-        references.put("Region", "gene");
+        // Region is inherited by loads of things, causes conflicts
+        //references.put("Region", "gene");
         references.put("RegulatoryRegion", "gene");
         references.put("SequenceVariant", "gene");
         references.put("FivePrimeUTR", "MRNAs");
+        references.put("ThreePrimeUTR", "MRNAs");
+        references.put("CDS", "MRNAs");
+
+        tgtNs = tgtModel.getNameSpace().toString();
     }
 
     /**
      * @see GFF3RecordHandler#process()
      */
     public void process(GFF3Record record) {
-        setReferences(references);
 
         Item feature = getFeature();
         String clsName = XmlUtil.getFragmentFromURI(feature.getClassName());
@@ -81,64 +90,166 @@ public class ChadoGFF3RecordHandler extends GFF3RecordHandler
         // name set in core - if no name symbol is CGxx?
         // create some synonyms
         if ("Gene".equals(clsName) || "Pseudogene".equals(clsName)) {
-            String organismDbId = parseFlyBaseId(record.getDbxrefs(), "FBgn");
-            if (organismDbId != null) {
-                feature.setAttribute("organismDbId", organismDbId);
-            } else {
+            Iterator fbgnIter = parseFlyBaseId(record.getDbxrefs(), "FBgn").iterator();
+            while (fbgnIter.hasNext()) {
+                String organismDbId = (String) fbgnIter.next();
+                if (!feature.hasAttribute("organismDbId")) {
+                    feature.setAttribute("organismDbId", organismDbId);
+                }
+                addItem(createSynonym(feature, "identifier", organismDbId, "FlyBase"));
+            }
+            if (!feature.hasAttribute("organismDbId")) {
                 LOG.warn("FlyBase gene with no FBgn: "
                          + feature.getAttribute("identifier").getValue());
             }
 
             // if no name set for gene then use CGxx (FlyBase symbol rules)
             if (feature.getAttribute("name") == null) {
+                // TODO create additional synonym with type "name"??
                 feature.setAttribute("name", feature.getAttribute("identifier").getValue());
+            }
+
+        }
+
+        // release 4.0 GFF3 is incorrect for some insertion_sites - have Parent reference
+        // to an unknown integer istead of a CG/CR number
+        if (record.getParents() != null) {
+            Iterator parentIter = record.getParents().iterator();
+            while (parentIter.hasNext()) {
+                String parentId = (String) parentIter.next();
+                if (!parentId.startsWith("C")) {
+                    LOG.warn("removing parent relation for " + clsName
+                             + " (" + feature.getIdentifier() + ")"
+                             + " because it has Parent=" + parentId);
+                    Iterator relIter = getParentRelations().iterator();
+                    while (relIter.hasNext()) {
+                        Item relation = (Item) relIter.next();
+                        items.remove("_relation" + relation.getIdentifier());
+                    }
+                }
             }
         }
 
+
         // set MRNA.organismDbId
         if ("MRNA".equals(clsName)) {
-            String organismDbId = parseFlyBaseId(record.getDbxrefs(), "FBtr");
-            if (organismDbId != null) {
-                feature.setAttribute("organismDbId", organismDbId);
+            // FlyBase GFF3 release 4.0 has non-coding RNAs modelled badly (fixed in 4.1)
+
+            Iterator fbIter = parseFlyBaseId(record.getDbxrefs(), "FBtr").iterator();
+            while (fbIter.hasNext()) {
+                String organismDbId = (String) fbIter.next();
+                if (!feature.hasAttribute("organismDbId")) {
+                    feature.setAttribute("organismDbId", organismDbId);
+                }
+                addItem(createSynonym(feature, "identifier", organismDbId, "FlyBase"));
             }
         }
 
         // set TransposableElement.organismDbId
         if ("TransposableElement".equals(clsName)) {
-            String organismDbId = parseFlyBaseId(record.getDbxrefs(), "FBti");
-            if (organismDbId != null) {
-                feature.setAttribute("organismDbId", organismDbId);
+            Iterator fbIter = parseFlyBaseId(record.getDbxrefs(), "FBti").iterator();
+            while (fbIter.hasNext()) {
+                String organismDbId = (String) fbIter.next();
+                if (!feature.hasAttribute("organismDbId")) {
+                    feature.setAttribute("organismDbId", organismDbId);
+                }
+                addItem(createSynonym(feature, "identifier", organismDbId, "FlyBase"));
             }
         }
 
-        // create additional referenced gene
+        // create additional referenced gene - this is parent of
         if ("SnRNA".equals(clsName) || "NcRNA".equals(clsName) || "SnoRNA".equals(clsName)
             || "TRNA".equals(clsName)) {
             Item gene = getItemFactory().makeItem(null, tgtNs + "Gene", "");
-            String organismDbId = parseFlyBaseId(record.getDbxrefs(), "FBgn");
-            if (organismDbId != null) {
-                gene.setAttribute("organismDbId", organismDbId);
-                addItem(gene);
-                feature.setReference("gene", gene.getIdentifier());
+
+            Iterator fbIter = parseFlyBaseId(record.getDbxrefs(), "FBgn").iterator();
+            while (fbIter.hasNext()) {
+                String organismDbId = (String) fbIter.next();
+                if (!gene.hasAttribute("organismDbId")) {
+                    gene.setAttribute("organismDbId", organismDbId);
+                    addItem(gene);
+                    feature.setReference("gene", gene.getIdentifier());
+                }
+                addItem(createSynonym(gene, "identifier", organismDbId, "FlyBase"));
+
+                // look in synonyms for a CG identifier for gene
                 List list = (List) record.getAttributes().get("synonym_2nd");
                 if (list != null) {
                     Iterator iter = list.iterator();
                     while (iter.hasNext()) {
                         String synonym = (String) iter.next();
                         if (synonym.startsWith("CG") && Character.isDigit(synonym.charAt(2))) {
-                            if (gene.hasAttribute("identifier")) {
-                                throw new RuntimeException("multiple CG identifiers found for"
-                                                           + "reference gene: " + organismDbId);
+                            // first CG as identifier, rest as synonyms
+                            if (!gene.hasAttribute("identifier")) {
+                                gene.setAttribute("identifier", synonym);
                             }
-                            gene.setAttribute("identifier", synonym);
+                            addItem(createSynonym(gene, "identifier", organismDbId, "FlyBase"));
                         }
                     }
                 }
             }
         }
+
+        // for CDS create additional Translation object
+        if ("CDS".equals(clsName)) {
+            // CDSs have identifiers like CG1234-PA - we want this to be the Translation
+            // identifier, for the CDS add an _CDS to the end.
+            String identifier = feature.getAttribute("identifier").getValue();
+            feature.setAttribute("identifier", identifier + "_CDS");
+
+            // create and reference additional Translation object, add to polypeptides collection
+            Item translation = getItemFactory().makeItem(null, tgtNs + "Translation", "");
+            translation.setAttribute("identifier", identifier);
+
+            addItem(translation);
+
+            feature.addCollection(new ReferenceList("polypeptides",
+                new ArrayList(Collections.singleton(translation.getIdentifier()))));
+
+            Iterator fbIter = parseFlyBaseId(record.getDbxrefs(), "FBpp").iterator();
+            while (fbIter.hasNext()) {
+                String organismDbId = (String) fbIter.next();
+                if (!feature.hasAttribute("organismDbId")) {
+                    feature.setAttribute("organismDbId", organismDbId);
+                }
+                addItem(createSynonym(feature, "identifier", organismDbId, "FlyBase"));
+            }
+            // TODO add GenBank protein identifier as synonym
+
+        }
+
+
+        setReferences(references);
     }
 
 
+    private Item createSynonym(Item subject, String type, String value, String sourceName) {
+        Item synonym = getItemFactory().makeItem(null, tgtNs + "Synonym", "");
+        synonym.setAttribute("type", type);
+        synonym.setAttribute("value", value);
+        synonym.setReference("subject", subject.getIdentifier());
+        synonym.setReference("source", getSourceIdentifier(sourceName));
+        return synonym;
+    }
+
+
+    private String getSourceIdentifier(String sourceName) {
+        String sourceId = null;
+        if (sourceName.equals("FlyBase")) {
+            sourceId = getInfoSource().getIdentifier();
+        } else {
+            sourceId = (String) sources.get(sourceName);
+            if (sourceId == null) {
+                Item infoSource = getItemFactory().makeItem(null, tgtNs + "InfoSource", "");
+                infoSource.setAttribute("title", sourceName);
+                // store once
+                addItem(infoSource);
+                sources.put(sourceName, infoSource.getIdentifier());
+                sourceId = infoSource.getIdentifier();
+            }
+        }
+        return sourceId;
+    }
 
 
     /**
@@ -148,22 +259,17 @@ public class ChadoGFF3RecordHandler extends GFF3RecordHandler
      * @param prefix the prefix of a FlyBase identifier type - e.g. FBgn
      * @return the identifier or null if none found
      */
-    protected String parseFlyBaseId(List dbxrefs, String prefix) {
-        String fb = null;
+    protected List parseFlyBaseId(List dbxrefs, String prefix) {
+        List fbs = new ArrayList();
         if (dbxrefs != null) {
             Iterator iter = dbxrefs.iterator();
             while (iter.hasNext()) {
                 String s = (String) iter.next();
                 if (s.startsWith("FlyBase:" + prefix)) {
-                    if (fb != null) {
-                        throw new RuntimeException("Multiple " + prefix + " ids found in dbxref: "
-                                                   + dbxrefs);
-                    }
-                    fb = s.substring(s.indexOf(":") + 1);
+                    fbs.add(s.substring(s.indexOf(":") + 1));
                 }
             }
         }
-        return fb;
+        return fbs;
     }
-
 }
