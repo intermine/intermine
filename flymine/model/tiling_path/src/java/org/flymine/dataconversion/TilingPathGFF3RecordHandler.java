7318543cf310c1f058d36a7ffc6c69fd3e053612
kmr <kmr>
2005-03-30 14:52:50 +0000

    A converter/retriever for the Drosophila tiling path GFF3 files.

--- flymine/model/tiling_path/src/java/org/flymine/dataconversion/TilingPathGFF3RecordHandler.java
+++ flymine/model/tiling_path/src/java/org/flymine/dataconversion/TilingPathGFF3RecordHandler.java
@@ -10,6 +10,9 @@ package org.flymine.dataconversion;
  *
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.intermine.metadata.Model;
 
 import org.flymine.io.gff3.GFF3Record;
@@ -22,18 +25,28 @@ import org.flymine.io.gff3.GFF3Record;
 
 public class TilingPathGFF3RecordHandler extends GFF3RecordHandler
 {
+    private Map references;
+
     /**
      * Create a new TilingPathGFF3RecordHandler for the given target model.
      * @param tgtModel the model for which items will be created
      */
     public TilingPathGFF3RecordHandler (Model tgtModel) {
         super(tgtModel);
+
+        // create a map of classname to reference name for parent references
+        // this will add the parents of any SimpleRelations from getParents() to the
+        // given collection
+        references = new HashMap();
+        references.put("PCRProduct", "tilingPathSpan");
+        references.put("ForwardPrimer", "pcrProduct");
+        references.put("ReversePrimer", "pcrProduct");
     }
 
     /**
      * @see GFF3RecordHandler#process()
      */
     public void process(GFF3Record record) {
-
+        setReferences(references);
     }
 }
