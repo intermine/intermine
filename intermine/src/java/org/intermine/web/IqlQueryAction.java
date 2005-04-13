16b8ecd53e22eb06c4d1ee70873ce1627770580f
tom <tom>
2005-04-13 09:45:59 +0000

    added small sleep into PortalQueryAction (as in ViewAction) and fixed IqlQueryAction
    Former-commit-id: 1fd08d5b6fa6241ef00aa27e868ad17e94236ebf

--- intermine/src/java/org/intermine/web/IqlQueryAction.java
+++ intermine/src/java/org/intermine/web/IqlQueryAction.java
@@ -76,10 +76,9 @@ public class IqlQueryAction extends InterMineLookupDispatchAction
             Query q = new IqlQuery(queryform.getQuerystring(),
                                    ((Model) os.getModel()).getPackageName()).toQuery();
             PagedTable table = TableHelper.makeTable(os, q);
-            //SessionMethods session.setAttribute(Constants.RESULTS_TABLE,
-            //      TableHelper.makeTable(os, q));
-            // TODO tom fix this
-            return mapping.findForward("results");
+            session.setAttribute(Constants.QUERY_RESULTS, TableHelper.makeTable(os, q));
+            return new ForwardParameters(mapping.findForward("results"))
+                    .addParameter("table", "results").forward();
         } catch (java.lang.IllegalArgumentException e) {
             recordError(new ActionMessage("errors.iqlquery.illegalargument",
                                           e.getMessage()), request, e, LOG);
