a6a82c719ad26a0deeb09bce0661a88a0e7ff6dc
mnw21 <mnw21>
2005-04-21 15:53:36 +0000

    Additional logging, to track down Wenyan's bug.
    Former-commit-id: 42346bacaf25603aae243691b68ca9236795c318

--- intermine/src/java/org/intermine/dataloader/ObjectStoreDataLoader.java
+++ intermine/src/java/org/intermine/dataloader/ObjectStoreDataLoader.java
@@ -17,6 +17,7 @@ import org.intermine.model.InterMineObject;
 import org.intermine.model.datatracking.Source;
 import org.intermine.objectstore.ObjectStore;
 import org.intermine.objectstore.ObjectStoreException;
+import org.intermine.objectstore.fastcollections.ObjectStoreFastCollectionsForTranslatorImpl;
 import org.intermine.objectstore.query.Query;
 import org.intermine.objectstore.query.QueryClass;
 import org.intermine.objectstore.query.SingletonResults;
@@ -70,59 +71,65 @@ public class ObjectStoreDataLoader extends DataLoader
      */
     public void process(ObjectStore os, Source source, Source skelSource, Class queryClass)
         throws ObjectStoreException {
-        long times[] = new long[20];
-        for (int i = 0; i < 20; i++) {
-            times[i] = -1;
-        }
-        Query q = new Query();
-        QueryClass qc = new QueryClass(queryClass);
-        q.addFrom(qc);
-        q.addToSelect(qc);
-        q.setDistinct(false);
-        long opCount = 0;
-        long time = (new Date()).getTime();
-        long startTime = time;
-        iw.beginTransaction();
-        SingletonResults res = new SingletonResults(q, os, os.getSequence());
-        res.setNoOptimise();
-        res.setNoExplain();
-        res.setBatchSize(1000);
-        Iterator iter = res.iterator();
-        while (iter.hasNext()) {
-            InterMineObject obj = (InterMineObject) iter.next();
-            //if (obj.getClass().getName().equals("org.intermine.model.chado.feature")) {
-            //    String objText = obj.toString();
-            //    int objTextLen = objText.length();
-            //    System//.out.println("Storing " + objText.substring(0, (objTextLen > 60 ? 60
-            //                    : objTextLen)));
-            //}
-            iw.store(obj, source, skelSource);
-            opCount++;
-            if (opCount % 1000 == 0) {
-                long now = (new Date()).getTime();
-                if (times[(int) ((opCount / 1000) % 20)] == -1) {
-                    LOG.info("Dataloaded " + opCount + " objects - running at "
-                            + (60000000 / (now - time)) + " (avg "
-                            + ((60000L * opCount) / (now - startTime))
-                            + ") objects per minute -- now on "
-                            + DynamicUtil.decomposeClass(obj.getClass()));
-                } else {
-                    LOG.info("Dataloaded " + opCount + " objects - running at "
-                            + (60000000 / (now - time)) + " (20000 avg "
-                            + (1200000000 / (now - times[(int) ((opCount / 1000) % 20)]))
-                            + ") (avg = " + ((60000L * opCount) / (now - startTime))
-                            + ") objects per minute -- now on "
-                            + DynamicUtil.decomposeClass(obj.getClass()));
-                }
-                time = now;
-                times[(int) ((opCount / 1000) % 20)] = now;
-                iw.commitTransaction();
-                iw.beginTransaction();
+        try {
+            long times[] = new long[20];
+            for (int i = 0; i < 20; i++) {
+                times[i] = -1;
             }
+            Query q = new Query();
+            QueryClass qc = new QueryClass(queryClass);
+            q.addFrom(qc);
+            q.addToSelect(qc);
+            q.setDistinct(false);
+            long opCount = 0;
+            long time = (new Date()).getTime();
+            long startTime = time;
+            iw.beginTransaction();
+            SingletonResults res = new SingletonResults(q, os, os.getSequence());
+            res.setNoOptimise();
+            res.setNoExplain();
+            res.setBatchSize(1000);
+            Iterator iter = res.iterator();
+            while (iter.hasNext()) {
+                InterMineObject obj = (InterMineObject) iter.next();
+                //if (obj.getClass().getName().equals("org.intermine.model.chado.feature")) {
+                //    String objText = obj.toString();
+                //    int objTextLen = objText.length();
+                //    System//.out.println("Storing " + objText.substring(0, (objTextLen > 60 ? 60
+                //                    : objTextLen)));
+                //}
+                iw.store(obj, source, skelSource);
+                opCount++;
+                if (opCount % 1000 == 0) {
+                    long now = (new Date()).getTime();
+                    if (times[(int) ((opCount / 1000) % 20)] == -1) {
+                        LOG.info("Dataloaded " + opCount + " objects - running at "
+                                + (60000000 / (now - time)) + " (avg "
+                                + ((60000L * opCount) / (now - startTime))
+                                + ") objects per minute -- now on "
+                                + DynamicUtil.decomposeClass(obj.getClass()));
+                    } else {
+                        LOG.info("Dataloaded " + opCount + " objects - running at "
+                                + (60000000 / (now - time)) + " (20000 avg "
+                                + (1200000000 / (now - times[(int) ((opCount / 1000) % 20)]))
+                                + ") (avg = " + ((60000L * opCount) / (now - startTime))
+                                + ") objects per minute -- now on "
+                                + DynamicUtil.decomposeClass(obj.getClass()));
+                    }
+                    time = now;
+                    times[(int) ((opCount / 1000) % 20)] = now;
+                    iw.commitTransaction();
+                    iw.beginTransaction();
+                }
+            }
+            LOG.info("Finished dataloading " + opCount + " objects at " + ((60000L * opCount)
+                        / ((new Date()).getTime() - startTime)) + " object per minute");
+            iw.commitTransaction();
+            iw.close();
+        } catch (RuntimeException e) {
+            LOG.error("Exception while dataloading - doneAlreadyMap = "
+                    + ((ObjectStoreFastCollectionsForTranslatorImpl) os).getDoneAlready(), e);
+            throw e;
         }
-        LOG.info("Finished dataloading " + opCount + " objects at " + ((60000L * opCount)
-                    / ((new Date()).getTime() - startTime)) + " object per minute");
-        iw.commitTransaction();
-        iw.close();
     }
 }
