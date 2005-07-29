4b2f8a433bcc27c77e953667986c86c953848d69
kmr <kmr>
2005-07-29 16:07:12 +0000

    Added a dummy implementation of ObjectStoreWriter for testing.
    Former-commit-id: ff7d715d9ecfe1f71305cea681580eb851b0c137

---
+++ intermine/objectstore/test/src/org/intermine/objectstore/dummy/ObjectStoreWriterDummyImplTest.java
@@ -0,0 +1,60 @@
+package org.intermine.objectstore.dummy;
+
+import org.intermine.metadata.Model;
+import org.intermine.model.InterMineObject;
+import org.intermine.model.testmodel.Company;
+import org.intermine.util.DynamicUtil;
+
+import java.util.Collections;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for the ObjectStoreWriterDummyImpl class.
+ * @author Kim Rutherford
+ */
+public class ObjectStoreWriterDummyImplTest extends TestCase {
+    public void testStore() throws Exception {
+        ObjectStoreDummyImpl os = new ObjectStoreDummyImpl();
+        os.setModel(Model.getInstanceByName("testmodel"));
+        ObjectStoreWriterDummyImpl osw = new ObjectStoreWriterDummyImpl(os);
+        
+        InterMineObject o1 = 
+            (InterMineObject) DynamicUtil.createObject(Collections.singleton(Company.class));
+        osw.store(o1);
+        
+        InterMineObject o2 = 
+            (InterMineObject) DynamicUtil.createObject(Collections.singleton(Company.class));
+        o2.setId(new Integer(1));
+        osw.store(o2);
+        
+        InterMineObject o3 =
+            (InterMineObject) DynamicUtil.createObject(Collections.singleton(Company.class));
+        osw.store(o3);
+
+        InterMineObject o4 = 
+            (InterMineObject) DynamicUtil.createObject(Collections.singleton(Company.class));
+        o4.setId(new Integer(100));
+        // store twice to make sure we get only one copy 
+        osw.store(o4);
+        osw.store(o4);
+
+        
+        InterMineObject o5 = 
+            (InterMineObject) DynamicUtil.createObject(Collections.singleton(Company.class));
+        // store twice to make sure we get only one copy 
+        osw.store(o5);
+        osw.store(o5);
+        
+        Map storedObjects = osw.getStoredObjects();
+        
+        assertEquals(5, storedObjects.size());
+        
+        assertTrue(storedObjects.get(new Integer(0)) == o1);
+        assertTrue(storedObjects.get(new Integer(1)) == o2);
+        assertTrue(storedObjects.get(new Integer(2)) == o3);
+        assertTrue(storedObjects.get(new Integer(3)) == o5);
+        assertTrue(storedObjects.get(new Integer(100)) == o4);
+    }
+}
