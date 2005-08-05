3265d5c779ac3a5d85ee8b9cdfc64c9e4911a030
tom <tom>
2005-08-05 15:33:21 +0000

    fixes #417, decoding escaping in gff
    Former-commit-id: f4beaad750b3932c36db7d2dffa6634a12b17070

--- flymine/src/test/org/flymine/io/gff3/GFF3ParserTest.java
+++ flymine/src/test/org/flymine/io/gff3/GFF3ParserTest.java
@@ -16,6 +16,9 @@ import java.util.*;
 import java.io.StringReader;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.URLDecoder;
+
+import org.apache.commons.lang.StringEscapeUtils;
 
 /**
  * Tests for the GFF3Parser class.
@@ -92,6 +95,35 @@ public class GFF3ParserTest extends TestCase
         }
     }
 
+    public void testDecoding() throws Exception {
+        String input = "2L\t.\tgene\t14263522\t14328265\t.\t+\t.\t" +
+                "ID=CG15288;Name=wb;Dbxref=FlyBase:FBan0015288,FlyBase:FBgn0004002;cyto_range=35A3-35A4;" +
+                "dbxref_2nd=FlyBase:FBgn0025691,FlyBase:FBgn0032544,GB:CG15288;gbunit=AE003643;synonym=wb;" +
+                "synonym_2nd=A1,BEST:CK02229,BG:DS03792.1,CK02229,CT35236,DLAM,D-laminin+%26agr%3B2,l(2)09437," +
+                "l(2)34Fb,l(2)br1,l34Fb,laminin,laminin+%26agr%3B1%2C2,Laminin+%26agr%3B1%2C2,LM-A/%26agr%3B2,wing+blistered";
+        GFF3Record record = new GFF3Record(input);
+    }
+    
+    public void testFixGreek() {
+        String input = "<br/>&agr;<br/>&Agr;<br/>&bgr;<br/>&Bgr;<br/>&ggr;" +
+                "<br/>&Ggr;<br/>&dgr;<br/>&Dgr;<br/>&egr;<br/>&Egr;<br/>&zgr;" +
+                "<br/>&Zgr;<br/>&eegr;<br/>&EEgr;<br/>&thgr;<br/>&THgr;<br/>&igr;" +
+                "<br/>&Igr;<br/>&kgr;<br/>&Kgr;<br/>&lgr;<br/>&Lgr;<br/>&mgr;<br/>&Mgr;" +
+                "<br/>&ngr;<br/>&Ngr;<br/>&xgr;<br/>&Xgr;<br/>&ogr;<br/>&Ogr;<br/>&pgr;<br/>" +
+                "&Pgr;<br/>&rgr;<br/>&Rgr;<br/>&sgr;<br/>&Sgr;<br/>&sfgr;<br/>&tgr;<br/>&Tgr;" +
+                "<br/>&ugr;<br/>&Ugr;<br/>&phgr;<br/>&PHgr;<br/>&khgr;<br/>&KHgr;<br/>&psgr;" +
+                "<br/>&PSgr;<br/>&ohgr;<br/>&OHgr;";
+        String expect = "<br/>&alpha;<br/>&Alpha;<br/>&beta;<br/>&Beta;<br/>&gamma;" +
+                "<br/>&Gamma;<br/>&delta;<br/>&Delta;<br/>&epsilon;<br/>&Epsilon;<br/>&zeta;" +
+                "<br/>&Zeta;<br/>&eta;<br/>&Eta;<br/>&theta;<br/>&Theta;<br/>&iota;" +
+                "<br/>&Iota;<br/>&kappa;<br/>&Kappa;<br/>&lambda;<br/>&Lambda;<br/>&mu;<br/>&Mu;" +
+                "<br/>&nu;<br/>&Nu;<br/>&xi;<br/>&Xi;<br/>&omicron;<br/>&Omicron;<br/>&pi;" +
+                "<br/>&Pi;<br/>&rho;<br/>&Rho;<br/>&sigma;<br/>&Sigma;<br/>&sigmaf;<br/>&tau;<br/>&Tau;" +
+                "<br/>&upsilon;<br/>&Upsilon;<br/>&phi;<br/>&Phi;<br/>&chi;<br/>&Chi;<br/>&psi;" +
+                "<br/>&Psi;<br/>&omega;<br/>&Omega;";
+        String output = GFF3Record.fixEntityNames(input);
+        assertEquals(expect, output);
+    }
 
     public void testToGFF3() throws Exception {
         String original="4\t.\texon\t22335\t22528\t.\t-\t.\tID=CG32013:2;Parent=CG32013-RA\n"
