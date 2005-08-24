a86a457e0d939f16bf275010a912e16f3141c9f8
mnw21 <mnw21>
2005-08-24 14:47:50 +0000

    Accidentally checked in broken tests - this disables them

--- intermine/objectstore/test/src/org/intermine/objectstore/intermine/SqlGeneratorTest.java
+++ intermine/objectstore/test/src/org/intermine/objectstore/intermine/SqlGeneratorTest.java
@@ -313,10 +313,12 @@ public class SqlGeneratorTest extends SetupDataTestCase
         results2.put("NotSubqueryExistsConstraint", Collections.singleton("Company"));
         results.put("SubqueryExistsConstraintNeg", "SELECT 'hello' AS a1_ WHERE EXISTS(SELECT a1_.id FROM Bank AS a1_)");
         results2.put("SubqueryExistsConstraintNeg", Collections.singleton("Bank"));
-        results.put("ObjectPathExpression", "SELECT a1_.id AS a1_id FROM Employee AS a1_ ORDER BY a1_.id");
-        results2.put("ObjectPathExpression", new HashSet(Arrays.asList(new String[] {"InterMineObject", "Employee", "Department"})));
-        results.put("FieldPathExpression", "SELECT a1_.id AS a1_id FROM Company AS a1_ ORDER BY a1_.id");
-        results2.put("FieldPathExpression", new HashSet(Arrays.asList(new String[] {"InterMineObject", "Company", "CEO"})));
+        //results.put("ObjectPathExpression", "SELECT a1_.id AS a1_id FROM Employee AS a1_ ORDER BY a1_.id");
+        //results2.put("ObjectPathExpression", new HashSet(Arrays.asList(new String[] {"InterMineObject", "Employee", "Department"})));
+        //results.put("FieldPathExpression", "SELECT a1_.id AS a1_id FROM Company AS a1_ ORDER BY a1_.id");
+        //results2.put("FieldPathExpression", new HashSet(Arrays.asList(new String[] {"InterMineObject", "Company", "CEO"})));
+        results.put("ObjectPathExpression", NO_RESULT);
+        results.put("FieldPathExpression", NO_RESULT);
         results.put("ForeignKey", "SELECT a1_.id AS a1_id, a1_.CEOId AS a2_ FROM Company AS a1_ ORDER BY a1_.id");
         results2.put("ForeignKey", new HashSet(Arrays.asList(new String[] {"InterMineObject", "Company"})));
         results.put("ForeignKey2", "SELECT a1_.id AS a1_id, a1_.CEOId AS a2_ FROM Company AS a1_ ORDER BY a1_.id");
