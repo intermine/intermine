61d4c8f481b283ee1236b92c36056ee9c6b2da36
wenyan <wenyan>
2005-09-09 15:19:29 +0000

    for imageClone with multiple reference to gene, temporarily remove clone and genes

--- flymine/model/cdnaclone/src/java/org/flymine/dataconversion/ImageCloneConverter.java
+++ flymine/model/cdnaclone/src/java/org/flymine/dataconversion/ImageCloneConverter.java
@@ -14,6 +14,8 @@ import java.io.Reader;
 import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.metadata.Model;
@@ -22,8 +24,8 @@ import org.intermine.xml.full.Item;
 import org.intermine.xml.full.ReferenceList;
 import org.intermine.xml.full.ItemHelper;
 import org.intermine.xml.full.ItemFactory;
-import org.intermine.dataconversion.FileConverter;
 import org.intermine.dataconversion.ItemWriter;
+import org.intermine.util.StringUtil;
 
 import org.apache.log4j.Logger;
 
@@ -41,6 +43,8 @@ public class ImageCloneConverter extends CDNACloneConverter
     protected Item dataSet;
     protected Item organism;
     protected ItemFactory itemFactory;
+    protected Map geneMap = new HashMap();
+    protected Map cloneMap = new HashMap();
 
     /**
      * Constructor
@@ -48,8 +52,8 @@ public class ImageCloneConverter extends CDNACloneConverter
      * @throws ObjectStoreException if an error occurs in storing
      * @throws MetaDataException if cannot generate model
      */
-    public ImageCloneConverter(ItemWriter writer) throws ObjectStoreException,
-                                                        MetaDataException {
+    public ImageCloneConverter(ItemWriter writer)
+        throws ObjectStoreException, MetaDataException {
         super(writer);
 
         itemFactory = new ItemFactory(Model.getInstanceByName("genomic"), "-1_");
@@ -61,10 +65,11 @@ public class ImageCloneConverter extends CDNACloneConverter
         dataSet = createItem("DataSet");
         dataSet.setAttribute("title", "RZPD uniprot data set");
         writer.store(ItemHelper.convert(dataSet));
- 
+
         organism = createItem("Organism");
         organism.setAttribute("abbreviation", "HS");
         writer.store(ItemHelper.convert(organism));
+
     }
 
 
@@ -86,33 +91,72 @@ public class ImageCloneConverter extends CDNACloneConverter
                 continue;
             }
 
-            String geneId = array[16].trim();
-            Item gene = createGene("Gene", geneId, organism.getIdentifier());
+            String[] geneId = array[16].trim().split(",");
+            if (geneId.length == 1) {
+                Item gene = createGene("Gene", geneId[0], organism.getIdentifier(), writer);
+                String cloneId = array[5].trim();
+                if (StringUtil.allDigits(cloneId)) {
+                    Item clone = createClone("CDNAClone", cloneId, organism.getIdentifier(),
+                    gene.getIdentifier(), dataSource.getIdentifier(),dataSet.getIdentifier(), writer);
+                 }
+            }
+        }
+
+    }
+
+    /**
+     * @param clsName = target class name
+     * @param id = identifier
+     * @param ordId = ref id for organism
+     * @param writer = itemWriter write item to objectstore
+     * @return item
+     * @throws exception if anything goes wrong when writing items to objectstore
+     */
+    private Item createGene(String clsName, String id, String orgId, ItemWriter writer)
+        throws Exception {
+        Item gene = (Item) geneMap.get(id);
+        if (gene == null) {
+            gene = createItem(clsName);
+            gene.setAttribute("organismDbId", id);
+            gene.setReference("organism", orgId);
+            geneMap.put(id, gene);
             writer.store(ItemHelper.convert(gene));
+        }
+        return gene;
+    }
 
-            String cloneId = array[5];
-
-            Item clone = createBioEntity("CDNAClone", cloneId, organism.getIdentifier());
-            clone.setReference("gene", gene.getIdentifier());
+    /**
+     * @param clsName = target class name
+     * @param id = identifier
+     * @param ordId = ref id for organism
+     * @param geneId = ref id for gene item
+     * @param dbId = ref id for db item
+     * @param writer = itemWriter write item to objectstore
+     * @return item
+     * @throws exception if anything goes wrong when writing items to objectstore
+     */
+     private Item createClone(String clsName, String id, String orgId, String geneId, String datasourceId,
+                  String datasetId, ItemWriter writer)  throws Exception {
+        Item clone = (Item) cloneMap.get(id);
+        if (clone == null) {
+            clone = createItem(clsName);
+            clone.setAttribute("identifier", id);
+            clone.setReference("organism", orgId);
+            clone.setReference("gene", geneId);
+            clone.addCollection(new ReferenceList("evidence",
+                                        new ArrayList(Collections.singleton(datasetId))));
+            cloneMap.put(id, clone);
+            writer.store(ItemHelper.convert(clone));
 
             Item synonym = createItem("Synonym");
             synonym.setAttribute("type", "identifier");
-            synonym.setAttribute("value", cloneId);
-            synonym.setReference("source", dataSource.getIdentifier());
+            synonym.setAttribute("value", id);
+            synonym.setReference("source", datasourceId);
             synonym.setReference("subject", clone.getIdentifier());
             writer.store(ItemHelper.convert(synonym));
 
-            clone.addCollection(new ReferenceList("evidence",
-                    new ArrayList(Collections.singleton(dataSet.getIdentifier()))));
-            writer.store(ItemHelper.convert(clone));
         }
-    }
-
-    private Item createGene(String clsName, String id, String orgId) {
-        Item gene = createItem(clsName);
-        gene.setAttribute("organismDbId", id);
-        gene.setReference("organism", orgId);
-        return gene;
+        return clone;
     }
 
 }
