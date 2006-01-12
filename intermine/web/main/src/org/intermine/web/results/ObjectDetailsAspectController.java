5d8425d13329fa9564fd745e5b07c63d91131ce5
tom <tom>
2006-01-12 11:34:07 +0000

    added missing file

---
+++ intermine/web/main/src/org/intermine/web/results/ObjectDetailsAspectController.java
@@ -0,0 +1,50 @@
+package org.intermine.web.results;
+
+/*
+ * Copyright (C) 2002-2005 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.intermine.objectstore.ObjectStore;
+import org.intermine.web.Constants;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.tiles.ComponentContext;
+import org.apache.struts.tiles.actions.TilesAction;
+
+/**
+ * 
+ * @author Thomas Riley
+ */
+public class ObjectDetailsAspectController extends TilesAction
+{
+    /**
+     * @see TilesAction#execute
+     */
+    public ActionForward execute(ComponentContext context,
+                                 ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+        throws Exception {
+        HttpSession session = request.getSession();
+        ServletContext servletContext = session.getServletContext();
+        ObjectStore os = (ObjectStore) servletContext.getAttribute(Constants.OBJECTSTORE);
+        DisplayObject dobj = (DisplayObject) context.getAttribute("displayObject");
+        
+        return null;
+    }
+
+}
