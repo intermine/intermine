516e7f9130f48355255f911cb7e44fe24f8dd306
rnsmith <rns>
2006-01-23 18:39:38 +0000

    project to test webservice
    Former-commit-id: 25628d1f56d1a8a23826a4a4f87b56fb232fb11c

---
+++ testmodel/webservice/test/src/org/intermine/objectstore/webservice/ObjectStoreClientTest.java
@@ -0,0 +1,54 @@
+package org.intermine.objectstore.webservice;
+
+/*
+ * Copyright (C) 2002-2005 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+import org.intermine.objectstore.ObjectStoreAbstractImplTestCase;
+import org.intermine.objectstore.ObjectStoreFactory;
+import org.intermine.objectstore.query.Query;
+import org.intermine.objectstore.query.iql.IqlQuery;
+import org.intermine.objectstore.safe.ObjectStoreSafeImpl;
+
+public class ObjectStoreClientTest extends TestCase //extends ObjectStoreAbstractImplTestCase
+{
+    private ObjectStoreClient osai;
+
+//     public static void oneTimeSetUp() throws Exception {
+//         osai = (ObjectStoreClient) ObjectStoreFactory.getObjectStore("os.unittest-client");
+//         os = new ObjectStoreSafeImpl(osai);
+//         ObjectStoreAbstractImplTestCase.oneTimeSetUp();
+//     }
+
+    public void setUp() throws Exception {
+        osai = (ObjectStoreClient) ObjectStoreFactory.getObjectStore("os.unittest-client");
+    }
+
+    public ObjectStoreClientTest(String arg) {
+        super(arg);
+    }
+
+//      public static Test suite() {
+//          return buildSuite(ObjectStoreClientTest.class);
+//      }
+
+    public void testQueryId() throws Exception {
+        Query q1 = new IqlQuery("select a1_ from Company as a1_", "org.intermine.model.testmodel").toQuery();
+        Query q2 = new IqlQuery("select a1_ from Company as a1_", "org.intermine.model.testmodel").toQuery();
+
+        assertEquals(((ObjectStoreClient) osai).getQueryId(q1), ((ObjectStoreClient) osai).getQueryId(q1));
+        assertEquals(((ObjectStoreClient) osai).getQueryId(q2), ((ObjectStoreClient) osai).getQueryId(q2));
+
+        assertTrue(((ObjectStoreClient) osai).getQueryId(q1) != ((ObjectStoreClient) osai).getQueryId(q2));
+    }
+
+}
