fd926b54b82e545ea71c092f211d98673cb617ed
wenyan <wenyan>
2006-02-10 16:20:33 +0000

    added length attribute to sequence

--- flymine/model/tfbscluster/src/java/org/flymine/dataconversion/CnsGFF3RecordHandler.java
+++ flymine/model/tfbscluster/src/java/org/flymine/dataconversion/CnsGFF3RecordHandler.java
@@ -104,6 +104,7 @@ public class CnsGFF3RecordHandler extends GFF3RecordHandler
         if (sequence == null) {
             sequence = createItem("Sequence");
             sequence.setAttribute("residues", residues);
+            sequence.setAttribute("length", residues.size());
             addItem(sequence);
             sequenceMap.put(residues, sequence);
         }
