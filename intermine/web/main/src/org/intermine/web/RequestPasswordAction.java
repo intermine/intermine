1c229b4ba462628c841740e7004d5e9d815ec27e
watkins <watkins>
2006-05-02 15:16:24 +0000

    Added pages and logic to let a user change his password and create an account.

--- intermine/web/main/src/org/intermine/web/RequestPasswordAction.java
+++ intermine/web/main/src/org/intermine/web/RequestPasswordAction.java
@@ -10,31 +10,30 @@ package org.intermine.web;
  *
  */
 
-import javax.mail.Transport;
+import java.text.MessageFormat;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Random;
+
 import javax.mail.Message;
 import javax.mail.Session;
-import javax.mail.internet.MimeMessage;
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionMessage;
 
-import java.text.MessageFormat;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Random;
-import java.util.Properties;
-
-import org.apache.log4j.Logger;
-
 /**
  * Action to handle button presses RequestPasswordForm
+ * 
  * @author Mark Woodbridge
  */
 public class RequestPasswordAction extends InterMineAction
@@ -43,21 +42,23 @@ public class RequestPasswordAction extends InterMineAction
 
     protected static Random random = new Random();
 
-    /** 
+    /**
      * Method called when user has finished updating a constraint
-     * @param mapping The ActionMapping used to select this instance
-     * @param form The optional ActionForm bean for this request (if any)
-     * @param request The HTTP request we are processing
-     * @param response The HTTP response we are creating
+     * 
+     * @param mapping
+     *            The ActionMapping used to select this instance
+     * @param form
+     *            The optional ActionForm bean for this request (if any)
+     * @param request
+     *            The HTTP request we are processing
+     * @param response
+     *            The HTTP response we are creating
      * @return an ActionForward object defining where control goes next
-     * @exception Exception if the application business logic throws
-     *  an exception
+     * @exception Exception
+     *                if the application business logic throws an exception
      */
-    public ActionForward execute(ActionMapping mapping,
-                                 ActionForm form,
-                                 HttpServletRequest request,
-                                 HttpServletResponse response)
-        throws Exception {
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
         HttpSession session = request.getSession();
         ServletContext servletContext = session.getServletContext();
         ProfileManager pm = (ProfileManager) servletContext.getAttribute(Constants.PROFILE_MANAGER);
@@ -67,26 +68,18 @@ public class RequestPasswordAction extends InterMineAction
         boolean successful = false;
         if (pm.hasProfile(username)) {
             successful = email(username, pm.getPassword(username), webProperties);
-        } else {
-            String password = generatePassword();
-            successful = email(username, password, webProperties);
-            if (successful) {
-                pm.saveProfile(new Profile(pm, username, password,
-                                           new HashMap(), new HashMap(), new HashMap()));
-            }
         }
-
         if (successful) {
             recordMessage(new ActionMessage("login.emailed", username), request);
         } else {
             recordError(new ActionMessage("login.invalidemail"), request);
         }
-
         return mapping.findForward("login");
     }
 
     /**
      * Generate a random 8-letter String of lower-case characters
+     * 
      * @return the String
      */
     public static String generatePassword() {
@@ -99,9 +92,13 @@ public class RequestPasswordAction extends InterMineAction
 
     /**
      * Email a password to an email address
-     * @param to the address to send to 
-     * @param password the password to send
-     * @param webProperties properties such as the from address
+     * 
+     * @param to
+     *            the address to send to
+     * @param password
+     *            the password to send
+     * @param webProperties
+     *            properties such as the from address
      * @return true if sending was successful
      */
     public static boolean email(String to, String password, Map webProperties) {
@@ -110,7 +107,8 @@ public class RequestPasswordAction extends InterMineAction
             String from = (String) webProperties.get("mail.from");
             String subject = (String) webProperties.get("mail.subject");
             String text = (String) webProperties.get("mail.text");
-            text = MessageFormat.format(text, new Object[] {password});
+            text = MessageFormat.format(text, new Object[] { 
+                       password });
             Properties properties = System.getProperties();
             properties.put("mail.smtp.host", host);
             MimeMessage message = new MimeMessage(Session.getDefaultInstance(properties, null));
