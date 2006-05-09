2cb09e3419fe5de8fc9198e5117c37b1e59b756a
watkins <watkins>
2006-05-09 17:02:28 +0000

    Boolean fields now only constrained using as [true, false, null]
    Former-commit-id: 8bda38555d466044290422494a8ee7d32b2c63b7

--- intermine/web/main/src/org/intermine/web/MainAction.java
+++ intermine/web/main/src/org/intermine/web/MainAction.java
@@ -22,38 +22,41 @@ import org.apache.struts.action.ActionMapping;
 
 /**
  * Action to handle button presses on the main tile
+ * 
  * @author Mark Woodbridge
  */
 public class MainAction extends InterMineAction
 {
-    /** 
+    /**
      * Method called when user has finished updating a constraint
-     * @param mapping The ActionMapping used to select this instance
-     * @param form The optional ActionForm bean for this request (if any)
-     * @param request The HTTP request we are processing
-     * @param response The HTTP response we are creating
+     * 
+     * @param mapping
+     *            The ActionMapping used to select this instance
+     * @param form
+     *            The optional ActionForm bean for this request (if any)
+     * @param request
+     *            The HTTP request we are processing
+     * @param response
+     *            The HTTP response we are creating
      * @return an ActionForward object defining where control goes next
-     * @exception Exception if the application business logic throws
-     *  an exception
+     * @exception Exception
+     *                if the application business logic throws an exception
      */
-    public ActionForward execute(ActionMapping mapping,
-                                 ActionForm form,
-                                 HttpServletRequest request,
-                                 HttpServletResponse response)
-        throws Exception {
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
         HttpSession session = request.getSession();
         PathQuery query = (PathQuery) session.getAttribute(Constants.QUERY);
         MainForm mf = (MainForm) form;
 
         PathNode node = (PathNode) query.getNodes().get(mf.getPath());
-        
-        Integer cindex = (request.getParameter("cindex") != null)
-                ? new Integer(request.getParameter("cindex")) : null;
+
+        Integer cindex = (request.getParameter("cindex") != null) ? new Integer(request
+                .getParameter("cindex")) : null;
 
         String label = null, id = null, code = query.getUnusedConstraintCode();
         boolean editable = false;
         int previousConstraintCount = query.getAllConstraints().size();
-        
+
         if (cindex != null) {
             // We're updating an existing constraint, just remove the old one
             Constraint c = (Constraint) node.getConstraints().get(cindex.intValue());
@@ -62,76 +65,82 @@ public class MainAction extends InterMineAction
             id = c.getIdentifier();
             editable = c.isEditable();
             code = c.getCode();
-            
+
             if (request.getParameter("template") != null) {
                 // We're just updating template settings
-                node.getConstraints().add(new Constraint(c.getOp(), c.getValue(), mf.isEditable(),
-                        mf.getTemplateLabel(), c.getCode(), mf.getTemplateId()));
+                node.getConstraints().add(
+                        new Constraint(c.getOp(), c.getValue(), mf.isEditable(), mf
+                                .getTemplateLabel(), c.getCode(), mf.getTemplateId()));
                 mf.reset(mapping, request);
                 return mapping.findForward("query");
             }
         }
-        
+
         if (request.getParameter("attribute") != null && cindex == null) {
             // New constraint
             label = mf.getTemplateLabel();
             id = mf.getTemplateId();
             editable = mf.isEditable();
         }
-        
+
         if (request.getParameter("attribute") != null) {
-            ConstraintOp constraintOp = ConstraintOp.
-                getOpForIndex(Integer.valueOf(mf.getAttributeOp()));
+            ConstraintOp constraintOp = ConstraintOp.getOpForIndex(Integer.valueOf(mf
+                    .getAttributeOp()));
             Object constraintValue = mf.getParsedAttributeValue();
-            Constraint c = new Constraint(constraintOp, constraintValue, editable, label, code, id);
-            node.getConstraints().add(c);
+            if (constraintValue.equals("NULL")) {
+                node.getConstraints().add(
+                        new Constraint(ConstraintOp.IS_NULL, null, false, label, code, id));
+            } else {
+                Constraint c = new Constraint(constraintOp, constraintValue, editable, label, code,
+                        id);
+                node.getConstraints().add(c);
+            }
         }
 
         if (request.getParameter("bag") != null) {
-            ConstraintOp constraintOp = ConstraintOp.
-                getOpForIndex(Integer.valueOf(mf.getBagOp()));
+            ConstraintOp constraintOp = ConstraintOp.getOpForIndex(Integer.valueOf(mf.getBagOp()));
             Object constraintValue = mf.getBagValue();
             Constraint c = new Constraint(constraintOp, constraintValue, false, label, code, id);
             node.getConstraints().add(c);
         }
 
-        if (request.getParameter ("loop") != null) {
-            ConstraintOp constraintOp = ConstraintOp.
-                getOpForIndex(Integer.valueOf(mf.getLoopQueryOp()));
+        if (request.getParameter("loop") != null) {
+            ConstraintOp constraintOp = ConstraintOp.getOpForIndex(Integer.valueOf(mf
+                    .getLoopQueryOp()));
             Object constraintValue = mf.getLoopQueryValue();
             Constraint c = new Constraint(constraintOp, constraintValue, false, label, code, id);
             node.getConstraints().add(c);
         }
-        
+
         if (request.getParameter("subclass") != null) {
             node.setType(mf.getSubclassValue());
             session.setAttribute("path", mf.getSubclassValue());
             session.setAttribute("prefix", mf.getPath());
         }
-        
+
         if (request.getParameter("nullnotnull") != null) {
             if (mf.getNullConstraint().equals("NotNULL")) {
-                node.getConstraints()
-                    .add(new Constraint(ConstraintOp.IS_NOT_NULL, null, false, label, code, id));
+                node.getConstraints().add(
+                        new Constraint(ConstraintOp.IS_NOT_NULL, null, false, label, code, id));
             } else {
-                node.getConstraints()
-                    .add(new Constraint(ConstraintOp.IS_NULL, null, false, label, code, id));
+                node.getConstraints().add(
+                        new Constraint(ConstraintOp.IS_NULL, null, false, label, code, id));
             }
         }
-        
+
         if (request.getParameter("expression") != null) {
             query.setConstraintLogic(request.getParameter("expr"));
             query.syncLogicExpression(SessionMethods.getDefaultOperator(session));
         }
-        
+
         if (cindex != null) {
             session.setAttribute(Constants.DEFAULT_OPERATOR, mf.getOperator());
         }
-        
+
         if (query.getAllConstraints().size() == previousConstraintCount + 1) {
             query.syncLogicExpression(mf.getOperator());
         }
-        
+
         mf.reset(mapping, request);
 
         return mapping.findForward("query");
