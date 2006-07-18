4c9bdaa53bf571c135051ebfcf4fbf3542b054b3
mnw21 <mnw21>
2006-07-18 10:15:55 +0000

    Less clutter produced by tests on stdout
    Former-commit-id: d94c0f367ef89397090df321f9d4d27c2a6d753c

--- intermine/integrate/test/src/org/intermine/dataconversion/DBReaderTestCase.java
+++ intermine/integrate/test/src/org/intermine/dataconversion/DBReaderTestCase.java
@@ -42,13 +42,13 @@ public class DBReaderTestCase extends TestCase {
         try {
             s.execute("DROP TABLE testread");
         } catch (SQLException e) {
-            e.printStackTrace(System.out);
+            //e.printStackTrace(System.out);
             c.rollback();
         }
         try {
             s.execute("DROP TABLE company_contractor");
         } catch (SQLException e) {
-            e.printStackTrace(System.out);
+            //e.printStackTrace(System.out);
             c.rollback();
         }
         s.execute("CREATE TABLE testread (value int, id text)");
