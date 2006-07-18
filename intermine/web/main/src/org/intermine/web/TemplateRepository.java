d7df918dd1027971eae2e8e6e1c3bcf31c186256
watkins <watkins>
2006-07-18 13:40:52 +0000

    Upgraded the version of Lucene. Fixes Ticket #849

--- intermine/web/main/src/org/intermine/web/TemplateRepository.java
+++ intermine/web/main/src/org/intermine/web/TemplateRepository.java
@@ -306,10 +306,11 @@ public class TemplateRepository
             TemplateQuery template = (TemplateQuery) iter.next();
             
             Document doc = new Document();
-            doc.add(Field.Text("name", template.getName()));
-            doc.add(Field.UnStored("content", template.getDescription() 
-                    + " " + template.getKeywords()));
-            doc.add(Field.UnIndexed("type", type));
+            doc.add(new Field("name", template.getName(),Field.Store.YES, Field.Index.TOKENIZED));
+            doc.add(new Field   ("content",template.getDescription()
+                              + " " + template.getKeywords(), Field.Store.NO, 
+                              Field.Index.TOKENIZED));
+            doc.add(new Field("type", type,Field.Store.YES, Field.Index.NO));
             
             try {
                 writer.addDocument(doc);
