8b99eb33184a6d7a26d7a3b0c8631083ff97e499
kmr <kmr>
2006-07-26 11:34:55 +0000

    Made a TiffinGFF3RecordHandler.
    Former-commit-id: 5f9f7516e634010e3902d919243d40b74e13cc9a

---
+++ bio/sources/tiffin/main/src/org/intermine/bio/dataconversion/TiffinGFF3RecordHandler.java
@@ -0,0 +1,38 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2005 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import org.intermine.bio.io.gff3.GFF3Record;
+import org.intermine.metadata.Model;
+
+/**
+ * A converter/retriever for Tiffin motifs.  See http://servlet.sanger.ac.uk/tiffin/
+ *
+ * @author Kim Rutherford
+ */
+
+public class TiffinGFF3RecordHandler extends GFF3RecordHandler
+{
+    /**
+     * Create a new TiffinGFF3RecordHandler for the given target model.
+     * @param tgtModel the model for which items will be created
+     */
+    public TiffinGFF3RecordHandler (Model tgtModel) {
+        super(tgtModel);
+    }
+
+    /**
+     * @see GFF3RecordHandler#process(GFF3Record)
+     */
+    public void process(GFF3Record record) {
+        // empty
+    }
+}
