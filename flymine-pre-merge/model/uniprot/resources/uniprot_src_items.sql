fca161dfba52f145e95c1d302cbb67ffac78f17f
kmr <kmr>
2006-08-01 10:59:47 +0000

    Removed the last of the old build system.
    Former-commit-id: c0b588ec8cf68536d8e5ba72ac5e663cd679e1c6

--- flymine-pre-merge/model/uniprot/resources/uniprot_src_items.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX reference_refid ON reference(refid)
-CREATE INDEX referencelist_refids ON referencelist(refids) WHERE name = 'organisms';
-CREATE INDEX item_classname ON item(classname);
