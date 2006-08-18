ebc1e9315632fc8a5c5b5a0f2279034a973dd7bf
pmclaren <pmclaren>
2006-08-18 09:43:30 +0000

    UniprotXmlFilter now makes a file listing all the Uniprot accessions that are labeled 'fragment'

--- bio/sources/uniprot/main/src/org/intermine/bio/dataconversion/UniprotFilterTask.java
+++ bio/sources/uniprot/main/src/org/intermine/bio/dataconversion/UniprotFilterTask.java
@@ -105,10 +105,18 @@ public class UniprotFilterTask extends Task
                     + "_filtered.xml";
                 File out = new File(tgtDir, outName);
                 BufferedWriter writer = new BufferedWriter(new FileWriter(out));
-                filter.filter(new BufferedReader(new FileReader(toRead)), writer);
+
+                String fragOutName = toRead.getName().substring(0, toRead.getName().indexOf('.'))
+                    + "_fragment.txt";
+                File fragOut = new File(tgtDir, fragOutName);
+                BufferedWriter fragWriter = new BufferedWriter(new FileWriter(fragOut));
+                filter.filter(new BufferedReader(new FileReader(toRead)), writer, fragWriter);
+
                 writer.flush();
                 writer.close();
 
+                fragWriter.flush();
+                fragWriter.close();
             }
         } catch (Exception e) {
             throw new BuildException (e);
