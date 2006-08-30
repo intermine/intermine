9ca24705cff16c9505eda905b10a6a43135dea61
kmr <kmr>
2006-08-30 17:09:15 +0000

    Added a flybase-proteinbindingsites source - unfinished.
    Former-commit-id: 4a49ff58f8e81b35e5823365861616623b6ec2b7

---
+++ bio/sources/flybase-proteinbindingsites/main/src/org/intermine/bio/dataconversion/FBProteinBindingSiteHandler.java
@@ -0,0 +1,84 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2005 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.intermine.dataloader.DirectDataLoader;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * A DefaultHandler for parsing protein binding site information from FlyBase.
+ *
+ * @author Kim Rutherford
+ */
+class FBProteinBindingSiteHandler extends DefaultHandler
+{
+    private List stack = new ArrayList();
+    private DirectDataLoader ddl;
+    private String geneName;
+    private StringBuffer charData = new StringBuffer(1000);
+
+    /**
+     * Create a new 
+     * @param ddl
+     */
+    public FBProteinBindingSiteHandler(DirectDataLoader ddl) {
+        this.ddl = ddl;
+    }
+
+    /**
+     * @see DefaultHandler#startElement(String, String, String, Attributes)
+     */
+    public void startElement(String uri, String localName, String qName, Attributes attrs) {
+        stack.add(0, new StackElement(qName, attrs));
+    }
+    
+    /**
+     * @see DefaultHandler#endElement(String, String, String)
+     */
+    public void endElement(String uri, String localName, String qName) {
+        stack.remove(0);
+        if (qName.equals("fbid") && stackGet(0).name.equals("ID") 
+            && stackGet(1).name.equals("GADR")) {
+            System.err.println("qName: " + charData.toString());
+            geneName = charData.toString();
+        }
+
+        charData.setLength(0);
+    }
+
+    public void characters (char ch[], int start, int length) {
+        charData.append(ch, start, length);
+    }
+    
+    private StackElement stackGet(int i) {
+        return (StackElement) stack.get(i);
+    }
+
+
+    
+    private class StackElement
+    {
+        final String name;
+        final Attributes attrs;
+
+        StackElement(String name, Attributes attrs) {
+            this.name = name;
+            this.attrs = attrs;
+        }
+
+    }
+}
