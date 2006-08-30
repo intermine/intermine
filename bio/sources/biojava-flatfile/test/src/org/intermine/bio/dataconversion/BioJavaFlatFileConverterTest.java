56ff67a877434fc15c452be7a7823418ea40cdac
kmr <kmr>
2006-08-30 16:57:23 +0000

    Added biojava-flatfile source - unfinished.
    Former-commit-id: e2854b5c0ff1168db0c7299be976f190bbca1be7

---
+++ bio/sources/biojava-flatfile/test/src/org/intermine/bio/dataconversion/BioJavaFlatFileConverterTest.java
@@ -0,0 +1,55 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2005 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.intermine.dataconversion.MockItemWriter;
+import org.intermine.metadata.Model;
+import org.intermine.objectstore.ObjectStore;
+import org.intermine.objectstore.ObjectStoreFactory;
+import org.intermine.xml.full.FullParser;
+import org.intermine.xml.full.ItemFactory;
+
+import java.io.StringReader;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.io.IOUtils;
+
+public class BioJavaFlatFileConverterTest extends TestCase {
+
+    private ObjectStore os;
+    private Model model;
+    private ItemFactory itemFactory;
+
+    public void setUp() throws Exception {
+        os = ObjectStoreFactory.getObjectStore("os.bio-test");
+        model = Model.getInstanceByName("genomic");
+        itemFactory = new ItemFactory(model);
+    }
+    
+    public void testProcess() throws Exception {
+        String input = 
+            IOUtils.toString(getClass().getClassLoader().getResourceAsStream("embl_test_1.embl"));
+        
+        MockItemWriter itemWriter = new MockItemWriter(new HashMap());
+        BioJavaFlatFileConverter converter = new BioJavaFlatFileConverter(itemWriter);
+        converter.process(new StringReader(input));
+        converter.close();
+        System.out.println(itemWriter.getItems());
+        Set expected = 
+            new HashSet(FullParser.parse(getClass().getClassLoader().getResourceAsStream("embl_test_1.xml")));
+        assertEquals(expected, itemWriter.getItems());
+    }
+}
