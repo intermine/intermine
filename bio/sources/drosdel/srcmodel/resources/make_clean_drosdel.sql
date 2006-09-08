494955714293a59ef48b48649c47498b4727c91f
kmr <kmr>
2006-09-08 11:58:06 +0000

    Changed the script that makes drosdel_clean to add an available column to the element table and confirmedByPCR/confirmedByGenetics to the deletion table.
    Former-commit-id: be1e810856dba983ba1793eac50a275de03fe51c

--- bio/sources/drosdel/srcmodel/resources/make_clean_drosdel.sql
+++ bio/sources/drosdel/srcmodel/resources/make_clean_drosdel.sql
@@ -1,5 +1,5 @@
 -- Copy the useful bits of the drosdel database to drosdel_clean.
--- Only two tables are copied drosdel_release4, which contains the element information and 
+-- Only two tables are copied: drosdel_release4, which contains the element information and 
 -- drosdel_del_release4, which contains information about the deletions
 
 USE drosdel_clean;
@@ -16,6 +16,7 @@ CREATE TABLE element (
     orientation INTEGER,
     type VARCHAR(20),
     subType VARCHAR(20),
+    available BOOLEAN,
     PRIMARY KEY (elementid)
 );
 
@@ -28,21 +29,28 @@ CREATE TABLE deletion (
     start INTEGER,
     end INTEGER,
     available BOOLEAN,
+    confirmedByPCR BOOLEAN,
+    confirmedByGenetics BOOLEAN,
     europeanDrosophilaNumber VARCHAR(20)
 );
 
 USE drosdel;
 
-INSERT INTO drosdel_clean.element (name, chromosomeName, start, end, orientation, type, subType)
+INSERT INTO drosdel_clean.element (name, chromosomeName, start, end, orientation, type, subType,
+       available)
 SELECT name, chr,
        (CASE WHEN orient = 'forward' THEN start ELSE stop END),
        (CASE WHEN orient = 'forward' THEN stop ELSE start END),
-       (CASE WHEN orient = 'forward' THEN 1 ELSE -1 END), type, sub_type
+       (CASE WHEN orient = 'forward' THEN 1 ELSE -1 END), type, sub_type,
+       (CASE WHEN available = 'y' THEN true ELSE false END)
        FROM drosdel_release4 where start is not null and end is not null;
 
 INSERT INTO drosdel_clean.deletion (element1_id, element2_id, chromosomeName, start, end, available,
-                                    europeanDrosophilaNumber)
+                                    confirmedByPCR, confirmedByGenetics, europeanDrosophilaNumber)
 SELECT (SELECT elementid from drosdel_clean.element where name = name2),
        (SELECT elementid from drosdel_clean.element where name = name1),
-       chr, loc2, loc1, (CASE WHEN available = 'y' THEN true ELSE false END), ED_no
+       chr, loc2, loc1, (CASE WHEN available = 'y' THEN true ELSE false END), 
+       (CASE WHEN confirm = 'y' THEN true ELSE false END),
+       (CASE WHEN genetic = 1 THEN true ELSE false END),
+       ED_no
 FROM drosdel_del_release4 where loc1 is not null and loc2 is not null;
