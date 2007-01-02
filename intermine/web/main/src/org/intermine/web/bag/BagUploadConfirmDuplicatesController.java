0a3689ffef844639517421294b7eb1feed0754f0
kmr <kmr>
2007-01-02 18:10:17 +0000

    Added checkboxes beside the duplicated objects in the bagUploadConfirm page.

--- intermine/web/main/src/org/intermine/web/bag/BagUploadConfirmDuplicatesController.java
+++ intermine/web/main/src/org/intermine/web/bag/BagUploadConfirmDuplicatesController.java
@@ -10,20 +10,22 @@ package org.intermine.web.bag;
  *
  */
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
 
 import org.intermine.model.InterMineObject;
-import org.intermine.util.DynamicUtil;
-import org.intermine.util.TypeUtil;
+import org.intermine.objectstore.ObjectStore;
+import org.intermine.web.Constants;
+import org.intermine.web.results.ResultElement;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
@@ -47,54 +49,33 @@ public class BagUploadConfirmDuplicatesController extends TilesAction
                                  HttpServletRequest request,
                                  HttpServletResponse response)
         throws Exception {
-
         Map duplicates = (Map) context.getAttribute("duplicates");
-        Map classDuplicates = makeClassDuplicatesMap(duplicates);
-        context.putAttribute("classDuplicates", classDuplicates);
+        Map resultElementMap = makeResultElementMap(request, duplicates);
+        context.putAttribute("resultElementMap", resultElementMap);
         return null;
     }
 
-
-    /**
-     * Given a Map of identifier to List of objects, create a Map from unqualified class name to
-     * Map of identifiers to List of objects 
-     * @param duplicates A Map from identifier to List of objects
-     * @return a Map from unqualified class name to Map of identifiers to objects
-     */
-    protected static Map makeClassDuplicatesMap(Map duplicates) {
-        Map classDuplicates = new HashMap();
+    private static Map makeResultElementMap(HttpServletRequest request, Map duplicates) {
+        HttpSession session = request.getSession();
+        ServletContext servletContext = session.getServletContext();
+        ObjectStore os = (ObjectStore) servletContext.getAttribute(Constants.OBJECTSTORE);
+        Map resultElementMap = new HashMap();
         Iterator duplicatesIter = duplicates.entrySet().iterator();
         while (duplicatesIter.hasNext()) {
             Map.Entry entry = (Entry) duplicatesIter.next();
             String identifier = (String) entry.getKey();
             Set objects = (Set) entry.getValue();
+            Set resultsElementList = new HashSet();
+            resultElementMap.put(identifier, resultsElementList);
             Iterator objectsIter = objects.iterator();
             while (objectsIter.hasNext()) {
                 InterMineObject imo = (InterMineObject) objectsIter.next();
-                addToMap(classDuplicates, identifier, imo);
+                String bagType = (String) request.getAttribute("bagType");
+                ResultElement resultElement =
+                    new ResultElement(os, identifier, imo.getId(), bagType, true);
+                resultsElementList.add(resultElement);
             }
         }
-        return classDuplicates;
-    }
-
-
-    private static void addToMap(Map classDuplicates, String identifier, InterMineObject imo) {
-        List classes = new ArrayList(DynamicUtil.decomposeClass(imo.getClass()));
-        String className = TypeUtil.unqualifiedName(((Class) classes.iterator().next()).getName());
-        Map objectMap;
-        if (classDuplicates.containsKey(className)) {
-            objectMap = (Map) classDuplicates.get(className);
-        } else {
-            objectMap = new HashMap();
-            classDuplicates.put(className, objectMap);
-        }
-        List objectList;
-        if (objectMap.containsKey(identifier)) {
-            objectList = (List) objectMap.get(identifier);
-        } else {
-            objectList = new ArrayList();
-            objectMap.put(identifier, objectList);
-        }
-        objectList.add(imo);
+        return resultElementMap;
     }
 }
