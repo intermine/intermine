8cb18df4657035e6f4a5eaa1dae45a4077725e71
kmr <kmr>
2007-01-11 14:11:37 +0000

    Fixed application.xml so that it includes the class_keys.properties in the WAR file. Added ReadResourceTask and read-resource task to imbuild.

--- imbuild/im-ant-tasks/src/org/intermine/task/ReadResourceTask.java
+++ imbuild/im-ant-tasks/src/org/intermine/task/ReadResourceTask.java
@@ -19,6 +19,8 @@ import java.io.StringWriter;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.Path;
+import org.apache.tools.ant.types.Reference;
 
 /**
  * A Task that reads a resource (from the class path) and sets a property with its contents.
@@ -29,6 +31,7 @@ public class ReadResourceTask extends Task
 {
     private String resource;
     private String property;
+    private Path classpath;
 
     /**
      * Set the resource to search for.
@@ -46,12 +49,66 @@ public class ReadResourceTask extends Task
         this.property = property;
     }
 
+    // copied from Ant's Property Task.
+    /**
+     * The classpath to use when looking up a resource.
+     * @param classpath to add to any existing classpath
+     */
+    public void setClasspath(Path classpath) {
+        if (this.classpath == null) {
+            this.classpath = classpath;
+        } else {
+            this.classpath.append(classpath);
+        }
+    }
+
+    /**
+     * Create an empty Path.
+     * @return the new Path
+     */
+    public Path createClasspath() {
+        if (this.classpath == null) {
+            this.classpath = new Path(getProject());
+        }
+        return this.classpath.createPath();
+    }
+
+    /**
+     * Set the classpath to use when looking up a resourca,
+     * given as reference to a Path defined elsewhere
+     * @param reference the Reference
+     */
+    public void setClasspathRef(Reference reference) {
+        createClasspath().setRefid(reference);
+    }
+
     /**
      * Execute the task to set a property from a resource.
      * @throws BuildException if something goes wrong
      */
     public void execute() throws BuildException {
-        InputStream is = getProject().getClass().getResourceAsStream(resource);
+        if (resource == null) {
+            throw new BuildException("no resource given");
+        }
+        if (property == null) {
+            throw new BuildException("no property given");
+        }
+
+        ClassLoader classLoader = null;
+
+        if (classpath != null) {
+            classLoader = getProject().createClassLoader(classpath);
+        } else {
+            classLoader = this.getClass().getClassLoader();
+        }
+
+        InputStream is;
+        if (classLoader == null) {
+            is = ClassLoader.getSystemResourceAsStream(resource);
+        } else {
+            is = classLoader.getResourceAsStream(resource);
+        }
+        
         if (is == null) {
             throw new BuildException("cannot find resource \"" + resource + "\" in the classpath");
         }
