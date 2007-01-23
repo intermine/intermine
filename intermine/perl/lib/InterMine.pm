a2d76d6c7b67669caf9ce66275a7025a27321087
kmr <kmr>
2007-01-23 09:47:59 +0000

    Fixes for Perl mapping - now reads the class_keys.properties to setup the right primary keys for Rose::DB and added a list of PostgreSQL keywords (not use everywhere yet).
    Former-commit-id: 360b0d7f06ab4bd14e5d6720535d05db3b9a3afd

--- intermine/perl/lib/InterMine.pm
+++ intermine/perl/lib/InterMine.pm
@@ -71,10 +71,13 @@ under the same terms as Perl itself.
 =cut
 
 use InterMine::Model;
+use Config::Properties;
 
 our $model_file;
 our $properties_file;
 our $db_prefix;
+our $class_keys;
+local %InterMine::class_keys_map = ();
 
 our $model;
 
@@ -86,6 +89,37 @@ sub get_model {
   } else {
     $model = new InterMine::Model(file => $model_file);
   }
+}
+
+sub _get_class_keys
+{
+  my $class_name = shift;
+
+  die "class_keys not set\n" unless defined $class_keys;
+
+  if (scalar(keys %InterMine::class_keys_map) == 0) {
+    open PROPS, '<', $class_keys
+      or die "unable to open class keys file: $InterMine::class_keys";
+
+    my $properties = new Config::Properties();
+    $properties->load(*PROPS);
+
+    for my $class_name ($properties->propertyNames()) {
+      $InterMine::class_keys_map{$class_name} =
+        [split /[,\s]+/, $properties->getProperty($class_name)];
+    }
+  }
+
+  my @class_keys = ();
+  if (exists $InterMine::class_keys_map{$class_name}) {
+    @class_keys = @{$InterMine::class_keys_map{$class_name}};
+  }
+  my $class_desc = get_model()->get_classdescriptor_by_name($class_name);
+  for my $extendee_class_desc ($class_desc->extends_class_descriptors()) {
+    push @class_keys, _get_class_keys($extendee_class_desc->unqualified_name());
+  }
+
+  return @class_keys;
 }
 
 sub import {
@@ -110,7 +144,7 @@ sub import {
     for my $field ($cd->fields()) {
 
       if ($field->field_type() eq 'attribute') {
-        push @columns, $field->field_name(), {type => $field->attribute_type()}
+        push @columns, _pg_identifier_from_intermine($field->field_name()), {type => $field->attribute_type()}
       } else {
         my $referenced_type_name = $field->referenced_type_name();
         $referenced_type_name =~ s/.*\.(.*)/InterMine::$1/;
@@ -161,6 +195,8 @@ sub import {
     $setup_args{columns} = \@columns;
     $setup_args{foreign_keys} = \@foreign_keys;
     $setup_args{relationships} = \@relationships;
+    my @unique_keys = _get_class_keys($class);
+    $setup_args{unique_keys} = [@unique_keys];
 
     my $lc_class = lc $class;
 
@@ -184,19 +220,67 @@ EOF
   }
 }
 
-my %postgres_keywords = (
-                         translation => 1
-);
 
-sub _table_for_class
+
+my %postgres_keywords;
+
+my @keyword_list =
+  qw(abs absolute action add admin after aggregate alias all allocate alter
+     analyse analyze and any are array as asc asensitive assertion asymmetric
+     at atomic authorization avg before begin between bigint binary bit
+     bit_length blob boolean both breadth by call called cardinality cascade
+     cascaded case cast catalog ceil ceiling char character character_length
+     char_length check class clob close coalesce collate collation collect
+     column commit completion condition connect connection constraint
+     constraints constructor continue convert corr corresponding count
+     covar_pop covar_samp create cross cube cume_dist current current_date
+     current_default_tran current_path current_role current_time
+     current_timestamp current_transform_gr current_user cursor cycle data
+     database date day deallocate dec decimal declare default deferrable
+     deferred delete dense_rank depth deref desc describe descriptor destroy
+     destructor deterministic diagnostics dictionary disconnect distinct do
+     domain double drop dynamic each element else end end-exec equals escape
+     every except exception exec execute exists exp external extract false
+     fetch filter first float floor for foreign found free freeze from full
+     function fusion general get global go goto grant group grouping having
+     hold host hour identity ignore ilike immediate in indicator initialize
+     initially inner inout input insensitive insert int integer intersect
+     intersection interval into is isnull isolation iterate join key language
+     large last lateral leading left less level like limit ln local localtime
+     localtimestamp locator lower map match max member merge method min minute
+     mod modifies modify module month multiset names national natural nchar
+     nclob new next no none normalize not notnull null nullif numeric object
+     objectclass octet_length of off offset old on only open operation option
+     or order ordinality out outer output over overlaps overlay pad parameter
+     parameters partial partition path percentile_cont percentile_disc
+     percent_rank placing position postfix power precision prefix preorder
+     prepare preserve primary prior privileges procedure public range read
+     reads real recursive ref references referencing regr_avgx regr_avgy
+     regr_count regr_intercept regr_r2 regr_slope regr_sxx regr_sxy regr_syy
+     relative release restrict result return returns revoke right role
+     rollback rollup routine row rows row_number savepoint schema scope scroll
+     search second section select sensitive sequence session session_user set
+     setof sets similar size smallint some space specific specifictype sql
+     sqlcode sqlerror sqlexception sqlstate sqlwarning sqrt start state
+     statement static stddev_pop stddev_samp structure submultiset substring
+     sum symmetric system system_user table tablesample temporary terminate
+     than then time timestamp timezone_hour timezone_minute to trailing
+     transaction translate translation treat trigger trim true uescape under
+     union unique unknown unnest update upper usage user using value values
+     varchar variable varying var_pop var_samp verbose view when whenever
+     where width_bucket window with within without work write year zone);
+
+
+@postgres_keywords{@keyword_list} = ();
+
+sub _pg_identifier_from_intermine
 {
-  my $class_name = lc shift;
-  warn "class_name: $class_name\n";
+  my $name = shift;
 
-  if (exists $postgres_keywords{$class_name}) {
-    return "intermine_$class_name";
+  if (exists $postgres_keywords{lc $name}) {
+    return "intermine_$name";
   } else {
-    return $class_name;
+    return $name;
   }
 }
 
@@ -237,13 +321,13 @@ __PACKAGE__->meta->setup
   ${lc_field_name}_key =>
   {
    class       => 'InterMine::$reverse_field_class_name',
-   key_columns => { ${lc_field_name} => 'id' },
+   key_columns => { $lc_field_name => 'id' },
   },
 
   ${lc_reverse_field_name}_key =>
   {
    class       => 'InterMine::$field_class_name',
-   key_columns => { ${lc_reverse_field_name} => 'id' },
+   key_columns => { $lc_reverse_field_name => 'id' },
   },
  ],
 );
