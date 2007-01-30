5e23fcb049709001ddacade36de8870e3341976a
kmr <kmr>
2007-01-30 16:17:07 +0000

    Reformatting.
    Former-commit-id: 12805667276793a811c17a12abfde726661921dc

--- intermine/webapp/main/resources/webapp/js/baguploadconfirm.js
+++ intermine/webapp/main/resources/webapp/js/baguploadconfirm.js
@@ -6,11 +6,11 @@ function addId2Bag(id,row,parentId){
     document.getElementById('rem_'+id).className = "fakelink";
     var elements = document.getElementsByTagName('td');
     for (var i = 0; i < elements.length; i++) {
-		var idtd = elements.item(i).id;
-		if(idtd == 'row_' + row) {
-			tdColorArray['row_'+row] = elements.item(i).style.backgroundColor;
-		    elements.item(i).style.backgroundColor = "#CCCCCC";
-		}
+        var idtd = elements.item(i).id;
+        if(idtd == 'row_' + row) {
+            tdColorArray['row_'+row] = elements.item(i).style.backgroundColor;
+            elements.item(i).style.backgroundColor = "#CCCCCC";
+        }
     }
     var bagList = document.getElementById('matchIDs').value;
     if(bagList.indexOf(id) == -1){
@@ -20,15 +20,15 @@ function addId2Bag(id,row,parentId){
         document.getElementById('matchIDs').value = bagList + id;
     }
     document.getElementById('matchCount').innerHTML++;
-	var idArray = duplicateArray[parentId];
-	if(idArray == null) {
-		duplicateArray[parentId] = new Array(id);
+    var idArray = duplicateArray[parentId];
+    if(idArray == null) {
+        duplicateArray[parentId] = new Array(id);
         document.getElementById('duplicateCount').innerHTML--;
         document.getElementById("td_"+parentId).style.backgroundColor = '#CCCCCC';
-	} else {
-		idArray[idArray.length] = id;
-		duplicateArray[parentId] = idArray;
-	}
+    } else {
+        idArray[idArray.length] = id;
+        duplicateArray[parentId] = idArray;
+    }
 }
 
 function removeIdFromBag(id,row, parentId){
@@ -36,10 +36,10 @@ function removeIdFromBag(id,row, parentId){
     document.getElementById('add_'+id).className = "fakelink";
     var elements = document.getElementsByTagName('td');
     for (var i = 0; i < elements.length; i++) {
-		var idtd = elements.item(i).id;
-		if(idtd == 'row_' + row) {
-		    elements.item(i).style.backgroundColor = tdColorArray['row_'+row];
-		}
+        var idtd = elements.item(i).id;
+        if(idtd == 'row_' + row) {
+            elements.item(i).style.backgroundColor = tdColorArray['row_'+row];
+        }
     }
     var bagList = document.getElementById('matchIDs').value;
     if(bagList.indexOf(id) != -1){
@@ -49,17 +49,17 @@ function removeIdFromBag(id,row, parentId){
     var bagList = document.getElementById('matchIDs').value;
     document.getElementById('matchCount').innerHTML--;
     var idArray = duplicateArray[parentId];
-	if(idArray.length == 1){
+    if(idArray.length == 1){
         document.getElementById('duplicateCount').innerHTML++;
         document.getElementById("td_"+parentId).style.backgroundColor = '#FFFFFF';
-		duplicateArray[parentId] = null;
-	} else {
-		var idArrayCopy = new Array();
-		for (var i=0;i<idArray.length;i++){
-			if(id!=idArray[i]){
-				idArrayCopy[idArrayCopy.length]=idArray[i];
-			}
-		}
-		duplicateArray[parentId] = idArrayCopy;
-	}
+        duplicateArray[parentId] = null;
+    } else {
+        var idArrayCopy = new Array();
+        for (var i=0;i<idArray.length;i++){
+            if(id!=idArray[i]){
+                idArrayCopy[idArrayCopy.length]=idArray[i];
+            }
+        }
+        duplicateArray[parentId] = idArrayCopy;
+    }
 }
