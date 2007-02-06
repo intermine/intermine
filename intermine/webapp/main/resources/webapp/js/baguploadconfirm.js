99cf751eac3f1de0485a9420a5c3c1ad15eed20b
watkins <watkins>
2007-02-06 14:54:12 +0000

    Added description for converted type section and fixed js bug

--- intermine/webapp/main/resources/webapp/js/baguploadconfirm.js
+++ intermine/webapp/main/resources/webapp/js/baguploadconfirm.js
@@ -2,64 +2,68 @@ var duplicateArray = new Array();
 var tdColorArray = new Array();
 
 function addId2Bag(id,row,parentId,issueType){
-    document.getElementById('add_'+issueType+'_'+id).className = '';
-    document.getElementById('rem_'+issueType+'_'+id).className = "fakelink";
-    var elements = document.getElementsByTagName('td');
-    for (var i = 0; i < elements.length; i++) {
-        var idtd = elements.item(i).id;
-        if(idtd == 'row_'+issueType+'_' + row) {
-            tdColorArray['row_'+issueType+'_'+row] = elements.item(i).style.backgroundColor;
-            elements.item(i).style.backgroundColor = "#CCCCCC";
+    if(document.getElementById('add_'+issueType+'_'+id).className=='fakelink'){
+        document.getElementById('add_'+issueType+'_'+id).className = '';
+        document.getElementById('rem_'+issueType+'_'+id).className = "fakelink";
+        var elements = document.getElementsByTagName('td');
+        for (var i = 0; i < elements.length; i++) {
+            var idtd = elements.item(i).id;
+            if(idtd == 'row_'+issueType+'_' + row) {
+                tdColorArray['row_'+issueType+'_'+row] = elements.item(i).style.backgroundColor;
+                elements.item(i).style.backgroundColor = "#CCCCCC";
+            }
         }
-    }
-    var bagList = document.getElementById('matchIDs').value;
-    if(bagList.indexOf(id) == -1){
-        if (bagList.length != 0 ) {
-            bagList += " ";
+        var bagList = document.getElementById('matchIDs').value;
+        if(bagList.indexOf(id) == -1){
+            if (bagList.length != 0 ) {
+                bagList += " ";
+            }
+            document.getElementById('matchIDs').value = bagList + id;
         }
-        document.getElementById('matchIDs').value = bagList + id;
-    }
-    document.getElementById('matchCount').innerHTML++;
-    var idArray = duplicateArray[parentId];
-    if(idArray == null) {
-        duplicateArray[parentId] = new Array(id);
-        document.getElementById(issueType+'Count').innerHTML--;
-        document.getElementById('td_'+issueType+'_'+parentId).style.backgroundColor = '#CCCCCC';
-    } else {
-        idArray[idArray.length] = id;
-        duplicateArray[parentId] = idArray;
+        document.getElementById('matchCount').innerHTML++;
+        var idArray = duplicateArray[parentId];
+        if(idArray == null) {
+            duplicateArray[parentId] = new Array(id);
+            document.getElementById(issueType+'Count').innerHTML--;
+            document.getElementById('td_'+issueType+'_'+parentId).style.backgroundColor = '#CCCCCC';
+        } else {
+            idArray[idArray.length] = id;
+            duplicateArray[parentId] = idArray;
+        }
     }
 }
 
 function removeIdFromBag(id,row, parentId, issueType){
-    document.getElementById('rem_'+issueType+'_'+id).className = '';
-    document.getElementById('add_'+issueType+'_'+id).className = "fakelink";
-    var elements = document.getElementsByTagName('td');
-    for (var i = 0; i < elements.length; i++) {
-        var idtd = elements.item(i).id;
-        if(idtd == 'row_'+issueType+'_' + row) {
-            elements.item(i).style.backgroundColor = tdColorArray['row_'+issueType+'_'+row];
-        }
-    }
-    var bagList = document.getElementById('matchIDs').value;
-    if(bagList.indexOf(id) != -1){
-        bagList = bagList.split(id).join('').split('  ').join(' ');
-        document.getElementById('matchIDs').value = bagList.replace(/^s+/, '').replace(/s+$/, '');//Trim
-    }
-    var bagList = document.getElementById('matchIDs').value;
-    document.getElementById('matchCount').innerHTML--;
-    var idArray = duplicateArray[parentId];
-    if(idArray.length == 1){
-        document.getElementById(issueType+'Count').innerHTML++;
-        document.getElementById('td_'+issueType+'_'+parentId).style.backgroundColor = '#FFFFFF';
-        duplicateArray[parentId] = null;
-    } else {
-        var idArrayCopy = new Array();
-        for (var i=0;i<idArray.length;i++){
-            if(id!=idArray[i]){
-                idArrayCopy[idArrayCopy.length]=idArray[i];
+    if(document.getElementById('rem_'+issueType+'_'+id).className=='fakelink'){
+        document.getElementById('rem_'+issueType+'_'+id).className = '';
+        document.getElementById('add_'+issueType+'_'+id).className = "fakelink";
+        var elements = document.getElementsByTagName('td');
+        for (var i = 0; i < elements.length; i++) {
+            var idtd = elements.item(i).id;
+            if(idtd == 'row_'+issueType+'_' + row) {
+                elements.item(i).style.backgroundColor = tdColorArray['row_'+issueType+'_'+row];
             }
         }
-        duplicateArray[parentId] = idArrayCopy;
+        var bagList = document.getElementById('matchIDs').value;
+        if(bagList.indexOf(id) != -1){
+            bagList = bagList.split(id).join('').split('  ').join(' ');
+            document.getElementById('matchIDs').value = bagList.replace(/^s+/, '').replace(/s+$/, '');//Trim
+        }
+        var bagList = document.getElementById('matchIDs').value;
+        document.getElementById('matchCount').innerHTML--;
+        var idArray = duplicateArray[parentId];
+        if(idArray.length == 1){
+            document.getElementById(issueType+'Count').innerHTML++;
+            document.getElementById('td_'+issueType+'_'+parentId).style.backgroundColor = '#FFFFFF';
+            duplicateArray[parentId] = null;
+        } else {
+            var idArrayCopy = new Array();
+            for (var i=0;i<idArray.length;i++){
+                if(id!=idArray[i]){
+                    idArrayCopy[idArrayCopy.length]=idArray[i];
+                }
+            }
+            duplicateArray[parentId] = idArrayCopy;
+        }
     }
 }
