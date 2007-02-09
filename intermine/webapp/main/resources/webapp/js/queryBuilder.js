5aec43fa61f13166c81ead1fefaafab363c90ecb
kmr <kmr>
2007-02-09 11:40:10 +0000

    Removed tabs.

--- intermine/webapp/main/resources/webapp/js/queryBuilder.js
+++ intermine/webapp/main/resources/webapp/js/queryBuilder.js
@@ -1,43 +1,43 @@
-      // ***********************************************************
-      // * Disables form fields based on user toggle
-      // ***********************************************************
-      
-   function swapInputs(open) {
-   
-   		// different constraints available to the user
-		var constraints = new Array("attribute","subclass","loopQuery","bag","empty"); 
-		// field names, different fields will be visible for different constraints
-		const maxVariableCount = 7; // there are seven attribute variables
-		
-		for (var i = 0; i < constraints.length; i++) {   
-		
-			// if this constraint exists, it may not  		
-			if(document.getElementById(constraints[i] + 'Submit')) {
-			
-				// enable if this is what the user just selected
-				if (constraints[i] == open) {
-					document.getElementById(constraints[i] + 'Submit').disabled = false;
-    		  		document.getElementById(constraints[i] + 'Toggle').src = 'images/disclosed.gif';
-  		
-    		  		// loop through other fields that may exist
-					for (var j = 0; j < maxVariableCount; j++) {      
-    		  			if(document.getElementById(constraints[i] + j)) {
-    		  				document.getElementById(constraints[i] + j).disabled = false;
-    		  			}    		  			
-    		  		}
+// ***********************************************************
+// * Disables form fields based on user toggle
+// ***********************************************************
 
-    		  	// disable everything else	
-				} else {
-		      		document.getElementById(constraints[i] + 'Submit').disabled = true;
-    		  		document.getElementById(constraints[i] + 'Toggle').src = 'images/undisclosed.gif';    		  		
-    		  		
-					// loop through other fields that may exist
-					for (var k = 0; k < maxVariableCount; k++) {      		  		
-    		  			if(document.getElementById(constraints[i] + k)) {
-    		  				document.getElementById(constraints[i] + k).disabled = true;
-    		  			}    		  			
-    		  		}   		  		
-				}
-			}
-		}        
-	}
+function swapInputs(open) {
+
+    // different constraints available to the user
+    var constraints = new Array("attribute","subclass","loopQuery","bag","empty");
+    // field names, different fields will be visible for different constraints
+    const maxVariableCount = 7; // there are seven attribute variables
+
+    for (var i = 0; i < constraints.length; i++) {
+
+        // if this constraint exists, it may not
+        if(document.getElementById(constraints[i] + 'Submit')) {
+
+            // enable if this is what the user just selected
+            if (constraints[i] == open) {
+                document.getElementById(constraints[i] + 'Submit').disabled = false;
+                document.getElementById(constraints[i] + 'Toggle').src = 'images/disclosed.gif';
+
+                // loop through other fields that may exist
+                for (var j = 0; j < maxVariableCount; j++) {
+                    if(document.getElementById(constraints[i] + j)) {
+                        document.getElementById(constraints[i] + j).disabled = false;
+                    }
+                }
+
+                // disable everything else
+            } else {
+                document.getElementById(constraints[i] + 'Submit').disabled = true;
+                document.getElementById(constraints[i] + 'Toggle').src = 'images/undisclosed.gif';
+
+                // loop through other fields that may exist
+                for (var k = 0; k < maxVariableCount; k++) {
+                    if(document.getElementById(constraints[i] + k)) {
+                        document.getElementById(constraints[i] + k).disabled = true;
+                    }
+                }
+            }
+        }
+    }
+}
