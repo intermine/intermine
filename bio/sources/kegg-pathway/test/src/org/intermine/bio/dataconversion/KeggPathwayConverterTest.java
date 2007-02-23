ea2a78da4bd29c4a91184793da3096779611a068
watkins <watkins>
2007-02-23 18:40:02 +0000

    Former-commit-id: 931483176d00524a07761cd131a9dc92d62a3027

---
+++ bio/sources/kegg-pathway/test/src/org/intermine/bio/dataconversion/KeggPathwayConverterTest.java
@@ -0,0 +1,30 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2005 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import junit.framework.TestCase;
+
+public class KeggPathwayConverterTest extends TestCase
+{
+    private String ENDL = System.getProperty("line.separator");
+
+    public KeggPathwayConverterTest(String arg) {
+        super(arg);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testProcess() throws Exception {
+        //TODO write a test case
+    }
+}
