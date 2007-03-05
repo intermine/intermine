962b63dcbb6e271adee6d73f8425e5b62b4ba354
watkins <watkins>
2007-03-05 12:19:44 +0000

    trim strings before renaming

--- intermine/webapp/main/resources/webapp/js/imdwr.js
+++ intermine/webapp/main/resources/webapp/js/imdwr.js
@@ -31,7 +31,7 @@ function renameElement(name, type, index){
 	document.getElementById('form_'+name).style.display="none";
 	document.getElementById('name_'+name).innerHTML="<i>saving...</i>";
 	document.getElementById('name_'+name).style.display="block";
-	AjaxServices.rename(name,type, document.getElementById('newName_'+name).value, function(str){
+	AjaxServices.rename(name,type, (document.getElementById('newName_'+name).value).replace(/^\s*|\s*$/g,""), function(str){
 		document.getElementById('name_'+name).innerHTML=str;
 		document.getElementById('selected_' + type + '_' + index).value=str;
 	});
