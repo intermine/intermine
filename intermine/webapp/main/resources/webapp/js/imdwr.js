2ea223a5ddf38b392dd5f09af54fe4ddfda274eb
kmr <kmr>
2007-04-30 13:31:51 +0000

    Removed tabs.

--- intermine/webapp/main/resources/webapp/js/imdwr.js
+++ intermine/webapp/main/resources/webapp/js/imdwr.js
@@ -1,17 +1,17 @@
 function setFavouriteTemplate(templateName, image){
-	AjaxServices.setFavouriteTemplate(templateName);
-	image.src='images/star_active.gif';
-	image.onclick='';
-	image.style.cursor='';
-	image.title='This template is a favourite';
+    AjaxServices.setFavouriteTemplate(templateName);
+    image.src='images/star_active.gif';
+    image.onclick='';
+    image.style.cursor='';
+    image.title='This template is a favourite';
 }
 
 function precomputeTemplate(templateName){
-	document.getElementById('precompute_'+templateName).innerHTML="Precomputing..";
-	AjaxServices.preCompute(templateName,function(str) {
-		document.getElementById('precompute_'+templateName).style.color="#777";
-		document.getElementById('precompute_'+templateName).innerHTML="Precomputed";
-	 });
+    document.getElementById('precompute_'+templateName).innerHTML="Precomputing..";
+    AjaxServices.preCompute(templateName,function(str) {
+        document.getElementById('precompute_'+templateName).style.color="#777";
+        document.getElementById('precompute_'+templateName).innerHTML="Precomputed";
+     });
 }
 
 function summariseTemplate(templateName){
@@ -28,13 +28,13 @@ function editName(name){
 }
 
 function renameElement(name, type, index){
-	document.getElementById('form_'+name).style.display="none";
-	document.getElementById('name_'+name).innerHTML="<i>saving...</i>";
-	document.getElementById('name_'+name).style.display="block";
-	AjaxServices.rename(name,type, (document.getElementById('newName_'+name).value).replace(/^\s*|\s*$/g,""), function(str){
-		document.getElementById('name_'+name).innerHTML=str;
-		document.getElementById('selected_' + type + '_' + index).value=str;
-	});
+    document.getElementById('form_'+name).style.display="none";
+    document.getElementById('name_'+name).innerHTML="<i>saving...</i>";
+    document.getElementById('name_'+name).style.display="block";
+    AjaxServices.rename(name,type, (document.getElementById('newName_'+name).value).replace(/^\s*|\s*$/g,""), function(str){
+        document.getElementById('name_'+name).innerHTML=str;
+        document.getElementById('selected_' + type + '_' + index).value=str;
+    });
 }
 
 function changeViewPathDescription(pathName){
@@ -57,48 +57,48 @@ function changeViewPathDescription(pathName){
 
 function saveBagDescription(bagName){
     var textarea = document.getElementById('textarea').value;
-	textarea = textarea.replace(/[\n\r]+/g, "\n\r<br/>");
-	document.getElementById('bagDescriptionDiv').innerHTML = '<i>Saving...</i>';
-	AjaxServices.saveBagDescription(bagName,textarea, function(str){
-		document.getElementById('bagDescriptionDiv').innerHTML = str;
-	});
-	swapDivs('bagDescriptionTextarea','bagDescriptionDiv');
+    textarea = textarea.replace(/[\n\r]+/g, "\n\r<br/>");
+    document.getElementById('bagDescriptionDiv').innerHTML = '<i>Saving...</i>';
+    AjaxServices.saveBagDescription(bagName,textarea, function(str){
+        document.getElementById('bagDescriptionDiv').innerHTML = str;
+    });
+    swapDivs('bagDescriptionTextarea','bagDescriptionDiv');
 }
 
 function swapDivs(div1,div2){
-	document.getElementById(div1).style.display = 'none';
-	document.getElementById(div2).style.display = 'block';
+    document.getElementById(div1).style.display = 'none';
+    document.getElementById(div2).style.display = 'block';
 }
 
 
 function roundToSignificantDecimal(n) {
-	var log10n = Math.log(Math.abs(n)) / Math.log(10);
-	if(log10n<1 && n!=0) {
-		var rf = 100*Math.pow(10,-Math.round(log10n));
-		return rf;
-	} else return 100;
+    var log10n = Math.log(Math.abs(n)) / Math.log(10);
+    if(log10n<1 && n!=0) {
+        var rf = 100*Math.pow(10,-Math.round(log10n));
+        return rf;
+    } else return 100;
 }
 
 function getColumnSummary(columnName) {
-	DWRUtil.removeAllRows('summary_table');
+    DWRUtil.removeAllRows('summary_table');
     document.getElementById('summary_loaded').style.display = "none";
     document.getElementById('summary_loading').style.display = "block";
     Effect.Appear('summary', { duration: 0.30 });
-	AjaxServices.getColumnSummary(columnName, function(str){
-	    var rows = str;
-	    var cellFuncs = new Array();
-	    for (var i=0;i<rows[0].length;i++){
-	      cellFuncs[i] = eval('function(data) {var cell = data['+i+']; if (! isNaN(cell)) {var rf = roundToSignificantDecimal(cell+0);return Math.round(cell*rf)/rf;} else return cell; }');
-	    }
-	    if(cellFuncs.length == 2){
-	      document.getElementById('summary_head').innerHTML = '<tr><th>Value</th><th>Count</th></tr>';
-	    } else if (cellFuncs.length == 4){
-	      document.getElementById('summary_head').innerHTML = '<tr><th>Min</th><th>Max</th><th>Average</th><th>Deviation</th></tr>';
-	    }
-		DWRUtil.addRows("summary_table", rows, cellFuncs);
-		document.getElementById('summary_loading').style.display = "none";
-		document.getElementById('summary_loaded').style.display = "block";
-	});
+    AjaxServices.getColumnSummary(columnName, function(str){
+        var rows = str;
+        var cellFuncs = new Array();
+        for (var i=0;i<rows[0].length;i++){
+            cellFuncs[i] = eval('function(data) {var cell = data['+i+']; if (! isNaN(cell)) {var rf = roundToSignificantDecimal(cell+0);return Math.round(cell*rf)/rf;} else return cell; }');
+        }
+        if(cellFuncs.length == 2){
+            document.getElementById('summary_head').innerHTML = '<tr><th>Value</th><th>Count</th></tr>';
+        } else if (cellFuncs.length == 4){
+            document.getElementById('summary_head').innerHTML = '<tr><th>Min</th><th>Max</th><th>Average</th><th>Deviation</th></tr>';
+        }
+        DWRUtil.addRows("summary_table", rows, cellFuncs);
+        document.getElementById('summary_loading').style.display = "none";
+        document.getElementById('summary_loaded').style.display = "block";
+    });
 }
 
 function getResultsPoller(qid, timeout, userCallback) {
