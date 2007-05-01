707ab0765153c06921d84d2e67cd9aa6000b67c0
kmr <kmr>
2007-05-01 16:33:02 +0000

    Untabification.
    Former-commit-id: d38634034d144596e81771bfc72fddff3dc85a2f

--- intermine/webapp/main/resources/webapp/js/table.js
+++ intermine/webapp/main/resources/webapp/js/table.js
@@ -1,105 +1,105 @@
 
-  function selectColumnCheckbox(column, bagType) {
+function selectColumnCheckbox(column, bagType) {
     setBagType(bagType);
     selectColumnCheckboxes(column, bagType);
     if (isClear()) {
-      enableAll();
+        enableAll();
     }
-  }
+}
 
 
-  function enableAll() {
+function enableAll() {
     with(document.saveBagForm) {
-      for(i=0;i < elements.length;i++) {
-        thiselm = elements[i];
-        if(thiselm.id.indexOf('selectedObjects_') != -1) {
-          thiselm.disabled = false;
+        for(i=0;i < elements.length;i++) {
+            thiselm = elements[i];
+            if(thiselm.id.indexOf('selectedObjects_') != -1) {
+                thiselm.disabled = false;
+            }
         }
-      }
     }
-  }
+}
 
 
-  function isClear() {
+function isClear() {
     with(document.saveBagForm) {
-      for(i=0;i < elements.length;i++) {
-        thiselm = elements[i];
-        if(thiselm.id.indexOf('selectedObjects_') != -1 && thiselm.checked) {
-          return false;
+        for(i=0;i < elements.length;i++) {
+            thiselm = elements[i];
+            if(thiselm.id.indexOf('selectedObjects_') != -1 && thiselm.checked) {
+                return false;
+            }
         }
-      }
     }
     return true;
-  }
+}
 
 
-  function selectColumnCheckboxes(column, bagType) {
+function selectColumnCheckboxes(column, bagType) {
     var columnCheckBox = 'selectedObjects_' + column;
     with(document.saveBagForm) {
-    for(i=0;i < elements.length;i++) {
-      thiselm = elements[i];
-      var testString = 'selectedObjects_' + column + '_';
-      if(thiselm.id.indexOf(testString) != -1)
-        thiselm.checked = document.getElementById(columnCheckBox).checked;
-      }
+        for(i=0;i < elements.length;i++) {
+            thiselm = elements[i];
+            var testString = 'selectedObjects_' + column + '_';
+            if(thiselm.id.indexOf(testString) != -1)
+                thiselm.checked = document.getElementById(columnCheckBox).checked;
+        }
     }
     elements = document.getElementsByTagName('td');
     for (var i = 0; i < elements.length; i++) {
-		var id = elements.item(i).id;
-		if (id.indexOf('cell') != -1
-			&& id.indexOf(bagType) != -1){
-			if (document.getElementById(columnCheckBox).checked){
-				elements.item(i).className = 'highlightCell';
-			} else {
-				elements.item(i).className = '';
-			}
-		 }
+        var id = elements.item(i).id;
+        if (id.indexOf('cell') != -1
+            && id.indexOf(bagType) != -1){
+            if (document.getElementById(columnCheckBox).checked){
+                elements.item(i).className = 'highlightCell';
+            } else {
+                elements.item(i).className = '';
+            }
+        }
     }
-  }
+}
 
-  /**
-  * Ran when a user selects a keyfield in the results table
-  **/
-  function itemChecked(row, column, bagType, checkbox) {
+/**
+ * Ran when a user selects a keyfield in the results table
+ **/
+function itemChecked(row, column, bagType, checkbox) {
     setBagType(bagType)
-    unselectColumnCheckbox('' + column);
+        unselectColumnCheckbox('' + column);
     if (isClear()) {
-      enableAll();
+        enableAll();
     }
     var elements = document.getElementsByTagName('td');
     for (var i = 0; i < elements.length; i++) {
-		var id = elements.item(i).id;
-		if (id.indexOf('cell') != -1
-			&& id.indexOf(bagType) != -1
-			&& id.split(',')[2] == row){
-			if (checkbox.checked){
-				elements.item(i).className = 'highlightCell';
-			} else {
-				elements.item(i).className = '';
-			}
-		 }
+        var id = elements.item(i).id;
+        if (id.indexOf('cell') != -1
+            && id.indexOf(bagType) != -1
+            && id.split(',')[2] == row){
+            if (checkbox.checked){
+                elements.item(i).className = 'highlightCell';
+            } else {
+                elements.item(i).className = '';
+            }
+        }
     }
-  }
+}
 
-  /**
-  * disables all checkboxes which are not of the specified
-  * type
-  **/
-  function setBagType(bagType){
+/**
+ * disables all checkboxes which are not of the specified
+ * type
+ **/
+function setBagType(bagType){
     with(document.saveBagForm) {
-	      for(i=0;i < elements.length;i++) {
-	        thiselm = elements[i];
-	        if(thiselm.id.indexOf('selectedObjects_') != -1 
-	        		&& thiselm.value.indexOf(bagType) == -1) {
-	          thiselm.disabled = 'true';
-			}
-		  }
-	  }
-  }
+        for(i=0;i < elements.length;i++) {
+            thiselm = elements[i];
+            if(thiselm.id.indexOf('selectedObjects_') != -1 
+               && thiselm.value.indexOf(bagType) == -1) {
+                thiselm.disabled = 'true';
+            }
+        }
+    }
+}
 
-  /**
-  * de-selects a whole column of a given number
-  **/
-  function unselectColumnCheckbox(column) {
+/**
+ * de-selects a whole column of a given number
+ **/
+function unselectColumnCheckbox(column) {
     document.getElementById('selectedObjects_' + column).checked = false;
-  }
+}
