cba8a0ad6d4a349f2eb853bc704fe98be16591fa
kmr <kmr>
2007-05-03 12:59:39 +0000

    Added javascript addEvent() and removeEvent() methods from http://www.quirksmode.org/blog/archives/2005/10/_and_the_winner_1.html

---
+++ intermine/webapp/main/resources/webapp/js/imutils.js
@@ -0,0 +1,25 @@
+// from http://www.quirksmode.org/blog/archives/2005/10/_and_the_winner_1.html
+
+function addEvent( obj, type, fn )
+{
+	if (obj.addEventListener)
+		obj.addEventListener( type, fn, false );
+	else if (obj.attachEvent)
+	{
+		obj["e"+type+fn] = fn;
+		obj[type+fn] = function() { obj["e"+type+fn]( window.event ); }
+		obj.attachEvent( "on"+type, obj[type+fn] );
+	}
+}
+
+function removeEvent( obj, type, fn )
+{
+	if (obj.removeEventListener)
+		obj.removeEventListener( type, fn, false );
+	else if (obj.detachEvent)
+	{
+		obj.detachEvent( "on"+type, obj[type+fn] );
+		obj[type+fn] = null;
+		obj["e"+type+fn] = null;
+	}
+}
