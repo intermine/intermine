8b3665ad4b31b68ee40140b94fe2dafca6fd1de5
kmr <kmr>
2007-05-24 10:30:39 +0000

    Don't use ArtificialDeletion or PCRProduct in WriteGFFTask as they aren't in the MalariaMine model.
    Former-commit-id: 373bbcc74274ae409cc8b9a19aa1c7149b413e78

--- bio/tools/main/src/org/intermine/bio/gbrowse/WriteGFFTask.java
+++ bio/tools/main/src/org/intermine/bio/gbrowse/WriteGFFTask.java
@@ -44,7 +44,6 @@ import org.intermine.util.DynamicUtil;
 import org.intermine.util.TypeUtil;
 
 import org.flymine.model.genomic.Analysis;
-import org.flymine.model.genomic.ArtificialDeletion;
 import org.flymine.model.genomic.CDS;
 import org.flymine.model.genomic.Chromosome;
 import org.flymine.model.genomic.ChromosomeBand;
@@ -57,7 +56,6 @@ import org.flymine.model.genomic.LocatedSequenceFeature;
 import org.flymine.model.genomic.Location;
 import org.flymine.model.genomic.MRNA;
 import org.flymine.model.genomic.NcRNA;
-import org.flymine.model.genomic.PCRProduct;
 import org.flymine.model.genomic.Sequence;
 import org.flymine.model.genomic.Synonym;
 import org.flymine.model.genomic.Transcript;
@@ -168,17 +166,20 @@ public class WriteGFFTask extends Task
                 continue;
             }
 
-            final String featureClassName = feature.getClass().getName();
-            if (featureClassName.equals("org.flymine.model.genomic.ArtificialDeletion")) {
-                try {
-                    if (TypeUtil.getFieldValue(feature, "available") != Boolean.TRUE) {
-                        // write only the available deletions because there are too many
-                        // ArtificialDeletions for GBrowse to work well
-                        continue;
+            try {
+                if (TypeUtil.isInstanceOf(feature, "org.flymine.model.genomic.ArtificialDeletion")) {
+                    try {
+                        if (TypeUtil.getFieldValue(feature, "available") != Boolean.TRUE) {
+                            // write only the available deletions because there are too many
+                            // ArtificialDeletions for GBrowse to work well
+                            continue;
+                        }
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException("can't access 'available' field in: " + feature);
                     }
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException("can't access 'available' field in: " + feature);
                 }
+            } catch (ClassNotFoundException e) {
+               // ignore - ArtificialDeletion is not in the model
             }
 
             if (feature instanceof CDS) {
@@ -554,32 +555,40 @@ public class WriteGFFTask extends Task
             }
         }
 
-        if (bioEntity instanceof PCRProduct) {
-            Boolean fieldValue;
-            try {
-                fieldValue = (Boolean) TypeUtil.getFieldValue(bioEntity, "promoter");
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException("can't access 'promoter' field in: " + bioEntity);
+        try {
+            if (TypeUtil.isInstanceOf(bioEntity, "org.flymine.model.genomic.PCRProduct")) {
+                Boolean fieldValue;
+                try {
+                    fieldValue = (Boolean) TypeUtil.getFieldValue(bioEntity, "promoter");
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("can't access 'promoter' field in: " + bioEntity);
+                }
+                ArrayList promoterFlagList = new ArrayList();
+                promoterFlagList.add(fieldValue.toString());
+                attributes.put("promoter", promoterFlagList);
             }
-            ArrayList promoterFlagList = new ArrayList();
-            promoterFlagList.add(fieldValue.toString());
-            attributes.put("promoter", promoterFlagList);
+        } catch (ClassNotFoundException e) {
+            // ignore - PCRProduct is not in the model
         }
-
-        if (bioEntity instanceof ArtificialDeletion) {
-            Boolean fieldValue;
-            try {
-                fieldValue = (Boolean) TypeUtil.getFieldValue(bioEntity, "available");
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException("can't access 'available' field in: " + bioEntity);
+        
+        try {
+            if (TypeUtil.isInstanceOf(bioEntity, "org.flymine.model.genomic.ArtificialDeletion")) {
+                Boolean fieldValue;
+                try {
+                    fieldValue = (Boolean) TypeUtil.getFieldValue(bioEntity, "available");
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("can't access 'available' field in: " + bioEntity);
+                }
+                if (fieldValue != null) {
+                    ArrayList availableFlagList = new ArrayList();
+                    availableFlagList.add(fieldValue.toString());
+                    attributes.put("available", availableFlagList);
+                }
             }
-            if (fieldValue != null) {
-                ArrayList availableFlagList = new ArrayList();
-                availableFlagList.add(fieldValue.toString());
-                attributes.put("available", availableFlagList);
-            }
+        } catch (ClassNotFoundException e) {
+            // ignore - ArtificialDeletion is not in the model
         }
-
+        
         lineBuffer.append(stringifyAttributes(attributes));
         gffWriter.println(lineBuffer.toString());
     }
