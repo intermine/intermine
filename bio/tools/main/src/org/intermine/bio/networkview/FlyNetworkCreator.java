cb82a40bfa96c207f57084166f9d6052e50b8b00
rnsmith <rns>
2007-05-30 14:09:54 +0000

    Missing files from [11754].

--- bio/tools/main/src/org/intermine/bio/networkview/FlyNetworkCreator.java
+++ bio/tools/main/src/org/intermine/bio/networkview/FlyNetworkCreator.java
@@ -10,22 +10,19 @@ package org.intermine.bio.networkview;
  *
  */
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
-
-import org.intermine.bio.networkview.network.FlyEdge;
-import org.intermine.bio.networkview.network.FlyNetwork;
-import org.intermine.bio.networkview.network.FlyNode;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.flymine.model.genomic.Protein;
 import org.flymine.model.genomic.ProteinInteraction;
-import org.flymine.model.genomic.ProteinInteractor;
+import org.intermine.bio.networkview.network.FlyNetwork;
+import org.intermine.bio.networkview.network.FlyNode;
 
 /**
  * creates a FlyNetwork from a given Collection of ProteinInteractionS
  * @author Florian Reisinger
+ * @author Richard Smith
  *
  */
 public class FlyNetworkCreator
@@ -33,68 +30,43 @@ public class FlyNetworkCreator
     private static final Logger LOG = Logger.getLogger(FlyNetworkCreator.class);
     
     /**
-     * Creates a flymine network from a given Collection of ProteinInteractionS
+     * Creates a flymine network from a given Collection of ProteinInteractions
      * @param proteinInteractions Collection of ProteinInteractionS
      * @return the network representing the protein interactions
      */
-    public static FlyNetwork createFlyNetwork(Collection proteinInteractions) {
+    public static FlyNetwork createFlyNetwork(Collection<ProteinInteraction> proteinInteractions) {
         FlyNetwork fn = new FlyNetwork();
 
-        // TODO: check that Collection really contains ProteinInteractions
-        for (Iterator iter = proteinInteractions.iterator(); iter.hasNext();) {
-            ProteinInteraction pIon = (ProteinInteraction) iter.next();
+        for (ProteinInteraction pIon : proteinInteractions) {
+            
             // TODO: do we want to use the short name? is it unique? -> label or attribute
             // !?there are more than one ProteinInteraction objects for one real interaction!?
             //String name = pIon.getShortName();
 
             if (pIon != null) {
-                ArrayList preys = new ArrayList();
-                ArrayList baits = new ArrayList();
-                ArrayList others = new ArrayList();
-
-                Collection pIors = pIon.getInteractors();
-                for (Iterator iterator = pIors.iterator(); iterator.hasNext();) {
-                    ProteinInteractor pIor = (ProteinInteractor) iterator.next();
-                    if (pIor.getRole().equalsIgnoreCase("prey")) {
-                        preys.add(pIor.getProtein());
-                    } else if (pIor.getRole().equalsIgnoreCase("bait")) {
-                        baits.add(pIor.getProtein());
-                    } else {
-                        others.add(pIor.getProtein());
-                    }
-                }
-                // one prey one bait -> standard case
-                if (baits.size() == 1 && preys.size() == 1 && others.size() == 0) {
-                    // TODO: check that all proteins have primaryAccession
-                    FlyNode prey = new FlyNode(((Protein) preys.get(0)).getPrimaryAccession());
-                    FlyNode bait = new FlyNode(((Protein) baits.get(0)).getPrimaryAccession());
-                    FlyEdge fe = new FlyEdge(prey, bait);
-
-                    fn.addNode(prey);
-                    fn.addNode(bait);
-                    fn.addEdge(fe);
-                } else { // not prey-bait standard case
-                    // link all proteins to each other
-                    // TODO: test this
-                    others.addAll(preys);
-                    others.addAll(baits);
-                    Protein[] p = new Protein[1];
-                    p = (Protein[]) others.toArray(p);
-                    FlyNode[] nx = new FlyNode[p.length];
-                    for (int i = 0; i < p.length; i++) {
-                        String s = p[i].getPrimaryAccession();
-                        nx[i] = new FlyNode(s);
-                        fn.addNode(nx[i]);
-                    }
-                    for (int i = 0; i < others.size(); i++) {
-                        for (int j = i + 1; j < others.size(); j++) {
-                            FlyEdge ex = new FlyEdge(nx[i], nx[j],
-                                    FlyNetwork.COMPLEX_INTERACTION_TYPE);
-                            fn.addEdge(ex);
-                        }
-                    }
-                }
                 
+                Set<Protein> interacting = (Set<Protein>) pIon.getInteractingProteins();
+                if (interacting != null && !interacting.isEmpty()) {
+                    // set whether this is a binary interaction or a complex
+                    String type = (interacting.size() > 1) ? FlyNetwork.COMPLEX_INTERACTION_TYPE
+                                                           : FlyNetwork.DEFAULT_INTERACTION_TYPE;
+                    
+                    FlyNode fnProtein = new FlyNode(pIon.getProtein().getPrimaryAccession());
+                    fn.addNode(fnProtein);
+                
+                    // TODO if we want to add multiple edges if an interaction is observed
+                    // in more than one experiment then we should add the experiment name
+                    // or pubmed id as a label.  Otherwise only one edge will be created
+                    // between each pair of proteins.
+                    // TODO this may create two edges as we create interactions in both
+                    // directions.  Should check for source and target instead of label.
+                    for (Protein interact : interacting) {
+                        FlyNode fnInteract = new FlyNode(interact.getPrimaryAccession());
+                        fn.addNode(fnInteract);
+                        
+                        fn.addEdge(fnProtein, fnInteract, type);
+                    }
+                }
             }
         }
         LOG.debug("FlyNetwork created by FlyNetworkCreator: \n" + fn.toString(true));
