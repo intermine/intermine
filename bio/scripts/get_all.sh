2ecc3009f9a690d804addcc4bcbd576b2f48af8c
pnorth <pnorth>
2007-07-06 15:11:48 +0000

    modified scripts so that the paths to the log file, config file and /shared/data are passed from get_all.sh
    Former-commit-id: 7195f6da3bb57fce741f0066724872ee80e2115e

--- bio/scripts/get_all.sh
+++ bio/scripts/get_all.sh
@@ -1,74 +1,82 @@
 #!/bin/sh
-tempfile="/shared/data/download_logs/temp_log.txt"
+logdir="/shared/data/download_logs/"
+if [ ! -d $logdir ]
+then
+	mkdir -p $logdir
+fi
+tempname="temp_log.txt"
+tempfile=$logdir$tempname
+shared_data="/shared/data"
+config_file="./resources/get_scripts.config"
 rm $tempfile
 echo "==========================================================="
 echo "Getting Fly Anatomy Ontology"
-./get_fly_anatomy_ontology
+./get_fly_anatomy_ontology $logdir $tempname $shared_data
 echo "==========================================================="
 echo
 echo "==========================================================="
 echo "Getting FlyAtlas"
-./get_flyatlas
+./get_flyatlas $logdir $tempname $shared_data
 echo "==========================================================="
 echo
 echo "==========================================================="
 echo "Getting go annotation"
-./get_go-annotation
+./get_go-annotation $logdir $tempname $shared_data $config_file
 echo "==========================================================="
 echo
 echo "==========================================================="
 echo "Getting intact data"
-./get_intact
+./get_intact $logdir $tempname $shared_data $config_file
 echo "==========================================================="
 echo
 echo "==========================================================="
 echo "Getting KEGG data"
-./get_kegg
+./get_kegg $logdir $tempname $shared_data
 echo "==========================================================="
 echo
 echo "==========================================================="
 echo "Getting wormbase identifiers"
-./get_wormbase_identifiers
+./get_wormbase_identifiers $logdir $tempname $shared_data
 echo "==========================================================="
 echo
 echo "==========================================================="
 echo "Getting wormbase rnai"
-./get_wormbase_rnai
+./get_wormbase_rnai $logdir $tempname $shared_data
 echo "==========================================================="
 echo
 echo "==========================================================="
 echo "Getting pubmed data"
-./get_ncbi_pubmed
+./get_ncbi_pubmed $logdir $tempname $shared_data $config_file
 echo "==========================================================="
 echo 
 echo "==========================================================="
 echo "Getting homophila"
-./get_homophila
+./get_homophila $logdir $tempname $shared_data
 echo "==========================================================="
 echo
 echo "==========================================================="
 echo "Getting uniprot"
-./get_uniprot
+./get_uniprot $logdir $tempname $shared_data $config_file
 echo "==========================================================="
 echo
 echo "==========================================================="
 echo "Getting Ensembl GeneId to PeptideId data"
-./get_ensemblgeneID2peptideID
+./get_ensemblgeneID2peptideID $logdir $tempname $shared_data $config_file
 echo "==========================================================="
 echo
 echo "==========================================================="
 echo "Getting InParanoid"
-./get_inparanoid
+./get_inparanoid $logdir $tempname $shared_data $config_file
 echo "==========================================================="
 echo
 #echo "==========================================================="
 #echo "Getting Interpro xml file"
-#./get_interproXML
+#./get_interproXML $logdir $tempname $shared_data
 #echo "==========================================================="
 echo
 echo "==========================================================="
 echo "Getting FlyBase version"
-./get_flybase_version
+./get_flybase_version $logdir $tempname $shared_data
 echo "==========================================================="
 
 today=$(date +"%F")
