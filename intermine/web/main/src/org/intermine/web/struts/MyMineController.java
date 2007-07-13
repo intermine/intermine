d0168573dc8c123af5b991a25dae970ec322d9b6
kmr <kmr>
2007-07-13 13:49:27 +0000

    Moved bag specific initialisation from MyMineController to BagController.
    Former-commit-id: 24750f61eefa9ea0162646aec3b711875d11a1bb

--- intermine/web/main/src/org/intermine/web/struts/MyMineController.java
+++ intermine/web/main/src/org/intermine/web/struts/MyMineController.java
@@ -101,64 +101,8 @@ public class MyMineController extends TilesAction
             }
         }
 
-        ObjectStore os = (ObjectStore) servletContext.getAttribute(Constants.OBJECTSTORE);
-        ObjectStoreSummary oss =
-            (ObjectStoreSummary) servletContext.getAttribute(Constants.OBJECT_STORE_SUMMARY);
-        Collection qualifiedTypes = os.getModel().getClassNames();
-        ArrayList typeList = new ArrayList();
-        ArrayList preferedTypeList = new ArrayList();
-        String superUserName = (String) servletContext.getAttribute(Constants.SUPERUSER_ACCOUNT);
-
-        List preferredBagTypeTags = pm.getTags("im:preferredBagType", null, "class", superUserName);
-        for (Iterator iter = preferredBagTypeTags.iterator(); iter.hasNext();) {
-            Tag tag = (Tag) iter.next();
-            preferedTypeList.add(TypeUtil.unqualifiedName(tag.getObjectIdentifier()));
-        }
-        Map classKeys = (Map) servletContext.getAttribute(Constants.CLASS_KEYS);
-        for (Iterator iter = qualifiedTypes.iterator(); iter.hasNext();) {
-            String className = (String) iter.next();
-            String unqualifiedName = TypeUtil.unqualifiedName(className);
-            if (ClassKeyHelper.hasKeyFields(classKeys, unqualifiedName)
-                && oss.getClassCount(className) > 0
-                && !preferedTypeList.contains(unqualifiedName)) {
-                typeList.add(unqualifiedName);
-            }
-        }
-        Collections.sort(preferedTypeList);
-        Collections.sort(typeList);
-        request.setAttribute("typeList", typeList);
-        request.setAttribute("preferredTypeList", preferedTypeList);
-
-        BagQueryConfig bagQueryConfig =
-            (BagQueryConfig) servletContext.getAttribute(Constants.BAG_QUERY_CONFIG);
-        String extraClassName = bagQueryConfig.getExtraConstraintClassName();
-        if (extraClassName != null) {
-            request.setAttribute("extraBagQueryClass", TypeUtil.unqualifiedName(extraClassName));
-
-            List extraClassFieldValues =
-                getFieldValues(os, oss, extraClassName, bagQueryConfig.getConstrainField());
-            request.setAttribute("extraClassFieldValues", extraClassFieldValues);
-
-            // find the types in typeList that contain a field with the name given by
-            // bagQueryConfig.getConnectField()
-            List typesWithConnectingField = new ArrayList();
-            Iterator allTypesIterator =
-                new IteratorChain(typeList.iterator(), preferedTypeList.iterator());
-            while (allTypesIterator.hasNext()) {
-                String connectFieldName = bagQueryConfig.getConnectField();
-                String typeName = (String) allTypesIterator.next();
-                String qualifiedTypeName = model.getPackageName() + "." + typeName;
-                ClassDescriptor cd = model.getClassDescriptorByName(qualifiedTypeName);
-                FieldDescriptor fd = cd.getFieldDescriptorByName(connectFieldName);
-                if (fd != null && fd instanceof ReferenceDescriptor) {
-                    typesWithConnectingField.add(typeName);
-                }
-            }
-            request.setAttribute("typesWithConnectingField", typesWithConnectingField);
-        }
 
         request.setAttribute("queryAgeClasses", getQueryAgeClasses(profile.getSavedQueries()));
-        request.setAttribute("bagAgeClasses", getBagAgeClasses(profile.getSavedBags()));
 
         return null;
     }
@@ -173,20 +117,6 @@ public class MyMineController extends TilesAction
         for (Map.Entry<String, SavedQuery> entry: queries.entrySet()) {
             String queryName = entry.getKey();
             ageClassMap.put(queryName, getCSSClassForAge(entry.getValue().getDateCreated()));
-        }
-        return ageClassMap;
-    }
-
-    /**
-     * Return a Map from bag name to css class to use on the div/tr/td displaying the bag.
-     * @param bags a Map from bag name to InterMineBag
-     * @return a Map from query name to CSS class
-     */
-    static Map<String, String> getBagAgeClasses(Map<String, InterMineBag> bags) {
-        Map<String, String> ageClassMap = new HashMap<String, String>();
-        for (Map.Entry<String, InterMineBag> entry: bags.entrySet()) {
-            String bagName = entry.getKey();
-            ageClassMap.put(bagName, getCSSClassForAge(entry.getValue().getDateCreated()));
         }
         return ageClassMap;
     }
@@ -216,28 +146,4 @@ public class MyMineController extends TilesAction
         }
     }
 
-    private List getFieldValues(ObjectStore os, ObjectStoreSummary oss, String extraClassName,
-                                String constrainField) {
-        List fieldValues = oss.getFieldValues(extraClassName, constrainField);
-        if (fieldValues == null) {
-            Query q = new Query();
-            q.setDistinct(true);
-            QueryClass qc;
-            try {
-                qc = new QueryClass(Class.forName(extraClassName));
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException("Can't find class for: " + extraClassName);
-            }
-            q.addToSelect(new QueryField(qc, constrainField));
-            q.addFrom(qc);
-            Results results = os.execute(q);
-            fieldValues = new ArrayList();
-            for (Iterator j = results.iterator(); j.hasNext();) {
-                Object fieldValue = ((ResultsRow) j.next()).get(0);
-                fieldValues.add(fieldValue == null ? null : fieldValue.toString());
-            }
-        }
-
-        return fieldValues;
-    }
 }
