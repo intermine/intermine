6a1596a33256edc7d412898c46f964e1a9b707cb
julie-sullivan <julie@flymine.org>
2007-08-03 12:13:21 +0000

    removed debugging info
    Former-commit-id: 9f91cd265b1178bc76de7a1df6a2df8c04db4806

--- intermine/webapp/main/resources/webapp/js/imdwr.js
+++ intermine/webapp/main/resources/webapp/js/imdwr.js
@@ -2,16 +2,12 @@ function setFavourite(name, type, image){
    
     var img = image.src;
     var isFavourite;
-    alert(img.substring(img.length-12, img.length));
     if (img.substring(img.length-12, img.length) == 'unactive.gif') {
     	isFavourite = false;
     } else {
     	isFavourite = true;
     }
-    
     AjaxServices.setFavourite(name, type, isFavourite);
-    alert(image.src);
-    
     // already a favourite.  turning off.
     if (isFavourite) {
 	    image.src='images/star_unactive.gif';
