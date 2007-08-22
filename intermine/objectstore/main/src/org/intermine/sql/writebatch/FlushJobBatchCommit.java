03b8d4a9a63536082364900cfe146c0dfa988deb
mnw21 <mnw21>
2007-08-22 14:33:56 +0000

    Check in new file needed by previous check-in, and do ignores
    Former-commit-id: e7bc80dd96045659ed8ff0bce928446749cb6bcf

---
+++ intermine/objectstore/main/src/org/intermine/sql/writebatch/FlushJobBatchCommit.java
@@ -0,0 +1,42 @@
+package org.intermine.sql.writebatch;
+
+/*
+ * Copyright (C) 2002-2007 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
+/**
+ * A FlushJob that commits and re-opens a transaction on the database.
+ *
+ * @author Matthew Wakeling
+ */
+public class FlushJobBatchCommit implements FlushJob
+{
+    Connection con;
+
+    /**
+     * Constructor for this class
+     *
+     * @param con a Connection with which to perform the updates
+     */
+    public FlushJobBatchCommit(Connection con) {
+        this.con = con;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void flush() throws SQLException {
+        con.commit();
+    }
+}
+
