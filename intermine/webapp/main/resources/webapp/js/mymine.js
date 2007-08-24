22b0bbe754c807a45993a1eb2aa45d938f5059fe
julie-sullivan <julie@flymine.org>
2007-08-24 13:37:18 +0000

    missing file from [12621]
    Former-commit-id: 3dc0000385739eed4053761cdf53e1653488fe32

---
+++ intermine/webapp/main/resources/webapp/js/mymine.js
@@ -0,0 +1,77 @@
+
+   function selectColumnCheckbox(form, type) {
+       	var deleteButton = document.getElementById('delete_button');
+   		var removeButton = document.getElementById('remove_button');
+  	 	var exportButton = document.getElementById('export_button');
+  	 	
+       var columnCheckBox = 'selected_' + type;
+       var checked = document.getElementById(columnCheckBox).checked;
+       if (deleteButton != null) {
+           deleteButton.disabled = !checked;
+       }
+       if (removeButton != null) {
+           removeButton.disabled = !checked;
+       }
+       if (exportButton != null) {
+           exportButton.disabled = !checked;
+       }
+       with(form) {
+           for(var i=0;i < elements.length;i++) {
+               var thiselm = elements[i];
+               if(thiselm.id.indexOf(testString) != -1)
+                   thiselm.checked = checked;
+           }
+       }
+   }
+   function setDeleteDisabledness(form, type) {
+
+       	var deleteButton = document.getElementById('delete_button');
+   		var removeButton = document.getElementById('remove_button');
+  	 	var exportButton = document.getElementById('export_button');
+
+       var checkBoxPrefix = 'selected_' + type + '_';
+       var deleteDisable = true;
+       var columnCheckBoxChecked = true;
+       with(form) {
+        	
+           for(var i=0;i < elements.length;i++) {
+               var thiselm = elements[i];
+             
+               if (thiselm.id.indexOf(checkBoxPrefix) != -1) {
+                   if (thiselm.checked) {
+                       deleteDisable = false;
+                   } else {
+                       columnCheckBoxChecked = false;
+                   }
+               }
+           }
+       }
+       if (deleteButton != null) {
+           deleteButton.disabled = deleteDisable;
+       }
+       if (removeButton != null) {
+           removeButton.disabled = deleteDisable;
+       }
+       if (exportButton != null) {
+           exportButton.disabled = deleteDisable;
+       }
+       document.getElementById('selected_' + type).checked = columnCheckBoxChecked;
+       return true;
+   }
+   
+ function selectColumnCheckbox(form, type, scope) {
+
+    var checkBoxId = 'selected_' + type;
+    var checked = document.getElementById(checkBoxId).checked;
+    with(form) {
+      for(i=0;i < elements.length;i++) {
+        thiselm = elements[i];
+        var testString = checkBoxId + '_';
+        if(thiselm.id.indexOf(testString) != -1)
+          thiselm.checked = checked;
+      }
+    }
+  }
+  function noenter() {
+    return !(window.event && window.event.keyCode == 13);
+  }
