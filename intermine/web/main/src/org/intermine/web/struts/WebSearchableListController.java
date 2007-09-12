9fbf43a28206154039de0c0f9f875117d7d78e86
julie-sullivan <julie@flymine.org>
2007-09-12 09:20:26 +0000

    checkstyle fix
    Former-commit-id: 8e0cc32b2739beb76bc284a9ca19a3415d6c36fa

--- intermine/web/main/src/org/intermine/web/struts/WebSearchableListController.java
+++ intermine/web/main/src/org/intermine/web/struts/WebSearchableListController.java
@@ -10,12 +10,10 @@ package org.intermine.web.struts;
  *
  */
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
