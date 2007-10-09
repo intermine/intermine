de88bacc58108467322603446c24fe37d5b9ef64
julie-sullivan <julie@flymine.org>
2007-10-09 12:20:29 +0000

    write sitemap_index.xml during generate sitemap task

--- www/common/scripts/sitemap_gen.pl
+++ www/common/scripts/sitemap_gen.pl
@@ -4,20 +4,42 @@ use File::Find;
 
 $dir = $ARGV[0];
 $filename="$dir/sitemap.xml";
+
 $url = "http://www.flymine.org";
-$urlset = "< urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">";
+$urlset = "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">";
 $head = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+$indexfilename = "$dir/sitemap_index.xml";
+$indexhead = "<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"\n xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9\n http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd\">";
+@sitemaps = ("/sitemap.xml", "/query/sitemap.xml", "/query/sitemapGenes0.xml", "/query/sitemapGenes1.xml", "/query/sitemapGenes2.xml", "/query/sitemapProts0.xml" , "/query/sitemapProts1.xml", "/query/sitemapProts2.xml");
+
 
 ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);
 $y = $year+1900;
 $m = $mon+1;
 
+# write sitemap
 open(FILE,">$filename") || die("Cannot Open File");
 print FILE "$head\n";
 print FILE "$urlset\n";
 find(\&crawl, $dir);
 print FILE "</urlset>";
 close(FILE);
+
+# write sitemap index
+open(FILE,">$indexfilename") || die("Cannot Open File");
+print FILE "$indexhead\n";
+foreach (@sitemaps) {
+      	print FILE "<sitemap>\n";
+       	print FILE "<loc>\n";
+        	print FILE "$url$_\n";
+	     	print FILE "</loc>\n";
+         print FILE "<lastmod>\n";         
+        	print FILE "$y-$m-$mday\n";
+	     	print FILE "</lastmod>\n"; 	
+      	print FILE "</sitemap>\n";
+}
+print FILE "</sitemapindex>";
+close(FILE);       
 
  sub crawl {	
    	if ((/\.html$/) || (/\.shtml$/)){
