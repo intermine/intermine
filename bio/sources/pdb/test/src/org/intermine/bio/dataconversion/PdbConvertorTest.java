43f89d8ae8b269e9ea278546ff8d25d37151c457
watkins <watkins>
2007-10-19 14:42:44 +0000

    improved pdb loader

---
+++ bio/sources/pdb/test/src/org/intermine/bio/dataconversion/PdbConvertorTest.java
@@ -0,0 +1,55 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2007 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Set;
+
+import org.intermine.dataconversion.ItemsTestCase;
+import org.intermine.dataconversion.MockItemWriter;
+import org.intermine.metadata.Model;
+
+/**
+ * Test for the PdbConvertor
+ * @author Xavier Watkins
+ *
+ */
+public class PdbConvertorTest extends ItemsTestCase
+{
+
+    public PdbConvertorTest(String arg) {
+        super(arg);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testProcess() throws Exception {
+        Reader reader = new InputStreamReader(getClass().getClassLoader()
+                                              .getResourceAsStream("1AZE.pdb"));
+        
+        MockItemWriter itemWriter = new MockItemWriter(new HashMap());
+        PdbConvertor converter = new PdbConvertor(itemWriter, Model.getInstanceByName("genomic"));
+        File f = new File("1AZE.pdb");
+        converter.setCurrentFile(f);
+        converter.setSrcDataDir("resouces/");
+        converter.process(reader);
+        converter.close();
+        Set expected = readItemSet("PdbConverterTest_tgt.xml");
+        assertEquals(expected, itemWriter.getItems());
+        
+    }
+
+}
