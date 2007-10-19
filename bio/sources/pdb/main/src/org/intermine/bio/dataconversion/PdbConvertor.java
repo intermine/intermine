639f1ef70b0a59ae40c3de53374c2e9ea272e7de
watkins <watkins>
2007-10-19 14:42:44 +0000

    improved pdb loader
    Former-commit-id: 43f89d8ae8b269e9ea278546ff8d25d37151c457

--- bio/sources/pdb/main/src/org/intermine/bio/dataconversion/PdbConvertor.java
+++ bio/sources/pdb/main/src/org/intermine/bio/dataconversion/PdbConvertor.java
@@ -10,17 +10,24 @@ package org.intermine.bio.dataconversion;
  *
  */
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.io.Reader;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
+import org.biojava.bio.program.formats.Ligand.Compound;
+import org.biojava.bio.structure.Structure;
+import org.biojava.bio.structure.io.PDBFileParser;
 import org.intermine.dataconversion.FileConverter;
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
-import org.intermine.util.TextFileUtil;
+import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.xml.full.Item;
-import org.intermine.xml.full.ItemFactory;
-import org.intermine.xml.full.ItemHelper;
 
 /**
  * @author Xavier Watkins
@@ -31,75 +38,106 @@ public class PdbConvertor extends FileConverter
 
     private static final String GENOMIC_NS = "http://www.flymine.org/model/genomic#";
     private static final Logger LOG = Logger.getLogger(PdbConvertor.class);
-    private ItemFactory itemFactory;
     private String dataLocation;
     protected static final String ENDL = System.getProperty("line.separator");
+    private Item dataSource, dataSet;
     
     /**
      * @param writer
      */
-    public PdbConvertor(ItemWriter writer) {
-        super(writer);
-        itemFactory = new ItemFactory(Model.getInstanceByName("genomic"));
-    }
+    public PdbConvertor(ItemWriter writer, Model model) throws ObjectStoreException {
+        super(writer, model);
+        dataSource = createItem("DataSource");
+        dataSource.setAttribute("name", "PDB");
+        store(dataSource);
 
+        dataSet = createItem("DataSet");
+        dataSet.setAttribute("title", "PDB data");
+        dataSet.setReference("dataSource", dataSource.getIdentifier());
+        store(dataSet);
+    }
+    
     /**
      * @see FileConverter#process(Reader)
      */
     public void process(Reader reader) throws Exception {
-        Iterator fileIterator = TextFileUtil.parseTabDelimitedReader(reader);
-        StringBuffer title = new StringBuffer();
-        StringBuffer experiment = new StringBuffer();
-        StringBuffer atm = new StringBuffer();
-        StringBuffer keywords = new StringBuffer();
-        while (fileIterator .hasNext()) {
-            String [] lineArray = (String []) fileIterator .next();
-            if (lineArray[0].equals("TITLE")) {
-                title.append(lineArray[1]);
-            } else if (lineArray[0].equals("KEYWDS")) {
-                keywords.append(lineArray[1]);
-            } else if (lineArray[0].equals("ATOM")) {
-                atm.append(lineArray[1]);
-            } else if (lineArray[0].equals("EXPDTA")) {
-                experiment.append(lineArray[1]);
-            } else if (lineArray[0].equals("DBREF")) {
-                for (int i=1; i<lineArray.length ; i++) {
-                    if (lineArray[i].equals("SWS")) {
-                        String uniprotId = lineArray[i+1];
-                        Item protein = createItem("Protein");
-                        
-                        break;
-                    }
-                }
-                //look for SWS
+        File currentFile = getCurrentFile();
+        if (currentFile.getName().endsWith(".pdb")) {
+            Item proteinStructure = createItem("ProteinStructure");
+           
+            PDBFileParser pdbfileparser = new PDBFileParser();
+            PdbBufferedReader pdbBuffReader = new PdbBufferedReader(reader);
+            Structure structure = pdbfileparser.parsePDBFile(pdbBuffReader);
+            String atm = structure.toPDB();
+            
+            proteinStructure.setAttribute("identifier", "pdb_" + getCurrentFile()
+                                          .getName().substring(0,getCurrentFile().getName().lastIndexOf(".pdb")));
+    
+            List<String> proteins = new ArrayList<String>();
+            List<String> dbrefs = pdbBuffReader.getDbrefs();
+            for(String accnum: dbrefs) {
+                Item protein = getAndStoreItemOnce("Protein", "primaryAccession", accnum);
+                proteins.add(protein.getIdentifier());
             }
+            
+            proteinStructure.setAttribute("idCode", (String) structure.getHeader().get("idCode"));
+            proteinStructure.setAttribute("title", (String) structure.getHeader().get("title"));
+            proteinStructure.setAttribute("technique", (String) structure.getHeader().get("technique"));
+            proteinStructure.setAttribute("classification", (String) structure.getHeader().get("classification"));
+            proteinStructure.setAttribute("depDate", (String) structure.getHeader().get("depDate"));
+            proteinStructure.setAttribute("modDate", (String) structure.getHeader().get("modDate"));
+            proteinStructure.setAttribute("resolution", (String) structure.getHeader().get("resolution"));
+            
+            proteinStructure.setAttribute("atm", atm);
+            proteinStructure.setCollection("proteins", proteins);
+            store(proteinStructure);
         }
-        Item proteinStructure = createItem("ProteinStructure");
-        proteinStructure
-                        .setAttribute(
-                                      "identifier",
-                                      "pdb_"
-                                                      + getCurrentFile()
-                                                                        .getName()
-                                                                        .substring(
-                                                                                   0,
-                                                                                   getCurrentFile()
-                                                                                                   .getName()
-                                                                                                   .lastIndexOf(
-                                                                                                                ".pdb")));
-        proteinStructure.setAttribute("keywords", keywords.toString());
-        proteinStructure.setAttribute("atm", atm.toString());
-        proteinStructure.setAttribute("title", title.toString());
-        getItemWriter().store(ItemHelper.convert(proteinStructure));
     }
     
     /**
-     * Convenience method for creating a new Item
-     * @param className the name of the class
-     * @return a new Item
+     * Pick up the data location from the ant, the translator needs to open some more files.
+     * @param srcdatadir location of the source data
      */
-    private Item createItem(String className) {
-        return itemFactory.makeItemForClass(GENOMIC_NS + className);
+    public void setSrcDataDir(String srcdatadir) {
+        this.dataLocation = srcdatadir;
+    }
+    
+    /**
+     * BioJava doesn't support getting DBREF so
+     * we get it as the file is read.
+     * 
+     * @author Xavier Watkins
+     *
+     */
+    public class PdbBufferedReader extends BufferedReader{
+
+        private List<String> dbrefs = new ArrayList<String>();
+        
+        public PdbBufferedReader(Reader reader) {
+            super(reader);
+        }
+
+        /* (non-Javadoc)
+         * @see java.io.BufferedReader#readLine()
+         */
+        @Override
+        public String readLine() throws IOException {
+            String line = super.readLine();
+            if (line != null && line.matches("^DBREF.*")) {
+                String [] split = line.split("\\s+");
+                if (split[5].equals("SWS")) {
+                    dbrefs.add(split[6]);
+                }
+            }
+            return line;
+        }
+        
+        public List getDbrefs() {
+            return dbrefs;
+        }
+        
+        
     }
 
+    
 }
