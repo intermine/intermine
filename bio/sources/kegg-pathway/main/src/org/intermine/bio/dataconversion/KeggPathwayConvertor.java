d79057e4fcfdf0f43ef612ab52865ea6c27c8d49
watkins <watkins>
2007-10-24 10:39:12 +0000

    Added test for kegg and made it more generic t import data from differents organisms later

--- bio/sources/kegg-pathway/main/src/org/intermine/bio/dataconversion/KeggPathwayConvertor.java
+++ bio/sources/kegg-pathway/main/src/org/intermine/bio/dataconversion/KeggPathwayConvertor.java
@@ -14,6 +14,9 @@ import java.io.File;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.intermine.dataconversion.DataConverter;
 import org.intermine.dataconversion.FileConverter;
@@ -33,8 +36,10 @@ import org.intermine.xml.full.ReferenceList;
  */
 public class KeggPathwayConvertor extends FileConverter
 {
-    protected Item dataSource, dmel;
+    protected Item dataSource;
+    protected Map<String, String> keggOrganismToTaxonId = new HashMap<String, String>();
     protected HashMap pathwayMap = new HashMap();
+    private String dataLocation;
 
     /**
      * Constructor
@@ -46,15 +51,19 @@ public class KeggPathwayConvertor extends FileConverter
     public KeggPathwayConvertor(ItemWriter writer, Model model)
         throws ObjectStoreException, MetaDataException {
         super(writer, model);
+        
+        // Drosophila melanogaster
+        keggOrganismToTaxonId.put("dme", "7227");
+        // Drosophila pseudoobscura
+//        keggOrganismToTaxonId.put("dpo", "7237");
+        // Anopheles gambiae
+//        keggOrganismToTaxonId.put("aga", "180454");
+        // Apis mellifera
+//        keggOrganismToTaxonId.put("dame", "7460");
 
         dataSource = createItem("DataSource");
         dataSource.setAttribute("name", "Kegg");
         store(dataSource);
-
-        dmel = createItem("Organism");
-        dmel.setAttribute("taxonId", "7227");
-        store(dmel);
-
     }
 
 
@@ -76,45 +85,55 @@ public class KeggPathwayConvertor extends FileConverter
 
         while (lineIter.hasNext()) {
             String[] line = (String[]) lineIter.next();
+            Pattern filePattern = Pattern.compile("^(\\S+)_gene_map.*");
+            Matcher matcher = filePattern.matcher(currentFile.getName());
             if (line.length <= 1 || line[0].startsWith("#")) {
                 continue;
             }
             if (currentFile.getName().startsWith("map_title")) {
                 String mapIdentifier = line[0];
                 String mapName = line[1];
-                Item pathway = getPathway(mapIdentifier);
+                Item pathway = getAndStoreItemOnce("Pathway","identifier", mapIdentifier);
                 pathway.setAttribute("name", mapName);
                 store(pathway);
-            } else if (currentFile.getName().startsWith("dme_gene_map")) {
-                String geneName = line[0];
-                if (geneName.startsWith("Dmel_")) {
-                    geneName = geneName.substring(5);
-                } else {
-                    continue;
+            } else if (matcher.find()){
+                String keggOrgName = matcher.group(1);
+                String taxonId = keggOrganismToTaxonId.get(keggOrgName);
+                
+                if (taxonId != null && taxonId.length() !=0 ) {
+                    Item organism = getAndStoreItemOnce("Organism", "taxonId", taxonId);
+                    
+                    String geneName = line[0];
+                    
+                    // There are a couple of Transcripts ID's so for the moment we don't want them
+                    if (geneName.startsWith("Dmel_")) {
+                        geneName = geneName.substring(5);
+                    } else {
+                        continue;
+                    }
+                    String mapIdentifiers = line[1];
+                    ReferenceList referenceList = new ReferenceList("pathways");
+                    String [] mapArray = mapIdentifiers.split(" ");
+                    for (int i = 0; i < mapArray.length; i++) {
+                        referenceList.addRefId(getAndStoreItemOnce("Pathway","identifier", mapArray[i])
+                                               .getIdentifier());
+                    }
+                    Item gene = createItem("Gene");
+                    gene.setAttribute("identifier", geneName);
+                    gene.setReference("organism", organism);
+    
+                    gene.addCollection(referenceList);
+                    store(gene);
                 }
-                String mapIdentifiers = line[1];
-                ReferenceList referenceList = new ReferenceList("pathways");
-                String [] mapArray = mapIdentifiers.split(" ");
-                for (int i = 0; i < mapArray.length; i++) {
-                    referenceList.addRefId(getPathway(mapArray[i]).getIdentifier());
-                }
-                Item gene = createItem("Gene");
-                gene.setAttribute("identifier", geneName);
-                gene.setReference("organism", dmel);
-
-                gene.addCollection(referenceList);
-                store(gene);
             }
         }
     }
 
-    private Item getPathway(String identifier) {
-        Item pathway = (Item) pathwayMap.get(identifier);
-        if (pathway == null) {
-            pathway = createItem("Pathway");
-            pathway.setAttribute("identifier", "dme" + identifier);
-            pathwayMap.put(identifier, pathway);
-        }
-        return pathway;
+    /**
+     * Pick up the data location from the ant, the translator needs to open some more files.
+     * @param srcdatadir location of the source data
+     */
+    public void setSrcDataDir(String srcdatadir) {
+        this.dataLocation = srcdatadir;
     }
 }
