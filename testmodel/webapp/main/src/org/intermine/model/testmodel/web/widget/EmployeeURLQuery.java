e8e57ef22dbf0bb82efdee379865680fd1a160f1
julie-sullivan <julie@flymine.org>
2007-11-23 14:18:50 +0000

    updated widget queries for testmodel

---
+++ testmodel/webapp/main/src/org/intermine/model/testmodel/web/widget/EmployeeURLQuery.java
@@ -0,0 +1,88 @@
+package org.intermine.model.testmodel.web.widget;
+
+/*
+ * Copyright (C) 2002-2007 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.intermine.objectstore.query.ConstraintOp;
+
+
+import org.intermine.metadata.Model;
+import org.intermine.objectstore.ObjectStore;
+import org.intermine.web.logic.bag.InterMineBag;
+import org.intermine.web.logic.query.Constraint;
+import org.intermine.web.logic.query.MainHelper;
+import org.intermine.web.logic.query.PathNode;
+import org.intermine.web.logic.query.PathQuery;
+import org.intermine.web.logic.widget.EnrichmentWidgetURLQuery;
+
+/**
+ * Builds a query to get all the employees (in bag) associated with specified dept.
+ * @author Julie Sullivan
+ */
+public class EmployeeURLQuery implements EnrichmentWidgetURLQuery
+{
+
+    InterMineBag bag;
+    String key;
+    ObjectStore os;
+    /**
+     * @param key
+     * @param bag
+     * @param os
+     */
+    public EmployeeURLQuery(ObjectStore os, InterMineBag bag, String key) {
+        this.bag = bag;
+        this.key = key;
+        this.os = os;
+    }
+
+    /**
+     * @return Query a query to generate the results needed
+     */
+    public PathQuery generatePathQuery() {
+                       
+       
+        Model model = os.getModel();
+        PathQuery q = new PathQuery(model);
+        
+        List view = new ArrayList();
+        view.add(MainHelper.makePath(model, q, "Employee.name"));
+        view.add(MainHelper.makePath(model, q, "Employee.department.name"));
+        view.add(MainHelper.makePath(model, q, "Employee.department.company.name"));
+        view.add(MainHelper.makePath(model, q, "Employee.fullTime"));
+        
+        q.setView(view);
+        
+        String bagType = bag.getType();
+        ConstraintOp constraintOp = ConstraintOp.IN;
+        String constraintValue = bag.getName();        
+        String label = null, id = null, code = q.getUnusedConstraintCode();
+        Constraint c = new Constraint(constraintOp, constraintValue, false, label, code, id, null);
+        q.addNode(bagType).getConstraints().add(c);
+        
+        // dept
+        constraintOp = ConstraintOp.EQUALS;
+        code = q.getUnusedConstraintCode();
+        PathNode deptNode = q.addNode("Employee.department.name");
+        Constraint deptConstraint 
+                        = new Constraint(constraintOp, key, false, label, code, id, null);
+        deptNode.getConstraints().add(deptConstraint);
+        
+        q.setConstraintLogic("A and B");
+        q.syncLogicExpression("and");
+                
+        return q;
+    }
+}
+
