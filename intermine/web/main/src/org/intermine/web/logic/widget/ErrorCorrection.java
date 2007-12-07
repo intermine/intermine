302c0d8153f6c4cf8e24214a7dc9a2344930c3cd
julie-sullivan <julie@flymine.org>
2007-12-07 13:55:00 +0000

    error correction interface.

---
+++ intermine/web/main/src/org/intermine/web/logic/widget/ErrorCorrection.java
@@ -0,0 +1,30 @@
+package org.intermine.web.logic.widget;
+
+/* 
+ * Copyright (C) 2002-2007 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Julie Sullivan
+ */
+public interface ErrorCorrection
+{
+    /**
+     * @param maxValue maximum value to display
+     */
+    public void calculate(Double maxValue);
+    
+    /**
+     * @return adjusted map
+     */
+    public HashMap getAdjustedMap();
+}
