6d3a10cdb308ab707420b0f245e76f62f510177b
julie-sullivan <julie@flymine.org>
2007-12-20 10:30:24 +0000

    checkstyle
    Former-commit-id: f6e5e3f5b91b8129aaf1eb1fedf33aa65c534964

--- flymine/webapp/src/org/flymine/web/widget/FlyFishGraphURLGenerator.java
+++ flymine/webapp/src/org/flymine/web/widget/FlyFishGraphURLGenerator.java
@@ -17,6 +17,7 @@ import org.intermine.objectstore.query.ConstraintOp;
 
 import org.intermine.metadata.Model;
 import org.intermine.objectstore.ObjectStore;
+import org.intermine.path.Path;
 import org.intermine.web.logic.bag.InterMineBag;
 import org.intermine.web.logic.query.Constraint;
 import org.intermine.web.logic.query.MainHelper;
@@ -77,7 +78,7 @@ public class FlyFishGraphURLGenerator implements GraphCategoryURLGenerator
         Model model = os.getModel();
         PathQuery q = new PathQuery(model);
         
-        List view = new ArrayList();
+        List<Path> view = new ArrayList<Path>();
         view.add(MainHelper.makePath(model, q, "Gene.identifier"));
         view.add(MainHelper.makePath(model, q, "Gene.organismDbId"));
         view.add(MainHelper.makePath(model, q, "Gene.name"));
