3913a8ccaee7bdd2b5a42398d8b27f53bf0af7b8
kmr <kmr>
2007-12-31 16:02:03 +0000

    Re-write of BruiseControl in Perl.
    Former-commit-id: 091e90f4d9e01dd44cd8c8caaafcc242768b5b76

--- flymine-private/scripts/bruisecontrol/update_and_full_test.sh
+++ flymine-private/scripts/bruisecontrol/update_and_full_test.sh
@@ -1,224 +1,391 @@
-#!/bin/sh -
+#!/usr/bin/perl -w
 
-RUNNING_FILE=~/public_html/tests/.running
-TRUNK_DIR=~/public_html/tests/trunk
-BUILD_PROJ=$TRUNK_DIR/intermine/all
-ARCHIVED_DIR=~/public_html/tests/archived
-TIME_STAMP=`date '+%y%m%d_%H%M'`
-ARCHIVE_TO=$ARCHIVED_DIR/$TIME_STAMP
-STD_ERR=~/public_html/tests/stderr.log
-JUNIT_FAIL_FILE=~/public_html/tests/previous_junit_failures
-URL_PREFIX='http://bc.flymine.org'
-ANT_COMMAND='ant -lib /software/noarch/junit/'
+use strict;
 
-. ~/.bashrc
+use Date::Manip;
+use File::Path;
+use Email::Simple;
 
-echo "trying new build"
+my $debug = 0;
+
+if (@ARGV > 0) {
+  if ($ARGV[0] eq '-d') {
+    $debug = 1;
+  } else {
+    die "unknown args: @ARGV\n";
+  }
+}
+
+# time in minutes between updates
+my $UPDATE_TIME = 10;
+my @TEST_PROJECTS = qw(bio-fulldata-test bio-test webservice-test unittest
+                       notxmltest truncunittest fulldatatest flatmodetest genomictest);
+
+my $RUNNING_FILE="$ENV{'HOME'}/public_html/tests/.running";
+my $TRUNK_DIR="$ENV{'HOME'}/public_html/tests/trunk";
+my $BUILD_PROJ="$TRUNK_DIR/intermine/all";
+my $ARCHIVED_DIR="$ENV{'HOME'}/public_html/tests/archived";
+my $TIME_STAMP=UnixDate("now", "%O");
+my $ARCHIVE_TO="$ARCHIVED_DIR/$TIME_STAMP";
+my $PREVIOUS_JUNIT_FAIL_FILE="$ENV{'HOME'}/public_html/tests/previous_junit_failures";
+my $URL_PREFIX="http://bc.flymine.org";
+my $ANT_COMMAND="ant -v -lib /software/noarch/junit/";
+my $JUNIT_FAIL_FILE_NAME = "/tmp/bc_$$.junit_failures";
+my $BRUISER_EMAIL = 'bruiser@flymine.org';
+
+print "trying new build\n";
 
 # -------------------------------------------------------------------------- #
 # Check whether we really need to do anything at all
 # -------------------------------------------------------------------------- #
 
-if [ -f "$RUNNING_FILE" ]; then
-  echo `date`: Not starting tests because $RUNNING_FILE exists
-  exit
-fi
+sub now
+{
+  return UnixDate("now", "%g");
+}
 
-cd "$TRUNK_DIR"
-svn status -u | grep '^.......\*'
-
-if [ $? -eq 1 -a "$#" -eq "0" ]; then
-  echo "*** $TIME_STAMP no updates in repository ***"
+if (-f "$RUNNING_FILE" ) {
+  print now(), ": Not starting tests because $RUNNING_FILE exists\n";
   exit
-fi
+}
+
+chdir $TRUNK_DIR or die "couldn't change to $TRUNK_DIR\n";
+
+my $updates_available = 0;
+
+{
+  open my $svn_status, "svn status -u |" or die "failed to open pipe to svn: $!\n";
+
+  while (my $stat_line = <$svn_status>) {
+    if ($stat_line =~ /^\s+\*\s+\d+/) {
+      $updates_available = 1;
+    }
+  }
+
+  close $svn_status or die "couldn't close pipe to svn: $!\n";
+}
+
+$updates_available = 1;   # XXX
+
+if (!$updates_available) {
+  print "*** ", now(), " no updates in repository ***\n";
+  exit(0);
+}
 
 # -------------------------------------------------------------------------- #
 # Check the last update was at least 10 minutes ago
 # -------------------------------------------------------------------------- #
 
-LAST_CHANGE=$(svn log --revision HEAD | perl -ne 'print "$1" if /.*\|.*\| (.*) \|/')
-LAST_CHANGE=`date -d "$LAST_CHANGE" +%s`
-DIFF=$((`date +%s`-$LAST_CHANGE))
-echo "$DIFF seconds since last change"
+my $last_change = undef;
 
-if [ $DIFF -lt $((60*10)) ]; then
-  echo "`date`: Need to wait $((60*10-$DIFF)) more seconds"
-  if [ "$#" -eq "0" ]; then
-  exit
-:
-  fi
-else
-  echo "`date`: 10 minutes have pasted since last update - lets do it..."
-  date
-fi
+{
+  open my $svn_log, "svn log --revision HEAD|" or die "can't open pipe to svn: $!\n";
 
-touch "$RUNNING_FILE"
+  while (my $log_line = <$svn_log>) {
+    if ($log_line =~ m/.*\|.*\|\s+(.*)\s+\+\d+ \(.*\)\s+\|/) {
+      $last_change = UnixDate(ParseDate($1), "%s");
+    }
+  }
+
+  close $svn_log or die "couldn't close pipe to svn: $!\n";
+}
+
+my $diff = UnixDate("now", "%s") - $last_change;
+print "$diff seconds since last change\n";
+
+if ($diff < 60 * $UPDATE_TIME) {
+  print now(), ": Need to wait ", (60 * $UPDATE_TIME - $diff), " more seconds\n";
+  exit(0);
+} else {
+  print now(), ": $UPDATE_TIME minutes have pasted since last update - starting tests\n";
+}
+
+{
+  # create empty file
+  open my $run_file, ">$RUNNING_FILE" or die "failed to create $RUNNING_FILE: $!\n";
+  close $run_file or die "failed to close $RUNNING_FILE: $!\n";
+}
 
 # -------------------------------------------------------------------------- #
 # Find out who to blame when things go wrong
 # -------------------------------------------------------------------------- #
 
-NEXT=$(($(svn info | grep "Revision" | cut -c 11-)+1))
-BLAME=`svn log -r $NEXT:HEAD | grep '^r[0-9]' | awk '{print $3;}' | sort | uniq`
-BLAME=`echo $BLAME | sed 's/ /@flymine.org, /g'`@flymine.org
-echo "BLAME = $BLAME"
+my $local_version = undef;
 
-umask 0022
-mkdir -p "$ARCHIVE_TO"
+{
+  open my $svn_info, "svn info|" or die "can't open pipe to svn: $!\n";
 
-echo "trying new build" > $ARCHIVE_TO/ant_log.txt 2>&1
+  while (my $info_line = <$svn_info>) {
+    if ($info_line =~ /^Revision: (\d+)/) {
+      $local_version = $1;
+    }
+  }
 
-UPDATE=$(svn update svn://svn.flymine.org/flymine/trunk $TRUNK_DIR)
+  close $svn_info or die "couldn't close pipe to svn: $!\n";
+}
 
-cd $BUILD_PROJ
-ant clean-all >> $ARCHIVE_TO/ant_log.txt 2>&1
+if (!defined $local_version) {
+  die "can't find the local revision in the output of svn info\n";
+}
 
-if [ "$#" -eq "0" ]; then
-  TARGET=fulltest
-else
-  TARGET=$1
-fi
+print "local version: $local_version\n";
 
-for i in bio-fulldata-test bio-test webservice-test unittest testmodel-webapp-userprofile testmodel-webapp notxmltest truncunittest fulldatatest flatmodetest genomictest
-do
-  dropdb $i
-  createdb $i
-done >> $ARCHIVE_TO/ant_log.txt 2>&1
+my @blame = ('kmr');
 
-cd ../../testmodel/dbmodel
-ant build-db >> $ARCHIVE_TO/ant_log.txt 2>&1
+{
+  warn "svn log -r $local_version:HEAD\n";
+  open my $svn_log, "svn log -r $local_version:HEAD|" or die "can't open pipe to svn: $!\n";
 
+  while (my $line = <$svn_log>) {
+    if ($line =~ /^r\d+ \| (\S+)/) {
+      if (!grep {$_ eq $1} @blame) {
+        push @blame, $1;
+      }
+    }
+  }
+
+  close $svn_log or die "couldn't close pipe to svn: $!\n";
+}
+
+if (@blame == 0) {
+  die "couldn't find any user names in svn log output - noone to blame\n";
+}
+
+@blame = map { $_ . '@flymine.org' } @blame;
+
+print "BLAME = @blame\n";
+
+mkpath ($ARCHIVE_TO, {verbose => 1});
+
+my $ANT_LOG_NAME = "$ARCHIVE_TO/ant_log.txt";
+open my $ant_log, '>', $ANT_LOG_NAME or die "can't open log file ($ANT_LOG_NAME): $!\n";
+
+sub log_and_print
+{
+  my $message = shift;
+  print $message, "\n";
+  print $ant_log $message, "\n";
+}
+
+log_and_print ("trying new build");
+log_and_print ("updating ...");
+
+my $update_output = "";
+
+# {
+#   open my $svn_update, "svn update|" or die "couldn't open pipe to svn: $!\n";
+
+#   while (my $update_line = <$svn_update>) {
+#     $update_output .= $update_line;
+#   }
+
+#   close $svn_update or die "couldn't close pipe to svn: $!\n";
+# }
+
+sub pipe_to_log
+{
+  my $arg = shift;
+
+  my @commands = ();
+
+  if (ref $arg) {
+    @commands = @$arg;
+  } else {
+    push @commands, $arg;
+  }
+
+  for my $command (@commands) {
+    log_and_print ("opening pipe to $command");
+
+    open my $pipe, "$command 2>&1|" or die "can't open pipe to $command: $!\n";
+
+    while (my $line = <$pipe>) {
+      print STDERR $line if $debug;
+      print $ant_log $line;
+    }
+
+    close $pipe or die "can't close pipe to $command: $!\n";
+  }
+
+  return $?;
+}
+
+log_and_print ("cleaning ...");
+pipe_to_log("cd $BUILD_PROJ; ant clean-all");
+
+log_and_print ("creating databases ...");
+for my $test_project (@TEST_PROJECTS) {
+  pipe_to_log ("dropdb $test_project");
+  pipe_to_log ("createdb $test_project");
+}
+
+# the "true" is beacuse the dropdb will fail if /intermine-query webapp exists
+pipe_to_log("dropdb testmodel-webapp; createdb testmodel-webapp; true");
+pipe_to_log("dropdb testmodel-webapp-userprofile; createdb testmodel-webapp-userprofile; true");
+
+
+log_and_print ("testmodel build-db ...");
+pipe_to_log("cd testmodel/dbmodel; ant build-db");
 
 # intermine tests
 
-cd $BUILD_PROJ
-date; $ANT_COMMAND default >> $ARCHIVE_TO/ant_log.txt 2>&1
-BUILD_RESULT=$?
+log_and_print ("intermine fulltest ...");
+my $build_result = 0; #pipe_to_log("cd $BUILD_PROJ; date; $ANT_COMMAND fulltest");
 
-#date; $ANT_COMMAND $TARGET >> $ARCHIVE_TO/ant_log.txt 2>&1
-#date; $ANT_COMMAND test-report >> $ARCHIVE_TO/ant_log.txt 2>&1
+log_and_print ("intermine test-report ...");
+#pipe_to_log("cd $BUILD_PROJ; date; $ANT_COMMAND test-report");
 
 # testmodel webapp tests
 
-cd $BUILD_PROJ/../../testmodel/webapp/test
-(cd ../../dbmodel; $ANT_COMMAND build-db >> $ARCHIVE_TO/ant_log.txt)
-ant clean
-date; $ANT_COMMAND >> $ARCHIVE_TO/ant_log.txt 2>&1
-date; $ANT_COMMAND test-report >> $ARCHIVE_TO/ant_log.txt
+pipe_to_log(["(cd $TRUNK_DIR/testmodel/dbmodel; $ANT_COMMAND build-db)",
+            "cd $TRUNK_DIR/testmodel/webapp/test; ant clean",
+            "cd $TRUNK_DIR/testmodel/webapp/test; date; $ANT_COMMAND",
+            "cd $TRUNK_DIR/testmodel/webapp/test; date; $ANT_COMMAND test-report"]);
 
 # bio tests
 
-cd $BUILD_PROJ/../../bio/test-all
-(cd dbmodel; $ANT_COMMAND build-db >> $ARCHIVE_TO/ant_log.txt)
-ant clean
-date; $ANT_COMMAND fulltest >> $ARCHIVE_TO/ant_log.txt 2>&1
-date; $ANT_COMMAND test-report >> $ARCHIVE_TO/ant_log.txt
+# pipe_to_log(["(cd dbmodel; $ANT_COMMAND build-db)",
+#             "cd $TRUNK_DIR/bio/test-all; ant clean",
+#             "cd $TRUNK_DIR/bio/test-all; date; $ANT_COMMAND fulltest",
+#             "cd $TRUNK_DIR/bio/test-all; date; $ANT_COMMAND test-report"]);
 
 
-cd $BUILD_PROJ
-grep '\[junit]' $ARCHIVE_TO/ant_log.txt | grep FAILED | tee $ARCHIVE_TO/junit_failures.txt
-TEST_RESULT=$?
-grep 'BUILD FAILED' $STD_ERR
-BUILD_BROKEN_STATUS=$?
-echo "BUILD_RESULT=$BUILD_RESULT  TEST_RESULT=$TEST_RESULT"
+my @junit_failures = ();
 
+close $ant_log or die "can't close $ANT_LOG_NAME: $!\n";
 
-ant checkstyle >> $ARCHIVE_TO/ant_log.txt
-CHECKSTYLE_RESULT=$?
+open $ant_log, '>', $ANT_LOG_NAME or die "can't open log file ($ANT_LOG_NAME): $!\n";
+open my $junit_fail_file, ">$JUNIT_FAIL_FILE_NAME";
 
-if [ $BUILD_RESULT -eq 0 ]; then
-  echo '*** BUILD SUCCESS ***'
-else
-  echo '*** build failed - see log for errors ***'
-fi
+my $build_failed = 0;
+my $test_failures = 0;
 
-if [ $BUILD_BROKEN_STATUS -ne 0 ]; then
-  echo '*** TESTS COMPILED ***'
-else
-  echo '*** tests failed to compile - see log for errors ***'
-fi
+{
+  while (my $ant_log_line = <$ant_log>) {
+    if ($ant_log_line =~ /\[junit\].*FAILED/ ) {
+      print $ant_log_line;
+      print $junit_fail_file $ant_log_line;
+      push @junit_failures, $ant_log_line;
+      ++$test_failures;
+    } else {
+      if ($ant_log_line =~ /BUILD FAILED/) {
+        $build_failed = 1;
+      }
+    }
+  }
+}
 
-if [ $TEST_RESULT -ne 0 ]; then
-  echo '*** TEST SUCCESS ***'
-else
-  echo '*** tests failed - see log for errors ***'
-fi
+my $checkstyle_exitcode = pipe_to_log("cd $BUILD_PROJ; ant checkstyle");
 
-if [ $BUILD_BROKEN_STATUS -ne 0 ]; then
-  BUILD_BROKEN=0
-else
-  BUILD_BROKEN=1
-fi
+if ($build_failed) {
+  print "*** build failed - see log for errors ***\n";
+} else {
+  print "*** BUILD SUCCESS ***\n";
+}
 
-if [ $CHECKSTYLE_RESULT -ne 0 ]; then
-    CHECKSTYLE_STATUS=" (AND CHECKSTYLE FAILURES)"
-else
-    CHECKSTYLE_STATUS=""
-fi
+if ($test_failures == 0) {
+  print "*** TEST SUCCESS ***\n";
+} else {
+  print "*** $test_failures tests failed - see log for errors ***\n";
+}
 
-if [ -f $BUILD_PROJ/build/test/results/index.html ]; then
-  mkdir $ARCHIVE_TO/junit
-  cp -R $BUILD_PROJ/build/test/results/* $ARCHIVE_TO/junit/
-else
-  echo "There don't seem to be any intermine results"
-fi
+if ($checkstyle_exitcode) {
+  print "*** CHECKSTYLE FAILURES ***\n";
+} else {
+  print "*** NO CHECKSTYLE FAILURES ***\n";
+}
 
-TESTMODEL_RESULTS=$BUILD_PROJ/../../testmodel/webapp/test/build/test/results
-if [ -f $TESTMODEL_RESULTS/index.html ]; then
-  mkdir $ARCHIVE_TO/junit_testmodel
-  cp -R $BIO_RESULTS/* $ARCHIVE_TO/junit_testmodel
-else
-  echo "There don't seem to be any testmodel results"
-fi
+if (-f "$BUILD_PROJ/build/test/results/index.html") {
+  mkpath "$ARCHIVE_TO/junit" or die "can't mkpath $ARCHIVE_TO/junit: $!\n";
+  system "cp -r $BUILD_PROJ/build/test/results/* $ARCHIVE_TO/junit/";
+} else {
+  print "There don't seem to be any intermine results\n";
+}
 
-BIO_RESULTS=$BUILD_PROJ/../../bio/test-all/build/test/results
-if [ -f $BIO_RESULTS/index.html ]; then
-  mkdir $ARCHIVE_TO/junit_bio
-  cp -R $BIO_RESULTS/* $ARCHIVE_TO/junit_bio
-else
-  echo "There don't seem to be any bio results"
-fi
+my $testmodel_results_dir = "$BUILD_PROJ/../../testmodel/webapp/test/build/test/results";
+if (-f "$testmodel_results_dir/index.html") {
+  mkpath "$ARCHIVE_TO/junit_testmodel" or die "can't mkpath $ARCHIVE_TO/junit_testmodel: $!\n";
+  system "cp -r $testmodel_results_dir/* $ARCHIVE_TO/junit_testmodel";
+} else {
+  print "There don't seem to be any testmodel results\n";
+}
 
+my $bio_results_dir = "$BUILD_PROJ/../../bio/test-all/build/test/results";
+if (-f "$bio_results_dir/index.html") {
+  mkpath "$ARCHIVE_TO/junit_bio" or die "can't mkpath $ARCHIVE_TO/junit_bio: $!\n";
+  system "cp -r $bio_results_dir/* $ARCHIVE_TO/junit_bio";
+} else {
+  print "There don't seem to be any bio results\n";
+}
 
-if [ -f $BUILD_PROJ/build/checkstyle/index.html ]; then
-  mkdir $ARCHIVE_TO/checkstyle
-  cp -R $BUILD_PROJ/build/checkstyle/* $ARCHIVE_TO/checkstyle/
-else
-  echo "There don't seem to be any checkstyle results!"
-fi
+if (-f "$BUILD_PROJ/build/checkstyle/index.html") {
+  mkpath "$ARCHIVE_TO/checkstyle" or die "can't mkpath $ARCHIVE_TO/checkstyle: $!\n";
+  system "cp -r $BUILD_PROJ/build/checkstyle/* $ARCHIVE_TO/checkstyle/";
+} else {
+  print "There don't seem to be any checkstyle results\n";
+}
 
 # Blame people via email
-FAILED=$?
 
-touch $JUNIT_FAIL_FILE
-echo "Emailing $BLAME..."
-printf "JUnit results: $URL_PREFIX/$TIME_STAMP/junit/\n\n" > MSG
-printf "               $URL_PREFIX/$TIME_STAMP/junit_testmodel/\n\n" >> MSG
-printf "               $URL_PREFIX/$TIME_STAMP/junit_bio/\n\n" >> MSG
-printf "Checkstyle results: $URL_PREFIX/$TIME_STAMP/checkstyle/\n\n" >> MSG
-printf "Ant output: $URL_PREFIX/$TIME_STAMP/ant_log.txt\n\n" >> MSG
-printf "Last update:\n\n" >> MSG
-echo $UPDATE >> MSG
-printf "\n\n------------------------------------------------------------\nTest failures now:\n\n" >> MSG
-cat $ARCHIVE_TO/junit_failures.txt >> MSG
-printf "\n\n------------------------------------------------------------\nPrevious test failures:\n\n" >> MSG
-cat $JUNIT_FAIL_FILE >> MSG
-printf "\n\n------------------------------------------------------------\nTest differences:\n\n" >> MSG
-diff $JUNIT_FAIL_FILE $ARCHIVE_TO/junit_failures.txt >> MSG
-printf "\n\n------------------------------------------------------------\nstderr output:\n\n" >> MSG
+my $subject = "[BruiseControl] SUCCESS at " . now();
 
-if [ $BUILD_BROKEN -eq 1 -a $FAILED -ne 0 ]; then
-  cat MSG | mail -s "[BruiseControl] Build BROKEN at $TIME_STAMP $CHECKSTYLE_STATUS" "$BLAME"
-elif [ $FAILED -eq 0 ]; then
-  cat MSG | mail -s "[BruiseControl] Tests FAILING at $TIME_STAMP $CHECKSTYLE_STATUS" "$BLAME"
-else
-  cat MSG | mail -s "[BruiseControl] SUCCESS at $TIME_STAMP $CHECKSTYLE_STATUS" "$BLAME"
-fi
-    
+if ($build_failed) {
+  $subject = "[BruiseControl] Build BROKEN at $TIME_STAMP";
+} else {
+  if ($test_failures > 0) {
+    $subject = "[BruiseControl] $test_failures tests FAILING at $TIME_STAMP";
+  }
+}
+
+my $recipients = join ', ', @blame;
+
+my $message = <<"__MESSAGE__";
+To: $recipients
+From: Bruise Control <$BRUISER_EMAIL>
+Subject: $subject
+
+JUnit results: $URL_PREFIX/$TIME_STAMP/junit/
+               $URL_PREFIX/$TIME_STAMP/junit_testmodel/
+               $URL_PREFIX/$TIME_STAMP/junit_bio/\n\n
+Checkstyle results: $URL_PREFIX/$TIME_STAMP/checkstyle/
+Ant output: $URL_PREFIX/$TIME_STAMP/ant_log.txt
+Last update:
+$update_output
+
+------------------------------------------------------------
+Test failures now:
+
+@junit_failures
+
+------------------------------------------------------------
+Previous test failures:
+
+   @ previous_junit_failures
+
+------------------------------------------------------------
+Test differences:
+__MESSAGE__
+
+{
+  open my $diff_pipe, "diff $PREVIOUS_JUNIT_FAIL_FILE $JUNIT_FAIL_FILE_NAME"
+    or die "can't open diff pipe\n";
+
+  while (my $diff_line = <$diff_pipe>) {
+    $message .= $diff_line;
+  }
+
+  close $diff_pipe or die "can't close diff pipe\n";
+}
+
+my $sender = Email::Send->new({mailer => 'SMTP'});
+$sender->mailer_args([Host => 'mail.flymine.org']);
+$sender->send($message);
+
 # Update previous failures file
-cat $ARCHIVE_TO/junit_failures.txt > $JUNIT_FAIL_FILE
+#cat $ARCHIVE_TO/junit_failures.txt > $JUNIT_FAIL_FILE
 
-TIME_NOW=`date '+%y-%m-%d_%H:%M'`
-echo "*** Finished build $TIME_STAMP at $TIME_NOW ***"
+print "*** Finished build $TIME_STAMP at ", now(), " ***\n";
 
-rm "$RUNNING_FILE"
+close $ant_log or die "can't close $ANT_LOG_NAME: $!\n";
+
+unlink $RUNNING_FILE or die "can't unlink $RUNNING_FILE: $!\n";
