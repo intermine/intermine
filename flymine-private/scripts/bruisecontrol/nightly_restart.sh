1659bc858f27feadfbf501375ed66b8de4227e5a
kmr <kmr>
2008-01-02 13:40:43 +0000

    Fixed BruiseControl nightly restart script.
    Former-commit-id: 15e5b5a17fcb796dee45948d4d49328a2d97fb8e

--- flymine-private/scripts/bruisecontrol/nightly_restart.sh
+++ flymine-private/scripts/bruisecontrol/nightly_restart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/home/bruiser/apache-tomcat-5.5.16/bin/shutdown.sh
-sleep 15
-/home/bruiser/apache-tomcat-5.5.16/bin/startup.sh
+/home/bruiser/apache-tomcat-5.5.25/bin/shutdown.sh
+sleep 30
+/home/bruiser/apache-tomcat-5.5.25/bin/startup.sh
