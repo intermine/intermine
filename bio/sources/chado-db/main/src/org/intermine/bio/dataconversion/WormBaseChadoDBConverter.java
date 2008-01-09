bf29cf15d5d36821e77abf26bbe238f81037b9bb
kmr <kmr>
2008-01-09 15:50:33 +0000

    Added a WormBaseChadoDBConverter. Fixed WormBase loading from chado so that both the organismDbId and identifier fields of Genes are filled in, which should help the Gene objects merge with uniprot and inparanoid-orthologue.

---
+++ bio/sources/chado-db/main/src/org/intermine/bio/dataconversion/WormBaseChadoDBConverter.java
@@ -0,0 +1,90 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2007 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.intermine.dataconversion.ItemWriter;
+import org.intermine.metadata.Model;
+import org.intermine.sql.Database;
+
+import org.apache.commons.collections.keyvalue.MultiKey;
+import org.apache.commons.collections.map.MultiKeyMap;
+
+/**
+ * A converter for chado that handles WormBase specific configuration.
+ * @author Kim Rutherford
+ */
+public class WormBaseChadoDBConverter extends ChadoDBConverter
+{
+    private MultiKeyMap config;
+
+    /**
+     * Create a new WormBaseChadoDBConverter.
+     * @param database the Database object representing the chado db
+     * @param tgtModel the target Model
+     * @param writer the ItemWriter
+     */
+    public WormBaseChadoDBConverter(Database database, Model tgtModel, ItemWriter writer) {
+        super(database, tgtModel, writer);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    protected Map<MultiKey, List<ConfigAction>> getConfig() {
+       if (config == null) {
+           config = new MultiKeyMap();
+           config.put(new MultiKey("feature", "Gene", "WormBase", "uniqueName"),
+                      Arrays.asList(new SetFieldConfigAction("organismDbId"),
+                                    new SetFieldConfigAction("identifier"),
+                                    CREATE_SYNONYM_ACTION));
+       }
+
+       return config;
+    }
+
+    private static final List<String> FEATURES = Arrays.asList(
+       "gene", "mRNA", "transcript", "CDS", "intron", "exon", "five_prime_untranslated_region",
+       "five_prime_UTR", "three_prime_untranslated_region", "three_prime_UTR"
+    );
+
+    /**
+     * Get a list of the chado/so types of the LocatedSequenceFeatures we wish to load.  The list
+     * will not include chromosome-like features.
+     * @return the list of features
+     */
+    @Override
+    protected List<String> getFeatures() {
+        return FEATURES;
+    }
+    /**
+     * Process the identifier and return a "cleaned" version.  For WormBase, remove the class name
+     * prefix on identifiers from the uniqueName in the feature table ("Gene:WBGene00023466" ->
+     * "WBGene00023466")
+     * @param type the InterMine type of the feature that this identifier came from
+     * @param identifier the identifier
+     * @return a cleaned identifier
+     */
+    @Override
+    protected String fixIdentifier(String type, String identifier) {
+        if (identifier.startsWith(type + ":")) {
+            return identifier.substring(type.length() + 1);
+        } else {
+            return identifier;
+        }
+    }
+
+}
