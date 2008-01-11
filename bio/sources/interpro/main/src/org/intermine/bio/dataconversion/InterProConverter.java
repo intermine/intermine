4f34591c7972708d9e99f91a339f51e0c0b97860
julie-sullivan <julie@flymine.org>
2008-01-11 10:51:45 +0000

    new converter for interpro.  Fixes #997.
    Note I'm putting the dtd file in flymine/integrate.  We obviously need a better solution, but I'm not sure what to do.
    
    
    
    Former-commit-id: 1dcb5389e87a6a4321dac47df6faacba1e150884

---
+++ bio/sources/interpro/main/src/org/intermine/bio/dataconversion/InterProConverter.java
@@ -0,0 +1,431 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2007 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+import org.intermine.dataconversion.FileConverter;
+import org.intermine.dataconversion.ItemWriter;
+import org.intermine.metadata.Model;
+import org.intermine.objectstore.ObjectStoreException;
+import org.intermine.util.SAXParser;
+import org.intermine.xml.full.Attribute;
+import org.intermine.xml.full.Item;
+import org.intermine.xml.full.ItemFactory;
+import org.intermine.xml.full.ItemHelper;
+import org.intermine.xml.full.Reference;
+import org.intermine.xml.full.ReferenceList;
+
+import java.io.Reader;
+
+import org.apache.log4j.Logger;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+/**
+ * DataConverter to parse InterPro data into items
+ * @author Julie Sullivan
+ */
+public class InterProConverter extends FileConverter
+{
+    private Map<String, Object> mapMaster = new HashMap<String, Object>(); 
+    protected static final String GENOMIC_NS = "http://www.flymine.org/model/genomic#";
+    private static final Logger LOG = Logger.getLogger(InterProConverter.class);
+    private Map<String, Item> pubMaster = new HashMap<String, Item>();
+    private Map<String, Item> dbMaster = new HashMap<String, Item>();
+    private Map<String, Item> dsMaster = new HashMap<String, Item>();
+    private Map<String, Item> proteinDomains = new HashMap<String, Item>();
+    
+    private Map ids = new HashMap();
+    private Map aliases = new HashMap();
+
+    /**
+     * Constructor
+     * @param writer the ItemWriter used to handle the resultant items
+     * @param model the Model
+     */
+    public InterProConverter(ItemWriter writer, Model model) {
+        super(writer, model);
+    }
+
+    /**
+     * @see FileConverter#process(Reader)
+     */
+    public void process(Reader reader) throws Exception {
+        mapMaps();
+        InterProHandler handler = new InterProHandler(getItemWriter(), mapMaster);
+
+        try {
+            SAXParser.parse(new InputSource(reader), handler);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void mapMaps() {
+        mapMaster.put("pubMaster", pubMaster);
+        mapMaster.put("dbMaster", dbMaster);
+        mapMaster.put("dsMaster", dsMaster);
+        mapMaster.put("ids", ids);
+        mapMaster.put("aliases", aliases);
+        mapMaster.put("proteinDomains", proteinDomains);
+    }
+
+
+    class InterProHandler extends DefaultHandler {
+
+        private Item proteinDomain;      
+        private Map synonyms;
+        private StringBuffer description;
+        private ReferenceList pubCollection;
+        private ReferenceList parentsCollection;
+        private ReferenceList kidsCollection;
+        private ReferenceList foundInCollection;
+        private ReferenceList containsCollection;
+
+        private Item domainRelationships;
+        private Map<String, Item> pubMaster;
+        private Map<String, Item> dbMaster;
+        private Map<String, Item> dsMaster;
+        private Map<String, Item> proteinDomains;
+        private Item datasource;
+        private Item dataset;
+
+        private int nextClsId = 0;
+        private ItemFactory itemFactory;
+        private Map<String, Integer> ids;
+        private Map<String, String> aliases;
+        private ItemWriter writer;
+        private Stack stack = new Stack();
+        private String attName = null;
+        private StringBuffer attValue = null;
+
+        private ArrayList<Item> delayedItems = new ArrayList();
+
+        /**
+         * Constructor
+         * @param writer the ItemWriter used to handle the resultant items
+         * @param mapMaster the Map of maps
+         * @param createInterpro whether or not to create interpro items
+         */
+        public InterProHandler(ItemWriter writer, Map mapMaster) {
+
+            itemFactory = new ItemFactory(Model.getInstanceByName("genomic"));
+            this.writer = writer;
+            this.proteinDomains = (Map) mapMaster.get("proteinDomains");
+            this.pubMaster = (Map) mapMaster.get("pubMaster");
+            this.dbMaster = (Map) mapMaster.get("dbMaster");
+            this.dsMaster = (Map) mapMaster.get("dsMaster");
+            this.ids = (Map) mapMaster.get("ids");
+            this.aliases = (Map) mapMaster.get("aliases");
+        }
+
+
+        /**
+         * {@inheritDoc}
+         */
+        public void startElement(String uri, String localName, String qName, Attributes attrs)
+        throws SAXException {
+            if (attName != null && !attName.equals("description")) { 
+                attName = null;
+            }
+
+            // <interpro id="IPR000002" type="Domain" short_name="Fizzy" protein_count="256">
+            if (qName.equals("interpro")) {
+
+                String identifier = attrs.getValue("id");                    
+                initProteinDomain(identifier);                   
+                proteinDomain.setAttribute("type", attrs.getValue("type"));
+                proteinDomain.setAttribute("shortName", attrs.getValue("short_name"));
+
+                // <publication><db_xref db="PUBMED" dbkey="8606774" />
+            }  else if (qName.equals("db_xref") && stack.peek().equals("publication")) {
+
+                String identifier = attrs.getValue("dbkey");
+                Item pub = getAndStoreItem(pubMaster, identifier, "Publication", "pubMedId");
+
+                if (pubCollection.getRefIds().isEmpty()) {
+                    proteinDomain.addCollection(pubCollection);
+                }
+                pubCollection.addRefId(pub.getIdentifier());
+
+                // <interpro><name>
+            } else if (qName.equals("name") && stack.peek().equals("interpro")) {
+
+                attName = "name";
+
+                // <interpro><abstract>
+            } else if (qName.equals("abstract") && stack.peek().equals("interpro")) {
+
+                attName = "description";
+                
+            } else if (qName.equals("sec_ac")) {
+
+                createSynonym(proteinDomain.getIdentifier(), "identifier",
+                              attrs.getValue("acc"), datasource.getIdentifier());
+   
+                //<member_list><db_xref db="PFAM" dbkey="PF01167" name="SUPERTUBBY" />
+            } else if (qName.equals("db_xref") && stack.peek().equals("member_list")) {
+
+                String dbkey = attrs.getValue("dbkey");
+                String name = attrs.getValue("name");
+
+                Item db = getAndStoreItem(dbMaster, attrs.getValue("db"), "DataSource", "name");
+
+                createSynonym(proteinDomain.getIdentifier(), "identifier",
+                              dbkey, db.getIdentifier());
+                createSynonym(proteinDomain.getIdentifier(), "name",
+                              name, db.getIdentifier());
+
+                //<interpro><foundin>
+            } else if (qName.equals("rel_ref") && (stack.peek().equals("found_in")
+                            || stack.peek().equals("contains")
+                            || stack.peek().equals("child_list")
+                            || stack.peek().equals("parent_list"))) {
+
+                String domainRelationship = stack.peek().toString();
+                String interproId = attrs.getValue("ipr_ref");
+                Item domain = getItem(proteinDomains, interproId, "ProteinDomain", "identifier");
+                                
+                ReferenceList relationCollection = null;
+
+                if (domainRelationship.equals("found_in")) {
+                    relationCollection = foundInCollection;
+                } else if (domainRelationship.equals("contains")) {
+                    relationCollection = containsCollection;
+                } else if (domainRelationship.equals("parent_list")) {
+                    relationCollection = parentsCollection;
+                } else if (domainRelationship.equals("child_list")) {
+                    relationCollection = kidsCollection;
+                }
+                if (relationCollection != null) {
+                    if (relationCollection.getRefIds().isEmpty()) {
+                        domainRelationships.addCollection(relationCollection);
+                    }
+                    relationCollection.addRefId(domain.getIdentifier());
+                }
+
+                // <interprodb>
+            }    else if (qName.equals("interprodb")) {
+                initData();
+            }
+
+            super.startElement(uri, localName, qName, attrs);
+            stack.push(qName);
+            attValue = new StringBuffer();
+        }
+
+
+        /**
+         * {@inheritDoc}
+         */
+        public void characters(char[] ch, int start, int length) {
+            if (attName != null) {
+
+                // DefaultHandler may call this method more than once for a single
+                // attribute content -> hold text & create attribute in endElement
+                while (length > 0) {
+                    boolean whitespace = false;
+                    switch(ch[start]) {
+                    case ' ':
+                    case '\r':
+                    case '\n':
+                    case '\t':
+                        whitespace = true;
+                        break;
+                    default:
+                        break;
+                    }
+                    if (!whitespace) {
+                        break;
+                    }
+                    ++start;
+                    --length;
+                }
+
+                if (length > 0) {
+                    StringBuffer s = new StringBuffer();
+                    s.append(ch, start, length);
+                    attValue.append(s);
+                    if (attName.equals("description")) {
+                        description.append(s);
+                    }
+                }
+            }
+        }
+
+
+        /**
+         * {@inheritDoc}
+         */
+        public void endElement(String uri, String localName, String qName)
+        throws SAXException {
+            super.endElement(uri, localName, qName);
+
+            try {
+                stack.pop();
+
+                // <interpro>
+                if (qName.equals("interprodb")) {
+
+                    for (Item item : proteinDomains.values()) {
+                        writer.store(ItemHelper.convert(item));
+                    }
+
+                    for (Item item : delayedItems) {
+                        writer.store(ItemHelper.convert(item));
+                    }
+
+                // <interpro><name>
+                } else if (qName.equals("name") && stack.peek().equals("interpro")) {
+                    
+                    if (attName != null) {                        
+                        proteinDomain.setAttribute(attName, attValue.toString());
+                    }
+                    
+                // <interpro><abstract>
+                } else if (qName.equals("abstract") && stack.peek().equals("interpro")) {
+                    
+                    proteinDomain.setAttribute("description", description.toString());
+                    attName = null;
+                    
+                }
+
+            } catch (ObjectStoreException e) {
+                throw new SAXException(e);
+            }
+
+        }
+        private void initData()
+        throws SAXException    {
+            try {
+                datasource = getAndStoreItem(dbMaster, "InterPro", "DataSource", "name");
+                dataset = getAndStoreItem(dsMaster, "InterPro data set", "DataSet", "title");
+                
+            } catch (Exception e) {
+                throw new SAXException(e);
+            }
+
+        }
+
+        private void createSynonym(String subjectId, String type, String value, String dbId) {
+            String key = value.toLowerCase();
+
+            if (synonyms.get(key) == null) {
+                Item syn = createItem("Synonym");
+                syn.addReference(new Reference("subject", subjectId));
+                syn.addAttribute(new Attribute("type", type));
+                syn.addAttribute(new Attribute("value", value));
+                syn.addReference(new Reference("source", dbId));
+                synonyms.put(key, syn);
+                delayedItems.add(syn);
+            }
+
+        }
+
+        private void initProteinDomain(String identifier) {
+
+            proteinDomain = getItem(proteinDomains, identifier, "ProteinDomain", "identifier");
+            
+            domainRelationships = createItem("DomainRelationship");
+            proteinDomain.setReference("domainRelationships", domainRelationships);
+            delayedItems.add(domainRelationships);
+            
+            pubCollection = new ReferenceList("publications", new ArrayList());
+            parentsCollection = new ReferenceList("parentFeatures", new ArrayList());
+            kidsCollection = new ReferenceList("childFeatures", new ArrayList());
+            foundInCollection = new ReferenceList("foundIn", new ArrayList());
+            containsCollection = new ReferenceList("contains", new ArrayList());            
+
+            synonyms = new HashMap();
+
+            ReferenceList evidenceColl = new ReferenceList("evidence", new ArrayList());
+            proteinDomain.addCollection(evidenceColl);
+            evidenceColl.addRefId(dataset.getIdentifier());
+            
+            description = new StringBuffer();
+
+        }
+
+        private Item getItem(Map map, String identifier, String itemType, String attr) {
+            Item item = (Item) map.get(identifier);
+            if (item == null) {
+                item = createItem(itemType);
+                item.setAttribute(attr, identifier);
+                map.put(identifier, item);
+            }
+            return item;
+        }
+
+        private Item getAndStoreItem(Map map, String identifier, String itemType, String attr)
+        throws SAXException {
+            Item item = (Item) map.get(identifier);
+            try {
+                if (item == null) {
+                    item = createItem(itemType);
+                    item.setAttribute(attr, identifier);
+                    map.put(identifier, item);
+                    if (itemType.equals("DataSet")) {
+                        item.setReference("dataSource", datasource);
+                    }
+                    
+                    writer.store(ItemHelper.convert(item));
+                }
+            } catch (ObjectStoreException e) {
+                throw new SAXException(e);
+            }
+            return item;
+        }
+
+        /**
+         * Convenience method for creating a new Item
+         * @param className the name of the class
+         * @return a new Item
+         */
+        protected Item createItem(String className) {
+            return InterProConverter.this.createItem(className);
+        }
+
+        private String newId(String className) {
+            Integer id = (Integer) ids.get(className);
+            if (id == null) {
+                id = new Integer(0);
+                ids.put(className, id);
+            }
+            id = new Integer(id.intValue() + 1);
+            ids.put(className, id);
+            return id.toString();
+        }
+
+        /**
+         * Uniquely alias a className
+         * @param className the class name
+         * @return the alias
+         */
+        protected String alias(String className) {
+            String alias = (String) aliases.get(className);
+            if (alias != null) {
+                return alias;
+            }
+            String nextIndex = "" + (nextClsId++);
+            aliases.put(className, nextIndex);
+            return nextIndex;
+        }
+    }
+}
