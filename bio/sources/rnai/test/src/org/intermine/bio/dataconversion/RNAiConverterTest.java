2d123688eb409a28e10fb0e0a3ac619223b55b4e
kmr <kmr>
2008-01-23 12:31:31 +0000

    Fixed RNAiConverterTest.

--- bio/sources/rnai/test/src/org/intermine/bio/dataconversion/RNAiConverterTest.java
+++ bio/sources/rnai/test/src/org/intermine/bio/dataconversion/RNAiConverterTest.java
@@ -31,11 +31,11 @@ public class RNAiConverterTest extends ItemsTestCase
     public void testProcess() throws Exception {
         String ENDL = System.getProperty("line.separator");
         String input = "Gene WB ID\tGene Public Name\tPhenotype WB ID\tPhenotype Primary Name\tPhenotype Short Name\tPhenotype Is Observed\tPhenotype Is Not Observed\tPhenotype Penetrance From (%)\tPhenotype Penetrance To (%)\tRNAi WB ID\tRNAi Remark\tPubMed ID" + ENDL
-                + "WBGene00000516\tcki-1\tWBPhenotype0000202\talae_abnormal\t\t1\t\t\t\tWBRNAi00064855\tclone does not match to the reported genomic sequence\t9716524" + ENDL 
+                + "WBGene00000516\tcki-1\tWBPhenotype0000202\talae_abnormal\t\t1\t\t\t\tWBRNAi00064855\tclone does not match to the reported genomic sequence\t9716524" + ENDL
                 + "WBGene00006974\tzen-4\tWBPhenotype0000765\tspindle_elongation_integrity_abnormal_early_emb\tEmb\t\t1\t27\t27\tWBRNAi00063891\t\t9693365";
-        
+
         File file = new File ("wormrnai-final.txt");
-        
+
         MockItemWriter itemWriter = new MockItemWriter(new HashMap());
         RNAiConverter converter = new RNAiConverter(itemWriter,
                                                     Model.getInstanceByName("genomic"));
@@ -46,7 +46,7 @@ public class RNAiConverterTest extends ItemsTestCase
         // uncomment to write out a new target items file
         // writeItemsFile(itemWriter.getItems(), "test/resources/RNAiConverterTest.xml");
 
-        Set expected = readItemSet("resources/RNAiConverterTest.xml");
+        Set expected = readItemSet("RNAiConverterTest.xml");
 
         assertEquals(expected, itemWriter.getItems());
     }
