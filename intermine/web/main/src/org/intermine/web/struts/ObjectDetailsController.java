1e4779aa45ddc2d8d13b1c21c042b6769b16e4e1
jakub <jakub>
2008-01-24 13:01:03 +0000

    New tag that the superuser can apply to hide some references/collections from report pages Fixes #1422.

--- intermine/web/main/src/org/intermine/web/struts/ObjectDetailsController.java
+++ intermine/web/main/src/org/intermine/web/struts/ObjectDetailsController.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.Map.Entry;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -54,28 +55,32 @@ import org.intermine.web.logic.session.SessionMethods;
 import org.intermine.web.logic.tagging.TagTypes;
 
 /**
- * Implementation of <strong>Action</strong> that assembles data for viewing an object.
- *
+ * Implementation of <strong>Action</strong> that assembles data for viewing an
+ * object.
+ * 
  * @author Mark Woodbridge
  * @author Thomas Riley
  */
 public class ObjectDetailsController extends InterMineAction
 {
 
-    protected static final Logger LOG = Logger.getLogger(ObjectDetailsController.class);
+    protected static final Logger LOG = Logger
+            .getLogger(ObjectDetailsController.class);
+    private static final Object HIDDEN_TAG_NAME = "hidden";
 
     /**
      * {@inheritDoc}
      */
-    public ActionForward execute(@SuppressWarnings("unused") ActionMapping mapping,
-                                 @SuppressWarnings("unused") ActionForm form,
-                                 HttpServletRequest request,
-                                 @SuppressWarnings("unused") HttpServletResponse response)
-        throws Exception {
+    public ActionForward execute(@SuppressWarnings("unused")
+    ActionMapping mapping, @SuppressWarnings("unused")
+    ActionForm form, HttpServletRequest request, @SuppressWarnings("unused")
+    HttpServletResponse response) throws Exception {
         HttpSession session = request.getSession();
         ServletContext servletContext = session.getServletContext();
-        ObjectStore os = (ObjectStore) servletContext.getAttribute(Constants.OBJECTSTORE);
-        Map<Integer, DisplayObject> displayObjects = SessionMethods.getDisplayObjects(session);
+        ObjectStore os = (ObjectStore) servletContext
+                .getAttribute(Constants.OBJECTSTORE);
+        Map<Integer, DisplayObject> displayObjects = SessionMethods
+                .getDisplayObjects(session);
 
         String idString = request.getParameter("id");
 
@@ -85,8 +90,10 @@ public class ObjectDetailsController extends InterMineAction
             return null;
         }
 
-        ProfileManager pm = (ProfileManager) servletContext.getAttribute(Constants.PROFILE_MANAGER);
-        String superuser = (String) servletContext.getAttribute(Constants.SUPERUSER_ACCOUNT);
+        ProfileManager pm = (ProfileManager) servletContext
+                .getAttribute(Constants.PROFILE_MANAGER);
+        String superuser = (String) servletContext
+                .getAttribute(Constants.SUPERUSER_ACCOUNT);
 
         DisplayObject dobj = displayObjects.get(id);
         if (dobj == null) {
@@ -101,24 +108,27 @@ public class ObjectDetailsController extends InterMineAction
         }
 
         Map<String, Map> placementRefsAndCollections = new TreeMap<String, Map>();
-        Set aspects = new HashSet((Set) servletContext.getAttribute(Constants.CATEGORIES));
+        Set aspects = new HashSet((Set) servletContext
+                .getAttribute(Constants.CATEGORIES));
 
         Map placementMap = new TreeMap(String.CASE_INSENSITIVE_ORDER);
         placementRefsAndCollections.put("placement:summary", placementMap);
 
-        Set cds = os.getModel().getClassDescriptorsForClass(dobj.getObject().getClass());
+        Set cds = os.getModel().getClassDescriptorsForClass(
+                dobj.getObject().getClass());
 
         Iterator cdIter = cds.iterator();
 
         while (cdIter.hasNext()) {
             ClassDescriptor cd = (ClassDescriptor) cdIter.next();
 
-            // get all placement:summary tags for all refs and collections of this class
-            List placementTags =
-                new ArrayList(pm.getTags("placement:summary", cd.getUnqualifiedName()
-                                         + ".%", "reference", superuser));
-            placementTags.addAll(pm.getTags("placement:summary", cd.getUnqualifiedName() + ".%",
-                                            "collection", superuser));
+            // get all placement:summary tags for all refs and collections of
+            // this class
+            List placementTags = new ArrayList(pm.getTags("placement:summary",
+                    cd.getUnqualifiedName() + ".%", "reference", superuser));
+            placementTags.addAll(pm.getTags("placement:summary", cd
+                    .getUnqualifiedName()
+                    + ".%", "collection", superuser));
 
             Iterator placementTagIter = placementTags.iterator();
 
@@ -129,46 +139,57 @@ public class ObjectDetailsController extends InterMineAction
                 int dotIndex = objectIdentifier.indexOf(".");
                 String fieldName = objectIdentifier.substring(dotIndex + 1);
 
-                placementMap.put(fieldName, dobj.getRefsAndCollections().get(fieldName));
+                placementMap.put(fieldName, dobj.getRefsAndCollections().get(
+                        fieldName));
             }
 
         }
 
-        for (Iterator i = aspects.iterator(); i.hasNext(); ) {
+        for (Iterator i = aspects.iterator(); i.hasNext();) {
             String aspect = (String) i.next();
-            placementRefsAndCollections.put(AspectController.ASPECT_PREFIX + aspect,
-                                            new TreeMap(String.CASE_INSENSITIVE_ORDER));
+            placementRefsAndCollections.put(AspectController.ASPECT_PREFIX
+                    + aspect, new TreeMap(String.CASE_INSENSITIVE_ORDER));
         }
 
         Map miscRefs = new TreeMap(dobj.getRefsAndCollections());
         placementRefsAndCollections.put(SearchRepository.MISC, miscRefs);
 
-        for (Iterator iter = dobj.getRefsAndCollections().entrySet().iterator();
-            iter.hasNext(); ) {
+        for (Iterator iter = dobj.getRefsAndCollections().entrySet().iterator(); iter
+                .hasNext();) {
             Map.Entry entry = (Map.Entry) iter.next();
             DisplayField df = (DisplayField) entry.getValue();
             if (df instanceof DisplayReference) {
-                categoriseBasedOnTags(((DisplayReference) df).getDescriptor(), "reference",
-                                      df, miscRefs, pm, superuser, placementRefsAndCollections);
+                categoriseBasedOnTags(((DisplayReference) df).getDescriptor(),
+                        "reference", df, miscRefs, pm, superuser,
+                        placementRefsAndCollections, SessionMethods
+                                .isSuperUser(session));
             } else if (df instanceof DisplayCollection) {
-                categoriseBasedOnTags(((DisplayCollection) df).getDescriptor(), "collection",
-                                      df, miscRefs, pm, superuser, placementRefsAndCollections);
+                categoriseBasedOnTags(((DisplayCollection) df).getDescriptor(),
+                        "collection", df, miscRefs, pm, superuser,
+                        placementRefsAndCollections, SessionMethods
+                                .isSuperUser(session));
             }
         }
 
-        String publicBagsWithThisObject = getBags(os, session, servletContext, id, true);
-        String myBagsWithThisObject = getBags(os, session, servletContext, id, false);
+        String publicBagsWithThisObject = getBags(os, session, servletContext,
+                id, true);
+        String myBagsWithThisObject = getBags(os, session, servletContext, id,
+                false);
 
-        request.setAttribute("bagsWithThisObject", publicBagsWithThisObject
-                             + ((publicBagsWithThisObject.length() != 0
-                               && myBagsWithThisObject.length() != 0) ? "," :"")
-                             + myBagsWithThisObject);
-        request.setAttribute("placementRefsAndCollections", placementRefsAndCollections);
+        request
+                .setAttribute(
+                        "bagsWithThisObject",
+                        publicBagsWithThisObject
+                                + ((publicBagsWithThisObject.length() != 0 && myBagsWithThisObject
+                                        .length() != 0) ? "," : "")
+                                + myBagsWithThisObject);
+        request.setAttribute("placementRefsAndCollections",
+                placementRefsAndCollections);
 
         Set<Class> cls = DynamicUtil.decomposeClass(object.getClass());
         String type = null;
         for (Class class1 : cls) {
-            type =  class1.getCanonicalName();
+            type = class1.getCanonicalName();
         }
         request.setAttribute("objectType", type);
 
@@ -176,43 +197,67 @@ public class ObjectDetailsController extends InterMineAction
     }
 
     /**
-     * For a given FieldDescriptor, look up its 'aspect:' tags and place it in the correct
-     * map within placementRefsAndCollections. If categorised, remove it from the supplied
-     * miscRefs map.
-     *
+     * For a given FieldDescriptor, look up its 'aspect:' tags and place it in
+     * the correct map within placementRefsAndCollections. If categorised,
+     * remove it from the supplied miscRefs map.
+     * 
      * @param fd the FieldDecriptor (a references or collection)
      * @param taggedType 'reference' or 'collection'
      * @param dispRef the corresponding DisplayReference or DisplayCollection
      * @param miscRefs map that contains dispRef (may be removed by this method)
      * @param pm the ProfileManager
-     * @param sup the superuser account name
+     * @param sup  the superuser account name
      * @param placementRefsAndCollections take from the DisplayObject
+     * @param isSuperUser if current user is superuser
      */
     public static void categoriseBasedOnTags(FieldDescriptor fd,
-                                             String taggedType,
-                                             DisplayField dispRef,
-                                             Map miscRefs,
-                                             ProfileManager pm,
-                                             String sup,
-                                             Map<String, Map> placementRefsAndCollections) {
-        List tags = pm.getTags(null, fd.getClassDescriptor().getUnqualifiedName() + "."
-                               + fd.getName(), taggedType, sup);
-        for (Iterator ti = tags.iterator(); ti.hasNext(); ) {
+            String taggedType, DisplayField dispRef, Map miscRefs,
+            ProfileManager pm, String sup,
+            Map<String, Map> placementRefsAndCollections, boolean isSuperUser) {
+        List tags = pm.getTags(null, fd.getClassDescriptor()
+                .getUnqualifiedName()
+                + "." + fd.getName(), taggedType, sup);
+        for (Iterator ti = tags.iterator(); ti.hasNext();) {
             Tag tag = (Tag) ti.next();
             String tagName = tag.getTagName();
-            if (tagName.startsWith(AspectController.ASPECT_PREFIX)) {
-                Map<String, DisplayField> refs = placementRefsAndCollections.get(tagName);
-                if (refs != null) {
-                    refs.put(fd.getName(), dispRef);
-                    miscRefs.remove(fd.getName());
+            if (!isSuperUser && tagName.equals(HIDDEN_TAG_NAME)) {
+                miscRefs.remove(fd.getName());
+                // Maybe it was added already to some placement and
+                // that's why it must be removed
+                removeField(fd.getName(), placementRefsAndCollections);
+                return;
+            } else {
+                if (tagName.startsWith(AspectController.ASPECT_PREFIX)) {
+                    Map<String, DisplayField> refs = placementRefsAndCollections.get(tagName); 
+                    if (refs != null) {
+                        refs.put(fd.getName(), dispRef);
+                        miscRefs.remove(fd.getName());
+                    }
                 }
             }
         }
     }
 
     /**
-     * The prefix to use before properties the specify which collection fields are open when coming
-     * from the portal page.  eg. portal.verbose.fields.Gene = proteins,chromosome
+     * Removes field from placements.
+     * 
+     * @param name
+     * @param placementRefsAndCollections
+     */
+    private static void removeField(String name,
+            Map<String, Map> placementRefsAndCollections) {
+        Iterator<Entry<String, Map>> it = placementRefsAndCollections
+                .entrySet().iterator();
+        while (it.hasNext()) {
+            Entry<String, Map> entry = it.next();
+            entry.getValue().remove(name);
+        }
+    }
+
+    /**
+     * The prefix to use before properties the specify which collection fields
+     * are open when coming from the portal page. eg. portal.verbose.fields.Gene =
+     * proteins,chromosome
      */
     public static final String PORTAL_VERBOSE_FIELDS_PREFIX = "portal.verbose.fields.";
 
@@ -220,9 +265,11 @@ public class ObjectDetailsController extends InterMineAction
      * Read the port.verbose.fields.* properties from WEB_PROPERTIES and call
      * DisplayObject.setVerbosity(true) on the field in the property value.
      */
-    private static void setVerboseCollections(HttpSession session, DisplayObject dobj) {
+    private static void setVerboseCollections(HttpSession session,
+            DisplayObject dobj) {
         ServletContext servletContext = session.getServletContext();
-        Map webProperties = (Map) servletContext.getAttribute(Constants.WEB_PROPERTIES);
+        Map webProperties = (Map) servletContext
+                .getAttribute(Constants.WEB_PROPERTIES);
 
         Set clds = dobj.getClds();
 
@@ -231,7 +278,8 @@ public class ObjectDetailsController extends InterMineAction
         while (iter.hasNext()) {
             ClassDescriptor cd = (ClassDescriptor) iter.next();
 
-            String propName = PORTAL_VERBOSE_FIELDS_PREFIX + TypeUtil.unqualifiedName(cd.getName());
+            String propName = PORTAL_VERBOSE_FIELDS_PREFIX
+                    + TypeUtil.unqualifiedName(cd.getName());
             String fieldNamesString = (String) webProperties.get(propName);
 
             if (fieldNamesString != null) {
@@ -246,33 +294,37 @@ public class ObjectDetailsController extends InterMineAction
 
     /**
      * Make a new DisplayObject from the given object.
-     * @param session used to get WEB_PROPERTIES and DISPLAYERS Maps
-     * @param object the InterMineObject
+     * 
+     * @param session
+     *            used to get WEB_PROPERTIES and DISPLAYERS Maps
+     * @param object
+     *            the InterMineObject
      * @return the new DisplayObject
-     * @throws Exception if an error occurs
+     * @throws Exception
+     *             if an error occurs
      */
-    public static DisplayObject makeDisplayObject(HttpSession session, InterMineObject object)
-        throws Exception {
+    public static DisplayObject makeDisplayObject(HttpSession session,
+            InterMineObject object) throws Exception {
         ServletContext servletContext = session.getServletContext();
-        ObjectStore os = (ObjectStore) servletContext.getAttribute(Constants.OBJECTSTORE);
-        WebConfig webConfig = (WebConfig) servletContext.getAttribute(Constants.WEBCONFIG);
-        Map webPropertiesMap = (Map) servletContext.getAttribute(Constants.WEB_PROPERTIES);
+        ObjectStore os = (ObjectStore) servletContext
+                .getAttribute(Constants.OBJECTSTORE);
+        WebConfig webConfig = (WebConfig) servletContext
+                .getAttribute(Constants.WEBCONFIG);
+        Map webPropertiesMap = (Map) servletContext
+                .getAttribute(Constants.WEB_PROPERTIES);
         Map classKeys = (Map) servletContext.getAttribute(Constants.CLASS_KEYS);
         return new DisplayObject(object, os.getModel(), webConfig,
-                                 webPropertiesMap, classKeys);
+                webPropertiesMap, classKeys);
     }
 
-    private String getBags(ObjectStore os,
-                           HttpSession session,
-                           ServletContext servletContext,
-                           Integer id,
-                           boolean isGlobal) {
+    private String getBags(ObjectStore os, HttpSession session,
+            ServletContext servletContext, Integer id, boolean isGlobal) {
 
         Map webSearchables = null;
         // get all of the bags with this object
         if (isGlobal) {
-            SearchRepository searchRepository =
-            (SearchRepository) servletContext.getAttribute(Constants.GLOBAL_SEARCH_REPOSITORY);
+            SearchRepository searchRepository = (SearchRepository) servletContext
+                    .getAttribute(Constants.GLOBAL_SEARCH_REPOSITORY);
             webSearchables = searchRepository.getWebSearchableMap(TagTypes.BAG);
         } else {
             Profile profile = (Profile) session.getAttribute(Constants.PROFILE);
@@ -280,7 +332,6 @@ public class ObjectDetailsController extends InterMineAction
         }
         Collection<WebSearchable> webSearchableColl = webSearchables.values();
         Collection<ObjectStoreBag> objectStoreBags = new ArrayList<ObjectStoreBag>();
-
 
         // loop though and convert InterMineBag to ObjectStoreBag
         for (WebSearchable o : webSearchableColl) {
@@ -290,7 +341,8 @@ public class ObjectDetailsController extends InterMineAction
         }
 
         // this searches bags for an object
-        ObjectStoreBagsForObject osbo = new ObjectStoreBagsForObject(id, objectStoreBags);
+        ObjectStoreBagsForObject osbo = new ObjectStoreBagsForObject(id,
+                objectStoreBags);
 
         // run query
         Query q = new Query();
@@ -301,9 +353,11 @@ public class ObjectDetailsController extends InterMineAction
         StringBuffer sb = new StringBuffer();
         for (Object object : results) {
             List list = (List) object;
-            if (sb.length() != 0) { sb.append(","); }
+            if (sb.length() != 0) {
+                sb.append(",");
+            }
             sb.append(list.get(0));
         }
         return sb.toString();
-   }
+    }
 }
