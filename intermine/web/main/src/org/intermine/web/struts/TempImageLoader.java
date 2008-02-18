174d04f3ef2817dea3ca762f425a036b85c87510
julie-sullivan <julie@flymine.org>
2008-02-18 14:20:01 +0000

    checkstyle fixes

--- intermine/web/main/src/org/intermine/web/struts/TempImageLoader.java
+++ intermine/web/main/src/org/intermine/web/struts/TempImageLoader.java
@@ -55,15 +55,15 @@ public class TempImageLoader extends InterMineAction
             }
 
             FileInputStream is = new FileInputStream(file);
-//          Create the byte array to hold the data
+            // Create the byte array to hold the data
             byte[] bytes = new byte[(int) file.length()];
 
-//             Read in the bytes
+            // Read in the bytes
             int offset = 0;
             int numRead = 0;
-            while (offset < bytes.length
-                   && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
-                offset += numRead;
+            while (offset < bytes.length && numRead >= 0) {
+                numRead = is.read(bytes, offset, bytes.length - offset);
+                offset += numRead;                
             }
             response.setContentType("image/png");
             out.write(bytes);
