5f70d8ad09734157c90722cbd60ffa5033a671f4
julie-sullivan <julie@flymine.org>
2008-02-21 13:16:20 +0000

    revert accidental check in

--- flymine/webapp/src/org/flymine/web/widget/FlyAtlasDataSetLdr.java
+++ flymine/webapp/src/org/flymine/web/widget/FlyAtlasDataSetLdr.java
@@ -19,8 +19,10 @@ import java.util.Map;
 import org.intermine.objectstore.query.BagConstraint;
 import org.intermine.objectstore.query.ConstraintOp;
 import org.intermine.objectstore.query.ConstraintSet;
+import org.intermine.objectstore.query.ContainsConstraint;
 import org.intermine.objectstore.query.Query;
 import org.intermine.objectstore.query.QueryClass;
+import org.intermine.objectstore.query.QueryCollectionReference;
 import org.intermine.objectstore.query.QueryField;
 import org.intermine.objectstore.query.Results;
 import org.intermine.objectstore.query.ResultsRow;
@@ -30,6 +32,7 @@ import org.intermine.web.logic.bag.InterMineBag;
 import org.intermine.web.logic.widget.DataSetLdr;
 import org.intermine.web.logic.widget.GraphDataSet;
 
+import org.flymine.model.genomic.FlyAtlasResult;
 import org.flymine.model.genomic.Gene;
 import org.flymine.model.genomic.MicroArrayAssay;
 
@@ -55,15 +58,18 @@ public class FlyAtlasDataSetLdr implements DataSetLdr
         super();
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
 
-        Query q = new Query();     
+        Query q = new Query();
+        QueryClass far = new QueryClass(FlyAtlasResult.class);
         QueryClass maa = new QueryClass(MicroArrayAssay.class);
         QueryClass gene = new QueryClass(Gene.class);
+        q.addFrom(far);
         q.addFrom(maa);
         q.addFrom(gene);
 
         QueryField tissueName = new QueryField(maa, "name");
 
-
+        // q.addToSelect(new QueryField(far,"enrichment"));
+        q.addToSelect(new QueryField(far, "affyCall"));
         q.addToSelect(tissueName);
         q.addToSelect(new QueryField(gene, "primaryAccession"));
 
@@ -73,7 +79,13 @@ public class FlyAtlasDataSetLdr implements DataSetLdr
         BagConstraint bagC = new BagConstraint(qf, ConstraintOp.IN, bag.getOsb());
         cs.addConstraint(bagC);
 
+        QueryCollectionReference r = new QueryCollectionReference(far, "genes");
+        ContainsConstraint cc = new ContainsConstraint(r, ConstraintOp.CONTAINS, gene);
+        QueryCollectionReference r2 = new QueryCollectionReference(far, "assays");
+        ContainsConstraint cc2 = new ContainsConstraint(r2, ConstraintOp.CONTAINS, maa);
 
+        cs.addConstraint(cc);
+        cs.addConstraint(cc2);
         q.setConstraint(cs);
         q.addToOrderBy(tissueName);
 
