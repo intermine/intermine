9d8f33000070acf6b99ebe9d20cca37ad7596f97
kmr <kmr>
2008-02-22 13:13:27 +0000

    Fix FlyBaseModuleProcessor to cope better with new Drosophilas.

--- bio/sources/chado-db/main/src/org/intermine/bio/dataconversion/FlyBaseModuleProcessor.java
+++ bio/sources/chado-db/main/src/org/intermine/bio/dataconversion/FlyBaseModuleProcessor.java
@@ -95,133 +95,131 @@ public class FlyBaseModuleProcessor extends ChadoSequenceProcessor
     protected Map<MultiKey, List<ConfigAction>> getConfig() {
        if (config == null) {
            config = new MultiKeyMap();
-           if (getChadoDBConverter().getTaxonIdInt() == 7227
-               || getChadoDBConverter().getTaxonIdInt() == 7237) {
+           // synomym configuration example: for features of class "Gene", if the type name of
+           // the synonym is "fullname" and "is_current" is true, set the "name" attribute of
+           // the new Gene to be this synonym and then make a Synonym object
+           config.put(new MultiKey("synonym", "Gene", "fullname", Boolean.TRUE),
+                      Arrays.asList(new SetFieldConfigAction("name"),
+                                    CREATE_SYNONYM_ACTION));
 
-               // synomym configuration example: for features of class "Gene", if the type name of
-               // the synonym is "fullname" and "is_current" is true, set the "name" attribute of
-               // the new Gene to be this synonym and then make a Synonym object
-               config.put(new MultiKey("synonym", "Gene", "fullname", Boolean.TRUE),
-                          Arrays.asList(new SetFieldConfigAction("name"),
-                                        CREATE_SYNONYM_ACTION));
+           config.put(new MultiKey("synonym", "Gene", "fullname", Boolean.FALSE),
+                      Arrays.asList(CREATE_SYNONYM_ACTION));
+           config.put(new MultiKey("synonym", "Gene", "symbol", Boolean.TRUE),
+                      Arrays.asList(new SetFieldConfigAction("symbol"),
+                                    CREATE_SYNONYM_ACTION));
+           config.put(new MultiKey("synonym", "Gene", "symbol", Boolean.FALSE),
+                      Arrays.asList(CREATE_SYNONYM_ACTION));
 
-               config.put(new MultiKey("synonym", "Gene", "fullname", Boolean.FALSE),
-                          Arrays.asList(CREATE_SYNONYM_ACTION));
-               config.put(new MultiKey("synonym", "Gene", "symbol", Boolean.TRUE),
+
+           // dbxref table configuration example: for features of class "Gene", where the
+           // db.name is "FlyBase Annotation IDs" and "is_current" is true, set the
+           // "secondaryIdentifier" attribute of the new Gene to be this dbxref and then make a
+           // Synonym object
+           config.put(new MultiKey("dbxref", "Gene", "FlyBase Annotation IDs", Boolean.TRUE),
+                      Arrays.asList(new SetFieldConfigAction("secondaryIdentifier"),
+                                    CREATE_SYNONYM_ACTION));
+           config.put(new MultiKey("dbxref", "Gene", "FlyBase Annotation IDs", Boolean.FALSE),
+                      Arrays.asList(CREATE_SYNONYM_ACTION));
+           // null for the "is_current" means either TRUE or FALSE is OK.
+           config.put(new MultiKey("dbxref", "Gene", "FlyBase", null),
+                      Arrays.asList(CREATE_SYNONYM_ACTION));
+
+           config.put(new MultiKey("dbxref", "MRNA", "FlyBase Annotation IDs", Boolean.TRUE),
+                      Arrays.asList(new SetFieldConfigAction("secondaryIdentifier"),
+                                    CREATE_SYNONYM_ACTION));
+
+           config.put(new MultiKey("synonym", "ChromosomalDeletion", "fullname", Boolean.TRUE),
+                      Arrays.asList(new SetFieldConfigAction("name"),
+                                    CREATE_SYNONYM_ACTION));
+
+           config.put(new MultiKey("synonym", "MRNA", "symbol", Boolean.TRUE),
+                      Arrays.asList(new SetFieldConfigAction("symbol"),
+                                    CREATE_SYNONYM_ACTION));
+           config.put(new MultiKey("synonym", "MRNA", "symbol", Boolean.FALSE),
+                      Arrays.asList(CREATE_SYNONYM_ACTION));
+           config.put(new MultiKey("dbxref", "MRNA", "FlyBase Annotation IDs", null),
+                      Arrays.asList(CREATE_SYNONYM_ACTION));
+           config.put(new MultiKey("dbxref", "MRNA", "FlyBase", null),
+                      Arrays.asList(CREATE_SYNONYM_ACTION));
+
+           config.put(new MultiKey("relationship", "Translation", "producedby", "MRNA"),
+                      Arrays.asList(new SetFieldConfigAction("MRNA")));
+
+           // featureprop configuration example: for features of class "Gene", if the type name
+           // of the prop is "cyto_range", set the "cytoLocation" attribute of the
+           // new Gene to be this property
+           config.put(new MultiKey("prop", "Gene", "cyto_range"),
+                      Arrays.asList(new SetFieldConfigAction("cytoLocation")));
+           config.put(new MultiKey("prop", "Gene", "symbol"),
+                      Arrays.asList(CREATE_SYNONYM_ACTION));
+           config.put(new MultiKey("prop", "TransposableElementInsertionSite",
+           "curated_cytological_location"),
+           Arrays.asList(new SetFieldConfigAction("cytoLocation")));
+
+           // feature configuration example: for features of class "Exon", from "FlyBase",
+           // set the Gene.symbol to be the "name" field from the chado feature
+           config.put(new MultiKey("feature", "Exon", "FlyBase", "name"),
+                      Arrays.asList(new SetFieldConfigAction("symbol"),
+                                    CREATE_SYNONYM_ACTION));
+           // DO_NOTHING_ACTION means skip the name from this feature
+           config.put(new MultiKey("feature", "Chromosome", "FlyBase", "name"),
+                      Arrays.asList(DO_NOTHING_ACTION));
+
+           config.put(new MultiKey("feature", "ChromosomeBand", "FlyBase", "name"),
+                      Arrays.asList(DO_NOTHING_ACTION));
+
+           config.put(new MultiKey("feature", "TransposableElementInsertionSite", "FlyBase",
+           "name"),
+           Arrays.asList(new SetFieldConfigAction("symbol"),
+                         new SetFieldConfigAction("secondaryIdentifier"),
+                         CREATE_SYNONYM_ACTION));
+           config.put(new MultiKey("feature", "TransposableElementInsertionSite", "FlyBase",
+           "uniquename"),
+           Arrays.asList(new SetFieldConfigAction("primaryIdentifier")));
+
+           config.put(new MultiKey("feature", "Gene", "FlyBase", "uniquename"),
+                      Arrays.asList(new SetFieldConfigAction("primaryIdentifier")));
+           config.put(new MultiKey("feature", "Gene", "FlyBase", "name"),
+                      Arrays.asList(DO_NOTHING_ACTION));
+
+           config.put(new MultiKey("feature", "ChromosomalDeletion", "FlyBase", "name"),
+                      Arrays.asList(new SetFieldConfigAction("name"),
+                                    new SetFieldConfigAction("symbol"),
+                                    CREATE_SYNONYM_ACTION));
+
+           config.put(new MultiKey("feature", "MRNA", "FlyBase", "uniquename"),
+                      Arrays.asList(new SetFieldConfigAction("primaryIdentifier")));
+
+           config.put(new MultiKey("feature", "PointMutation", "FlyBase", "uniquename"),
+                      Arrays.asList(new SetFieldConfigAction("name"),
+                                    new SetFieldConfigAction("primaryIdentifier"),
+                                    CREATE_SYNONYM_ACTION));
+           // name isn't set in flybase:
+           config.put(new MultiKey("feature", "PointMutation", "FlyBase", "name"),
+                      Arrays.asList(DO_NOTHING_ACTION));
+
+           if (getChadoDBConverter().getTaxonIdInt() == 7227) {
+               config.put(new MultiKey("dbxref", "Translation", "FlyBase Annotation IDs",
+                                       Boolean.TRUE),
+                                       Arrays.asList(new SetFieldConfigAction("secondaryIdentifier"),
+                                                     CREATE_SYNONYM_ACTION));
+               config.put(new MultiKey("feature", "Translation", "FlyBase", "name"),
                           Arrays.asList(new SetFieldConfigAction("symbol"),
                                         CREATE_SYNONYM_ACTION));
-               config.put(new MultiKey("synonym", "Gene", "symbol", Boolean.FALSE),
-                          Arrays.asList(CREATE_SYNONYM_ACTION));
-
-
-               // dbxref table configuration example: for features of class "Gene", where the
-               // db.name is "FlyBase Annotation IDs" and "is_current" is true, set the
-               // "secondaryIdentifier" attribute of the new Gene to be this dbxref and then make a
-               // Synonym object
-               config.put(new MultiKey("dbxref", "Gene", "FlyBase Annotation IDs", Boolean.TRUE),
+               config.put(new MultiKey("feature", "Translation", "FlyBase", "uniquename"),
+                          Arrays.asList(new SetFieldConfigAction("primaryIdentifier")));
+           } else {
+               config.put(new MultiKey("feature", "Translation", "FlyBase", "uniquename"),
+                          Arrays.asList(new SetFieldConfigAction("primaryIdentifier")));
+               config.put(new MultiKey("feature", "Translation", "FlyBase", "name"),
+                          Arrays.asList(new SetFieldConfigAction("symbol"),
+                                        CREATE_SYNONYM_ACTION));
+               config.put(new MultiKey("dbxref", "Translation", "GB_protein", null),
                           Arrays.asList(new SetFieldConfigAction("secondaryIdentifier"),
                                         CREATE_SYNONYM_ACTION));
-               config.put(new MultiKey("dbxref", "Gene", "FlyBase Annotation IDs", Boolean.FALSE),
-                          Arrays.asList(CREATE_SYNONYM_ACTION));
-               // null for the "is_current" means either TRUE or FALSE is OK.
-               config.put(new MultiKey("dbxref", "Gene", "FlyBase", null),
-                          Arrays.asList(CREATE_SYNONYM_ACTION));
-
-               config.put(new MultiKey("dbxref", "MRNA", "FlyBase Annotation IDs", Boolean.TRUE),
-                          Arrays.asList(new SetFieldConfigAction("secondaryIdentifier"),
-                                        CREATE_SYNONYM_ACTION));
-
-               config.put(new MultiKey("synonym", "ChromosomalDeletion", "fullname", Boolean.TRUE),
-                          Arrays.asList(new SetFieldConfigAction("name"),
-                                        CREATE_SYNONYM_ACTION));
-
-               config.put(new MultiKey("synonym", "MRNA", "symbol", Boolean.TRUE),
-                          Arrays.asList(new SetFieldConfigAction("symbol"),
-                                        CREATE_SYNONYM_ACTION));
-               config.put(new MultiKey("synonym", "MRNA", "symbol", Boolean.FALSE),
-                          Arrays.asList(CREATE_SYNONYM_ACTION));
-               config.put(new MultiKey("dbxref", "MRNA", "FlyBase Annotation IDs", null),
-                          Arrays.asList(CREATE_SYNONYM_ACTION));
-               config.put(new MultiKey("dbxref", "MRNA", "FlyBase", null),
-                          Arrays.asList(CREATE_SYNONYM_ACTION));
-
-               config.put(new MultiKey("relationship", "Translation", "producedby", "MRNA"),
-                          Arrays.asList(new SetFieldConfigAction("MRNA")));
-
-               // featureprop configuration example: for features of class "Gene", if the type name
-               // of the prop is "cyto_range", set the "cytoLocation" attribute of the
-               // new Gene to be this property
-               config.put(new MultiKey("prop", "Gene", "cyto_range"),
-                          Arrays.asList(new SetFieldConfigAction("cytoLocation")));
-               config.put(new MultiKey("prop", "Gene", "symbol"),
-                          Arrays.asList(CREATE_SYNONYM_ACTION));
-               config.put(new MultiKey("prop", "TransposableElementInsertionSite",
-                                       "curated_cytological_location"),
-                          Arrays.asList(new SetFieldConfigAction("cytoLocation")));
-
-               // feature configuration example: for features of class "Exon", from "FlyBase",
-               // set the Gene.symbol to be the "name" field from the chado feature
-               config.put(new MultiKey("feature", "Exon", "FlyBase", "name"),
-                          Arrays.asList(new SetFieldConfigAction("symbol"),
-                                        CREATE_SYNONYM_ACTION));
-               // DO_NOTHING_ACTION means skip the name from this feature
-               config.put(new MultiKey("feature", "Chromosome", "FlyBase", "name"),
-                          Arrays.asList(DO_NOTHING_ACTION));
-
-               config.put(new MultiKey("feature", "ChromosomeBand", "FlyBase", "name"),
-                          Arrays.asList(DO_NOTHING_ACTION));
-
-               config.put(new MultiKey("feature", "TransposableElementInsertionSite", "FlyBase",
-                                       "name"),
-                          Arrays.asList(new SetFieldConfigAction("symbol"),
-                                        new SetFieldConfigAction("secondaryIdentifier"),
-                                        CREATE_SYNONYM_ACTION));
-               config.put(new MultiKey("feature", "TransposableElementInsertionSite", "FlyBase",
-                                       "uniquename"),
-                          Arrays.asList(new SetFieldConfigAction("primaryIdentifier")));
-
-               config.put(new MultiKey("feature", "Gene", "FlyBase", "uniquename"),
-                          Arrays.asList(new SetFieldConfigAction("primaryIdentifier")));
-               config.put(new MultiKey("feature", "Gene", "FlyBase", "name"),
-                          Arrays.asList(DO_NOTHING_ACTION));
-
-               config.put(new MultiKey("feature", "ChromosomalDeletion", "FlyBase", "name"),
-                          Arrays.asList(new SetFieldConfigAction("name"),
-                                        new SetFieldConfigAction("symbol"),
-                                        CREATE_SYNONYM_ACTION));
-
-               config.put(new MultiKey("feature", "MRNA", "FlyBase", "uniquename"),
-                          Arrays.asList(new SetFieldConfigAction("primaryIdentifier")));
-
-               config.put(new MultiKey("feature", "PointMutation", "FlyBase", "uniquename"),
-                          Arrays.asList(new SetFieldConfigAction("name"),
-                                        new SetFieldConfigAction("primaryIdentifier"),
-                                        CREATE_SYNONYM_ACTION));
-               // name isn't set in flybase:
-               config.put(new MultiKey("feature", "PointMutation", "FlyBase", "name"),
-                          Arrays.asList(DO_NOTHING_ACTION));
-
-               if (getChadoDBConverter().getTaxonIdInt() == 7227) {
-                   config.put(new MultiKey("dbxref", "Translation", "FlyBase Annotation IDs",
-                                           Boolean.TRUE),
-                              Arrays.asList(new SetFieldConfigAction("secondaryIdentifier"),
-                                            CREATE_SYNONYM_ACTION));
-                   config.put(new MultiKey("feature", "Translation", "FlyBase", "name"),
-                              Arrays.asList(new SetFieldConfigAction("symbol"),
-                                            CREATE_SYNONYM_ACTION));
-                   config.put(new MultiKey("feature", "Translation", "FlyBase", "uniquename"),
-                              Arrays.asList(new SetFieldConfigAction("primaryIdentifier")));
-               } else {
-                   config.put(new MultiKey("feature", "Translation", "FlyBase", "uniquename"),
-                              Arrays.asList(new SetFieldConfigAction("primaryIdentifier")));
-                   config.put(new MultiKey("feature", "Translation", "FlyBase", "name"),
-                              Arrays.asList(new SetFieldConfigAction("symbol"),
-                                            CREATE_SYNONYM_ACTION));
-                   config.put(new MultiKey("dbxref", "Translation", "GB_protein", null),
-                              Arrays.asList(new SetFieldConfigAction("secondaryIdentifier"),
-                                            CREATE_SYNONYM_ACTION));
-               }
            }
+
+           return config;
        }
 
        return config;
@@ -269,7 +267,7 @@ public class FlyBaseModuleProcessor extends ChadoSequenceProcessor
             return null;
         }
 
-        if (getChadoDBConverter().getTaxonIdInt() == 7237
+        if (getChadoDBConverter().getTaxonIdInt() != 7227
             && chadoFeatureType.equals("chromosome_arm")) {
             // nothing is located on a chromosome_arm
             return null;
