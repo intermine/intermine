7890d68498f83af9982bc8596e1f5e03d23c8cb5
watkins <watkins>
2008-02-28 17:41:10 +0000

    had wrongly checked in these files
    Former-commit-id: f9477f4ea088af2b72a2fead2e1bc78d24c8bdb9

--- flymine/webapp/src/org/flymine/web/widget/BDGPinsituDataSetLdr.java
+++ flymine/webapp/src/org/flymine/web/widget/BDGPinsituDataSetLdr.java
@@ -37,7 +37,7 @@ import org.intermine.web.logic.widget.GraphDataSet;
 
 import org.flymine.model.genomic.DataSet;
 import org.flymine.model.genomic.Gene;
-//import org.flymine.model.genomic.MRNAExpressionResult;
+import org.flymine.model.genomic.MRNAExpressionResult;
 
 import org.jfree.data.category.DefaultCategoryDataset;
 
@@ -82,105 +82,105 @@ public class BDGPinsituDataSetLdr implements DataSetLdr
     }
 
     private void buildDataSets(InterMineBag bag, String geneIdentifier, ObjectStore os) {
-//        
-//        Query q = new Query();
-//        
-//        QueryClass mrnaResult = new QueryClass(MRNAExpressionResult.class);
-//        QueryClass gene = new QueryClass(Gene.class);
-//        QueryClass ds = new QueryClass(DataSet.class);
-//
-//        q.addFrom(mrnaResult);
-//        q.addFrom(gene);
-//        q.addFrom(ds);
-//        
-//        QueryField stageName = new QueryField(mrnaResult, "stageRange");
-//
-//        q.addToSelect(new QueryField(mrnaResult, "expressed"));
-//        q.addToSelect(stageName);
-//        q.addToSelect(new QueryField(gene, "primaryIdentifier"));
-//        ConstraintSet cs = new ConstraintSet(ConstraintOp.AND);
-//
-//        if (bag != null) {
-//            QueryField qf = new QueryField(gene, "id");
-//            cs.addConstraint(new BagConstraint(qf, ConstraintOp.IN, bag.getOsb()));
-//        }
-//        if (geneIdentifier != null) {
-//            cs.addConstraint(new SimpleConstraint(new QueryField(gene, "primaryIdentifier"),
-//                                                  ConstraintOp.EQUALS,
-//                                                  new QueryValue(geneIdentifier)));
-//        }
-//
-//        QueryCollectionReference r = new QueryCollectionReference(gene, "mRNAExpressionResults");
-//        cs.addConstraint(new ContainsConstraint(r, ConstraintOp.CONTAINS, mrnaResult));
-//
-//        QueryObjectReference qcr = new QueryObjectReference(mrnaResult, "source");
-//        cs.addConstraint(new ContainsConstraint(qcr, ConstraintOp.CONTAINS, ds));
-//        
-//        
-//        cs.addConstraint(new SimpleConstraint(new QueryField(ds, "title"), ConstraintOp.EQUALS,
-//        new QueryValue("BDGP in situ data set")));
-//        
-//        q.setConstraint(cs);
-//        q.addToOrderBy(stageName);
-//
-//
-//        results = os.execute(q);
-//        results.setBatchSize(100000);
-//        Iterator iter = results.iterator();
-//        LinkedHashMap<String, int[]> callTable = new LinkedHashMap<String, int[]>();
-//        LinkedHashMap<String, ArrayList<String>> geneMap
-//                                                = new LinkedHashMap<String, ArrayList<String>>();
-//        while (iter.hasNext()) {
-//            ResultsRow resRow = (ResultsRow) iter.next();
-//            Boolean expressed = (Boolean) resRow.get(0);
-//            String stage = (String) resRow.get(1);
-//            String identifier = (String) resRow.get(2);
-//
-//                if (callTable.get(stage) != null) {
-//                    if (expressed.booleanValue()) {
-//                        (callTable.get(stage))[0]++;
-//                        (geneMap.get(stage + "_Expressed")).add(identifier);
-//                    } else {
-//                        (callTable.get(stage))[1]++;
-//                        (geneMap.get(stage + "_NotExpressed")).add(identifier);
-//                    }
-//                } else {
-//                    int[] count = new int[2];
-//                    ArrayList<String> genesArray = new ArrayList<String>();
-//                    genesArray.add(identifier);
-//                    if (expressed.booleanValue()) {
-//                        count[0]++;
-//                        geneMap.put(stage + "_Expressed", genesArray);
-//                        geneMap.put(stage + "_NotExpressed", new ArrayList<String>());
-//                    } else {
-//                        count[1]++;
-//                        geneMap.put(stage + "_Expressed", new ArrayList<String>());
-//                        geneMap.put(stage + "_NotExpressed", genesArray);
-//                    }
-//                    callTable.put(stage, count);
-//                }
-//
-//        }
-//        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-//        // Build a map from tissue/UpDown to gene list
-//        geneCategoryArray = new Object[callTable.size()];
-//        int i = 0;
-//        for (Iterator iterator = callTable.keySet().iterator(); iterator.hasNext();) {
-//            String stage = (String) iterator.next();
-//
-//            dataSet.addValue((callTable.get(stage))[0], "Expressed", stage);
-//            dataSet.addValue((callTable.get(stage))[1], "NotExpressed", stage);
-//
-//            Object[] geneSeriesArray = new Object[2];
-//            geneSeriesArray[0] = geneMap.get(stage + "_Expressed");
-//            geneSeriesArray[1] = geneMap.get(stage + "_NotExpressed");
-//            geneCategoryArray[i] = geneSeriesArray;
-//            i++;
-//        }
-//        GraphDataSet graphDataSet = new GraphDataSet(dataSet, geneCategoryArray);
-//        if (results.size() > 0) {
-//            dataSets.put("any", graphDataSet);
-//        }
+        
+        Query q = new Query();
+        
+        QueryClass mrnaResult = new QueryClass(MRNAExpressionResult.class);
+        QueryClass gene = new QueryClass(Gene.class);
+        QueryClass ds = new QueryClass(DataSet.class);
+
+        q.addFrom(mrnaResult);
+        q.addFrom(gene);
+        q.addFrom(ds);
+        
+        QueryField stageName = new QueryField(mrnaResult, "stageRange");
+
+        q.addToSelect(new QueryField(mrnaResult, "expressed"));
+        q.addToSelect(stageName);
+        q.addToSelect(new QueryField(gene, "primaryIdentifier"));
+        ConstraintSet cs = new ConstraintSet(ConstraintOp.AND);
+
+        if (bag != null) {
+            QueryField qf = new QueryField(gene, "id");
+            cs.addConstraint(new BagConstraint(qf, ConstraintOp.IN, bag.getOsb()));
+        }
+        if (geneIdentifier != null) {
+            cs.addConstraint(new SimpleConstraint(new QueryField(gene, "primaryIdentifier"),
+                                                  ConstraintOp.EQUALS,
+                                                  new QueryValue(geneIdentifier)));
+        }
+
+        QueryCollectionReference r = new QueryCollectionReference(gene, "mRNAExpressionResults");
+        cs.addConstraint(new ContainsConstraint(r, ConstraintOp.CONTAINS, mrnaResult));
+
+        QueryObjectReference qcr = new QueryObjectReference(mrnaResult, "source");
+        cs.addConstraint(new ContainsConstraint(qcr, ConstraintOp.CONTAINS, ds));
+        
+        
+        cs.addConstraint(new SimpleConstraint(new QueryField(ds, "title"), ConstraintOp.EQUALS,
+        new QueryValue("BDGP in situ data set")));
+        
+        q.setConstraint(cs);
+        q.addToOrderBy(stageName);
+
+
+        results = os.execute(q);
+        results.setBatchSize(100000);
+        Iterator iter = results.iterator();
+        LinkedHashMap<String, int[]> callTable = new LinkedHashMap<String, int[]>();
+        LinkedHashMap<String, ArrayList<String>> geneMap
+                                                = new LinkedHashMap<String, ArrayList<String>>();
+        while (iter.hasNext()) {
+            ResultsRow resRow = (ResultsRow) iter.next();
+            Boolean expressed = (Boolean) resRow.get(0);
+            String stage = (String) resRow.get(1);
+            String identifier = (String) resRow.get(2);
+
+                if (callTable.get(stage) != null) {
+                    if (expressed.booleanValue()) {
+                        (callTable.get(stage))[0]++;
+                        (geneMap.get(stage + "_Expressed")).add(identifier);
+                    } else {
+                        (callTable.get(stage))[1]++;
+                        (geneMap.get(stage + "_NotExpressed")).add(identifier);
+                    }
+                } else {
+                    int[] count = new int[2];
+                    ArrayList<String> genesArray = new ArrayList<String>();
+                    genesArray.add(identifier);
+                    if (expressed.booleanValue()) {
+                        count[0]++;
+                        geneMap.put(stage + "_Expressed", genesArray);
+                        geneMap.put(stage + "_NotExpressed", new ArrayList<String>());
+                    } else {
+                        count[1]++;
+                        geneMap.put(stage + "_Expressed", new ArrayList<String>());
+                        geneMap.put(stage + "_NotExpressed", genesArray);
+                    }
+                    callTable.put(stage, count);
+                }
+
+        }
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        // Build a map from tissue/UpDown to gene list
+        geneCategoryArray = new Object[callTable.size()];
+        int i = 0;
+        for (Iterator iterator = callTable.keySet().iterator(); iterator.hasNext();) {
+            String stage = (String) iterator.next();
+
+            dataSet.addValue((callTable.get(stage))[0], "Expressed", stage);
+            dataSet.addValue((callTable.get(stage))[1], "NotExpressed", stage);
+
+            Object[] geneSeriesArray = new Object[2];
+            geneSeriesArray[0] = geneMap.get(stage + "_Expressed");
+            geneSeriesArray[1] = geneMap.get(stage + "_NotExpressed");
+            geneCategoryArray[i] = geneSeriesArray;
+            i++;
+        }
+        GraphDataSet graphDataSet = new GraphDataSet(dataSet, geneCategoryArray);
+        if (results.size() > 0) {
+            dataSets.put("any", graphDataSet);
+        }
     }
 
 }
