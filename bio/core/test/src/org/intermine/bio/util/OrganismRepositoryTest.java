6fca565cda4cb5507581819ccf800670b45f75f5
kmr <kmr>
2008-03-06 18:24:41 +0000

    Added OrganismRepository - a generic holder for organism information.

---
+++ bio/core/test/src/org/intermine/bio/util/OrganismRepositoryTest.java
@@ -0,0 +1,38 @@
+package org.intermine.bio.util;
+
+/*
+ * Copyright (C) 2002-2007 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for the OrganismRepositoryclass.
+ * @author Kim Rutherford
+ */
+public class OrganismRepositoryTest extends TestCase
+{
+    /*
+     * Test method for OrganismRepository.getOrganismRepository().
+     */
+    public void testGetOrganismRepository() {
+        OrganismRepository organismRepository = OrganismRepository.getOrganismRepository();
+        OrganismData drosOrganismData = organismRepository.getOrganismDataByTaxon(7227);
+        assertEquals(new Integer(7227), drosOrganismData.getTaxonId());
+        assertEquals("melanogaster", drosOrganismData.getSpecies());
+        assertEquals("Drosophila", drosOrganismData.getGenus());
+        assertEquals("Dmel", drosOrganismData.getAbbreviation());
+
+        OrganismData celegansData = organismRepository.getOrganismDataByAbbreviation("C.elegans");
+        assertEquals(new Integer(6239), celegansData.getTaxonId());
+        assertEquals("elegans", celegansData.getSpecies());
+        assertEquals("Caenorhabditis", celegansData.getGenus());
+        assertEquals("C.elegans", celegansData.getAbbreviation());
+    }
+}
