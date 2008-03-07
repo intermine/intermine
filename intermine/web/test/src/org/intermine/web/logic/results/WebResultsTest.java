78e744e117ddd11901b1653237c66bf28c9d2290
watkins <watkins>
2008-03-07 17:40:20 +0000

    Started adding functional tests for WebResults

--- intermine/web/test/src/org/intermine/web/logic/results/WebResultsTest.java
+++ intermine/web/test/src/org/intermine/web/logic/results/WebResultsTest.java
@@ -11,32 +11,39 @@ package org.intermine.web.logic.results;
  */
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import junit.framework.TestCase;
+
+import org.apache.tools.ant.types.CommandlineJava.SysProperties;
+import org.intermine.metadata.FieldDescriptor;
+import org.intermine.metadata.Model;
+import org.intermine.model.testmodel.CEO;
+import org.intermine.model.testmodel.Company;
+import org.intermine.model.testmodel.Department;
+import org.intermine.model.testmodel.Employee;
+import org.intermine.model.testmodel.Manager;
+import org.intermine.objectstore.dummy.ObjectStoreDummyImpl;
 import org.intermine.objectstore.query.Query;
 import org.intermine.objectstore.query.QueryClass;
 import org.intermine.objectstore.query.QueryField;
+import org.intermine.objectstore.query.QueryNode;
 import org.intermine.objectstore.query.Results;
 import org.intermine.objectstore.query.ResultsRow;
 import org.intermine.objectstore.query.iql.IqlQuery;
-
-import org.intermine.metadata.FieldDescriptor;
-import org.intermine.metadata.Model;
-import org.intermine.model.testmodel.Company;
-import org.intermine.model.testmodel.Department;
-import org.intermine.model.testmodel.Manager;
-import org.intermine.objectstore.dummy.ObjectStoreDummyImpl;
 import org.intermine.path.Path;
 import org.intermine.util.DynamicUtil;
+import org.intermine.web.logic.pathqueryresult.PathQueryResultHelper;
+import org.intermine.web.logic.profile.Profile;
+import org.intermine.web.logic.query.MainHelper;
 import org.intermine.web.logic.query.PathQuery;
-import org.intermine.web.logic.results.WebResults;
+import org.intermine.web.logic.query.PathQueryHandler;
 
-import junit.framework.TestCase;
+import com.sun.org.apache.bcel.internal.generic.GETSTATIC;
 
 /**
  * Tests for the WebResults class
@@ -46,7 +53,6 @@ import junit.framework.TestCase;
 
 public class WebResultsTest extends TestCase
 {
-    private Results results;
     private Company company1;
     private Company company2;
     private Company company3;
@@ -55,8 +61,10 @@ public class WebResultsTest extends TestCase
     private Department department3;
     private Manager man1;
     private Manager man2;
-    private Manager man3;
-    private WebResults webResults;
+    private CEO man3;
+    private ObjectStoreDummyImpl os;
+    private final Model model = Model.getInstanceByName("testmodel");
+    private Map classKeys;
 
     public WebResultsTest (String arg) {
         super(arg);
@@ -64,22 +72,8 @@ public class WebResultsTest extends TestCase
 
     public void setUp() throws Exception {
         super.setUp();
-        ObjectStoreDummyImpl os = new ObjectStoreDummyImpl();
+        os = new ObjectStoreDummyImpl();
         os.setResultsSize(15);
-        IqlQuery fq =
-            new IqlQuery("SELECT DISTINCT a1_, a3_, a4_ FROM org.intermine.model.testmodel.Department AS a1_," +
-                    " org.intermine.model.testmodel.CEO AS a2_, org.intermine.model.testmodel.Company " +
-                    "AS a3_, org.intermine.model.testmodel.Manager AS a4_ " +
-                    "WHERE (a1_.manager CONTAINS a2_ AND a2_.company CONTAINS a3_ " +
-                    "AND a1_.employees CONTAINS a4_)",
-                    "org.intermine.model.testmodel");
-/*        "SELECT DISTINCT a3_, a4_ FROM org.intermine.model.testmodel.Department AS a1_, " +
-        "org.intermine.model.testmodel.CEO AS a2_, org.intermine.model.testmodel.Company AS a3_, " +
-        "org.intermine.model.testmodel.Manager AS a4_ " +
-        "WHERE (a1_.manager CONTAINS a2_ AND a2_.company CONTAINS a3_ AND a1_.employees CONTAINS a4_)";
-*/
-        Query query = fq.toQuery();
-        results = os.execute(query);
 
         // Set up some known objects in the first 3 results rows
         department1 = new Department();
@@ -113,10 +107,11 @@ public class WebResultsTest extends TestCase
         man2.setName("Manager2");
         man2.setSeniority(new Integer(200));
         man2.setId(new Integer(2));
-        man3 = (Manager) DynamicUtil.createObject(Collections.singleton(Manager.class));
+        man3 = (CEO) DynamicUtil.createObject(Collections.singleton(CEO.class));
         man3.setName("Manager3");
         man3.setSeniority(new Integer(300));
         man3.setId(new Integer(3));
+        man3.setCompany(company3);
 
         ResultsRow row = new ResultsRow();
         row.add(department1);
@@ -133,8 +128,102 @@ public class WebResultsTest extends TestCase
         row.add(company3);
         row.add(man3);
         os.addRow(row);
+        classKeys = new HashMap();
+        FieldDescriptor fd = model.getClassDescriptorByName("org.intermine.model.testmodel.Company").getFieldDescriptorByName("name");
+        ArrayList<FieldDescriptor> keys = new ArrayList();
+        keys.add(fd);
+        classKeys.put("Company", keys);
+    }
 
-        final Model model = Model.getInstanceByName("testmodel");
+    // create a query with MainHelper.makeQuery() that contains both QueryClasses and QueryFields
+     public void testGetPathToIndex() throws Exception {
+         List<Path> view = new ArrayList<Path>() {{ // see: http://www.c2.com/cgi/wiki?DoubleBraceInitialization
+             add(new Path(model, "Department.name"));
+             add(new Path(model, "Department.manager.name"));
+             add(new Path(model, "Department.employees.name"));
+         }};
+         PathQuery pathQuery = new PathQuery(model);
+         pathQuery.setView(view);
+         QueryClass dept1 = new QueryClass(Department.class);
+         QueryField depName = new QueryField(dept1, "name");
+         
+         QueryClass man1 = new QueryClass(Manager.class);
+         QueryField manName = new QueryField(man1, "name");
+         
+         QueryClass emp1 = new QueryClass(Employee.class);
+         QueryField empName = new QueryField(emp1, "name");
+         
+
+         Map<String, QueryNode> pathToQueryNode = new HashMap<String, QueryNode>();
+         pathToQueryNode.put("Department", dept1);
+         pathToQueryNode.put("Department.name", depName);
+         pathToQueryNode.put("Department.manager", man1);
+         pathToQueryNode.put("Department.manager.name", manName);
+         pathToQueryNode.put("Department.employees", emp1);
+         pathToQueryNode.put("Department.employees.name", empName);
+
+         Query query = MainHelper.makeQuery(pathQuery , new HashMap(), pathToQueryNode, null, null, true);
+         LinkedHashMap<String, Integer> actual = WebResults.getPathToIndex(query, pathToQueryNode);
+         LinkedHashMap<String, Integer> expected = new LinkedHashMap<String, Integer>();
+         expected.put("Department.employees.name", 2);
+         expected.put("Department.name", 0);
+         expected.put("Department.manager.name", 1);
+         expected.put("Department.employees", 2);
+         expected.put("Department.manager", 1);
+         expected.put("Department", 0);
+         assertEquals(expected, actual);
+    }
+    
+    // create a PathQuery, create expected column objects and compare.  Include:
+    //   - some paths with descriptions
+    //   - select fields that are/aren't class keys
+    public void testSetColumns() throws Exception {
+        PathQuery pq = new PathQuery(model);
+        List view = new ArrayList() {{ // see: http://www.c2.com/cgi/wiki?DoubleBraceInitialization
+            add(new Path(model, "Company.name"));
+            add(new Path(model, "Company.vatNumber"));
+            add(new Path(model, "Company.CEO.name"));
+            add(new Path(model, "Employee.name"));
+        }};
+        pq.addPathStringDescription("Company", "description 1");
+        pq.setView(view);
+        Map<String, QueryNode> pathToQueryNode = new HashMap<String, QueryNode>();
+        Query query = MainHelper.makeQuery(pq , new HashMap(), pathToQueryNode, null, null, true);
+        Results results = os.execute(query);
+        WebResults webResults = new WebResults(pq, results, model, pathToQueryNode, classKeys, null);
+        List<Column> expectedColumns = new ArrayList<Column>();
+        Path path = new Path(model, "Company.CEO.name");
+        Column col1 = new Column("description 1.name",0 ,Company.class);
+        Column col2 = new Column("description 1.vatNumber",1 ,Company.class);
+        // TODO should Company be replaced by description 1 for CEO bellow?
+        Column col3 = new Column("Company.CEO.name",2 ,CEO.class);
+        Column col4 = new Column("Employee.name",3 ,Employee.class);
+        expectedColumns.add(col1);
+        expectedColumns.add(col2);
+        expectedColumns.add(col3);
+        expectedColumns.add(col4);
+        assertEquals(expectedColumns.get(0), webResults.getColumns().get(0));
+        assertEquals(expectedColumns.get(1), webResults.getColumns().get(1));
+        assertEquals(expectedColumns.get(2), webResults.getColumns().get(2));
+        assertEquals(expectedColumns.get(3), webResults.getColumns().get(3));
+    }
+    
+    // TODO add test
+    // public void testTranslateRow() throws Exception {
+    // Test with a PathQuery and some dummy results, call method with a made up row,
+    // create expected ResultElements.  Doesn't need too much testing as Path.resolve() is tested.
+    
+    public void test() {
+        IqlQuery fq =
+            new IqlQuery("SELECT DISTINCT a1_, a3_, a4_ FROM org.intermine.model.testmodel.Department AS a1_," +
+                    " org.intermine.model.testmodel.CEO AS a2_, org.intermine.model.testmodel.Company " +
+                    "AS a3_, org.intermine.model.testmodel.Manager AS a4_ " +
+                    "WHERE (a1_.manager CONTAINS a2_ AND a2_.company CONTAINS a3_ " +
+                    "AND a1_.employees CONTAINS a4_)",
+                    "org.intermine.model.testmodel");
+        Query query = fq.toQuery();
+        Results results = os.execute(query);
+
         List view = new ArrayList() {{ // see: http://www.c2.com/cgi/wiki?DoubleBraceInitialization
             add(new Path(model, "Department.name"));
             add(new Path(model, "Department.manager[CEO].company.name"));
@@ -143,7 +232,7 @@ public class WebResultsTest extends TestCase
         }};
         PathQuery pathQuery = new PathQuery(model);
         pathQuery.setView(view);
-        Map pathToQueryNode = new HashMap();
+        Map<String, QueryNode> pathToQueryNode = new HashMap<String, QueryNode>();
         QueryClass deptQC = (QueryClass) query.getSelect().get(0);
         pathToQueryNode.put("Department", deptQC);
         QueryField deptNameQF = new QueryField(deptQC, "name");
@@ -159,31 +248,8 @@ public class WebResultsTest extends TestCase
         QueryClass manQC = (QueryClass) query.getSelect().get(2);
         QueryField manSeniority = new QueryField(manQC, "seniority");
         pathToQueryNode.put("Department.employees.seniority", manSeniority);
+        WebResults webResults = new WebResults(pathQuery, results, model, pathToQueryNode, classKeys, null);
 
-        Map classKeys = new HashMap();
-        FieldDescriptor fd = model.getClassDescriptorByName("org.intermine.model.testmodel.Company").getFieldDescriptorByName("name");
-        ArrayList<FieldDescriptor> keys = new ArrayList();
-        keys.add(fd);
-        classKeys.put("Company", keys);
-        webResults = new WebResults(pathQuery, results, model, pathToQueryNode, classKeys, null);
-    }
-
-    // TODO add test
-    // public void testGetPathToIndex() throws Exception {
-    // create a query with MainHelper.makeQuery() that contains both QueryClasses and QueryFields
-    
-    // TODO add test
-    // public void testSetColumns() throws Exception {
-    // create a PathQuery, create expected column objects and compare.  Include:
-    //   - some paths with descriptions
-    //   - select fields that are/aren't class keys
-    
-    // TODO add test
-    // public void testTranslateRow() throws Exception {
-    // Test with a PathQuery and some dummy results, call method with a made up row,
-    // create expected ResultElements.  Doesn't need too much testing as Path.resolve() is tested.
-    
-    public void test() {
         assertEquals("Department1", ((List) webResults.get(0)).get(0));
         assertEquals("Company1", ((List) webResults.get(0)).get(1));
         assertEquals(new Integer(101), ((List) webResults.get(0)).get(2));
