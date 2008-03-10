c0c68a9b45d099d4be72ac1d143b42554b4fffd5
jakub <jakub>
2008-03-10 15:40:02 +0000

    SequenceExporter refactoring.

--- bio/webapp/src/org/intermine/bio/web/export/SequenceExporter.java
+++ bio/webapp/src/org/intermine/bio/web/export/SequenceExporter.java
@@ -11,56 +11,27 @@ package org.intermine.bio.web.export;
  */
 
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Properties;
-import java.util.Set;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.struts.Globals;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
-import org.apache.struts.action.ActionMessages;
-import org.biojava.bio.Annotation;
-import org.biojava.bio.seq.io.FastaFormat;
-import org.biojava.bio.seq.io.SeqIOTools;
-import org.biojava.bio.symbol.IllegalSymbolException;
-import org.biojava.utils.ChangeVetoException;
-import org.flymine.model.genomic.BioEntity;
-import org.flymine.model.genomic.Gene;
 import org.flymine.model.genomic.LocatedSequenceFeature;
 import org.flymine.model.genomic.Protein;
 import org.flymine.model.genomic.Sequence;
 import org.flymine.model.genomic.Translation;
-import org.intermine.bio.web.biojava.BioSequence;
-import org.intermine.bio.web.biojava.BioSequenceFactory;
-import org.intermine.bio.web.biojava.BioSequenceFactory.SequenceType;
-import org.intermine.metadata.ClassDescriptor;
-import org.intermine.metadata.Model;
-import org.intermine.model.InterMineObject;
 import org.intermine.objectstore.ObjectStore;
-import org.intermine.objectstore.ObjectStoreException;
-import org.intermine.path.Path;
-import org.intermine.util.DynamicUtil;
 import org.intermine.util.StringUtil;
-import org.intermine.util.TypeUtil;
 import org.intermine.web.logic.Constants;
+import org.intermine.web.logic.export.ExportHelper;
 import org.intermine.web.logic.export.TableExporter;
-import org.intermine.web.logic.results.Column;
 import org.intermine.web.logic.results.PagedTable;
 import org.intermine.web.logic.results.ResultElement;
-import org.intermine.web.logic.results.WebResults;
 import org.intermine.web.logic.session.SessionMethods;
-import org.intermine.web.struts.InterMineAction;
 
 /**
  * An implementation of TableExporter that exports sequence objects using the BioJava sequence and
@@ -101,349 +72,41 @@ public class SequenceExporter implements TableExporter
             (ObjectStore) session.getServletContext().getAttribute(Constants.OBJECTSTORE);
         setSequenceExportHeader(response);
 
-        OutputStream outputStream = null;
+        OutputStream outputStream = response.getOutputStream();
 
         PagedTable pt = SessionMethods.getResultsTable(session, request.getParameter("table"));
 
         // the first column that contains exportable features
-        Column featureColumn = getFeatureColumn(pt);
+        int realFeatureIndex = getFeatureColumnIndex(pt);
 
-        int realFeatureIndex = featureColumn.getIndex();
+        List<List<ResultElement>> rowList = pt.getRearrangedResults();
 
-        // IDs of the features we have successfully output - used to avoid duplicates
-        Set exportedIDs = new HashSet();
-
-        try {
-            List rowList = pt.getAllRows();
-
-            for (int rowIndex = 0;
-                 rowIndex < rowList.size() && rowIndex <= pt.getMaxRetrievableIndex();
-                 rowIndex++) {
-                List row;
-                try {
-                    if (rowList instanceof WebResults) {
-                        row = ((WebResults) rowList).getResultElements(rowIndex);
-                    } else {
-                        row = (List) rowList.get(rowIndex);
-                    }
-                } catch (RuntimeException e) {
-                    // re-throw as a more specific exception
-                    if (e.getCause() instanceof ObjectStoreException) {
-                        throw (ObjectStoreException) e.getCause();
-                    } else {
-                        throw e;
-                    }
-                }
-
-                StringBuffer header = new StringBuffer();
-
-                Object resultObject = row.get(realFeatureIndex);
-
-                ResultElement resultElement;
-                if (resultObject instanceof ResultElement) {
-                    resultElement = (ResultElement) resultObject;
-                } else {
-                    // ignore other objects
-                    continue;
-                }
-
-                BioSequence bioSequence;
-                Object object = os.getObjectById(resultElement.getId());
-                if (!(object instanceof InterMineObject)) {
-                    continue;
-                }
-
-                Integer objectId = ((InterMineObject) object).getId();
-                if (exportedIDs.contains(objectId)) {
-                    // exported already
-                    continue;
-                }
-
-                if (object instanceof LocatedSequenceFeature) {
-                    bioSequence = createLocatedSequenceFeature(header, object);
-                } else if (object instanceof Protein) {
-                    bioSequence = createProtein(header, object);
-                } else if (object instanceof Translation) {
-                    Model model = os.getModel();
-                    bioSequence = createTranslation(header, object, model);
-                } else {
-                    // ignore other objects
-                    continue;
-                }
-
-                if (bioSequence == null) {
-                    // the object doesn't have a sequence
-                    continue;
-                }
-
-                Annotation annotation = bioSequence.getAnnotation();
-                String headerString = header.toString();
-
-                if (row.size() > 1 && headerString.length() > 0) {
-                    annotation.setProperty(FastaFormat.PROPERTY_DESCRIPTIONLINE, headerString);
-                } else {
-                    if (object instanceof BioEntity) {
-                        annotation.setProperty(FastaFormat.PROPERTY_DESCRIPTIONLINE,
-                                               ((BioEntity) object).getPrimaryIdentifier());
-                    } else {
-                        // last resort
-                        annotation.setProperty(FastaFormat.PROPERTY_DESCRIPTIONLINE,
-                                               "sequence_" + exportedIDs.size());
-                    }
-                }
-
-                if (outputStream == null) {
-                    // try to avoid opening the OutputStream until we know that the query is
-                    // going to work - this avoids some problems that occur when
-                    // getOutputStream() is called twice (once by this method and again to
-                    // write the error)
-                    outputStream = response.getOutputStream();
-                }
-                SeqIOTools.writeFasta(outputStream, bioSequence);
-
-                exportedIDs.add(objectId);
-            }
-
-            if (outputStream != null) {
-                outputStream.close();
-            }
-
-            if (exportedIDs.size() == 0) {
-                ActionMessages messages = new ActionMessages();
-                ActionMessage error = new ActionMessage("errors.export.nothingtoexport");
-                messages.add(ActionMessages.GLOBAL_MESSAGE, error);
-                request.setAttribute(Globals.ERROR_KEY, messages);
-                return mapping.findForward("results");
-            }
-        } catch (ObjectStoreException e) {
-            ActionMessages messages = new ActionMessages();
-            ActionMessage error = new ActionMessage("errors.query.objectstoreerror");
-            messages.add(ActionMessages.GLOBAL_MESSAGE, error);
-            request.setAttribute(Globals.ERROR_KEY, messages);
-        }
+        SequenceExporterLogic exporter = new SequenceExporterLogic(os, outputStream, 
+                realFeatureIndex);
+        
+        exporter.export(rowList);
 
         return null;
     }
 
-    private BioSequence createTranslation(StringBuffer header, Object object,
-            Model model) throws IllegalSymbolException {
-        BioSequence bioSequence;
-        ClassDescriptor cld = model.getClassDescriptorByName(model.getPackageName()
-                                                             + "." + "Translation");
-        if (cld.getReferenceDescriptorByName("sequence", true) != null) {
-            Translation translation = (Translation) object;
-            bioSequence = BioSequenceFactory.make(translation);
-            header.append(translation.getPrimaryIdentifier());
-            header.append(' ');
-            if (translation.getName() == null) {
-                header.append("[unknown_name]");
-            } else {
-                header.append(translation.getName());
+    
+    private int getFeatureColumnIndex(PagedTable pt) {
+        List<Class> clazzes = ExportHelper.getColumnClasses(pt);
+        for (int i = 0; i < clazzes.size(); i++) {
+            if (Protein.class.isAssignableFrom(clazzes.get(i))
+                || LocatedSequenceFeature.class.isAssignableFrom(clazzes.get(i))
+                || Sequence.class.isAssignableFrom(clazzes.get(i))
+                || Translation.class.isAssignableFrom(clazzes.get(i))) {
+                return i;
             }
-            if (translation.getGene() != null) {
-                Gene gene = translation.getGene();
-                String geneIdentifier = gene.getPrimaryIdentifier();
-                if (geneIdentifier != null) {
-                    header.append(' ');
-                    header.append("gene:");
-                    header.append(geneIdentifier);
-                }
-            }
-        } else {
-            bioSequence = null;
         }
-        return bioSequence;
-    }
-
-    private BioSequence createProtein(StringBuffer header, Object object)
-            throws IllegalSymbolException {
-        BioSequence bioSequence;
-        Protein protein = (Protein) object;
-        bioSequence = BioSequenceFactory.make(protein);
-        header.append(protein.getPrimaryIdentifier());
-        header.append(' ');
-        if (protein.getName() == null) {
-            header.append("[unknown_name]");
-        } else {
-            header.append(protein.getName());
-        }
-        Iterator iter = protein.getGenes().iterator();
-        while (iter.hasNext()) {
-            Gene gene = (Gene) iter.next();
-            String geneIdentifier = gene.getPrimaryIdentifier();
-            if (geneIdentifier != null) {
-                header.append(' ');
-                header.append("gene:");
-                header.append(geneIdentifier);
-            }
-
-        }
-        return bioSequence;
-    }
-
-    private BioSequence createLocatedSequenceFeature(StringBuffer header,
-            Object object) throws IllegalSymbolException {
-        BioSequence bioSequence;
-        LocatedSequenceFeature feature = (LocatedSequenceFeature) object;
-        bioSequence = BioSequenceFactory.make(feature);
-        if (feature.getPrimaryIdentifier() == null) {
-            if (feature instanceof Gene) {
-                header.append(((Gene) feature).getPrimaryIdentifier());
-            } else {
-                header.append("[unknown_identifier]");
-            }
-        } else {
-            header.append(feature.getPrimaryIdentifier());
-        }
-        header.append(' ');
-        if (feature.getName() == null) {
-            header.append("[unknown_name]");
-        } else {
-            header.append(feature.getName());
-        }
-        if (feature.getChromosomeLocation() != null) {
-            header.append(' ').append(feature.getChromosome().getPrimaryIdentifier());
-            header.append(':').append(feature.getChromosomeLocation().getStart());
-            header.append('-').append(feature.getChromosomeLocation().getEnd());
-            header.append(' ').append(feature.getLength());
-        }
-        try {
-            Gene gene = (Gene) TypeUtil.getFieldValue(feature, "gene");
-            if (gene != null) {
-                String geneIdentifier = gene.getPrimaryIdentifier();
-                if (geneIdentifier != null) {
-                    header.append(' ').append("gene:").append(geneIdentifier);
-                }
-            }
-        } catch (IllegalAccessException e) {
-            // ignore
-        }
-        return bioSequence;
+        return -1;
     }
 
     /**
      * {@inheritDoc}
      */
     public boolean canExport(PagedTable pt) {
-        return getFeatureColumn(pt) != null;
-    }
-
-    /**
-     * Check whether the argument is one of the types we handle
-     * @param type the type
-     * @return true if we handle the type
-     */
-    public boolean validType(Class type) {
-        return (LocatedSequenceFeature.class.isAssignableFrom(type)
-                        || Protein.class.isAssignableFrom(type)
-                        || Translation.class.isAssignableFrom(type)
-                        || Sequence.class.isAssignableFrom(type));
-    }
-
-    private boolean validType(String className) {
-        try {
-            if (className.indexOf('.') == -1) {
-                // FIXME this is a HACK - the ResultElement should include type package name
-                return validType(Class.forName("org.flymine.model.genomic." + className));
-            } else {
-                return validType(Class.forName(className));
-            }
-        } catch (ClassNotFoundException e) {
-            return false;
-        }
-    }
-
-    private boolean validType(InterMineObject imo) {
-        Set classes = DynamicUtil.decomposeClass(imo.getClass());
-        Iterator iter = classes.iterator();
-        while (iter.hasNext()) {
-            if (validType((Class) iter.next())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Return the first column that contains features that can be exported.  It first checks the
-     * Column objects from the PagedTable.
-     *
-     * If there are no LocatedSequenceFeature or Protein columns it checks the visible rows of the
-     * Results objects and returns the first Column that contains a LocatedSequenceFeature or
-     * Protein.  This is needed, for example, when the user select BioEntity and constrains the
-     * identifier to "zen" - the Results contain only Genes, but the column type is BioEntity.
-     *
-     * If there is more than one LocatedSequenceFeature or Protein column, return null to prevent
-     * confusion about which column is being exported.
-     * @throws ObjectStoreException
-     */
-    private Column getFeatureColumn(PagedTable pt) {
-        List columns = pt.getColumns();
-
-        Column returnColumn = null;
-
-        // find and remember the first valid Sequence-containing column
-        for (int i = 0; i < columns.size(); i++) {
-            Column column = (Column) columns.get(i);
-            if (column.isVisible()) {
-                Path columnPath = column.getPath();
-                if (columnPath != null && columnPath.endIsAttribute()) {
-                    ClassDescriptor columnCD = columnPath.getLastClassDescriptor();
-                    if (validType(columnCD.getName())) {
-                        if (returnColumn == null) {
-                            returnColumn = column;
-                        } else {
-                            // there are two or more sequence columns
-                            return null;
-                        }
-                    }
-                }
-
-                // only consider the first visible column
-                break;
-            }
-        }
-
-        // search the visible rows for any validType()s
-        List rowList = pt.getResultElementRows();
-
-        for (int columnIndex = 0; columnIndex < columns.size(); columnIndex++) {
-            Column thisColumn = (Column) columns.get(columnIndex);
-
-            if (!thisColumn.isVisible()) {
-                continue;
-            }
-
-            // the column order from PagedTable.getList() isn't necessarily the order that
-            // the user has chosen for the columns
-            int realColumnIndex = thisColumn.getIndex();
-
-            Iterator rowListIter = rowList.iterator();
-
-            while (rowListIter.hasNext()) {
-                List row = (List) rowListIter.next();
-
-                if (realColumnIndex < row.size()) {
-                    Object o = row.get(realColumnIndex);
-
-                    if (o != null && o instanceof ResultElement) {
-                        ResultElement resultElement = (ResultElement) o;
-                        InterMineObject imo;
-                        try {
-                            imo = resultElement.getInterMineObject();
-                        } catch (ObjectStoreException e) {
-                            // give up
-                            return null;
-                        }
-
-                        if (resultElement.isKeyField() && validType(imo)) {
-                            return thisColumn;
-                        }
-                    }
-                }
-            }
-        }
-
-        return returnColumn;
+        return SequenceExporterLogic.canExport2(ExportHelper.getColumnClasses(pt));
     }
 }
---
+++ bio/webapp/src/org/intermine/bio/web/export/SequenceExporterLogic.java
@@ -0,0 +1,261 @@
+package org.intermine.bio.web.export;
+
+/*
+ * Copyright (C) 2002-2008 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.io.OutputStream;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.biojava.bio.Annotation;
+import org.biojava.bio.seq.io.FastaFormat;
+import org.biojava.bio.seq.io.SeqIOTools;
+import org.biojava.bio.symbol.IllegalSymbolException;
+import org.flymine.model.genomic.BioEntity;
+import org.flymine.model.genomic.Gene;
+import org.flymine.model.genomic.LocatedSequenceFeature;
+import org.flymine.model.genomic.Protein;
+import org.flymine.model.genomic.Sequence;
+import org.flymine.model.genomic.Translation;
+import org.intermine.bio.web.biojava.BioSequence;
+import org.intermine.bio.web.biojava.BioSequenceFactory;
+import org.intermine.metadata.ClassDescriptor;
+import org.intermine.metadata.Model;
+import org.intermine.model.InterMineObject;
+import org.intermine.objectstore.ObjectStore;
+import org.intermine.util.TypeUtil;
+import org.intermine.web.logic.export.ExportException;
+import org.intermine.web.logic.export.ExportHelper;
+import org.intermine.web.logic.export.Exporter;
+import org.intermine.web.logic.results.ResultElement;
+
+
+/**
+ * Export data in FASTA format. Select cell in each row
+ * that can be exported as a sequence and fetch associated sequence.
+ * @author Jakub Kulaviak
+ **/
+public class SequenceExporterLogic implements Exporter
+{
+
+    private ObjectStore os;
+    private OutputStream out;
+    private int featureIndex;
+
+    /**
+     * Constructor.
+     * @param os object store used for fetching sequence for  exported object
+     * @param outputStream output stream
+     * @param featureIndex index of cell in row that contains object to be exported 
+     */
+    public SequenceExporterLogic(ObjectStore os, OutputStream outputStream,
+            int featureIndex) {
+        this.os = os;
+        this.out = outputStream;
+        this.featureIndex = featureIndex;
+    }
+    
+    /**
+     * {@inheritDoc}}
+     */
+    public int getWrittenResultsCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void export(List<List<ResultElement>> results) {
+        // IDs of the features we have successfully output - used to avoid duplicates
+        Set<Integer> exportedIDs = new HashSet<Integer>();
+        
+        try {
+            for (int rowIndex = 0; rowIndex < results.size(); rowIndex++) {
+                List<ResultElement> row = results.get(rowIndex);
+
+                StringBuffer header = new StringBuffer();
+
+                ResultElement resultElement = row.get(featureIndex);
+
+                BioSequence bioSequence;
+                Object object = os.getObjectById(resultElement.getId());
+                if (!(object instanceof InterMineObject)) {
+                    continue;
+                }
+
+                Integer objectId = ((InterMineObject) object).getId();
+                if (exportedIDs.contains(objectId)) {
+                    // exported already
+                    continue;
+                }
+
+                if (object instanceof LocatedSequenceFeature) {
+                    bioSequence = createLocatedSequenceFeature(header, object);
+                } else if (object instanceof Protein) {
+                    bioSequence = createProtein(header, object);
+                } else if (object instanceof Translation) {
+                    Model model = os.getModel();
+                    bioSequence = createTranslation(header, object, model);
+                } else {
+                    // ignore other objects
+                    continue;
+                }
+
+                if (bioSequence == null) {
+                    // the object doesn't have a sequence
+                    continue;
+                }
+
+                Annotation annotation = bioSequence.getAnnotation();
+                String headerString = header.toString();
+
+                if (row.size() > 1 && headerString.length() > 0) {
+                    annotation.setProperty(FastaFormat.PROPERTY_DESCRIPTIONLINE, headerString);
+                } else {
+                    if (object instanceof BioEntity) {
+                        annotation.setProperty(FastaFormat.PROPERTY_DESCRIPTIONLINE,
+                                               ((BioEntity) object).getPrimaryIdentifier());
+                    } else {
+                        // last resort
+                        annotation.setProperty(FastaFormat.PROPERTY_DESCRIPTIONLINE,
+                                               "sequence_" + exportedIDs.size());
+                    }
+                }
+
+                SeqIOTools.writeFasta(out, bioSequence);
+                exportedIDs.add(objectId);
+            }
+
+            if (out != null) {
+                out.close();
+            }
+        } catch (Exception e) {
+              throw new ExportException("Export failed.", e);
+        }
+    }
+
+    private BioSequence createTranslation(StringBuffer header, Object object,
+            Model model) throws IllegalSymbolException {
+        BioSequence bioSequence;
+        ClassDescriptor cld = model.getClassDescriptorByName(model.getPackageName()
+                                                             + "." + "Translation");
+        if (cld.getReferenceDescriptorByName("sequence", true) != null) {
+            Translation translation = (Translation) object;
+            bioSequence = BioSequenceFactory.make(translation);
+            header.append(translation.getPrimaryIdentifier());
+            header.append(' ');
+            if (translation.getName() == null) {
+                header.append("[unknown_name]");
+            } else {
+                header.append(translation.getName());
+            }
+            if (translation.getGene() != null) {
+                Gene gene = translation.getGene();
+                String geneIdentifier = gene.getPrimaryIdentifier();
+                if (geneIdentifier != null) {
+                    header.append(' ');
+                    header.append("gene:");
+                    header.append(geneIdentifier);
+                }
+            }
+        } else {
+            bioSequence = null;
+        }
+        return bioSequence;
+    }
+
+    private BioSequence createProtein(StringBuffer header, Object object)
+            throws IllegalSymbolException {
+        BioSequence bioSequence;
+        Protein protein = (Protein) object;
+        bioSequence = BioSequenceFactory.make(protein);
+        header.append(protein.getPrimaryIdentifier());
+        header.append(' ');
+        if (protein.getName() == null) {
+            header.append("[unknown_name]");
+        } else {
+            header.append(protein.getName());
+        }
+        Iterator iter = protein.getGenes().iterator();
+        while (iter.hasNext()) {
+            Gene gene = (Gene) iter.next();
+            String geneIdentifier = gene.getPrimaryIdentifier();
+            if (geneIdentifier != null) {
+                header.append(' ');
+                header.append("gene:");
+                header.append(geneIdentifier);
+            }
+
+        }
+        return bioSequence;
+    }
+
+    private BioSequence createLocatedSequenceFeature(StringBuffer header,
+            Object object) throws IllegalSymbolException {
+        BioSequence bioSequence;
+        LocatedSequenceFeature feature = (LocatedSequenceFeature) object;
+        bioSequence = BioSequenceFactory.make(feature);
+        if (feature.getPrimaryIdentifier() == null) {
+            if (feature instanceof Gene) {
+                header.append(((Gene) feature).getPrimaryIdentifier());
+            } else {
+                header.append("[unknown_identifier]");
+            }
+        } else {
+            header.append(feature.getPrimaryIdentifier());
+        }
+        header.append(' ');
+        if (feature.getName() == null) {
+            header.append("[unknown_name]");
+        } else {
+            header.append(feature.getName());
+        }
+        if (feature.getChromosomeLocation() != null) {
+            header.append(' ').append(feature.getChromosome().getPrimaryIdentifier());
+            header.append(':').append(feature.getChromosomeLocation().getStart());
+            header.append('-').append(feature.getChromosomeLocation().getEnd());
+            header.append(' ').append(feature.getLength());
+        }
+        try {
+            Gene gene = (Gene) TypeUtil.getFieldValue(feature, "gene");
+            if (gene != null) {
+                String geneIdentifier = gene.getPrimaryIdentifier();
+                if (geneIdentifier != null) {
+                    header.append(' ').append("gene:").append(geneIdentifier);
+                }
+            }
+        } catch (IllegalAccessException e) {
+            // ignore
+        }
+        return bioSequence;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean canExport(List<Class> clazzes) {
+        return canExport2(clazzes);
+    }
+
+    /**
+     * @param clazzes classes of result
+     * @return true if this exporter can export result composed of specified classes 
+     */
+    public static boolean canExport2(List<Class> clazzes) {        
+        return (
+                ExportHelper.getFirstColumnForClass(clazzes, LocatedSequenceFeature.class) >= 0 || 
+                ExportHelper.getFirstColumnForClass(clazzes, Protein.class) >= 0 ||
+                ExportHelper.getFirstColumnForClass(clazzes, Translation.class) >= 0 ||
+                ExportHelper.getFirstColumnForClass(clazzes, Sequence.class) >= 0);
+    }
+}
