94872e818a44d2a5cef621735954f3d3ff89a935
watkins <watkins>
2008-03-11 14:28:07 +0000

    Fixed some checkstyle errors

--- intermine/web/main/src/org/intermine/web/tags/disclosure/DisclosureDetailsTag.java
+++ intermine/web/main/src/org/intermine/web/tags/disclosure/DisclosureDetailsTag.java
@@ -34,7 +34,8 @@ public class DisclosureDetailsTag extends BaseDisclosureTag
 
     /**
      * Renders tag.
-     * @throws JspException, IOException exceptions
+     * @throws JspException if an error occurs
+     * @throws IOException if an error occurs
      */
    public void doTag() throws JspException, IOException {
        DisclosureHeadTag parent =  (DisclosureHeadTag) getParent();
