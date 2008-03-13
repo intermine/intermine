803cba231932498728f15c61aa4c8a7ff108a4bd
jakub <jakub>
2008-03-13 12:43:49 +0000

    Added simple exporters for table composed only from strings.

---
+++ intermine/web/main/src/org/intermine/web/logic/export/rowformatters/TabRowFormatter.java
@@ -0,0 +1,28 @@
+package org.intermine.web.logic.export.rowformatters;
+
+/*
+ * Copyright (C) 2002-2008 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import org.intermine.web.logic.export.RowFormatterImpl;
+
+/**
+ * 
+ * Row formatter formats line as tab separated strings.
+ * @author Jakub Kulaviak
+ **/
+public class TabRowFormatter extends RowFormatterImpl
+{
+    /**
+     * Constructor.
+     */
+    public TabRowFormatter() {
+        super("\t", false);
+    }    
+}
---
+++ intermine/web/main/src/org/intermine/web/logic/export/string/StringExporterImpl.java
@@ -0,0 +1,58 @@
+package org.intermine.web.logic.export.string;
+
+/*
+ * Copyright (C) 2002-2008 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.io.PrintWriter;
+import java.util.List;
+
+import org.intermine.web.logic.export.ExportException;
+import org.intermine.web.logic.export.RowFormatter;
+import org.intermine.web.logic.export.RowFormatterImpl;
+
+
+/**
+ * Simple exporter exporting table of strings. Format depends at given rowFormatter.
+ * @author Jakub Kulaviak
+ **/
+public class StringExporterImpl
+{
+
+    private PrintWriter writer;
+    private RowFormatter rowFormatter = new RowFormatterImpl(",", true);
+    
+    /**
+     * Constructor.
+     * @param writer used writer
+     * @param rowFormatter object formatting row from list of objects (in this case strings)
+     * to string flushed  as line
+     */
+    public StringExporterImpl(PrintWriter writer, RowFormatter rowFormatter) {
+        this.writer = writer;
+        this.rowFormatter = rowFormatter;
+    }
+    
+    /**
+     * Perform export.
+     * @param rows rows to be exported
+     */
+    @SuppressWarnings("unchecked")
+    public void export(List<List<String>> rows) {
+        try {
+            for (List<String> row : rows) {
+                // upcasting because rowFormatter expect List<Object>
+                writer.println(rowFormatter.format((List<Object>) (List) row));
+                writer.flush();
+            }
+        } catch (RuntimeException e) {
+            throw new ExportException("Export failed.", e);
+        }
+    }    
+}
