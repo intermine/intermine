5086266c63165327daa006d0c4b31d175e605139
jakub <jakub>
2008-03-13 12:43:49 +0000

    Added simple exporters for table composed only from strings.
    Former-commit-id: 803cba231932498728f15c61aa4c8a7ff108a4bd

---
+++ intermine/web/main/src/org/intermine/web/logic/export/rowformatters/CSVRowFormatter.java
@@ -0,0 +1,29 @@
+package org.intermine.web.logic.export.rowformatters;
+
+/*
+ * Copyright (C) 2002-2008 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import org.intermine.web.logic.export.RowFormatterImpl;
+
+
+/**
+ * Row formatter formats line as comma separated strings.
+ * @author Jakub Kulaviak
+ **/
+public class CSVRowFormatter extends RowFormatterImpl
+{
+
+    /**
+     * Constructor.
+     */
+    public CSVRowFormatter() {
+        super("\t", true);
+    }
+}
