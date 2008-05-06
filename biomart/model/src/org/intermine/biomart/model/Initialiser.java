ce1a1c43cf62d9ba18d12bbb4f541f8738d02c20
watkins <watkins>
2008-05-06 10:59:38 +0000

    Biomart project
    Former-commit-id: 17fda426c1ea6155eb5771e038e95a7e2f619a15

---
+++ biomart/model/src/org/intermine/biomart/model/Initialiser.java
@@ -0,0 +1,22 @@
+package org.intermine.biomart.model;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+/**
+ * @author "Xavier Watkins"
+ *
+ */
+public class Initialiser extends Task
+{
+
+    /* (non-Javadoc)
+     * @see org.apache.tools.ant.Task#execute()
+     */
+    @Override
+    public void execute() throws BuildException {
+        // TODO Read from the xml file and generate the model.
+        super.execute();
+    }
+
+}
