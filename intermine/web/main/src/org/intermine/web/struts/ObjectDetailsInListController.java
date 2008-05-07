0cd3b2d9d59978b151508be5233ff86956183e1d
watkins <watkins>
2008-05-07 13:37:36 +0000

    Moved Lists to the top of the object details page

---
+++ intermine/web/main/src/org/intermine/web/struts/ObjectDetailsInListController.java
@@ -0,0 +1,59 @@
+package org.intermine.web.struts;
+
+/*
+ * Copyright (C) 2002-2008 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.tiles.ComponentContext;
+import org.apache.struts.tiles.actions.TilesAction;
+import org.intermine.web.logic.search.Scope;
+import org.intermine.web.logic.search.WebSearchable;
+import org.intermine.web.logic.template.TemplateHelper;
+
+/**
+ * @author "Xavier Watkins"
+ *
+ */
+public class ObjectDetailsInListController extends TilesAction
+{
+   /**
+     * {@inheritDoc}
+     */
+   @Override
+   public ActionForward execute(ComponentContext context,
+                                @SuppressWarnings("unused") ActionMapping mapping,
+                                @SuppressWarnings("unused") ActionForm form,
+                                HttpServletRequest request,
+                                @SuppressWarnings("unused") HttpServletResponse response)
+   throws Exception {
+        String list = (String) context.getAttribute("list");
+        String objectid = (String) context.getAttribute("objectid");
+        Map globalWebSearchables = WebSearchableListController.getFilterWebSearchables(request,
+                        "bag", TemplateHelper.GLOBAL_TEMPLATE, null);
+        Map userWebSearchables = WebSearchableListController.getFilterWebSearchables(request,
+                        "bag", TemplateHelper.USER_TEMPLATE, null);
+        Map filteredWebSearchables = new HashMap<String, WebSearchable>(userWebSearchables);
+        filteredWebSearchables.putAll(globalWebSearchables);
+        if (list != null) {
+            filteredWebSearchables = WebSearchableListController.filterByList(
+                            filteredWebSearchables, list);
+        }
+        request.setAttribute("filteredWebSearchables", filteredWebSearchables);
+        return null;
+    }
+}
---
+++ intermine/webapp/main/resources/webapp/objectDetailsInList.jsp
@@ -0,0 +1,50 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html" %>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="im" %>
+<%@ taglib uri="/WEB-INF/struts-tiles.tld" prefix="tiles" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+
+<!-- objectDetailsInList.jsp -->
+<c:if test="${(!empty filteredWebSearchables) || (! empty PROFILE.savedBags)}">
+  <div class="heading">
+    Lists
+  </div>
+
+<div class="body" style="padding:10px;border:1px #CCC solid">
+<c:if test="${! empty filteredWebSearchables}">
+<div>
+      Lists in which this 
+      <c:forEach items="${object.clds}" var="cld">
+            ${cld.unqualifiedName}
+      </c:forEach> can be found:
+      <c:forEach items="${filteredWebSearchables}" var="webSearchable" varStatus="status">
+        <c:if test="${status.count > 1}">,&nbsp;</c:if>
+        <html:link href="bagDetails.do?bagName=${webSearchable.key}"><c:out value="${webSearchable.key}"/></html:link>
+      </c:forEach>
+</div>
+</c:if>
+ 
+<%-- Add to list --%>
+<c:if test="${!empty PROFILE.savedBags}">
+<div style="margin-top:10px">
+  <form action="<html:rewrite page="/addToBagAction.do"/>" method="POST">
+    <fmt:message key="objectDetails.addToBag"/>
+    <input type="hidden" name="__intermine_forward_params__" value="${pageContext.request.queryString}"/>
+    <select name="bag">
+      <option name="">--------</option>
+      <c:forEach items="${PROFILE.savedBags}" var="entry">
+       <c:if test="${empty filteredWebSearchables[entry.key]}">
+        <option name="${entry.key}">${entry.key}</option>
+       </c:if>
+      </c:forEach>
+    </select>
+    <input type="hidden" name="object" value="${object.id}"/>
+    <input type="submit" value="<fmt:message key="button.add"/>"/>
+  </form>
+</div>     
+
+</c:if>
+</div>
+</c:if>
+<!-- /objectDetailsInList.jsp -->
