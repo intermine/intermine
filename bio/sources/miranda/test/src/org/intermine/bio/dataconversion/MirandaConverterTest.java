fdb48989d981a0b4ce4a4d050b4b28be8e5fed4c
watkins <watkins>
2008-05-15 14:11:52 +0000

    Fixed test for miRanda
    Former-commit-id: 5e985170824ac09014933cdf49ff0c5097e42061

--- bio/sources/miranda/test/src/org/intermine/bio/dataconversion/MirandaConverterTest.java
+++ bio/sources/miranda/test/src/org/intermine/bio/dataconversion/MirandaConverterTest.java
@@ -10,23 +10,14 @@ package org.intermine.bio.dataconversion;
  *
  */
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.StringReader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
-import org.intermine.bio.io.gff3.GFF3Parser;
-import org.intermine.bio.io.gff3.GFF3Record;
 import org.intermine.dataconversion.ItemsTestCase;
 import org.intermine.dataconversion.MockItemWriter;
 import org.intermine.metadata.Model;
-import org.intermine.util.TypeUtil;
-import org.intermine.xml.full.Item;
-import org.intermine.xml.full.ItemFactory;
 
 /**
  * Test for data from miRanda
@@ -37,45 +28,39 @@ import org.intermine.xml.full.ItemFactory;
 public class MirandaConverterTest extends ItemsTestCase
 {
 
-    private Model tgtModel;
-    private MirandaConverter converter;
-    private MockItemWriter tgtIw = new MockItemWriter(new LinkedHashMap());
-    MockItemWriter itemWriter;
-    
     public MirandaConverterTest(String arg) {
         super(arg);
     }
 
     protected void setUp() throws Exception {
-        itemWriter = new MockItemWriter(new HashMap());
-        tgtModel = Model.getInstanceByName("genomic");
-        converter = new MirandaConverter(itemWriter, tgtModel);
+        super.setUp();
     }
 
     protected void tearDown() throws Exception {
         super.tearDown();
-        converter.close();
     }
     
-//    public void testMirandaHandler() throws Exception {
-//        String gff =
-//            "dme-miR-312\tmiRanda\tmiRNA_target\t9403\t9424\t16.9418\t+\t.\ttarget=CG11023-RA;score=3.057390e-02"
-//                     + ENDL
-//                     + "dme-miR-92b\tmiRanda\tmiRNA_target\t9403\t9424\t17.7377\t+\t.\ttarget=CG11023-RA;score=1.179130e-02"
-//                     + ENDL
-//                     + "dme-miR-313\tmiRanda\tmiRNA_target\t9404\t9424\t17.3966\t+\t.\ttarget=CG11023-RA;score=1.917020e-02"
-//                     + ENDL
-//                     + "dme-miR-7\tmiRanda\tmiRNA_target\t9445\t9467\t18.101\t+\t.\ttarget=CG11023-RA;score=1.308960e-02";
-//
-//        converter.process(new StringReader(gff));
-//        converter.close();
-//        
-//        // uncomment to write a new tgt items file
-////        writeItemsFile(itemWriter.getItems(), "/tmp/miranda-tgt-items.xml");
-//
-//        Set expected = readItemSet("miranda-tgt-items.xml");
-////       System.out.println(ItemsTestCase.compareItemSets(expected, allItems));
-//        assertEquals(expected, itemWriter.getItems());
-//    }
+    public void testMirandaHandler() throws Exception {
+        String gff =
+            "dme-miR-312\tmiRanda\tmiRNA_target\t9403\t9424\t16.9418\t+\t.\ttarget=CG11023-RA;score=3.057390e-02"
+                     + ENDL
+                     + "dme-miR-92b\tmiRanda\tmiRNA_target\t9403\t9424\t17.7377\t+\t.\ttarget=CG11023-RA;score=1.179130e-02"
+                     + ENDL
+                     + "dme-miR-313\tmiRanda\tmiRNA_target\t9404\t9424\t17.3966\t+\t.\ttarget=CG11023-RA;score=1.917020e-02"
+                     + ENDL
+                     + "dme-miR-7\tmiRanda\tmiRNA_target\t9445\t9467\t18.101\t+\t.\ttarget=CG11023-RA;score=1.308960e-02";
+        MockItemWriter itemWriter = new MockItemWriter(new HashMap());
+        MirandaConverter converter = new MirandaConverter(itemWriter, Model.getInstanceByName("genomic"));
+        converter.setCurrentFile(new File("v5.gff.drosophila_melanogaster"));
+        converter.process(new StringReader(gff));
+        converter.close();
+        
+        // uncomment to write a new tgt items file
+//        writeItemsFile(itemWriter.getItems(), "/tmp/miranda-tgt-items.xml");
+
+        Set expected = readItemSet("miranda-tgt-items.xml");
+//       System.out.println(ItemsTestCase.compareItemSets(expected, allItems));
+        assertEquals(expected, itemWriter.getItems());
+    }
 
 }
