5412be57dcf47e3624c035f907ed102d7041eda1
julie-sullivan <julie@flymine.org>
2008-05-16 09:14:21 +0000

    removed unused import

--- bio/sources/fly-fish/main/src/org/intermine/bio/dataconversion/FlyFishConverter.java
+++ bio/sources/fly-fish/main/src/org/intermine/bio/dataconversion/FlyFishConverter.java
@@ -18,7 +18,6 @@ import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.intermine.dataconversion.DataConverter;
 import org.intermine.dataconversion.FileConverter;
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
