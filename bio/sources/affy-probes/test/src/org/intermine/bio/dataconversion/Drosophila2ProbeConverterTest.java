be7aa7d2ec78127a706f8af9e05b73074c760f83
rnsmith <rns>
2008-05-16 15:48:48 +0000

    Updated affy-probes sources to use FlyBase IdResolver.

--- bio/sources/affy-probes/test/src/org/intermine/bio/dataconversion/Drosophila2ProbeConverterTest.java
+++ bio/sources/affy-probes/test/src/org/intermine/bio/dataconversion/Drosophila2ProbeConverterTest.java
@@ -10,36 +10,41 @@ package org.intermine.bio.dataconversion;
  *
  */
 
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Collections;
 import java.util.HashMap;
 
-import org.intermine.dataconversion.FileConverter;
 import org.intermine.dataconversion.ItemsTestCase;
 import org.intermine.dataconversion.MockItemWriter;
 import org.intermine.metadata.Model;
 
-import java.io.InputStreamReader;
-import java.io.Reader;
-
 public class Drosophila2ProbeConverterTest extends ItemsTestCase
 {
     private String ENDL = System.getProperty("line.separator");
-
+    Model model = Model.getInstanceByName("genomic");
+    Drosophila2ProbeConverter converter;
+    MockItemWriter itemWriter;
+    
     public Drosophila2ProbeConverterTest(String arg) {
         super(arg);
     }
 
     public void setUp() throws Exception {
         super.setUp();
+        itemWriter = new MockItemWriter(new HashMap());
+        converter = new Drosophila2ProbeConverter(itemWriter, model);  
+        MockIdResolverFactory resolverFactory = new MockIdResolverFactory("Gene");
+        resolverFactory.addResolverEntry("7227", "FBgn001", Collections.singleton("CG9042"));
+        resolverFactory.addResolverEntry("7227", "FBgn002", Collections.singleton("CG33057"));
+        resolverFactory.addResolverEntry("7227", "FBgn003", Collections.singleton("CG7163"));
+        converter.resolverFactory = resolverFactory;
     }
 
     public void testProcess() throws Exception {
 
         Reader reader = new InputStreamReader(getClass().getClassLoader()
                                               .getResourceAsStream("Drosophilia2ProbeConverterTest_src.csv"));
-        
-        MockItemWriter itemWriter = new MockItemWriter(new HashMap());
-        FileConverter converter = new Drosophila2ProbeConverter(itemWriter,
-                                                                Model.getInstanceByName("genomic"));
         converter.process(reader);
         converter.close();
 
