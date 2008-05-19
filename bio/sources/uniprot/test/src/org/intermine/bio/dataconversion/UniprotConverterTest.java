06f007bef296ab951c5eae4c9b193a2d03b7ac26
julie-sullivan <julie@flymine.org>
2008-05-19 08:57:12 +0000

    Moved code from constructor to setUp() method in !UniProt test

--- bio/sources/uniprot/test/src/org/intermine/bio/dataconversion/UniprotConverterTest.java
+++ bio/sources/uniprot/test/src/org/intermine/bio/dataconversion/UniprotConverterTest.java
@@ -22,27 +22,25 @@ import org.intermine.metadata.Model;
 
 public class UniprotConverterTest extends ItemsTestCase
 {
-
-    private IdResolver resolver;
     private UniprotConverter converter;
     private MockItemWriter itemWriter;
 
     public UniprotConverterTest(String arg) {
         super(arg);
-        itemWriter = new MockItemWriter(new HashMap());
-        converter = new UniprotConverter(itemWriter, Model.getInstanceByName("genomic"));
-        MockIdResolverFactory resolverFactory = new MockIdResolverFactory("Gene");
-        resolverFactory = new MockIdResolverFactory("Gene");
-        resolverFactory.addResolverEntry("7227", "FBgn0037874", Collections.singleton("CG4800"));
-        resolverFactory.addResolverEntry("7227", "FBgn0020415", Collections.singleton("CG4475"));
-        resolverFactory.addResolverEntry("7227", "FBgn0039830", Collections.singleton("CG1746"));
-        resolverFactory.addResolverEntry("7227", "FBgn0019830", Collections.singleton("CG3057"));
-        converter.resolverFactory = resolverFactory;
+
 //        resolverFactory = new FlyBaseIdResolverFactory();
 //        converter.resolverFactory = resolverFactory;
     }
 
     public void setUp() throws Exception {
+        itemWriter = new MockItemWriter(new HashMap());
+        converter = new UniprotConverter(itemWriter, Model.getInstanceByName("genomic"));
+        MockIdResolverFactory resolverFactory = new MockIdResolverFactory("Gene");
+        resolverFactory.addResolverEntry("7227", "FBgn0037874", Collections.singleton("CG4800"));
+        resolverFactory.addResolverEntry("7227", "FBgn0020415", Collections.singleton("CG4475"));
+        resolverFactory.addResolverEntry("7227", "FBgn0039830", Collections.singleton("CG1746"));
+        resolverFactory.addResolverEntry("7227", "FBgn0019830", Collections.singleton("CG3057"));
+        converter.resolverFactory = resolverFactory;
         super.setUp();
     }
 
