6992a4557a8d9cbfa4a8cc3bd1a2e4060982b69f
mm719 <mm719>
2008-05-27 15:42:06 +0000

    edit view of protein interaction widget

--- bio/webapp/src/org/intermine/bio/web/widget/ProteinInteractionURLQuery.java
+++ bio/webapp/src/org/intermine/bio/web/widget/ProteinInteractionURLQuery.java
@@ -77,11 +77,11 @@ public class ProteinInteractionURLQuery implements WidgetURLQuery
         view.add(primaryIdentifier);
         view.add(primaryAccesion);
         view.add(interactPrimaryIdentifier);
+	view.add(interactPathRole);
         view.add(interactPrimaryAccession);
         view.add(interactName);
         view.add(interactPubMedId);
         view.add(interactShortname);
-        view.add(interactPathRole);
 
         q.setView(view);
 
