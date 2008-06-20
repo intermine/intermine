d4ffad0089fb1f90f16fd8bad04f33e9aa3463b9
kmr <kmr>
2008-06-20 12:16:22 +0000

    Fixed HomophilaConverterTest.
    Former-commit-id: e8f0709934836d4b370f00d954f0d987dbc5e2ee

--- bio/sources/homophila/test/src/org/intermine/bio/dataconversion/HomophilaConverterTest.java
+++ bio/sources/homophila/test/src/org/intermine/bio/dataconversion/HomophilaConverterTest.java
@@ -61,7 +61,7 @@ public class HomophilaConverterTest extends ItemsTestCase
         converter.close();
 
         // uncomment to create a new target items files
-        // writeItemsFile(itemWriter.getItems(), "homophila-tgt-items.xml");
+        //writeItemsFile(itemWriter.getItems(), "/tmp/homophila-tgt-items.xml");
 
         Set expected = readItemSet("test/HomophilaConverterTest.xml");
         assertEquals(expected, itemWriter.getItems());
