be5bfa39f4527c7b08501b4889e0d29d7436e6d0
julie-sullivan <julie@flymine.org>
2008-06-23 12:32:19 +0000

    updated biogrid's dataset
    Former-commit-id: acfcc07f17392bf1367316d56c98ce90f1f3281c

--- bio/sources/biogrid/main/src/org/intermine/bio/dataconversion/BioGridConverter.java
+++ bio/sources/biogrid/main/src/org/intermine/bio/dataconversion/BioGridConverter.java
@@ -52,7 +52,7 @@ public class BioGridConverter extends BioFileConverter
      * @param model the Model
      */
     public BioGridConverter(ItemWriter writer, Model model) {
-        super(writer, model, "BioGrid", "BioGrid data set");
+        super(writer, model, "BioGRID", "BioGRID data set");
 
         // only construct factory here so can be replaced by mock factory in tests
         resolverFactory = new FlyBaseIdResolverFactory();
