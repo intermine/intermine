6bb2f26ef3d52aedba98f2f03a18d002d3dc0ba7
kmr <kmr>
2008-07-30 17:49:01 +0000

    Fixed pattern for flybase deletion locations.

--- bio/sources/chado-db/main/src/org/intermine/bio/dataconversion/FlyBaseModuleProcessor.java
+++ bio/sources/chado-db/main/src/org/intermine/bio/dataconversion/FlyBaseModuleProcessor.java
@@ -1,8 +1,6 @@
 package org.intermine.bio.dataconversion;
 
 /*
- * Copyright (C) 2002-2008 FlyMine
- *
  * This code may be freely distributed and modified under the
  * terms of the GNU Lesser General Public Licence.  This should
  * be distributed with the code.  See the LICENSE file for more
@@ -599,7 +597,7 @@ public class FlyBaseModuleProcessor extends ChadoSequenceProcessor
     }
 
     private static final Pattern DELETION_LOC_PATTERN =
-        Pattern.compile("^([^:]+):(\\d+)..(\\d+) \\(([^;\\)]+);?([^\\)]*)\\)$");
+        Pattern.compile("^([^:]+):(\\d+)(?:-\\d+)?..(?:\\d+-)?(\\d+) \\(([^;\\)]+);?([^\\)]*)\\)$");
 
     /**
      * Create Location objects for deletions (chromosome_structure_variation) as they don't have
