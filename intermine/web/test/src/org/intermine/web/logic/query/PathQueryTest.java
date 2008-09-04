a220b1f5a77b18f1adb1b6df2bb435afc7539adc
julie-sullivan <julie@flymine.org>
2008-09-04 15:57:20 +0000

    validate order by, more documentation, tests
    Former-commit-id: 8c95f0ae5bbaf95d4c964ec3a239d484f151e8f5

--- intermine/web/test/src/org/intermine/web/logic/query/PathQueryTest.java
+++ intermine/web/test/src/org/intermine/web/logic/query/PathQueryTest.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,6 +16,7 @@ import org.intermine.objectstore.query.ResultsInfo;
 import org.intermine.path.Path;
 import org.intermine.path.PathError;
 import org.intermine.pathquery.Constraints;
+import org.intermine.pathquery.OrderBy;
 import org.intermine.pathquery.PathQuery;
 import org.intermine.pathquery.PathQueryBinding;
 
@@ -376,19 +378,63 @@ public class PathQueryTest extends TestCase
         q.addView(view);
         q.addConstraint("Employee.department.name", Constraints.eq("DepartmentA1"));
         assertEquals(e.getAllConstraints(), q.getAllConstraints());
+
+        // TODO bad constraints, multiple constraints, different ops
+
     }
 
+    public void testAddConstraintStringConstraintString() {
+        e = (PathQuery) expected.get("employeeDepartmentCompany");
+        q = new PathQuery(model);
+        List<String> view = new ArrayList<String>() {{
+            add("Employee.name");
+            add("Employee.department.name");
+            add("Employee.department.company.name");
+        }};
+        q.addView(view);
+        q.addConstraint("Employee.department.name", Constraints.eq("DepartmentA1"));
+        assertEquals(e.getAllConstraints(), q.getAllConstraints());
+    }
 
-//    public void testAddConstraintStringConstraintString() {
-//        fail("Not yet implemented");
-//    }
-//
-//    public void testGetAllConstraints() {
-//        fail("Not yet implemented");
-//    }
+    public void testAddConstraintSubclass() {
+        e = (PathQuery) expected.get("employeeDepartmentCompany");
+        q = new PathQuery(model);
+        List<String> view = new ArrayList<String>() {{
+            add("Employee.name");
+            add("Employee.department.name");
+            add("Employee.department.company.name");
+        }};
+        q.addView(view);
+        q.addConstraint("Employee.department.name", Constraints.eq("DepartmentA1"));
+        assertEquals(e.getAllConstraints(), q.getAllConstraints());
+    }
+
+    public void testGetAllConstraints() {
+
+        // no constraints
+        e = (PathQuery) expected.get("employeeName");
+        assertEquals(e.getAllConstraints(), new ArrayList());
+
+        // one constraint
+        e = (PathQuery) expected.get("employeeDepartmentCompany");
+        q = new PathQuery(model);
+        List<String> view = new ArrayList<String>() {{
+            add("Employee.name");
+            add("Employee.department.name");
+            add("Employee.department.company.name");
+        }};
+        q.addView(view);
+        q.addConstraint("Employee.department.name", Constraints.eq("DepartmentA1"));
+        assertEquals(e.getAllConstraints(), q.getAllConstraints());
+
+        // multiple constraints
+        e = (PathQuery) expected.get("departmentBagConstraint");
+        assertEquals(2, e.getAllConstraints().size());
+    }
 
     public void testGetConstraintLogic() {
 
+        // null logic
         e = (PathQuery) expected.get("employeeDepartmentCompany");
         q = new PathQuery(model);
         List<String> view = new ArrayList<String>() {{
@@ -400,16 +446,15 @@ public class PathQueryTest extends TestCase
         q.addConstraint("Employee.department.name", Constraints.eq("DepartmentA1"));
         assertEquals(e.getConstraintLogic(), q.getConstraintLogic());
 
+        // A and B
         e = (PathQuery) expected.get("departmentBagConstraint");
         assertEquals(e.getConstraintLogic(), "A and B");
 
-        // test me!
-//        q.addConstraint("Employee.name", Constraints.eq("monkey"));
-//        assertEquals(e.getConstraintLogic(), q.getConstraintLogic());
-
+        // TODO test bad logic, OR logic, A and B and C, logic with user-created code
 
     }
 
+    // same as above, except above method returns string
     public void testGetLogic() {
 
         // null logic
@@ -421,24 +466,57 @@ public class PathQueryTest extends TestCase
         }};
         q.addView(view);
         q.addConstraint("Employee.department.name", Constraints.eq("DepartmentA1"));
-        assertEquals(null, q.getLogic());
-
-        // TODO test A and B, A or B, bad logic
-
+        assertNull(q.getLogic());
     }
 
 //    public void testSyncLogicExpression() {
 //        fail("Not yet implemented");
 //    }
-//
-//    public void testGetUnusedConstraintCode() {
-//        fail("Not yet implemented");
-//    }
-//
-//    public void testGetConstraintByCode() {
-//        fail("Not yet implemented");
-//    }
-//
+
+    public void testGetUnusedConstraintCode() {
+
+        // A
+        q = new PathQuery(model);
+        assertEquals("A", q.getUnusedConstraintCode());
+
+        // B
+        q.addConstraint("Employee.department.name", Constraints.eq("DepartmentA1"));
+        assertEquals("B", q.getUnusedConstraintCode());
+
+        // B
+        e = (PathQuery) expected.get("employeeDepartmentCompany");
+        assertEquals("B", e.getUnusedConstraintCode());
+
+        // C
+        e = (PathQuery) expected.get("departmentBagConstraint");
+        assertEquals("C", e.getUnusedConstraintCode());
+
+        // add a constraint, but assign a different code
+        e.addConstraint("Department.name", Constraints.eq("monkey"), "pants");
+        assertEquals("C", e.getUnusedConstraintCode());
+
+    }
+
+    public void testGetConstraintByCode() {
+
+        // monkey
+        q = new PathQuery(model);
+        q.addConstraint("Employee.department.name", Constraints.eq("*pants*"), "monkey");
+        assertEquals("Constraint(=, *pants*)", q.getConstraintByCode("monkey").toString());
+
+        // A
+        e = (PathQuery) expected.get("employeeDepartmentCompany");
+        assertEquals("Constraint(=, DepartmentA1)", e.getConstraintByCode("A").toString());
+
+        // B
+        e = (PathQuery) expected.get("departmentBagConstraint");
+        assertEquals("Constraint(LOOKUP, *ABC*)", e.getConstraintByCode("B").toString());
+
+        // null
+        assertNull(e.getConstraintByCode("monkey"));
+
+    }
+
 //    public void testAddCodesToLogic() {
 //        fail("Not yet implemented");
 //    }
@@ -560,18 +638,167 @@ public class PathQueryTest extends TestCase
 
     }
 
-//    public void testSetOrderByListOfString() {
-//        fail("Not yet implemented");
-//    }
-//
-//    public void testSetOrderByListOfStringBoolean() {
-//        fail("Not yet implemented");
-//    }
-//
-//    public void testSetOrderByList() {
-//        fail("Not yet implemented");
-//    }
-//
+    public void testSetOrderByListOfString() {
+
+        // empty
+        e = (PathQuery) expected.get("orderByAsc");
+        q = new PathQuery(model);
+        List orderBy = new ArrayList<String>() {{
+            add("");
+        }};
+        q.setOrderBy(orderBy);
+        assertEquals(new ArrayList(), q.getSortOrder());
+
+        // null
+        e = (PathQuery) expected.get("orderByAsc");
+        q = new PathQuery(model);
+        orderBy = new ArrayList<String>() {{
+            add(null);
+        }};
+        q.setOrderBy(orderBy);
+        assertEquals(new ArrayList(), q.getSortOrder());
+
+    }
+
+    public void testSetOrderByListOfStringBoolean() {
+
+        // empty
+        e = (PathQuery) expected.get("orderByAsc");
+        q = new PathQuery(model);
+        List orderBy = new ArrayList<String>() {{
+            add("");
+        }};
+        q.setOrderBy(orderBy, true);
+        assertEquals(new ArrayList(), q.getSortOrder());
+
+        // null
+        e = (PathQuery) expected.get("orderByAsc");
+        q = new PathQuery(model);
+        orderBy = new ArrayList<String>() {{
+            add(null);
+        }};
+        q.setOrderBy(orderBy, true);
+        assertEquals(new ArrayList(), q.getSortOrder());
+
+        // one
+        e = (PathQuery) expected.get("orderByAsc");
+        q = new PathQuery(model);
+        orderBy = new ArrayList<String>() {{
+            add("Employee.name");
+        }};
+        q.setOrderBy(orderBy, true);
+        assertEquals(e.getSortOrder(), q.getSortOrder());
+
+        // desc
+        e = (PathQuery) expected.get("orderByDesc");
+        q = new PathQuery(model);
+        orderBy = new ArrayList<String>() {{
+            add("Employee.name");
+        }};
+        q.setOrderBy(orderBy, false);
+        assertEquals(e.getSortOrder(), q.getSortOrder());
+
+        //three
+        e = (PathQuery) expected.get("orderByVat");
+        q = new PathQuery(model);
+        orderBy = new ArrayList<String>() {{
+            add("Company.vatNumber");
+            add("Company.name");
+            add("Company.departments.name");
+        }};
+        q.setOrderBy(orderBy, true);
+        assertEquals(e.getSortOrder(), q.getSortOrder());
+
+        // bad path
+        q = new PathQuery(model);
+        orderBy = new ArrayList<String>() {{
+            add("Monkey.paths");
+        }};
+        q.setOrderBy(orderBy, true);
+        assertTrue(q.getSortOrderStrings().isEmpty());
+        assertFalse(q.isValid());
+
+        // bad path with good paths
+        q = new PathQuery(model);
+        orderBy = new ArrayList<String>() {{
+            add("Company.vatNumber");
+            add("Company.name");
+            add("Monkey.paths");
+            add("Company.departments.name");
+        }};
+        q.setOrderBy(orderBy, true);
+        assertEquals(e.getSortOrderStrings(), q.getSortOrderStrings());
+        assertFalse(q.isValid());
+
+        // bad path with empty path
+        q = new PathQuery(model);
+        orderBy = new ArrayList<String>() {{
+            add("Company.vatNumber");
+            add("");
+            add("Company.name");
+            add("Company.departments.name");
+        }};
+        q.setOrderBy(orderBy, true);
+        assertEquals(e.getSortOrderStrings(), q.getSortOrderStrings());
+        assertFalse(q.isValid());
+
+        // bad paths with empty path
+        q = new PathQuery(model);
+        orderBy = new ArrayList<String>() {{
+            String isnull = null;
+            add(isnull);
+            add("Company.vatNumber");
+            add("Company.name");
+            add("Company.departments.name");
+        }};
+        q.setOrderBy(orderBy, true);
+        assertEquals(e.getSortOrderStrings(), q.getSortOrderStrings());
+        assertFalse(q.isValid());
+    }
+
+    public void testSetOrderByList() {
+
+        e = (PathQuery) expected.get("orderByVat");
+        q = new PathQuery(model);
+        List<OrderBy> orderBy = new ArrayList() {{
+            Path p =  PathQuery.makePath(model, q, "Company.vatNumber");
+            add(new OrderBy(p, "asc"));
+            p = PathQuery.makePath(model, q, "Company.name");
+            add(new OrderBy(p, "asc"));
+            p = PathQuery.makePath(model, q, "Company.departments.name");
+            add(new OrderBy(p, "asc"));
+        }};
+        q.setOrderByList(orderBy);
+        assertEquals(e.getSortOrder(), q.getSortOrder());
+
+        // adding the same path again, should reset
+        orderBy = new ArrayList<OrderBy>() {{
+            Path p =  PathQuery.makePath(model, q, "Company.name");
+            add(new OrderBy(p, "asc"));
+        }};
+        q.setOrderByList(orderBy);
+        assertEquals(1, q.getSortOrder().size());
+
+        // null paths
+        e = (PathQuery) expected.get("orderByVat");
+        q = new PathQuery(model);
+        orderBy = new ArrayList<OrderBy>() {{
+            Path p = null;
+            add(new OrderBy(p, ""));
+            p = PathQuery.makePath(model, q, "Company.vatNumber");
+            add(new OrderBy(p, "asc"));
+            p = PathQuery.makePath(model, q, "Company.name");
+            add(new OrderBy(p, "asc"));
+            p = PathQuery.makePath(model, q, "Company.departments.name");
+            add(new OrderBy(p, "asc"));
+
+        }};
+        q.setOrderByList(orderBy);
+        assertEquals(e.getSortOrder(), q.getSortOrder());
+        assertFalse(q.isValid());
+
+    }
+
 //    public void testAddOrderByString() {
 //        fail("Not yet implemented");
 //    }
@@ -617,10 +844,16 @@ public class PathQueryTest extends TestCase
         q.getModel().equals(model);
     }
 
-//    public void testGetNodes() {
-//        fail("Not yet implemented");
-//    }
-//
+    public void testGetNodes() {
+        // no nodes
+        q = new PathQuery(model);
+        assertEquals(new HashMap(), q.getNodes());
+
+        // 3
+        e = (PathQuery) expected.get("departmentBagConstraint");
+        assertEquals(3, e.getNodes().size());
+    }
+
 //    public void testGetNode() {
 //        fail("Not yet implemented");
 //    }
@@ -628,13 +861,18 @@ public class PathQueryTest extends TestCase
 //    public void testAddNode() {
 //        fail("Not yet implemented");
 //    }
-//
-//    public void testClone() {
-//        fail("Not yet implemented");
-//    }
-//
+
+    public void testClone() {
+        e = (PathQuery) expected.get("departmentBagConstraint");
+        q = e.clone();
+        assertEquals(e, q);
+
+        // TODO no constraints, many constraints, no view, bad view, no sort order
+        // subclasses, problems
+    }
+
 //    public void testCloneNode() {
-//        fail("Not yet implemented");
+//        // TODO test me
 //    }
 
     public void testInfo() {
