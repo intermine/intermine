757a3c8b71839bf19eacd1b70c007d5779ca98e4
kmr <kmr>
2008-09-09 14:10:09 +0000

    Split code from ChadoSequenceProcessor into one class per file for ConfigAction classes.

---
+++ bio/sources/chado-db/main/src/org/intermine/bio/chado/config/ConfigAction.java
@@ -0,0 +1,19 @@
+package org.intermine.bio.chado.config;
+
+/*
+ * Copyright (C) 2002-2008 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+/**
+ * A class that represents an action while processing synonyms, dbxrefs, etc.
+ * @author Kim Rutherford
+ */
+public class ConfigAction
+{
+    // empty
+}
---
+++ bio/sources/chado-db/main/src/org/intermine/bio/chado/config/CreateSynonymAction.java
@@ -0,0 +1,57 @@
+package org.intermine.bio.chado.config;
+
+/*
+ * Copyright (C) 2002-2008 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.regex.Pattern;
+
+/**
+ * An action that sets a Synonym.
+ */
+public class CreateSynonymAction extends MatchingFieldConfigAction
+{
+    private final String synonymType;
+
+    /**
+     * Make a synonym and use the type from chado ("symbol", "identifier" etc.) as the Synonym
+     * type
+     */
+    public CreateSynonymAction() {
+        super(null);
+        synonymType = null;
+    }
+
+    /**
+     * Make a synonym and use the type from chado ("symbol", "identifier" etc.) as the Synonym
+     * type.  Only create the synonym if the pattern matches.
+     * @param pattern the pattern that the value must match
+     */
+    public CreateSynonymAction(Pattern pattern) {
+        super(pattern);
+        synonymType = null;
+    }
+
+    /**
+     * Make a synonym and use given type as the Synonym type
+     * @param synonymType the synonym type
+     */
+    public CreateSynonymAction(String synonymType) {
+        super(null);
+        this.synonymType = synonymType;
+    }
+
+    /**
+     * Return the synonym type that was passed to the constructor.
+     * @return the synonym type
+     */
+    public String getSynonymType() {
+        return synonymType;
+    }
+}
