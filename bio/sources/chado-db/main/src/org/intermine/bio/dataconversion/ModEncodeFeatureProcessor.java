7537b4b94c9b3192397069e9fa9d57d1790d6621
sergiocontrino <sergi@modencode.org>
2008-10-16 13:22:49 +0000

    modmine meta: added histoneBindingSite and changed reference from submissionData to feature (now features is a collection)
    Former-commit-id: d36078d2b6a56ac049d3c7a77ab9c6a1b737c337

--- bio/sources/chado-db/main/src/org/intermine/bio/dataconversion/ModEncodeFeatureProcessor.java
+++ bio/sources/chado-db/main/src/org/intermine/bio/dataconversion/ModEncodeFeatureProcessor.java
@@ -48,7 +48,8 @@ public class ModEncodeFeatureProcessor extends ChadoSequenceProcessor
          "five_prime_untranslated_region",
          "five_prime_UTR", "three_prime_untranslated_region",
          "three_prime_UTR", "origin_of_replication",
-         "binding_site", "protein_binding_site", "TF_binding_site", "transcript_region"
+         "binding_site", "protein_binding_site", "TF_binding_site", 
+         "transcript_region", "histone_binding_site"
     );
 
     /**
