78f4d189bea2e7045613efa9cc6f256e8e5a29a2
jakub <jakub>
2008-10-29 16:52:08 +0000

    Script fixed.
    Former-commit-id: 5360f8b38c09c25657226d0674a2bddd7bba7a57

--- intermine/webservice/client/samples/compile-run.sh
+++ intermine/webservice/client/samples/compile-run.sh
@@ -13,7 +13,9 @@ if !(test -e ${FILE}); then
     exit; 
 fi;
 
+CLASSPATH=../../lib/intermine-objectstore.jar:../../lib/intermine-pathquery.jar:../../lib/commons-codec-1.3.jar:../../lib/commons-httpclient-3.0.jar:../../lib/commons-logging-1.1.1.jar:../../lib/intermine-client.jar:../../lib/log4j.jar:../../lib/stax-api-1.0.jar:../../lib/stax-ri-1.0.jar
+
 cd $1;
 mkdir -p build;
-javac -d ./build -classpath ../../lib/commons-codec-1.3.jar:../../lib/commons-httpclient-3.0.jar:../../lib/commons-logging-1.1.1.jar:../../lib/intermine-client.jar:../../lib/intermine-pathquery.jar:../../lib/log4j.jar ./src/samples/$1.java;
-java -classpath ./build:../../lib/commons-codec-1.3.jar:../../lib/commons-httpclient-3.0.jar:../../lib/commons-logging-1.1.1.jar:../../lib/intermine-client.jar:../../lib/intermine-pathquery.jar:../../lib/log4j.jar samples.$1;
+javac -d ./build -classpath ${CLASSPATH} ./src/samples/$1.java && \
+java -classpath ./build:${CLASSPATH} samples.$1;
