297f588259de11edbec5870a7e36b51eaabcdd60
kmr <kmr>
2008-11-04 18:01:08 +0000

    Added make-new-mine-props script and added InterMine::Util::get_latest_properties_version()
    Former-commit-id: 56833289eb01165f4424c292f7035557b4ded2f0

--- intermine/perl/lib/InterMine/Util.pm
+++ intermine/perl/lib/InterMine/Util.pm
@@ -11,7 +11,7 @@ use strict;
 use Exporter;
 
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(get_property_value $INTERMINE_CONF_DIR);
+our @EXPORT_OK = qw(get_property_value get_latest_properties_version $INTERMINE_CONF_DIR);
 
 # location of the InterMine properties files
 our $INTERMINE_CONF_DIR = "$ENV{HOME}/.intermine";
@@ -46,6 +46,45 @@ sub get_property_value
   close F;
 
   return $ret_val;
+}
+
+=head2 get_latest_properties_version
+ Title   : get_latest_properties_version
+ Usage   : $version = 
+              get_latest_properties_version('flymine.properties.r');
+ Function: return the version of the most properties file by looking for files
+           starting with the prefix, eg. if these files exist:
+           'flymine.properties.r1', 'flymine.properties.r2',
+           'flymine.properties.r3' then return "3" as an integer
+ Args    : $property_file_name_prefix
+=cut
+sub get_latest_properties_version
+{
+  my $prop_file_prefix = shift;
+
+  my $current_version = -1;
+
+  opendir DIR, $INTERMINE_CONF_DIR
+    or die "can't open directory $INTERMINE_CONF_DIR for reading\n";
+
+  while (defined(my $filename = readdir(DIR))) {
+    if ($filename =~ /^$prop_file_prefix(\d+)$/) {
+      if ($1 > $current_version) {
+        $current_version = $1;
+      }
+    }
+  }
+
+  closedir DIR;
+
+  if ($current_version == -1) {
+    die <<"MESSAGE";
+$0: can't find a properties file with prefix $prop_file_prefix -
+looked for ${prop_file_prefix}0, ${prop_file_prefix}1, ${prop_file_prefix}2, etc
+MESSAGE
+  }
+
+  return $current_version;
 }
 
 1;
