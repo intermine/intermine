f5956d4c63d0b0419d26317f908e1ec8a2b8ba42
kmr <kmr>
2009-01-20 17:27:35 +0000

    Changed QueryService test to create a PathQuery and to use the ModelService.

--- intermine/perl/samples/query_service.pl
+++ intermine/perl/samples/query_service.pl
@@ -3,19 +3,27 @@
 use strict;
 use warnings;
 
+use InterMine::PathQuery;
 use InterMine::WebService::Service::QueryService;
+use InterMine::WebService::Service::ModelService;
 
-my $service = new InterMine::WebService::Service::QueryService('http://www.flymine.org/query/service', 'service_example');
+my @service_args = ('http://www.flymine.org/query/service', 'service_example');
 
-my $query = q(
-   <query name="" model="genomic" view="Organism.name Organism.taxonId" sortOrder="Organism.name"/>
-);
+my $query_service = new InterMine::WebService::Service::QueryService(@service_args);
+my $model_service = new InterMine::WebService::Service::ModelService(@service_args);
 
-my $count = $service->get_count($query);
+my $path_query = new InterMine::PathQuery($model_service->get_model());
+
+$path_query->add_view("Organism.name Organism.taxonId");
+$path_query->sort_order("Organism.name");
+
+warn 'xml: ', $path_query->to_xml_string(), "\n";
+
+my $count = $query_service->get_count($path_query);
 
 print "result count: $count\n";
 
-my $res = $service->get_result($query);
+my $res = $query_service->get_result($path_query);
 
 print $res->content();
 
