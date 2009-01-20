b43150bd1a19f9ef886f21509d5f821c5745ca77
kmr <kmr>
2009-01-20 17:35:51 +0000

    Added missing documentation.
    Former-commit-id: 43affffe68e5ce9b3b984b34d8131e90aa9d0a57

--- intermine/perl/lib/InterMine/Model/Field.pm
+++ intermine/perl/lib/InterMine/Model/Field.pm
@@ -1,5 +1,46 @@
 package InterMine::Model::Field;
 
+=head1 NAME
+
+InterMine::Model::Field - Representation of a field of a class
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+FlyMine C<< <support@flymine.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<support@flymine.org>.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc InterMine::Model::Field
+
+You can also look for information at:
+
+=over 4
+
+=item * FlyMine
+
+L<http://www.flymine.org>
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 FlyMine, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 FUNCTIONS
+
+=cut
+
 use strict;
 
 my %type_map = (
@@ -10,6 +51,15 @@ my %type_map = (
                 'java.lang.Integer' => 'int'
                );
 
+=head2 new
+
+ Usage   : this is an abstract class, construct an Attribute, Collection or
+           Reference instead
+ Function: create a new Field object
+ Args    : name - the field name
+           model - the Model
+
+=cut
 sub new
 {
   my $class = shift;
@@ -27,19 +77,38 @@ sub new
   return $self;
 }
 
+=head2
+
+ Usage   : $name = $field->field_name();
+ Function: return the name of this field
+
+=cut
 sub field_name
 {
   my $self = shift;
   return $self->{name};
 }
 
+=head2
+
+ Usage   : $name = $field->field_type();
+ Function: return the type of this field, "Attribute", "Reference" or
+           "Collection"
+
+=cut
 sub field_type
 {
   my $self = shift;
   return lc (((ref $self) =~ /.*::(.*)/)[0]);
 }
 
-# returns the ClassDescriptor of the (base) class that defines this field
+=head2
+
+ Usage   : my $class = $field->field_class();
+ Function: returns the ClassDescriptor of the (base) class that defines this
+           field
+
+=cut
 sub field_class
 {
   my $self = shift;
