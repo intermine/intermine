d39952e2d825c121916919cbe628f270f7a6c1e7
rnsmith <rns>
2009-02-20 14:25:24 +0000

    Added missing class from check in.

---
+++ bio/sources/go-annotation/main/src/org/intermine/bio/postprocess/GoPostprocess.java
@@ -0,0 +1,164 @@
+package org.intermine.bio.postprocess;
+
+/*
+ * Copyright (C) 2002-2009 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.flymine.model.genomic.GOAnnotation;
+import org.flymine.model.genomic.Gene;
+import org.flymine.model.genomic.Protein;
+import org.intermine.bio.util.Constants;
+import org.intermine.objectstore.ObjectStore;
+import org.intermine.objectstore.ObjectStoreException;
+import org.intermine.objectstore.ObjectStoreWriter;
+import org.intermine.objectstore.intermine.ObjectStoreInterMineImpl;
+import org.intermine.objectstore.query.ConstraintOp;
+import org.intermine.objectstore.query.ConstraintSet;
+import org.intermine.objectstore.query.ContainsConstraint;
+import org.intermine.objectstore.query.Query;
+import org.intermine.objectstore.query.QueryClass;
+import org.intermine.objectstore.query.QueryCollectionReference;
+import org.intermine.objectstore.query.QueryObjectReference;
+import org.intermine.objectstore.query.Results;
+import org.intermine.objectstore.query.ResultsRow;
+import org.intermine.postprocess.PostProcessor;
+
+/**
+ * Take any GOAnnotation objects assigned to proteins and copy them to corresponding genes.
+ *
+ * @author Richard Smith
+ */
+public class GoPostprocess extends PostProcessor
+{
+    private static final Logger LOG = Logger.getLogger(GoPostprocess.class);
+    protected ObjectStore os;
+
+    /**
+     * Create a new UpdateOrthologes object from an ObjectStoreWriter
+     * @param osw writer on genomic ObjectStore
+     */
+    public GoPostprocess(ObjectStoreWriter osw) {
+        super(osw);
+        this.os = osw.getObjectStore();
+    }
+
+
+    /**
+     * Copy all GO annotations from the Protein objects to the corresponding Gene(s)
+     * @throws ObjectStoreException if anything goes wrong
+     */
+    public void postProcess() throws ObjectStoreException {
+
+        long startTime = System.currentTimeMillis();
+
+        osw.beginTransaction();
+
+        Iterator resIter = findProteinProperties(false);
+
+        int count = 0;
+        Gene lastGene = null;
+        Set<GOAnnotation> goCollection = new HashSet<GOAnnotation>();
+
+        while (resIter.hasNext()) {
+            ResultsRow rr = (ResultsRow) resIter.next();
+            Gene thisGene = (Gene) rr.get(0);
+            GOAnnotation thisAnnotation = (GOAnnotation) rr.get(1);
+
+            GOAnnotation tempAnnotation;
+            try {
+                tempAnnotation =
+                    (GOAnnotation) PostProcessUtil.copyInterMineObject(thisAnnotation);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+            tempAnnotation.setSubject(thisGene);
+            if (lastGene != null && !(lastGene.equals(thisGene))) {
+                lastGene.setGoAnnotation(goCollection);
+                LOG.debug("store gene " + lastGene.getSecondaryIdentifier() + " with "
+                        + lastGene.getGoAnnotation().size() + " GO.");
+                osw.store(lastGene);
+
+                lastGene = thisGene;
+                goCollection = new HashSet<GOAnnotation>();
+            }
+            goCollection.add(tempAnnotation);
+
+            osw.store(tempAnnotation);
+
+            lastGene = thisGene;
+            count++;
+        }
+
+        if (lastGene != null) {
+            lastGene.setGoAnnotation(goCollection);
+            LOG.debug("store gene " + lastGene.getSecondaryIdentifier() + " with "
+                    + lastGene.getGoAnnotation().size() + " GO.");
+            osw.store(lastGene);
+        }
+
+        LOG.info("Created " + count + " new GOAnnotation objects for Genes"
+                + " - took " + (System.currentTimeMillis() - startTime) + " ms.");
+        osw.commitTransaction();
+    }
+
+
+    /**
+     * Query Gene->Protein->Annotation->GOTerm and return an iterator over the Gene,
+     *  Protein and GOTerm.
+     *
+     * @param restrictToPrimaryGoTermsOnly Only get primary Annotation items linking the gene
+     *  and the go term.
+     */
+    private Iterator findProteinProperties(boolean restrictToPrimaryGoTermsOnly)
+    throws ObjectStoreException {
+        Query q = new Query();
+
+        q.setDistinct(false);
+
+        QueryClass qcGene = new QueryClass(Gene.class);
+        q.addFrom(qcGene);
+        q.addToSelect(qcGene);
+        q.addToOrderBy(qcGene);
+
+        QueryClass qcProtein = new QueryClass(Protein.class);
+        q.addFrom(qcProtein);
+
+        QueryClass qcAnnotation = new QueryClass(GOAnnotation.class);
+        q.addFrom(qcAnnotation);
+        q.addToSelect(qcAnnotation);
+
+        ConstraintSet cs = new ConstraintSet(ConstraintOp.AND);
+
+        QueryCollectionReference geneProtRef = new QueryCollectionReference(qcProtein, "genes");
+        ContainsConstraint geneProtConstraint =
+            new ContainsConstraint(geneProtRef, ConstraintOp.CONTAINS, qcGene);
+        cs.addConstraint(geneProtConstraint);
+
+        QueryObjectReference annSubjectRef =
+            new QueryObjectReference(qcAnnotation, "subject");
+        ContainsConstraint annSubjectConstraint =
+            new ContainsConstraint(annSubjectRef, ConstraintOp.CONTAINS, qcProtein);
+        cs.addConstraint(annSubjectConstraint);
+        
+
+        q.setConstraint(cs);
+
+        ObjectStore os = osw.getObjectStore();
+
+        ((ObjectStoreInterMineImpl) os).precompute(q, Constants
+                                                   .PRECOMPUTE_CATEGORY);
+        Results res = os.execute(q, 5000, true, true, true);
+        return res.iterator();
+    }
+}
