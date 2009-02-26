9fd71679725b61f2432006bd26e80704f871c769
julie-sullivan <julie@flymine.org>
2009-02-26 13:36:43 +0000

    don't print test file, classpath fix
    Former-commit-id: 65ba04c9681061927a1994130412107f475cc0ff

--- bio/sources/flymine/experimental/biogrid-human/test/src/org/intermine/bio/dataconversion/BioGridHumanConverterTest.java
+++ bio/sources/flymine/experimental/biogrid-human/test/src/org/intermine/bio/dataconversion/BioGridHumanConverterTest.java
@@ -12,38 +12,44 @@ package org.intermine.bio.dataconversion;
 
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Set;
 
 import org.intermine.dataconversion.ItemsTestCase;
 import org.intermine.dataconversion.MockItemWriter;
 import org.intermine.metadata.Model;
-import org.intermine.model.fulldata.Item;
 
 public class BioGridHumanConverterTest extends ItemsTestCase
 {
+
+    Model model = Model.getInstanceByName("genomic");
+    BioGridHumanConverter converter;
+    MockItemWriter itemWriter;
+
     public BioGridHumanConverterTest(String arg) {
         super(arg);
-        // TODO Auto-generated constructor stub
     }
 
     public void setUp() throws Exception {
         super.setUp();
+        itemWriter = new MockItemWriter(new HashMap());
+        converter = new BioGridHumanConverter(itemWriter, model);
+        MockIdResolverFactory resolverFactory = new MockIdResolverFactory("Gene");
+        resolverFactory.addResolverEntry("7227", "FBgn001", Collections.singleton("FBgn001"));
+        resolverFactory.addResolverEntry("7227", "FBgn003", Collections.singleton("FBgn002"));
+        converter.resolverFactory = resolverFactory;
     }
 
     public void testProcess() throws Exception {
 
-        Reader reader = 
+        Reader reader =
             new InputStreamReader(getClass().getClassLoader().getResourceAsStream("test.tab"));
-
-        MockItemWriter itemWriter = new MockItemWriter(new HashMap<String, Item>());
-        BioGridHumanConverter converter = new BioGridHumanConverter(itemWriter,
-                                                          Model.getInstanceByName("genomic"));
-
         converter.process(reader);
         converter.close();
 
         // uncomment to write out a new target items file
-        writeItemsFile(itemWriter.getItems(), "BioGridHuman_test.xml");
+        //writeItemsFile(itemWriter.getItems(), "BioGridHuman_test.xml");
 
         //Set expected = readItemSet("UniprotConverterTest_tgt.xml");
 
