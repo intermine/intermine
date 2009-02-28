6bbdc4a5ee90d26aaf9086a1c5b812b66f186c26
rnsmith <rns>
2009-02-28 11:45:12 +0000

    Temporarily revert [18532] that throws an exception if there are duplicate objects found when loading.
    Former-commit-id: d74e5b8c6dc7cf3f9aad0ecd1c7df429ce207a4a

--- intermine/integrate/main/src/org/intermine/dataloader/IntegrationWriterDataTrackingImpl.java
+++ intermine/integrate/main/src/org/intermine/dataloader/IntegrationWriterDataTrackingImpl.java
@@ -280,12 +280,6 @@ public class IntegrationWriterDataTrackingImpl extends IntegrationWriterAbstract
                 if (type == SOURCE) {
                     if (writtenObjects.contains(newId)) {
                         // There are duplicate objects
-                        if (!ignoreDuplicates) {
-                            // Yes, this *can* happen, if two items in the tgt-items-database have
-                            // the same item.identifier.
-                            throw new IllegalArgumentException("Duplicate objects exist. Storing "
-                                    + "again to id " + newId + " object from source " + o);
-                        }
                         duplicateObjects.add(newId);
                         isDuplicates = true;
                     } else {
@@ -360,10 +354,6 @@ public class IntegrationWriterDataTrackingImpl extends IntegrationWriterAbstract
             if (type == SOURCE) {
                 if (writtenObjects.contains(newObj.getId())) {
                     // There are duplicate objects
-                    if (!ignoreDuplicates) {
-                        throw new IllegalArgumentException("Duplicate objects exist. Storing "
-                                + "again to id " + newId + " object from source " + o);
-                    }
                     duplicateObjects.add(newObj.getId());
                     isDuplicates = true;
                 } else {
