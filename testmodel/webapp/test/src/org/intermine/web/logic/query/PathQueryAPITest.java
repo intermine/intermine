6bfb10d4aec8f0325da2fe3a38a6b7c044f88fd4
mnw21 <mnw21>
2009-03-17 17:57:16 +0000

    A test
    Former-commit-id: 1d60e24d5c6c3821d00925267274a3ac3292ac90

---
+++ testmodel/webapp/test/src/org/intermine/web/logic/query/PathQueryAPITest.java
@@ -0,0 +1,25 @@
+package org.intermine.web.logic.query;
+
+/*
+ * Copyright (C) 2002-2009 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import junit.framework.TestCase;
+
+/**
+ * Tests the PathQueryAPI class.
+ *
+ * @author Matthew Wakeling
+ */
+public class PathQueryAPITest extends TestCase
+{
+    public void test1() throws Exception {
+        PathQueryAPI.getPathQueryExecutor();
+    }
+}
