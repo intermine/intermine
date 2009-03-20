1e42beda30433776eb3895601bb728e3c8b925c9
julie-sullivan <julie@flymine.org>
2009-03-20 15:26:41 +0000

    go stat url query fix
    Former-commit-id: 801601e56c4278c46abbe04dfbc16608abb16561

--- bio/webapp/src/org/intermine/bio/web/widget/GoStatURLQuery.java
+++ bio/webapp/src/org/intermine/bio/web/widget/GoStatURLQuery.java
@@ -10,6 +10,7 @@ package org.intermine.bio.web.widget;
  *
  */
 
+import org.apache.log4j.Logger;
 import org.intermine.objectstore.ObjectStore;
 import org.intermine.pathquery.Constraints;
 import org.intermine.pathquery.PathQuery;
@@ -23,6 +24,7 @@ import org.intermine.web.logic.widget.WidgetURLQuery;
  */
 public class GoStatURLQuery implements WidgetURLQuery
 {
+    private static final Logger LOG = Logger.getLogger(GoStatURLQuery.class);
     private ObjectStore os;
     private InterMineBag bag;
     private String key;
@@ -45,40 +47,38 @@ public class GoStatURLQuery implements WidgetURLQuery
 
         PathQuery q = new PathQuery(os.getModel());
         String bagType = bag.getType();
-        String pathStrings;
+        String pathStrings = "";
+
+        String prefix = (bagType.equals("Protein") ? "Protein.genes" : "Gene");
 
         if (bagType.equals("Protein")) {
-            pathStrings = "Protein.genes.primaryAccession,Protein.genes.primaryIdentifier,"
-                + "Protein.genes.name,Protein.genes.organism.name,"
-                + "Protein.primaryIdentifier,Protein.primaryAccession,"
-                + "Protein.genes.goAnnotation.ontologyTerm.relations.childTerm.identifier,"
-                + "Protein.genes.goAnnotation.ontologyTerm.relations.childTerm.name,"
-                + "Protein.genes.goAnnotation.ontologyTerm.relations.parentTerm.identifier,"
-                + "Protein.genes.goAnnotation.ontologyTerm.relations.parentTerm.name";
-        } else {
-            pathStrings = "Gene.secondaryIdentifier,Gene.primaryIdentifier,"
-                + "Gene.name,Gene.organism.name,"
-                + "Gene.goAnnotation.ontologyTerm.relations.childTerm.identifier,"
-                + "Gene.goAnnotation.ontologyTerm.relations.childTerm.name,"
-                + "Gene.goAnnotation.ontologyTerm.relations.parentTerm.identifier,"
-                + "Gene.goAnnotation.ontologyTerm.relations.parentTerm.name";
+            pathStrings = "Protein.primaryAccession,";
         }
+
+        pathStrings += prefix + ".primaryIdentifier,"
+            + prefix + ".symbol,"
+            + prefix + ".organism.name,"
+            + prefix + ".goAnnotation.ontologyTerm.relations.childTerm.identifier,"
+            + prefix + ".goAnnotation.ontologyTerm.relations.childTerm.name,"
+            + prefix + ".goAnnotation.ontologyTerm.relations.parentTerm.identifier,"
+            + prefix + ".goAnnotation.ontologyTerm.relations.parentTerm.name";
+
         q.setView(pathStrings);
         q.setOrderBy(pathStrings);
 
         q.addConstraint(bagType, Constraints.in(bag.getName()));
+
         // can't be a NOT relationship!
-        String pathString = (bagType.equals("Protein") ? "Protein.genes.goAnnotation.qualifier"
-                                                         : "Gene.goAnnotation.qualifier");
+        String pathString = prefix + ".goAnnotation.qualifier";
         q.addConstraint(pathString, Constraints.isNull());
 
         // go term
-        pathString = (bagType.equals("Protein") ? "Protein.genes" : "Gene");
-        pathString += ".goAnnotation.ontologyTerm.relations.parentTerm.identifier";
-        q.addConstraint(pathString, Constraints.eq(key), "C", "GOTerm");
-
+        pathString = prefix + ".goAnnotation.ontologyTerm.relations.parentTerm.identifier";
+        q.addConstraint(pathString, Constraints.eq(key), "C", "OntologyTerm");
         q.setConstraintLogic("A and B and C");
         q.syncLogicExpression("and");
+
+        LOG.error("~~WIDGET QUERY:" + q);
         return q;
     }
 }
