98f40c2c812300582b8dfc62c65d0cef673563bf
rnsmith <rns>
2009-03-21 20:14:24 +0000

    Added first draft of overlaps retrieval to submission displayer.
    Former-commit-id: a8fc7a30d70a93b2858c0756ecaa54e90e58704a

---
+++ modmine/webapp/src/org/modmine/web/SubmissionOverlapsAction.java
@@ -0,0 +1,125 @@
+package org.modmine.web;
+
+/*
+ * Copyright (C) 2002-2009 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.apache.struts.Globals;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.util.MessageResources;
+import org.intermine.objectstore.ObjectStore;
+import org.intermine.pathquery.Constraints;
+import org.intermine.pathquery.PathNode;
+import org.intermine.pathquery.PathQuery;
+import org.intermine.web.logic.Constants;
+import org.intermine.web.logic.query.QueryMonitorTimeout;
+import org.intermine.web.logic.query.WebResultsExecutor;
+import org.intermine.web.logic.results.PagedTable;
+import org.intermine.web.logic.results.WebResults;
+import org.intermine.web.logic.session.SessionMethods;
+import org.intermine.web.struts.ForwardParameters;
+import org.intermine.web.struts.InterMineAction;
+
+/**
+ * Generate queries for overlaps of submission features and overlaps with gene flanking regions.
+ * @author Richard Smith
+ *
+ */
+public class SubmissionOverlapsAction extends InterMineAction {
+
+    private static final Logger LOG = Logger.getLogger(SubmissionOverlapsAction.class);
+    
+    /**
+     * Action for creating a bag of InterMineObjects or Strings from identifiers in text field.
+     *
+     * @param mapping The ActionMapping used to select this instance
+     * @param form The optional ActionForm bean for this request (if any)
+     * @param request The HTTP request we are processing
+     * @param response The HTTP response we are creating
+     * @return an ActionForward object defining where control goes next
+     * @exception Exception if the application business logic throws
+     *  an exception
+     */
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 @SuppressWarnings("unused") HttpServletResponse response)
+    throws Exception {
+        HttpSession session = request.getSession();
+        SubmissionOverlapsForm submissionOverlapsForm = (SubmissionOverlapsForm) form;
+        ServletContext servletContext = session.getServletContext();
+        ObjectStore os = (ObjectStore) servletContext.getAttribute(Constants.OBJECTSTORE);
+
+        String submissionTitle = submissionOverlapsForm.getSubmissionTitle();
+
+        PathQuery q = new PathQuery(os.getModel());
+        
+        if (request.getParameter("overlaps") != null) {
+            String featureType = submissionOverlapsForm.getOverlapFeatureType();
+            String findFeatureType = submissionOverlapsForm.getOverlapFindType();
+            
+            q.addView(findFeatureType + ".primaryIdentifier");            
+            q.addView(findFeatureType + ".overlappingFeatures.secondaryIdentifier");
+            
+
+            PathNode featureNode = q.addNode(findFeatureType + ".overlappingFeatures");
+            featureNode.setType(featureType);
+            q.addConstraint(findFeatureType + ".overlappingFeatures.dataSets.title",
+                    Constraints.eq(submissionTitle));
+            
+            
+        } else if (request.getParameter("flanking") != null) {
+            String direction = submissionOverlapsForm.getDirection();
+            String distance = submissionOverlapsForm.getDistance();
+            String featureType = submissionOverlapsForm.getFlankingFeatureType();
+
+            q.addView("GeneFlankingRegion.gene.primaryIdentifier");
+
+            PathNode featureNode = q.addNode("GeneFlankingRegion.overlappingFeatures");
+            featureNode.setType(featureType);
+
+            q.addConstraint("GeneFlankingRegion.distance", Constraints.eq(distance));
+            q.addConstraint("GeneFlankingRegion.direction", Constraints.eq(direction));
+            q.addConstraint("GeneFlankingRegion.overlappingFeatures.dataSets.title", 
+                    Constraints.eq(submissionTitle));
+
+            q.setOrderBy("GeneFlankingRegion.gene.primaryIdentifier");
+
+        }
+
+        LOG.info("OVERLAPS: " + q.toString());
+        
+        //WebResultsExecutor executor = SessionMethods.getWebResultsExecutor(session);
+        //WebResults webResults = executor.execute(q);
+        
+        QueryMonitorTimeout clientState = new QueryMonitorTimeout(
+                Constants.QUERY_TIMEOUT_SECONDS * 1000);
+        MessageResources messages = (MessageResources) request.getAttribute(Globals.MESSAGES_KEY);
+        String qid = SessionMethods.startQuery(clientState, session, messages,
+                                               false, q);
+        Thread.sleep(200);
+        
+        return new ForwardParameters(mapping.findForward("waiting"))
+        .addParameter("qid", qid)
+        //.addParameter("trail", trail)
+        .forward();
+        
+        
+        
+        //return mapping.findForward("results");
+    }
+}
---
+++ modmine/webapp/src/org/modmine/web/SubmissionOverlapsForm.java
@@ -0,0 +1,131 @@
+package org.modmine.web;
+
+/*
+ * Copyright (C) 2002-2009 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * Form to hold selections from submissionDisplayer to find overlapping features.
+ * @author Richard Smith
+ *
+ */
+public class SubmissionOverlapsForm extends ActionForm
+{
+
+    private String direction;
+    private String submissionTitle;
+    private String distance;
+    private String overlapFeatureType;
+    private String flankingFeatureType;
+    private String overlapFindType;
+
+    
+    /**
+     * @return the overlapFeatureType
+     */
+    public String getOverlapFeatureType() {
+        return overlapFeatureType;
+    }
+
+    /**
+     * @param overlapFeatureType the overlapFeatureType to set
+     */
+    public void setOverlapFeatureType(String overlapFeatureType) {
+        this.overlapFeatureType = overlapFeatureType;
+    }
+
+
+    /**
+     * @return the overlapFindType
+     */
+    public String getOverlapFindType() {
+        return overlapFindType;
+    }
+
+    /**
+     * @param overlapFindType the overlapFindType to set
+     */
+    public void setOverlapFindType(String overlapFindType) {
+        this.overlapFindType = overlapFindType;
+    }
+
+
+    
+    /**
+     * @return the flankingFeatureType
+     */
+    public String getFlankingFeatureType() {
+        return flankingFeatureType;
+    }
+
+    /**
+     * @param flankingFeatureType the flankingFeatureType to set
+     */
+    public void setFlankingFeatureType(String flankingFeatureType) {
+        this.flankingFeatureType = flankingFeatureType;
+    }
+
+    /**
+     * @return the submissionTitle
+     */
+    public String getSubmissionTitle() {
+        return submissionTitle;
+    }
+
+    /**
+     * @param submissionTitle the submissionTitle to set
+     */
+    public void setSubmissionTitle(String submissionTitle) {
+        this.submissionTitle = submissionTitle;
+    }
+
+    /**
+     * @return the distance
+     */
+    public String getDistance() {
+        return distance;
+    }
+
+    /**
+     * @param distance the distance to set
+     */
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+        
+    /**
+     * @param direction the direction to set
+     */
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    /**
+     * Get the bag type
+     * @return the bag type string
+     */
+    public String getDirection() {
+        return direction;
+    }
+
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override    
+    public void reset(ActionMapping mapping, HttpServletRequest request) {
+        super.reset(mapping, request);
+        direction = null;
+    }
+}
