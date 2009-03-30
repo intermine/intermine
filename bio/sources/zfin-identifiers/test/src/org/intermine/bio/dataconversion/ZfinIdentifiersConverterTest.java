d4a7f63c6909e626e24a1bf6789b7ddafc451176
julie-sullivan <julie@flymine.org>
2009-03-30 11:39:22 +0000

    add processing zfin id to uniprot accession text file.  identify files by name and not data.

--- bio/sources/zfin-identifiers/test/src/org/intermine/bio/dataconversion/ZfinIdentifiersConverterTest.java
+++ bio/sources/zfin-identifiers/test/src/org/intermine/bio/dataconversion/ZfinIdentifiersConverterTest.java
@@ -10,6 +10,7 @@ package org.intermine.bio.dataconversion;
  *
  */
 
+import java.io.File;
 import java.io.StringReader;
 import java.util.Collection;
 import java.util.HashMap;
@@ -21,42 +22,30 @@ import org.intermine.xml.full.FullParser;
 
 public class ZfinIdentifiersConverterTest extends ItemsTestCase
 {
-    private String ENDL = System.getProperty("line.separator");
+
+    private ZfinIdentifiersConverter converter;
+    private MockItemWriter itemWriter;
 
     public ZfinIdentifiersConverterTest(String arg) {
         super(arg);
     }
 
     public void setUp() throws Exception {
+        itemWriter = new MockItemWriter(new HashMap());
+        converter = new ZfinIdentifiersConverter(itemWriter, Model.getInstanceByName("genomic"));
         super.setUp();
     }
 
     public void testProcess() throws Exception {
-        String zbd_history_txt = "ZDB-GENE-111111-1111\tZDB-GENE-111111-1112" + ENDL
-        + "ZDB-BAC-030131-406\tZDB-BAC-030131-7240\t" + ENDL;
-
-        String ensembl = "#\tZDBID\tSYMBOL\tEnsembl(Zv7)\t" + ENDL
-        + "ZDB-GENE-111111-1111\tsymbol1111\tENSDARG00000001111\t" + ENDL
-        + "ZDB-GENE-222222-2222\tsymbol2222\tENSDARG00000002222\t" + ENDL;
-
-        String aliases_txt = "ZDB-GENE-222222-2222\tcalcium channel, voltage-dependent, beta 1 subunit\tsymbol2222\tsyn2222" + ENDL
-        + "ZDB-GENE-111111-1111\tSTARD3 N-terminal like\tstard3nl\twu:fa01c03" + ENDL
-        + "ZDB-GENE-111111-1111\tSTARD3 N-terminal like\tstard3nl\tzgc:86628" + ENDL;
-
-
-
-        MockItemWriter itemWriter = new MockItemWriter(new HashMap());
-        BioFileConverter converter = new ZfinIdentifiersConverter(itemWriter,
-                                                                   Model.getInstanceByName("genomic"));
-        converter.process(new StringReader(zbd_history_txt));
-        converter.process(new StringReader(ensembl));
-        converter.process(new StringReader(aliases_txt));
+        File tmp = new File(getClass().getClassLoader().getResource("ZfinIdentifiersConverterTest_tgt.xml").toURI());
+        File datadir = tmp.getParentFile();
+        converter.process(datadir);
         converter.close();
 
         // uncomment to write out a new target items file
-        //writeItemsFile(itemWriter.getItems(), "zfin_tgt.xml");
+        writeItemsFile(itemWriter.getItems(), "zfin_tgt.xml");
 
-        assertEquals(readItemSet("ZfinIdentifiersConverterTest.xml"), itemWriter.getItems());
+        assertEquals(readItemSet("ZfinIdentifiersConverterTest_tgt.xml"), itemWriter.getItems());
     }
 
     protected Collection getExpectedItems() throws Exception {
