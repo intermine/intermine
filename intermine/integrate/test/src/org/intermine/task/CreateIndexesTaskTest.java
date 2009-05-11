28d5f4ac6f01c3c2b90365f339d04d5562cb7f36
mnw21 <mnw21>
2009-05-11 14:12:34 +0000

    Fix tests. Refs #1944

--- intermine/integrate/test/src/org/intermine/task/CreateIndexesTaskTest.java
+++ intermine/integrate/test/src/org/intermine/task/CreateIndexesTaskTest.java
@@ -34,9 +34,7 @@ public class CreateIndexesTaskTest extends TestCase
     public void testCreateStandardIndexes1() throws Exception {
         List expected = new ArrayList();
         expected.add("create index Department__key1 on Department(name, companyId, id)");
-        expected.add("create index Department__key1__nulls on Department((name IS NULL))");
         expected.add("create index Department__key2 on Department(name, managerId, id)");
-        expected.add("create index Department__key2__nulls on Department((name IS NULL))");
         expected.add("create index Department__company on Department(companyId, id)");
         expected.add("create index Department__manager on Department(managerId, id)");
 
@@ -66,7 +64,6 @@ public class CreateIndexesTaskTest extends TestCase
 
         expected = new ArrayList();
         expected.add("create index Secretary__key on Secretary(name, id)");
-        expected.add("create index Secretary__key__nulls on Secretary((name IS NULL))");
         task = new CreateIndexesTask();
         task.setAlias("os.unittest");
         task.setUp();
@@ -80,13 +77,9 @@ public class CreateIndexesTaskTest extends TestCase
     public void testCreateIndexesSubclasses() throws Exception {
         Set expected = new HashSet();
         expected.add("create index Contractor__ImportantPerson__key on Contractor(seniority, id)");
-        expected.add("create index Contractor__ImportantPerson__key__nulls on Contractor((seniority IS NULL))");
         expected.add("create index CEO__ImportantPerson__key on CEO(seniority, id)");
-        expected.add("create index CEO__ImportantPerson__key__nulls on CEO((seniority IS NULL))");
         expected.add("create index ImportantPerson__key on ImportantPerson(seniority, id)");
-        expected.add("create index ImportantPerson__key__nulls on ImportantPerson((seniority IS NULL))");
         expected.add("create index Manager__ImportantPerson__key on Manager(seniority, id)");
-        expected.add("create index Manager__ImportantPerson__key__nulls on Manager((seniority IS NULL))");
 
         CreateIndexesTask task = new CreateIndexesTask();
         task.setAlias("os.unittest");
@@ -105,17 +98,13 @@ public class CreateIndexesTaskTest extends TestCase
         expected.add("create index CEO__salary on CEO(salary)");
         expected.add("create index CEO__title_like on CEO(lower(title) text_pattern_ops)");
         expected.add("create index CEO__title_equals on CEO(lower(title))");
-        expected.add("create index CEO__title__nulls on CEO((title IS NULL))");
         expected.add("create index CEO__fullTime on CEO(fullTime)");
         expected.add("create index CEO__age on CEO(age)");
         expected.add("create index CEO__end_like on CEO(lower(intermine_end) text_pattern_ops)");
         expected.add("create index CEO__end_equals on CEO(lower(intermine_end))");
-        expected.add("create index CEO__end__nulls on CEO((intermine_end IS NULL))");
         expected.add("create index CEO__name_like on CEO(lower(name) text_pattern_ops)");
         expected.add("create index CEO__name_equals on CEO(lower(name))");
-        expected.add("create index CEO__name__nulls on CEO((name IS NULL))");
         expected.add("create index CEO__seniority on CEO(seniority)");
-        expected.add("create index CEO__seniority__nulls on CEO((seniority IS NULL))");
 
         CreateIndexesTask task = new CreateIndexesTask();
         task.setAlias("os.unittest");
