3994cbcc5baeaea45bd3da4b4e266a41b3430920
mnw21 <mnw21>
2009-07-02 16:35:46 +0000

    Fix some bio/test-all tests. The priority file is only read if it is needed.

--- intermine/integrate/main/src/org/intermine/dataloader/IntegrationWriterDataTrackingImpl.java
+++ intermine/integrate/main/src/org/intermine/dataloader/IntegrationWriterDataTrackingImpl.java
@@ -159,7 +159,6 @@ public class IntegrationWriterDataTrackingImpl extends IntegrationWriterAbstract
         super(osw);
         this.dataTracker = dataTracker;
         this.trackerMissingClasses = Collections.emptySet();
-        priorityConfig = new PriorityConfig(osw.getModel());
     }
 
     /**
@@ -175,7 +174,6 @@ public class IntegrationWriterDataTrackingImpl extends IntegrationWriterAbstract
         super(osw);
         this.dataTracker = dataTracker;
         this.trackerMissingClasses = trackerMissingClasses;
-        priorityConfig = new PriorityConfig(osw.getModel());
     }
 
     /**
@@ -388,6 +386,9 @@ public class IntegrationWriterDataTrackingImpl extends IntegrationWriterAbstract
             Set typeUtilFieldNames = TypeUtil.getFieldInfos(newObj.getClass()).keySet();
             if (!modelFieldNames.equals(typeUtilFieldNames)) {
                 throw new ObjectStoreException("Failed to store data not in the model");
+            }
+            if (priorityConfig == null) {
+                priorityConfig = new PriorityConfig(osw.getModel());
             }
             Iterator fieldIter = fieldDescriptors.entrySet().iterator();
             while (fieldIter.hasNext()) {
