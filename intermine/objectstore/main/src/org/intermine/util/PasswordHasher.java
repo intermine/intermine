a0fedf631501e31b1995e91eb6e02d920a7ab799
mnw21 <mnw21>
2009-07-09 12:22:40 +0000

    Passwords are now hashed, as per requests by other installations. Refs #2067

---
+++ intermine/objectstore/main/src/org/intermine/util/PasswordHasher.java
@@ -0,0 +1,81 @@
+package org.intermine.util;
+
+/*
+ * Copyright (C) 2002-2009 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import net.sourceforge.iharder.Base64;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * Utility methods for managing hashed passwords
+ *
+ * @author Matthew Wakeling
+ */
+public class PasswordHasher
+{
+    private PasswordHasher() {
+        // TODO do we need this?
+    }
+
+    /**
+     * Converts a password into a hashed password, with a salt.
+     *
+     * @param password the password to hash
+     * @return a 88-character String containing the salt and the hash
+     */
+    public static String hashPassword(String password) {
+        try {
+            byte salt[] = new byte[32];
+            SecureRandom sr = new SecureRandom();
+            sr.nextBytes(salt);
+            String saltString = Base64.encodeBytes(salt);
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update((saltString + password).getBytes());
+            byte digest[] = md.digest();
+            String hashString = Base64.encodeBytes(digest);
+            return saltString + hashString;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Checks to see if a password matches an optionally hashed password entry.
+     *
+     * @param password the given password
+     * @param hash the hashed password entry
+     * @return true if the password matches
+     */
+    public static boolean checkPassword(String password, String hash) {
+        if (password.equals(hash)) {
+            return true;
+        }
+        try {
+            if (hash.length() == 88) {
+                String saltString = hash.substring(0, 44);
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+                md.update((saltString + password).getBytes());
+                byte digest[] = md.digest();
+                String hashString = Base64.encodeBytes(digest);
+                if (hashString.equals(hash.substring(44))) {
+                    return true;
+                }
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    }
+}
