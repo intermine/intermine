c06d136cc76548a3773544c6ec7e19f651dd8d3c
julie-sullivan <julie@flymine.org>
2009-07-10 15:45:52 +0000

    run acceptance test after template comparison
    Former-commit-id: 8517cff0c02c063af489c33b2d5d55ddaad3f456

---
+++ bio/scripts/template_comparison.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+# this script runs templates for two different releases and compares the results
+
+mine='flymine'
+release1='release-17.0'
+release2='preview-18.0'
+
+run_performance_test() {
+# release webapp                                                                                                                                                                                               
+    echo "changing to webapp directory"
+    cd ~/svn/dev/flymine/webapp
+    echo "building webapp"
+    ant default -Drelease=$release1 > ant_output.log
+    
+# run the performance_test script                                                                                                                                                                              
+    echo "changing back to the script directory"
+    cd ~/svn/dev/bio/scripts
+    echo "running the performance test"
+    ./performance_test flymine > $release1
+
+# release webapp                                                                                                                                                                                               
+    echo "changing to webapp directory"
+    cd ~/svn/dev/flymine/webapp
+    echo "removing temporary directories"
+    ant clean-all > ant_output.log
+    echo "building webapp"
+    ant default -Drelease=$release2 > ant_output.log
+    
+# run the performance_test script                                                                                                                                                                              
+    echo "changing back to the script directory"
+    cd ~/svn/dev/bio/scripts
+    echo "running the performance test"
+    ./performance_test flymine > $release2
+
+# compare the output                                                                                                                                                                                           
+    ./compare_releases  $release1 $release2 > compare_releases.tmp
+
+# let everyone know                                                                                                                                                                                            
+    mail -s "Outcome of template comparison comparing $release1 and $release2" 'julie@flymine.org' < compare_releases.tmp
+}
+
+run_acceptance_tests() {
+    echo "running acceptance tests"
+    cd ~/svn/dev/flymine/integrate
+    #ant acceptance-tests
+
+    attfile='/home/julie/svn/dev/flymine/integrate/build/acceptance_test.html'     
+    subject="Outcome of acceptance tests run for $release2"
+    mailto='julie@flymine.org'
+
+    mutt -s "$subject" -a $attfile $mailto < /dev/null
+}
+run_performance_test
+run_acceptance_tests
+echo "done"
+
+
+
