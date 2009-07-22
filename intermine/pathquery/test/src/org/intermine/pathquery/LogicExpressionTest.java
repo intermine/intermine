d1f8e78ce581a19e76e39ac54689c08a1911c7e0
mnw21 <mnw21>
2009-07-22 15:12:15 +0000

    Fix some tests. Merge the duplicated LogicExpressionTests
    Former-commit-id: 3ac1c53bbcac9c0b47759661736c29f1ed413b96

--- intermine/pathquery/test/src/org/intermine/pathquery/LogicExpressionTest.java
+++ intermine/pathquery/test/src/org/intermine/pathquery/LogicExpressionTest.java
@@ -30,6 +30,43 @@ public class LogicExpressionTest extends TestCase
         super(arg);
     }
 
+    public void testParseSimple() {
+        new LogicExpression("A and B");
+    }
+
+    public void testParseSimple2() {
+        new LogicExpression("A and B and C");
+    }
+
+    public void testParseSimple3() {
+        new LogicExpression("A and B or C");
+    }
+
+    public void testSimpleBrackets() {
+        new LogicExpression("(A and B)");
+    }
+
+    public void testDoubleBrackets() {
+        new LogicExpression("((A and B ))");
+    }
+
+    public void testNestedBrackets() {
+        roundtrip("A or (B and C) or D");
+        roundtrip("A or (B and (C or D))");
+        roundtrip("(A or B) and (C or D)");
+    }
+
+    public void testInvalidNot() {
+        try {
+            fail("" + new LogicExpression("A not B"));
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+    private void roundtrip(String expr) {
+        assertEquals(expr, new LogicExpression(expr).toString());
+    }
+
     public void test1() {
         LogicExpression l = new LogicExpression("A");
         assertEquals(Collections.singleton("A"), l.getVariableNames());
