1ce38fda89375b38fe43d674d48f0c8c5d5b88fd
rnsmith <rns>
2009-07-30 12:20:57 +0000

    Set Sumission.experimentType based on protocol types.
    Former-commit-id: 2caee5102c0001a7e7b9deb9f2071fc314b30af6

--- bio/sources/chado-db/test/src/org/intermine/bio/dataconversion/ModEncodeMetaDataProcessorTest.java
+++ bio/sources/chado-db/test/src/org/intermine/bio/dataconversion/ModEncodeMetaDataProcessorTest.java
@@ -1,6 +1,8 @@
 package org.intermine.bio.dataconversion;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import junit.framework.TestCase;
 
@@ -18,14 +20,19 @@ import org.intermine.metadata.Model;
  */
 
 public class ModEncodeMetaDataProcessorTest extends TestCase {
-
+    ModEncodeMetaDataProcessor processor;
     
-    public void testCorrectOfficialName() throws Exception {
+    public void setUp() throws Exception {
         MockItemWriter itemWriter =
             new MockItemWriter(new HashMap<String, org.intermine.model.fulldata.Item>());
         ChadoDBConverter converter =
             new ChadoDBConverter(null, Model.getInstanceByName("genomic"), itemWriter);
-        ModEncodeMetaDataProcessor processor = new ModEncodeMetaDataProcessor(converter);
+        processor = new ModEncodeMetaDataProcessor(converter);
+        
+    }
+    
+    public void testCorrectOfficialName() throws Exception {
+        
         
         // actual examples from metadata
         assertEquals("Adult Female", processor.correctOfficialName("Adult_Female", "DevelopmentalStage"));
@@ -35,5 +42,46 @@ public class ModEncodeMetaDataProcessorTest extends TestCase {
         assertEquals("Embryo 2-4 h", processor.correctOfficialName("Embryo 2-4 hr", "DevelopmentalStage"));
         assertEquals("L1 stage larvae", processor.correctOfficialName("DevStage: L1 stage larvae", "DevelopmentalStage"));
         assertEquals("L1 stage larvae", processor.correctOfficialName("L1", "DevelopmentalStage"));
+    }
+    
+    
+    public void testInferExperimentType() throws Exception {
+        String[] test = new String[] {"chromatin_immunoprecipitation", "dummy"};
+        assertEquals("ChIP-seq", processor.inferExperimentType(makeProtocols(test)));
+        
+        test = new String[] {"chromatin_immunoprecipitation", "hybridization"};
+        assertEquals("ChIP-chip", processor.inferExperimentType(makeProtocols(test)));
+    
+        test = new String[] {"nucleic_acid_extraction", "sequencing_protocol", "reverse_transcription"};
+        assertEquals("RTPCR", processor.inferExperimentType(makeProtocols(test)));
+        
+        test = new String[] {"RNA extraction", "sequencing_protocol"};
+        assertEquals("RNA-seq", processor.inferExperimentType(makeProtocols(test)));
+        
+        test = new String[] {"RNA extraction", "sequencing"};
+        assertEquals("RNA-seq", processor.inferExperimentType(makeProtocols(test)));
+        
+        test = new String[] {"reverse_transcription", "PCR"};
+        assertEquals("RTPCR", processor.inferExperimentType(makeProtocols(test)));
+        
+        test = new String[] {"reverse_transcription", "PCR_amplification", "RACE"};
+        assertEquals("RACE", processor.inferExperimentType(makeProtocols(test)));
+        
+        test = new String[] {"hybridization"};
+        assertEquals("RNA tiling array", processor.inferExperimentType(makeProtocols(test)));
+        
+        test = new String[] {"annotation"};
+        assertEquals("Computational annotation", processor.inferExperimentType(makeProtocols(test)));
+    
+        test = new String[] {"grow"};
+        assertEquals("RNA sample creation", processor.inferExperimentType(makeProtocols(test)));
+    }
+    
+    private Set<String> makeProtocols(String[] types) {
+        Set<String> protocolTypes = new HashSet<String>();
+        for (String type : types) {
+            protocolTypes.add(type);
+        }
+        return protocolTypes;
     }
 }
