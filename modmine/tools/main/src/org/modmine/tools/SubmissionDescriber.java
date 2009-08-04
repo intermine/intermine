306597631f7b5b201cf71795ad4f838dbff50195
rnsmith <rns>
2009-08-04 10:40:39 +0000

    Add modmine tools project.
    Former-commit-id: 67a4ae94af91b9574593ab5f5584d8fc9925a69d

---
+++ modmine/tools/main/src/org/modmine/tools/SubmissionDescriber.java
@@ -0,0 +1,13 @@
+package org.modmine.tools;
+
+import org.intermine.model.bio.Submission;
+
+public class SubmissionDescriber {
+
+    // private static Map<Integer, String> descriptionCache;
+    
+    public static void describeSubmission(Submission sub) {
+        
+    }
+    
+}
