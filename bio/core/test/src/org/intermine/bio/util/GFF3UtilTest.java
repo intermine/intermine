b6ad8526c062e2644dce857f79ac9f2a454ab3a4
rnsmith <rns>
2009-08-11 13:14:46 +0000

    GFF generation no longer puts a '.' instead of '-' if no strand is set, added test.  Refs #2111.
    Former-commit-id: 655baa7ed61615bc43ac68f1f92cebde4de992ce

--- bio/core/test/src/org/intermine/bio/util/GFF3UtilTest.java
+++ bio/core/test/src/org/intermine/bio/util/GFF3UtilTest.java
@@ -57,23 +57,20 @@ public class GFF3UtilTest extends TestCase
         Map<String, List<String>> extraAttributes = new LinkedHashMap<String, List<String>>();
 
         // test adding multiple values
-        List valList = new ArrayList();
+        List<String> valList = new ArrayList<String>();
         valList.add("test_string1");
         valList.add("test_string2");
         extraAttributes.put("name3", valList);
 
-        Map soClassNameMap = new LinkedHashMap();
-
-        soClassNameMap.put("Gene", "gene");
-        soClassNameMap.put("Exon", "exon");
-        soClassNameMap.put("Chromosome", "chromosome");
+        Map<String, String> soClassNameMap = getSoClassNameMap();
 
         GFF3Record gff3Gene = GFF3Util.makeGFF3Record(gene, soClassNameMap, "FlyMine", extraAttributes);
 
         GFF3Record gff3Exon = GFF3Util.makeGFF3Record(exon, soClassNameMap, "FlyMine",
                                                       new HashMap<String, List<String>>());
         GFF3Record gff3Chromosome =
-            GFF3Util.makeGFF3Record(chromosome, soClassNameMap, "FlyMine", new HashMap());
+            GFF3Util.makeGFF3Record(chromosome, soClassNameMap, "FlyMine", 
+                    new HashMap<String, List<String>>());
 
         System.err.println (gff3Gene);
         System.err.println (gff3Exon);
@@ -89,5 +86,41 @@ public class GFF3UtilTest extends TestCase
                      gff3Exon.toGFF3());
         assertEquals("4\tFlyMine\tchromosome\t1\t1000\t.\t.\t.\tID=4",
                      gff3Chromosome.toGFF3());
+    }
+    
+    // Exon location has no strand information - should default to '.'
+    public void testNoStrandSet() throws Exception {
+        Exon exon = (Exon) DynamicUtil.createObject(Collections.singleton(Exon.class));
+        Chromosome chromosome =
+            (Chromosome) DynamicUtil.createObject(Collections.singleton(Chromosome.class));
+        Location exonLocation =
+            (Location) DynamicUtil.createObject(Collections.singleton(Location.class));
+
+        exon.setChromosome(chromosome);
+        exon.setChromosomeLocation(exonLocation);
+        exon.setPrimaryIdentifier("exon1");
+
+        exonLocation.setStart(new Integer(200));
+        exonLocation.setEnd(new Integer(300));
+        
+        chromosome.setPrimaryIdentifier("4");
+        chromosome.setLength(new Integer(1000));
+
+        Map<String, String> soClassNameMap = getSoClassNameMap();
+
+        GFF3Record gff3Exon = GFF3Util.makeGFF3Record(exon, soClassNameMap, "FlyMine",
+                                                      new HashMap<String, List<String>>());
+
+        assertEquals("4\tFlyMine\texon\t200\t300\t.\t.\t.\tID=exon1",
+                     gff3Exon.toGFF3());
+    }
+    
+    private Map<String, String> getSoClassNameMap() {
+        Map<String, String> soClassNameMap = new LinkedHashMap<String, String>();
+        soClassNameMap.put("Gene", "gene");
+        soClassNameMap.put("Exon", "exon");
+        soClassNameMap.put("Chromosome", "chromosome");
+
+        return soClassNameMap;
     }
 }
