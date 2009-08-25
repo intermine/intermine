d9ad03cb154b0093460f97b8106169473e4a8dad
julie-sullivan <julie@flymine.org>
2009-08-25 09:31:33 +0000

    add pathways to proteins
    Former-commit-id: 8fddabc6799af042fa69e2cd4dbab98b6373e00b

--- bio/sources/biopax/main/src/org/intermine/bio/dataconversion/BioPAXConverter.java
+++ bio/sources/biopax/main/src/org/intermine/bio/dataconversion/BioPAXConverter.java
@@ -15,11 +15,12 @@ import java.io.FileInputStream;
 import java.io.Reader;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.biopax.paxtools.io.jena.JenaIOHandler;
-import org.biopax.paxtools.model.BioPAXElement;
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
-
+import org.intermine.objectstore.ObjectStoreException;
+import org.intermine.xml.full.Item;
 
 /**
  * 
@@ -27,7 +28,7 @@ import org.intermine.metadata.Model;
  */
 public class BioPAXConverter extends BioFileConverter
 {
-    //
+    private static final Logger LOG = Logger.getLogger(BioPAXConverter.class);
     private static final String DATASET_TITLE = "BioPAX";
     private static final String DATA_SOURCE_NAME = "BioPAX data set";
 
@@ -36,42 +37,75 @@ public class BioPAXConverter extends BioFileConverter
      * @param writer the ItemWriter used to handle the resultant items
      * @param model the Model
      */
-    public BioPAXConverter(ItemWriter writer, Model model) {
+    public BioPAXConverter(ItemWriter writer, Model model) 
+    throws ObjectStoreException {
         super(writer, model, DATA_SOURCE_NAME, DATASET_TITLE);
+        Item o = createItem("Organism");
+        o.setAttribute("taxonId", "4932");
+        try {
+            store(o);
+        } catch (ObjectStoreException e) {
+            throw new ObjectStoreException(e);
+        }
     }
 
     /**
-     * 
-     *
      * {@inheritDoc}
      */
+    @Override
     public void process(Reader reader) throws Exception {
+
+        // TODO the reader is never used
+        
         JenaIOHandler jenaIOHandler = new JenaIOHandler();
         File file = getCurrentFile();
         org.biopax.paxtools.model.Model jenaModel 
             = jenaIOHandler.convertFromOWL(new FileInputStream(file));
-        Set<BioPAXElement> elements = jenaModel.getObjects();
-        for (BioPAXElement element : elements) {
-            element.getRDFId();
+//        Set<org.biopax.paxtools.model.level2.pathway> pathways = jenaModel.getObjects(org.biopax.paxtools.model.level2.pathway.class);
+        
+        Set objects = jenaModel.getObjects();
+        
+        for (Object object : objects) {
+            if (object.getClass().getCanonicalName().equals("org.biopax.paxtools.impl.level2.pathwayImpl")) {
+                storePathways((org.biopax.paxtools.model.level2.pathway) object);
+            } else if (object.getClass().getCanonicalName().contains("biochemicalReaction")) {
+                org.biopax.paxtools.model.level2.biochemicalReaction br = (org.biopax.paxtools.model.level2.biochemicalReaction) object;
+                Set<org.biopax.paxtools.model.level2.InteractionParticipant> participants = br.getPARTICIPANTS();
+                
+                for (org.biopax.paxtools.model.level2.InteractionParticipant participant : participants) {
+                    
+                }
+                
+            }
+//            physicalEntityParticipant
         }
-
-//        model.getObjects();
-//        or you can use the filtering feature in order to get only 
-//           model.getObjects(interaction.class);
-//        When you have an interaction, getting its participants i
-//           Set<InteractionParticipant> participant
-
-        
-        
-//        A Model object contains multiple BioPAX elements. Each element within a model
-//        to have a unique id. A model can have multiple namespaces and these namespaces
-//        be obtained as a map in which key is the prefix and value is the namespace:
-//           Map<String, String> nspMap = model.getNameSpacePrefixMap();
-
-        
-        
+    }
+    
+    private void storePathways(org.biopax.paxtools.model.level2.pathway pathway) 
+    throws ObjectStoreException {
+        String shortName = pathway.getSHORT_NAME();
+        String name = pathway.getNAME();
+        Set<String> comments = pathway.getCOMMENT();
+        Set<org.biopax.paxtools.model.level2.dataSource> datasources = pathway.getDATA_SOURCE();
+        org.biopax.paxtools.model.level2.bioSource organism = pathway.getORGANISM();
+        Set<org.biopax.paxtools.model.level2.evidence> evidences = pathway.getEVIDENCE();
+        Set<org.biopax.paxtools.model.level2.pathwayComponent> components = pathway.getPATHWAY_COMPONENTS();
+        String rfId = pathway.getRDFId();
+        Set<String> synonyms = pathway.getSYNONYMS();
+        Set<org.biopax.paxtools.model.level2.xref> xrefs = pathway.getXREF();
+        String id = "";
+        for (org.biopax.paxtools.model.level2.xref xref : xrefs) {
+            id = xref.getID();
+        }
+            
+        Item item = createItem("Pathway");
+        item.setAttribute("identifier", id);
+        item.setAttribute("name", name);
+        try {
+            store(item);
+        } catch (ObjectStoreException e) {
+            throw new ObjectStoreException(e);
+        }        
     }
     
   }
-
-
---
+++ bio/sources/biopax/test/src/org/intermine/bio/dataconversion/BioPAXConverterTest.java
@@ -0,0 +1,55 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2009 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.io.File;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Set;
+
+import org.apache.commons.io.IOUtils;
+import org.intermine.dataconversion.ItemsTestCase;
+import org.intermine.dataconversion.MockItemWriter;
+import org.intermine.metadata.Model;
+
+
+public class BioPAXConverterTest extends ItemsTestCase
+{
+    private BioPAXConverter converter;
+    private MockItemWriter itemWriter;
+    
+    
+    public BioPAXConverterTest(String arg) {
+        super(arg);
+    }
+
+    public void testProcess() throws Exception {
+        
+        itemWriter = new MockItemWriter(new HashMap());
+        converter = new BioPAXConverter(itemWriter, Model.getInstanceByName("genomic"));
+        
+        ClassLoader loader = getClass().getClassLoader();
+        String input = IOUtils.toString(loader.getResourceAsStream("BioPAXConverterTest_src.owl"));
+        
+        
+        File currentFile = new File(getClass().getClassLoader().getResource("BioPAXConverterTest_src.owl").toURI());
+        converter.setCurrentFile(currentFile);
+        converter.process(new StringReader(input));
+        converter.close();
+
+        // uncomment to write out a new target items file
+        writeItemsFile(itemWriter.getItems(), "BioPAX-tgt-items.xml");
+
+        Set expected = readItemSet("BioPAXConverterTest_tgt.xml");
+
+        assertEquals(expected, itemWriter.getItems());
+    }
+}
