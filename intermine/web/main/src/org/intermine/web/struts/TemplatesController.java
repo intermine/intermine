4b643a908a8183c450100f6845eb45af0a0754b1
rnsmith <rns>
2009-09-02 14:28:52 +0000

    Missing copyright notice.

--- intermine/web/main/src/org/intermine/web/struts/TemplatesController.java
+++ intermine/web/main/src/org/intermine/web/struts/TemplatesController.java
@@ -1,5 +1,15 @@
 package org.intermine.web.struts;
 
+/*
+ * Copyright (C) 2002-2009 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
