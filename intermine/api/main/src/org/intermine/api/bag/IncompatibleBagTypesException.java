11cd1a06ac54b93f45bdaae43abddc7be0002e87
rnsmith <rns>
2009-09-04 11:09:43 +0000

    Move bag logical operations out of struts and into api.

---
+++ intermine/api/main/src/org/intermine/api/bag/IncompatibleBagTypesException.java
@@ -0,0 +1,68 @@
+package org.intermine.api.bag;
+
+/*
+ * Copyright (C) 2002-2009 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+import java.util.Collection;
+
+/**
+ * Exception thrown when given bags are of incompatible types, has methods to set and fetch the
+ * types
+ * @author Richard Smith
+ *
+ */
+public class IncompatibleBagTypesException extends RuntimeException 
+{
+    private Collection<String> types;
+   
+    /**
+     * Constructs an IncompatibleBagTypesException with the specified detail message.
+     *
+     * @param msg the detail message
+     */
+    public IncompatibleBagTypesException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * Constructs an IncompatibleBagTypesException with the specified nested throwable.
+     *
+     * @param t the nested throwable
+     */
+    public IncompatibleBagTypesException(Throwable t) {
+        super(t);
+    }
+
+    /**
+     * Constructs an IncompatibleBagTypesException with the specified detail message and nested
+     * throwable.
+     *
+     * @param msg the detail message
+     * @param t the nested throwable
+     */
+    public IncompatibleBagTypesException(String msg, Throwable t) {
+        super(msg, t);
+    }
+    
+    /**
+     * Set the bag types that were incompatible.
+     * @param types the bag types
+     */
+    public void setTypes(Collection<String> types) {
+        this.types = types;
+    }
+    
+    /**
+     * Get the bag types that were incompatible.
+     * @return the incompatible types
+     */
+    public Collection<String> getTypes() {
+        return this.types;
+    }
+}
--- intermine/web/main/src/org/intermine/web/struts/ModifyBagAction.java
+++ intermine/web/main/src/org/intermine/web/struts/ModifyBagAction.java
@@ -11,8 +11,9 @@ package org.intermine.web.struts;
  */
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
@@ -28,15 +29,12 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionMessage;
 import org.intermine.api.bag.BagManager;
+import org.intermine.api.bag.BagOperations;
+import org.intermine.api.bag.IncompatibleBagTypesException;
 import org.intermine.api.bag.InterMineBag;
 import org.intermine.api.profile.Profile;
-import org.intermine.metadata.ClassDescriptor;
-import org.intermine.metadata.Model;
 import org.intermine.objectstore.ObjectStoreException;
-import org.intermine.objectstore.query.ObjectStoreBagCombination;
-import org.intermine.objectstore.query.Query;
 import org.intermine.util.StringUtil;
-import org.intermine.util.TypeUtil;
 import org.intermine.web.logic.Constants;
 import org.intermine.web.logic.bag.BagHelper;
 import org.intermine.web.logic.session.SessionMethods;
@@ -80,18 +78,15 @@ public class ModifyBagAction extends InterMineAction
         if (request.getParameter("union") != null
                 || (mbf.getListsButton() != null && mbf.getListsButton()
                         .equals("union"))) {
-            combine(form, request, ObjectStoreBagCombination.UNION,
-                    "UNION");
+            combine(form, request, BagOperations.UNION);
         } else if (request.getParameter("intersect") != null
                 || (mbf.getListsButton() != null && mbf.getListsButton()
                         .equals("intersect"))) {
-            combine(form, request,
-                    ObjectStoreBagCombination.INTERSECT, "INTERSECT");
+            combine(form, request, BagOperations.INTERSECT);
         } else if (request.getParameter("subtract") != null
                 || (mbf.getListsButton() != null && mbf.getListsButton()
                         .equals("subtract"))) {
-            combine(form, request,
-                    ObjectStoreBagCombination.ALLBUTINTERSECT, "SUBTRACT");
+            combine(form, request, BagOperations.SUBTRACT);
         } else if (request.getParameter("delete") != null
                 || (mbf.getListsButton() != null && mbf.getListsButton()
                         .equals("delete"))) {
@@ -106,7 +101,6 @@ public class ModifyBagAction extends InterMineAction
     }
 
     private void copy(ActionForm form, HttpServletRequest request) throws ObjectStoreException {
-
         HttpSession session = request.getSession();
         Profile profile = (Profile) session.getAttribute(Constants.PROFILE);
         ModifyBagForm frm = (ModifyBagForm) form;
@@ -194,42 +188,36 @@ public class ModifyBagAction extends InterMineAction
     }
 
 
-    private void combine(ActionForm form, HttpServletRequest request, int op, 
-            String opText) throws Exception {
+    private void combine(ActionForm form, HttpServletRequest request, String opText) {
         HttpSession session = request.getSession();
         Profile profile = (Profile) session.getAttribute(Constants.PROFILE);
         ModifyBagForm mbf = (ModifyBagForm) form;
         ServletContext servletContext = session.getServletContext();
-        Model model = (Model) servletContext.getAttribute(Constants.MODEL);
         
         BagManager bagManager = SessionMethods.getBagManager(servletContext);        
         Map<String, InterMineBag> allBags = bagManager.getUserAndGlobalBags(profile);
 
-        String[] selectedBags = mbf.getSelectedBags();
+        String[] selectedBagNames = mbf.getSelectedBags();
 
-        String type = getTypesMatch(allBags, selectedBags, model);
-        if (type == null) {
-            // TODO this didn't get message from message bundle correctly
+        Collection<InterMineBag> selectedBags = getSelectedBags(allBags, selectedBagNames);
+        
+        String newBagName = BagHelper.findNewBagName(allBags.keySet(), mbf.getNewBagName());
+
+        int newBagSize = 0;
+        try {
+            if (opText.equals(BagOperations.UNION)) {
+                newBagSize = BagOperations.union(selectedBags, newBagName, profile);
+            } else if (opText.equals(BagOperations.INTERSECT)) {
+                newBagSize = BagOperations.intersect(selectedBags, newBagName, profile);
+            } else if (opText.equals(BagOperations.SUBTRACT)) {
+                newBagSize = BagOperations.subtract(selectedBags, newBagName, profile);
+            }
+        } catch (IncompatibleBagTypesException e) {
             SessionMethods.recordError(
                     "You can only perform operations on lists of the same type."
-                            + " Lists "
-                            + StringUtil
-                                    .prettyList(Arrays.asList(selectedBags))
+                            + " Lists " + StringUtil.prettyList(Arrays.asList(selectedBagNames))
                             + " do not match.", session);
             return;
-        }
-
-        // Now combine
-        String name = BagHelper.findNewBagName(allBags.keySet(), mbf.getNewBagName());
-        InterMineBag combined = profile.createBag(name, type, "");
-        ObjectStoreBagCombination osbc = new ObjectStoreBagCombination(op);
-        for (int i = 0; i < selectedBags.length; i++) {
-            osbc.addBag(allBags.get(selectedBags[i]).getOsb());
-        }
-        Query q = new Query();
-        q.addToSelect(osbc);
-        try {
-            combined.addToBagFromQuery(q);
         } catch (ObjectStoreException e) {
             LOG.error(e);
             ActionMessage actionMessage = new ActionMessage(
@@ -238,59 +226,25 @@ public class ModifyBagAction extends InterMineAction
             return;
         }
 
-        if (combined.size() > 0) {
-            profile.saveBag(name, combined);
-            SessionMethods.recordMessage("Created list \"" + combined.getName()
+        if (newBagSize > 0) {
+            SessionMethods.recordMessage("Created list \"" + newBagName
                     + "\" as " + opText + " of  "
-                    + StringUtil.prettyList(Arrays.asList(selectedBags)) + ".",
+                    + StringUtil.prettyList(Arrays.asList(selectedBagNames)) + ".",
                     session);
         } else {
-            deleteBag(session, profile, combined);
             SessionMethods.recordError(opText + " operation on lists "
-                    + StringUtil.prettyList(Arrays.asList(selectedBags))
+                    + StringUtil.prettyList(Arrays.asList(selectedBagNames))
                     + " produced no results.", session);
         }
     }
 
-    /**
-     * Given a set of bag names, find out whether they are all of the same type.
-     *
-     * @param bags
-     *            map from bag name to InterMineIdBag subclass
-     * @param selectedBags
-     *            names of bags to match
-     * @param os
-     *            the obejct store
-     * @return a String containing the type name or null if there was no match
-     */
-    private static String getTypesMatch(Map bags, String selectedBags[], Model model) {
-        // Check that all selected bags are of the same type
-        String type = ((InterMineBag) bags.get(selectedBags[0])).getType();
-        String packageName = model.getPackageName();
-        StringBuffer className = new StringBuffer(packageName + "." + type);
-        try {
-            Set classDescriptors = model.getClassDescriptorsForClass(Class
-                    .forName(className.toString()));
-            for (int i = 1; i < selectedBags.length; i++) {
-                boolean currentMatch = false;
-                String otherType = ((InterMineBag) bags.get(selectedBags[i]))
-                        .getType();
-                for (Iterator iter = classDescriptors.iterator(); iter
-                        .hasNext();) {
-                    ClassDescriptor cld = (ClassDescriptor) iter.next();
-                    if (otherType.equals(TypeUtil
-                            .unqualifiedName(cld.getName()))) {
-                        currentMatch = true;
-                    }
-                }
-                if (!currentMatch) {
-                    return null;
-                }
-            }
-        } catch (ClassNotFoundException e) {
-            LOG.error("error while matching bag types", e);
+    private Collection<InterMineBag> getSelectedBags(Map<String, InterMineBag> allBags,
+            String[] selectedBagNames) {
+        Set<InterMineBag> selectedBags = new HashSet<InterMineBag>();
+        for (String bagName : selectedBagNames) {
+            selectedBags.add(allBags.get(bagName));
         }
-        return type;
+        return selectedBags;
     }
 
 
