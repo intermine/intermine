b3d7cf666892e2e58cf07d237e158fd5435618cc
julie-sullivan <julie@flymine.org>
2009-10-16 13:36:26 +0000

    don't hardcode release names
    Former-commit-id: bbe337e58048bdbe33fdaa4c80d6e64172034fa4

--- bio/scripts/template_comparison.sh
+++ bio/scripts/template_comparison.sh
@@ -2,13 +2,20 @@
 
 # this script runs templates for two different releases and compares the results
 
-mine='flymine'
-release1='release-19.0'
-release2='preview-20.0'
-#mailto='julie@flymine.org'
-mailto='all@flymine.org'
+# $*
 
-run_performance_test() {
+if test $# -ne 4
+then
+    echo "Please enter four arguments:  mine_name release1 release2 email_address"
+    echo "eg. flymine release-19.0 preview-20.0 email@domain.org"
+exit 1
+else
+    mine=$0
+    release1=$1
+    release2=$2
+    mailto=$3
+
+    run_performance_test() {
 # release webapp                                                                                                                                                                                               
     echo "changing to webapp directory"
     cd ~/svn/dev/flymine/webapp
@@ -30,7 +37,7 @@ run_performance_test() {
     ant clean-all > ant_output.log
     echo "building webapp"
     ant default -Drelease=$release2 > ant_output.log
-    
+   
 # run the performance_test script                                                                                                                                                                              
     echo "changing back to the script directory"
     cd ~/svn/dev/bio/scripts
@@ -49,14 +56,16 @@ run_acceptance_tests() {
     cd ~/svn/dev/flymine/integrate
     ant acceptance-tests 
 
-    attfile='/home/julie/svn/dev/flymine/integrate/build/acceptance_test.html'     
+    attfile='~/svn/dev/' . $mine . '/integrate/build/acceptance_test.html'     
     subject="Outcome of acceptance tests run for $release2"
         
     mutt -s "$subject" -a $attfile $mailto < /dev/null
 }
 run_performance_test
 run_acceptance_tests
+fi
 echo "done"
+
 
 
 
