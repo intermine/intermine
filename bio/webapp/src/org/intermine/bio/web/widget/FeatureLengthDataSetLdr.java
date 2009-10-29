7c9ce6fccaec9dff3f868ceae48aad588bd5cb31
rnsmith <rns>
2009-10-29 13:22:51 +0000

    Correct for new package names.
    Former-commit-id: 8fa93cc29fc1eacc88ad10372e2cf892cbfcc83d

--- bio/webapp/src/org/intermine/bio/web/widget/FeatureLengthDataSetLdr.java
+++ bio/webapp/src/org/intermine/bio/web/widget/FeatureLengthDataSetLdr.java
@@ -11,11 +11,11 @@ package org.intermine.bio.web.widget;
  */
 
 import java.text.DecimalFormat;
-import java.text.FieldPosition;
 import java.util.Iterator;
 
 import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
 import org.apache.log4j.Logger;
+import org.intermine.api.profile.InterMineBag;
 import org.intermine.metadata.Model;
 import org.intermine.model.bio.Organism;
 import org.intermine.objectstore.ObjectStore;
@@ -32,7 +32,6 @@ import org.intermine.objectstore.query.QueryValue;
 import org.intermine.objectstore.query.Results;
 import org.intermine.objectstore.query.ResultsRow;
 import org.intermine.objectstore.query.SimpleConstraint;
-import org.intermine.web.logic.bag.InterMineBag;
 import org.intermine.web.logic.widget.DataSetLdr;
 import org.jfree.data.function.Function2D;
 import org.jfree.data.function.NormalDistributionFunction2D;
