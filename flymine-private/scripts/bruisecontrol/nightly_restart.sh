3a18dada2998dcd27e31d8a6702e7d1d6dfd0031
mnw21 <mnw21>
2009-12-08 16:13:34 +0000

    Correctly warn about old bruise control directories

--- flymine-private/scripts/bruisecontrol/nightly_restart.sh
+++ flymine-private/scripts/bruisecontrol/nightly_restart.sh
@@ -3,8 +3,8 @@
 sleep 30
 /home/bruiser/apache-tomcat-5.5.25/bin/startup.sh
 
-OLD_DIRS=`find /home/bruiser/public_html/tests/archived -maxdepth 1 -name 20\* -atime +30`
+OLD_DIRS=`find /home/bruiser/public_html/tests/archived -maxdepth 1 -name 20\* -ctime +30`
 if ! test -z "$OLD_DIRS" ; then
     echo "There are some old test result directories:"
-    find /home/bruiser/public_html/tests/archived -maxdepth 1 -name 20\* -atime +30
+    find /home/bruiser/public_html/tests/archived -maxdepth 1 -name 20\* -ctime +30
 fi
