15a7cd13f6dac112da3753651f5c4476d1c469c8
rnsmith <rns>
2009-12-14 20:45:02 +0000

    Updates for api refactoring.
    Former-commit-id: d971385bf7fe88ef976808abea414e0fb76ab1c2

--- flymine/webapp/src/org/flymine/web/widget/MirandaURLQuery.java
+++ flymine/webapp/src/org/flymine/web/widget/MirandaURLQuery.java
@@ -13,7 +13,7 @@ package org.flymine.web.widget;
 import org.intermine.objectstore.ObjectStore;
 import org.intermine.pathquery.Constraints;
 import org.intermine.pathquery.PathQuery;
-import org.intermine.web.logic.bag.InterMineBag;
+import org.intermine.api.profile.InterMineBag;
 import org.intermine.web.logic.widget.WidgetURLQuery;
 
 /**
