8be357c15d0355c4bcaf903d5a27f30823493672
mnw21 <mnw21>
2010-03-15 15:12:21 +0000

    Compress bruisecontrol backups more frequently

--- flymine-private/scripts/bruisecontrol/nightly_restart.sh
+++ flymine-private/scripts/bruisecontrol/nightly_restart.sh
@@ -4,9 +4,9 @@ ARCHIVES_DIR="/home/bruiser/public_html/tests/archived"
 
 cd $ARCHIVES_DIR
 
-OLD_DIRS=`find . -maxdepth 1 -type d -name 20\* -ctime +30`
+OLD_DIRS=`find . -maxdepth 1 -type d -name 20\* -ctime +10`
 if ! test -z "$OLD_DIRS" ; then
-    OLD_DIRS=`find . -maxdepth 1 -type d -name 20\* -ctime +16 | sed -e "s/^.\///" | sort`
+    OLD_DIRS=`find . -maxdepth 1 -type d -name 20\* -ctime +3 | sed -e "s/^.\///" | sort`
     echo "`date`  There are `echo "$OLD_DIRS" | wc -l` old test result directories: `echo $OLD_DIRS`" 1>&2
     echo "`date`  There are `echo "$OLD_DIRS" | wc -l` old test result directories: `echo $OLD_DIRS`"
     ARC_NAME=`echo $OLD_DIRS | sed -e "s/^[^ ]*\(2[0-9][0-9][0-9]-[01][0-9]-[0123][0-9]T[012][0-9]\):\([0-5][0-9]\):\([0-5][0-9]\) .*\(2[0-9][0-9][0-9]-[01][0-9]-[0123][0-9]T[012][0-9]\):\([0-5][0-9]\):\([0-5][0-9]\)$/\1-\2-\3--\4-\5-\6/"`
