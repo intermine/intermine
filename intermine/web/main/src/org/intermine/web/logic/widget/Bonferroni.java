41fa0b4a9c7ec8f5d3dda0aebab4be2634e64059
julie-sullivan <julie@flymine.org>
2010-03-22 16:20:33 +0000

    use less strict interpretation of the bonferroni test correction algorithm

--- intermine/web/main/src/org/intermine/web/logic/widget/Bonferroni.java
+++ intermine/web/main/src/org/intermine/web/logic/widget/Bonferroni.java
@@ -19,16 +19,18 @@ import java.util.Map;
  * See online documentation for an in depth description of error correction and bonferroni.
  * Briefly, the p-values are adjusted (multiple hypothesis test correction) by adding the
  * alpha divided by the total number of tests to the original number.
- * 
- * For example, given 100 tests and an alpha value of .05, we would expect 5 false positives.  
- * 
+ *
+ * For example, given 100 tests and an alpha value of .05, we would expect 5 false positives.
+ *
  * @author Julie Sullivan
  */
 public class Bonferroni implements ErrorCorrection
 {
     private HashMap<String, BigDecimal> originalMap = new HashMap<String, BigDecimal>();
     private HashMap<String, BigDecimal> adjustedMap = new HashMap<String, BigDecimal>();
-    private BigDecimal numberOfTests;
+    private BigDecimal numberOfTests, alphaPerTest;
+    private BigDecimal alpha = new BigDecimal(0.05);
+
 
     /**
      * @param originalMap HashMap of go terms and their p-value
@@ -36,6 +38,7 @@ public class Bonferroni implements ErrorCorrection
     public Bonferroni(HashMap<String, BigDecimal> originalMap) {
         this.originalMap = originalMap;
         numberOfTests = new BigDecimal(originalMap.size());
+        alphaPerTest = alpha.divide(numberOfTests);
     }
 
     /**
@@ -51,7 +54,7 @@ public class Bonferroni implements ErrorCorrection
             BigDecimal p = entry.getValue();
 
             // calc new value - p * n
-            BigDecimal adjustedP = p.multiply(numberOfTests);
+            BigDecimal adjustedP = p.add(alphaPerTest);
 
             // don't store values >= maxValue
             if (adjustedP.doubleValue() < max.doubleValue()) {
