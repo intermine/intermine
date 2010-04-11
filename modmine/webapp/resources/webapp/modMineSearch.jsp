10081cfeb41c2477ccacf0ade29a61a60006593b
rnsmith <rns>
2010-04-11 15:07:04 +0000

    Initial version of search, currently disabled on front page.

---
+++ modmine/webapp/resources/webapp/modMineSearch.jsp
@@ -0,0 +1,38 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html"%>
+<%@ taglib uri="/WEB-INF/struts-tiles.tld" prefix="tiles"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="im"%>
+<%@ taglib uri="http://flymine.org/imutil" prefix="imutil"%>
+<%@ taglib uri="http://jakarta.apache.org/taglibs/string-1.1"
+    prefix="str"%>
+
+<!--  modMineSearch.jsp -->
+
+<link rel="stylesheet" href="model/css/modmine_search.css" type="text/css" media="screen" title="no title" charset="utf-8">
+
+<tiles:importAttribute />
+
+<html:xhtml />
+
+<div class="modMineSearch">
+
+  <h2>Search modMine</h2>
+  <p><i>Search for modENCODE submissions by metadata</i></p>
+    <html:form action="/modMineSearchAction" focus="searchTerm">
+      <!-- <input style="" id="modMineSearchInput" name="value" type="text" focus = "value" class="modMineSearchBox">-->
+      <html:text property="searchTerm"/>     
+      <html:submit>Search</html:submit>
+    </html:form>
+    <div class="examples">
+    <ul>
+    <li>Antibody names: sadf, asdf, asdf</li>
+    <li>Experiment types: ChIP-chip, RNA-seq</li>
+    <li>Use " to search for a phrase, "Chromatin binding"</li>
+    <li>Combine terms with AND, fly AND embryo</li>
+    </ul>
+    </div>
+</div>
+
+<!-- /modMineSearch.jsp -->
---
+++ modmine/webapp/src/org/modmine/web/ModMineSearchController.java
@@ -0,0 +1,43 @@
+package org.modmine.web;
+
+/*
+ * Copyright (C) 2002-2010 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.tiles.ComponentContext;
+import org.apache.struts.tiles.actions.TilesAction;
+import org.intermine.api.InterMineAPI;
+import org.intermine.web.logic.session.SessionMethods;
+
+
+public class ModMineSearchController extends TilesAction 
+{
+    /**
+     * {@inheritDoc}
+     */
+    public ActionForward execute(@SuppressWarnings("unused")  ComponentContext context,
+                                 @SuppressWarnings("unused") ActionMapping mapping,
+                                 @SuppressWarnings("unused") ActionForm form,
+                                 HttpServletRequest request,
+                                 @SuppressWarnings("unused") HttpServletResponse response)
+    throws Exception {
+
+        final InterMineAPI im = SessionMethods.getInterMineAPI(request.getSession());
+
+        ModMineSearch.initModMineSearch(im);
+
+        return null;
+    }
+}
