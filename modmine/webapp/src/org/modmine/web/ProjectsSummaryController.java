a196c37bf818e10bfa7a6d95f23c25e8760ddf4a
sergiocontrino <sergi@modencode.org>
2010-04-13 11:21:31 +0000

    modmine web: all experiment name correct now (no empty header). todo: allow for duplications.
    Former-commit-id: eac668f7e58916ceda8f01f30a20f8ea1354a8d0

--- modmine/webapp/src/org/modmine/web/ProjectsSummaryController.java
+++ modmine/webapp/src/org/modmine/web/ProjectsSummaryController.java
@@ -52,41 +52,7 @@ public class ProjectsSummaryController extends TilesAction
 
             
             final Map<String, String> expCat = new HashMap<String,String>();
-            expCat.put("Small RNA expression in C. elegans embryos", "Transcriptional profiling");
-            expCat.put("Changes in expression of small RNAs during aging in C. elegans", "Transcriptional profiling");
-            expCat.put("Definition of comprehensive set of C. elegans transcripts and expression for various stages and conditions", "Transcriptional profiling");
-            expCat.put("Short read sequencing of fly mRNA", "Transcriptional profiling");
-            expCat.put("Paired End RNA-Seq of Drosophila Cell Lines", "Transcriptional profiling");
-            expCat.put("Developmental Time Course Transcriptional Profiling of D. melanogaster Embryo Using SOLiD Stranded Total RNA-Seq", "Transcriptional profiling");
-            expCat.put("Transcriptional Profiling of additional Drosophila species with RNA-Seq", "Transcriptional profiling");
-            expCat.put("C-tailed RNA-Seq of Drosophila Cell lines and stages", "Transcriptional profiling");
-            expCat.put("Developmental Stage Timecourse Transcriptional Profiling with RNA-Seq", "Transcriptional profiling");
-            expCat.put("RNAi of RNA splicing factors in D. melanogaster", "Transcriptional profiling");
-            expCat.put("Identification of transcribed sequences under pathogenic bacterial growth conditions with expression profile maps", "Transcriptional profiling");
-            expCat.put("Identification of tissue and stage-specific transcribed sequences with expression profile maps", "Transcriptional profiling");
-            expCat.put("Confirmation of Drosophila transcripts by full-length cDNA screening", "Transcriptional profiling");
-            expCat.put("Identification of transcribed sequences with expression profile", "Transcriptional profiling");
-            
-            expCat.put("Small RNA identification", "Small RNAs");
-            expCat.put("Identification of small RNAs in C. elegans", "Small RNAs");
-            
-            expCat.put("Encyclopedia of C. elegans 3'UTRs and their regulatory elements", "Genome Annotation");
-            expCat.put("Annotation of Drosophila splice junctions by RNA-seq", "Genome Annotation");
-            expCat.put("Cap Analysis of Gene Expression", "Genome Annotation");
-            expCat.put("Intron Confirmation in C. elegans", "Genome Annotation");
-            expCat.put("RT-PCR","Genome Annotation");
-            expCat.put("Gene Model Prediction","Genome Annotation");
-            expCat.put("5' RACE","Genome Annotation");
-
-            expCat.put("Nucleosome mapping", "Chromatin Structure");
-            expCat.put("Genome-wide Chromatin Profiling", "Chromatin Structure");
-            
-            expCat.put("Replication Timing Characterization", "DNA replication");
-            expCat.put("MacAlpine Early Origin of Replication Identification", "DNA replication");
-            expCat.put("Differential Replication of Polytene Chromosomes", "DNA replication");
-            expCat.put("Genome-wide localization of essential replication initiators", "DNA replication");
-            expCat.put("CGH drosophila cell lines", "DNA replication");
-            expCat.put("CNV in Drosophila cell lines", "DNA replication");
+            populate(expCat);
 
             
             Map <String, List<DisplayExperiment>> catExp = new HashMap<String, List<DisplayExperiment>>();
@@ -115,5 +81,68 @@ public class ProjectsSummaryController extends TilesAction
             err.printStackTrace();
         }
         return null;
+    }
+
+    /**
+     * @param expCat
+     */
+    private void populate(final Map<String, String> expCat) {
+
+        expCat.put("ChIP-Seq Identification of C. elegans TF Binding Sites", "Transcription Factor Binding Sites");
+        expCat.put("Chromatin Binding Site Mapping", "Transcription Factor Binding Sites");
+        expCat.put("RNA-seq support of the ChIP data", "Transcription Factor Binding Sites");
+
+        
+        expCat.put("Chromatin ChIP-chip", "Chromatin Marks and Chromatin Binding Proteins");
+        expCat.put("Genomic Distributions of Histone Modifications", "Chromatin Marks and Chromatin Binding Proteins");
+        expCat.put("Chromatin Binding Site Mapping", "Chromatin Marks and Chromatin Binding Proteins");
+        expCat.put("RNA-seq support of the ChIP data", "Chromatin Marks and Chromatin Binding Proteins");
+
+        
+        expCat.put("Nucleosome mapping", "Chromatin Structure");
+        expCat.put("Genome-wide Chromatin Profiling", "Chromatin Structure");
+
+        
+        expCat.put("Replication Timing Characterization", "DNA Replication");
+        expCat.put("MacAlpine Early Origin of Replication Identification", "DNA Replication");
+        expCat.put("Differential Replication of Polytene Chromosomes", "DNA Replication");
+        expCat.put("Genome-wide localization of essential replication initiators", "DNA Replication");
+        expCat.put("CGH drosophila cell lines", "DNA Replication");
+        expCat.put("CNV in Drosophila cell lines", "DNA Replication");
+        
+
+        expCat.put("Short read sequencing of fly mRNA", "mRNA Profiling");
+        expCat.put("Paired End RNA-Seq of Drosophila Cell Lines", "mRNA Profiling");
+        expCat.put("Developmental Time Course Transcriptional Profiling of D. melanogaster Embryo Using SOLiD Stranded Total RNA-Seq", "mRNA Profiling");
+        expCat.put("Transcriptional Profiling of additional Drosophila species with RNA-Seq", "mRNA Profiling");
+        expCat.put("C-tailed RNA-Seq of Drosophila Cell lines and stages", "mRNA Profiling");
+        expCat.put("Developmental Stage Timecourse Transcriptional Profiling with RNA-Seq", "mRNA Profiling");
+        expCat.put("Confirmation of Drosophila transcripts by full-length cDNA screening", "mRNA Profiling");
+        expCat.put("Identification of transcribed sequences with expression profile maps", "mRNA Profiling");
+        expCat.put("RNA made in Bloomington", "mRNA Profiling");
+        expCat.put("RNAi of RNA splicing factors in D. melanogaster", "mRNA Profiling");
+
+        expCat.put("Definition of comprehensive set of C. elegans transcripts and expression for various stages and conditions", "mRNA Profiling");
+        expCat.put("Identification of transcribed sequences under pathogenic bacterial growth conditions with expression profile maps", "mRNA Profiling");
+        expCat.put("Identification of tissue and stage-specific transcribed sequences with expression profile maps", "mRNA Profiling");
+
+        
+        expCat.put("Small RNA identification", "Small RNAs Profiling");
+
+        expCat.put("Small RNA expression in C. elegans embryos", "Small RNAs Profiling");
+        expCat.put("Identification of small RNAs in C. elegans", "Small RNAs Profiling");
+        expCat.put("Changes in expression of small RNAs during aging in C. elegans", "Small RNAs Profiling");        
+        
+        
+        expCat.put("Annotation of Drosophila splice junctions by RNA-seq", "Gene Structure");
+        expCat.put("Cap Analysis of Gene Expression", "Gene Structure");
+        expCat.put("RT-PCR","Gene Structure");
+        expCat.put("Gene Model Prediction","Gene Structure");
+        expCat.put("5' RACE","Gene Structure");
+
+        expCat.put("Encyclopedia of C. elegans 3'UTRs and their regulatory elements", "Gene Structure");
+        expCat.put("Intron Confirmation in C. elegans", "Gene Structure");
+        expCat.put("Definition of comprehensive set of C. elegans transcripts and expression for various stages and conditions", "Gene Structure");
+        
     }
 }
