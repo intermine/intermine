ee9366f9bb63ce8d870aa98ba5787b1271384908
rnsmith <rns>
2010-04-16 13:20:24 +0000

    No exception on blank search.

--- modmine/webapp/src/org/modmine/web/ModMineSearchResultsController.java
+++ modmine/webapp/src/org/modmine/web/ModMineSearchResultsController.java
@@ -21,6 +21,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
@@ -53,35 +54,39 @@ public class ModMineSearchResultsController extends TilesAction
         final InterMineAPI im = SessionMethods.getInterMineAPI(request.getSession());
 
         ModMineSearch.initModMineSearch(im);
+        LinkedHashMap<Submission, Integer> submissions = new LinkedHashMap<Submission, Integer>();
+        Map<Float, Integer> scores = new TreeMap<Float, Integer>();
         
         String searchTerm = (String) request.getParameter("searchTerm");
+        LOG.info("SEARCH TERM: '" + searchTerm + "'");
+        if (!StringUtils.isBlank(searchTerm) && !searchTerm.trim().equals('*')) {
+            Map<Integer, Float> searchResults = ModMineSearch.runLuceneSearch(searchTerm);
+
+            Set<Integer> objectIds = searchResults.keySet();
+
         
-        Map<Integer, Float> searchResults = ModMineSearch.runLuceneSearch(searchTerm);
+            LOG.info("SEARCH HITS: " + searchResults.size());
 
-        Set<Integer> objectIds = searchResults.keySet();
+            
+            Map<Integer, Integer> subMap = ModMineSearch.getSubMap();
+            for (Map.Entry<Integer, Float> entry : searchResults.entrySet()) {
+                scores.put(entry.getValue(), subMap.get(entry.getKey()));
+            }
+            
 
-        
-        LOG.info("SEARCH HITS: " + searchResults.size());
+            Map<Integer, Submission> objMap = new HashMap<Integer, Submission>();
+            for (InterMineObject obj : im.getObjectStore().getObjectsByIds(objectIds)) {
+                objMap.put(obj.getId(), (Submission) obj);
+            }
+            LOG.info("SEARCH - OBJS: " + objMap.size());
 
-        Map<Float, Integer> scores = new TreeMap<Float, Integer>();
-        Map<Integer, Integer> subMap = ModMineSearch.getSubMap();
-        for (Map.Entry<Integer, Float> entry : searchResults.entrySet()) {
-            scores.put(entry.getValue(), subMap.get(entry.getKey()));
+
+            for (Map.Entry<Integer, Float> entry : searchResults.entrySet()) {
+                submissions.put(objMap.get(entry.getKey()), new Integer(Math.round(entry.getValue() * 10)));
+            }
         }
-        request.setAttribute("scores", scores);
-
-        Map<Integer, Submission> objMap = new HashMap<Integer, Submission>();
-        for (InterMineObject obj : im.getObjectStore().getObjectsByIds(objectIds)) {
-            objMap.put(obj.getId(), (Submission) obj);
-        }
-        LOG.info("SEARCH - OBJS: " + objMap.size());
-        
-        LinkedHashMap<Submission, Integer> submissions = new LinkedHashMap<Submission, Integer>();
-        for (Map.Entry<Integer, Float> entry : searchResults.entrySet()) {
-            submissions.put(objMap.get(entry.getKey()), new Integer(Math.round(entry.getValue() * 10)));
-        }
-        
         LOG.info("SEARCH SUBS: " + submissions.size());
+        request.setAttribute("scores", scores);
         request.setAttribute("submissions", submissions);
         
         
