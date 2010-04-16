81048be3adfc7d6b64199c064a433c8b5ac6ec92
alexkalderimis <alex.kalderimis@gmail.com>
2010-04-16 15:03:05 +0000

    Improved comments
    Former-commit-id: f4fecf75384df1fd881b9fb9c778746c6a53cc5a

--- intermine/perl/samples/FlyMine/query_service.pl
+++ intermine/perl/samples/FlyMine/query_service.pl
@@ -1,46 +1,58 @@
 #!/usr/bin/perl -w
 
+# This sample shows how to use these modules to call the API,
+# returning results as either a tab delimited list or
+# formated by perl by using the result_table method
+
 use strict;
 use warnings;
 
-use InterMine::PathQuery;
-use InterMine::WebService::Service::QueryService;
-use InterMine::WebService::Service::ModelService;
 
-my @service_args = ('http://preview.flymine.org/query/service', 'service_example');
+use InterMine::PathQuery;                          # This module creates objects to pass to the service
+use InterMine::WebService::Service::QueryService;  # This module configures the access to the webservice
+use InterMine::WebService::Service::ModelService;  # This module fetches the relevant data model, 
+                                                   # which we use to check that our queries are legal.
 
-my $query_service = new InterMine::WebService::Service::QueryService(@service_args);
-my $model_service = new InterMine::WebService::Service::ModelService(@service_args);
+my ($url, $app_name) = ('http://preview.flymine.org/query/service', 'service_example');
 
-my $path_query = new InterMine::PathQuery($model_service->get_model());
+my $query_service = InterMine::WebService::Service::QueryService->new($url, $app_name);
+my $model_service = InterMine::WebService::Service::ModelService->new($url, $app_name);
 
-# set the output columns, argument can be a array of paths instead
+my $model         = $model_service->get_model;
+my $path_query    = InterMine::PathQuery->new($model);
+
+# set the output columns, argument can be a array of paths instead, 
+# or equally add_view can be called multiple times
 $path_query->add_view('Organism.name Organism.taxonId');
 
 # set the sort order, default is the first path in the view
 $path_query->sort_order('Organism.taxonId');
 
-
 # get the results as one string, containing multiple lines and tab delimited
 # columns
-my $res = $query_service->get_result($path_query);
-print "All organisms:\n";
-print $res->content();
+# get_result takes between 1 and 3 arguments
+# The query is the first (either as an InterMine::PathQuery object or as an xml string), 
+#     this is the only mandatory argument
+# Then optionally $start (the index of the first result you want, useful if paging - defaults to 0)
+# Then optionally $max_count (the total number of results you want - defaults to 100)
+my $res = $query_service->get_result($path_query);# This returns an HTTP::Request object 
+                                                  # (see perldoc HTTP::Request
 
+print "All organisms:\n";
+print $res->content() unless $res->is_error;
 
 # now constrain the genus
 $path_query->add_constraint('Organism.genus = "Drosophila"');
 
-
 # print the result table again - will be smaller
-my $drosophila_res = $query_service->get_result($path_query);
+my $drosophila_res = $query_service->get_result($path_query); # This returns an HTTP::Request object 
+                                                              # (see perldoc HTTP::Request
 print "\n", '-' x 70, "\nOnly Drosophilas:\n";
-print $drosophila_res->content();
-
+print $drosophila_res->content() unless $drosophila_res->is_error;
 
 # get the results as a table instead
 print "\n", '-' x 70, "\nDrosophila results as a table:\n";
-my @res = $query_service->get_result_table($path_query);
+my @res = $query_service->get_result_table($path_query); # This returns a reference to an array of arrays
 for my $row_ref (@res) {
   my @row = @{$row_ref};
 
