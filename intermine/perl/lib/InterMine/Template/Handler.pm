f69957082fc7c9191259561518e80ee3220f2f47
alexkalderimis <alex.kalderimis@gmail.com>
2010-04-28 16:15:19 +0000

    Inital check-in of InterMine::Template::Handler, the XML parser for InterMine::Template
    Former-commit-id: dee5c6fd2c78eef77cf202c7c78976c6607a0c08

---
+++ intermine/perl/lib/InterMine/Template/Handler.pm
@@ -0,0 +1,59 @@
+package InterMine::Template::Handler;
+
+use strict;
+use warnings;
+
+use InterMine::PathQuery::Constraint;
+
+sub new {
+    my $class = shift;
+    my $self = {}; 
+    return bless $self, $class;
+}
+
+sub start_element {
+    my $self = shift;
+    my $args = shift;
+    
+    my $nameattr = $args->{Attributes}{name};
+    if ($args->{Name} eq 'template') {
+	$self->{template}{name}  = $nameattr;
+	$self->{template}{title} = $args->{Attributes}{title};
+	$self->{template}{longDescription} = $args->{Attributes}{longDescription};
+	$self->{template}{comment} = $args->{Attributes}{comment};
+    }
+    else {
+        my $template = $self->{template};
+        if ($args->{Name} eq 'query') {
+            $template->{model} = $args->{Attributes}{model};
+            my @views = ();
+            if (exists $args->{Attributes}{view}) {
+                @views = split(/\s+/, $args->{Attributes}{view});
+            }
+            $template->{views} = \@views;
+	    $template->{sort_order} = $args->{Attributes}{sortOrder};
+	    $template->{logic} = $args->{Attributes}{constraintLogic};
+        }
+        elsif ($args->{Name} eq 'node') {
+            $self->{current_node} = $args->{Attributes}{path};
+            $template->{paths}{$args->{Attributes}{path}}{type} = $args->{Attributes}{type};
+        }
+        elsif ($args->{Name} eq 'constraint') {
+	    my %args;
+	    $args{path}        = $self->{current_node};
+	    $args{code}        = $args->{Attributes}{code};
+	    $args{op}          = $args->{Attributes}{op};
+	    $args{value}       = $args->{Attributes}{value} unless ($args->{Attributes}{value} eq 'null');
+	    $args{extraValue}  = $args->{Attributes}{extraValue};
+	    $args{editable}    = $args->{Attributes}{editable};
+	    my $con = InterMine::PathQuery::Constraint->new(%args);
+	    
+	    push @{$template->{paths}{$args{path}}{constraints}}, $con;
+        }
+        elsif ($args->{Name} ne 'pathDescription') {
+            die "unexpected element: ", $args->{Name}, "\n";
+        }
+    }
+}
+
+1; 
