3fb7bbce56746c6515426ac2c354a5a545990f72
julie-sullivan <julie@flymine.org>
2010-05-19 13:47:07 +0000

    dataset.title to dataset.name
    Former-commit-id: 5dd5f42a0f9e42e1230b3d68fbe2bb110ffe00af

--- bio/core/main/src/org/intermine/bio/dataconversion/BioDirectoryConverter.java
+++ bio/core/main/src/org/intermine/bio/dataconversion/BioDirectoryConverter.java
@@ -93,7 +93,7 @@ public abstract class BioDirectoryConverter extends DirectoryConverter
         Item dataSet = dataSets.get(title);
         if (dataSet == null) {
             dataSet = createItem("DataSet");
-            dataSet.setAttribute("title", title);
+            dataSet.setAttribute("name", title);
             dataSet.setReference("dataSource", dataSourceItem);
             if (url != null) {
                 dataSet.setAttribute("url", url);
