25677158dc5c38c608b64061d5304664b24e7ae2
boboppie <fengyuan.huu@gmail.com>
2010-06-03 10:01:19 +0000

    Integration of Cytoscape Web to FlyMine Refs #2185
    Former-commit-id: 535aba51ebd4f27aedff8855d4b409312828e7ec

---
+++ bio/webapp/src/org/intermine/bio/web/struts/CytoscapeNetworkExportAction.java
@@ -0,0 +1,78 @@
+package org.intermine.bio.web.struts;
+
+/*
+ * Copyright (C) 2002-2010 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * Export network from Cytoscape Web as in different formats:
+ * "png", "pdf", "xgmml", "graphml", "sif".
+ *
+ * @author Fengyuan Hu
+ */
+public class CytoscapeNetworkExportAction extends Action
+{
+    /**
+     * {@inheritDoc}
+     */
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+        throws Exception {
+
+        String type = request.getParameter("type");
+
+        if (type.equals("sif")) {
+            response.setContentType("text/plain");
+            response.setHeader("Content-Disposition", "attachment; filename=\"network.sif\"");
+           }
+        else if (type.equals("pdf")) {
+            response.setContentType("application/pdf");
+            response.setHeader("Content-Disposition", "attachment; filename=\"network.pdf\"");
+           }
+        else if (type.equals("xgmml")) {
+            response.setContentType("text/xml");
+            response.setHeader("Content-Disposition", "attachment; filename=\"network.xgmml\"");
+           }
+        else if (type.equals("png")) {
+            response.setContentType("image/png");
+            response.setHeader("Content-Disposition", "attachment; filename=\"network.png\"");
+           }
+        else if (type.equals("graphml")) {
+            response.setContentType("text/xml");
+            response.setHeader("Content-Disposition", "attachment; filename=\"network.graphml\"");
+           }
+
+        ServletInputStream is = request.getInputStream();
+        ServletOutputStream out = response.getOutputStream();
+
+        byte[] b = new byte[16384];
+
+        int i = 0;
+        while ((i = is.read(b)) != -1) {
+          out.write(b, 0, i);
+        }
+
+        out.flush();
+        out.close();
+
+        return null;
+    }
+}
