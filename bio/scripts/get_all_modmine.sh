03f83238c5ef1ce52685ded6120370148deafab8
sergiocontrino <sergi@modencode.org>
2010-06-11 09:37:23 +0000

    modmine script: not checking any longer for uniprot and swissprot sources (relying on a centralised updating mechanisms)

--- bio/scripts/get_all_modmine.sh
+++ bio/scripts/get_all_modmine.sh
@@ -6,7 +6,8 @@ then
 fi
 tempname="temp_log.txt"
 tempfile=$logdir$tempname
-shared_data="/shared/data/modmine/sources"
+modmine_data="/shared/data/modmine/sources"
+shared_data="/shared/data"
 config_file="./resources/get_scripts_modmine.config"
 if [ -a $tempfile ]
 then
@@ -17,7 +18,7 @@ $errmsg="";
 
 echo "==========================================================="
 echo "Getting Fly Anatomy Ontology" 
-./get_fly_anatomy_ontology $logdir $tempname $shared_data
+./get_fly_anatomy_ontology $logdir $tempname $modmine_data
     if [ $? -ne 0 ]
     then
         echo "error getting Fly Anatomy Ontology" 2>&1        
@@ -27,7 +28,7 @@ echo "==========================================================="
 echo
 echo "==========================================================="
 echo "Getting GO annotation"
-./get_go-annotation $logdir $tempname $shared_data $config_file 
+./get_go-annotation $logdir $tempname $modmine_data $config_file
     if [ $? -ne 0 ]
     then
         echo "error gettingGO annotation" 2>&1
@@ -36,33 +37,33 @@ echo "Getting GO annotation"
 echo
 echo "==========================================================="
 echo "Getting PubMed data"
-./get_ncbi_pubmed $logdir $tempname $shared_data $config_file
+./get_ncbi_pubmed $logdir $tempname $modmine_data $config_file
     if [ $? -ne 0 ]
     then
         "error getting PubMed data" 2>&1 >> tempfile
     fi
 
-echo "==========================================================="
-echo
-echo "==========================================================="
-echo "Getting Interpro xml file"
-./get_interproXML $logdir $tempname $shared_data || (echo "error gettin InterPro XML file" 2>&1; exit 1)
+# echo "==========================================================="
+# echo
+# echo "==========================================================="
+# echo "Getting Interpro xml file"
+# ./get_interproXML $logdir $tempname $shared_data || (echo "error gettin InterPro XML file" 2>&1; exit 1)
 
-echo "==========================================================="
-echo
-echo "==========================================================="
-echo "Getting UniProt data"
-./get_uniprot $logdir $tempname $shared_data "../../modmine/project.xml"
-    if [ $? -ne 0 ]
-    then
-        echo "error getting UniProt data" 2>&1
-#        exit 1
-    fi
+# echo "==========================================================="
+# echo
+# echo "==========================================================="
+# echo "Getting UniProt data"
+# ./get_uniprot $logdir $tempname $shared_data "../../modmine/project.xml"
+#     if [ $? -ne 0 ]
+#     then
+#         echo "error getting UniProt data" 2>&1
+# #        exit 1
+#     fi
 echo "==========================================================="
 echo
 echo "==========================================================="
 echo "Getting InParanoid data"
-./get_inparanoid $logdir $tempname $shared_data $config_file 
+./get_inparanoid $logdir $tempname $modmine_data $config_file
     if [ $? -ne 0 ]
     then
         echo "error getting InParanoid data" 2>&1
