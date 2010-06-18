3805f088e91530841ee9fa9cc2275a6b958d42eb
alexkalderimis <alex.kalderimis@gmail.com>
2010-06-18 13:21:56 +0000

    Now reads information from the xml more aggressively, including nodes, types and pathdescriptions

--- intermine/perl/lib/InterMine/Template/Handler.pm
+++ intermine/perl/lib/InterMine/Template/Handler.pm
@@ -34,23 +34,33 @@ sub start_element {
 	    $template->{sort_order} = $args->{Attributes}{sortOrder};
 	    $template->{constraintLogic} = $args->{Attributes}{constraintLogic};
         }
+	elsif ($args->{Name} eq 'pathDescription') {
+	    $template->{pathDescriptions}{$args->{Attributes}{pathString}} =
+		$args->{Attributes}{description};
+	}
         elsif ($args->{Name} eq 'node') {
-            $self->{current_node} = $args->{Attributes}{path};
-#            $template->{paths}{$args->{Attributes}{path}}{type} = $args->{Attributes}{type};
+	    my $path = $args->{Attributes}{path};
+            $self->{current_node} = $path;
+	    $template->{constraints}{$path} = [];
+	    $template->{type_of}{$path} = $args->{Attributes}{type};
         }
         elsif ($args->{Name} eq 'constraint') {
-	    my %args;
-	    $args{path}        = $self->{current_node};
-	    $args{code}        = $args->{Attributes}{code};
-	    $args{op}          = $args->{Attributes}{op};
-	    $args{value}       = $args->{Attributes}{value} unless ($args->{Attributes}{value} eq 'null');
-	    $args{extraValue}  = $args->{Attributes}{extraValue};
-	    $args{editable}    = $args->{Attributes}{editable};
+	    my %args = (
+		path        => $self->{current_node},
+		code        => $args->{Attributes}{code},
+		op          => $args->{Attributes}{op},
+		extraValue  => $args->{Attributes}{extraValue},
+		editable    => $args->{Attributes}{editable},
+		description => $args->{Attributes}{description},
+		identifier  => $args->{Attributes}{identifier},
+		);
+	    $args{value}    = $args->{Attributes}{value} unless ($args->{Attributes}{value} eq 'null');
+
 	    my $con = InterMine::PathQuery::Constraint->new(%args);
 	    
 	    push @{$template->{constraints}{$args{path}}}, $con;
         }
-        elsif ($args->{Name} ne 'pathDescription') {
+       	else {
             die "unexpected element: ", $args->{Name}, "\n";
         }
     }
