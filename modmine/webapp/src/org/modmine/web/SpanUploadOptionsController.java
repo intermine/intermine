638b4a3f451989bf2bee8c3d0d725b5fea5d885b
boboppie <fengyuan.huu@gmail.com>
2010-06-18 16:29:59 +0000

    comments added

--- modmine/webapp/src/org/modmine/web/SpanUploadOptionsController.java
+++ modmine/webapp/src/org/modmine/web/SpanUploadOptionsController.java
@@ -11,6 +11,9 @@ package org.modmine.web;
  */
 
 
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -19,6 +22,7 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.tiles.ComponentContext;
 import org.apache.struts.tiles.actions.TilesAction;
+import org.intermine.objectstore.ObjectStore;
 
 /**
  * Set up environment for the spanUploadOptions page. It is called immediately before the
@@ -39,6 +43,11 @@ public class SpanUploadOptionsController extends TilesAction
                                  HttpServletRequest request,
                                  HttpServletResponse response)
         throws Exception {
+
+        // get data from MetadataCache
+        // Map cagExp
+        // Map<Integer, List<String>> submissionLocatedFeatureTypes
+        // List<DisplayExperiment> getExperiments(ObjectStore os)
 
         return null;
     }
