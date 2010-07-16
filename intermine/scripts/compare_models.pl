14c6c6ad2e72e8bef071e6389530583b1f80dfdf
alexkalderimis <alex.kalderimis@gmail.com>
2010-07-16 16:35:06 +0000

    Prettified the code through formatting, and the output, through colour choices

--- intermine/scripts/compare_models.pl
+++ intermine/scripts/compare_models.pl
@@ -35,12 +35,12 @@ sub make_table_from_rows {
 	    $colour = 'orange';
 	}
 	else {
-	    $colour = 'green';
+	    $colour = 'YellowGreen';
 	}
 	for my $col (2 .. @columns) {
 	    $table->setCellBGColor(
 		$row, $col, 
-		(($table->getCell($row, $col) eq 'NO') ? 'red' : $colour),
+		(($table->getCell($row, $col) eq 'NO') ? 'OrangeRed' : $colour),
 		);
 	}
     }
@@ -50,15 +50,23 @@ sub make_table_from_rows {
 my %model_from;
 my %link_to;
 my %version_of;
+my @services;
 while (<DATA>) {
     chomp;
     next unless /^http/;
     my $ms = 
 	InterMine::WebService::Service::ModelService->new($_ . '/service', APP);
     my ($service_name) = /([a-z]+mine)/;
+    push @services, $service_name;
     $model_from{$service_name}    = $ms->get_model();
     $link_to{$service_name}       = $_;
     $version_of{$service_name}    = get($_ . '/service/version/release');
+}
+
+
+my $service_table = HTML::Table->new();
+while (my ($service, $url) = each %link_to) {
+    $service_table->addRow(qq{<strong><a href="$url">$service</a></strong>}, "Version: $version_of{$service}");
 }
 
 my %classes;
@@ -104,7 +112,7 @@ for my $class_name (sort($order grep {$classes{$_} > 1} keys %classes)) {
     my @rows = map { {Field => $_} } sort($order keys %fields);
     next unless @rows;
     for my $row (@rows) {
-	for my $service (keys %model_from) {
+	for my $service (@services) {
 	    $row->{$service} = (grep {$row->{Field} eq $_} @{$fields_for{$service}})
 		              ? 'YES'
                               : 'NO';
@@ -115,24 +123,28 @@ for my $class_name (sort($order grep {$classes{$_} > 1} keys %classes)) {
     push @class_tables, {name => $class_name, tbl => $class_table};
 }
 
-my $dt = DateTime->now;
+my $dt   = DateTime->now;
+$dt->set_time_zone('Europe/London');
+my $date =  $dt->dmy('/');
+my $time =  $dt->time;
 
 open(my $HTMLFH, '>', $file) or die "Blah blah blah error schmeror: $!";
-print $HTMLFH '<html><head></head><body>';
-print $HTMLFH '<h1>Model Comparison Tables</h1>';
-print $HTMLFH '<p><i>Produced on ', $dt->dmy('-'), ' at ', $dt->hms(':'), '</i></p>';
-print $HTMLFH q{<p>This page has several tables to compare different elements found within the genomic models used by different InterMine implementations. These models were fetched from the respective webservices, and then analysed to create these tables.</p> 
-<p>The main table lists which classes appear in which Mine's model. Where the class is absent, the word "NO" appears in a red box. If the class is present, then the number of fields in that class appear in the cell. If the models with this class all have a class the same number of fields, then their cells are green. If there is a difference in the number of fields, then the cell is coloured orange.</>
-<p>To further investigate classes with differences you can click on the class name, which is a link that will take you to a sub-table, showing which fields are present or absent in a particular model. Only classes that are present in more than one model have a sub-table, and thus a link.</p>};
-print $HTMLFH '<ul>';
-my $service_table = HTML::Table->new();
-while (my ($service, $url) = each %link_to) {
-    $service_table->addRow(qq{<strong><a href="$url">$service</a></strong>}, "Version: $version_of{$service}");
-}
-print $HTMLFH $service_table->getTable, '</br>';
-print $HTMLFH '</li>';
-print $HTMLFH '<h2>Classes by Model</h2></br>';
-print $HTMLFH $model_class_table->getTable, '</br>';
+print $HTMLFH <<"ENDHTML";
+<html>
+    <head></head>
+    <body>
+    <h1>Model Comparison Tables</h1>
+    <p>
+      <small>Automatically generated on $date at $time</small>
+    </p>
+    <p>This page has several tables to compare different elements found within the genomic models used by different InterMine implementations. These models were fetched from the respective webservices, and then analysed to create these tables.</p> 
+    <p>The main table lists which classes appear in which Mine's model. Where the class is absent, the word "NO" appears in a red box. If the class is present, then the number of fields in that class appear in the cell. If the models with this class all have a class the same number of fields, then those models' cells are green. If there is a difference in the number of fields, then the cell is coloured orange.</p>
+    <p>To further investigate classes with differences you can click on the class name, which is a link that will take you to a sub-table, showing which fields are present or absent in a particular model. Only classes that are present in more than one model have a sub-table, and thus a link.</p>
+    <ul>$service_table</br>
+    <h2>Classes by Model</h2></br>
+    $model_class_table</br>
+ENDHTML
+
 for my $table (@class_tables) {
     print $HTMLFH '<h2><a name="',$table->{name},'">',$table->{name},'</a></h2></br>';
     print $HTMLFH $table->{tbl}->getTable, '</br>';
