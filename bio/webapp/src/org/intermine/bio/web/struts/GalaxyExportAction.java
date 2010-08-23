8a3cdba99698cd268408ab20881c4ff64d607a72
boboppie <fengyuan.huu@gmail.com>
2010-08-23 13:04:17 +0000

    Galaxy - removed duplicated copyright info
    Former-commit-id: 8009ef0e9eaefd46a67abfd4b1a4b0f8594f765e

--- bio/webapp/src/org/intermine/bio/web/struts/GalaxyExportAction.java
+++ bio/webapp/src/org/intermine/bio/web/struts/GalaxyExportAction.java
@@ -45,16 +45,6 @@ import org.intermine.web.logic.session.SessionMethods;
 import org.intermine.web.struts.InterMineAction;
 import org.intermine.web.util.URLGenerator;
 
-/*
- * Copyright (C) 2002-2010 FlyMine
- *
- * This code may be freely distributed and modified under the
- * terms of the GNU Lesser General Public Licence.  This should
- * be distributed with the code.  See the LICENSE file for more
- * information or http://www.gnu.org/copyleft/lesser.html.
- *
- */
-
 /**
  * Generate feature path query.
  *
