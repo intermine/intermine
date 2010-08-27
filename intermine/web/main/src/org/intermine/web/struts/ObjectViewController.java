829a4b5410cc45e4e543e680c950f93bfb5b6af5
julie-sullivan <julie@flymine.org>
2010-08-27 13:00:53 +0000

    more config changes

--- intermine/web/main/src/org/intermine/web/struts/ObjectViewController.java
+++ intermine/web/main/src/org/intermine/web/struts/ObjectViewController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
@@ -52,6 +53,12 @@ public class ObjectViewController extends TilesAction
         DisplayObjectFactory displayObjects = SessionMethods.getDisplayObjects(session);
 
         String idString = (String) context.getAttribute("id");
+
+        // not a key field
+        if (StringUtils.isEmpty(idString)) {
+            return null;
+        }
+
         String fieldName = (String) context.getAttribute("fieldName");
         fieldName = fieldName.trim();
 
