ce023f5d3a2ea9c8849a297fff1c48b9228cceef
julie-sullivan <julie@flymine.org>
2010-09-13 10:38:36 +0000

    made project build screen more readable.  make project build screen smaller by removing unneeded labels and options, putting text fields on same line as labels.
    Former-commit-id: 8d71bd0c0fe962df3556bcc83038eee74cd6750c

--- intermine/MineManager/installer/src/main/java/org/intermine/install/swing/BuildProjectDialog.java
+++ intermine/MineManager/installer/src/main/java/org/intermine/install/swing/BuildProjectDialog.java
@@ -32,7 +32,7 @@ import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.ButtonGroup;
-import javax.swing.JRadioButton; 
+import javax.swing.JRadioButton;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.BorderFactory;
@@ -57,8 +57,8 @@ import org.intermine.common.swing.text.RestrictedInputDocument;
 public class BuildProjectDialog extends StandardJDialog
 {
     private static final long serialVersionUID = -523173830816907855L;
-    
-    
+
+
     /**
      * Start policy group label.
      * @serial
@@ -70,44 +70,44 @@ public class BuildProjectDialog extends StandardJDialog
      * Restart with last dump file radio button.
      * @serial
      */
-    private JRadioButton restart1RadioButton 
+    private JRadioButton restart1RadioButton
         = new JRadioButton(Messages.getMessage("build.project.flag.l"));
-    
+
     /**
      * Restart without loading dump radio button.
      * @serial
      */
-    private JRadioButton restart2RadioButton 
+    private JRadioButton restart2RadioButton
         = new JRadioButton(Messages.getMessage("build.project.flag.r"));
-    
+
     /**
      * Run <code>build-db</code> before build radio button.
      * @serial
      */
-    private JRadioButton buildDbRadioButton 
+    private JRadioButton buildDbRadioButton
         = new JRadioButton(Messages.getMessage("build.project.flag.b"), true);
-    
+
     /**
      * Test run radio button.
      * @serial
      */
-    private JRadioButton testRadioButton 
+    private JRadioButton testRadioButton
         = new JRadioButton(Messages.getMessage("build.project.flag.n"));
-    
+
     /**
      * Set release number check box.
      * @serial
      */
     private JCheckBox releaseNumberCheckBox =
         new JCheckBox(Messages.getMessage("build.project.flag.V"));
-    
+
     /**
      * Release number text field label.
      * @serial
      */
-    private JLabel releaseNumberLabel =
-        new JLabel(Messages.getMessage("build.project.release.number"));
-    
+//    private JLabel releaseNumberLabel =
+//        new JLabel(Messages.getMessage("build.project.release.number"));
+
     /**
      * Release number text field.
      * @serial
@@ -116,21 +116,21 @@ public class BuildProjectDialog extends StandardJDialog
         new JTextField(
                 new RestrictedInputDocument(
                         RestrictedInputDocument.WORD_CHARACTERS + ".+/"), "", 10);
-    
+
     /**
      * Make database backups check box.
      * @serial
      */
 //    private JCheckBox serverBackupCheckBox =
 //        new JCheckBox(Messages.getMessage("build.project.flag.T"));
-    
+
     /**
      * Create destination database check box.
-     * @serial 
+     * @serial
      */
     private JCheckBox destinationCheckBox =
         new JCheckBox(Messages.getMessage("build.project.flag.D"));
-    
+
     /**
       * Create label for userprofile options group
       * @serial
@@ -142,7 +142,7 @@ public class BuildProjectDialog extends StandardJDialog
      * Create write userprofile database without override option radio button
      * @serial
      */
-    private JRadioButton writeUserDbRadio = 
+    private JRadioButton writeUserDbRadio =
         new JRadioButton(Messages.getMessage("build.project.flag.u"));
 
     /**
@@ -156,7 +156,7 @@ public class BuildProjectDialog extends StandardJDialog
      * Create do not write userprofile database option radio button
      * @serial
      */
-    private JRadioButton nowriteUserDbRadio = 
+    private JRadioButton nowriteUserDbRadio =
         new JRadioButton(Messages.getMessage("build.project.NOWRITE"), true);
 
     /**
@@ -170,18 +170,18 @@ public class BuildProjectDialog extends StandardJDialog
       * Database encoding text field
       * @serial
       */
-    private JTextField encodingTextField = 
+    private JTextField encodingTextField =
         new JTextField(
                 new RestrictedInputDocument(
                     RestrictedInputDocument.UPPER_CASE
-                    + RestrictedInputDocument.DIGITS + "_", true), "UTF8", 25);
+                    + RestrictedInputDocument.DIGITS + "_", true), "UTF8", 12);
     /**
      * Destination database text field label.
      * @serial
      */
     private JLabel destinationLabel =
         new JLabel(Messages.getMessage("build.project.destination.db"));
-    
+
     /**
      * Destination database text field.
      * @serial
@@ -190,7 +190,7 @@ public class BuildProjectDialog extends StandardJDialog
         new JTextField(
                 new RestrictedInputDocument(
                         RestrictedInputDocument.WORD_CHARACTERS), "", 12);
-    
+
     /**
      * Dump host text field.
      * @serial
@@ -199,33 +199,33 @@ public class BuildProjectDialog extends StandardJDialog
         new JTextField(
                 new RestrictedInputDocument(
                         RestrictedInputDocument.LOWER_CASE
-                        + RestrictedInputDocument.DIGITS + "_-.", true), "", 20);
-    
+                        + RestrictedInputDocument.DIGITS + "_-.", true), "", 12);
+
     /**
      * Dump prefix text field.
      * @serial
      */
-    private JTextField prefixTextField 
-        = new JTextField(Messages.getMessage("build.project.dump.prefix.default"), 30);
+    private JTextField prefixTextField
+        = new JTextField(Messages.getMessage("build.project.dump.prefix.default"), 12);
 
     /**
      * Action to start running <code>project_build</code>.
-     * @serial 
+     * @serial
      */
     private Action buildAction = new BuildProjectAction();
-    
+
     /**
      * Progress dialog to display the output from <code>project_build</code>.
-     * @serial 
+     * @serial
      */
     private SystemProcessProgressDialog progressDialog;
-    
+
     /**
      * The project file for the mine.
      */
     private transient File projectFile;
-    
-    
+
+
     /**
      * Initialise with a parent Frame.
      * @param owner The parent Frame.
@@ -255,81 +255,76 @@ public class BuildProjectDialog extends StandardJDialog
 
     /**
      * Common initialisation: lays out the child components and wires up the necessary
-     * event listeners. 
+     * event listeners.
      */
     private void init() {
 
-        
+
         setName("Build Project Dialog");
         setTitle(Messages.getMessage("build.project.title"));
-        
-        releaseNumberLabel.setLabelFor(releaseNumberTextField);
+
+//        releaseNumberLabel.setLabelFor(releaseNumberTextField);
         destinationLabel.setLabelFor(destinationTextField);
-        
+
         Container cp = getContentPane();
-        
+
         GridBagConstraints cons = GridBagHelper.setup(cp);
-        
+
         JLabel infoLabel = new JLabel(Messages.getMessage("build.project.info.message"));
         infoLabel.setFont(infoLabel.getFont().deriveFont(Font.PLAIN));
-        cons.gridwidth = GridBagConstraints.REMAINDER;
+        cons.gridwidth = 2;
         cons.weightx = 1;
         cp.add(infoLabel, cons);
-        
+
         JPanel startPolicyPanel = new JPanel();
-        startPolicyPanel.setBorder(BorderFactory.createLoweredBevelBorder());
+//        startPolicyPanel.setBorder(BorderFactory.createLoweredBevelBorder());
         GridBagConstraints startPolicyPanelCons = GridBagHelper.setup(startPolicyPanel);
         startPolicyPanelCons.gridwidth = GridBagConstraints.REMAINDER;
         startPolicyPanel.add(startPolicyLabel, startPolicyPanelCons);
 
         startPolicyPanelCons.gridy++;
-        startPolicyPanelCons.gridwidth = 3;
+        startPolicyPanelCons.gridwidth = 2;
+        buildDbRadioButton.setFont(infoLabel.getFont().deriveFont(Font.PLAIN));
         startPolicyPanel.add(buildDbRadioButton, startPolicyPanelCons);
-        
-        startPolicyPanelCons.gridy++;
-        startPolicyPanel.add(restart1RadioButton, startPolicyPanelCons);
-        
-        startPolicyPanelCons.gridy++;
-        startPolicyPanel.add(restart2RadioButton, startPolicyPanelCons);
 
         startPolicyPanelCons.gridy++;
-        startPolicyPanel.add(testRadioButton, startPolicyPanelCons);
-        
+        restart1RadioButton.setFont(infoLabel.getFont().deriveFont(Font.PLAIN));
+        startPolicyPanel.add(restart1RadioButton, startPolicyPanelCons);
+
+//        startPolicyPanelCons.gridy++;
+//        restart2RadioButton.setFont(infoLabel.getFont().deriveFont(Font.PLAIN));
+//        startPolicyPanel.add(restart2RadioButton, startPolicyPanelCons);
+//
+//        startPolicyPanelCons.gridy++;
+//        testRadioButton.setFont(infoLabel.getFont().deriveFont(Font.PLAIN));
+//        startPolicyPanel.add(testRadioButton, startPolicyPanelCons);
+
         cons.gridy++;
         cp.add(startPolicyPanel, cons);
 
         JPanel userDBPanel = new JPanel();
-        userDBPanel.setBorder(BorderFactory.createLoweredBevelBorder());
+//        userDBPanel.setBorder(BorderFactory.createLoweredBevelBorder());
         GridBagConstraints userDBCons = GridBagHelper.setup(userDBPanel);
-
-        cons.gridwidth = GridBagConstraints.REMAINDER;
+        userDBPanel.setAlignmentY(LEFT_ALIGNMENT);
+//        cons.gridwidth = GridBagConstraints.REMAINDER;
 
         userDBPanel.add(userDBOptionsLabel, userDBCons);
 
         userDBCons.gridy++;
+        nowriteUserDbRadio.setFont(infoLabel.getFont().deriveFont(Font.PLAIN));
         userDBPanel.add(nowriteUserDbRadio, userDBCons);
 
         userDBCons.gridy++;
+        writeUserDbRadio.setFont(infoLabel.getFont().deriveFont(Font.PLAIN));
         userDBPanel.add(writeUserDbRadio, userDBCons);
 
         userDBCons.gridy++;
+        overwriteUserDbRadio.setFont(infoLabel.getFont().deriveFont(Font.PLAIN));
         userDBPanel.add(overwriteUserDbRadio, userDBCons);
-    
+
         cons.gridy++;
         cp.add(userDBPanel, cons);
 
-        cons.gridy++;
-        cp.add(releaseNumberCheckBox, cons);
-
-        cons.gridy++;
-        cons.weightx = 0;
-        cons.gridwidth = 1;
-        cp.add(releaseNumberLabel, cons);
-        
-        cons.gridx++;
-        cons.weightx = 0.5;
-        cp.add(releaseNumberTextField, cons);
-        
         cons.gridy++;
         cons.gridx = 0;
         cons.weightx = 0;
@@ -345,18 +340,22 @@ public class BuildProjectDialog extends StandardJDialog
 //        cons.gridwidth = 3;
 //        cons.weightx = 1;
 //        cp.add(serverBackupCheckBox, cons);
-        
-        cons.gridy++;
-        cp.add(destinationCheckBox, cons);
-        
-        cons.gridy++;
-        cons.weightx = 0;
-        cons.gridwidth = 1;
-        cp.add(destinationLabel, cons);
-        
-        cons.gridx++;
-        cons.weightx = 0.5;
-        cp.add(destinationTextField, cons);
+
+//        cons.gridy++;
+//        cons.gridx = 0;
+//        cons.weightx = 0;
+//        cons.gridwidth = 1;
+//        cp.add(destinationCheckBox, cons);
+
+//        cons.gridy++;
+//        cons.weightx = 0;
+//        cons.gridwidth = 1;
+//        cp.add(destinationLabel, cons);
+
+//        cons.gridx++;
+//        cons.weightx = 0.75;
+//        cons.gridwidth = 2;
+//        cp.add(destinationTextField, cons);
 
         cons.gridy++;
         cons.gridx = 0;
@@ -366,7 +365,7 @@ public class BuildProjectDialog extends StandardJDialog
 
         cons.gridx++;
         cons.weightx = 0.75;
-        cons.gridwidth = 2;
+        cons.gridwidth = 1;
         cp.add(hostTextField, cons);
 
         cons.gridy++;
@@ -377,33 +376,46 @@ public class BuildProjectDialog extends StandardJDialog
 
         cons.gridx++;
         cons.weightx = 0.75;
-        cons.gridwidth = 2;
+        cons.gridwidth = 1;
         cp.add(prefixTextField, cons);
+
+        cons.gridy++;
+        cons.gridx = 0;
+        cp.add(releaseNumberCheckBox, cons);
+
+//        cons.gridy++;
+//        cons.weightx = 0;
+//        cons.gridwidth = 1;
+//        cp.add(releaseNumberLabel, cons);
+
+        cons.gridx++;
+        cons.weightx = 0.5;
+        cp.add(releaseNumberTextField, cons);
 
         cons.gridy++;
         cons.gridwidth = GridBagConstraints.REMAINDER;
         cons.gridx = 0;
         cons.weightx = 1;
         cp.add(new ButtonPanel(buildAction, new CancelAction()), cons);
-        
+
        // encodingTextField.setText("SQL_ASCII");
-        releaseNumberLabel.setEnabled(false);
+//        releaseNumberLabel.setEnabled(false);
         releaseNumberTextField.setEnabled(false);
-        destinationLabel.setEnabled(false);
-        destinationTextField.setEnabled(false);
-        
+//        destinationLabel.setEnabled(false);
+//        destinationTextField.setEnabled(false);
+
 
         /**
             * Create Button group for userdb radio buttons
             * @serial
         */
-        ButtonGroup userDBButtonGroup = 
+        ButtonGroup userDBButtonGroup =
             new ButtonGroup();
         userDBButtonGroup.add(writeUserDbRadio);
         userDBButtonGroup.add(overwriteUserDbRadio);
         userDBButtonGroup.add(nowriteUserDbRadio);
-        
-        ButtonGroup startPolicyGroup = 
+
+        ButtonGroup startPolicyGroup =
             new ButtonGroup();
         startPolicyGroup.add(restart1RadioButton);
         startPolicyGroup.add(restart2RadioButton);
@@ -411,38 +423,37 @@ public class BuildProjectDialog extends StandardJDialog
         startPolicyGroup.add(testRadioButton);
 
         releaseNumberCheckBox.addActionListener(
-                new LinkedFieldListener(releaseNumberCheckBox, releaseNumberLabel,
-                                        releaseNumberTextField));
+                new LinkedFieldListener(releaseNumberCheckBox, null, releaseNumberTextField));
         destinationCheckBox.addActionListener(
                 new LinkedFieldListener(destinationCheckBox, destinationLabel,
                                         destinationTextField));
-        
+
         DocumentListener requiredTextListener = new RequiredTextListener();
         releaseNumberTextField.getDocument().addDocumentListener(requiredTextListener);
-        destinationTextField.getDocument().addDocumentListener(requiredTextListener);
+//        destinationTextField.getDocument().addDocumentListener(requiredTextListener);
         hostTextField.getDocument().addDocumentListener(requiredTextListener);
         prefixTextField.getDocument().addDocumentListener(requiredTextListener);
-        
+
         hostTextField.setToolTipText(Messages.getMessage("build.project.dump.host.tooltip"));
         hostTextField.setText("localhost");
-        
+
         pack();
-        
+
         progressDialog = new SystemProcessProgressDialog(this);
         progressDialog.setModalityType(ModalityType.APPLICATION_MODAL);
         progressDialog.setTitle(Messages.getMessage("build.project.title"));
         progressDialog.setInformationLabel(Messages.getMessage("build.project.message"));
     }
-    
+
     /**
      * Initialise this dialog to set the project file.
-     * 
+     *
      * @param projectFile The project file.
      */
     public void initialise(File projectFile) {
         this.projectFile = projectFile;
     }
-    
+
     /**
      * Update the state of the "build project" action based on fields that require
      * input being completed correctly.
@@ -458,8 +469,8 @@ public class BuildProjectDialog extends StandardJDialog
         }
         buildAction.setEnabled(ok);
     }
-    
-    
+
+
     /**
      * Action listener for check boxes controlling whether other components are relevant
      * (and so enabled).
@@ -470,15 +481,15 @@ public class BuildProjectDialog extends StandardJDialog
          * The controlling check box.
          */
         private JCheckBox checkBox;
-        
+
         /**
          * The components to enable and disable as a result of the check box change.
          */
         private JComponent[] components;
-        
+
         /**
          * Initialise with the source check box and the components it is controlling.
-         * 
+         *
          * @param checkBox The source check box.
          * @param linked The components under control.
          */
@@ -490,24 +501,24 @@ public class BuildProjectDialog extends StandardJDialog
         /**
          * Turn the linked components on or off as a result of this event from the
          * source check box.
-         * 
+         *
          * @param event The action event object.
          */
         @Override
         public void actionPerformed(ActionEvent event) {
             assert event.getSource() == checkBox : "Unexpected source of the event";
-            
+
             boolean on = checkBox.isSelected();
             for (JComponent c : components) {
                 c.setEnabled(on);
             }
         }
     }
-    
+
     /**
      * Document listener for all text fields that are required for the project build to
      * take place.
-     * 
+     *
      * @see BuildProjectDialog#updateState()
      */
     private class RequiredTextListener implements DocumentListener
@@ -527,7 +538,7 @@ public class BuildProjectDialog extends StandardJDialog
             updateState();
         }
     }
-    
+
     /**
      * Action to start the <code>project_build</code> executing and to launch the progress dialog
      * so the user can see its output.
@@ -547,17 +558,17 @@ public class BuildProjectDialog extends StandardJDialog
         /**
          * Called to start the execution of <code>project_build</code>. Starts the worker
          * thread and displays the progress dialog.
-         * 
+         *
          * @param event The action event object.
          */
         @Override
         public void actionPerformed(ActionEvent event) {
-            
+
             File projectHome = projectFile.getParentFile();
             File intermineHome = projectHome.getParentFile();
-            
+
             File projectBuildScript = new File(intermineHome, "bio/scripts/project_build");
-            
+
             List<String> commands = new ArrayList<String>();
             commands.add(projectBuildScript.getAbsolutePath());
             commands.add("-v"); // always be verbose
@@ -597,17 +608,17 @@ public class BuildProjectDialog extends StandardJDialog
                 commands.add("-D");
                 commands.add(destinationTextField.getText());
             }
-            
+
             assert StringUtils.isNotEmpty(encodingTextField.getText()) : "No encoding value";
             commands.add("-E");
             commands.add(encodingTextField.getText());
 
             assert StringUtils.isNotEmpty(hostTextField.getText()) : "No host value";
             commands.add(hostTextField.getText());
-            
+
             assert StringUtils.isNotEmpty(prefixTextField.getText()) : "No prefix";
             commands.add('"' + prefixTextField.getText() + '"');
-            
+
             StringBuilder b = new StringBuilder();
             Iterator<String> iter = commands.iterator();
             while (iter.hasNext()) {
@@ -617,10 +628,10 @@ public class BuildProjectDialog extends StandardJDialog
                 }
             }
             logger.debug(b);
-            
+
             SystemProcessSwingWorker worker =
                 new SystemProcessSwingWorker(commands, projectHome, true);
-            
+
             progressDialog.setWorker(worker);
             progressDialog.writeOutput(b + "\n\n");
             WindowUtils.centreOverWindow(progressDialog, BuildProjectDialog.this);
