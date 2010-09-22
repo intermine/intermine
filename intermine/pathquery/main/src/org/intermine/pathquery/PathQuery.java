49939b444f3a545751c4d9b4ac98229ae9fd00f5
julie-sullivan <julie@flymine.org>
2010-09-22 14:44:35 +0000

    typo
    Former-commit-id: 39051aa1219d515ecb9176f0840a413916ea44b3

--- intermine/pathquery/main/src/org/intermine/pathquery/PathQuery.java
+++ intermine/pathquery/main/src/org/intermine/pathquery/PathQuery.java
@@ -1924,7 +1924,7 @@ public class PathQuery implements Cloneable
             }
         }
         return retval;
-    }intermine/web/main/src/org/intermine/web/struts/QueryBuilderChange.java
+    }
 
     /**
      * Returns the outer join constraint codes groups map for this query, if the query verifies
