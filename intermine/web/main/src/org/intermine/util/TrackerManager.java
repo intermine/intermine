92a13fc012f5723e312179f8b4abef7ba713d26e
danielabutano <daniela.butano@gmail.com>
2010-10-15 08:22:30 +0000

    Class to access to TemplateTracker
    Former-commit-id: a9d9371750fde7449fb03d92e5fc9926fcf59168

--- intermine/web/main/src/org/intermine/util/TrackerManager.java
+++ intermine/web/main/src/org/intermine/util/TrackerManager.java
@@ -5,26 +5,46 @@ import javax.servlet.http.HttpSession;
 import org.intermine.api.InterMineAPI;
 import org.intermine.api.profile.Profile;
 import org.intermine.tracker.TemplateTracker;
-import org.intermine.tracker.TemplateTrackerInput;
-import org.intermine.tracker.Tracker;
+import org.intermine.tracker.TemplateTrack;
 import org.intermine.web.logic.session.SessionMethods;
 
-public class TrackerManager {
+public class TrackerManager
+{
+    private static TrackerManager tm = null;
+    private TemplateTracker tracker = null;
 
-    public static void trackTemplate(String templateName, InterMineAPI im, HttpSession session) {
-        TemplateTrackerInput tti = new TemplateTrackerInput();
-        tti.setTemplateName(templateName);
-        tti.setSessionIdentifier(session.getId());
-        tti.setTimestamp(System.currentTimeMillis());
+    private TrackerManager(InterMineAPI im) {
+        if (!im.getTrackers().isEmpty()) {
+            tracker = (TemplateTracker) im.getTrackers().get(TemplateTracker.getTrackerName());
+        }
+    }
+
+    public static TrackerManager getInstance(InterMineAPI im) {
+        if (tm == null) {
+            tm = new TrackerManager(im);
+        }
+        return tm;
+    }
+
+    public void trackTemplate(String templateName, HttpSession session) {
+        TemplateTrack tt = new TemplateTrack(templateName, session.getId(),
+                                              System.currentTimeMillis());
         Profile profile = SessionMethods.getProfile(session);
         if (profile != null && profile.getUsername() != null) {
-            tti.setUserName(profile.getUsername());
+            tt.setUserName(profile.getUsername());
         }
-        if (!im.getTrackers().isEmpty()) {
-            Tracker templateTracker = im.getTrackers().get(TemplateTracker.getTrackerName());
-            if (templateTracker != null) {
-                templateTracker.storeTrack(tti);
-            }
+        tracker.storeTrack(tt);
+    }
+
+    public TemplateTrack getMostPopularTemplate() {
+        return tracker.getMostPopularTemplate();
+    }
+
+    public TemplateTrack getMostPopularTemplateByUser(HttpSession session) {
+        Profile profile = SessionMethods.getProfile(session);
+        if (profile != null) {
+            return tracker.getMostPopularTemplate(profile.getUsername(), session.getId());
         }
+        return null;
     }
 }
