62d3fba761a47dafa61109d279347bcc573f83a9
danielabutano <daniela.butano@gmail.com>
2010-10-15 09:12:26 +0000

    Commented the code linked to trackers
    Former-commit-id: a69c5a4af870230699bc944917a391ad96bc08d5

--- intermine/web/main/src/org/intermine/web/struts/TemplatesController.java
+++ intermine/web/main/src/org/intermine/web/struts/TemplatesController.java
@@ -19,7 +19,9 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.tiles.ComponentContext;
 import org.apache.struts.tiles.actions.TilesAction;
+import org.intermine.api.InterMineAPI;
 import org.intermine.api.profile.Profile;
+import org.intermine.util.TrackerManager;
 import org.intermine.web.logic.session.SessionMethods;
 
 /**
@@ -41,6 +43,10 @@ public class TemplatesController extends TilesAction
                                  @SuppressWarnings("unused") HttpServletResponse response)
         throws Exception {
         HttpSession session = request.getSession();
+        /*final InterMineAPI im = SessionMethods.getInterMineAPI(session);
+        String mostPopularTemplate =
+            TrackerManager.getInstance(im).getMostPopularTemplate().getTemplateName();
+        request.setAttribute("mostPopularTemplate", mostPopularTemplate);*/
         Profile profile = SessionMethods.getProfile(session);
         MyMineController.getPrecomputedSummarisedInfo(profile, session, request);
         return null;
