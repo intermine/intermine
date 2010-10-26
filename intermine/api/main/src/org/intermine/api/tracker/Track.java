ec9c1a025dcacb3459ba4536990a5154c247a2b5
danielabutano <daniela.butano@gmail.com>
2010-10-26 11:00:36 +0000

    Renamed. Java doc
    Former-commit-id: c53dc9f6203be58b9731a6d53847d037186ac838

---
+++ intermine/api/main/src/org/intermine/api/tracker/Track.java
@@ -0,0 +1,24 @@
+package org.intermine.api.tracker;
+
+/*
+ * Copyright (C) 2002-2010 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+/**
+ * Class to represent the track
+ * @author dbutano
+ */
+public interface Track
+{
+    /**
+     * Validate the track before saving into the database
+     * @return true if the track is valid
+     */
+    boolean validate();
+}
