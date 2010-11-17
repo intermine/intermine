9afeb99d0e919778037d1d8a638c3c4b8e592a09
boboppie <fengyuan.huu@gmail.com>
2010-11-17 10:54:45 +0000

    WebServiceCodeGen - test class update

--- intermine/api/test/src/org/intermine/api/query/codegen/WebserviceJavaCodeGeneratorTest.java
+++ intermine/api/test/src/org/intermine/api/query/codegen/WebserviceJavaCodeGeneratorTest.java
@@ -21,7 +21,7 @@ import org.intermine.pathquery.PathQueryBinding;
 import junit.framework.TestCase;
 
 /**
- * Tests for the WebserviceJavaCodeGenerator class
+ * Tests for the WebserviceJavaCodeGenerator class.
  *
  * @author Fengyuan Hu
  */
@@ -34,6 +34,9 @@ public class WebserviceJavaCodeGeneratorTest extends TestCase {
     private String TEST_STRING = WebserviceJavaCodeGenerator.TEST_STRING;
     private String INVALID_QUERY = WebserviceJavaCodeGenerator.INVALID_QUERY;
     private String NULL_QUERY = WebserviceJavaCodeGenerator.NULL_QUERY;
+
+    private String serviceRootURL = "http://www.flymine.org";
+    private String projectTitle = "FlyMine";
 
     private WebserviceJavaCodeGenerator cg;
 
@@ -58,7 +61,55 @@ public class WebserviceJavaCodeGeneratorTest extends TestCase {
     }
 
     /**
-     * This method tests when a path query has no constraints
+     * This method tests when a path query is null.
+     */
+    public void testPathQueryCodeGenerationWithNullQuery() {
+        PathQuery pathQuery = null;
+
+        WebserviceCodeGenInfo wsCodeGenInfo =
+            new WebserviceCodeGenInfo(pathQuery, serviceRootURL, projectTitle);
+
+        String expected = NULL_QUERY;
+        assertEquals(expected, cg.generate(wsCodeGenInfo));
+    }
+
+    /**
+     * This method tests when a path query has no views.
+     *
+     * Test PathQuery:
+     * <query name="" model="genomic" view="Gene.primaryIdentifier Gene.secondaryIdentifier
+     *   Gene.symbol Gene.name Gene.organism.shortName" sortOrder="Gene.primaryIdentifier asc">
+     * </query>
+     *
+     * Views will be removed from the PathQuery object.
+     *
+     */
+    public void testPathQueryCodeGenerationWithInvalidQuery() {
+        String queryXml = "<query name=\"\" model=\"genomic\" view=\"Gene.primaryIdentifier " +
+        "Gene.secondaryIdentifier Gene.symbol Gene.name Gene.organism.shortName\" " +
+        "sortOrder=\"Gene.primaryIdentifier asc\"></query>";
+
+        // Parse xml to PathQuery - PathQueryBinding
+        PathQuery pathQuery = PathQueryBinding.unmarshalPathQuery(
+                new StringReader(queryXml), PathQuery.USERPROFILE_VERSION);
+
+        WebserviceCodeGenInfo wsCodeGenInfo =
+            new WebserviceCodeGenInfo(pathQuery, serviceRootURL, projectTitle);
+
+        // Mock up
+        pathQuery.clearView();
+        String expected = INVALID_QUERY;
+        assertEquals(expected, cg.generate(wsCodeGenInfo));
+    }
+
+    /**
+     * This method tests when a path query has no constraints.
+     *
+     * Test PathQuery:
+     * <query name="" model="genomic" view="Gene.primaryIdentifier Gene.secondaryIdentifier
+     *   Gene.symbol Gene.name Gene.organism.shortName" sortOrder="Gene.primaryIdentifier asc">
+     * </query>
+     *
      */
     public void testPathQueryCodeGenerationWithNoConstraints() {
         String queryXml = "<query name=\"\" model=\"genomic\" view=\"Gene.primaryIdentifier " +
@@ -68,7 +119,10 @@ public class WebserviceJavaCodeGeneratorTest extends TestCase {
         PathQuery pathQuery = PathQueryBinding.unmarshalPathQuery(
                 new StringReader(queryXml), PathQuery.USERPROFILE_VERSION);
 
-        String expected = "package your.foo.bar.package.name;" + ENDL + ENDL +
+        WebserviceCodeGenInfo wsCodeGenInfo =
+            new WebserviceCodeGenInfo(pathQuery, serviceRootURL, projectTitle);
+
+        String expected = "package flymine;" + ENDL + ENDL +
         "import java.io.IOException;" + ENDL +
         "import java.util.List;" + ENDL + ENDL +
         "import org.intermine.metadata.Model;" + ENDL +
@@ -78,9 +132,10 @@ public class WebserviceJavaCodeGeneratorTest extends TestCase {
         "import org.intermine.pathquery.PathQuery;" + ENDL +
         "import org.intermine.pathquery.OrderDirection;" + ENDL + ENDL +
         "/**" + ENDL +
-        SPACE + "* Add some description to the class..." + ENDL +
+        SPACE + "* This is an automatically generated Java program to run the FlyMine query." + ENDL +
         SPACE + "*" + ENDL +
-        SPACE + "* @auther auther name" + ENDL +
+        SPACE + "* @auther FlyMine" + ENDL +
+        SPACE + "*" + ENDL +
         SPACE + "**/" + ENDL +
         "public class PathQueryClient" + ENDL +
         "{" + ENDL +
@@ -119,49 +174,37 @@ public class WebserviceJavaCodeGeneratorTest extends TestCase {
         INDENT + "}" + ENDL +
         "}" + ENDL;
 
-        assertEquals(expected, cg.generate(pathQuery));
+        assertEquals(expected, cg.generate(wsCodeGenInfo));
     }
 
     /**
-     * This method tests when a path query is null.
+     *
      */
-    public void testPathQueryCodeGenerationWithNullQuery() {
-        PathQuery pathQuery = null;
-        String expected = NULL_QUERY;
-        assertEquals(expected, cg.generate(pathQuery));
-    }
-
-    /**
-     * This method tests when a path query has no views.
-     */
-    public void testPathQueryCodeGenerationWithInvalidQuery() {
-        String queryXml = "<query name=\"\" model=\"genomic\" view=\"Gene.primaryIdentifier " +
-        "Gene.secondaryIdentifier Gene.symbol Gene.name Gene.organism.shortName\" " +
-        "sortOrder=\"Gene.primaryIdentifier asc\"></query>";
-
-        // Parse xml to PathQuery - PathQueryBinding
-        PathQuery pathQuery = PathQueryBinding.unmarshalPathQuery(
-                new StringReader(queryXml), PathQuery.USERPROFILE_VERSION);
-        // Mock up
-        pathQuery.clearView();
-        String expected = INVALID_QUERY;
-        assertEquals(expected, cg.generate(pathQuery));
-    }
-
     public void testTemplateQueryCodeGeneration() {
         String queryXml = "";
         // Parse xml to TemplateQuery - TemplateQueryBinding
         TemplateQueryBinding tqb = new TemplateQueryBinding();
-        TemplateQuery tq = null;
+        TemplateQuery templateQuery = null;
+
+        WebserviceCodeGenInfo wsCodeGenInfo =
+            new WebserviceCodeGenInfo(templateQuery, serviceRootURL, projectTitle);
 
         String expected = "";
-        assertEquals(expected, cg.generate(tq));
+        assertEquals(expected, cg.generate(wsCodeGenInfo));
     }
 
+    /**
+     *
+     */
     public void testTemplateQueryCodeGenerationWithNullQuery() {
-        TemplateQuery tq = null;
+        TemplateQuery templateQuery = null;
+
+        WebserviceCodeGenInfo wsCodeGenInfo =
+            new WebserviceCodeGenInfo(templateQuery, serviceRootURL, projectTitle);
+
         String expected = NULL_QUERY;
-        assertEquals(expected, cg.generate(tq));
+
+        assertEquals(expected, cg.generate(wsCodeGenInfo));
     }
 
     // Other private methods can be added...
