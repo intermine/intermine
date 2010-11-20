1ccbe1c6a46b254ed0884bec9a35e9fdb377ebd3
alexkalderimis <alex.kalderimis@gmail.com>
2010-11-20 18:15:56 +0000

    Now only operates on matching files

--- intermine/scripts/replace_bad_equals.sh
+++ intermine/scripts/replace_bad_equals.sh
@@ -1,1 +1,1 @@
-find . -name '*.java' -exec perl -np -i.orig -e 's/((?:\w|\.|_|\w\(.*?\))+)\.equals\((".*?")/$2.equals($1/' {} \;
+perl -nlp -i.orig -e 's/((?:\w|\.|_|\w\(.*?\))+)\.equals\((".*?")/$2.equals($1/' $(find . -type f -name '*.java' -exec grep -l '.equals("' {} +)
