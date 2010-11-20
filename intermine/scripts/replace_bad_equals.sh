67b92ad663316b600aa527fa6bba7f1aa8e9a0b5
alexkalderimis <alex.kalderimis@gmail.com>
2010-11-20 22:43:01 +0000

    Now with some comments, a usage string, and better handling of edge cases (such as when no problems are found)

--- intermine/scripts/replace_bad_equals.sh
+++ intermine/scripts/replace_bad_equals.sh
@@ -1,1 +1,16 @@
-perl -nlp -i.orig -e 's/((?:\w|\.|_|\w\(.*?\))+)\.equals\((".*?")/$2.equals($1/' $(find . -type f -name '*.java' -exec grep -l '.equals("' {} +)
+#!/bin/bash
+
+# This script searches recursively for java source files
+# and replaces unsafe string equals tests of the type
+# foo.equals("bar") with the null-pointer safe 
+# "bar".equals(foo). Back-ups of the original files are made
+# with the ".orig" suffix.
+
+if [ $1 ]; then
+    echo "This script searches recursively for .java files and replaces unsafe string equals patterns in those files, making back-ups with the suffix '.orig'"
+    exit
+fi
+files=$(find . -type f -name '*.java' -exec grep -l '.equals("' {} +)
+if [ $files ]; then
+    perl -nlp -i.orig -e 's/((?:\w|\.|_|\w\(.*?\))+)\.equals\((".*?")/$2.equals($1/' $files
+fi
