6947d72a2b6c9b84e1b81c9d2b3fabcd4e8bd470
rnsmith <rns>
2010-11-24 15:05:42 +0000

    Clean up build files, add comple-run.sh script.

---
+++ intermine/webservice/client/main/compile-run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Compiles sample source and runs sample.
+
+if [ $# = 0 ]; then 
+    echo "Usage: compile-run sample_name"
+    exit;
+fi;
+
+FILE=$1.java
+if !(test -e ${FILE}); then 
+    echo "File ${FILE} doesn't exist.";
+    echo "Usage: compile-run sample_name"
+    exit; 
+fi;
+
+CLASSPATH=./lib/intermine-objectstore.jar:./lib/intermine-pathquery.jar:./lib/commons-codec-1.3.jar:./lib/commons-httpclient-3.0.jar:./lib/commons-logging-1.1.1.jar:./lib/intermine-client.jar:./lib/log4j.jar:./lib/stax-api-1.0.jar:./lib/stax-ri-1.0.jar
+
+cd $1;
+mkdir -p build;
+javac -d ./build -classpath ${CLASSPATH} ./$1.java && \
+java -classpath ./build:${CLASSPATH} $@;
