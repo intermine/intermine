c8ef74148e1d901cfb8385c3f8b582890b9ec8f2
danielabutano <daniela.butano@gmail.com>
2010-12-10 14:28:12 +0000

    Added the method to manage data of bagvalues table. Added isCurrent property. Modify the method that retrieve the primaryIdentifierField (in upgradign phase the primary identifiers fielda are loaded from the bagvalues table, the osbag_int table is empty!)
    Former-commit-id: 0f8535887ea623b116abf15e4d5efcc2c4ce95a5

--- intermine/api/main/src/org/intermine/api/profile/InterMineBag.java
+++ intermine/api/main/src/org/intermine/api/profile/InterMineBag.java
@@ -10,11 +10,16 @@ package org.intermine.api.profile;
  *
  */
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
@@ -23,25 +28,25 @@ import org.intermine.api.bag.IncompatibleTypesException;
 import org.intermine.api.bag.UnknownBagTypeException;
 import org.intermine.api.search.WebSearchable;
 import org.intermine.metadata.ClassDescriptor;
+import org.intermine.metadata.FieldDescriptor;
 import org.intermine.metadata.Model;
 import org.intermine.model.userprofile.SavedBag;
 import org.intermine.model.userprofile.UserProfile;
 import org.intermine.objectstore.ObjectStore;
 import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.objectstore.ObjectStoreWriter;
+import org.intermine.objectstore.intermine.ObjectStoreInterMineImpl;
+import org.intermine.objectstore.intermine.ObjectStoreWriterInterMineImpl;
 import org.intermine.objectstore.proxy.ProxyReference;
 import org.intermine.objectstore.query.BagConstraint;
 import org.intermine.objectstore.query.ConstraintOp;
-import org.intermine.objectstore.query.ContainsConstraint;
 import org.intermine.objectstore.query.ObjectStoreBag;
 import org.intermine.objectstore.query.Query;
 import org.intermine.objectstore.query.QueryClass;
-import org.intermine.objectstore.query.QueryClassBag;
 import org.intermine.objectstore.query.QueryField;
-import org.intermine.objectstore.query.QueryValue;
-import org.intermine.objectstore.query.Results;
-import org.intermine.objectstore.query.SimpleConstraint;
 import org.intermine.objectstore.query.SingletonResults;
+import org.intermine.sql.writebatch.Batch;
+import org.intermine.sql.writebatch.BatchWriterPostgresCopyImpl;
 import org.intermine.util.TypeUtil;
 
 
@@ -62,6 +67,8 @@ public class InterMineBag implements WebSearchable, Cloneable
     protected final String type;
     private String description;
     private Date dateCreated;
+    private String primaryIdentifierField;
+    private boolean isCurrent;
     private ObjectStoreBag osb;
     private ObjectStore os;
     private ObjectStoreWriter uosw;
@@ -82,8 +89,34 @@ public class InterMineBag implements WebSearchable, Cloneable
      */
     public InterMineBag(String name, String type, String description, Date dateCreated,
             ObjectStore os, Integer profileId, ObjectStoreWriter uosw) throws ObjectStoreException {
-        checkAndSetName(name);
         this.type = type;
+        init(name, description, dateCreated, os, profileId, uosw);
+    }
+
+    /**
+     * Constructs a new InterMineIdBag, and saves it in the UserProfile database.
+     *
+     * @param name the name of the bag
+     * @param type the class of objects stored in the bag
+     * @param description the description of the bag
+     * @param dateCreated the Date when this bag was created
+     * @param os the production ObjectStore
+     * @param profileId the ID of the user in the userprofile database
+     * @param uosw the ObjectStoreWriter of the userprofile database
+     * @param primaryIdentifierField the primary identifier defined for this bag
+     * @throws ObjectStoreException if an error occurs
+     */
+    public InterMineBag(String name, String type, String description, Date dateCreated,
+        ObjectStore os, Integer profileId, ObjectStoreWriter uosw, String primaryIdentifierField)
+        throws ObjectStoreException {
+        this.type = type;
+        init(name, description, dateCreated, os, profileId, uosw);
+        this.primaryIdentifierField = primaryIdentifierField;
+    }
+
+    private void init(String name, String description, Date dateCreated, ObjectStore os,
+        Integer profileId, ObjectStoreWriter uosw) throws ObjectStoreException {
+        checkAndSetName(name);
         this.description = description;
         this.dateCreated = dateCreated;
         this.os = os;
@@ -139,6 +172,7 @@ public class InterMineBag implements WebSearchable, Cloneable
             savedBag.setDateCreated(dateCreated);
             savedBag.proxyUserProfile(new ProxyReference(null, profileId, UserProfile.class));
             savedBag.setOsbId(osb.getBagId());
+            savedBag.setCurrent(true);
             uosw.store(savedBag);
         }
         return savedBag;
@@ -154,6 +188,7 @@ public class InterMineBag implements WebSearchable, Cloneable
             SavedBag savedBag = (SavedBag) uosw.getObjectStore().getObjectById(savedBagId,
                     SavedBag.class);
             uosw.delete(savedBag);
+            deleteBagValues();
             this.profileId = null;
             this.savedBagId = null;
         }
@@ -171,26 +206,78 @@ public class InterMineBag implements WebSearchable, Cloneable
         SingletonResults res = os.executeSingleton(q, 1000, false, true, true);
         return (List<Integer>) ((List) res);
     }
-    
+
     /**
-     * Returns a List which contains the primaryIdentifiers of the content of this bag.
-     *
-     * @return a List of Integers
+     * Returns a List of primaryIdentifiers values contained by this bag.
+     * @return the list of primary identifier values
      */
-    public List<String> getContentsASPrimaryIdentifier(String primaryIdentifierField) {
+    public List<String> getContentsASPrimaryIdentifierValues() {
+        if (isCurrent) {
+            Query q = new Query();
+            q.setDistinct(false);
+            try {
+                QueryClass qc = new QueryClass(Class.forName(getQualifiedType()));
+                q.addFrom(qc);
+                q.addToSelect(new QueryField(qc, primaryIdentifierField));
+                QueryField idField = new QueryField(qc, "id");
+                BagConstraint c = new BagConstraint(idField, ConstraintOp.IN, osb);
+                q.setConstraint(c);
+                SingletonResults res = os.executeSingleton(q, 1000, false, true, true);
+                return (List<String>) ((List) res);
+            } catch (ClassNotFoundException cne) {
+                return new ArrayList<String>();
+            }
+        } else {
+            //we are upgrading bags, the osbag_int is empty, we need to use bagvalues table
+            Connection conn = null;
+            Statement stm = null;
+            ResultSet rs = null;
+            List<String> primaryIdentifiersList = new ArrayList<String>();
+            ObjectStoreInterMineImpl uos = null;
+            try {
+                uos = (ObjectStoreInterMineImpl) uosw.getObjectStore();
+                conn = uos.getConnection();
+                stm = conn.createStatement();
+                String sql = "SELECT value FROM bagvalues WHERE savedbagid = " + savedBagId;
+                rs = stm.executeQuery(sql);
+                while (rs.next()) {
+                    primaryIdentifiersList.add(rs.getString(1));
+                }
+            } catch (SQLException sqe) {
+                LOG.error("Connection problems during loadings primary identifiers fields for" +
+                          "the bag " + name, sqe);
+            } finally {
+                if (rs != null) {
+                    try {
+                        rs.close();
+                        if (stm != null) {
+                            stm.close();
+                        }
+                    } catch (SQLException sqle) {
+                        LOG.error("Problems closing resources in the method "
+                            + "getContentsASPrimaryIdentifierValues for the bag " + name, sqle);
+                    }
+                }
+                uos.releaseConnection(conn);
+            }
+            return primaryIdentifiersList;
+        }
+    }
+
+    public List<String> getPrimaryIdentifierValues(Collection<Integer> ids) {
         Query q = new Query();
         q.setDistinct(false);
         try {
-	        QueryClass qc = new QueryClass(Class.forName(getQualifiedType()));
-	        q.addFrom(qc);
-	        q.addToSelect(new QueryField(qc, primaryIdentifierField));
-	        QueryField idField = new QueryField(qc, "id");
-	        BagConstraint c = new BagConstraint(idField, ConstraintOp.IN, osb);
-	        q.setConstraint(c);
-	        SingletonResults res = os.executeSingleton(q, 1000, false, true, true);
-	        return (List<String>) ((List) res);
+            QueryClass qc = new QueryClass(Class.forName(getQualifiedType()));
+            q.addFrom(qc);
+            q.addToSelect(new QueryField(qc, primaryIdentifierField));
+            QueryField idField = new QueryField(qc, "id");
+            BagConstraint idsConstraints = new BagConstraint(idField, ConstraintOp.IN, ids);
+            q.setConstraint(idsConstraints);
+            SingletonResults res = os.executeSingleton(q, 1000, false, true, true);
+            return (List<String>) ((List) res);
         } catch (ClassNotFoundException cne) {
-        	return new ArrayList<String>();
+            return new ArrayList<String>();
         }
     }
 
@@ -237,6 +324,8 @@ public class InterMineBag implements WebSearchable, Cloneable
         this.profileId = profileId;
         SavedBag savedBag = store();
         this.savedBagId = savedBag.getId();
+        isCurrent = savedBag.getCurrent();
+        addBagValues();
     }
 
     /**
@@ -323,6 +412,36 @@ public class InterMineBag implements WebSearchable, Cloneable
     }
 
     /**
+     * Return the savedbagId of this bag
+     * @return savedbagId
+     */
+    public Integer getSavedBagId() {
+        return savedBagId;
+    }
+
+
+    /**
+     * Set the primaryIdentifierField
+     * @param primaryIdentifierField
+     */
+    public void setPrimaryIdentifierField(Map<String, List<FieldDescriptor>> classKeys) {
+        FieldDescriptor fieldDescriptor = classKeys.get(type).get(0);
+        this.primaryIdentifierField = fieldDescriptor.getName();
+    }
+
+    public String getPrimaryIdentifierField() {
+        return primaryIdentifierField;
+    }
+
+    public boolean isCurrent() {
+        return isCurrent;
+    }
+
+    public void setCurrent(boolean isCurrent) {
+        this.isCurrent=isCurrent;
+    }
+
+    /**
      * Create copy of bag. Bag is saved to objectstore.
      * @return create bag
      */
@@ -365,6 +484,7 @@ public class InterMineBag implements WebSearchable, Cloneable
             copy.savedBagId = null;
             SavedBag savedBag = copy.store();
             copy.savedBagId = savedBag.getId();
+            copy.primaryIdentifierField = primaryIdentifierField;
         } catch (ObjectStoreException ex) {
             throw new RuntimeException("Clone failed.", ex);
         } catch (CloneNotSupportedException ex) {
@@ -415,6 +535,9 @@ public class InterMineBag implements WebSearchable, Cloneable
                 oswProduction.close();
             }
         }
+        if (profileId != null) {
+            addBagValues(ids);
+        }
     }
 
     /**
@@ -459,6 +582,9 @@ public class InterMineBag implements WebSearchable, Cloneable
                 oswProduction.close();
             }
         }
+        if (profileId != null) {
+            addBagValues();
+        }
     }
 
     /**
@@ -484,6 +610,112 @@ public class InterMineBag implements WebSearchable, Cloneable
             if (oswProduction != null) {
                 oswProduction.close();
             }
+        }
+        if (profileId != null) {
+            deleteBagValues(ids);
+        }
+    }
+
+    /**
+     * Save the primary identifiers associated to the bag into bagvalues table
+     */
+    public void addBagValues() {
+        addBagValues(null);
+    }
+
+    /**
+     * Save the primary identifiers associated to the bag into bagvalues table
+     */
+    private void addBagValues(Collection<Integer> ids) {
+        Connection conn = null;
+        if (profileId != null) {
+            List<String> values = null;
+            if (ids == null) {
+                values = getContentsASPrimaryIdentifierValues();
+            } else {
+                values = getPrimaryIdentifierValues(ids);
+            }
+            try {
+                conn = ((ObjectStoreWriterInterMineImpl) uosw).getConnection();
+                if (conn.getAutoCommit()) {
+                    conn.setAutoCommit(false);
+                    Batch batch = new Batch(new BatchWriterPostgresCopyImpl());
+                    String[] colNames = new String[] {"savedbagid", "value"};
+                    for (String value : values) {
+                        batch.addRow(conn, "bagvalues", null, colNames,
+                                    new Object[] {savedBagId, value});
+                    }
+                    batch.flush(conn);
+                    conn.commit();
+                    conn.setAutoCommit(true);
+                }
+            } catch (SQLException sqle) {
+                try {
+                    conn.rollback();
+                    conn.setAutoCommit(true);
+                } catch (SQLException sqlex) {
+                    throw new RuntimeException("Error aborting transaction", sqlex);
+                }
+            } finally {
+                ((ObjectStoreWriterInterMineImpl) uosw).releaseConnection(conn);
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    private void deleteBagValues() {
+        Connection conn = null;
+        Statement stm = null;
+        try {
+            conn = ((ObjectStoreWriterInterMineImpl) uosw).getConnection();
+            stm = conn.createStatement();
+            String sql = "DELETE FROM bagvalues WHERE savedBagId='" + savedBagId + "'";
+            stm.executeUpdate(sql);
+        } catch (SQLException sqle) {
+            throw new RuntimeException("Error deleting bagvalues of bag : " + savedBagId, sqle);
+        } finally {
+            if (stm != null) {
+                try {
+                    stm.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException("Problem closing  resources in deleteBagValues()",
+                                               e);
+                }
+            }
+            ((ObjectStoreWriterInterMineImpl) uosw).releaseConnection(conn);
+        }
+    }
+
+    private void deleteBagValues(Collection<Integer> ids) {
+        Connection conn = null;
+        Statement stm = null;
+        List<String> values = getPrimaryIdentifierValues(ids);
+        try {
+            conn = ((ObjectStoreWriterInterMineImpl) uosw).getConnection();
+            stm = conn.createStatement();
+            String sql;
+            String valuesList = "";
+            for (String value : values) {
+                valuesList = valuesList + "'" + value + "',";
+            }
+            valuesList = valuesList.substring(0, valuesList.length() - 1);
+            sql = "DELETE FROM bagvalues WHERE savedBagId='" + savedBagId + "' "
+                     + " AND value IN (" + valuesList + ")";
+            stm.executeUpdate(sql);
+        } catch (SQLException sqle) {
+            throw new RuntimeException("Error deleting bagvalues of bag : " + savedBagId, sqle);
+        } finally {
+            if (stm != null) {
+                try {
+                    stm.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException("Problem closing  resources in deleteBagValues()",
+                                               e);
+                }
+            }
+            ((ObjectStoreWriterInterMineImpl) uosw).releaseConnection(conn);
         }
     }
 }
