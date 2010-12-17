ce4039769af4c6140576dc33805d7946d6088d0c
danielabutano <daniela.butano@gmail.com>
2010-12-17 11:11:51 +0000

    Added the bagUpgrade action called when the user selects 'Upgrade' from the MyMine->List page, for a bag not current
    Former-commit-id: 71382b2a387a99b50424f7b8b405d2729243d5cd

---
+++ intermine/web/main/src/org/intermine/web/struts/BagUpgradeAction.java
@@ -0,0 +1,72 @@
+package org.intermine.web.struts;
+
+/*
+ * Copyright (C) 2002-2010 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.intermine.api.InterMineAPI;
+import org.intermine.api.bag.BagQueryResult;
+import org.intermine.api.bag.BagQueryRunner;
+import org.intermine.api.profile.InterMineBag;
+import org.intermine.api.profile.Profile;
+import org.intermine.web.logic.session.SessionMethods;
+
+
+/**
+ * An action that prepare a BagQueryResult and save it into the session.
+ *
+ * @author dbutano
+ */
+
+public class BagUpgradeAction extends InterMineAction
+{
+    /**
+     * Action for creating BagQueryResult for a specific bag not yet current
+     *
+     * @param mapping The ActionMapping used to select this instance
+     * @param form The optional ActionForm bean for this request (if any)
+     * @param request The HTTP request we are processing
+     * @param response The HTTP response we are creating
+     * @return an ActionForward object defining where control goes next
+     * @exception Exception if the application business logic throws
+     *  an exception
+     */
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        String bagName = (String) request.getParameter("bagName");
+        String bagType = (String) request.getParameter("bagType");
+        HttpSession session = request.getSession();
+        BagQueryResult bagQueryResult;
+        bagQueryResult = (BagQueryResult) session.getAttribute("bagQueryResult_" + bagName);
+        
+        if (bagQueryResult == null) {
+            final InterMineAPI im = SessionMethods.getInterMineAPI(session);
+            Profile profile = SessionMethods.getProfile(session);
+            InterMineBag savedBag = profile.getSavedBags().get(bagName);
+            List<String> primaryIdentifiersList =
+            	savedBag.getContentsASPrimaryIdentifierValues();
+            BagQueryRunner bagRunner = im.getBagQueryRunner();
+            bagQueryResult = bagRunner.searchForBag(bagType, primaryIdentifiersList, "", false);
+            session.setAttribute("bagQueryResult_" + bagName, bagQueryResult);
+        }
+        request.setAttribute("newBagName", bagName);
+        request.setAttribute("bagType", bagType);
+        return mapping.findForward("bagUploadConfirm");
+    }
+}
