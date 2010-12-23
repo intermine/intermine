128ca444d77441e422a6105328be6a5308ff6d17
alexkalderimis <alex.kalderimis@gmail.com>
2010-12-23 19:54:16 +0000

    Refactored comparison code to another module
    Former-commit-id: 12f9f4cbd950b88c209b03a7083366beafd8364b

--- intermine/web/test/src/org/intermine/webservice/server/output/JSONResultsIteratorTest.java
+++ intermine/web/test/src/org/intermine/webservice/server/output/JSONResultsIteratorTest.java
@@ -52,130 +52,6 @@ import junit.framework.TestCase;
 
 public class JSONResultsIteratorTest extends TestCase {
 	
-	/**
-	 * Compare two JSONObjects for equality
-	 * @param left The reference JSONObject (this is referred to as "expected" in any messages)
-	 * @param right The candidate object to check.
-	 * @return a String with messages explaining the problems if there are any, otherwise null (equal)
-	 * @throws JSONException
-	 */
-	private static String getProblemsComparing(JSONObject left, Object right) throws JSONException {
-		List<String> problems = new ArrayList<String>();
-		
-		if (left == null ) {
-			if (right != null) {
-				return "Expected null, but got " + right;
-			} else {
-				return null;
-			}
-		}
-		
-		if (! (right instanceof JSONObject)) {
-			if (right == null) {
-				return "Didn't expect null, but got null";
-			} else {
-				return "Expected a JSONObject, is got a " + right.getClass();
-			}
-		}
-		JSONObject rjo = (JSONObject) right;
-		Set<String> leftNames = new HashSet<String>(Arrays.asList(JSONObject.getNames(left)));
-		Set<String> rightNames = new HashSet<String>(Arrays.asList(JSONObject.getNames(rjo)));
-		if (! leftNames.equals(rightNames)) {
-			problems.add("Expected the keys " + 
-					leftNames + ", but got these: " + rightNames);
-		}
-		
-		for (String name : leftNames) {
-			Object leftValue = left.get(name);
-			String problem = null;
-			try {
-				if (leftValue == null) {
-					if ( rjo.get(name) != null ) {
-						problem = "Expected null, but got " + rjo.get(name);
-					}
-				} else if (leftValue instanceof JSONObject) {
-					problem = getProblemsComparing((JSONObject) leftValue, rjo.get(name));
-				} else if (leftValue instanceof JSONArray) {
-					problem = getProblemsComparing((JSONArray) leftValue, rjo.get(name));
-				} else {
-					if (! leftValue.toString().equals(rjo.get(name).toString())) {
-						problem = "Expected " + leftValue + " but got " + rjo.get(name); 
-					}
-				}
-			} catch (Throwable e) {
-				problem = e.toString();
-			}
-			if (problem != null) {
-				problems.add("Problem with " + name + ": " + problem);
-			}
-		}
-		
-		if (problems.isEmpty()) {
-			return null;
-		} 
-		return problems.toString();
-	}
-	
-	/**
-	 * Compare two JSONArrays for equality
-	 * @param left The reference array (referred to as "expected" in any messages)
-	 * @param right The candidate object to check.
-	 * @return a String with messages explaining the problems if there are any, otherwise null (equal)
-	 * @throws JSONException
-	 */
-	private static String getProblemsComparing(JSONArray left, Object right) throws JSONException {
-		List<String> problems = new ArrayList<String>();
-		
-		if (left == null ) {
-			if (right != null) {
-				return "Expected null, but got " + right;
-			} else {
-				return null;
-			}
-		}
-		
-		if (! (right instanceof JSONArray)) {
-			return "Expected a JSONArray, but got a " + right.getClass();
-		}
-		
-		JSONArray rja = (JSONArray) right;
-		if (left.length() != rja.length()) {
-			problems.add("Expected the size of this array to be " + left.length() + 
-					" but got " + rja.length());
-		}
-		for (int index = 0; index < left.length(); index++) {
-			Object leftMember = left.get(index);
-			String problem = null;
-			try {
-				if (leftMember== null) {
-					if ( rja.get(index) != null ) {
-						problem = "Expected null, but got " + rja.get(index);
-					}
-				} else if (leftMember instanceof JSONObject) {
-					problem = getProblemsComparing((JSONObject) leftMember, rja.get(index));
-				} else if (leftMember instanceof JSONArray) {
-					problem = getProblemsComparing((JSONArray) leftMember, rja.get(index));
-				} else {
-					if (! leftMember.toString().equals(rja.get(index).toString())) {
-						problem = "Expected " + leftMember + 
-							" but got " + rja.get(index);
-					}
-				}
-			} catch (Throwable e) {
-				problem = e.toString();
-			}
-			if (problem != null) {
-				problems.add("Problem with index " + index + ": " + problem);
-			}
-		}
-		
-		if (problems.isEmpty()) {
-			return null;
-		} 
-		return problems.toString();
-		
-	}
-
 	private ObjectStoreDummyImpl os;
 	private Company wernhamHogg;
 	private CEO jennifer;
@@ -344,7 +220,7 @@ public class JSONResultsIteratorTest extends TestCase {
         
         assertEquals(1, got.size());
         
-        assertEquals(null, getProblemsComparing(expected, got.get(0)));	
+        assertEquals(null, JSONObjTester.getProblemsComparing(expected, got.get(0)));	
         
     }
     
@@ -395,7 +271,7 @@ public class JSONResultsIteratorTest extends TestCase {
         assertEquals(5, got.size());
         for (int i = 0; i < jsonStrings.size(); i++) {
         	JSONObject jo = new JSONObject(jsonStrings.get(i));
-        	assertEquals(null, getProblemsComparing(jo, got.get(i)));	
+        	assertEquals(null, JSONObjTester.getProblemsComparing(jo, got.get(i)));	
         }
         
     }
@@ -497,7 +373,7 @@ public class JSONResultsIteratorTest extends TestCase {
         
         
         JSONObject expected = new JSONObject(jsonString);
-        assertEquals(null, getProblemsComparing(expected, got.get(0)));
+        assertEquals(null, JSONObjTester.getProblemsComparing(expected, got.get(0)));
         
     }
 	public void testSingleObjectWithNestedCollectionsAndMultipleAttributes() throws Exception {
@@ -603,7 +479,7 @@ public class JSONResultsIteratorTest extends TestCase {
         
         
         JSONObject expected = new JSONObject(jsonString);
-        assertEquals(null, getProblemsComparing(expected, got.get(0)));
+        assertEquals(null, JSONObjTester.getProblemsComparing(expected, got.get(0)));
         
     }
 	
@@ -661,7 +537,7 @@ public class JSONResultsIteratorTest extends TestCase {
         
         assertEquals(got.size(), 1);
         
-        assertEquals(null, getProblemsComparing(expected, got.get(0)));
+        assertEquals(null, JSONObjTester.getProblemsComparing(expected, got.get(0)));
         
 	}
 	
@@ -770,7 +646,7 @@ public class JSONResultsIteratorTest extends TestCase {
         assertEquals(got.size(), 1);
 
         JSONObject expected = new JSONObject(jsonString);
-        assertEquals(null, getProblemsComparing(expected, got.get(0)));
+        assertEquals(null, JSONObjTester.getProblemsComparing(expected, got.get(0)));
                     
 	}
 	
@@ -950,7 +826,7 @@ public class JSONResultsIteratorTest extends TestCase {
 	    
 	    assertEquals(got.size(), 1);
         
-        assertEquals(null, getProblemsComparing(expected, got.get(0)));
+        assertEquals(null, JSONObjTester.getProblemsComparing(expected, got.get(0)));
 
     }	
 	
@@ -1206,7 +1082,7 @@ public class JSONResultsIteratorTest extends TestCase {
         assertEquals(4, got.size());
         for (int i = 0; i < jsonStrings.size(); i++) {
         	JSONObject jo = new JSONObject(jsonStrings.get(i));
-        	assertEquals(null, getProblemsComparing(jo, got.get(i)));	
+        	assertEquals(null, JSONObjTester.getProblemsComparing(jo, got.get(i)));	
         }
 	}
 	 
@@ -1291,7 +1167,7 @@ public class JSONResultsIteratorTest extends TestCase {
         assertEquals(4, got.size());
         for (int i = 0; i < jsonStrings.size(); i++) {
         	JSONObject jo = new JSONObject(jsonStrings.get(i));
-        	assertEquals(null, getProblemsComparing(jo, got.get(i)));	
+        	assertEquals(null, JSONObjTester.getProblemsComparing(jo, got.get(i)));	
         }
 	}                 
 	
@@ -1451,7 +1327,7 @@ public class JSONResultsIteratorTest extends TestCase {
         assertEquals(4, got.size());
         for (int i = 0; i < jsonStrings.size(); i++) {
         	JSONObject jo = new JSONObject(jsonStrings.get(i));
-        	assertEquals(null, getProblemsComparing(jo, got.get(i)));	
+        	assertEquals(null, JSONObjTester.getProblemsComparing(jo, got.get(i)));	
         }
 	}
 	
@@ -1604,7 +1480,7 @@ public class JSONResultsIteratorTest extends TestCase {
         
         assertEquals(1, got.size());
         JSONObject expected = new JSONObject(jsonString);
-        assertEquals(null, getProblemsComparing(expected, got.get(0)));
+        assertEquals(null, JSONObjTester.getProblemsComparing(expected, got.get(0)));
         
 	}
 	
