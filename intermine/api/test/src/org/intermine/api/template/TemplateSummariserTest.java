60cd672b6087b709a6476f11c578e85efa5dbb77
danielabutano <daniela.butano@gmail.com>
2011-01-11 14:37:42 +0000

    Fix bug
    Former-commit-id: fdb2cab9970521dcc83fdf5bce1286afd5995f59

--- intermine/api/test/src/org/intermine/api/template/TemplateSummariserTest.java
+++ intermine/api/test/src/org/intermine/api/template/TemplateSummariserTest.java
@@ -11,6 +11,7 @@ package org.intermine.api.template;
  */
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -24,6 +25,9 @@ import org.intermine.objectstore.ObjectStore;
 import org.intermine.objectstore.ObjectStoreWriter;
 import org.intermine.objectstore.ObjectStoreWriterFactory;
 import org.intermine.objectstore.StoreDataTestCase;
+import org.intermine.pathquery.Constraints;
+import org.intermine.pathquery.PathConstraint;
+import org.intermine.pathquery.PathQuery;
 
 /**
  * Tests for the TemplateSummariser.
@@ -47,6 +51,19 @@ public class TemplateSummariserTest extends StoreDataTestCase
 
         uosw =  ObjectStoreWriterFactory.getObjectStoreWriter("osw.userprofile-test");
         pm = new ProfileManager(os, uosw);
+        profile = new Profile(pm, "testUser", null, "password", new HashMap(),
+                new HashMap(), new HashMap());
+        pm.createProfile(profile);
+
+        TemplateQuery twoConstraints = new TemplateQuery("twoConstraints", "twoConstraints", "", new PathQuery(model));
+        twoConstraints.addViews("Employee.name", "Employee.age");
+        PathConstraint ageCon = Constraints.greaterThan("Employee.age", "30");
+        twoConstraints.addConstraint(ageCon);
+        twoConstraints.setEditable(ageCon, true);
+        PathConstraint depCon = Constraints.greaterThan("Employee.department.name", "Finance");
+        twoConstraints.addConstraint(depCon);
+        twoConstraints.setEditable(depCon, true);
+        profile.saveTemplate("template", twoConstraints);
     }
 
     public void executeTest(String type) {
@@ -54,33 +71,25 @@ public class TemplateSummariserTest extends StoreDataTestCase
 
     public void testQueries() throws Throwable {
     }
-    
+
     public static void oneTimeSetUp() throws Exception {
         StoreDataTestCase.oneTimeSetUp();
     }
-    
+
     public static Test suite() {
         return buildSuite(TemplateSummariserTest.class);
     }
-    
+
     public void test1() throws Exception {
-        profile = pm.getSuperuserProfile();
-        if (profile == null) {
-            fail("Superuser " + pm.getSuperuser() + " in " + pm.getProfileUserNames() + " is null");
-        }
-        if (profile.getSavedTemplates() == null) {
-            fail("Templates map is null");
-        }
-        TemplateQuery t = profile.getSavedTemplates().get("employeesOfACertainAge");
+        TemplateQuery t = profile.getSavedTemplates().get("template");
         TemplateSummariser summariser = new TemplateSummariser(os, uosw);
         assertFalse(summariser.isSummarised(t));
         summariser.summarise(t);
         assertTrue(summariser.isSummarised(t));
         Map<String, List<Object>> possibleValues = summariser.getPossibleValues(t);
-        assertEquals(1, possibleValues.size());
+        assertEquals(2, possibleValues.size());
         assertEquals("Employee.age", possibleValues.keySet().iterator().next());
         Set<Object> expected = new HashSet<Object>(Arrays.asList(10, 20, 30, 40, 50, 60));
         assertEquals(expected, new HashSet<Object>(possibleValues.values().iterator().next()));
-        //fail("" + possibleValues);
     }
 }
