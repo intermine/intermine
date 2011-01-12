54c1ab8169675149ce6cad2d0c5d9d81a96bce7c
julie-sullivan <julie@flymine.org>
2011-01-12 12:02:23 +0000

    update algorithm for enrichment widgets.
    Former-commit-id: 245822e2fcb3e6bd1b06e91dd77a82f353b560e3

--- intermine/web/test/src/org/intermine/web/logic/WidgetUtilTest.java
+++ intermine/web/test/src/org/intermine/web/logic/WidgetUtilTest.java
@@ -14,12 +14,13 @@ import org.intermine.web.logic.widget.WidgetUtil;
 
 public class WidgetUtilTest extends TestCase
 {
+    private final int REFERENCE_SIZE = 5;
     private Double maxValue = new Double(1.0);
     private HashMap<String, BigDecimal> resultsMap = new HashMap();
-    private String[] id = new String[4];
-    private int[] taggedSample = new int[4];
-    private int[] taggedPopulation = new int[4];
-    private BigDecimal[] expectedResults = new BigDecimal[4];
+    private String[] id = new String[REFERENCE_SIZE];
+    private int[] taggedSample = new int[REFERENCE_SIZE];
+    private int[] taggedPopulation = new int[REFERENCE_SIZE];
+    private BigDecimal[] expectedResults = new BigDecimal[REFERENCE_SIZE];
     private int bagsize = 3;
     private int total = 100;
     private HashMap<String, BigDecimal> bonferroniMap = new HashMap();
@@ -34,63 +35,48 @@ public class WidgetUtilTest extends TestCase
         // these numbers are generated via this website:
         // http://www.quantitativeskills.com/sisa/distributions/hypghlp.htm
 
-        id[1] = "notEnriched";
-        taggedSample[1] = 1;
-        taggedPopulation[1] = 10;
-        expectedResults[1] = new BigDecimal(0.27346938775509510577421679045073688030242919921875);
+        id[1] = "overrepresented";
+        taggedSample[1] = 3;
+        taggedPopulation[1] = 20;
+        expectedResults[1] = new BigDecimal(0.00705009276437833);
 
-        id[2] = "underrepresented";
+        id[2] = "notEnriched";
         taggedSample[2] = 1;
-        taggedPopulation[2] = 50;
-        expectedResults[2] = new BigDecimal(0.8787878787878582);
+        taggedPopulation[2] = 10;
+        expectedResults[2] = new BigDecimal(0.27346938775509510577421679045073688030242919921875);
 
-        id[0] = "overrepresented";
-        taggedSample[0] = 3;
-        taggedPopulation[0] = 20;
-        expectedResults[0] = new BigDecimal(0.00705009276437833);
+        id[3] = "underrepresented";
+        taggedSample[3] = 1;
+        taggedPopulation[3] = 50;
+        expectedResults[3] = new BigDecimal(0.8787878787878582);
 
-        id[3] = "one";
-        taggedSample[3] = 3;
-        taggedPopulation[3] = 100;
-        expectedResults[3] = new BigDecimal(1);
+        id[4] = "one";
+        taggedSample[4] = 3;
+        taggedPopulation[4] = 100;
+        expectedResults[4] = new BigDecimal(1);
 
         BigDecimal numberOfTests = new BigDecimal(id.length);
-        BigDecimal alpha = new BigDecimal(0.05);
-        BigDecimal alphaPerTest = alpha.divide(numberOfTests, MathContext.DECIMAL32);
 
-        for (int i = 3; i >= 0; i--) {
-            BigDecimal p = new BigDecimal(Hypergeometric.calculateP(taggedSample[i], bagsize, taggedPopulation[i], total));
+        for (int i = 1; i <= 4; i++) {
+
+            BigDecimal p = new BigDecimal(Hypergeometric.calculateP(taggedSample[i], bagsize,
+                    taggedPopulation[i], total));
             resultsMap.put(id[i], p);
 
-            BigDecimal bonferroniP =  p.add(alphaPerTest);
-            if (bonferroniP.compareTo(ONE) >= 0) {
+            //p * n
+            BigDecimal bonferroniP =  p.multiply(numberOfTests);
+            if (bonferroniP.compareTo(ONE) > 0) {
                 bonferroniP = ONE;
             }
             bonferroniMap.put(id[i], bonferroniP);
 
-            //p-value * (n/ n - index)
-            if (i == 3) {
-                if (p.compareTo(ONE) >= 0) {
-                    p = ONE;
-                }
-
-                // biggest one isn't changed
-                if (p.compareTo(ONE) >= 0) {
-                    benjaminiMap.put(id[i], p);
-                }
-
-            } else {
-                BigDecimal divisor = numberOfTests.subtract(new BigDecimal(i + 1));
-                BigDecimal m = numberOfTests.divide(divisor, MathContext.DECIMAL128);
-                BigDecimal adjustedP = p.multiply(m, MathContext.DECIMAL128);
-                if (adjustedP.compareTo(ONE) >= 0) {
-                    adjustedP = ONE;
-                }
-                if (p.compareTo(ONE) >= 0) {
-                    benjaminiMap.put(id[i], adjustedP);
-                }
-
+            //p-value*(n/rank)
+            BigDecimal m = numberOfTests.divide(new BigDecimal(i), MathContext.DECIMAL128);
+            BigDecimal adjustedP = p.multiply(m, MathContext.DECIMAL128);
+            if (adjustedP.compareTo(ONE) > 0) {
+                adjustedP = ONE;
             }
+            benjaminiMap.put(id[i], adjustedP);
         }
     }
 
@@ -101,14 +87,14 @@ public class WidgetUtilTest extends TestCase
     }
 
     public void testBonferroni() throws Exception {
-        Map<String, BigDecimal> adjustedMap = WidgetUtil.calcErrorCorrection("Bonferroni", maxValue, resultsMap);
+        Map<String, BigDecimal> adjustedMap = WidgetUtil.calcErrorCorrection("Bonferroni", maxValue, resultsMap, REFERENCE_SIZE);
         for (String label : bonferroniMap.keySet()) {
             assertEquals(bonferroniMap.get(label), adjustedMap.get(label));
         }
     }
 
     public void testBenjaminiHochberg() throws Exception {
-       Map<String, BigDecimal> adjustedMap = WidgetUtil.calcErrorCorrection("BenjaminiHochberg", maxValue, resultsMap);
+       Map<String, BigDecimal> adjustedMap = WidgetUtil.calcErrorCorrection("BenjaminiHochberg", maxValue, resultsMap, REFERENCE_SIZE);
         for (String label : benjaminiMap.keySet()) {
             assertEquals(benjaminiMap.get(label), adjustedMap.get(label));
         }
