bdc1b27dcd2b5024a19b1ec2a6c8954a95c5d3d2
alexkalderimis <alex.kalderimis@gmail.com>
2011-01-24 13:45:36 +0000

    Added json output format
    Former-commit-id: b58cedb7ec094bd82116d025a273217a49bba794

--- intermine/web/main/src/org/intermine/webservice/server/model/ModelService.java
+++ intermine/web/main/src/org/intermine/webservice/server/model/ModelService.java
@@ -11,14 +11,19 @@ package org.intermine.webservice.server.model;
  */
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.intermine.api.InterMineAPI;
 import org.intermine.metadata.Model;
+import org.intermine.web.logic.export.ResponseUtil;
 import org.intermine.webservice.server.WebService;
 import org.intermine.webservice.server.exceptions.InternalErrorException;
+import org.intermine.webservice.server.output.JSONFormatter;
 
 /**
  * Web service that returns xml representation of model.
@@ -26,6 +31,10 @@ import org.intermine.webservice.server.exceptions.InternalErrorException;
  */
 public class ModelService extends WebService
 {
+    private static final String DEFAULT_CALLBACK = "parseModel";
+
+    private static final String FILE_BASE_NAME= "model";
+    
     public ModelService(InterMineAPI im) {
         super(im);
     }
@@ -36,7 +45,22 @@ public class ModelService extends WebService
     protected void execute(HttpServletRequest request, HttpServletResponse response) {
         Model model = this.im.getModel();
         try {
-            response.getWriter().append(model.toString());
+            if (formatIsJSON()) {
+                ResponseUtil.setJSONHeader(response, FILE_BASE_NAME + ".json");
+                Map<String, String> attributes = new HashMap<String, String>();
+                if (formatIsJSONP()) {
+                    String callback = getCallback();
+                    if (callback == null || "".equals(callback)) {
+                        callback = DEFAULT_CALLBACK;
+                    }
+                    attributes.put(JSONFormatter.KEY_CALLBACK, callback);
+                }
+                output.setHeaderAttributes(attributes);
+                output.addResultItem(Arrays.asList(model.toJSONString()));
+            } else {
+                ResponseUtil.setXMLHeader(response, FILE_BASE_NAME + ".xml");
+                response.getWriter().append(model.toString());
+            }
         } catch (IOException e) {
             throw new InternalErrorException(e);
         }
