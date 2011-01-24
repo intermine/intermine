a1cc51cea61553b67cf9230e87e0a0e7cc031267
alexkalderimis <alex.kalderimis@gmail.com>
2011-01-24 14:00:21 +0000

    removed unneeded argument
    Former-commit-id: 34f4edc6f59696c68d9d6ca637bd785323ed2699

--- intermine/web/main/src/org/intermine/webservice/server/output/JSONRowResultProcessor.java
+++ intermine/web/main/src/org/intermine/webservice/server/output/JSONRowResultProcessor.java
@@ -1,39 +1,35 @@
 package org.intermine.webservice.server.output;
 
 /*
- * Copyright (C) 2002-2011 FlyMine
- *
- * This code may be freely distributed and modified under the
- * terms of the GNU Lesser General Public Licence.  This should
- * be distributed with the code.  See the LICENSE file for more
- * information or http://www.gnu.org/copyleft/lesser.html.
- *
- */
+* Copyright (C) 2002-2011 FlyMine
+*
+* This code may be freely distributed and modified under the
+* terms of the GNU Lesser General Public Licence.  This should
+* be distributed with the code.  See the LICENSE file for more
+* information or http://www.gnu.org/copyleft/lesser.html.
+*
+*/
+
 
 import java.util.Iterator;
 
 import org.intermine.api.results.ExportResultsIterator;
 
-/**
- * @author Alexis Kalderimis
- * A class for producing rows of results as JSON arrays being written to the output.
- */
-public class JSONRowResultProcessor extends JSONResultProcessor
+public class JSONRowResultProcessor extends JSONResultProcessor 
 {
-
-    private String baseUrl;
-
+    
     /**
      * Constructor.
      * @param baseUrl The base URL to be used for constructing links with.
      */
-    public JSONRowResultProcessor(String baseUrl) {
-        this.baseUrl = baseUrl;
+    public JSONRowResultProcessor() {
+        super();
     }
 
     @Override
     protected Iterator<? extends Object> getResultsIterator(ExportResultsIterator it) {
-        JSONRowIterator jsonIter = new JSONRowIterator(it, baseUrl);
+        JSONRowIterator jsonIter = new JSONRowIterator(it);
         return jsonIter;
     }
+    
 }
