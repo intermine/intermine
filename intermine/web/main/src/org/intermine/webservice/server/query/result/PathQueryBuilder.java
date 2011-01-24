0c2f58e068516e2a04b8b58f005a96b0420770bd
alexkalderimis <alex.kalderimis@gmail.com>
2011-01-24 13:43:23 +0000

    Better error message
    Former-commit-id: 23aa7b1679d89fb2364c151f1a6c706add1d784d

--- intermine/web/main/src/org/intermine/webservice/server/query/result/PathQueryBuilder.java
+++ intermine/web/main/src/org/intermine/webservice/server/query/result/PathQueryBuilder.java
@@ -31,7 +31,7 @@ public class PathQueryBuilder
 {
 
     private PathQuery pathQuery;
-    
+
     protected PathQueryBuilder() {
     	// empty constructor for testing
     }
@@ -71,7 +71,9 @@ public class PathQueryBuilder
                         + "by this query don't exist: " + missingBags + " query: " + xml);
             }
         } else {
-            throw new BadRequestException(formatMessage(validator.getErrorsAndWarnings()));
+            throw new BadRequestException("This query is invalid:\n"
+                    + "query string: " + xml + "\n"
+                    + formatMessage(validator.getErrorsAndWarnings()));
         }
     }
 
