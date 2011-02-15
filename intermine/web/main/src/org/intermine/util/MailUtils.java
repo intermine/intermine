3e393aa2f91365acc26375e33e0f0b52e89697f7
alexkalderimis <alex.kalderimis@gmail.com>
2011-02-15 16:37:12 +0000

    A registration class for the initialiser, as well as refactored mail-utils
    Former-commit-id: 9bb6812bb9962a59adb010c79abfa48a192bcc4a

--- intermine/web/main/src/org/intermine/util/MailUtils.java
+++ intermine/web/main/src/org/intermine/util/MailUtils.java
@@ -16,6 +16,7 @@ import java.util.Properties;
 
 import javax.mail.Authenticator;
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -42,9 +43,24 @@ public abstract class MailUtils
      * @throws Exception if there is a problem creating the email
      */
     public static void email(String to, final Map webProperties) throws Exception {
+        String subject = (String) webProperties.get("mail.subject");
+        String text = (String) webProperties.get("mail.text");
+        email(to, subject, text, webProperties);
+    }
+
+    /**
+     * Send an email to an address, supplying the recipient, subject and body.
+     *
+     * @param to the address to send to
+     * @param subject The Subject of the email
+     * @param body The content of the email
+     * @param webProperties Common properties for all emails (such as from, authentication)
+     * @throws Exception if there is a problem creating the email
+     */
+    public static void email(String to, String subject, String body, String from,
+            final Map webProperties) throws MessagingException {
         final String user = (String) webProperties.get("mail.smtp.user");
         String smtpPort = (String) webProperties.get("mail.smtp.port");
-        String text = (String) webProperties.get("mail.text");
         String authFlag = (String) webProperties.get("mail.smtp.auth");
         String starttlsFlag = (String) webProperties.get("mail.smtp.starttls.enable");
 
@@ -81,11 +97,16 @@ public abstract class MailUtils
             session = Session.getDefaultInstance(properties);
         }
         MimeMessage message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(((String) webProperties.get("mail.from"))));
+        message.setFrom(new InternetAddress(from));
         message.addRecipient(Message.RecipientType.TO, InternetAddress.parse(to, true)[0]);
-        message.setSubject((String) webProperties.get("mail.subject"));
-        message.setContent(text, "text/plain");
+        message.setSubject(subject);
+        message.setContent(body, "text/plain");
         Transport.send(message);
+    }
+    public static void email(String to, String subject, String body, final Map webProperties)
+        throws MessagingException {
+        String from = (String) webProperties.get("mail.from");
+        email(to, subject, body, from, webProperties);
     }
 
     /**
@@ -98,56 +119,12 @@ public abstract class MailUtils
      */
     public static void emailPasswordToken(String to, String url, final Map webProperties)
         throws Exception {
-        final String user = (String) webProperties.get("mail.smtp.user");
-        String smtpPort = (String) webProperties.get("mail.smtp.port");
-        String mailText = (String) webProperties.get("mail.passwordText");
-        String authFlag = (String) webProperties.get("mail.smtp.auth");
-        String starttlsFlag = (String) webProperties.get("mail.smtp.starttls.enable");
-
-        Properties properties = System.getProperties();
-        mailText = MessageFormat.format(mailText, new Object[] {url});
-
-        properties.put("mail.smtp.host", webProperties.get("mail.host"));
-        properties.put("mail.smtp.user", user);
-        // Fix to "javax.mail.MessagingException: 501 Syntactically
-        // invalid HELO argument(s)" problem
-        // See http://forum.java.sun.com/thread.jspa?threadID=487000&messageID=2280968
-        properties.put("mail.smtp.localhost", "localhost");
-        if (smtpPort != null) {
-            properties.put("mail.smtp.port", smtpPort);
-        }
-
-        if (starttlsFlag != null) {
-            properties.put("mail.smtp.starttls.enable", starttlsFlag);
-        }
-        if (authFlag != null) {
-            properties.put("mail.smtp.auth", authFlag);
-        }
-
-        Session session;
-        if (authFlag != null && ("true".equals(authFlag) || "t".equals(authFlag))) {
-            Authenticator authenticator = new Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    String password = (String) webProperties.get("mail.server.password");
-                    return new PasswordAuthentication(user, password);
-                }
-            };
-            session = Session.getDefaultInstance(properties, authenticator);
-        } else {
-            session = Session.getDefaultInstance(properties);
-        }
 
         String projectTitle = (String) webProperties.get("project.title");
         String baseSubject = (String) webProperties.get("mail.passwordSubject");
         String mailSubject = MessageFormat.format(baseSubject, new Object[] {projectTitle});
-
-        MimeMessage message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(((String) webProperties.get("mail.from"))));
-        message.addRecipient(Message.RecipientType.TO, InternetAddress.parse(to, true)[0]);
-        message.setSubject(mailSubject);
-        message.setContent(mailText, "text/plain");
-        Transport.send(message);
+        String mailText = (String) webProperties.get("mail.passwordText");
+        email(to, mailSubject, mailText, webProperties);
     }
 
 
@@ -159,45 +136,9 @@ public abstract class MailUtils
      * @throws Exception when somethign goes wrong
      */
     public static void subscribe(String email, final Map webProperties) throws Exception {
-        final String user = (String) webProperties.get("mail.smtp.user");
-        String smtpPort = (String) webProperties.get("mail.smtp.port");
-        String authFlag = (String) webProperties.get("mail.smtp.auth");
-        String starttlsFlag = (String) webProperties.get("mail.smtp.starttls.enable");
-
-        Properties properties = System.getProperties();
-
-        properties.put("mail.smtp.host", webProperties.get("mail.host"));
-        properties.put("mail.smtp.user", user);
-        properties.put("mail.smtp.localhost", "localhost");
-        if (smtpPort != null) {
-            properties.put("mail.smtp.port", smtpPort);
-        }
-
-        if (starttlsFlag != null) {
-            properties.put("mail.smtp.starttls.enable", starttlsFlag);
-        }
-        if (authFlag != null) {
-            properties.put("mail.smtp.auth", authFlag);
-        }
-        Session session;
-        if (authFlag != null && ("true".equals(authFlag) || "t".equals(authFlag))) {
-            Authenticator authenticator = new Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    String password = (String) webProperties.get("mail.server.password");
-                    return new PasswordAuthentication(user, password);
-                }
-            };
-            session = Session.getDefaultInstance(properties, authenticator);
-        } else {
-            session = Session.getDefaultInstance(properties);
-        }
-
-        MimeMessage message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(email));
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress(((String) webProperties
-                        .get("mail.mailing-list"))));
-        message.setContent("", "text/plain");
-        Transport.send(message);
+        String to = (String) webProperties.get("mail.mailing-list");
+        String subject = "";
+        String body = "";
+        email(to, subject, body, email, webProperties);
     }
 }
--- intermine/web/main/src/org/intermine/web/struts/InitialiserPlugin.java
+++ intermine/web/main/src/org/intermine/web/struts/InitialiserPlugin.java
@@ -103,6 +103,7 @@ public class InitialiserPlugin implements PlugIn
      * @throws ServletException if this <code>PlugIn</code> cannot
      * be successfully initialized
      */
+    @Override
     public void init(ActionServlet servlet, ModuleConfig config) throws ServletException {
 
         // NOTE throwing exceptions other than a ServletException from this class causes the
@@ -168,8 +169,14 @@ public class InitialiserPlugin implements PlugIn
         servletContext.setAttribute(Constants.KEYLESS_CLASSES_MAP, keylessClasses);
 
         setupClassSummaryInformation(servletContext, oss, os.getModel());
+
+        doRegistration(webProperties);
     }
 
+    private void doRegistration(Properties webProperties) {
+        Registrar reg = new Registrar(webProperties);
+        reg.start();
+    }
 
     private ObjectStore getProductionObjectStore(Properties webProperties) throws ServletException {
         ObjectStore os;
@@ -421,9 +428,11 @@ public class InitialiserPlugin implements PlugIn
         servletContext.setAttribute(Constants.EMPTY_FIELD_MAP, emptyFields);
         // Build map interface that takes an object and returns set of leaf class descriptors
         Map leafDescriptorsMap = new AbstractMap() {
+            @Override
             public Set entrySet() {
                 return null;
             }
+            @Override
             public Object get(Object key) {
                 if (key == null) {
                     return Collections.EMPTY_SET;
@@ -454,6 +463,7 @@ public class InitialiserPlugin implements PlugIn
     /**
      * Destroy method called at Servlet destroy
      */
+    @Override
     public void destroy() {
         try {
             profileManager.close();
