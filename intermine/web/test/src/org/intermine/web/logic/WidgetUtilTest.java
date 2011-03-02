fd3b6f888b0b9d2cfaea21fd1c5cee00d1e8b1bf
julie-sullivan <julie@flymine.org>
2011-03-02 15:23:47 +0000

    update hypergeometric distribution

--- intermine/web/test/src/org/intermine/web/logic/WidgetUtilTest.java
+++ intermine/web/test/src/org/intermine/web/logic/WidgetUtilTest.java
@@ -3,9 +3,9 @@ package org.intermine.web.logic;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
-import junit.framework.TestCase;
+import org.apache.commons.math.distribution.HypergeometricDistributionImpl;
 
-import org.intermine.web.logic.widget.Hypergeometric;
+import junit.framework.TestCase;
 
 public class WidgetUtilTest extends TestCase
 {
@@ -29,27 +29,28 @@ public class WidgetUtilTest extends TestCase
         id[1] = "overrepresented";
         taggedSample[1] = 3;
         taggedPopulation[1] = 20;
-        expectedResults[1] = new BigDecimal(0.00705009276437833);
+        expectedResults[1] = new BigDecimal(0.00705009276437847977325734660780653939582407474517822265625);
 
         id[2] = "notEnriched";
         taggedSample[2] = 1;
         taggedPopulation[2] = 10;
-        expectedResults[2] = new BigDecimal(0.27346938775509510577421679045073688030242919921875);
+        expectedResults[2] = new BigDecimal(0.273469387755102155690423160194768570363521575927734375);
 
         id[3] = "underrepresented";
         taggedSample[3] = 1;
         taggedPopulation[3] = 50;
-        expectedResults[3] = new BigDecimal(0.8787878787878582);
+        expectedResults[3] = new BigDecimal(0.8787878787878777853137535203131847083568572998046875);
 
         id[4] = "one";
         taggedSample[4] = 3;
         taggedPopulation[4] = 100;
         expectedResults[4] = new BigDecimal(1);
 
-        for (int i = 1; i <= 4; i++) {
+        HypergeometricDistributionImpl h = new HypergeometricDistributionImpl(total,bagsize,bagsize);
 
-            BigDecimal p = new BigDecimal(Hypergeometric.calculateP(taggedSample[i], bagsize,
-                    taggedPopulation[i], total));
+        for (int i = 1; i <= 4; i++) {
+            h.setNumberOfSuccesses(taggedPopulation[i]);
+            BigDecimal p = new BigDecimal(h.upperCumulativeProbability(taggedSample[i]));
             resultsMap.put(id[i], p);
         }
     }
