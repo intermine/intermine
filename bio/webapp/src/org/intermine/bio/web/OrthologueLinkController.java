62b79c9aeb108ed72c3d8324046917a26a254838
julie-sullivan <julie@flymine.org>
2011-03-03 14:58:47 +0000

    change orthologue links to use ajax, be links instead of popup form.  will change these links to be orthologue counts in next revision
    Former-commit-id: 03b8c065b8cf05983212abd5cfe4739a0bf2c285

--- bio/webapp/src/org/intermine/bio/web/OrthologueLinkController.java
+++ bio/webapp/src/org/intermine/bio/web/OrthologueLinkController.java
@@ -11,36 +11,39 @@ package org.intermine.bio.web;
  */
 
 import java.util.Collection;
-import java.util.Map;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.tiles.ComponentContext;
 import org.apache.struts.tiles.actions.TilesAction;
 import org.intermine.api.InterMineAPI;
-import org.intermine.api.mines.HomologueMapping;
-import org.intermine.api.mines.Mine;
-import org.intermine.api.mines.OrthologueLinkManager;
 import org.intermine.api.profile.InterMineBag;
 import org.intermine.bio.web.logic.BioUtil;
+import org.intermine.util.PropertiesUtil;
+import org.intermine.util.StringUtil;
 import org.intermine.util.TypeUtil;
 import org.intermine.web.logic.bag.BagHelper;
 import org.intermine.web.logic.session.SessionMethods;
 
 
 /**
- * Class that generates links to other intermines
- * @author julie
+ * Gets list of friendly intermines to show on list analysis page.
+ *
+ * @author Julie Sullivan
  */
 public class OrthologueLinkController  extends TilesAction
 {
-    private String identifierField = "primaryIdentifier";
-    private String alternativeIdentifierField = "ensemblIdentifier";
+    private static final String IDENTIFIER = "primaryIdentifier";
+    private static final String ALTERNATIVE_IDENTIFIER = "ensemblIdentifier";
 
     /**
      * {@inheritDoc}
@@ -48,11 +51,29 @@ public class OrthologueLinkController  extends TilesAction
     @Override
     public ActionForward execute(ComponentContext context, ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response) {
-
-        final InterMineAPI im = SessionMethods.getInterMineAPI(request.getSession());
         InterMineBag bag = (InterMineBag) request.getAttribute("bag");
+        final InterMineAPI im = SessionMethods.getInterMineAPI(request.getSession());
+        Collection<String> organismsInBag = BioUtil.getOrganisms(im.getObjectStore(), bag, false,
+                "shortName");
+        String organisms = StringUtil.join(organismsInBag, ",");
         Properties webProperties = SessionMethods.getWebProperties(request.getSession()
                 .getServletContext());
+        String localMineName = webProperties.getProperty("project.title");
+        Properties props = PropertiesUtil.stripStart("intermines",
+                PropertiesUtil.getPropertiesStartingWith("intermines", webProperties));
+        Enumeration<?> propNames = props.propertyNames();
+        Set<String> mines = new HashSet<String>();
+        while (propNames.hasMoreElements()) {
+            String mineId =  (String) propNames.nextElement();
+            mineId = mineId.substring(0, mineId.indexOf("."));
+            Properties mineProps = PropertiesUtil.stripStart(mineId,
+                    PropertiesUtil.getPropertiesStartingWith(mineId, props));
+            String mineName = mineProps.getProperty("name");
+            if (StringUtils.isNotEmpty(mineName) && !mineName.equals(localMineName)) {
+                mines.add(mineName);
+            }
+        }
+
         Class c = null;
         try {
             c = Class.forName(bag.getQualifiedType());
@@ -60,20 +81,17 @@ public class OrthologueLinkController  extends TilesAction
             e.printStackTrace();
             return null;
         }
-
+        String identifierField = IDENTIFIER;
         // hack so we can use ensembl for ratmine
-        if (TypeUtil.getFieldInfo(c, alternativeIdentifierField) != null) {
-            identifierField = alternativeIdentifierField;
+        if (TypeUtil.getFieldInfo(c, ALTERNATIVE_IDENTIFIER) != null) {
+            identifierField = ALTERNATIVE_IDENTIFIER;
         }
         String identifierList = BagHelper.getIdList(bag, im.getObjectStore(), "", identifierField);
         request.setAttribute("identifierList", identifierList);
 
-        OrthologueLinkManager olm = OrthologueLinkManager.getInstance(im, webProperties);
-        Collection<String> organismNamesInBag = BioUtil.getOrganisms(im.getObjectStore(), bag,
-                false, "shortName");
-        Map<Mine, Map<String, HomologueMapping>> mines = olm.getMines(organismNamesInBag);
         if (!mines.isEmpty()) {
             request.setAttribute("mines", mines);
+            request.setAttribute("organisms", organisms);
         }
         return null;
     }
