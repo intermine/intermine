c81509a11495b9f89c02850211b66f52c6e4e7b9
alexkalderimis <alex.kalderimis@gmail.com>
2011-03-15 11:41:49 +0000

    checkstyle
    Former-commit-id: 2816a8d77f43ab65224c6fc42d1d337c05c70327

--- intermine/web/main/src/org/intermine/webservice/server/output/FlatFileFormatter.java
+++ intermine/web/main/src/org/intermine/webservice/server/output/FlatFileFormatter.java
@@ -4,33 +4,38 @@ import java.util.List;
 import java.util.Map;
 
 import org.intermine.web.logic.export.RowFormatter;
-import org.intermine.web.logic.export.RowFormatterImpl;
 import org.intermine.webservice.server.StatusDictionary;
 
+/**
+ * A class to define behaviour common to flat files (csv, tsv)
+ * @author alex
+ *
+ */
 public abstract class FlatFileFormatter extends Formatter {
 
-	private static final String errorIntro = "[ERROR] ";
-	public static final String HEADER_COLUMNS = "view";
-	protected RowFormatter labourer = null;
-	
-	protected RowFormatter getRowFormatter() {
-		return labourer;
-	}
-	
-	protected void setRowFormatter(RowFormatter fmtr) {
-		labourer = fmtr;
-	}
-	
-	/** {@inheritDoc}} **/
+    private static final String ERROR_INTRO = "[ERROR] ";
+    /** The key for the header columns **/
+    public static final String COLUMN_HEADERS = "view";
+    protected RowFormatter labourer = null;
+
+    protected RowFormatter getRowFormatter() {
+        return labourer;
+    }
+
+    protected void setRowFormatter(RowFormatter fmtr) {
+        labourer = fmtr;
+    }
+
+    /** {@inheritDoc}} **/
     @Override
     public String formatHeader(Map<String, Object> attributes) {
-    	if (attributes != null && attributes.containsKey(HEADER_COLUMNS)) {
-    		List<Object> columns = (List<Object>) attributes.get(HEADER_COLUMNS);
-    		return getRowFormatter().format(columns);
-    	}
+        if (attributes != null && attributes.containsKey(COLUMN_HEADERS)) {
+            List<Object> columns = (List<Object>) attributes.get(COLUMN_HEADERS);
+            return getRowFormatter().format(columns);
+        }
         return "";
     }
-    
+
     /** {@inheritDoc}} **/
     @Override
     public String formatResult(List<String> resultRow) {
@@ -40,14 +45,14 @@ public abstract class FlatFileFormatter extends Formatter {
     /** {@inheritDoc}} **/
     @Override
     public String formatFooter(String errorMessage, int errorCode) {
-    	StringBuilder sb = new StringBuilder();
-    	if (errorCode != Output.SC_OK) {
-    		sb.append(errorIntro);
-	        sb.append(StatusDictionary.getDescription(errorCode));
-	        sb.append("\n");
-    		sb.append(errorIntro).append(errorMessage);
-    	}
-    	return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        if (errorCode != Output.SC_OK) {
+            sb.append(ERROR_INTRO);
+            sb.append(StatusDictionary.getDescription(errorCode));
+            sb.append("\n");
+            sb.append(ERROR_INTRO).append(errorMessage);
+        }
+        return sb.toString();
     }
-	
+
 }
