d444af2dc33fadfba5a8129a6186e25f99bc7989
julie-sullivan <julie@flymine.org>
2011-03-16 16:45:59 +0000

    checkstyle

--- bio/webapp/src/org/intermine/bio/web/displayer/CytoscapeNetworkDisplayer.java
+++ bio/webapp/src/org/intermine/bio/web/displayer/CytoscapeNetworkDisplayer.java
@@ -1,9 +1,16 @@
 package org.intermine.bio.web.displayer;
 
-import java.util.ArrayList;
-import java.util.Collection;
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,26 +20,19 @@ import org.intermine.api.InterMineAPI;
 import org.intermine.api.profile.InterMineBag;
 import org.intermine.api.profile.Profile;
 import org.intermine.api.query.PathQueryExecutor;
-import org.intermine.api.query.WebResultsExecutor;
-import org.intermine.api.results.WebResults;
 import org.intermine.bio.web.logic.CytoscapeNetworkDBQueryRunner;
 import org.intermine.bio.web.logic.CytoscapeNetworkUtil;
 import org.intermine.metadata.Model;
 import org.intermine.model.InterMineObject;
-import org.intermine.model.bio.Gene;
-import org.intermine.model.bio.Protein;
 import org.intermine.objectstore.ObjectStore;
 import org.intermine.objectstore.ObjectStoreException;
-import org.intermine.pathquery.Constraints;
-import org.intermine.pathquery.OrderDirection;
-import org.intermine.pathquery.OuterJoinStatus;
-import org.intermine.pathquery.PathQuery;
 import org.intermine.util.StringUtil;
 import org.intermine.web.displayer.CustomDisplayer;
 import org.intermine.web.logic.config.ReportDisplayerConfig;
 import org.intermine.web.logic.results.DisplayObject;
-import org.intermine.web.logic.results.PagedTable;
 import org.intermine.web.logic.session.SessionMethods;
+import org.intermine.model.bio.Gene;
+import org.intermine.model.bio.Protein;
 
 public class CytoscapeNetworkDisplayer extends CustomDisplayer {
 
@@ -48,7 +48,7 @@ public class CytoscapeNetworkDisplayer extends CustomDisplayer {
         Profile profile = SessionMethods.getProfile(request.getSession()); // Get Profile
         PathQueryExecutor executor = im.getPathQueryExecutor(profile); // Get PathQueryExecutor
 
-        Set<Integer> startingFeatureSet = new LinkedHashSet<Integer>(); // feature: gene or protein
+        Set<Integer> startingFeatureSet = new HashSet<Integer>(); // feature: gene or protein
         String featureType = "";
 
         //=== Get Interaction information ===
@@ -91,46 +91,18 @@ public class CytoscapeNetworkDisplayer extends CustomDisplayer {
         }
 
         // Check if interaction data available for the organism
-        Gene hubGene;
-		try {
-			hubGene = (Gene) os.getObjectById((Integer) startingFeatureSet.toArray()[0]);
-	        String orgName = hubGene.getOrganism().getName();
-	        if (!interactionInfoMap.containsKey(orgName)) {
-	            String orgWithNoDataMessage = "No interaction data found for "
-	                    + orgName + " genes";
-	            request.setAttribute("orgWithNoDataMessage", orgWithNoDataMessage);
-	        }
-		} catch (ObjectStoreException e) {
-			request.setAttribute("exception", "An exception occured");
-			e.printStackTrace();
-		}
-
-		// Add view interaction inline table
-        PathQuery q = new PathQuery(model);
-        q.addViews("Gene.symbol",
-        		"Gene.primaryIdentifier",
-                "Gene.interactions.interactionType",
-                "Gene.interactions.interactingGenes.symbol",
-                "Gene.interactions.interactingGenes.primaryIdentifier",
-                "Gene.interactions.dataSets.dataSource.name",
-                "Gene.interactions.experiment.publication.title",
-                "Gene.interactions.experiment.publication.pubMedId");
-
-        q.addOrderBy("Gene.symbol", OrderDirection.ASC);
-        q.addConstraint(Constraints.inIds("Gene", fullInteractingGeneSet), "B");
-        q.addConstraint(Constraints.inIds("Gene.interactions.interactingGenes",
-                fullInteractingGeneSet), "A");
-        q.setConstraintLogic("B and A");
-
+        Gene aTestGene;
         try {
-            WebResultsExecutor we = im.getWebResultsExecutor(profile);
-            WebResults webResults = we.execute(q);
-            PagedTable pagedResults = new PagedTable(webResults, 10);
-            pagedResults.setTableid("CytoscapeNetworkDisplayer");
-            request.setAttribute("cytoscapeNetworkPagedResults", pagedResults);
+            aTestGene = (Gene) os.getObjectById((Integer) fullInteractingGeneSet.toArray()[0]);
+            String orgName = aTestGene.getOrganism().getName();
+            if (!interactionInfoMap.containsKey(orgName)) {
+                String orgWithNoDataMessage = "No interaction data found for "
+                    + orgName + " genes";
+                request.setAttribute("orgWithNoDataMessage", orgWithNoDataMessage);
+            }
         } catch (ObjectStoreException e) {
-            throw new RuntimeException(e);
+            request.setAttribute("exception", "An exception occured");
+            e.printStackTrace();
         }
-
     }
 }
