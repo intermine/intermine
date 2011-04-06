04c4bbd69b27f1941dcd2321c060d46ce3dd4222
alexkalderimis <alex.kalderimis@gmail.com>
2011-04-06 17:15:31 +0000

    better compile-run script (no errors, and with comments - and handles classes and paths equally)

--- intermine/webservice/client/main/compile-run.sh
+++ intermine/webservice/client/main/compile-run.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
-# Compiles sample source and runs sample.
+#
+# Compiles the sample source and runs sample.
+#
+# ARGS: the path to the sample, as a class or file name
+#
+# EG:
+#
+#  ./compile-run flymine.Query
+#
+# OR:
+#
+#  ./compile-run flymine/Query.java
+#
+# LICENSE: This code is distributed under the LGPL
 
-if [ $# = 0 ]; then 
+
+if [ $# = 0 ]; then
     echo "Usage: compile-run sample_name"
     exit;
 fi;
 
-FILE=$1.java
-if !(test -e ${FILE}); then 
+case $1 in
+    *.java) FILE=$1 && CLASS=$(echo $1 | sed 's/\.java$//' | sed 's/\//./g');;
+    *) CLASS=$1 && FILE=$(echo $1 | sed 's/\./\//g').java;;
+esac
+
+if !(test -e ${FILE}); then
     echo "File ${FILE} doesn't exist.";
     echo "Usage: compile-run sample_name"
-    exit; 
+    exit;
 fi;
 
 # Puts all jars from ./lib dir on the classpath
@@ -19,7 +37,6 @@ for i in ./lib/*jar
     CLASSPATH=$CLASSPATH:$i
 done
 
-cd $1;
 mkdir -p build;
-javac -d ./build -classpath ${CLASSPATH} ./$1.java && \
-java -classpath ./build:${CLASSPATH} $@;
+javac -d ./build -classpath ${CLASSPATH} ./$FILE && \
+java -classpath ./build:${CLASSPATH} $CLASS;
