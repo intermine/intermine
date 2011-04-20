b714cd096baad8b2beaa1586f9f67d1e9280216f
julie-sullivan <julie@flymine.org>
2011-04-20 14:09:28 +0000

    update script to get human/fly homologues

--- bio/scripts/ensembl/ensemblCompara.pl
+++ bio/scripts/ensembl/ensemblCompara.pl
@@ -15,42 +15,50 @@ use strict;
 use warnings;
 use Switch;
 
-use InterMine::Util qw(get_property_value);
-
-
 use Bio::EnsEMBL::Registry;
 Bio::EnsEMBL::Registry->load_registry_from_db(
     -host => 'ensembldb.ensembl.org',
     -user => 'anonymous',
     -port => 5306);
 
-# get the MemberAdaptor
-my $member_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Multi','compara','Member');
+# load all genes
+my @organisms = ('homo_sapiens', 'danio_rerio', 'mus_musculus', 'caenorhabditis_elegans', 'saccharomyces_cerevisiae', 'drosophila_melanogaster', 'rattus_norvegicus');
+# only load genes if they are homologues of gene of organism of interest.  can be null
+my @homologues = ('homo_sapiens', 'danio_rerio', 'mus_musculus', 'caenorhabditis_elegans', 'saccharomyces_cerevisiae', 'drosophila_melanogaster', 'rattus_norvegicus');
 
-# fetch a Member
-
-#my $member = $member_adaptor->fetch_all_by_source_taxon(7227);
-#my $member = $member_adaptor->fetch_all();
-
-# fetch a Member
-my $member = $member_adaptor->fetch_by_source_stable_id(
-    'ENSEMBLGENE','ENSG00000004059');
-
-
-# print out some information about the Member
-print " dbID: "  . $member->dbID
-   . " stable_id: "  . $member-> stable_id
-   . " taxon_id: "  . $member-> taxon_id
-   . " genome_db_id: "  . $member-> genome_db_id    
-   . " source_name: "  . $member->source_name  . "\n";
-
+# human, zebrafish, mouse, worm, yeast, fruitfly, rat
 
 my $reg = "Bio::EnsEMBL::Registry";
 
-$reg->load_registry_from_db(-host=>"ensembldb.ensembl.org", -user=>"anonymous");
 my $genome_db_adaptor = $reg->get_adaptor("Multi", "compara", "GenomeDB");
-my $all_genome_dbs = $genome_db_adaptor->fetch_all();
-while ( my $db = shift @{$all_genome_dbs} ) {
-   print $db->dbID . " name " . $db->name . "\n";
+
+my $human_genome_db = $genome_db_adaptor->fetch_by_registry_name("human");
+my $dmel_genome_db = $genome_db_adaptor->fetch_by_registry_name("fruitfly");
+
+my $mlss_adaptor = $reg->get_adaptor("Multi", "compara", "MethodLinkSpeciesSet");
+#my $method_link_species_sets = $mlss_adaptor->fetch_all_by_GenomeDB($genome_db);
+
+my $method_link_species_set = $mlss_adaptor->fetch_by_method_link_type_GenomeDBs("ENSEMBL_ORTHOLOGUES", [$human_genome_db, $dmel_genome_db]);
+my $homology_adaptor = $reg->get_adaptor('Multi','compara','Homology');
+
+#my $member_adaptor = $reg->get_adaptor('Multi','compara','Member');
+#my $members = $member_adaptor->fetch_all_by_source_taxon('ENSEMBLGENE', 9606);
+
+my $homologues = $homology_adaptor->fetch_all_by_MethodLinkSpeciesSet($method_link_species_set);
+
+foreach my $homology (@$homologues) {
+   my $gene_list = $homology->gene_list();
+   while ( my $member = shift @{$gene_list} ) {
+         my $taxon_id = $member->taxon_id;
+         my $gene = $member->get_Gene();
+         print $gene->stable_id . "\t" . $taxon_id . "\t";
+         
+          @dblinks = @{ $gene->get_all_DBLinks() };
+             while ( my $member = shift @{$gene_list} ) {
+             
+             }
+   }
+   print "\n";
 }
+
 exit 0;
