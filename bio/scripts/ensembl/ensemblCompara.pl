2d7e443380811e7ab316b842a127ebd7e84f34c5
julie-sullivan <julie@flymine.org>
2011-04-26 10:23:33 +0000

    tidying up bio/scripts

--- bio/scripts/ensembl/ensemblCompara.pl
+++ bio/scripts/ensembl/ensemblCompara.pl
@@ -26,26 +26,18 @@ my @organisms = ('homo_sapiens', 'danio_rerio', 'mus_musculus', 'caenorhabditis_
 # only load genes if they are homologues of gene of organism of interest.  can be null
 my @homologues = ('homo_sapiens', 'danio_rerio', 'mus_musculus', 'caenorhabditis_elegans', 'saccharomyces_cerevisiae', 'drosophila_melanogaster', 'rattus_norvegicus');
 
+# human, zebrafish, mouse, worm, yeast, fruitfly, rat
+
 my $reg = "Bio::EnsEMBL::Registry";
 
-my $genome_db_adaptor = $reg->get_adaptor("Multi", "compara", "GenomeDB");
+my $member_adaptor = $reg->get_adaptor('Multi','compara','Member');
+my $members = $member_adaptor->fetch_all_by_source_taxon('ENSEMBLGENE', 9606);
 
-my $human_genome_db = $genome_db_adaptor->fetch_by_registry_name("human");
-my $dmel_genome_db = $genome_db_adaptor->fetch_by_registry_name("fruitfly");
 
-my $mlss_adaptor = $reg->get_adaptor("Multi", "compara", "MethodLinkSpeciesSet");
-
-my $method_link_species_set = $mlss_adaptor->fetch_by_method_link_type_GenomeDBs("ENSEMBL_ORTHOLOGUES", [$human_genome_db, $dmel_genome_db]);
-my $homology_adaptor = $reg->get_adaptor('Multi','compara','Homology');
-
-my $homologues = $homology_adaptor->fetch_all_by_MethodLinkSpeciesSet($method_link_species_set);
-
-foreach my $homology (@$homologues) {
-   my $gene_list = $homology->gene_list();
    while ( my $member = shift @{$gene_list} ) {
          my $taxon_id = $member->taxon_id;
          my $gene = $member->get_Gene();
-         print $taxon_id . "\t" . $gene->stable_id . "\t";
+         print $taxon_id . "\t" . $taxon_id . "\t";
          
          my $dblinks = $gene->get_all_DBLinks('Entrez%');
          my $symbol;
@@ -57,6 +49,6 @@ foreach my $homology (@$homologues) {
          print $symbol . "\t" . $secondaryIdentifier . "\t";
    }
    print "\n";
-}
+
 
 exit 0;
