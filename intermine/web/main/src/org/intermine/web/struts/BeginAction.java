30bef2733b7c3f4c25b91534ab2ec981a61e2400
julie-sullivan <julie@flymine.org>
2011-05-10 10:54:37 +0000

    add optional organism constraint on bag upload on home page

--- intermine/web/main/src/org/intermine/web/struts/BeginAction.java
+++ intermine/web/main/src/org/intermine/web/struts/BeginAction.java
@@ -28,6 +28,7 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionMessage;
 import org.intermine.api.InterMineAPI;
+import org.intermine.api.bag.BagQueryConfig;
 import org.intermine.api.profile.Profile;
 import org.intermine.api.profile.TagManager;
 import org.intermine.api.tag.TagNames;
@@ -160,6 +161,17 @@ public class BeginAction extends InterMineAction
         }
         request.setAttribute("preferredBags", preferredBags);
 
+        // organism dropdown on list upload
+        BagQueryConfig bagQueryConfig = im.getBagQueryConfig();
+        String extraClassName = bagQueryConfig.getExtraConstraintClassName();
+        if (extraClassName != null) {
+            request.setAttribute("extraBagQueryClass", TypeUtil.unqualifiedName(extraClassName));
+
+            List extraClassFieldValues = BagBuildController.getFieldValues(im.getObjectStore(),
+                    im.getObjectStoreSummary(), extraClassName, bagQueryConfig.getConstrainField());
+            request.setAttribute("extraClassFieldValues", extraClassFieldValues);
+        }
+
         // cookie business
         if (!hasUserVisited(request)) {
             // set cookie
@@ -204,5 +216,4 @@ public class BeginAction extends InterMineAction
         response.addCookie(cookie);
         return response;
     }
-
 }
