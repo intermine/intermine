aa270d32a585204a6ba1cbfa101716020253846f
danielabutano <daniela.butano@gmail.com>
2011-05-18 11:48:04 +0000

    Import/export XML file for all tracks (template, list, query, login, keysearch)
    Former-commit-id: 04462f8024e9e83b5090d9c01fe0c22be1a0cf3f

---
+++ intermine/api/main/src/org/intermine/api/tracker/xml/TrackManagerBinding.java
@@ -0,0 +1,43 @@
+package org.intermine.api.tracker.xml;
+
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.intermine.objectstore.ObjectStore;
+
+/**
+ * Code for reading and writing all tracks objects as XML
+ *
+ * @author Daniela Butano
+ */
+public class TrackManagerBinding
+{
+
+    /**
+     * Convert all tracks to XML and write XML to given writer.
+     * @param uos the UserObjectStore
+     * @param writer the XMLStreamWriter to write to
+     */
+    public static void marshal(ObjectStore uos, XMLStreamWriter writer) {
+        try {
+            writer.writeStartElement("tracks");
+            TemplateTrackBinding.marshal(uos, writer);
+            LoginTrackBinding.marshal(uos, writer);
+            ListTrackBinding.marshal(uos, writer);
+            QueryTrackBinding.marshal(uos, writer);
+            SearchTrackBinding.marshal(uos, writer);
+            writer.writeEndElement();
+        } catch (XMLStreamException e) {
+            throw new RuntimeException("exception while marshalling tracks", e);
+        }
+    }
+}
