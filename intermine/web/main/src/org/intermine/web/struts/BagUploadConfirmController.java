7d31308224b2d597aef804bac56440da6e06259e
radekstepan <dev@radekstepan.com>
2011-05-18 12:17:25 +0000

    Upload Confirm synonym handling

--- intermine/web/main/src/org/intermine/web/struts/BagUploadConfirmController.java
+++ intermine/web/main/src/org/intermine/web/struts/BagUploadConfirmController.java
@@ -20,7 +20,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
