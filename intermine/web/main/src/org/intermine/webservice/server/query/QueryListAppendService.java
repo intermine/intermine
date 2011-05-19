46da6bd705a631115a0175f2976f6d8f6afd68fd
alexkalderimis <alex.kalderimis@gmail.com>
2011-05-19 14:41:37 +0000

    Changed method signature to match that in superclass
    Former-commit-id: ad601a5726c061edf6310e975f8b983aa4d50fcd

--- intermine/web/main/src/org/intermine/webservice/server/query/QueryListAppendService.java
+++ intermine/web/main/src/org/intermine/webservice/server/query/QueryListAppendService.java
@@ -2,38 +2,28 @@ package org.intermine.webservice.server.query;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.lang.StringUtils;
 import org.intermine.api.InterMineAPI;
-import org.intermine.api.bag.BagManager;
 import org.intermine.api.bag.BagQueryResult;
 import org.intermine.api.profile.InterMineBag;
 import org.intermine.api.profile.Profile;
 import org.intermine.api.query.MainHelper;
 import org.intermine.objectstore.ObjectStoreException;
-import org.intermine.objectstore.intermine.CompletelyFalseException;
 import org.intermine.objectstore.query.Query;
 import org.intermine.objectstore.query.QuerySelectable;
-import org.intermine.pathquery.Path;
 import org.intermine.pathquery.PathException;
 import org.intermine.pathquery.PathQuery;
-import org.intermine.web.logic.session.SessionMethods;
-import org.intermine.webservice.server.exceptions.BadRequestException;
-import org.intermine.webservice.server.output.JSONFormatter;
-import org.intermine.webservice.server.query.result.PathQueryBuilder;
 
-public class QueryListAppendService extends QueryToListService { 
+public class QueryListAppendService extends QueryToListService {
 
     public QueryListAppendService(InterMineAPI im) {
         super(im);
     }
 
-    protected void generateListFromQuery(PathQuery pq, 
-        String name, String description,
+    @Override
+    protected void generateListFromQuery(PathQuery pq,
+        String name, String description, List<String> tags,
         Profile profile) throws ObjectStoreException, PathException {
         Query q = MainHelper.makeQuery(
                 pq,
@@ -47,7 +37,7 @@ public class QueryListAppendService extends QueryToListService {
             list.addToBagFromQuery(q);
         } finally {
             output.addResultItem(Arrays.asList("" + list.size()));
-        } 
+        }
     }
 }
 
