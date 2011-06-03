54776454524afd0f85fafbc58bd842c49fc14e5a
alexkalderimis <alex.kalderimis@gmail.com>
2011-06-03 13:09:07 +0000

    Now with column counts, to spare large transfers
    Former-commit-id: 1ae601a9e59c0975ec2d25a72f9a1c9a7f213d02

--- intermine/web/main/src/org/intermine/webservice/server/path/PossibleValuesService.java
+++ intermine/web/main/src/org/intermine/webservice/server/path/PossibleValuesService.java
@@ -1,6 +1,7 @@
 package org.intermine.webservice.server.path;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.intermine.api.InterMineAPI;
 import org.intermine.metadata.Model;
+import org.intermine.objectstore.ObjectStore;
 import org.intermine.objectstore.query.Query;
 import org.intermine.objectstore.query.QueryClass;
 import org.intermine.objectstore.query.QueryField;
@@ -43,7 +45,7 @@ public class PossibleValuesService extends WebService {
 
     private Map<String, Object> getHeaderAttributes() {
         Map<String, Object> attributes = new HashMap<String, Object>();
-        if (formatIsJSON()) {
+        if (formatIsJSON() && !formatIsCount()) {
             attributes.put(JSONFormatter.KEY_INTRO, "\"values\":[");
             attributes.put(JSONFormatter.KEY_OUTRO, "]");
         }
@@ -86,21 +88,27 @@ public class PossibleValuesService extends WebService {
         q.addToSelect(qf);
         q.addToGroupBy(qf1);
 
-        Results results = im.getObjectStore().execute(q, DEFAULT_BATCH_SIZE, true, true, false);
-        Iterator<Object> iter = results.iterator();
+        if (formatIsCount()) {
+            int count = im.getObjectStore().count(q, ObjectStore.SEQUENCE_IGNORE);
+            output.addResultItem(Arrays.asList(String.valueOf(count)));
+        } else {
 
-        while (iter.hasNext()) {
-            List row = (List) iter.next();
-            Map<String, Object> jsonMap = new HashMap<String, Object>();
-            jsonMap.put("value", row.get(0));
-            jsonMap.put("count", row.get(1));
-            JSONObject jo = new JSONObject(jsonMap);
-            List<String> forOutput = new ArrayList<String>();
-            forOutput.add(jo.toString());
-            if (iter.hasNext()) {
-                forOutput.add("");
+            Results results = im.getObjectStore().execute(q, DEFAULT_BATCH_SIZE, true, true, false);
+            Iterator<Object> iter = results.iterator();
+
+            while (iter.hasNext()) {
+                List row = (List) iter.next();
+                Map<String, Object> jsonMap = new HashMap<String, Object>();
+                jsonMap.put("value", row.get(0));
+                jsonMap.put("count", row.get(1));
+                JSONObject jo = new JSONObject(jsonMap);
+                List<String> forOutput = new ArrayList<String>();
+                forOutput.add(jo.toString());
+                if (iter.hasNext()) {
+                    forOutput.add("");
+                }
+                output.addResultItem(forOutput);
             }
-            output.addResultItem(forOutput);
         }
 
     }
