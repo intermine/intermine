2ece22c79d126a8630535ac3a2230fbc6c6e6c17
alexkalderimis <alex.kalderimis@gmail.com>
2011-06-11 22:22:40 +0000

    A script to mount all apps at once

---
+++ intermine/perl/MineViewer/bin/load_all_apps.pl
@@ -0,0 +1,38 @@
+use Dancer ':syntax';
+use Plack::Builder;
+use Cwd;
+
+setting apphandler => 'PSGI';
+
+my $appdir = cwd();
+warn("Running in $appdir\n");
+
+my $view_dir = $appdir . '/views';
+
+sub make_app {
+    my $config = shift;
+    return sub {
+        my $env = shift;
+        set appdir      => $appdir;
+        set appname     => 'mineview-' . $config;
+        warn("CONFIG $config");
+        set environment => $config;
+        set views       => $view_dir;
+        set public      => 'public';
+
+        load_app "MineViewer";
+        Dancer::Config->load;
+        Dancer::Handler->init_request_headers($env);
+        my $request = Dancer::Request->new($env);
+        Dancer->dance($request);
+    };
+}
+my $mineview    = make_app('testing');
+my $flymineview = make_app('flymineview');
+my $neurogenes  = make_app('neurogenes');
+
+builder {
+    mount "/mineview"    => builder { $mineview };
+    mount "/neurogenes"  => builder { $neurogenes };
+    mount "/flymineview" => builder { $flymineview };
+};
