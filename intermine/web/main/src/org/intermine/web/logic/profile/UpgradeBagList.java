8f18095b61a46d9d6ed5759081afc789a6c8bdc6
danielabutano <daniela.butano@gmail.com>
2011-06-27 15:51:41 +0000

    Checkstyle
    Former-commit-id: 2d4e5429c6223a14fa10adb5494a381e30c964ec

--- intermine/web/main/src/org/intermine/web/logic/profile/UpgradeBagList.java
+++ intermine/web/main/src/org/intermine/web/logic/profile/UpgradeBagList.java
@@ -1,8 +1,17 @@
 package org.intermine.web.logic.profile;
 
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
 import java.util.List;
 import java.util.Map;
-
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
@@ -13,18 +22,8 @@ import org.intermine.api.profile.InterMineBag;
 import org.intermine.api.profile.Profile;
 import org.intermine.objectstore.ObjectStoreException;
 
-/*
- * Copyright (C) 2002-2010 FlyMine
- *
- * This code may be freely distributed and modified under the
- * terms of the GNU Lesser General Public Licence.  This should
- * be distributed with the code.  See the LICENSE file for more
- * information or http://www.gnu.org/copyleft/lesser.html.
- *
- */
-
 /**
- * Runnable object providing upgrading osbag_int table. 
+ * Runnable object providing upgrading osbag_int table.
  * @author dbutano
  *
  */
@@ -34,7 +33,6 @@ public class UpgradeBagList implements Runnable
     private Profile profile;
     private BagQueryRunner bagQueryRunner;
     private HttpSession session;
-    
 
     public UpgradeBagList(Profile profile, BagQueryRunner bagQueryRunner, HttpSession session) {
         this.profile = profile;
@@ -52,10 +50,10 @@ public class UpgradeBagList implements Runnable
                     BagQueryResult result = bagQueryRunner.searchForBag(bag.getType(),
                                             primaryIdentifiersList, "", false);
                     if (result.getIssues().isEmpty() && result.getUnresolved().isEmpty()) {
-                    	Map<Integer, List> matches = result.getMatches();
-                    	bag.upgradeOsb(matches.keySet());
+                        Map<Integer, List> matches = result.getMatches();
+                        bag.upgradeOsb(matches.keySet());
                     } else {
-                        	session.setAttribute("bagQueryResult_" + bag.getName(), result);
+                        session.setAttribute("bagQueryResult_" + bag.getName(), result);
                     }
                 } catch (ClassNotFoundException cnfe) {
                     LOG.warn("The type " + bag.getType() + "isn't in the model."
@@ -64,7 +62,7 @@ public class UpgradeBagList implements Runnable
                     LOG.warn("Impossible upgrade the bags list " + bag.getTitle(), ie);
                 } catch (ObjectStoreException ose) {
                     LOG.warn("Impossible upgrade the bags list", ose);
-                } 
+                }
             }
         }
     }
