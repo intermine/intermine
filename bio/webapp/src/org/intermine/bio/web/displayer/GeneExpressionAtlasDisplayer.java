17f2772e3138ccd160c021c901dc26de521fa8fd
radekstepan <dev@radekstepan.com>
2011-07-13 10:41:46 +0000

    GEAE Displayer case insensitive sorting and sidebar

--- bio/webapp/src/org/intermine/bio/web/displayer/GeneExpressionAtlasDisplayer.java
+++ bio/webapp/src/org/intermine/bio/web/displayer/GeneExpressionAtlasDisplayer.java
@@ -12,11 +12,13 @@ package org.intermine.bio.web.displayer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -26,7 +28,11 @@ import org.intermine.api.profile.Profile;
 import org.intermine.api.query.PathQueryExecutor;
 import org.intermine.api.results.ExportResultsIterator;
 import org.intermine.api.results.ResultElement;
+import org.intermine.bio.web.model.GeneExpressionAtlasExpressions;
 import org.intermine.bio.web.model.ProteinAtlasExpressions;
+import org.intermine.bio.web.model.ProteinAtlasExpressions.ByLevelComparator;
+import org.intermine.bio.web.model.ProteinAtlasExpressions.ExpressionList;
+import org.intermine.bio.web.model.ProteinAtlasExpressions.ExpressionType;
 import org.intermine.metadata.Model;
 import org.intermine.model.InterMineObject;
 import org.intermine.pathquery.Constraints;
@@ -40,6 +46,15 @@ import org.intermine.web.logic.session.SessionMethods;
 import org.intermine.model.bio.Gene;
 
 public class GeneExpressionAtlasDisplayer extends ReportDisplayer {
+
+    /** @var column keys we have in the results table */
+    private ArrayList<String> expressionColumns =  new ArrayList<String>() {{
+        add("condition");
+        add("expression");
+        add("pValue");
+        add("tStatistic");
+        add("type");
+    }};
 
     public GeneExpressionAtlasDisplayer(ReportDisplayerConfig config, InterMineAPI im) {
         super(config, im);
@@ -70,8 +85,11 @@ public class GeneExpressionAtlasDisplayer extends ReportDisplayer {
             PathQueryExecutor executor = im.getPathQueryExecutor(profile);
             ExportResultsIterator values = executor.execute(query);
 
+            // convert to a map
+            GeneExpressionAtlasExpressions geae = new GeneExpressionAtlasExpressions(values);
+
             // attach to results
-            request.setAttribute("expressions", values);
+            request.setAttribute("expressions", geae);
             request.setAttribute("url", "http://www.ebi.ac.uk/gxa/experiment/E-MTAB-62/"+genePrimaryID);
         }
     }
@@ -93,6 +111,8 @@ public class GeneExpressionAtlasDisplayer extends ReportDisplayer {
                 "Gene.atlasExpression.type",
                 "Gene.primaryIdentifier");
         query.addConstraint(Constraints.eq("Gene.primaryIdentifier", genePrimaryID));
+        query.addConstraint(Constraints.eq("Gene.atlasExpression.type", "organism_part"));
+        query.addOrderBy("Gene.atlasExpression.condition", OrderDirection.ASC);
 
         return query;
     }
