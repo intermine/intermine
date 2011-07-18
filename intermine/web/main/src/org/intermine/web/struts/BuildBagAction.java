dddf20a614c7cd3eb4c5960b80f63197257450bc
danielabutano <daniela.butano@gmail.com>
2011-07-18 09:07:27 +0000

    added a comment

--- intermine/web/main/src/org/intermine/web/struts/BuildBagAction.java
+++ intermine/web/main/src/org/intermine/web/struts/BuildBagAction.java
@@ -177,6 +177,7 @@ public class BuildBagAction extends InterMineAction
         session.setAttribute("bagQueryResult", bagQueryResult);
         request.setAttribute("bagType", type);
         request.setAttribute("bagExtraFilter", buildBagForm.getExtraFieldValue());
+        //buildNewBag used by jsp to set editable the bag name field
         request.setAttribute("buildNewBag", "true");
         return mapping.findForward("bagUploadConfirm");
     }
