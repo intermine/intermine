28e40d3d1e1e2f888a7778cdebefa6949436f8de
julie-sullivan <julie@flymine.org>
2011-08-05 13:50:34 +0000

    URL fix

--- bio/scripts/DataDownloader/lib/DataDownloader/Source/RGDIdentifiers.pm
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/RGDIdentifiers.pm
@@ -3,11 +3,13 @@ package DataDownloader::Source::RGDIdentifiers;
 use Moose;
 extends 'DataDownloader::Source::FtpBase';
 
+# ftp://rgd.mcw.edu/pub/data_release/GENES_RAT.txt
+
 use constant {
     TITLE => "RGD Identifiers",
     DESCRIPTION => "Identifiers from MCW",
-    SOURCE_LINK => "http://rgd.mcw.edu",
-    HOST => "rgd.mcw.edu",
+    SOURCE_LINK => "ftp://rgd.mcw.edu",
+    HOST => "ftp://rgd.mcw.edu",
     SOURCE_DIR => "rgd-identifiers",
     REMOTE_DIR => "pub/data_release",
     FILE => "GENES_RAT.txt", 
