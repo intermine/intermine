be0710fe41254db0683c58c3c5b332148c94ab9a
alexkalderimis <alex.kalderimis@gmail.com>
2011-08-08 14:01:57 +0000

    Adjusted the profile creation code
    Former-commit-id: 03016f527c54220e02e7ba099d2ed0837eb7784f

--- intermine/webtasks/main/src/org/intermine/web/task/LoadDefaultTemplatesTask.java
+++ intermine/webtasks/main/src/org/intermine/web/task/LoadDefaultTemplatesTask.java
@@ -130,7 +130,7 @@ public class LoadDefaultTemplatesTask extends Task
                 LOG.info("Creating profile for " + username);
                 profileDest = new Profile(pm, username, null, password,
                         new HashMap<String, SavedQuery>(), new HashMap<String, InterMineBag>(),
-                        new HashMap<String, TemplateQuery>(), null);
+                        new HashMap<String, TemplateQuery>(), null, true);
                 profileDest.disableSaving();
                 pm.createProfile(profileDest);
             } else {
