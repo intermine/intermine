4066367edbf050cce658ce40119946f372572eb5
alexkalderimis <alex.kalderimis@gmail.com>
2011-08-08 13:46:33 +0000

    Servlet to manage openid logins

---
+++ intermine/web/main/src/org/intermine/web/logic/login/OpenIDAuthenticator.java
@@ -0,0 +1,154 @@
+/**
+ *
+ */
+package org.intermine.web.logic.login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.expressme.openid.Association;
+import org.expressme.openid.Authentication;
+import org.expressme.openid.Endpoint;
+import org.expressme.openid.OpenIdException;
+import org.expressme.openid.OpenIdManager;
+import org.intermine.api.InterMineAPI;
+import org.intermine.api.profile.Profile;
+import org.intermine.api.profile.ProfileManager;
+import org.intermine.web.logic.Constants;
+import org.intermine.web.logic.profile.LoginHandler;
+import org.intermine.web.logic.session.SessionMethods;
+import org.openid4java.association.AssociationException;
+import org.openid4java.consumer.ConsumerException;
+import org.openid4java.consumer.ConsumerManager;
+import org.openid4java.consumer.VerificationResult;
+import org.openid4java.discovery.DiscoveryException;
+import org.openid4java.discovery.DiscoveryInformation;
+import org.openid4java.discovery.Identifier;
+import org.openid4java.message.AuthRequest;
+import org.openid4java.message.MessageException;
+import org.openid4java.message.ParameterList;
+
+/**
+ * Authenticate requests using OpenID
+ * @author Alexis Kalderimis
+ */
+public class OpenIDAuthenticator extends HttpServlet {
+
+	static final long ONE_HOUR = 3600000L;
+	static final long TWO_HOUR = ONE_HOUR * 2L;
+	static final String ATTR_MAC = "openid_mac";
+	static final String ATTR_ALIAS = "openid_alias";
+	static final String PARAM_NONCE = "openid.response_nonce";
+	static final String PARAM_PROVIDER = "provider";
+	static final Set<String> NONCES = new HashSet<String>();
+
+	private String loginUrl;
+	private String returnTo;
+	private OpenIdManager manager;
+	private ProfileManager profileManager;
+
+	/**
+	 * Default constructor.
+	 */
+	public OpenIDAuthenticator() {
+		// Empty Constructor
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		manager = new OpenIdManager();
+		ServletContext context = config.getServletContext();
+		Properties webProperties = SessionMethods.getWebProperties(context);
+		String prefix = webProperties.getProperty("project.sitePrefix");
+		String path = webProperties.getProperty("webapp.path");
+		manager.setRealm(prefix);
+		returnTo = prefix + "/" + path + "/openid";
+		manager.setReturnTo(returnTo);
+		profileManager = SessionMethods.getInterMineAPI(context).getProfileManager();
+		loginUrl = prefix + "/" + path + "/mymine.do";
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String provider = request.getParameter(PARAM_PROVIDER);
+
+		if (provider == null) {
+			checkNonce(request.getParameter(PARAM_NONCE));
+			byte[] mackey = (byte[]) request.getSession().getAttribute(ATTR_MAC);
+			String alias = (String) request.getSession().getAttribute(ATTR_ALIAS);
+			Authentication auth = manager.getAuthentication(request, mackey, alias);
+
+			String email = auth.getEmail();
+			String fullName = auth.getFullname();
+			HttpSession s = request.getSession();
+			if (fullName != null) {
+				s.setAttribute(Constants.USERNAME, fullName);
+			} else {
+				s.setAttribute(Constants.USERNAME, email);
+			}
+			String identity = auth.getIdentity();
+			if (!profileManager.hasProfile(identity)) {
+				profileManager.createProfile(new Profile(profileManager, identity, null, null, new HashMap(), new HashMap(),
+		                new HashMap(), null, false));
+			}
+			Map<String, String> renamedBags = LoginHandler.doStaticLogin(request, identity, null);
+			response.sendRedirect(loginUrl);
+		} else {
+			 // redirect to provider sign on page:
+            Endpoint endpoint = manager.lookupEndpoint(provider);
+			request.getSession().setAttribute(Constants.PROVIDER, provider);
+            Association association = manager.lookupAssociation(endpoint);
+            request.getSession().setAttribute(ATTR_MAC, association.getRawMacKey());
+            request.getSession().setAttribute(ATTR_ALIAS, endpoint.getAlias());
+            String url = manager.getAuthenticationUrl(endpoint, association);
+            response.sendRedirect(url);
+		}
+	}
+
+	void checkNonce(String nonce) {
+		if (nonce == null || nonce.length() < 20) {
+			throw new OpenIdException("Verify failed - bad nonce");
+		}
+		if (NONCES.contains(nonce)) {
+			throw new OpenIdException("Verify failed - nonce has previously been used");
+		}
+		long nonceTime = getNonceTime(nonce);
+		long diff = System.currentTimeMillis() - nonceTime;
+		if (diff < 0) {
+			diff = -diff;
+		}
+		if (diff > ONE_HOUR) {
+			throw new OpenIdException("Verify failed - expired nonce");
+		}
+		NONCES.add(nonce);
+	}
+
+	long getNonceTime(String nonce) {
+        try {
+            return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                    .parse(nonce.substring(0, 19) + "+0000")
+                    .getTime();
+        }
+        catch(ParseException e) {
+            throw new OpenIdException("Bad nonce time.");
+        }
+    }
+
+}
