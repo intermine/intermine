bd5d967c4b2033128a4875a50a4074db05c2da96
julie-sullivan <julie@flymine.org>
2011-08-11 15:18:26 +0000

    new NCBI source

---
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/NCBIGene.pm
@@ -0,0 +1,20 @@
+package DataDownloader::Source::NCBIGene;
+
+use Moose;
+extends 'DataDownloader::Source::FtpBase';
+
+# ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/Mammalia/Homo_sapiens.gene_info.gz
+
+use constant {
+    TITLE => "NCBI Gene ",
+    DESCRIPTION => "Genes from NCBI",
+    SOURCE_LINK => "ftp.ncbi.nih.gov",
+    SOURCE_DIR => "ncbi",
+    SOURCES => [{
+        FILE => "Homo_sapiens.gene_info.gz", 
+        HOST => "ftp.ncbi.nih.gov",
+        REMOTE_DIR => "gene/DATA/GENE_INFO/Mammalia",
+    }],
+};
+
+1;
