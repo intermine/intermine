0beed6a42c55310bf12b00fa85eec685016ae4f7
julie-sullivan <julie@flymine.org>
2011-08-12 09:27:05 +0000

    unzip file, delete translation and old ID files
    Former-commit-id: b7c9a1831c1bd1490d4a2945c86970098401723d

--- bio/scripts/DataDownloader/lib/DataDownloader/Source/AnophelesIdentifiers.pm
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/AnophelesIdentifiers.pm
@@ -1,27 +1,37 @@
 package DataDownloader::Source::AnophelesIdentifiers;
 
 use Moose;
-extends 'DataDownloader::Source::FtpBase';
-
-use XML::Parser::PerlSAX;
-use PerlIO::gzip;
-use Number::Format qw(format_bytes);
-
-use autodie qw(close open);
+extends 'DataDownloader::Source::ABC';
+use LWP;
 
 # ftp://ftp.vectorbase.org/public_data/organism_data/agambiae/Other/AgamP3.3-to-AgamP3.4-Identifiers.tgz
 
-
 use constant {
     TITLE => 'Anopheles Identifiers',
-    DESCRIPTION =>
-"Gene Stable ID mappings from geneset AgamP3.3 to AgamP3.4",
+    DESCRIPTION => "Gene Stable ID mappings from geneset AgamP3.3 to AgamP3.4",
     SOURCE_LINK => 'http://agambiae.vectorbase.org/GetData/Downloads/',
     SOURCE_DIR  => 'anopheles-identifiers',
-   SOURCES => [{
-        FILE => "AgamP3.3-to-AgamP3.4-Identifiers.tgz", 
-        HOST        => "ftp.vectorbase.org",
-        REMOTE_DIR  => "public_data/organism_data/agambiae/Other",
-    }],
 };
+
+sub BUILD {
+    my $self = shift;
+    my @files_to_extract = {"New_IDs_to_Old_IDs-Genes.tsv", "New_IDs_to_Old_IDs-Transcripts.tsv"};
+
+    $self->set_sources([
+        {
+            SERVER => 'ftp://ftp.vectorbase.org/public_data/organism_data/agambiae/Other',
+            FILE => 'AgamP3.3-to-AgamP3.4-Identifiers.tgz',
+
+            CLEANER => sub {
+                my $self = shift;
+                my $file = $self->get_destination;
+                my @args = ('unzip', $file, @files_to_extract, '-d', 
+                    $self->get_destination_dir);
+                $self->execute_system_command(@args);
+                $self->debug("Removing original file: $file");
+                unlink($file);
+            },
+        },
+    ]);
+}
 
