da7faa8d58611854f69b9ba583a8833668c7f1be
julie-sullivan <julie@flymine.org>
2011-08-12 07:53:56 +0000

    update location of files for modmine, new config file
    Former-commit-id: aaca270a73449d2c0b961b126fcdbafcb14aebd2

--- bio/scripts/DataDownloader/lib/DataDownloader/Source/FlyAnatomyOntology.pm
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/FlyAnatomyOntology.pm
@@ -7,7 +7,7 @@ use constant {
     TITLE  => 'Fly Anatomy Ontology',
     DESCRIPTION => "Drosophila Anatomy ontology from FlyBase",
     SOURCE_LINK => "http://www.flybase.net/",
-    SOURCE_DIR => 'ontologies/fly-anatomy',
+    SOURCE_DIR => 'ontologies',
     SOURCES => [{
         FILE   => 'fly_anatomy.obo',
         SERVER => 'http://obo.cvs.sourceforge.net/*checkout*/obo/obo/ontology/anatomy/gross_anatomy/animal_gross_anatomy/fly',
