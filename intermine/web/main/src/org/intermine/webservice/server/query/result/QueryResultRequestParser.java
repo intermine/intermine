16c90e827ad4110e2ba738d97d84136729cc10c8
alexkalderimis <alex.kalderimis@gmail.com>
2011-08-25 14:51:36 +0000

    Moved query xml handling to more general class

--- intermine/web/main/src/org/intermine/webservice/server/query/result/QueryResultRequestParser.java
+++ intermine/web/main/src/org/intermine/webservice/server/query/result/QueryResultRequestParser.java
@@ -17,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.intermine.webservice.server.exceptions.BadRequestException;
+import org.intermine.webservice.server.query.QueryRequestParser;
 
 /**
  * Processes service request. Evaluates parameters and validates them and check if
@@ -24,7 +25,7 @@ import org.intermine.webservice.server.exceptions.BadRequestException;
  *
  * @author Jakub Kulaviak
  **/
-public class QueryResultRequestParser extends WebServiceRequestParser
+public class QueryResultRequestParser extends QueryRequestParser
 {
     /** Name of parameter with query **/
     public static final String QUERY_PARAMETER = "query";
@@ -37,14 +38,12 @@ public class QueryResultRequestParser extends WebServiceRequestParser
     
     private static final Logger logger = Logger.getLogger(QueryResultRequestParser.class);
 
-    private HttpServletRequest request;
-
     /**
      * RequestProcessor constructor.
      * @param request request
      */
     public QueryResultRequestParser(HttpServletRequest request) {
-        this.request = request;
+        super(request);
     }
 
     /**
@@ -58,70 +57,10 @@ public class QueryResultRequestParser extends WebServiceRequestParser
         return input;
     }
 
-    /**
-     * Function for dealing with encoding issues with various
-     * inputs.
-     */
-    public static String fixEncoding(String latin1) {
-        try {
-            byte[] bytes = latin1.getBytes("ISO-8859-1");
-            if (!validUTF8(bytes))
-                return latin1;   
-            return new String(bytes, "UTF-8");  
-        } catch (UnsupportedEncodingException e) {
-            // Impossible, throw unchecked
-            throw new IllegalStateException("No Latin1 or UTF-8: " + e.getMessage());
-        }
-
-    }
-
-    public static boolean validUTF8(byte[] input) {
-        int i = 0;
-        // Check for BOM
-        if (input.length >= 3 && (input[0] & 0xFF) == 0xEF
-                && (input[1] & 0xFF) == 0xBB & (input[2] & 0xFF) == 0xBF) {
-            i = 3;
-                }
-
-        int end;
-        for (int j = input.length; i < j; ++i) {
-            int octet = input[i];
-            if ((octet & 0x80) == 0) {
-                continue; // ASCII
-            }
-
-            // Check for UTF-8 leading byte
-            if ((octet & 0xE0) == 0xC0) {
-                end = i + 1;
-            } else if ((octet & 0xF0) == 0xE0) {
-                end = i + 2;
-            } else if ((octet & 0xF8) == 0xF0) {
-                end = i + 3;
-            } else {
-                // Java only supports BMP so 3 is max
-                return false;
-            }
-
-            while (i < end) {
-                i++;
-                octet = input[i];
-                if ((octet & 0xC0) != 0x80) {
-                    // Not a valid trailing byte
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
     private void parseRequest(HttpServletRequest req, QueryResultInput input) {
 
         super.parseRequest(req, input);
-
-        String xmlQuery = req.getParameter(QUERY_PARAMETER);
-        logger.debug(xmlQuery);
-        xmlQuery = fixEncoding(xmlQuery);
-        logger.debug(xmlQuery);
+        String xmlQuery = getQueryXml(req);
         if (StringUtils.isEmpty(xmlQuery)) {
             throw new BadRequestException("invalid " + QUERY_PARAMETER
                     + " parameter (empty or missing)");
