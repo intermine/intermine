d172f3cba8a07780d5f58c8071a241dbc7c90b6c
alexkalderimis <alex.kalderimis@gmail.com>
2011-09-20 16:43:26 +0000

    An exception for parsing regions
    Former-commit-id: 13f5862f18e7ecd9a96c41b6f981f519fe3032ae

---
+++ bio/webapp/src/org/intermine/bio/web/model/RegionParseException.java
@@ -0,0 +1,14 @@
+package org.intermine.bio.web.model;
+
+public class RegionParseException extends Exception {
+
+    public RegionParseException(String message) {
+        super(message);
+    }
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+}
