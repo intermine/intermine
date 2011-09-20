b644b950430321eb67d6c96db8b5e5e3375bc99a
radekstepan <dev@radekstepan.com>
2011-09-20 17:27:19 +0000

    mine coloring in a displayer
    Former-commit-id: e3f39c0b33fe8962ba2a7cbd979d099265d99109

--- bio/webapp/src/org/intermine/bio/web/displayer/MinePathwaysDisplayer.java
+++ bio/webapp/src/org/intermine/bio/web/displayer/MinePathwaysDisplayer.java
@@ -12,9 +12,12 @@ package org.intermine.bio.web.displayer;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
@@ -32,11 +35,13 @@ import org.intermine.model.bio.Gene;
 import org.intermine.pathquery.Constraints;
 import org.intermine.pathquery.OrderDirection;
 import org.intermine.pathquery.PathQuery;
+import org.intermine.util.PropertiesUtil;
 import org.intermine.util.StringUtil;
 import org.intermine.util.Util;
 import org.intermine.web.displayer.ReportDisplayer;
 import org.intermine.web.logic.config.ReportDisplayerConfig;
 import org.intermine.web.logic.results.ReportObject;
+import org.intermine.web.logic.session.SessionMethods;
 
 /**
  * For all friendly mines, query for pathways
@@ -68,6 +73,7 @@ public class MinePathwaysDisplayer extends ReportDisplayer
         Collection<Mine> mines = linkManager.getFriendlyMines();
         Map<Mine, String> mineToOrthologues = buildHomologueMap(mines, orthologues);
         request.setAttribute("mines", mineToOrthologues);
+        request.setAttribute("minePortals", getMinesSettings(request));
     }
 
     /* Using the provided list of organisms available in this mine, build list of genes to query
@@ -99,6 +105,47 @@ public class MinePathwaysDisplayer extends ReportDisplayer
         return mineToOrthologues;
     }
 
+    /**
+     * Generate mines settings (colors) from Properties
+     * @param request
+     * @return
+     */
+    private HashMap<String, LinkedHashMap<String, String>> getMinesSettings(HttpServletRequest request) {
+    	Properties webProperties = SessionMethods.getWebProperties(request.getSession()
+                .getServletContext());
+        String localMineName = webProperties.getProperty("project.title");
+        Properties props = PropertiesUtil.stripStart("intermines",
+                PropertiesUtil.getPropertiesStartingWith("intermines", webProperties));
+        Enumeration<?> propNames = props.propertyNames();
+        HashMap<String, LinkedHashMap<String, String>> minePortals =
+                new HashMap<String, LinkedHashMap<String, String>>();
+        while (propNames.hasMoreElements()) {
+            String mineId = (String) propNames.nextElement();
+            mineId = mineId.substring(0, mineId.indexOf("."));
+            Properties mineProps = PropertiesUtil.stripStart(mineId,
+                    PropertiesUtil.getPropertiesStartingWith(mineId, props));
+
+            // get name and url
+            String mineName = mineProps.getProperty("name");
+            String mineURL = mineProps.getProperty("url");
+            if (StringUtils.isNotEmpty(mineName) && StringUtils.isNotEmpty(mineURL)
+                    && !mineName.equals(localMineName)) {
+                LinkedHashMap<String, String> mineDetails = new LinkedHashMap<String, String>();
+                // colors for the mines
+                String mineBgColor = mineProps.getProperty("bgcolor");
+                String mineFrontColor = mineProps.getProperty("frontcolor");
+                if (StringUtils.isNotEmpty(mineBgColor)
+                        && StringUtils.isNotEmpty(mineFrontColor)) {
+                    mineDetails.put("bgcolor", mineBgColor);
+                    mineDetails.put("frontcolor", mineFrontColor);
+                }
+                mineDetails.put("url", mineURL);
+                minePortals.put(mineName, mineDetails);
+            }
+        }
+        return minePortals;
+    }
+    
     private PathQuery getQuery(Gene gene) {
         PathQuery q = new PathQuery(im.getModel());
         q.addViews("Gene.homologues.homologue.primaryIdentifier",
