4a18d0f038655e536be167834cb148bfb28c7c11
danielabutano <daniela.butano@gmail.com>
2011-09-20 12:18:37 +0000

    The UpgradingList thread  will automatically upgrade lists with only OTHER issues, containing ids already existing in the list.

--- intermine/web/main/src/org/intermine/web/struts/BagUploadConfirmController.java
+++ intermine/web/main/src/org/intermine/web/struts/BagUploadConfirmController.java
@@ -11,10 +11,13 @@ package org.intermine.web.struts;
  */
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -66,15 +69,34 @@ public class BagUploadConfirmController extends TilesAction
 
         // get all of the "low quality" matches ie. those found by queries other than matching
         // class keys
-        Map<String, ArrayList<String>> lowQualityMatches = new LinkedHashMap<String,
-            ArrayList<String>>();
+        Map<String, ArrayList<Object>> lowQualityMatches = new LinkedHashMap<String,
+            ArrayList<Object>>();
         Map<String, Map<String, List>> otherMatchMap = bagQueryResult.getIssues()
             .get(BagQueryResult.OTHER);
+        Set<Integer> matchesIds = bagQueryResult.getMatches().keySet();
         if (otherMatchMap != null) {
             Iterator otherMatchesIter = otherMatchMap.values().iterator();
             while (otherMatchesIter.hasNext()) {
-                Map<String, ArrayList<String>> inputToObjectsMap = (Map) otherMatchesIter.next();
-                lowQualityMatches.putAll(inputToObjectsMap);
+                Map<String, ArrayList<Object>> inputToObjectsMap = (Map) otherMatchesIter.next();
+                //before adding inputToObjectsMap to lowQualityMatches
+                //we removed all object having the id already contained in the matchesIds
+                Map<String, ArrayList<Object>> inputToObjectsMapUpdated = new LinkedHashMap<String, ArrayList<Object>>();
+                for (String key : inputToObjectsMap.keySet()) {
+                    ArrayList<Object> listObjects = inputToObjectsMap.get(key);
+                    ArrayList<Object> listObjectsUpdated = new ArrayList<Object>();
+                    for (Object obj : listObjects) {
+                        InterMineObject intermineObj= (InterMineObject) obj;
+                        if (matchesIds.isEmpty() || !matchesIds.contains(intermineObj.getId())) {
+                            listObjectsUpdated.add(obj);
+                        }
+                    }
+                    if (!listObjectsUpdated.isEmpty()) {
+                        inputToObjectsMapUpdated.put(key, listObjects);
+                    }
+                }
+                if (!inputToObjectsMapUpdated.isEmpty()) {
+                    lowQualityMatches.putAll(inputToObjectsMapUpdated);
+                }
             }
         }
         request.setAttribute("lowQualityMatches", lowQualityMatches);
@@ -83,13 +105,13 @@ public class BagUploadConfirmController extends TilesAction
         request.setAttribute("flatLowQualityMatches", flatLowQualityMatches);
 
         // find all input strings that match more than one object
-        Map<String, ArrayList<String>> duplicates = new LinkedHashMap<String, ArrayList<String>>();
+        Map<String, ArrayList<Object>> duplicates = new LinkedHashMap<String, ArrayList<Object>>();
         Map<String, Map<String, List>> duplicateMap = bagQueryResult.getIssues()
             .get(BagQueryResult.DUPLICATE);
         if (duplicateMap != null) {
             Iterator duplicateMapIter = duplicateMap.values().iterator();
             while (duplicateMapIter.hasNext()) {
-                Map<String, ArrayList<String>> inputToObjectsMap = (Map) duplicateMapIter.next();
+                Map<String, ArrayList<Object>> inputToObjectsMap = (Map) duplicateMapIter.next();
                 duplicates.putAll(inputToObjectsMap);
             }
         }
@@ -99,14 +121,14 @@ public class BagUploadConfirmController extends TilesAction
         request.setAttribute("flatDuplicate", flatDuplicate);
 
         // make a List of [input string, ConvertedObjectPair]
-        Map<String, ArrayList<String>> convertedObjects
-            = new LinkedHashMap<String, ArrayList<String>>();
+        Map<String, ArrayList<Object>> convertedObjects
+            = new LinkedHashMap<String, ArrayList<Object>>();
         Map<String, Map<String, List>> convertedMap = bagQueryResult.getIssues()
             .get(BagQueryResult.TYPE_CONVERTED);
         if (convertedMap != null) {
             Iterator convertedMapIter = convertedMap.values().iterator();
             while (convertedMapIter.hasNext()) {
-                Map<String, ArrayList<String>> inputToObjectsMap = (Map) convertedMapIter.next();
+                Map<String, ArrayList<Object>> inputToObjectsMap = (Map) convertedMapIter.next();
                 convertedObjects.putAll(inputToObjectsMap);
             }
         }
@@ -154,11 +176,11 @@ public class BagUploadConfirmController extends TilesAction
 
     // takes all the issues and puts them in a flattened array that the javascript can use for
     // the "add all" and "remove all" buttons
-    private String setJSArray(Map<String, ArrayList<String>> issues, String issueType) {
+    private String setJSArray(Map<String, ArrayList<Object>> issues, String issueType) {
 
         StringBuffer sb = new StringBuffer();
-        Map<String, ArrayList<String>> orderedIssuesMap
-            = new LinkedHashMap<String, ArrayList<String>>(issues);
+        Map<String, ArrayList<Object>> orderedIssuesMap
+            = new LinkedHashMap<String, ArrayList<Object>>(issues);
 
         // Make a Map from identifier to a List of rows for display.  Each row will contain
         // information about one object.  The row List will contain (first) the class name, then
