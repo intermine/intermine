d2fe038efe047cec92d1730f746cc01e096755cc
danielabutano <daniela.butano@gmail.com>
2011-09-21 11:50:31 +0000

    Added a new task to update bags, sabedbas, templatequery in the userprofile when the model has changed

---
+++ intermine/webtasks/main/src/org/intermine/web/task/ModelUpdateTask.java
@@ -0,0 +1,59 @@
+package org.intermine.web.task;
+
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.intermine.objectstore.ObjectStore;
+import org.intermine.objectstore.ObjectStoreFactory;
+import org.intermine.objectstore.ObjectStoreWriter;
+import org.intermine.objectstore.ObjectStoreWriterFactory;
+import org.intermine.pathquery.PathException;
+import org.intermine.web.ModelUpdate;
+
+public class ModelUpdateTask extends Task {
+    private String osAlias;
+    private String userProfileAlias;
+    private ObjectStore os = null;
+    private ObjectStoreWriter uosw = null;
+
+    /**
+     * Set the alias of the main object store.
+     * @param osAlias the object store alias
+     */
+    public void setOSAlias(String osAlias) {
+        this.osAlias = osAlias;
+    }
+
+    /**
+     * Set the alias of the userprofile object store.
+     * @param userProfileAlias the object store alias of the userprofile database
+     */
+    public void setUserProfileAlias(String userProfileAlias) {
+        this.userProfileAlias = userProfileAlias;
+    }
+
+    public void execute() {
+        try {
+            os = ObjectStoreFactory.getObjectStore(osAlias);
+            uosw = ObjectStoreWriterFactory.getObjectStoreWriter(userProfileAlias);
+        } catch (Exception e) {
+            throw new BuildException("Exception while creating ObjectStore", e);
+        }
+
+        ModelUpdate modelUpdate = new ModelUpdate(os, uosw);
+        try {
+            modelUpdate.update();
+        } catch (PathException pe) {
+            throw new BuildException("Exception while updating", pe);
+        }
+    }
+
+}
