b6e6fec951fc710cd3c5910b041e5155b22f79f1
julie-sullivan <julie@flymine.org>
2011-09-21 10:38:08 +0000

    rat disease displayer
    Former-commit-id: a32ed0181c43862076e9eef7a64e17a56a7e5e99

---
+++ bio/webapp/resources/webapp/model/diseaseDisplayer.jsp
@@ -0,0 +1,67 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html" %>
+<%@ taglib uri="/WEB-INF/struts-tiles.tld" prefix="tiles" %>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="im" %>
+<%@ taglib uri="http://jakarta.apache.org/taglibs/string-1.1" prefix="str" %>
+
+
+<!-- diseaseDisplayer.jsp -->
+
+<script type="text/javascript" charset="utf-8">
+function getRatDiseases(orthologues) {
+
+    AjaxServices.getRatDiseases(orthologues, function(diseases) {
+        var jSONObject = jQuery.parseJSON(diseases);
+        // switch off loading img
+        jQuery('#intermine_rat_disease').toggleClass('loading');
+        if (jSONObject && jSONObject['results'].length > 0) {
+            generate(jSONObject, "#intermine_rat_disease");
+        } else {
+            jQuery("#intermine_rat_disease").html("No diseases found.");
+        }
+    });
+}
+
+function generate(jSONObject, target) {
+          var url = '';
+          im.log(jSONObject);
+          if (jSONObject['mineURL'] != undefined) {
+            url = jSONObject['mineURL'];
+          }
+
+          if (jSONObject['results'] != undefined) {
+             jQuery.each(jSONObject['results'], function(index, pathway) {
+                  jQuery('<li/>', {
+                    'html': jQuery('<a/>', {
+                    'href': url + "/report.do?id=" + pathway['id'],
+                    'text': pathway['name'],
+                    'target': '_blank'
+                })
+                }).appendTo(target);
+             });
+          }
+}
+
+</script>
+
+<div id="mine-rat-disease" class="collection-table">
+<h3>Diseases (from RatMine)</h3>
+
+    <!-- one column for each mine -->
+    <table>
+      <tbody>
+      <tr>
+        <td>
+            <div id="intermine_rat_disease" class="loading"></div>
+            <script type="text/javascript" charset="utf-8">
+                getRatDiseases('${ratGenes}');
+            </script>
+          </td>
+      </tr>
+      </tbody>
+    </table>
+
+</div>
+<!-- /diseaseDisplayer.jsp -->
---
+++ bio/webapp/src/org/intermine/bio/web/displayer/DiseaseDisplayer.java
@@ -0,0 +1,94 @@
+package org.intermine.bio.web.displayer;
+
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.intermine.api.InterMineAPI;
+import org.intermine.api.profile.ProfileManager;
+import org.intermine.api.query.PathQueryExecutor;
+import org.intermine.api.results.ExportResultsIterator;
+import org.intermine.api.results.ResultElement;
+import org.intermine.model.bio.Gene;
+import org.intermine.pathquery.Constraints;
+import org.intermine.pathquery.PathQuery;
+import org.intermine.util.StringUtil;
+import org.intermine.util.Util;
+import org.intermine.web.displayer.ReportDisplayer;
+import org.intermine.web.logic.config.ReportDisplayerConfig;
+import org.intermine.web.logic.results.ReportObject;
+
+/**
+ * For ratmine, display all disease ontology objects for orthologues
+ *
+ * @author Julie Sullivan
+ */
+public class DiseaseDisplayer extends ReportDisplayer
+{
+    private static final String RAT = "R. norvegicus";
+
+    protected static final Logger LOG = Logger.getLogger(DiseaseDisplayer.class);
+
+    /**
+     * Construct with config and the InterMineAPI.
+     *
+     * @param config to describe the report displayer
+     * @param im the InterMine API
+     */
+    public DiseaseDisplayer(ReportDisplayerConfig config, InterMineAPI im) {
+        super(config, im);
+    }
+
+    @Override
+    public void display(HttpServletRequest request, ReportObject reportObject) {
+        Gene gene = (Gene) reportObject.getObject();
+        Set<String> orthologues = getLocalHomologues(gene);
+        request.setAttribute("ratGenes", StringUtil.join(orthologues, ","));
+    }
+
+    private PathQuery getQuery(Gene gene) {
+        PathQuery q = new PathQuery(im.getModel());
+        q.addViews("Gene.homologues.homologue.primaryIdentifier",
+                "Gene.homologues.homologue.secondaryIdentifier");
+        q.addConstraint(Constraints.eq("Gene.primaryIdentifier", gene.getPrimaryIdentifier()));
+        q.addConstraint(Constraints.eq("Gene.homologues.homologue.organism.shortName", RAT));
+        return q;
+    }
+
+    private Set<String> getLocalHomologues(Gene gene) {
+        Set<String> orthologues = new HashSet<String>();
+        ProfileManager profileManager = im.getProfileManager();
+        PathQueryExecutor executor = im.getPathQueryExecutor(profileManager.getSuperuserProfile());
+        PathQuery q = getQuery(gene);
+        if (!q.isValid()) {
+            return Collections.emptySet();
+        }
+        ExportResultsIterator it = executor.execute(q);
+        while (it.hasNext()) {
+            List<ResultElement> row = it.next();
+            String identifier = (String) row.get(0).getField();
+            String secondaryIdentifier = (String) row.get(1).getField();
+            if (!StringUtils.isEmpty(identifier)) {
+                orthologues.add(identifier);
+            } else if (!StringUtils.isEmpty(secondaryIdentifier)) {
+                orthologues.add(secondaryIdentifier);
+            }
+        }
+        return orthologues;
+    }
+}
