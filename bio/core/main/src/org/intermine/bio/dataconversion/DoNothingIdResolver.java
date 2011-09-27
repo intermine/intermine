3276cf1bf9b6bd1c69879fe5fbbe4d59937798ea
rnsmith <rns>
2011-09-27 15:24:15 +0000

    Add EnsemblIdResolver to Compara source, add DoNothingIdResolver for simpler testing.
    Former-commit-id: 490790b81437deebe8d19913a32d86c1f8115ab6

---
+++ bio/core/main/src/org/intermine/bio/dataconversion/DoNothingIdResolver.java
@@ -0,0 +1,43 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * An IdResolver for use in tests that will always resolve an id to the input id given.
+ *
+ * @author Richard Smith
+ *
+ */
+public class DoNothingIdResolver extends IdResolver
+{
+    /**
+     * Construct and empty IdResolver
+     * @param clsName the class to resolve identifiers for
+     */
+    public DoNothingIdResolver(String clsName) {
+        super(clsName);
+    }
+
+    @Override
+    public Set<String> resolveId(String taxonId, String id) {
+        Set<String> ids = new HashSet<String>();
+        ids.add(id);
+        return ids;
+    }
+
+    @Override
+    public int countResolutions(String taxonId, String id) {
+        return 1;
+    }
+}
---
+++ bio/core/main/src/org/intermine/bio/dataconversion/DoNothingIdResolverFactory.java
@@ -0,0 +1,36 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+/**
+ * Construct a DoNothingIdResolver that will resolve every id to the id that was given as input.
+ *
+ * @author Richard Smith
+ */
+public class DoNothingIdResolverFactory extends IdResolverFactory
+{
+
+    private IdResolver resolver = null;
+
+    /**
+     * Construct with class name for mock IdResolver
+     * @param clsName the type to resolve
+     */
+    public DoNothingIdResolverFactory(String clsName) {
+        resolver = new DoNothingIdResolver(clsName);
+    }
+
+    @Override
+    protected IdResolver createIdResolver() {
+        return resolver;
+    }
+
+}
