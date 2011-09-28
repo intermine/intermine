29a8e1b3118a18c005e8e98a0c8413df7ea374b4
boboppie <fengyuan.huu@gmail.com>
2011-09-28 17:28:50 +0000

    fixed a sequence export bug - when SequenceFeature or Protein is not root class of a path query, the getOrganism method failed
    Former-commit-id: 2be6036e9f82e32de9f055e6d125bca91dd1e213

--- bio/webapp/src/org/intermine/bio/web/logic/SequenceFeatureExportUtil.java
+++ bio/webapp/src/org/intermine/bio/web/logic/SequenceFeatureExportUtil.java
@@ -10,9 +10,14 @@ package org.intermine.bio.web.logic;
  *
  */
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.servlet.http.HttpSession;
@@ -29,8 +34,10 @@ import org.intermine.model.bio.Protein;
 import org.intermine.model.bio.SequenceFeature;
 import org.intermine.objectstore.query.ResultsRow;
 import org.intermine.pathquery.Path;
+import org.intermine.pathquery.PathException;
 import org.intermine.pathquery.PathQuery;
 import org.intermine.util.DynamicUtil;
+import org.intermine.util.StringUtil;
 import org.intermine.web.logic.results.PagedTable;
 import org.intermine.web.logic.session.SessionMethods;
 
@@ -45,7 +52,7 @@ public final class SequenceFeatureExportUtil
     private static final Logger LOG = Logger.getLogger(SequenceFeatureExportUtil.class);
 
     private static final Object ERROR_MSG = "Error happened during fetching organism information"
-        + " for LocatedSequenceFeature exporting.";
+        + " for SequenceFeature export.";
 
     private SequenceFeatureExportUtil() {
         super();
@@ -97,17 +104,19 @@ public final class SequenceFeatureExportUtil
     public static Set<String> getOrganisms(PathQuery pathQuery, HttpSession session)
         throws Exception {
 
-        try {
-            final InterMineAPI im = SessionMethods.getInterMineAPI(session);
-            Profile profile = SessionMethods.getProfile(session);
-            WebResultsExecutor webResultsExecutor = im.getWebResultsExecutor(profile);
+        final InterMineAPI im = SessionMethods.getInterMineAPI(session);
+        Profile profile = SessionMethods.getProfile(session);
+        WebResultsExecutor webResultsExecutor = im.getWebResultsExecutor(profile);
+        Model model = pathQuery.getModel();
 
-            String summaryPath = pathQuery.getRootClass() + ".organism.shortName";
+        try {
+            Entry<String, Class<?>> prefixEntry = getPathPrefix(pathQuery);
+
+            String summaryPath = prefixEntry.getKey() + ".organism.shortName";
 
             // BioEntity has organism as reference, SequenceFeature is subclass of BioEntity
-            Model model = pathQuery.getModel();
             Set<String> superClassNames = model.getClassDescriptorByName(
-                    pathQuery.getRootClass()).getAllSuperclassNames();
+                    prefixEntry.getValue().getName()).getAllSuperclassNames();
 
             int i = 0;
             for (String n : superClassNames) {
@@ -135,12 +144,82 @@ public final class SequenceFeatureExportUtil
             }
 
             pathQuery.removeView(summaryPath);
+
             return orgSet;
 
         } catch (RuntimeException e) {
             processException(e);
             return null;
         }
+    }
+
+    /**
+     * @param path the path to parse
+     * @return a path with out any [] constraints
+     */
+    private static String pathWithNoConstraints(String path) {
+        StringBuffer sb = new StringBuffer(path.length());
+        String[] queryBits = path.split("\\.");
+        for (int i = 0; i < queryBits.length; i++) {
+            String refName = queryBits[i];
+            if (refName.indexOf('[') > 0) {
+                refName = refName.substring(0, refName.indexOf('['));
+            }
+            if (sb.length() > 0) {
+                sb.append(".");
+            }
+            sb.append(refName);
+        }
+        return sb.toString();
+    }
+
+    private static Entry<String, Class<?>> getPathPrefix(PathQuery pathQuery)
+        throws PathException, ClassNotFoundException {
+
+        Model model = pathQuery.getModel();
+        String rootClsName = pathQuery.getRootClass();
+        ClassDescriptor rootCld = model.getClassDescriptorByName(rootClsName);
+        String rooClsFullName = rootCld.getName();
+        Class<?> rootCls = Class.forName(rooClsFullName);
+
+        if (SequenceFeature.class.isAssignableFrom(rootCls)
+                || Protein.class.isAssignableFrom(rootCls)) {
+            return new AbstractMap.SimpleEntry<String, Class<?>>(rootClsName, rootCls);
+        } else {
+            List<String> views = pathQuery.getView();
+            Map<String, Class<?>> pathClsMap = new HashMap<String, Class<?>>();
+            for (String view : views) {
+                view = pathWithNoConstraints(view);
+                String[] queryBits = view.split("\\.");
+                for (int i = 1; i < queryBits.length; i++) {
+                    // e.g. Pathway.genes.name > genes.name
+                    if (rootCld.getCollectionDescriptorByName(queryBits[i]) != null
+                            || rootCld.getReferenceDescriptorByName(queryBits[i]) != null) {
+                        String eleClsName = rootCld
+                                .getCollectionDescriptorByName(queryBits[i]) != null ? rootCld
+                                .getCollectionDescriptorByName(queryBits[i])
+                                    .getReferencedClassName() : rootCld
+                                    .getReferenceDescriptorByName(queryBits[i])
+                                        .getReferencedClassName();
+                        Class<?> eleCls = Class.forName(eleClsName);
+
+                        String trace = StringUtil.join(Arrays.asList(Arrays
+                                .copyOfRange(queryBits, 0, i + 1)), ".");
+
+                        pathClsMap.put(trace, eleCls);
+                    }
+                }
+            }
+
+            for (Entry<String, Class<?>> e: pathClsMap.entrySet()) {
+                if (SequenceFeature.class.isAssignableFrom(e.getValue())
+                        || Protein.class.isAssignableFrom(e.getValue())) {
+                    return e;
+                }
+            }
+        }
+
+        return null;
     }
 
     private static void processException(Exception e) {
