aa55bd08fedc4c3b05e875234b046c8e3c834932
julie-sullivan <julie@flymine.org>
2011-10-12 12:43:11 +0000

    put back resolver, I think I may have guessed which file is being used
    Former-commit-id: 2f7183e0e7d357f16212de37a38462086b3ad227

--- bio/sources/ensembl-compara/main/src/org/intermine/bio/dataconversion/EnsemblComparaConverter.java
+++ bio/sources/ensembl-compara/main/src/org/intermine/bio/dataconversion/EnsemblComparaConverter.java
@@ -62,9 +62,7 @@ public class EnsemblComparaConverter extends BioFileConverter
         super(writer, model, DATA_SOURCE_NAME, DATASET_TITLE);
         readConfig();
         flyResolverFactory = new FlyBaseIdResolverFactory("gene");
-        // TODO temporarily disable until I figure out what the filename is
-        //humanResolverFactory = new EnsemblIdResolverFactory();
-        humanResolverFactory = null;
+        humanResolverFactory = new EnsemblIdResolverFactory();
     }
 
     /**
