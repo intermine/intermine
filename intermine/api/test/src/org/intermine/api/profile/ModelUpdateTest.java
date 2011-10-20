2f07f1ec96799707034187e5c359b8068be6a2a1
danielabutano <daniela.butano@gmail.com>
2011-10-20 14:02:42 +0000

    Removed some commented code
    Former-commit-id: c691d8af63515acf438764040effec837ed344e7

--- intermine/api/test/src/org/intermine/api/profile/ModelUpdateTest.java
+++ intermine/api/test/src/org/intermine/api/profile/ModelUpdateTest.java
@@ -40,7 +40,7 @@ public class ModelUpdateTest extends InterMineAPITestCase {
     public void setUp() throws Exception {
         super.setUp();
         try {
-            modelUpdate = new ModelUpdate(os, uosw, "oldtestmodel");
+            modelUpdate = new ModelUpdate(os, uosw);
         } catch (BuildException be) {
             tearDown();
             throw be;
@@ -50,20 +50,6 @@ public class ModelUpdateTest extends InterMineAPITestCase {
     public void tearDown() throws Exception {
         super.tearDown();
     }
-
-/*    private void setUpUserProfiles() throws Exception {
-        //
-        MokaIntermineBag bag1 = new MokaInterMineBag("bag1", "Department", "This is some description",
-                date, BagState.CURRENT, os, testUser.getUserId(), uosw);
-        testUser.savedBags.put("bag1", bag1);
-        testUser.createBag("bag2", "CEOTest", "description bag2", im.getClassKeys(), false);
-        testUser.createBag("bag3", "Address", "description bag3", im.getClassKeys(), false);
-
-        TemplateQuery template =
-            new TemplateQuery("template", "ttitle", "tcomment",
-                              new PathQuery(Model.getInstanceByName("oldtestmodel")));
-        testUser.saveTemplate("template", template);
-    }*/
 
     public void testConstructor() throws Exception {
         assertEquals(1, modelUpdate.getDeletedClasses().size());
@@ -79,64 +65,4 @@ public class ModelUpdateTest extends InterMineAPITestCase {
         assertEquals(true, modelUpdate.getRenamedFields().containsKey("Company.CEOTest"));
         assertEquals("CEO", modelUpdate.getRenamedFields().get("Company.CEOTest"));
     }
-    /*
-    public void testDeleteBags() throws Exception {
-        setUpUserProfiles();
-        assertEquals(3, testUser.getSavedBags().size());
-        modelUpdate.deleteBags();
-        assertEquals(2, testUser.getSavedBags().size());
-    }
-
-    public void testUpdateTypeBag() throws Exception {
-        setUpUserProfiles();
-        InterMineBag bag2 = testUser.getSavedBags().get("bag2");
-        assertEquals("CEOTest", bag2.getType());
-        modelUpdate.updateTypeBag();
-        assertEquals("CEO", bag2.getType());
-    }
-*/
-/*
-    public void testUpdateReferredQueryAndTemplate() throws Exception {
-        //create savedquery
-        PathQuery query = new PathQuery(Model.getInstanceByName("oldtestmodel"));
-        query.addView("CEOTest.name");
-        query.addView("CEOTest.title");
-        query.addView("CEOTest.company.name");
-        query.setDescription("CEOTest.name", "CEO name");
-        query.addConstraint(new PathConstraintAttribute("CEOTest.name", ConstraintOp.CONTAINS, "ploy"));
-        query.addConstraint(new PathConstraintAttribute("CEOTest.company.name", ConstraintOp.CONTAINS, "pany"));
-        query.setOuterJoinStatus("CEOTest.company", OuterJoinStatus.OUTER);
-        query.addOrderBy("CEOTest.name", OrderDirection.ASC);
-        Date date = new Date();
-        SavedQuery sq = new SavedQuery("query1", date, query);
-        testUser.saveQuery("query1", sq);
-
-        modelUpdate.updateReferredQueryAndTemplate();
-        
-        PathQuery queryUpdated = testUser.getSavedQueries().get("query1").getPathQuery();
-        //verify view
-        assertEquals(3, queryUpdated.getView().size());
-        assertEquals("CEO.name", queryUpdated.getView().get(0));
-        assertEquals("CEO.title", queryUpdated.getView().get(1));
-        assertEquals("CEO.company.name", queryUpdated.getView().get(2));
-        //verify descriptions
-        assertEquals(1, queryUpdated.getDescriptions().size());
-        assertEquals("CEO name", queryUpdated.getDescription("CEOTest.name"));
-        //verify constraints
-        assertEquals(3, queryUpdated.getConstraints().size());
-        assertEquals(1, queryUpdated.getConstraintsForPath("CEO.name").size());
-        PathConstraintAttribute constraint1 = (PathConstraintAttribute) queryUpdated.getConstraintsForPath("CEO.name").get(0);
-        assertEquals("ploy", constraint1.getValue());
-        assertEquals(ConstraintOp.CONTAINS, constraint1.getOp().CONTAINS);
-        assertEquals(1, queryUpdated.getConstraintsForPath("Company.name").size());
-        PathConstraintAttribute constraint2 = (PathConstraintAttribute) queryUpdated.getConstraintsForPath("Company.name").get(0);
-        assertEquals("pany", constraint2.getValue());
-        assertEquals(ConstraintOp.CONTAINS, constraint2.getOp().CONTAINS);
-        //verify outer join
-        assertEquals(OuterJoinStatus.OUTER, queryUpdated.getOuterJoinStatus("CEO.company"));
-        //verify order by
-        assertEquals(1, queryUpdated.getOrderBy().size());
-        assertEquals("CEO.name", queryUpdated.getOrderBy().get(0).getOrderPath());
-    }
-    */
 }
