673d76a3e92ba29435a514109368862fed33ec5a
alexkalderimis <alex.kalderimis@gmail.com>
2011-10-27 23:32:16 +0000

    Version bump
    Former-commit-id: 1477902c30295ef4c90cd06fe3621f087e8fe271

--- intermine/ruby/intermine/lib/intermine/version.rb
+++ intermine/ruby/intermine/lib/intermine/version.rb
@@ -3,7 +3,8 @@ module Intermine
     # Webservice Client Version number
     #
     # Changes:
+    #   0.98.10 - Added status property to lists
     #   0.98.09 - Major changes to results - now with thorough-going Enumerable support
     #   0.98.08 - Added column summary support
-    VERSION = "0.98.09"
+    VERSION = "0.98.10"
 end
