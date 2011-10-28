36d50d7fae29a84501797424cb7372699dca271c
julie-sullivan <julie@flymine.org>
2011-10-28 13:38:09 +0000

    split ontologies file into two sources.  this is because we have another ontology source (anatomy) that is NOT an ftp source.
    Former-commit-id: b881fcc2c34b5dddd36fcbf22db10b7bf3f29658

--- bio/scripts/DataDownloader/lib/DataDownloader/Source/FlyCVTerms.pm
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/FlyCVTerms.pm
@@ -1,4 +1,4 @@
-package DataDownloader::Source::FlyOntologies;
+package DataDownloader::Source::FlyCVTerms;
 
 use Moose;
 extends 'DataDownloader::Source::FtpBase';
@@ -6,17 +6,10 @@ extends 'DataDownloader::Source::FtpBase';
 use constant {
     TITLE => 'Fly Ontologies',
     DESCRIPTION =>
-        'Ontologies from FlyBase - development and CV terms',
+        'Ontologies from FlyBase - CV terms',
     SOURCE_LINK => 'http://flybase.org',
     SOURCE_DIR => 'ontologies',
     SOURCES => [
-        {
-            SUBTITLE => 'Fly Development',
-            HOST => 'ftp.flybase.net',
-            REMOTE_DIR => "releases/current/precomputed_files/ontologies",
-            FILE => 'fly_development.obo.zip',
-            EXTRACT => 1,
-        },
         {
             SUBTITLE => 'Controlled Vocabulary',
             HOST => 'ftp.flybase.net',
