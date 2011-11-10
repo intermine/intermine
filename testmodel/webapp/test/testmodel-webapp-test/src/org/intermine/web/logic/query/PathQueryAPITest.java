412842bf36b575dff6e607902b1d06e7cb42c0e0
alexkalderimis <alex.kalderimis@gmail.com>
2011-11-10 17:18:02 +0000

    Former-commit-id: f813fa64f1637061f4e8adf2e213d638cb5d2601

---
+++ testmodel/webapp/test/testmodel-webapp-test/src/org/intermine/web/logic/query/PathQueryAPITest.java
@@ -0,0 +1,27 @@
+package org.intermine.web.logic.query;
+
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import junit.framework.TestCase;
+
+import org.intermine.api.query.PathQueryAPI;
+
+/**
+ * Tests the PathQueryAPI class.
+ *
+ * @author Matthew Wakeling
+ */
+public class PathQueryAPITest extends TestCase
+{
+    public void test1() throws Exception {
+        PathQueryAPI.getPathQueryExecutor();
+    }
+}
