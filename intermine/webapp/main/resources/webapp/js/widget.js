bd342a8c900df529fb5daa4047430ee893940f1d
danielabutano <daniela.butano@gmail.com>
2011-11-21 14:56:53 +0000

    webservices call and google.visualisation to display widgets
    Former-commit-id: 9a8c744759dac6c1195ff348fdc2d20a44dd7ea9

--- intermine/webapp/main/resources/webapp/js/widget.js
+++ intermine/webapp/main/resources/webapp/js/widget.js
@@ -1,150 +1,266 @@
-function getProcessGraphWidgetConfig(widgetId, bagName) {
-  display('widgetdata' + widgetId, true);
-  display('widgetdatawait' + widgetId, false);
+var CHART_OPTS = {
+    fontName: 'Sans-Serif',
+    fontSize: 10,
+    width: 400,
+    height:420,
+    legend: 'bottom',
+    colors:['#2F72FF', '#9FC0FF'],
+    chartArea:{top:30}
+};
+
+function displayGraphWidgetConfig(widgetId, domainLabel, rangeLabel, link, bagName) {
+  jQuery('#widgetdata' + widgetId).hide();
+  jQuery('#widgetdatanoresults' + widgetId).hide();
+  jQuery('#widgetdatawait' + widgetId).show();
+  var extraAttr = getExtraValue(widgetId);
+  var wsCall = function wsCall(token) {
+    var request_data = {
+          widget: widgetId,
+          list: bagName,
+          filter: extraAttr,
+          token: token
+      };
+    jQuery.getJSON(service + "list/chart", request_data, function(res) {
+    if (res.results.length != 0) {
+      var viz = google.visualization;
+      var data = google.visualization.arrayToDataTable(res.results, false);
+      var targetElem = document.getElementById("widgetdata" + widgetId);
+      var Chart = null;
+      var options = jQuery.extend({}, CHART_OPTS, {title: res.title});
+      if (res.chartType == "BarChart") {
+          Chart = viz.ColumnChart;
+      } else if (res.chartType == "StackedBarChart") {
+          Chart = viz.BarChart;
+          jQuery.extend(options, {isStacked: true, height: 450});
+      } else if (res.chartType == "ScatterPlot") {
+          Chart = viz.ScatterChart;
+      } else if (res.chartType == "PieChart") {
+          Chart = viz.PieChart;
+      } else if (res.chartType == "XYLineChart") {
+          Chart = viz.LineChart;
+      }
+
+      if (domainLabel) {
+          jQuery.extend(options, {hAxis: {title: domainLabel, titleTextStyle: {fontName: 'Sans-Serif'}}});
+      }
+      if (rangeLabel) {
+          jQuery.extend(options, {vAxis: {title: rangeLabel, titleTextStyle: {fontName: 'Sans-Serif'}}});
+      }
+      var viz;
+      if (Chart) {
+          viz = new Chart(targetElem);
+          viz.draw(data, options);
+          //if (link != "") {
+            google.visualization.events.addListener(viz, 'select', function() {
+              var selection = viz.getSelection();
+              for (var i = 0; i < selection.length; i++) {
+                var item = selection[i];
+                if (item.row != null && item.column != null) {
+                    var category = res.results[item.row + 1][0];
+                    var series = res.results[0][item.column];
+                    window.location.assign(webappUrl + "queryForGraphAction.do?bagName=" + bagName
+                      + "&category=" + category + "&series=" + series + "&urlGen=" + link);
+                } else if (item.row != null) {
+                  category = res.results[item.row + 1][0];
+                  window.location.assign(webappUrl + "queryForGraphAction.do?bagName=" + bagName
+                          + "&category=" + category + "&series=&urlGen=" + link);
+                }
+              }
+            });
+          //}
+      } else {
+          alert("Don't know how to draw " + res.chartType + "s yet!");
+      }
+      jQuery('#widgetdata' + widgetId).show();
+    } else {
+        jQuery('#widgetdatanoresults' + widgetId).show();
+    }
+    jQuery('#widgetdatawait' + widgetId).hide();
+    calcNotAnalysed(widgetId, res.notAnalysed);
+  });
+  }
+  AjaxServices.getSingleUseKey(wsCall);
+}
+
+function getExtraValue(widgetId) {
   var extraAttr;
   var extraAttrForm = document.getElementById("widgetselect" + widgetId);
   if(extraAttrForm != null) {
-      extraAttr = extraAttrForm.value;
+    extraAttr = extraAttrForm.value;
   }
-  var pValue = document.getElementById("pValue" + widgetId);
-  if(pValue != null) {
-    extraAttr = pValue.value;
-  }
-  AjaxServices.getProcessGraphWidget(widgetId,bagName,extraAttr,handleGraphWidget);
-}
-
-function handleGraphWidget(widget) {
-  display('widgetdatanoresults' + widget.configId, false);
-  if(widget.hasResults) {
-    var widgetdataname = document.getElementById('widgetdata' + widget.configId);
-    widgetdataname.innerHTML = widget.html;
-    display('widgetdatawait' + widget.configId, false);
-    widgetdataname.style.display = 'block';
-  } else {
-    display('widgetdatawait' + widget.configId, false);
-    display('widgetdata' + widget.configId, false);
-    display('widgetdatanoresults' + widget.configId, true);
-  }
-  calcNotAnalysed(widget);
-}
+  return extraAttr;
+ }
 
 function getProcessHTMLWidgetConfig(widgetId, bagName) {
-    AjaxServices.getProcessHTMLWidget(widgetId, bagName, handleHTMLWidget);
+  AjaxServices.getProcessHTMLWidget(widgetId, bagName, handleHTMLWidget);
 }
 
 function handleHTMLWidget(widget) {
-    var widgetdataname = document.getElementById('widgetdata' + widget.configId);
-    var widgetdatawait = document.getElementById('widgetdatawait' + widget.configId);
-    var widgetdatacontent = document.getElementById('widgetdatacontent' + widget.configId);
-    Element.hide($(widgetdatawait));
-    Element.hide($(widgetdataname));
-    Element.show($(widgetdatacontent));
+  jQuery('#widgetdata' + widget.configId).hide();
+  jQuery('#widgetdatanoresults' + widget.configId).hide();
+  jQuery('#widgetdatawait' + widget.configId).show();
 }
 
 function getProcessTableWidgetConfig(widgetId, bagName) {
   AjaxServices.getProcessTableWidget(widgetId,bagName,handleTableWidget);
 }
 
-function handleTableWidget(widget) {
-  display('widgetdatanoresults' + widget.configId, false);
-
-  if(widget.hasResults) {
-    removeChildren($("tablewidget"+widget.configId+"head"));
-    removeChildren($("tablewidget"+widget.configId+"body"));
-    var widgetdataname = document.getElementById('widgetdata' + widget.configId);
-    var widgetdatawait = document.getElementById('widgetdatawait' + widget.configId);
-
-    var row = document.createElement("tr");
+function createTableHeader(widgetId, columns) {
+   var $row = jQuery('<tr>');
 
     // checkbox
-    var checkboxCell = document.createElement("th");
-    var formName = "widgetaction" + widget.configId;
-    var checky = "<input type=\"checkbox\" name=\"select_all\" id=\"selected_all" + widget.configId + "\"";
-          checky += " onclick=\"toggleAllChecks('" + formName + "','" + widget.configId + "')\">";
-    checkboxCell.innerHTML = checky;
-    row.appendChild(checkboxCell);
-
-      // column names
-    for(var i = 0; i < widget.columns.length ; i++){
-      var cell = document.createElement("th");
-      cell.innerHTML = widget.columns[i];
-      row.appendChild(cell);
+    var formName = "widgetaction" + widgetId;
+    var $checkBoxAll = jQuery('<input />').attr({'type':'checkbox', 'id':'selected_all' + widgetId,
+                                                 'name': 'select_all'});
+    $checkBoxAll.click(function(){toggleAllChecks(formName, widgetId)});
+    $row.append(jQuery('<th>').append($checkBoxAll));
+    // column names
+    for(var i = 0; i < columns.length ; i++){
+      $row.append(jQuery('<th>').text(columns[i]));
     }
+    jQuery("#tablewidget"+widgetId+"head").append($row);
+}
 
-    $("tablewidget"+widget.configId+"head").appendChild(row);
+function handleTableWidget(widget) {
+  jQuery('#widgetdata' + widget.configId).hide();
+  jQuery('#widgetdatanoresults' + widget.configId).hide();
+  jQuery('#widgetdatawait' + widget.configId).show();
+  if(widget.hasResults) {
+    jQuery("#tablewidget"+widget.configId+"head").empty();
+    jQuery("#tablewidget"+widget.configId+"body").empty();
 
-  var bagContent;
-  if(widget.title == "Protein Interactions") {
-     bagContent = widget.elementInList;
-   }
+    createTableHeader(widget.configId, widget.columns);
 
-  for(var i = 0; i < widget.flattenedResults.length ; i++){
-    row = document.createElement("tr");
+    for(var i = 0; i < widget.flattenedResults.length ; i++) {
+      $row = jQuery("<tr>");
       var columns = widget.flattenedResults[i];
-      for(var j = 0; j < columns.length ; j++){
-      var cell = document.createElement("td");
+      for(var j = 0; j < columns.length ; j++) {
+         var $cell = jQuery("<td>");
          if(columns[j]!=null) {
              if(columns[j][1] != null) {
-               var link = document.createElement("a");
-               link.setAttribute("href",columns[j][1]);
-               link.innerHTML = columns[j][0];
-               cell.appendChild(link);
+               $a = jQuery('<a>').text(columns[j][0]);
+               $a.attr("href",columns[j][1]);
+               $cell.append($a);
              } else {
-              cell.innerHTML = columns[j][0];
+               $cell.text(columns[j][0]);
              }
          }
-         row.appendChild(cell);
+         $row.append($cell);
+      }
+      jQuery("#tablewidget"+widget.configId+"body").append($row);
     }
-      $("tablewidget"+widget.configId+"body").appendChild(row);
-    }
-    Element.hide($(widgetdatawait));
-     Element.show($(widgetdataname));
+
+     jQuery('#widgetdatawait' + widget.configId).hide();
+     jQuery('#widgetdata' + widget.configId).show();
   } else {
-    display('widgetdatawait' + widget.configId, false);
-    display('widgetdata' + widget.configId, false);
-    display('widgetdatanoresults' + widget.configId, true);
+    jQuery('#widgetdatawait' + widget.configId).hide();
+    jQuery('#widgetdatanoresults' + widget.configId).show();
   }
-  calcNotAnalysed(widget);
+  calcNotAnalysed(widget.configId, widget.notAnalysed);
 }
 
-function getProcessEnrichmentWidgetConfig(widgetId, bagName) {
-  display('widgetdata' + widgetId, false);
-  display('widgetdatawait' + widgetId, true);
-  var errorCorrection;
-  if($("errorCorrection" + widgetId)!=null) {
-    errorCorrection = $("errorCorrection" + widgetId).value;
-  }
-  var max;
-  if($("max" + widgetId)!=null) {
-    max = $("max" + widgetId).value;
-  }
-  var extraAttr;
-  if($("widgetselect" + widgetId)!=null) {
-    extraAttr = $("widgetselect" + widgetId).value;
-  }
-  var externalLink;
-  if($("externalLink" + widgetId)!=null) {
-    externalLink = $("externalLink" + widgetId).value;
+function displayEnrichmentWidgetConfig(widgetId, label, bagName) {
+    jQuery('#widgetdata' + widgetId).hide();
+    jQuery('#widgetdatanoresults' + widgetId).hide();
+    jQuery('#widgetdatawait' + widgetId).show();
+    var errorCorrection;
+    if($("errorCorrection" + widgetId)!=null) {
+      errorCorrection = $("errorCorrection" + widgetId).value;
+    }
+    var max;
+    if($("max" + widgetId)!=null) {
+      max = $("max" + widgetId).value;
+    }
+    var extraAttr = getExtraValue(widgetId);
+
+    var wsCall = function wsCall(token) {
+      var request_data = {
+            widget: widgetId,
+            list: bagName,
+            correction: errorCorrection,
+            maxp: max,
+            filter: extraAttr,
+            token: token
+      };
+      jQuery.getJSON(service + "list/enrichment", request_data, function(res) {
+        jQuery("#tablewidget"+widgetId+"head").empty();
+        jQuery("#tablewidget"+widgetId+"body").empty();
+        var results = res.results;
+        if (results.length != 0) {
+          var columns = [label, "p-Value", "Matches"];
+          createTableHeader(widgetId, columns)
+
+          //table
+          var $table = jQuery("#tablewidget"+widgetId+"body").empty();
+          var i = 0;
+          var externalLink;
+          if($("externalLink" + widgetId)!=null) {
+           externalLink = $("externalLink" + widgetId).value;
+          } 
+
+          var externalLinkLabel;
+          if($("externalLinkLabel" + widgetId)!=null) {
+            externalLinkLabel = $("externalLinkLabel" + widgetId).value;
+          }
+          for (i in results) {
+            $table.append(make_enrichment_row(results[i], externalLink, externalLinkLabel));
+          }
+          jQuery('#widgetdata' + widgetId).show();
+        } else {
+          jQuery('#widgetdatanoresults' + widgetId).show();
+        }
+        jQuery('#widgetdatawait' + widgetId).hide();
+        calcNotAnalysed(widgetId, res.notAnalysed);
+    });
   }
 
-  var externalLinkLabel;
-  if($("externalLinkLabel" + widgetId)!=null) {
-    externalLinkLabel = $("externalLinkLabel" + widgetId).value;
-  }
-  AjaxServices.getProcessEnrichmentWidget(widgetId,bagName,errorCorrection,max,extraAttr,externalLink,externalLinkLabel,handleTableWidget);
+  AjaxServices.getSingleUseKey(wsCall);
 }
 
-function removeChildren(node) {
-  while(node.childNodes.length > 0) {
-    node.removeChild(node.childNodes[0]);
-  }
+function make_enrichment_row(result, externalLink, externalLinkLabel) {
+    var $row = jQuery('<tr>');
+    var $checkBox = jQuery('<input />').attr({'type':'checkbox', 'id':'selected_' +result.item,
+                                             'value': result.item, 'name': 'selected'});
+    $row.append(jQuery('<td>').append($checkBox));
+
+    if (result.description) {
+        $td = jQuery('<td>').text(result.description + ' ');
+        if(externalLink) {
+          var label;
+          if (externalLinkLabel != undefined) {
+            label = externalLinkLabel;
+          }
+          label = label + result.item;
+          $a = jQuery('<a>').addClass('extlink').text('[' + label + ']');
+          $a.attr({'target': '_new', 'href': externalLink + result.item});
+          $td.append($a);
+        }
+        $row.append($td);
+    } else {
+        $row.append(jQuery('<td>').html("<em>no description</em>"));
+    }
+    $row.append(jQuery('<td>').text(result["p-value"]));
+    var $count = jQuery('<span>').addClass("match-count").text(result.matches.length);
+    var $matches = jQuery('<div>');
+    $matches.css({"display": "none"});
+    var $list = jQuery('<ul>');
+    var i = 0;
+    for (i in result.matches) {
+        $list.append(jQuery('<li>').text(result.matches[i]));
+    }
+    $matches.append($list);
+    $count.append($matches);
+    $count.click(function() {$matches.slideToggle()});
+    $row.append(jQuery('<td>').append($count));
+    return $row;
 }
 
 function checkSelected(formName) {
-  var elts = Form.getElements(formName);
+  var form = document.getElementById(formName);
+  var elts = form.getElementsByTagName("input");
   for(var i=0;i<elts.length;i++) {
-    if( $(elts[i]).name=='selected' && $(elts[i]).checked ) {
-     return true;
+    if( elts[i].name=='selected' && elts[i].checked) {
+      return true;
     }
   }
   return false;
@@ -163,25 +279,20 @@ function submitWidgetForm(widgetId,type,extra) {
     }
 }
 
-function displayNotAnalysed(widgetId,type,extra) {
-    $('action'+widgetId).value='notAnalysed';
-    $('widgetaction' + widgetId).submit();
-}
-
-function calcNotAnalysed(widget) {
-        var isMSIE = /*@cc_on!@*/false;
-        if (!isMSIE) {
-            document.getElementById('widgetnotanalysed' + widget.configId).innerHTML=widget.notAnalysed;
-            //$('widgetnotanalysed' + widget.configId).update(widget.notAnalysed);
-        }
+function calcNotAnalysed(widgetId, notAnalysed) {
+    var isMSIE = /*@cc_on!@*/false;
+    if (!isMSIE) {
+        document.getElementById('widgetnotanalysed' + widgetId).innerHTML=notAnalysed;
+    }
 }
 
 function toggleAllChecks(formName, widgetId) {
   var checked = document.getElementById('selected_all' + widgetId).checked;
-  var elts = Form.getElements(formName);
+  var form = document.getElementById(formName);
+  var elts = form.getElementsByTagName("input");
   for(var i=0;i<elts.length;i++) {
-    if( $(elts[i]).name=='selected') {
-        $(elts[i]).checked=checked;
+    if( elts[i].name=='selected') {
+        elts[i].checked=checked;
     }
   }
 }
