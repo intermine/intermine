b52d62037b0ec747590c6061535b21864879112c
alexkalderimis <alex.kalderimis@gmail.com>
2011-12-08 16:59:37 +0000

    Javadoc

--- intermine/web/main/src/org/intermine/webservice/server/output/PlainFormatter.java
+++ intermine/web/main/src/org/intermine/webservice/server/output/PlainFormatter.java
@@ -1,9 +1,25 @@
 package org.intermine.webservice.server.output;
 
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
 import java.util.List;
 import java.util.Map;
 
-public class PlainFormatter extends Formatter {
+/**
+ * Simplest possible formatting. Output rows are just joined with commas.
+ * @author Alex Kalderimis
+ *
+ */
+public class PlainFormatter extends Formatter
+{
 
     @Override
     public String formatHeader(Map<String, Object> attributes) {
