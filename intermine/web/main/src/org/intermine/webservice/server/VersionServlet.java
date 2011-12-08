eafd2440caf18d197cc94c9dbb4ebf8baa10370f
alexkalderimis <alex.kalderimis@gmail.com>
2011-12-08 17:02:02 +0000

    Move handler logic into webservice

--- intermine/web/main/src/org/intermine/webservice/server/VersionServlet.java
+++ intermine/web/main/src/org/intermine/webservice/server/VersionServlet.java
@@ -11,28 +11,22 @@ package org.intermine.webservice.server;
  */
 
 import java.io.IOException;
-import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
-import org.intermine.util.StringUtil;
-import org.intermine.web.logic.Constants;
+import org.intermine.api.InterMineAPI;
 import org.intermine.web.logic.session.SessionMethods;
 
 /**
  * Returns version of the deployed InterMine application.
- *
+ * @author Alex Kalderimis
  * @author Jakub Kulaviak
  */
 public class VersionServlet extends HttpServlet
 {
-
-    private static final Logger LOGGER = Logger.getLogger(VersionServlet.class);
-
     private static final long serialVersionUID = 1L;
 
     /**
@@ -51,30 +45,8 @@ public class VersionServlet extends HttpServlet
         runService(req, resp);
     }
 
-    private void runService(HttpServletRequest request,
-            HttpServletResponse response) {
-        String pathFromUrl = request.getPathInfo();
-        String version = getVersion(pathFromUrl, request);
-        try {
-            response.getWriter().print(version);
-        } catch (IOException e) {
-            LOGGER.error("Obtaining writer to write intermine version failed.", e);
-        }
-    }
-
-    private String getVersion(String versionType, HttpServletRequest request) {
-        if (versionType != null) {
-            versionType = StringUtil.trimSlashes(versionType);
-
-            if (versionType.equalsIgnoreCase("release")) {
-                Properties webProperties =
-                    SessionMethods.getWebProperties(request.getSession().getServletContext());
-                return webProperties.getProperty("project.releaseVersion");
-            } else if (versionType.equalsIgnoreCase("ws")) {
-                return "" + Constants.WEB_SERVICE_VERSION;
-            }
-        }
-        // for backwards compatibility default is the web service version
-        return "" + Constants.WEB_SERVICE_VERSION;
+    private void runService(HttpServletRequest request, HttpServletResponse response) {
+        final InterMineAPI api = SessionMethods.getInterMineAPI(request);
+        new VersionService(api).service(request, response);
     }
 }
