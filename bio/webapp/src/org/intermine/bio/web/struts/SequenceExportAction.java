79a1f1c3b94b9411dda2834b8ed5709a9e3f9fb5
boboppie <fengyuan.huu@gmail.com>
2011-12-12 19:13:10 +0000

    fixed ticket #2312

--- bio/webapp/src/org/intermine/bio/web/struts/SequenceExportAction.java
+++ bio/webapp/src/org/intermine/bio/web/struts/SequenceExportAction.java
@@ -11,6 +11,8 @@ package org.intermine.bio.web.struts;
  */
 
 import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +22,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -52,6 +55,8 @@ import org.intermine.web.struts.InterMineAction;
  */
 public class SequenceExportAction extends InterMineAction
 {
+    private static final Logger LOG = Logger.getLogger(SequenceExportAction.class);
+
     /**
      * This action is invoked directly to export SequenceFeatures.
      * @param mapping The ActionMapping used to select this instance
@@ -81,6 +86,7 @@ public class SequenceExportAction extends InterMineAction
         if (obj instanceof SequenceFeature || obj instanceof Protein) {
             bioSequence = createBioSequence(obj);
             if (bioSequence != null) {
+                response.setContentType("text/plain");
                 OutputStream out = response.getOutputStream();
                 SeqIOTools.writeFasta(out, bioSequence);
             }
