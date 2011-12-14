d5f30bf628e28abdad093dc20b202ab28e6c345d
alexkalderimis <alex.kalderimis@gmail.com>
2011-12-14 17:03:26 +0000

    tests for handling dodgy sort orders, and empty views (fixes #2743)
    Former-commit-id: d1eb84842ef029c71058853ab41c9871fa9a25ba

--- intermine/pathquery/test/src/org/intermine/pathquery/PathQueryUnmarshalTest.java
+++ intermine/pathquery/test/src/org/intermine/pathquery/PathQueryUnmarshalTest.java
@@ -45,12 +45,28 @@ public class PathQueryUnmarshalTest extends  TestCase
 
     public void testInvalidView() {
         PathQuery query = createQuery("InvalidView.xml");
-        assertTrue(query.verifyQuery().size() == 1);
+        assertEquals(1, query.verifyQuery().size());
+    }
+
+    public void testEmptyView() {
+        PathQuery query = createQuery("emptyView.xml");
+        assertFalse("Viewless queries should be invalid", query.isValid());
+        assertEquals(1, query.verifyQuery().size());
     }
 
     public void testInvalidSortOrder() {
         PathQuery query = createQuery("InvalidSortOrder.xml");
-        assertTrue(query.verifyQuery().size() == 0);
+        assertTrue("Invalid sort orders are not tolerated", !query.isValid());
+    }
+
+    public void testOrderlessSortOrder() {
+        PathQuery query = createQuery("OrderlessSortOrder.xml");
+        assertTrue("Orderless sort orders are tolerated", query.isValid());
+    }
+
+    public void testIrrelevantSortOrder() {
+        PathQuery query = createQuery("IrrelevantSortOrder.xml");
+        assertEquals(Arrays.asList("Order by element for path Employee.department.name is not relevant to the query"), query.verifyQuery());
     }
 
     public void testInvalidConstraintLogic() {
