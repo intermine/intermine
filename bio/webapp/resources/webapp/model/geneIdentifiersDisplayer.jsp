d2c61b353505356ae4fcbe5f6292d83473a70019
radekstepan <dev@radekstepan.com>
2012-01-05 18:02:07 +0000

    identifiers displayer for metabolicMine

---
+++ bio/webapp/resources/webapp/model/geneIdentifiersDisplayer.jsp
@@ -0,0 +1,15 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html" %>
+<%@taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+
+<c:if test="${!empty identifiers}">
+  <div id="gene-identifiers-displayer" class="inline-list">
+  <ul>
+  <li><span class="name">identifiers</span>:</li>
+  <c:set var="size" value="${fn:length(identifiers)}" />
+  <c:forEach var="identifier" items="${identifiers}" varStatus="status">
+    <li>${identifier.value}<c:if test="${status.count < size}">, </c:if></li>
+  </c:forEach>
+  </ul>
+  </div>
+</c:if>
---
+++ bio/webapp/src/org/intermine/bio/web/displayer/GeneIdentifiersDisplayer.java
@@ -0,0 +1,64 @@
+package org.intermine.bio.web.displayer;
+
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.LinkedHashMap;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.intermine.api.InterMineAPI;
+import org.intermine.model.InterMineObject;
+import org.intermine.web.displayer.ReportDisplayer;
+import org.intermine.web.logic.config.ReportDisplayerConfig;
+import org.intermine.web.logic.results.ReportObject;
+
+/**
+ *
+ * @author radek
+ *
+ */
+public class GeneIdentifiersDisplayer extends ReportDisplayer
+{
+
+    protected static final Logger LOG = Logger.getLogger(GeneIdentifiersDisplayer.class);
+
+    /**
+     * Construct with config and the InterMineAPI.
+     * @param config to describe the report displayer
+     * @param im the InterMine API
+     */
+    public GeneIdentifiersDisplayer(ReportDisplayerConfig config, InterMineAPI im) {
+        super(config, im);
+    }
+
+    /** @String[] which identifiers to use? */
+    private final String[] identifiers = new String []{"primaryIdentifier", "secondaryIdentifier",
+        "ncbiGeneIdentifier"};
+
+    @Override
+    public void display(HttpServletRequest request, ReportObject reportObject) {
+        LinkedHashMap<String, String> result = new LinkedHashMap<String, String>();
+        InterMineObject imObj = reportObject.getObject();
+        try {
+            for (String identifier : identifiers) {
+                String value = imObj.getFieldValue(identifier).toString();
+                if (!result.values().contains(value)) {
+                    result.put(identifier, value);
+                }
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        request.setAttribute("identifiers", result);
+    }
+
+}
