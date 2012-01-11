b64d8c1cc16aa07aebd6f190524c526026733e50
alexkalderimis <alex.kalderimis@gmail.com>
2012-01-11 13:55:46 +0000

    Checkstyle and javadoc
    Former-commit-id: e190996833fd2065cbc408da578105600da8b986

--- intermine/web/main/src/org/intermine/webservice/server/lists/ListUnionService.java
+++ intermine/web/main/src/org/intermine/webservice/server/lists/ListUnionService.java
@@ -1,5 +1,15 @@
 package org.intermine.webservice.server.lists;
 
+/*
+ * Copyright (C) 2002-2012 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
 import java.util.Collection;
 import java.util.Set;
 
@@ -8,19 +18,30 @@ import org.intermine.api.bag.BagOperations;
 import org.intermine.api.profile.InterMineBag;
 import org.intermine.api.profile.Profile;
 
+/**
+ * A class for exposing creating unions of lists as a resource.
+ * @author Alex Kalderimis.
+ */
 public class ListUnionService extends CommutativeOperationService
 {
 
+    /**
+     * A message to give to users when something goes wrong.
+     */
     public static final String USAGE =
-      "\nList Union Service\n"
-      + "===================\n"
-      + "Combine lists into a new one\n"
-      + "Parameters:\n"
-      + "lists: a list of list names - separated by semi-cola (';')\n"
-      + "       this parameter may be repeated."
-      + "name: the name of the new list resulting from the union\n"
-      + "description: an optional description of the new list\n";
+        "\nList Union Service\n"
+        + "===================\n"
+        + "Combine lists into a new one\n"
+        + "Parameters:\n"
+        + "lists: a list of list names - separated by semi-cola (';')\n"
+        + "       this parameter may be repeated."
+        + "name: the name of the new list resulting from the union\n"
+        + "description: an optional description of the new list\n";
 
+    /**
+     * Constructor.
+     * @param im The InterMine magic sauce.
+     */
     public ListUnionService(InterMineAPI im) {
         super(im);
     }
