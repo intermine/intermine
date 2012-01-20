f1a165001c255415dafad060f65bb1ce7a8a2f7f
julie-sullivan <julie@flymine.org>
2012-01-20 11:54:23 +0000

    allow for discontinuous locations in GFF3
    Former-commit-id: fb7d245bbc16eef182e852ddbb01bde7a2125923

--- bio/core/test/src/org/intermine/bio/dataconversion/GFF3ConverterTest.java
+++ bio/core/test/src/org/intermine/bio/dataconversion/GFF3ConverterTest.java
@@ -68,32 +68,23 @@ public class GFF3ConverterTest extends ItemsTestCase {
         converter.parse(srcReader);
         converter.storeAll();
 
-
-        Set expected = new HashSet(getExpectedLocatedItems());
-
         // uncomment to write out a new target items file
         //writeItemsFile(writer.getItems(), "gff_located_item_test.xml");
 
-        assertEquals(expected, writer.getItems());
+        assertEquals(readItemSet("GFF3ConverterTest.xml"), writer.getItems());
     }
 
-    protected Collection getExpectedLocatedItems() throws Exception {
-        InputStream resource =
-            getClass().getClassLoader().getResourceAsStream("GFF3ConverterTest.xml");
-        return FullParser.parse(resource);
-    }
 
     /**
      * Test creating items with dontCreateLocations flag true.
      */
     public void testParseUnLocated() throws Exception {
-        BufferedReader srcReader = new BufferedReader(new
-                                                      InputStreamReader(getClass().getClassLoader().getResourceAsStream("test.gff")));
+        BufferedReader srcReader = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("test.gff")));
         converter.setDontCreateLocations(true);
         converter.parse(srcReader);
         converter.storeAll();
 
-        Set expected = new HashSet(getExpectedUnLocatedItems());
+        Set expected = new HashSet(readItemSet("GFF3ConverterTestUnLocated.xml"));
 
         // uncomment to write out a new target items file
         //writeItemsFile(writer.getItems(), "gff_unlocated_item_test.xml");
@@ -101,35 +92,19 @@ public class GFF3ConverterTest extends ItemsTestCase {
         assertEquals(expected, writer.getItems());
     }
 
-
-    protected Collection getExpectedUnLocatedItems() throws Exception {
-        InputStream resource =
-            getClass().getClassLoader().getResourceAsStream("GFF3ConverterTestUnLocated.xml");
-        return FullParser.parse(resource);
-    }
-
     /**
-     * Given two sets of Items (a and b) return a set of Items that are present in a
-     * but not b.
-     * @param a a set of Items
-     * @param b a set of Items
-     * @return the set of Items in a but not in b
+     * Test creating items with dontCreateLocations flag true.
      */
-    /*
-    public Set compareItemSets(Set a, Set b) {
-        Set diff = new HashSet(a);
-        Iterator i = a.iterator();
-        while (i.hasNext()) {
-            Item itemA = (Item) i.next();
-            Iterator j = b.iterator();
-            while (j.hasNext()) {
-                Item itemB = (Item) j.next();
-                if (itemA.equals(itemB)) {
-                    diff.remove(itemA);
-                }
-            }
-        }
-        return diff;
+    public void testParseDiscontinuous() throws Exception {
+        BufferedReader srcReader = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("test-locs.gff")));
+        converter.parse(srcReader);
+        converter.storeAll();
+
+        Set expected = new HashSet(readItemSet("GFF3ConverterTestDiscontinuous.xml"));
+
+        // uncomment to write out a new target items file
+        //writeItemsFile(writer.getItems(), "gff_unlocated_item_test.xml");
+
+        assertEquals(expected, writer.getItems());
     }
-     */
 }
