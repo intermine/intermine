549b10822cf822114a919285278c72575dfd6916
rnsmith <rns>
2012-01-27 17:30:54 +0000

    Make ONTOLOGIES public.

--- bio/webapp/src/org/intermine/bio/web/displayer/GeneOntologyDisplayer.java
+++ bio/webapp/src/org/intermine/bio/web/displayer/GeneOntologyDisplayer.java
@@ -43,8 +43,10 @@ import org.intermine.web.logic.session.SessionMethods;
  */
 public class GeneOntologyDisplayer extends ReportDisplayer
 {
-
-    private static final Set<String> ONTOLOGIES = new HashSet<String>();
+    /**
+     * The names of ontology root terms.
+     */
+    public static final Set<String> ONTOLOGIES = new HashSet<String>();
     private static final Map<String, String> EVIDENCE_CODES = new HashMap<String, String>();
     private Map<String, Boolean> organismCache = new HashMap<String, Boolean>();
 
