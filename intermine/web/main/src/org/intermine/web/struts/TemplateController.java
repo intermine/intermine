c01fe3ca9e5f34f6a75cd684c1fd3cc771f4d96b
danielabutano <daniela.butano@gmail.com>
2012-01-30 12:15:56 +0000

    Fixed the bug: Lookup constraints only hide their extra-value input if an accompanying organism constraint is editable
    Former-commit-id: 6c8cbea6e2634eb138672ab12491da7c4ecedb5f

--- intermine/web/main/src/org/intermine/web/struts/TemplateController.java
+++ intermine/web/main/src/org/intermine/web/struts/TemplateController.java
@@ -13,6 +13,7 @@ package org.intermine.web.struts;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,13 +30,13 @@ import org.intermine.api.InterMineAPI;
 import org.intermine.api.profile.Profile;
 import org.intermine.api.profile.SavedQuery;
 import org.intermine.api.search.Scope;
-import org.intermine.metadata.ClassDescriptor;
+import org.intermine.metadata.Model;
 import org.intermine.pathquery.Path;
 import org.intermine.pathquery.PathConstraint;
 import org.intermine.pathquery.PathConstraintBag;
-import org.intermine.pathquery.PathConstraintLookup;
 import org.intermine.pathquery.PathConstraintMultiValue;
 import org.intermine.pathquery.PathConstraintNull;
+import org.intermine.pathquery.PathException;
 import org.intermine.pathquery.PathQuery;
 import org.intermine.api.template.TemplateManager;
 import org.intermine.template.TemplateQuery;
@@ -183,7 +184,7 @@ public class TemplateController extends TilesAction
             }
             index++;
         }
-        verifyDisplayExtraValue(displayConstraintList);
+        verifyDisplayExtraValue(displayConstraintList, template);
         request.setAttribute("dcl", displayConstraintList);
         request.setAttribute("templateQuery", displayTemplate);
         String constraintLogic = template.getConstraintLogic().toLowerCase();
@@ -210,21 +211,27 @@ public class TemplateController extends TilesAction
         return factory;
     }
 
-    private void verifyDisplayExtraValue(List<DisplayConstraint> displayConstraintList) {
-        List<DisplayConstraint> dcl = new ArrayList<DisplayConstraint>(displayConstraintList);
+    private void verifyDisplayExtraValue(List<DisplayConstraint> displayConstraintList,
+        TemplateQuery template) {
+        Set<PathConstraint> pathConstraints = template.getConstraints().keySet();
+        Model model = template.getModel();
         for (DisplayConstraint dc : displayConstraintList) {
             if (dc.isExtraConstraint()) {
                 String mainPath = dc.getPath().getPath().toStringNoConstraints();
                 String connectField = dc.getExtraConnectFieldPath();
-                for (DisplayConstraint displayConstraint : dcl) {
-                    String  path = displayConstraint.getPath().getPath().toStringNoConstraints();
+                for (PathConstraint pathConstraint : pathConstraints) {
+                    String  path = pathConstraint.getPath();
                     if (!mainPath.equals(path)) {
-                        if (path.equals(connectField)
-                            || (path.startsWith(connectField)
-                               && connectField.split("\\.").length
-                                  == displayConstraint.getPath().getIndentation())
-                               && displayConstraint.getPath().isAttribute()) {
-                            dc.setShowExtraConstraint(false);
+                        try {
+                            if (path.equals(connectField)
+                                || (path.startsWith(connectField)
+                                    && connectField.split("\\.").length
+                                        == path.split("\\.").length - 1)
+                                    && (new Path(model, path)).endIsAttribute()) {
+                                dc.setShowExtraConstraint(false);
+                            }
+                        } catch (PathException pe) {
+                            //nothing
                         }
                     }
                 }
