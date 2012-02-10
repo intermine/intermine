5ffab156864c83d2c7aa9dde0a3d7448cebe4232
alexkalderimis <alex.kalderimis@gmail.com>
2012-02-10 17:51:23 +0000

    Completely prevent instantiation. Still stringly typed, but a bit better now.

--- intermine/api/main/src/org/intermine/api/search/Scope.java
+++ intermine/api/main/src/org/intermine/api/search/Scope.java
@@ -1,7 +1,7 @@
 package org.intermine.api.search;
 
 /*
- * Copyright (C) 2002-2011 FlyMine
+ * Copyright (C) 2002-2012 FlyMine
  *
  * This code may be freely distributed and modified under the
  * terms of the GNU Lesser General Public Licence.  This should
@@ -10,24 +10,40 @@ package org.intermine.api.search;
  *
  */
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Scope constants.
  * @author Kim Rutherford
+ * @author Alex Kalderimis
  */
-public interface Scope
+public final class Scope
 {
+    private Scope() {
+        // hidden constructor. Do not instantiate.
+    }
+
     /**
      * Global scope for public bags, templates, etc.
      */
-    String GLOBAL = "global";
+    public static final String GLOBAL = "global";
 
     /**
      * User scope for private bags, templates, etc.
      */
-    String USER = "user";
+    public static final String USER = "user";
 
     /**
      * User or global scope for bags, templates, etc.
      */
-    String ALL = "all";
+    public static final String ALL = "all";
+
+    /**
+     *The possible scopes.
+     */
+    public static final Set<String> SCOPES =
+            new HashSet<String>(Arrays.asList(GLOBAL, USER, ALL));
+
 }
