17d42907a9c9dcbc5d3bac99cf8fe739c8166b65
rnsmith <rns>
2012-02-27 18:28:30 +0000

    Allow inline report tables to support simple objects, which don't inherit from InterMineObject.

--- intermine/web/main/src/org/intermine/web/logic/results/InlineTableResultElement.java
+++ intermine/web/main/src/org/intermine/web/logic/results/InlineTableResultElement.java
@@ -11,6 +11,7 @@ package org.intermine.web.logic.results;
  */
 
 import org.intermine.api.results.ResultElement;
+import org.intermine.model.FastPathObject;
 import org.intermine.model.InterMineObject;
 import org.intermine.pathquery.Path;
 import org.intermine.util.DynamicUtil;
@@ -33,7 +34,7 @@ public class InlineTableResultElement extends ResultElement
      * @param fc FieldConfig
      * @param isKeyField is this a key field?
      */
-    public InlineTableResultElement(InterMineObject imObj, Path path, FieldConfig fc,
+    public InlineTableResultElement(FastPathObject imObj, Path path, FieldConfig fc,
             Boolean isKeyField) {
         super(imObj, path, isKeyField);
         this.fc = fc;
