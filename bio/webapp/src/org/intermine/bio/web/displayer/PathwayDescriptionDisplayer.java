1c5804bf2d0495b00663016261a9bf017d982c2d
radekstepan <dev@radekstepan.com>
2012-02-28 12:04:21 +0000

    pathway description displayer
    Former-commit-id: 9c4d731d15edc4aa8f26d7ceb664861418e8a519

---
+++ bio/webapp/src/org/intermine/bio/web/displayer/PathwayDescriptionDisplayer.java
@@ -0,0 +1,37 @@
+package org.intermine.bio.web.displayer;
+
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.intermine.api.InterMineAPI;
+import org.intermine.web.displayer.ReportDisplayer;
+import org.intermine.web.logic.config.ReportDisplayerConfig;
+import org.intermine.web.logic.results.ReportObject;
+
+public class PathwayDescriptionDisplayer extends ReportDisplayer
+{
+
+    public PathwayDescriptionDisplayer(ReportDisplayerConfig config, InterMineAPI im) {
+        super(config, im);
+    }
+
+    @Override
+    public void display(HttpServletRequest request, ReportObject reportObject) {
+
+        try {
+            request.setAttribute("description", reportObject.getObject().getFieldValue("description"));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
