ea9b42754aa5f4f302d5d89780ccf2f6f74c4658
danielabutano <daniela.butano@gmail.com>
2012-04-05 15:08:40 +0000

    Possibel action type for graphwidgets
    Former-commit-id: 07504f3413abb17e08b5beaa2db71bb4f623d0bb

---
+++ intermine/web/main/src/org/intermine/web/logic/widget/GraphWidgetActionType.java
@@ -0,0 +1,17 @@
+package org.intermine.web.logic.widget;
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+/**
+ * Enumeration for possible actions when create the query for graph widgets.
+ *
+ * @author dbutano
+ */
+public enum GraphWidgetActionType { TOTAL, ACTUAL, EXPECTED };
