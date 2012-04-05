db40fbcd75d72db6ee0f5cc5e404a84d3b0de6bb
danielabutano <daniela.butano@gmail.com>
2012-04-05 15:09:36 +0000

    Utility class for widgetconfig
    Former-commit-id: b82bfe89eb0b78d87ba26a8b96a603434545b522

---
+++ intermine/web/main/src/org/intermine/web/logic/widget/config/WidgetConfigUtil.java
@@ -0,0 +1,45 @@
+package org.intermine.web.logic.widget.config;
+
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import org.intermine.metadata.Model;
+import org.intermine.pathquery.PathConstraint;
+
+public class WidgetConfigUtil {
+
+    public static boolean isListConstraint(PathConstraint pc) {
+        String value = PathConstraint.getValue(pc);
+        value = value.replace(" ", "");
+        if ("[list]".equals(value)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isPathContainingSubClass(Model model, String path) {
+        if (path.contains("[") && path.contains("]")) {
+            String name = path.substring(path.indexOf("[") + 1, path.indexOf("]"));
+            if (model.getClassDescriptorByName(name) != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isFilterConstraint(final WidgetConfig config, PathConstraint pc) {
+        String value = PathConstraint.getValue(pc);
+        value = value.replace(" ", "");
+        if (value.equals("[" + config.getFilterLabel() + "]")) {
+            return true;
+        }
+        return false;
+    }
+}
