4ef29bdfac8cef506f255256cef05ea070954891
Alex Kalderimis <alex@flymine.org>
2012-07-11 19:14:32 +0100

    Fixed broken list tagging.

--- intermine/webapp/main/resources/webapp/js/inlineTagEditor.js
+++ intermine/webapp/main/resources/webapp/js/inlineTagEditor.js
@@ -1,153 +1,156 @@
-var editingTag;
-var inputType;
+(function($) {
 
-function getAddedTag(editorId, type) {
-  if (getInputType(editorId, type) == 'select') {
-    return getSelectValue('tagSelect-' + editorId);
-  } else {
-    return $('tagValue-' + editorId).value;
-  }
-}
+	var editingTag = false;
+	var inputType;
+	var inputPrefix;
 
-function startEditingTag(editorId) {
-  if (editingTag) {
-    stopEditingTag();
-  }
-  editingTag = editorId;
-  showEl('switchLink-' + editorId);
-  hideEl('addLink-' + editingTag);
-  setInputType(editorId, 'select');
-  showEl('tagsEdit-' + editorId);
-}
+	this.getAddedTag = function(editorId) {
+	  return $('#' + inputPrefix + '-' + editorId).val();
+	};
 
-function setInputType(editorId, type) {
-  inputType = type;
-  if (type == 'select') {
-    var title = 'New tag';
-    showEl('tagSelect-' + editorId);
-    hideEl('tagValue-' + editorId);
-  } else {
-    var title = 'Select tag';
-    showEl('tagValue-' + editorId);
-    hideEl('tagSelect-' + editorId);
-    $('tagValue-' + editorId).focus();
-  }
-  $('switchLink-' + editorId).innerHTML = title;
-}
+	this.startEditingTag = function(editorId) {
+	  if (editingTag) {
+	    stopEditingTag();
+	  }
+	  editingTag = editorId;
+	  showEl('switchLink-' + editorId);
+	  hideEl('addLink-' + editingTag);
+	  setInputType(editorId, 'select');
+	  showEl('tagsEdit-' + editorId);
+	};
 
-function switchTagInput(editorId) {
-  if (getInputType(editorId) == 'select') {
-    setInputType(editorId, 'new');
-  } else {
-    setInputType(editorId, 'select');
-  }
-}
+	this.setInputType = function(editorId, type) {
+	  var title;
+	  if (type == 'select') {
+		inputPrefix = "tagSelect";
+	    title = 'New tag';
+	    showEl('tagSelect-' + editorId);
+	    hideEl('tagValue-' + editorId);
+	  } else {
+		inputPrefix = "tagValue";
+	    title = 'Select tag';
+	    showEl('tagValue-' + editorId);
+	    hideEl('tagSelect-' + editorId);
+	    $('#tagValue-' + editorId).focus();
+	  }
+	  $('#switchLink-' + editorId).innerHTML = title;
+	};
 
-function getInputType(editorId) {
-  return inputType;
-}
+	this.switchTagInput = function(editorId) {
+	  if (inputPrefix == 'tagSelect') {
+	    setInputType(editorId, 'new');
+	  } else {
+	    setInputType(editorId, 'select');
+	  }
+	};
 
-function stopEditingTag() {
-  if (editingTag) {
-    hideEl('tagsEdit-' + editingTag);
-    showEl('addLink-' + editingTag);
-    hideEl('switchLink-' + editingTag);
-  }
-  editingTag = '';
-}
+	this.stopEditingTag = function() {
+	  if (editingTag) {
+	    hideEl('tagsEdit-' + editingTag);
+	    showEl('addLink-' + editingTag);
+	    hideEl('switchLink-' + editingTag);
+	  }
+	  editingTag = false;
+	};
+	
+	var CANNOT_TAG_THIS = 'Adding tag failed. It is not possible to tag this item.';
+	var cannotTagNotification = new FailureNotification({message: CANNOT_TAG_THIS});
 
-function addTag(editorId, type) {
-  var tag = getAddedTag(editorId, type);
-  var callBack = function(returnStr) {
-    if (returnStr == 'ok') {
-      refreshTags(editorId, type);
-    } else {
-      window.alert(returnStr);
-    }
-  }
-  taggedObject = parseTagged(editorId);
-  if (taggedObject == '') {
-    window.alert('Adding tag failed. It is not possible to tag this item.');
-  }
-  if (tag != '') {
-    AjaxServices.addTag(tag, taggedObject, type, callBack);
-  }
-}
+	this.addTag = function(editorId, type) {
+	  var taggedObject = parseTagged(editorId);;
+	  var tag = getAddedTag(editorId, type);
+	  
+	  if ( !taggedObject ) {
+		  return cannotTagNotification.render();
+	  }
+	  
+	  var callBack = function(returnStr) {
+	    if (returnStr == 'ok') {
+	      refreshTags(editorId, type);
+	    } else {
+	      new FailureNotification({message: returnStr}).render();
+	    }
+	  }
+	  
+	  if (tag != '') {
+	    AjaxServices.addTag(tag, taggedObject, type, callBack);
+	  }
+	};
 
-function deleteTag(tag, editorId, type) {
-  var callBack = function(returnStr) {
-    if (returnStr == 'ok') {
-      refreshTags(editorId, type);
-    } else {
-      window.alert(returnStr);
-    }
-  }
-  taggedObject = parseTagged(editorId);
-  if (taggedObject == '') {
-    window.alert('Deleting tag failed. It is not possible delete tag for this item.');
-  }
-  AjaxServices.deleteTag(tag, taggedObject, type, callBack);
-}
+	this.deleteTag = function(tag, editorId, type) {
+	  var callBack = function(returnStr) {
+	    if (returnStr == 'ok') {
+	      refreshTags(editorId, type);
+	    } else {
+	      new FailureNotification({message: returnStr}).render();
+	    }
+	  }
+	  taggedObject = parseTagged(editorId);
+	  if (taggedObject == '') {
+	    window.alert('Deleting tag failed. It is not possible delete tag for this item.');
+	  }
+	  AjaxServices.deleteTag(tag, taggedObject, type, callBack);
+	};
 
-function displayTags(editorId, type, tags) {
-  var parent = $('currentTags-' + editorId);
-  parent.innerHTML = '';
-  for (var i = 0; i < tags.length; i++) {
-    var tag = tags[i];
-    addTagSpan(editorId, type, tag);
-  }
-}
+	this.displayTags = function(editorId, type, tags) {
+	  var i, tag, tagsl = tags.length, parent = $('#currentTags-' + editorId).empty();
+	  
+	  for (i = 0; i < tagsl; i++) {
+	    tag = tags[i];
+	    addTagSpan(editorId, type, tag);
+	  }
+	};
 
-function addTagSpan(editorId, type, tag) {
-  var parent = $('currentTags-' + editorId);
-  var span = document.createElement('span');
-  span.setAttribute('class', 'tag');
-  // for IE
-  span.setAttribute('className', 'tag');
-  span.innerHTML = tag + '<a class="deleteTagLink" onclick="javascript:deleteTag(\'' + tag + '\', \'' + editorId + '\', \'' + type + '\')">[x]</a>&nbsp;';
-  parent.appendChild(span);
-}
+	this.addTagSpan = function(editorId, type, tag) {
+	  var parent = $('#currentTags-' + editorId);
+	  var span = $('<span class="tag">');
+	  var a = $('<a class="deleteTagLink">[x]</a>');
+	  a.click(function() {
+		  deleteTag(tag, editorId, type);
+	  });
+	  span.append(tag).append(a);
+	  parent.append(span);
+	};
 
-// Refreshes displayed tags for specified tagged object and refreshes selects in InlineTagEditors
-function refreshTags(editorId, type) {
-  refreshObjectTags(parseTagged(editorId), type);
-  refreshTagSelects(type);
-}
+	// Refreshes displayed tags for specified tagged object and refreshes selects in InlineTagEditors
+	this.refreshTags = function(editorId, type) {
+	  refreshObjectTags(parseTagged(editorId), type);
+	  refreshTagSelects(type);
+	};
 
-function refreshObjectTags(tagged, type) {
-  var callBack = function(tags) {
-    var els = document.getElementsByTagName("*");
-    for (var i = 0; i < els.length; i++) {
-      var el = els[i];
-      if (el.id.indexOf('currentTags-' + tagged + '@') == 0) {
-          //&& el.id.indexOf(tagged) != -1) {
-        var editorId = el.id.replace('currentTags-', '');
-        displayTags(editorId, type, tags);
-      }
-    }
-  }
-  AjaxServices.getObjectTags(type, tagged, callBack);
-}
+	this.refreshObjectTags = function(tagged, type) {
+	  var callBack = function(tags) {
+		var currentTagHolders = $('.current-tags');
+		currentTagHolders.map(function(i) {
+			var editorId, id = this.id;
+			
+			if (id && id.indexOf('currentTags-') == 0) {
+				editorId = id.replace('currentTags-', '');
+				if (parseTagged(editorId) == tagged) {
+					displayTags(editorId, type, tags);
+				}
+			}
+			
+		});
+	  }
+	  AjaxServices.getObjectTags(type, tagged, callBack);
+	};
 
-function refreshTagSelects(type) {
-  var callBack = function(tags) {
-    var selects = document.getElementsByTagName('select');
-    for (var i = 0; i < selects.length; i++) {
-      var select = selects[i];
-      if (select.id.indexOf('tagSelect-') == 0) {
-        setSelectElement(select.id, getSelectTitle(select), tags);
-      }
-    }
-  }
-  AjaxServices.getTags(type, callBack);
-}
+	this.refreshTagSelects = function(type) {
+	  var callBack = function(tags) {
+	    var selects = document.getElementsByTagName('select');
+	    for (var i = 0; i < selects.length; i++) {
+	      var select = selects[i];
+	      if (select.id.indexOf('tagSelect-') == 0) {
+	        setSelectElement(select.id, getSelectTitle(select), tags);
+	      }
+	    }
+	  }
+	  AjaxServices.getTags(type, callBack);
+	};
 
-// retrieves tagged object id from editorId where it is hidden
-function parseTagged(editorId) {
-  var parts = editorId.split("@");
-  if (parts.length == 2) {
-    return parts[0];
-  } else {
-    return "";
-  }
-}
+	// retrieves tagged object id from editorId where it is hidden
+	this.parseTagged = function(editorId) {
+	  return $('#taggable-' + editorId).val();
+	};
+}).call(window, jQuery);
