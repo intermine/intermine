e04f4d58613de4b10e584a9d561b04c2e00e3d87
Julie Sullivan <julie@flymine.org>
2012-07-31 13:23:55 +0100

    update year
    Former-commit-id: 0ad3dc15484d2b98d7f2b25de9f62e077906ce80

--- intermine/pathquery/main/src/org/intermine/pathquery/PathConstraint.java
+++ intermine/pathquery/main/src/org/intermine/pathquery/PathConstraint.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import org.intermine.objectstore.query.ConstraintOp;
 
 /*
- * Copyright (C) 2002-2011 FlyMine
+ * Copyright (C) 2002-2012 FlyMine
  *
  * This code may be freely distributed and modified under the
  * terms of the GNU Lesser General Public Licence.  This should
