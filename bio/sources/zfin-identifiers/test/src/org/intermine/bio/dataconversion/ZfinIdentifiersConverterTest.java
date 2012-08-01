ead4a2e579e22965d6daa019b244b6bf0a89fafe
Fengyuan Hu <fengyuan.huu@gmail.com>
2012-08-01 15:23:51 +0100

    added flybase, sgd, zfin identifiers converters
    Former-commit-id: 3b570615dd6ef4895122f026976470398ae6d68e

---
+++ bio/sources/zfin-identifiers/test/src/org/intermine/bio/dataconversion/ZfinIdentifiersConverterTest.java
@@ -0,0 +1,26 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2012 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import org.intermine.dataconversion.ItemsTestCase;
+
+/**
+ * Unit test for ZfinIdentifiersConverter
+ * @author Fengyuan Hu
+ */
+public class ZfinIdentifiersConverterTest extends ItemsTestCase
+{
+
+    public ZfinIdentifiersConverterTest(String arg) {
+        super(arg);
+    }
+
+}
