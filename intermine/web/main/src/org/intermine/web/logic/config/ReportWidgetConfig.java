bb0b0c5f8ec8287a8a065b6e335afab16ddafa2a
Radek <dev@radekstepan.com>
2012-09-05 15:49:05 +0100

    a simple hello world of a report widget
    Former-commit-id: 6a7ba406821202d5a475b726f97b051d099d7e77

---
+++ intermine/web/main/src/org/intermine/web/logic/config/ReportWidgetConfig.java
@@ -0,0 +1,26 @@
+package org.intermine.web.logic.config;
+
+/*
+ * Copyright (C) 2002-2012 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+import java.util.HashSet;
+import java.util.Set;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * One instance of this class represents a Report Widget configuration.
+ * @author Radek
+ *
+ */
+public class ReportWidgetConfig
+{
+
+}
