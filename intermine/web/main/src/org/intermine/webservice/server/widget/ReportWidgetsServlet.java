88e64fc4c5ec20c43504f9f7048e860170254291
Radek <dev@radekstepan.com>
2012-09-12 18:58:10 +0100

    loading file, replacing error message
    Former-commit-id: 1c4977b99082601ee647491e7aa5350eef51dce4

--- intermine/web/main/src/org/intermine/webservice/server/widget/ReportWidgetsServlet.java
+++ intermine/web/main/src/org/intermine/webservice/server/widget/ReportWidgetsServlet.java
@@ -10,11 +10,13 @@ package org.intermine.webservice.server.widget;
  *
  */
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringReader;
-import java.util.Map;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -24,6 +26,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.apache.commons.lang.StringUtils;
 import org.intermine.pathquery.PathQuery;
 import org.intermine.pathquery.PathQueryBinding;
 import org.intermine.webservice.server.exceptions.InternalErrorException;
@@ -52,8 +55,7 @@ public class ReportWidgetsServlet extends HttpServlet
 	private static final String CONFIG      = "#@+CONFIG";
 	
 	// The config for all Widgets, serialized from `webconfig-model.xml`
-	private static JSONObject widgetsWebConfig = null;
-	private static String test = null;
+	private static JSONArray widgetsWebConfig = null;
 	
     /**
      * {@inheritDoc}}
@@ -64,19 +66,10 @@ public class ReportWidgetsServlet extends HttpServlet
         runService(request, response);
     }
     
-	private void runService(HttpServletRequest request, HttpServletResponse response) {
-    	// Do we have config?
-        if (widgetsWebConfig == null) {
-        	parseXML();
-        }
-    	
-    	// Get request params.
-        String paramId = request.getParameter("id");
-        String paramCallback = request.getParameter("callback");
-        
+	private void runService(HttpServletRequest request, HttpServletResponse response) {        
         // Set JavaScript header.
         response.setCharacterEncoding("UTF-8");
-        response.setContentType("application/javascript");        
+        response.setContentType("application/javascript");
         
         // The response writer.
         PrintWriter pw = null;
@@ -84,20 +77,88 @@ public class ReportWidgetsServlet extends HttpServlet
 			pw = response.getWriter();
 		} catch (IOException e) { }
         
-		if (pw != null) {
-			pw.write(test);
+		if (pw != null) {			
+	    	// Do we have config?
+	        if (widgetsWebConfig == null) {
+	        	try {
+					parseXML();
+				} catch (ReportWidgetException e) {
+					pw.write("new Error(\"Report Widget Config: " + e.getMessage() + "\");");
+				}
+	        }
+	        
+	    	// Do we have config?
+	        if (widgetsWebConfig != null) {
+	        	// Get request params.
+	            String paramId = request.getParameter("id");
+	            String paramCallback = request.getParameter("callback");	        	
+	        	
+	            if (paramId == null) {
+	            	pw.write("new Error(\"Widget `id` not provided\");");
+	            } else if (paramCallback == null) {
+	            	pw.write("new Error(\"Widget `callback` not provided\");");
+	            } else {
+	            	// Find the relevant widget.
+	            	JSONObject widget = null; 
+	            	for (int i = 0; i < widgetsWebConfig.length(); i++) {
+	            		try {
+							JSONObject w = widgetsWebConfig.getJSONObject(i);
+							if (w.get("id").equals(paramId)) {
+								widget = w;
+							}
+						} catch (JSONException e) {
+							pw.write("new Error(\"" + e.getMessage() + "\");");
+						}
+	            	}
+	            	
+	            	if (widget != null) {
+	            		
+	            		// Load the assoc file.
+	    				String file = null;
+	    				try {
+	    					file = readInFile(getServletContext().getRealPath("/js/widgets/" + paramId + ".js"));
+	    				} catch (IOException e) { }
+	    				if (file != null) {
+	    					// Remove the leading line preventing direct JS file use.
+	    					String[] arr = file.split("\n");
+	    					file = StringUtils.join(Arrays.copyOfRange(arr, 2, arr.length), "\n");
+	    					
+					        // Write the output.
+					        pw.write(widgetsWebConfig.toString());
+	    				} else {
+	    					pw.write("new Error(\"Could not load widget file `/js/widgets/" + paramId + ".js`\");");
+	    				}	            		
+	            	} else {
+	            		pw.write("new Error(\"Could not find widget `" + paramId + "`\");");
+	            	}
+	            }
+	        }
 		}
     }
     
-	private void parseXML() {		
+    private String readInFile(String path) throws java.io.IOException {
+        StringBuffer fileData = new StringBuffer(1000);
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        char[] buf = new char[1024];
+        int numRead=0;
+        while((numRead=reader.read(buf)) != -1){
+            String readData = String.valueOf(buf, 0, numRead);
+            fileData.append(readData);
+            buf = new char[1024];
+        }
+        reader.close();
+        return fileData.toString();
+    }
+	
+	private void parseXML() throws ReportWidgetException {		
         String path = getServletContext().getRealPath("/WEB-INF/webconfig-model.xml");
         SAXParser parser = parser();
         try {
             parser.parse(new File(path), handler());
         } catch (IOException e) {
-            throw new InternalErrorException(e);
+        	throw new ReportWidgetException(e.getMessage());
         } catch (SAXException e) {
-            throw new InternalErrorException(e);
+            throw new ReportWidgetException(e.getMessage());
         }
         
 	}
@@ -137,7 +198,7 @@ public class ReportWidgetsServlet extends HttpServlet
             }
 
             public void endDocument() throws SAXException {
-            	test = widgets.toString();
+            	widgetsWebConfig = widgets;
             }
 
             public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
@@ -153,74 +214,92 @@ public class ReportWidgetsServlet extends HttpServlet
                         }
                         try {
 							widget.put(attributes.getLocalName(i), o);
-						} catch (JSONException e) { }
+						} catch (JSONException e) {
+							throw new SAXException("Error saving attributes of `<query>`.");
+						}
                     }
             	} else {
             		// If we have a current widget...
             		if (widget != null) {
-            			try {
-            				// Is it a PathQuery or its child?
-            				if ("pathQuery".equals(qName) || pathQuery != null) {
-    		                    if (pathQueries == null) pathQueries = new JSONObject();
-    		                    
-    		                    // Open?
-    		                    if (pathQuery == null) {
-    		                    	pathQuery = "<query";
-    		                    } else {
-    		                    	pathQuery += "<" + qName;
-    		                    }
-    		                    
-    		                    // Query attributes.
-    		                    for (int i = 0; i < attributes.getLength(); i++) {    		                    	
-    		                    	String k = attributes.getLocalName(i);
-    		                        String v = attributes.getValue(i);
-    		                        
-    		                    	// Check for PQ name.
-    		                    	if ("pathQuery".equals(qName) && ("name".equals(k) || "title".equals("k"))) {
-    		                    		pathQueryName = v;
-    		                    	}
-    		                        
-    		                    	// Stringify back.
-    		                        pathQuery += " " + k + "=\"" + v + "\"";
-    		                    }
-    		                    
-    		                    // Close this level.
-    		                    pathQuery += ">";
-    		                    
-            				} else {
-                				// Parse the attrs into JSON.
-    							JSONObject attrs = new JSONObject();
-    		                    for (int i = 0; i < attributes.getLength(); i++) {
-    		                        Object o = attributes.getValue(i);
-    		                        // Make into Boolean value.
-    		                        if ("true".equals(o) || "false".equals(o)) {
-    		                            o = Boolean.valueOf(o.toString());
-    		                        }
-    		                        attrs.put(attributes.getLocalName(i), o);
-    		                    }
-    		                    
-    		                    // Is it a dependency?
-    		                    if ("dependency".equals(qName)) {
-    		                    	if (dependencies == null) dependencies = new JSONArray();
-    		                    	dependencies.put(attrs);
-    		                    } else {
-    		                    	// Do we have a PathQuery "open"?
-    		                    	if (pathQuery != null) {
-    		                    		pathQuery += qName + "/";
-    		                    	} else {
-    			                    	// A simple key value then.
-    			                    	String key = attrs.getString("key");
-    			                    	Object val = attrs.getString("value");
-    			                    	if (key != null && val != null) {
-    			                    		if (clientConfig == null) clientConfig = new JSONObject();
-    			                    		clientConfig.put(key, val);
-    			                    	} else {
-    			                    		// Exception.
-    			                    	}	
-    		                    	}
-    		                    }
-            				}
-						} catch (JSONException e) { }
+        				// Is it a PathQuery or its child?
+        				if ("pathQuery".equals(qName) || pathQuery != null) {
+		                    if (pathQueries == null) pathQueries = new JSONObject();
+		                    
+		                    // Open?
+		                    if (pathQuery == null) {
+		                    	pathQuery = "<query";
+		                    } else {
+		                    	pathQuery += "<" + qName;
+		                    }
+		                    
+		                    // Query attributes.
+		                    for (int i = 0; i < attributes.getLength(); i++) {    		                    	
+		                    	String k = attributes.getLocalName(i);
+		                        String v = attributes.getValue(i);
+		                        
+		                    	// Check for PQ name.
+		                    	if ("pathQuery".equals(qName) && ("name".equals(k) || "title".equals("k"))) {
+		                    		pathQueryName = v;
+		                    	}
+		                        
+		                    	// Stringify back.
+		                        pathQuery += " " + k + "=\"" + v + "\"";
+		                    }
+		                    
+		                    // Close this level.
+		                    pathQuery += ">";
+		                    
+        				} else {
+            				// Parse the attrs into JSON.
+							JSONObject attrs = new JSONObject();
+		                    for (int i = 0; i < attributes.getLength(); i++) {
+		                        Object o = attributes.getValue(i);
+		                        // Make into Boolean value.
+		                        if ("true".equals(o) || "false".equals(o)) {
+		                            o = Boolean.valueOf(o.toString());
+		                        }
+		                        try {
+									attrs.put(attributes.getLocalName(i), o);
+								} catch (JSONException e) {
+									throw new SAXException("Error saving attributes of `<" + qName + ">`.");
+								}
+		                    }
+		                    
+		                    // Is it a dependency?
+		                    if ("dependency".equals(qName)) {
+		                    	if (dependencies == null) dependencies = new JSONArray();
+		                    	dependencies.put(attrs);
+		                    } else {
+		                    	// Do we have a PathQuery "open"?
+		                    	if (pathQuery != null) {
+		                    		pathQuery += qName + "/";
+		                    	} else {
+			                    	// A simple key value then.
+			                    	String key;
+									try {
+										key = attrs.getString("key");
+									} catch (JSONException e) {
+										throw new SAXException("This key-value pair does not have the `key` attr.");
+									}
+			                    	Object val;
+									try {
+										val = attrs.getString("value");
+									} catch (JSONException e) {
+										throw new SAXException("This key-value pair does not have the `value` attr.");
+									}
+			                    	if (key != null && val != null) {
+			                    		if (clientConfig == null) clientConfig = new JSONObject();
+			                    		try {
+											clientConfig.put(key, val);
+										} catch (JSONException e) {
+											throw new SAXException("Could not save client config key-value pair.");
+										}
+			                    	} else {
+			                    		throw new SAXException("This key-value pair does not have the `key` and `attr` attrs defined.");
+			                    	}	
+		                    	}
+		                    }
+        				}
             		}
             	}
             }
@@ -232,19 +311,25 @@ public class ReportWidgetsServlet extends HttpServlet
             		if (dependencies != null) {
             			try {
 							widget.put("dependencies", dependencies);
-						} catch (JSONException e) { }
+						} catch (JSONException e) {
+							throw new SAXException("Error saving `dependencies` on widget.");
+						}
             		}
             		if (pathQueries != null) {
             			// Save on client config.
             			if (clientConfig == null) clientConfig = new JSONObject();
             			try {
             				clientConfig.put("pathQueries", pathQueries);
-						} catch (JSONException e) { }
+						} catch (JSONException e) {
+							throw new SAXException("Error saving `pathQuery` on widget.");
+						}
             		}
             		if (clientConfig != null) {
             			try {
 							widget.put("config", clientConfig);
-						} catch (JSONException e) { }
+						} catch (JSONException e) {
+							throw new SAXException("Error saving `config` on widget.");
+						}
             		}
             		
             		// Save the widget.
@@ -259,7 +344,7 @@ public class ReportWidgetsServlet extends HttpServlet
             	} else if ("pathQuery".equals(qName)) {
             		// Do we have the PQ name?
             		if (pathQueryName == null) {
-            			// Exception.
+            			throw new SAXException("PathQuery does not have a `name` or `title` attr defined. See, not I can't even tell you which PathQuery this is...");
             		}
             		
             		// Close us up.
@@ -267,14 +352,17 @@ public class ReportWidgetsServlet extends HttpServlet
             		
             		// Convert to PathQuery.
             		PathQuery pq = PathQueryBinding.unmarshalPathQuery(new StringReader(pathQuery), PathQuery.USERPROFILE_VERSION);
-            		if (!pq.isValid()) {
-            			// Exception.
-            		}
+            		// Do not check the validity of this PQ, as we would check the Model too even though we may run this PQ against a different mine.
+            		//if (!pq.isValid()) {
+					//	throw new SAXException("PathQuery `" + pathQueryName + "` is invalid (" + StringUtils.join(pq.verifyQuery(), ", ") + ").");
+            		//}
             		
             		// Save it.
             		try {
 						pathQueries.put(pathQueryName, new JSONObject(pq.toJson()));
-					} catch (JSONException e) { }
+					} catch (JSONException e) {
+						throw new SAXException("Error saving `" + pathQueryName + "` on `pathQueries`.");
+					}
             		// Reset.
             		pathQuery = null;
             		pathQueryName = null;
@@ -291,4 +379,20 @@ public class ReportWidgetsServlet extends HttpServlet
         return handler;
     }
 
+    class ReportWidgetException extends Exception {
+    	
+    	private static final long serialVersionUID = 1L;
+		private String message;
+    	
+		public ReportWidgetException(String message) {
+			super(message);
+			this.message = message;
+		}
+		
+		public String getError() {
+			return message;
+		}
+    	
+    }
+    
 }
