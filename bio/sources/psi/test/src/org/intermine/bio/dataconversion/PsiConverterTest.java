32740d3db4dfcd0790eec85da4094f89b16e4da0
Fengyuan Hu <fengyuan.huu@gmail.com>
2012-10-09 12:36:02 +0100

    update psi converter - id resolver

--- bio/sources/psi/test/src/org/intermine/bio/dataconversion/PsiConverterTest.java
+++ bio/sources/psi/test/src/org/intermine/bio/dataconversion/PsiConverterTest.java
@@ -35,10 +35,9 @@ public class PsiConverterTest extends ItemsTestCase
         itemWriter = new MockItemWriter(new HashMap<String, Item>());
         converter = new PsiConverter(itemWriter,  Model.getInstanceByName("genomic"));
         converter.setIntactOrganisms("7227");
-        MockIdResolverFactory resolverFactory = new MockIdResolverFactory("Gene");
-        resolverFactory.addResolverEntry("7227", "FBgn001", Collections.singleton("FBgn001"));
-        resolverFactory.addResolverEntry("7227", "FBgn002", Collections.singleton("FBgn002"));
-        converter.flyResolverFactory = resolverFactory;
+        converter.rslv = IdResolverService.getMockIdResolver("Gene");
+        converter.rslv.addResolverEntry("7227", "FBgn001", Collections.singleton("FBgn001"));
+        converter.rslv.addResolverEntry("7227", "FBgn002", Collections.singleton("FBgn002"));
         super.setUp();
     }
 
