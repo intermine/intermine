17fd4a0ac6de53d6a3fca3adb2f4b1c66d0f0623
Alex Kalderimis <alex.kalderimis@gmail.com>
2012-10-15 19:31:19 +0100

    Whitespace
    Former-commit-id: 7c113ba1a1072e263e2ab72d390dea9390769b8a

--- intermine/web/main/src/org/intermine/webservice/server/lists/ListSharingInvitationService.java
+++ intermine/web/main/src/org/intermine/webservice/server/lists/ListSharingInvitationService.java
@@ -13,71 +13,77 @@ import org.intermine.api.profile.Profile;
 import org.intermine.web.context.InterMineContext;
 import org.intermine.webservice.server.core.JSONService;
 import org.intermine.webservice.server.exceptions.BadRequestException;
+import org.intermine.webservice.server.exceptions.MissingParameterException;
 import org.intermine.webservice.server.exceptions.ResourceNotFoundException;
 import org.intermine.webservice.server.exceptions.ServiceException;
 import org.intermine.webservice.server.exceptions.ServiceForbiddenException;
 
 public class ListSharingInvitationService extends JSONService {
 
-	private static final Logger LOG = Logger.getLogger(ListSharingInvitationService.class);
-	
-	public ListSharingInvitationService(InterMineAPI im) {
-		super(im);
-	}
-	
-	protected String getResultsKey() {
-		return "invitation";
-	}
+    private static final Logger LOG = Logger.getLogger(ListSharingInvitationService.class);
+    
+    public ListSharingInvitationService(InterMineAPI im) {
+        super(im);
+    }
+    
+    protected String getResultsKey() {
+        return "invitation";
+    }
 
-	@Override
-	protected void execute() throws ServiceException {
-		Profile p = getPermission().getProfile();
-		if (!p.isLoggedIn()) {
-			throw new ServiceForbiddenException("You must be logged in to use this service");
-		}
-		String bagName = request.getParameter("list");
-		if (StringUtils.isBlank(bagName)) {
-			throw new BadRequestException("Missing parameter: 'list'");
-		}
-		InterMineBag bag = p.getSavedBags().get(bagName);
-		if (bag == null) {
-			throw new ResourceNotFoundException("You do not own a list called " + bagName);
-		}
-		String invitee = request.getParameter("to");
-		if (StringUtils.isBlank(invitee)) {
-			throw new BadRequestException("Missing parameter: 'to'");
-		}
-		SharingInvite invite = SharedBagManager.inviteToShare(bag, invitee);
-		
-		String sendEmail = request.getParameter("send-email");
-		if ("true".equalsIgnoreCase(sendEmail) || "1".equals(sendEmail)) {
-			notifyInvitee(invite);
-		}
-		Map<String, Object> toSerialise = new HashMap<String, Object>();
-		toSerialise.put("invite-token", invite.getToken());
-		toSerialise.put("list", bag.getName());
-		toSerialise.put("invitee", invitee);
-		
-		addResultItem(toSerialise, false);
-	}
-	
-	private final static String EMAIL_PROPERTY = "sharing-invite";
+    @Override
+    protected void execute() throws ServiceException {
+        Profile p = getPermission().getProfile();
+        if (!p.isLoggedIn()) {
+            throw new ServiceForbiddenException("You must be logged in to use this service");
+        }
+        String bagName = request.getParameter("list");
+        if (StringUtils.isBlank(bagName)) {
+            throw new MissingParameterException("list");
+        }
+        InterMineBag bag = p.getSavedBags().get(bagName);
+        if (bag == null) {
+            throw new ResourceNotFoundException("You do not own a list called " + bagName);
+        }
+        String invitee = request.getParameter("to");
+        if (StringUtils.isBlank(invitee)) {
+            throw new MissingParameterException("to");
+        }
+        SharingInvite invite = SharedBagManager.inviteToShare(bag, invitee);
+        
+        String sendEmail = request.getParameter("notify");
+        if ("true".equalsIgnoreCase(sendEmail) || "1".equals(sendEmail)) {
+            notifyInvitee(invite);
+        }
+        Map<String, Object> toSerialise = new HashMap<String, Object>();
+        toSerialise.put("invite-token", invite.getToken());
+        toSerialise.put("list", bag.getName());
+        toSerialise.put("invitee", invitee);
+        
+        addResultItem(toSerialise, false);
+    }
+    
+    private final static String EMAIL_PROPERTY = "sharing-invite";
 
-	private void notifyInvitee(final SharingInvite invite) {
-		(new Thread() { // Send message in background.
-			@Override
-			public void run() {
-				final InterMineBag bag = invite.getBag();
-				final Profile owner = getPermission().getProfile();
-				try {
-					InterMineContext.getEmailer().email(
-						invite.getInvitee(), EMAIL_PROPERTY,
-						bag.getName(), owner.getName(), invite.getToken());	
-				} catch (Exception e) {
-					LOG.error("Could not send invitation (" + invite + ")", e);
-				}
-			}		
-		}).start();
-	}
+    private void notifyInvitee(final SharingInvite invite) {
+        (new Thread() { // Send message in background.
+            @Override
+            public void run() {
+                final InterMineBag bag = invite.getBag();
+                final Profile owner = getPermission().getProfile();
+                try {
+                    InterMineContext.getEmailer().email(
+                        invite.getInvitee(), EMAIL_PROPERTY,
+                        owner.getName(),
+                        bag.getType(), bag.getName(), bag.getSize(),
+                        webProperties.getProperty("webapp.baseurl"),
+                        webProperties.getProperty("webapp.path"),
+                        invite.getToken(),
+                        webProperties.getProperty("project.title"));
+                } catch (Exception e) {
+                    LOG.error("Could not send invitation (" + invite + ")", e);
+                }
+            }        
+        }).start();
+    }
 
 }
