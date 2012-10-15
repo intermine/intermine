4dacd2ba9162d107c6e1f61334855174b2cb44c7
Alex Kalderimis <alex.kalderimis@gmail.com>
2012-10-15 19:27:56 +0100

    Handle missing templates nicely. Whitespace

--- intermine/web/main/src/org/intermine/util/Emailer.java
+++ intermine/web/main/src/org/intermine/util/Emailer.java
@@ -7,40 +7,45 @@ import java.util.Properties;
 
 import javax.mail.MessagingException;
 
+import org.apache.commons.lang.StringUtils;
+
 public class Emailer {
 
-	private static final String PREFIX = "mail.regarding.";
-	
-	private final Properties properties;
-	
-	public Emailer(Properties properties) {
-		this.properties = properties;
-	}
-	
-	public void email(String to, String messageKey) throws MessagingException {
-		String subject = properties.getProperty(PREFIX + messageKey + ".subject");
-		String body = properties.getProperty(PREFIX + messageKey + ".body");
-		MailUtils.email(to, subject, body, properties);
-	}
-	
-	public void email(String to, String messageKey, Object... vars) throws MessagingException {
-		String subject = properties.getProperty(PREFIX + messageKey + ".subject");
-		String bodyFmt = properties.getProperty(PREFIX + messageKey + ".body");
-		String body = String.format(bodyFmt, vars);
-		MailUtils.email(to, subject, body, properties);
-	}
-	
-	public void welcome(String to) throws MessagingException {
-		MailUtils.welcome(to, properties);
-	}
-	
-	public String subscribeToList(String address) throws MessagingException {
-		String mailingList = properties.getProperty("mail.mailing-list");
+    private static final String PREFIX = "mail.regarding.";
+    
+    private final Properties properties;
+    
+    public Emailer(Properties properties) {
+        this.properties = properties;
+    }
+    
+    public void email(String to, String messageKey) throws MessagingException {
+        String subject = properties.getProperty(PREFIX + messageKey + ".subject");
+        String body = properties.getProperty(PREFIX + messageKey + ".body");
+        MailUtils.email(to, subject, body, properties);
+    }
+    
+    public void email(String to, String messageKey, Object... vars) throws MessagingException {
+        String subject = properties.getProperty(PREFIX + messageKey + ".subject");
+        String bodyFmt = properties.getProperty(PREFIX + messageKey + ".body");
+        if (StringUtils.isBlank(bodyFmt)) {
+            throw new MessagingException("No email template configured for " + messageKey);
+        }
+        String body = String.format(bodyFmt, vars);
+        MailUtils.email(to, subject, body, properties);
+    }
+    
+    public void welcome(String to) throws MessagingException {
+        MailUtils.welcome(to, properties);
+    }
+    
+    public String subscribeToList(String address) throws MessagingException {
+        String mailingList = properties.getProperty("mail.mailing-list");
         if (!isBlank(mailingList)) {
             MailUtils.subscribe(address, properties);
             return mailingList;
         }
         return null;
-	}
-	
+    }
+    
 }
