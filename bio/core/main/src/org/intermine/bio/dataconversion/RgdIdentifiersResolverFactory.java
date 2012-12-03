b78334f02ab28b1d26e0fe08bc9b8c5b71dcf6cc
Fengyuan Hu <fengyuan.huu@gmail.com>
2012-12-03 16:06:53 +0000

    fixed a npe

--- bio/core/main/src/org/intermine/bio/dataconversion/RgdIdentifiersResolverFactory.java
+++ bio/core/main/src/org/intermine/bio/dataconversion/RgdIdentifiersResolverFactory.java
@@ -49,6 +49,7 @@ public class RgdIdentifiersResolverFactory extends IdResolverFactory
 
     /**
      * Construct with SO term of the feature type.
+     * TODO as class name is fixed as gene, this method is not useful
      * @param soTerm the feature type to resolve
      */
     public RgdIdentifiersResolverFactory(String clsName) {
