b7800a966b01d5368dd2e804a7c7ad9d6d0c811b
Alex Kalderimis <alex.kalderimis@gmail.com>
2012-12-04 14:50:24 +0000

    Allow query monitor timeouts to record their queries

--- intermine/web/main/src/org/intermine/web/struts/PollQueryAction.java
+++ intermine/web/main/src/org/intermine/web/struts/PollQueryAction.java
@@ -132,11 +132,7 @@ public class PollQueryAction extends InterMineAction
             } else {
                 trail = "|results." + qid;
             }
-            if (pr == null) {
-                LOG.info("PagedTable is null for '" + qid + "', umm, wat?");
-            } else {
-                request.setAttribute(Constants.QUERY, pr.getPathQuery());
-            }
+            request.setAttribute(Constants.QUERY, controller.getQuery());
             ForwardParameters fp =  new ForwardParameters(mapping.findForward("results"))
                                     .addParameter("trail", trail)
                                     .addParameter("table", "results." + qid);
