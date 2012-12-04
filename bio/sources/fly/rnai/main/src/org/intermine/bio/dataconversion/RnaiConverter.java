cc4e199ef0b780d25662b3f51baace0318c32754
Fengyuan Hu <fengyuan.huu@gmail.com>
2012-12-04 13:13:35 +0000

    fixed a db connection error
    Former-commit-id: cbd7452a0d62b37cc5795aa5dd32e8a32c494cc4

--- bio/sources/fly/rnai/main/src/org/intermine/bio/dataconversion/RnaiConverter.java
+++ bio/sources/fly/rnai/main/src/org/intermine/bio/dataconversion/RnaiConverter.java
@@ -12,14 +12,11 @@ package org.intermine.bio.dataconversion;
 
 import java.io.BufferedReader;
 import java.io.Reader;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.intermine.bio.dataconversion.IdResolver;
-import org.intermine.bio.dataconversion.IdResolverService;
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
 import org.intermine.objectstore.ObjectStoreException;
@@ -40,9 +37,9 @@ public class RnaiConverter extends BioFileConverter
     private Map<String, String> publications = new HashMap<String, String>();
     private Map<String, String> screens = new HashMap<String, String>();
     private static final String TAXON_FLY = "7227";
-    private static final String NCBI = "NCBI Entrez Gene identifiers"; 
+    private static final String NCBI = "NCBI Entrez Gene identifiers";
     private Item screen;
-    
+
     protected IdResolver rslv;
 
     /**
@@ -134,7 +131,7 @@ public class RnaiConverter extends BioFileConverter
         String geneRefId = null;
         String fbgn = line[2];    // FBgn
         String ncbi = line[1];
-        
+
         if (StringUtils.isEmpty(fbgn)) {
             // some only have entrez IDs and no FBgns.  try both
             geneRefId = getGene(ncbi);
@@ -184,7 +181,7 @@ public class RnaiConverter extends BioFileConverter
         if (identifier == null) {
             throw new RuntimeException("geneSymbol can't be null");
         }
-        
+
         if (rslv == null || !rslv.hasTaxon(TAXON_FLY)) {
             return null;
         }
