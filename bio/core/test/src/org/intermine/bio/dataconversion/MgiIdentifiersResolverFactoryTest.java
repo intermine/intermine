6b304463b4bdd6b8de1fb817c16297d279101a0b
Fengyuan Hu <fengyuan.huu@gmail.com>
2013-01-18 11:46:58 +0000

    added tests for MgiIdentifiersResolverFactory
    Former-commit-id: f9de729b4a30660267629fb2a02c2e134f36f4aa

---
+++ bio/core/test/src/org/intermine/bio/dataconversion/MgiIdentifiersResolverFactoryTest.java
@@ -0,0 +1,47 @@
+package org.intermine.bio.dataconversion;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+import junit.framework.TestCase;
+
+/**
+ * MgiIdentifiersResolverFactory Unit Tests
+ *
+ * @author Fengyuan Hu
+ *
+ */
+public class MgiIdentifiersResolverFactoryTest extends TestCase {
+    MgiIdentifiersResolverFactory factory;
+    String mgiDataFile = "resources/mgi.data.sample";
+
+    public MgiIdentifiersResolverFactoryTest() {
+    }
+
+    public MgiIdentifiersResolverFactoryTest(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        factory = new MgiIdentifiersResolverFactory();
+        IdResolverFactory.resolver = new IdResolver();
+    }
+
+    public void testCreateFromFile() throws Exception {
+        File f = new File(mgiDataFile);
+        if (!f.exists()) {
+            fail("data file not found");
+        }
+
+        factory.createFromFile(f);
+        assertTrue(IdResolverFactory.resolver.getTaxons().size() == 1);
+        assertEquals(new LinkedHashSet<String>(Arrays.asList(new String[] {"10090"})), IdResolverFactory.resolver.getTaxons());
+        assertEquals("MGI:87861", IdResolverFactory.resolver.resolveId("10090", "abn").iterator().next());
+        assertEquals("MGI:87862", IdResolverFactory.resolver.resolveId("10090", "ENSMUSG00000066583").iterator().next());
+    }
+
+}
