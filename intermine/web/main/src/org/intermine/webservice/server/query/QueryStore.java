4fbd755af15edc89e5a3c95ab5e2e5aef07db9d4
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-03-21 23:50:33 +0000

    Added the facilities for efficient bi-directional lookup.
    At the expense of some extra storage of course. But this should be
    more than compensated by avoiding storing duplicate queries.
    
    
    Former-commit-id: 48dafcb2d4e72ecd99800c8e03dfa6e5c0b69ab1

--- intermine/web/main/src/org/intermine/webservice/server/query/QueryStore.java
+++ intermine/web/main/src/org/intermine/webservice/server/query/QueryStore.java
@@ -3,37 +3,46 @@ package org.intermine.webservice.server.query;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.intermine.pathquery.PathQuery;
 import org.intermine.pathquery.PathQueryBinding;
 import org.intermine.webservice.server.exceptions.BadRequestException;
 
 public class QueryStore {
-    private static List<String> queries = new ArrayList<String>(1024);
+    private static final List<String> queries = new ArrayList<String>();
+    private static final Map<String, Integer> indexes = new HashMap<String, Integer>();
 
     public static int putQuery(String xml) {
+        Integer id = indexes.get(xml);
+        if (id != null) {
+            return id;
+        }
         try {
             PathQueryBinding.unmarshalPathQuery(new StringReader(xml), PathQuery.USERPROFILE_VERSION);
         } catch (Exception e) {
             String message = "XML is not well formatted.";
             throw new BadRequestException(message, e);
         }
+        id = Integer.valueOf(queries.size());
         queries.add(xml);
-        return queries.size() - 1;
+        indexes.put(xml, id);
+        return id.intValue();
     }
 
     public static String getQuery(String key) {
-        int idx;
+        Integer id;
         try {
-            idx = Integer.parseInt(key, 10);
+            id = Integer.valueOf(key, 10);
         } catch (NumberFormatException e) {
             throw new BadRequestException("qid must be an integer", e);
         }
-        String ret = queries.get(idx);
-        if (ret == null) {
+        try {
+            return queries.get(id);
+        } catch (IndexOutOfBoundsException e) {
             throw new BadRequestException("qid " + key + " not in QueryStore");
         }
-        return ret;
     }
 
 }
