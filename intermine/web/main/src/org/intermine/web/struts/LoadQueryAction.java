1533c411653744ee402034f6b2c37974b96a11c1
Julie Sullivan <julie@flymine.org>
2013-04-19 16:24:44 +0100

    add a way to link to querybuilder using list name

--- intermine/web/main/src/org/intermine/web/struts/LoadQueryAction.java
+++ intermine/web/main/src/org/intermine/web/struts/LoadQueryAction.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpSession;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.actions.DispatchAction;
+import org.apache.struts.action.ActionMessage;
 import org.intermine.api.InterMineAPI;
 import org.intermine.api.bag.BagManager;
 import org.intermine.api.profile.InterMineBag;
@@ -32,6 +32,7 @@ import org.intermine.pathquery.PathQueryBinding;
 import org.intermine.web.logic.config.WebConfig;
 import org.intermine.web.logic.export.http.TableExporterFactory;
 import org.intermine.web.logic.export.http.TableHttpExporter;
+import org.intermine.web.logic.pathqueryresult.PathQueryResultHelper;
 import org.intermine.web.logic.results.PagedTable;
 import org.intermine.web.logic.session.SessionMethods;
 
@@ -41,7 +42,7 @@ import org.intermine.web.logic.session.SessionMethods;
  *
  * @author Kim Rutherford
  */
-public class LoadQueryAction extends DispatchAction
+public class LoadQueryAction extends InterMineDispatchAction
 {
     /**
      * Load a query from path query XML passed as a request parameter.
@@ -117,5 +118,47 @@ public class LoadQueryAction extends DispatchAction
                 }
             }
         }
+    }
+    
+    /**
+     * Load a query using given list
+     * @param mapping The ActionMapping used to select this instance
+     * @param form The optional ActionForm bean for this request (if any)
+     * @param request The HTTP request we are processing
+     * @param response The HTTP response we are creating
+     * @return an ActionForward object defining where control goes next
+     * @exception Exception if the application business logic throws
+     *  an exception
+     */
+    public ActionForward list(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response)
+        throws Exception {
+        HttpSession session = request.getSession();
+        final InterMineAPI im = SessionMethods.getInterMineAPI(session);
+        Profile profile = SessionMethods.getProfile(session);
+        
+        String listName = request.getParameter("name");
+        if (listName == null) {
+            String msg = "No list name specified in URL";            
+            recordError(new ActionMessage("query.load.noName"), request);
+            return mapping.findForward("errors");
+        }
+        BagManager bagManager = im.getBagManager();
+        
+        Map<String, InterMineBag> allBags = bagManager.getBags(profile);
+        if (allBags == null) {
+            recordError(new ActionMessage("query.load.notFound"), request);
+            return mapping.findForward("errors");
+        }
+        InterMineBag bag = allBags.get(listName);
+        if (bag == null) {
+            recordError(new ActionMessage("query.load.notFound"), request);
+            return mapping.findForward("errors");
+        }
+        WebConfig webConfig = SessionMethods.getWebConfig(request);
+        PathQuery query = PathQueryResultHelper.makePathQueryForBag(bag, webConfig, im.getModel());
+        
+        SessionMethods.loadQuery(query, session, response);
+        return mapping.findForward("query");
     }
 }
