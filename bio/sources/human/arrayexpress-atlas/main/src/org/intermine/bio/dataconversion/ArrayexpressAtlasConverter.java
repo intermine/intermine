b9de803eb2722145bd908f383a36d93467880d8b
Fengyuan Hu <fengyuan.huu@gmail.com>
2013-06-13 13:43:38 +0100

    checkstyle
    Former-commit-id: 6b2c5497cf356f7107f9cdb2253fa36f0aecb3b1

--- bio/sources/human/arrayexpress-atlas/main/src/org/intermine/bio/dataconversion/ArrayexpressAtlasConverter.java
+++ bio/sources/human/arrayexpress-atlas/main/src/org/intermine/bio/dataconversion/ArrayexpressAtlasConverter.java
@@ -14,7 +14,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.Reader;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
