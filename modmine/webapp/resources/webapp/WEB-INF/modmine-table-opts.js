1f0eace6c918fd1a378a6495091fa92636cc31e5
sergio contrino <contrino@gmail.com>
2013-07-23 16:02:48 +0100

    adds table options file for modmine

---
+++ modmine/webapp/resources/webapp/WEB-INF/modmine-table-opts.js
@@ -0,0 +1,17 @@
+jQuery(function() { // Wait until the page is ready
+
+if (intermine) { // Don't do it unless intermine code loaded.
+
+intermine.setOptions({ // Set your own set of options.
+
+CellPreviewTrigger: 'hover'
+
+/* And any other preferences in here... */
+
+});
+
+}
+
+});
+
+ 
