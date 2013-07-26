69fa187db8fbef1acd41ce0691bb20bf23128c28
Julie Sullivan <julie@flymine.org>
2013-07-26 15:25:05 +0100

    fix breadcrumbs for QB queries

--- intermine/web/main/src/org/intermine/web/struts/ModifyQueryChangeAction.java
+++ intermine/web/main/src/org/intermine/web/struts/ModifyQueryChangeAction.java
@@ -14,12 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionMessage;
-import org.intermine.api.InterMineAPI;
 import org.intermine.api.profile.Profile;
 import org.intermine.api.profile.SavedQuery;
 import org.intermine.api.util.NameUtil;
@@ -113,6 +113,9 @@ public class ModifyQueryChangeAction extends InterMineDispatchAction
         }
 
         SessionMethods.loadQuery(sq.getPathQuery(), session, response);
+        if (StringUtils.isEmpty(trail)) {
+        	trail = "|query";
+        }
         return new ForwardParameters(mapping.findForward("results"))
                     .addParameter("trail", trail)
                     .forward();
