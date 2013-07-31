066c10ada46ef722e53f078c3dbefbcb9429f449
Fengyuan Hu <fengyuan.huu@gmail.com>
2013-07-31 14:15:48 +0100

    added human gene id to download script #291

---
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/HumanGeneIdentifiers.pm
@@ -0,0 +1,56 @@
+package DataDownloader::Source::HumanGeneIdentifiers;
+
+use Moose;
+use MooseX::FollowPBP;
+extends 'DataDownloader::Source::ABC';
+use URI;
+use autodie qw(open close);
+use Ouch;
+
+use constant {
+    TITLE       => 'Human Gene Identifiers',
+    DESCRIPTION => 'Human Gene identifiers from Ensembl biomart',
+    SOURCE_LINK => 'http://www.ensembl.org/',
+    SOURCE_DIR  => 'human/identifiers',
+    BIOMART_SERVER => 'http://www.ensembl.org/biomart/martservice',
+    COMPARE     => 1,
+};
+
+use constant BIOMART_QUERY => q{
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE Query>
+<Query  virtualSchemaName = "default" formatter = "TSV" header = "0" uniqueRows = "0" count = "" datasetConfigVersion = "0.6" >
+            
+    <Dataset name = "hsapiens_gene_ensembl" interface = "default" >
+        <Attribute name = "ensembl_gene_id" />
+        <Attribute name = "entrezgene" />
+        <Attribute name = "hgnc_id" />
+        <Attribute name = "hgnc_symbol" />
+    </Dataset>
+</Query>
+};
+
+sub BUILD {
+    my $self = shift;
+    my $uri = URI->new(BIOMART_SERVER);
+    $uri->query_form(query => BIOMART_QUERY);
+    $self->set_sources([
+        { 
+            URI => "$uri", 
+            FILE => 'humangeneidentifiers.tsv',
+            POST_PROCESSOR => sub {
+                my ($self, $temp, $destination) = @_;
+                open(my $in, '<:utf8', $temp);
+                open(my $out, '>:utf8', $destination);
+                while (my $content = <$in>) {
+                    if ($content =~ /^Query ERROR:/) {
+                        ouch DownloadError => "BioMart failed us: " . $content;
+                    }
+                    $out->print($content);
+                }
+            },
+        }
+    ]);
+}
+
+1;
