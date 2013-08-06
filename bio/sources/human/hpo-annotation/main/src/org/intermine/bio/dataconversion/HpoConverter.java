05997793a81bbcea404b1d7fceab0df56d2af6c4
Fengyuan Hu <fengyuan.huu@gmail.com>
2013-08-06 11:41:01 +0100

    added hpo-annotation source #291

---
+++ bio/sources/human/hpo-annotation/main/src/org/intermine/bio/dataconversion/HpoConverter.java
@@ -0,0 +1,207 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2013 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.intermine.dataconversion.ItemWriter;
+import org.intermine.metadata.Model;
+import org.intermine.objectstore.ObjectStoreException;
+import org.intermine.util.StringUtil;
+import org.intermine.xml.full.Item;
+
+/**
+ * DataConverter to parse a HPO annotation file into Items.
+ * *
+ * @author Fengyuan Hu
+ */
+public class HpoConverter extends BioDirectoryConverter
+{
+    @SuppressWarnings("unused")
+    private static final Logger LOG = Logger.getLogger(HpoConverter.class);
+
+    private static final String DATASET_TITLE = "HPO Annotation";
+    private static final String DATA_SOURCE_NAME = "HPO";
+
+    private List<String> ignoreDbList = Arrays.asList("DECIPHER", "ORPHANET");
+
+    private static final String HPOTEAM_FILE = "phenotype_annotation_hpoteam.tab";
+    private static final String NEG_FILE = "negative_phenotype_annotation.tab";
+
+    private Map<String, List<String[]>> diseaseToAnnoMap = new HashMap<String, List<String[]>>();
+    private Map<String, String> eviMap = new HashMap<String, String>();
+    private Map<String, String> hpoTermMap = new HashMap<String, String>();
+    private Map<String, String> diseaseMap = new HashMap<String, String>();
+    private Map<String, String> publicationMap = new HashMap<String, String>();
+
+    /**
+     * Constructor
+     * @param writer the ItemWriter used to handle the resultant items
+     * @param model the Model
+     */
+    public HpoConverter(ItemWriter writer, Model model) throws Exception {
+        super(writer, model, DATA_SOURCE_NAME, DATASET_TITLE);
+    }
+
+    @Override
+    public void process(File dataDir) throws Exception {
+        Map<String, File> files = readFilesInDir(dataDir);
+
+        String[] requiredFiles = new String[] {HPOTEAM_FILE, NEG_FILE};
+        Set<String> missingFiles = new HashSet<String>();
+        for (String requiredFile : requiredFiles) {
+            if (!files.containsKey(requiredFile)) {
+                missingFiles.add(requiredFile);
+            }
+        }
+
+        if (!missingFiles.isEmpty()) {
+            throw new RuntimeException("Not all required files for the OMIM sources were found in: "
+                    + dataDir.getAbsolutePath() + ", was missing " + missingFiles);
+        }
+
+        processAnnoFile(new FileReader(files.get(HPOTEAM_FILE)));
+        processAnnoFile(new FileReader(files.get(NEG_FILE)));
+    }
+
+    private Map<String, File> readFilesInDir(File dir) {
+        Map<String, File> files = new HashMap<String, File>();
+        for (File file : dir.listFiles()) {
+            files.put(file.getName(), file);
+        }
+        return files;
+    }
+
+    private void processAnnoFile(Reader reader) throws IOException, ObjectStoreException {
+
+//        initialiseMapsForFile();
+
+        BufferedReader br = new BufferedReader(reader);
+        String line = null;
+
+        // loop through entire file
+        while ((line = br.readLine()) != null) {
+            String[] array = line.split("\t", -1); // keep trailing empty Strings
+
+            // HPO Annotation File Format:
+            // http://www.human-phenotype-ontology.org/contao/index.php/annotation-guide.html
+            if (array.length < 9) {
+                throw new IllegalArgumentException("Not enough elements (should be > 8 not "
+                        + array.length + ") in line: " + line);
+            }
+
+            String db = array[0];
+            if (ignoreDbList.contains(db)) {
+                continue;
+            }
+
+            String dbId = db + ":" + array[1];
+            String qualifier = array[3];
+            String hpoId = array[4];
+            String dbRef = array[5];
+            String strEvidence = array[6];
+
+            storeHpoTerm(hpoId);
+            storeEvidenceCode(strEvidence);
+
+            String[] annoInfo = {qualifier, hpoId, dbRef, strEvidence};
+
+            if (diseaseToAnnoMap.get(dbId) == null) {
+                List<String[]> annoInfoList = new ArrayList<String[]>();
+                annoInfoList.add(annoInfo);
+                diseaseToAnnoMap.put(dbId, annoInfoList);
+            } else {
+                diseaseToAnnoMap.get(dbId).add(annoInfo);
+            }
+        }
+
+        for (String dbId : diseaseToAnnoMap.keySet()) {
+            List<String> annoRefIds = new ArrayList<String>();
+            for (String[] infoBits : diseaseToAnnoMap.get(dbId)) {
+                // Create Evidence item
+                Item eviItem = createItem("HPOEvidence");
+                eviItem.setAttribute("source", infoBits[2]);
+                eviItem.setReference("code", eviMap.get(infoBits[3]));
+                if (infoBits[2].toUpperCase().startsWith("PMID")) {
+                    eviItem.setCollection("publications",
+                            Arrays.asList(storePublication(infoBits[2])));
+                }
+                store(eviItem);
+
+                // Create HPOAnnotation item
+                Item annoItem = createItem("HPOAnnotation");
+                annoItem.setAttribute("qualifier", infoBits[0]);
+                annoItem.setReference("HPOTerm", hpoTermMap.get(infoBits[1]));
+                annoItem.setCollection("evidence", Arrays.asList(eviItem.getIdentifier()));
+                annoRefIds.add(annoItem.getIdentifier());
+                store(annoItem);
+            }
+            storeDisease(dbId, annoRefIds);
+        }
+    }
+
+    private void storeDisease(String dbId, List<String> annoRefIds) throws ObjectStoreException {
+        if (diseaseMap.get(dbId) == null) {
+            Item item = createItem("Disease");
+            item.setAttribute("identifier", dbId);
+            item.setCollection("hpoAnnotation", annoRefIds);
+            diseaseMap.put(dbId, item.getIdentifier());
+            store(item);
+        }
+    }
+
+    private void storeHpoTerm(String hpoTerm) throws ObjectStoreException {
+        if (hpoTermMap.get(hpoTerm) == null) {
+            Item item = createItem("HPOTerm");
+            item.setAttribute("identifier", hpoTerm);
+            hpoTermMap.put(hpoTerm, item.getIdentifier());
+            store(item);
+        }
+    }
+
+    private void storeEvidenceCode(String code) throws ObjectStoreException {
+        if (eviMap.get(code) == null) {
+            Item item = createItem("GOEvidenceCode");
+            item.setAttribute("code", code);
+            eviMap.put(code, item.getIdentifier());
+            store(item);
+        }
+    }
+
+    private String storePublication(String ref) throws ObjectStoreException {
+        String pubItemId = null;
+        Item item = null;
+        String pubMedId = ref.substring(5);
+        if (StringUtil.allDigits(pubMedId)) {
+            pubItemId = publicationMap.get(pubMedId);
+            if (pubItemId == null) {
+                item = createItem("Publication");
+                item.setAttribute("pubMedId", pubMedId);
+                pubItemId = item.getIdentifier();
+                publicationMap.put(pubMedId, pubItemId);
+                store(item);
+            }
+        }
+        return pubItemId;
+    }
+}
