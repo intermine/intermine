89a754e081df2dfbb4a200d0dc677e486921af29
sergio contrino <contrino@gmail.com>
2013-09-11 11:19:44 +0100

    adds new converter for ws220 cds

---
+++ bio/sources/ws220-CDS/main/src/org/intermine/bio/dataconversion/Ws220CDSGFF3Converter.java
@@ -0,0 +1,18 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2011 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+public class Ws220CDSGFF3Converter extends GFF3Converter {
+    private static final Logger LOG = Logger.getLogger(Ws220CDSGFF3Converter.class);
+
+
+
+}
