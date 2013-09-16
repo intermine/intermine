326dea2678f8ed502fe437c84a0f5e820c37c711
Fengyuan Hu <fengyuan.huu@gmail.com>
2013-09-16 12:01:48 +0100

    added HPO dataset download script #291
    Former-commit-id: 7fb0346de66889e77dc901594f15fffd653ea590

---
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/HPO.pm
@@ -0,0 +1,41 @@
+package DataDownloader::Source::HPO;
+
+use Moose;
+extends 'DataDownloader::Source::ABC';
+
+# Human Phenotype Ontology and anotation 
+
+
+use constant {
+    TITLE       => 
+        'Human Phenotype Ontology',
+    DESCRIPTION => 
+        "Human Phenotype Ontology and anotation",
+    SOURCE_LINK => 
+        "http://www.human-phenotype-ontology.org/",
+    SOURCE_DIR => "hpo",
+};
+
+sub BUILD {
+    my $self = shift;
+    $self->set_sources([
+        {
+            SERVER  => 'http://compbio.charite.de/hudson/job/hpo/lastStableBuild/artifact/ontology/release/',
+            FILE    => "hp.obo",
+        },
+        {
+            SERVER  => 'http://compbio.charite.de/hudson/job/hpo.annotations/lastStableBuild/artifact/misc/',
+            FILE    => "negative_phenotype_annotation.tab",
+        },
+        {
+            SERVER  => 'http://compbio.charite.de/hudson/job/hpo.annotations/lastStableBuild/artifact/misc/',
+            FILE    => "phenotype_annotation.tab",
+        }, 
+        {
+            SERVER  => 'http://compbio.charite.de/hudson/job/hpo.annotations.monthly/lastStableBuild/artifact/annotation/',
+            FILE    => "ALL_SOURCES_ALL_FREQUENCIES_phenotype_to_genes.txt",
+        },
+    ]);
+}
+
+1;
