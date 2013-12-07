15f511f45ab971427f3ec205abfdb11657d5e60c
Alex Kalderimis <alexkalderimis@gmail.com>
2013-12-07 02:52:12 +0000

    Copyright headers

--- intermine/web/main/src/org/intermine/webservice/server/core/ReadWriteJSONService.java
+++ intermine/web/main/src/org/intermine/webservice/server/core/ReadWriteJSONService.java
@@ -1,10 +1,25 @@
 package org.intermine.webservice.server.core;
 
+/*
+ * Copyright (C) 2002-2013 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
 import org.intermine.api.InterMineAPI;
 import org.intermine.webservice.server.exceptions.ServiceForbiddenException;
 
-// Convenience for services that need to combine JSON service behaviour
-// with strict RW authorisation. 
+/**
+ * Convenience for services that need to combine JSON service behaviour
+ * with strict RW authorisation.
+ * 
+ * @author Alex Kalderimis
+ *
+ */ 
 public abstract class ReadWriteJSONService extends JSONService {
 
 	private static final String DENIAL_MSG = "Access denied.";
