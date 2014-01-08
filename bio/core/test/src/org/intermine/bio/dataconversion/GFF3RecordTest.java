5eff647b7e2c4d4e0f88c90a72a286d66c40c90c
Julie Sullivan <julie@flymine.org>
2014-01-08 15:13:26 +0000

    update year

--- bio/core/test/src/org/intermine/bio/dataconversion/GFF3RecordTest.java
+++ bio/core/test/src/org/intermine/bio/dataconversion/GFF3RecordTest.java
@@ -12,7 +12,7 @@ import junit.framework.TestCase;
 import org.intermine.bio.io.gff3.GFF3Record;
 
 /*
- * Copyright (C) 2002-2013 FlyMine
+ * Copyright (C) 2002-2014 FlyMine
  *
  * This code may be freely distributed and modified under the
  * terms of the GNU Lesser General Public Licence.  This should
