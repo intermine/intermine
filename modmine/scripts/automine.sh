aae968f5375dc7cafc4fc1190249eb4c3029fbb4
sergiocontrino <contrino@gmail.com>
2014-03-26 10:07:06 +0000

    modifies path to scripts
    Former-commit-id: dd78af5ae91c78e6d4261dd2190e4f6ccf5b3a05

--- modmine/scripts/automine.sh
+++ modmine/scripts/automine.sh
@@ -29,7 +29,7 @@ PATCHDIR=$LOADDIR/patches
 
 FTPURL=http://submit.modencode.org/submit/public
 PROPDIR=$HOME/.intermine
-SCRIPTDIR=../bio/scripts/modmine
+SCRIPTDIR=./scripts
 
 ARKDIR=/micklem/releases/modmine
 
