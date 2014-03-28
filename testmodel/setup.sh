db72b171cdb2a3737335b28d9badc2519c0ede71
Alex Kalderimis <alexkalderimis@gmail.com>
2014-03-28 00:56:19 +0000

    Better setup.sh, learning lessions from biotestmine
    Conflicts:
    	testmodel/setup.sh
    
    Former-commit-id: d479e47cdf16042b37cc5be9cf144e026d907290

--- testmodel/setup.sh
+++ testmodel/setup.sh
@@ -1,30 +1,74 @@
 #!/usr/bin/bash
+# Build and deploy the testmodel webapp
+# This script requires the standard InterMine dependencies:
+#  * psql (createdb, psql) - your user should have a postgres
+#    role with password authentication set up.
+#  * ant
+#  * a deployment container (tomcat).
+
+set -e # Errors are fatal.
 
 USERPROFILEDB=userprofile-demo
 PRODDB=objectstore-demo
 DIR="$(cd $(dirname "$0"); pwd)"
+IMDIR=$HOME/.intermine
+LOG=$DIR/build.log
+PROP_FILE=$IMDIR/testmodel.properties
+
+# Inherit SERVER, PORT, PSQL_USER, PSQL_PWD, TOMCAT_USER and TOMCAT_PWD if in env.
+if test -z $SERVER; then
+    SERVER=localhost
+fi
+if test -z $PORT; then
+    PORT=8080
+fi
+if test -z $PSQL_USER; then
+    PSQL_USER=$USER
+fi
+if test -z $PSQL_PWD; then
+    PSQL_PWD=$USER;
+fi
+if test -z $TOMCAT_USER; then
+    TOMCAT_USER=manager
+fi
+if test -z $TOMCAT_PWD; then
+    TOMCAT_PWD=manager
+fi
 
 cd $HOME
 
-if test ! -d .intermine; then
-    mkdir .intermine
-    cp $DIR/testmodel.properties .
-    sed -i "s/USER/$USER/g" testmodel.properties
+if test ! -d $IMDIR; then
+    echo Making .intermine configuration directory.
+    mkdir $IMDIR
 fi
 
-if test ! psql --list | egrep -q \b$USERPROFILEDB\b; then
-    echo Creating $USERPROFILEDB
-    createdb $USERPROFILEDB
+if test ! -f $PROP_FILE; then
+    echo No properties found. Providing default properties file.
+    cd $IMDIR
+    cp $DIR/testmodel.properties $PROP_FILE
+    sed -i "s/PSQL_USER/$PSQL_USER/g" $PROP_FILE
+    sed -i "s/PSQL_PWD/$PSQL_PWD/g" $PROP_FILE
+    sed -i "s/TOMCAT_USER/$TOMCAT_USER/g" $PROP_FILE
+    sed -i "s/TOMCAT_PWD/$TOMCAT_PWD/g" $PROP_FILE
+    sed -i "s/localhost/$SERVER/g" $PROP_FILE
+    sed -i "s/8080/$PORT/g" $PROP_FILE
+    sed -i "s/USER/$USER/g" 
 fi
 
-if test ! psql --list | egrep -q \b$PRODDB\b; then
-    echo Creating $PRODDB
-    createdb $PRODDB
-fi
+echo Checking databases.
+for db in $USERPROFILEDB $PRODDB; do
+    if psql --list | egrep -q '\s'$db'\s'; then
+        echo $db exists.
+    else
+        echo Creating $db
+        createdb $db
+    fi
+done
 
 cd $DIR/dbmodel
 
-ant loadsadata
+echo Loading demo data set...
+ant loadsadata >> $LOG
 
 cd $DIR/webapp/main
 
@@ -33,5 +77,7 @@ ant -Ddont.minify=true \
     create-quicksearch-index \
     default \
     remove-webapp \
-    release-webapp
+    release-webapp >> $LOG
+
+echo All done. Build log is available in $LOG
 
