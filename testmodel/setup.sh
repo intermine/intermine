d767b7feb3e7aad9264ee8362796ee00761a0ee1
Alex Kalderimis <alexkalderimis@gmail.com>
2014-04-01 02:49:39 +0100

    Now building with more stuff.
    Former-commit-id: b55be9e69667e0c68cb813a6f5bc69060fa2948f

--- testmodel/setup.sh
+++ testmodel/setup.sh
@@ -1,37 +1,48 @@
 #!/usr/bin/bash
 
+set -e
+
 USERPROFILEDB=userprofile-demo
 PRODDB=objectstore-demo
 DIR="$(cd $(dirname "$0"); pwd)"
+LOG=$DIR/build.log
 
 cd $HOME
 
+echo checking properties file
 if test ! -d .intermine; then
     mkdir .intermine
     cp $DIR/testmodel.properties .
     sed -i "s/USER/$USER/g" testmodel.properties
 fi
 
-if test ! psql --list | egrep -q \b$USERPROFILEDB\b; then
-    echo Creating $USERPROFILEDB
-    createdb $USERPROFILEDB
-fi
+echo checking dbs.
+for db in $PRODDB $USERPROFILEDB; do
+    if psql --list | egrep -q '\s'$db'\s'; then
+        echo $db exists
+    else
+        echo Creating $db
+        createdb $db
+    fi
+done
 
-if test ! psql --list | egrep -q \b$PRODDB\b; then
-    echo Creating $PRODDB
-    createdb $PRODDB
-fi
+echo Processing data sources.
+cd $DIR/dbmodel/extra/books
+make 
 
 cd $DIR/dbmodel
-
-ant loadsadata
+echo "Loading test data set"
+ant -v clean load-workers-and-books >> $DIR/build.log
 
 cd $DIR/webapp/main
 
+echo "Building and releasing web-application"
 ant -Ddont.minify=true \
     build-test-userprofile-withuser \
     create-quicksearch-index \
     default \
     remove-webapp \
-    release-webapp
+    release-webapp | tee $DIR/build.log | grep tomcat-deploy
+
+echo build complete. Log available in $DIR/build.log
 
