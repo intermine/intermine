273b58fcc9f80867512a73b90c8c0154cbe45da3
JoeCarlson <JWCarlson@lbl.gov>
2014-05-15 10:21:59 -0700

    hack to enable genomic region export based on id field

--- bio/webapp/src/org/intermine/bio/web/export/GenomicRegionSequenceExporter.java
+++ bio/webapp/src/org/intermine/bio/web/export/GenomicRegionSequenceExporter.java
@@ -34,6 +34,58 @@ public class GenomicRegionSequenceExporter
 {
     private ObjectStore os;
     private OutputStream out;
+    
+    private static final HashMap<String,String> pacIdHack;
+    static {
+        pacIdHack = new HashMap<String,String>();
+        pacIdHack.put("195","A. coerulea");
+        pacIdHack.put("107","A. lyrata");
+        pacIdHack.put("167","A. thaliana");
+        pacIdHack.put("192","B. distachyon");
+        pacIdHack.put("277","B. rapa");
+        pacIdHack.put("278","B. stricta");
+        pacIdHack.put("182","C. clementina");
+        pacIdHack.put("266","C. grandiflora");
+        pacIdHack.put("113","C. papaya");
+        pacIdHack.put("281","C. reinhardtii");
+        pacIdHack.put("183","C. rubella");
+        pacIdHack.put("122","C. sativus");
+        pacIdHack.put("154","C. sinensis");
+        pacIdHack.put("227","C. subellipsoidea C-169");
+        pacIdHack.put("201","E. grandis");
+        pacIdHack.put("173","E. salsugineum");
+        pacIdHack.put("226","F. vesca");
+        pacIdHack.put("275","G. max");
+        pacIdHack.put("221","G. raimondii");
+        pacIdHack.put("200","L. usitatissimum");
+        pacIdHack.put("196","M. domestica");
+        pacIdHack.put("147","M. esculenta");
+        pacIdHack.put("256","M. guttatus");
+        pacIdHack.put("228","M. pusilla CCMP1545");
+        pacIdHack.put("229","M. sp. RCC299 ");
+        pacIdHack.put("285","M. truncatula");
+        pacIdHack.put("231","O. lucimarinus CCE9901");
+        pacIdHack.put("204","O. sativa");
+        pacIdHack.put("251","P. patens");
+        pacIdHack.put("139","P. persica");
+        pacIdHack.put("210","P. trichocarpa");
+        pacIdHack.put("273","P. virgatum");
+        pacIdHack.put("218","P. vulgaris");
+        pacIdHack.put("119","R. communis");
+        pacIdHack.put("255","S. bicolor");
+        pacIdHack.put("164","S. italica");
+        pacIdHack.put("225","S. lycopersicum");
+        pacIdHack.put("91","S. moellendorffii");
+        pacIdHack.put("206","S. tuberosum");
+        pacIdHack.put("233","T. cacao");
+        pacIdHack.put("199","V. carteri");
+        pacIdHack.put("145","V. vinifera");
+        pacIdHack.put("284","Z. mays");
+        pacIdHack.put("264","A. halleri");
+        pacIdHack.put("274","P. hallii");
+        pacIdHack.put("283","B. distachyon");
+    }
+
     // Map to hold DNA sequence of a whole chromosome in memory
     private static Map<MultiKey, String> chromosomeSequenceMap = new HashMap<MultiKey, String>();
 
@@ -56,6 +108,14 @@ public class GenomicRegionSequenceExporter
     @SuppressWarnings("deprecation")
     public void export(List<GenomicRegion> grList) throws Exception {
         GenomicRegion aRegion = grList.get(0);
+        // hack alert: if an integer, use the hack map
+        try {
+          Integer.parseInt(aRegion.getOrganism());
+          if (pacIdHack.containsKey(aRegion.getOrganism()) ) {
+            aRegion.setOrganism(pacIdHack.get(aRegion.getOrganism()));
+          }
+        } catch (NumberFormatException e) {}
+          
         Organism org = (Organism) DynamicUtil.createObject(Collections
                 .singleton(Organism.class));
         org.setShortName(aRegion.getOrganism());
