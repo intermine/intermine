2b00c14c6e37dc621f57d4538c482787ae3f712d
Selma Jacob <sbfj2@cam.ac.uk>
2014-05-16 13:47:28 +0100

    storing on database
    Former-commit-id: 56698d737450eb98e8548257f546cec85ecd4abc

--- bio/postprocess/main/src/org/intermine/bio/like/Storing.java
+++ bio/postprocess/main/src/org/intermine/bio/like/Storing.java
@@ -1,13 +1,22 @@
 package org.intermine.bio.like;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.HashMap;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import org.intermine.modelproduction.MetadataManager;
+import org.intermine.modelproduction.MetadataManager.LargeObjectOutputStream;
+import org.intermine.objectstore.ObjectStore;
+import org.intermine.objectstore.intermine.ObjectStoreInterMineImpl;
+import org.intermine.sql.Database;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -16,41 +25,184 @@ import java.util.Map;
  */
 public class Storing {
 
-    private Map<Coordinates, Integer> row;
+    private static Map<Coordinates, Integer> normMat;
+    private static Map<Coordinates, ArrayList<Integer>> commonMat;
+
+    private static final Logger LOG = Logger.getLogger(Storing.class);
 
     private Storing() {
         // Don't.
     }
 
     /**
+     * writes index and associated directory to the database using the metadatamanager.
      *
-     * @param matrixRow containing the gene ID at its relationship to all other genes
-     * @param geneID Gene ID, which shall be stored
-     * @throws IOException matrixRow is not serializable
+     * @param os intermine objectstore
+     * @param matrix one row of one precalculated matrix
+     * @param name save with this file name
      */
-    public static void storeRow(Map<Coordinates, Integer> matrixRow, Integer geneID)
-        throws IOException {
-        File rowIn = new File(geneID.toString());
-        FileOutputStream f = new FileOutputStream(rowIn);
-        ObjectOutputStream s = new ObjectOutputStream(f);
-        s.writeObject(matrixRow);
-        s.close();
+    public static void saveNormMatToDatabase(ObjectStore os,
+            Map<Coordinates, Integer> matrix, String name) {
+        try {
+            LOG.info("Deleting previous search index dirctory blob from db...");
+            long startTime = System.currentTimeMillis();
+            Database db = ((ObjectStoreInterMineImpl) os).getDatabase();
+            boolean blobExisted = MetadataManager.deleteLargeBinary(db, name);
+            if (blobExisted) {
+                LOG.debug("Deleting previous search index blob from db took: "
+                        + (System.currentTimeMillis() - startTime) + ".");
+            } else {
+                LOG.debug("No previous search index blob found in db");
+            }
+
+            LOG.debug("Saving search index information to database...");
+            writeObjectToDB(os, name, matrix);
+            LOG.debug("Successfully saved search index information to database.");
+
+        } catch (IOException e) {
+            LOG.error(null, e);
+            throw new RuntimeException("Index creation failed: ", e);
+        } catch (SQLException e) {
+            LOG.error(null, e);
+            throw new RuntimeException("Index creation failed: ", e);
+        }
     }
 
-    /**
-     *
-     * @param geneID Gene ID, which is wanted
-     * @return geneIDs relationships to all other genes
-     * @throws IOException
-     * @throws ClassNotFoundException file called "geneID" can't be found
-     */
-    public Map<Coordinates, Integer> getRow(Integer geneID)
-        throws IOException, ClassNotFoundException {
-        File rowOut = new File(geneID.toString());
-        FileInputStream f2 = new FileInputStream(rowOut);
-        ObjectInputStream s2 = new ObjectInputStream(f2);
-        row = (HashMap<Coordinates, Integer>) s2.readObject();
-        s2.close();
-        return row;
+    public static void saveCommonMatToDatabase(ObjectStore os,
+            Map<Coordinates, ArrayList<Integer>> matrix, String name) {
+        try {
+            LOG.info("Deleting previous search index dirctory blob from db...");
+            long startTime = System.currentTimeMillis();
+            Database db = ((ObjectStoreInterMineImpl) os).getDatabase();
+            boolean blobExisted = MetadataManager.deleteLargeBinary(db, name);
+            if (blobExisted) {
+                LOG.debug("Deleting previous search index blob from db took: "
+                        + (System.currentTimeMillis() - startTime) + ".");
+            } else {
+                LOG.debug("No previous search index blob found in db");
+            }
+
+            LOG.debug("Saving search index information to database...");
+            writeObjectToDB(os, name, matrix);
+            LOG.debug("Successfully saved search index information to database.");
+
+        } catch (IOException e) {
+            LOG.error(null, e);
+            throw new RuntimeException("Index creation failed: ", e);
+        } catch (SQLException e) {
+            LOG.error(null, e);
+            throw new RuntimeException("Index creation failed: ", e);
+        }
+    }
+
+    private static void writeObjectToDB(ObjectStore os, String key, Object object)
+            throws IOException, SQLException {
+        LOG.debug("Saving stream to database...");
+        Database db = ((ObjectStoreInterMineImpl) os).getDatabase();
+        LargeObjectOutputStream streamOut = MetadataManager.storeLargeBinary(db, key);
+
+        GZIPOutputStream gzipStream = new GZIPOutputStream(new BufferedOutputStream(streamOut));
+        ObjectOutputStream objectStream = new ObjectOutputStream(gzipStream);
+
+        LOG.debug("GZipping and serializing object...");
+        objectStream.writeObject(object);
+
+        objectStream.flush();
+        gzipStream.finish();
+        gzipStream.flush();
+
+        streamOut.close();
+    }
+
+    public static Map<Coordinates, Integer> loadNormMatFromDatabase(ObjectStore os, String path) {
+        long time = System.currentTimeMillis();
+        LOG.debug("Attempting to restore search index from database...");
+        if (os instanceof ObjectStoreInterMineImpl) {
+            Database db = ((ObjectStoreInterMineImpl) os).getDatabase();
+            try {
+                InputStream is = MetadataManager.readLargeBinary(db, path);
+
+                if (is != null) {
+                    GZIPInputStream gzipInput = new GZIPInputStream(is);
+                    ObjectInputStream objectInput = new ObjectInputStream(gzipInput);
+
+                    try {
+                        Object object = objectInput.readObject();
+
+                        if (object instanceof Map<?, ?>) {
+                            normMat = (Map<Coordinates, Integer>) object;
+
+                            LOG.info("Successfully restored search index information"
+                                    + " from database in " + (System.currentTimeMillis() - time)
+                                    + " ms");
+                            LOG.debug("Index: " + normMat);
+                        } else {
+                            LOG.warn("Object from DB has wrong class:"
+                                    + object.getClass().getName());
+                        }
+                    } finally {
+                        objectInput.close();
+                        gzipInput.close();
+                    }
+                } else {
+                    LOG.warn("IS is null");
+                }
+            } catch (ClassNotFoundException e) {
+                LOG.error("Could not load search index", e);
+            } catch (SQLException e) {
+                LOG.error("Could not load search index", e);
+            } catch (IOException e) {
+                LOG.error("Could not load search index", e);
+            }
+        } else {
+            LOG.error("ObjectStore is of wrong type!");
+        }
+        return normMat;
+    }
+
+    public static Map<Coordinates, ArrayList<Integer>> loadCommonMatFromDatabase(ObjectStore os, String path) {
+        long time = System.currentTimeMillis();
+        LOG.debug("Attempting to restore search index from database...");
+        if (os instanceof ObjectStoreInterMineImpl) {
+            Database db = ((ObjectStoreInterMineImpl) os).getDatabase();
+            try {
+                InputStream is = MetadataManager.readLargeBinary(db, path);
+
+                if (is != null) {
+                    GZIPInputStream gzipInput = new GZIPInputStream(is);
+                    ObjectInputStream objectInput = new ObjectInputStream(gzipInput);
+
+                    try {
+                        Object object = objectInput.readObject();
+
+                        if (object instanceof Map<?, ?>) {
+                            commonMat = (Map<Coordinates, ArrayList<Integer>>) object;
+
+                            LOG.info("Successfully restored search index information"
+                                    + " from database in " + (System.currentTimeMillis() - time)
+                                    + " ms");
+                            LOG.debug("Index: " + commonMat);
+                        } else {
+                            LOG.warn("Object from DB has wrong class:"
+                                    + object.getClass().getName());
+                        }
+                    } finally {
+                        objectInput.close();
+                        gzipInput.close();
+                    }
+                } else {
+                    LOG.warn("IS is null");
+                }
+            } catch (ClassNotFoundException e) {
+                LOG.error("Could not load search index", e);
+            } catch (SQLException e) {
+                LOG.error("Could not load search index", e);
+            } catch (IOException e) {
+                LOG.error("Could not load search index", e);
+            }
+        } else {
+            LOG.error("ObjectStore is of wrong type!");
+        }
+        return commonMat;
     }
 }
