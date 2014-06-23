a36334a26bebac1fa3cac781144d3bb06ce86437
joshkh <joshkh@gmail.com>
2014-06-23 11:56:00 +0100

    Added import of HashMap class.
    Former-commit-id: 0cdae6ddb822ba0ce45c67a3026e8780c60480ea

--- intermine/web/main/src/org/intermine/webservice/server/recommendations/SimilarityService.java
+++ intermine/web/main/src/org/intermine/webservice/server/recommendations/SimilarityService.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.zip.GZIPInputStream;
 
 import javax.naming.ConfigurationException;
