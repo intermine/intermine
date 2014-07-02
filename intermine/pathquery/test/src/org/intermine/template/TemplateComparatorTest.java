ce0709e8aad3a1fc5ad4fc44efa165966878cb25
sergiocontrino <contrino@gmail.com>
2014-07-02 15:10:59 +0100

    fixes runtime error in test (..)

--- intermine/pathquery/test/src/org/intermine/template/TemplateComparatorTest.java
+++ intermine/pathquery/test/src/org/intermine/template/TemplateComparatorTest.java
@@ -7,16 +7,14 @@ import junit.framework.TestCase;
 
 import org.intermine.metadata.Model;
 import org.intermine.pathquery.PathQuery;
-import org.junit.Before;
 import org.junit.Test;
 
 
 /**
- * @author sergio
+ * @author sc
  *
  */
 public class TemplateComparatorTest extends TestCase{
-
 
     private static final String TEMPLATE_NAME = "TEST_TEMPLATE";
     private static final String TEMPLATE_TITLE = "TEMPLATE --> TESTING";
@@ -27,23 +25,16 @@ public class TemplateComparatorTest extends TestCase{
     private static final String ATEMPLATE_TITLE = "TEMPLATE --> TESTING";
     private static final String ACOMMENT = "ANOTHER TEMPLATE THAT WE ARE USING FOR TESTING";
 
-    static TemplateQuery template, anotherTemplate;
-
-    @Before
-    public void setup() {
+    @Test
+    public static void testComparison() throws Exception {
         Model model = Model.getInstanceByName("testmodel");
         PathQuery pq = new PathQuery(model);
 
-        template = new TemplateQuery(TEMPLATE_NAME, TEMPLATE_TITLE, COMMENT, pq);
-        anotherTemplate = new TemplateQuery(ATEMPLATE_NAME, ATEMPLATE_TITLE, ACOMMENT, pq);
-
-    }
-    @Test
-    public static void testComparison() throws Exception {
-
         TemplateComparator comparator = new TemplateComparator();
+        TemplateQuery template = new TemplateQuery(TEMPLATE_NAME, TEMPLATE_TITLE, COMMENT, pq);
+        TemplateQuery anotherTemplate = new TemplateQuery(ATEMPLATE_NAME, ATEMPLATE_TITLE, ACOMMENT, pq);
+
         int comparison = comparator.compare(template, anotherTemplate);
-        // it should return template name, with anotherTemplate coming first
         assertTrue(comparison >1);
 
     }
