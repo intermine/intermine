4ddb8cf11e0488d3abce19ce47fe7d1e815fa08e
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-07-02 14:08:33 +0100

    Tomcat download urls are not stable. /shakes fist/

--- config/download_and_configure_tomcat.sh
+++ config/download_and_configure_tomcat.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Hackily scrape the currently available tomcat-7 version.
+TOMCAT_VERSION=$(wget -O - http://mirror.ox.ac.uk/sites/rsync.apache.org/tomcat/tomcat-7/ | grep folder.gif | perl -ne 'm/v(7\.\d+\.\d+)/; print $1;')
+echo Using tomcat $TOMCAT_VERSION
 wget http://mirror.ox.ac.uk/sites/rsync.apache.org/tomcat/tomcat-7/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.zip
 unzip apache-tomcat-${TOMCAT_VERSION}.zip
 cp config/tomcat-users.xml apache-tomcat-${TOMCAT_VERSION}/conf/tomcat-users.xml
