5e3daa797a494af1449adab3fd40cc2d6edf354b
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-07-04 15:19:12 +0100

    Raised useful things to superclass

--- testmodel/webapp/selenium/test/browsertestcase.py
+++ testmodel/webapp/selenium/test/browsertestcase.py
@@ -4,6 +4,15 @@ from selenium import webdriver
 class BrowserTestCase(unittest.TestCase):
 
     def setUp(self):
+        """Start a new browser session, and schedule the browser to be shutdown"""
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(10)
         self.addCleanup(self.browser.quit)
+
+    def elem(self, selector):
+        """Alias for self.browser.find_element_by_css_selector"""
+        return self.browser.find_element_by_css_selector(selector)
+
+    def findLink(self, name):
+        """Alias for self.browser.find_element_by_link_text"""
+        return self.browser.find_element_by_link_text(name)
