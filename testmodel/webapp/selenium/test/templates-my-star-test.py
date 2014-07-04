fc306eb31d8865c20880b23cebe6099d2b8314c3
Joshua Heimbach <joshkh@gmail.com>
2014-07-04 17:46:25 +0100

    Added templates test case for favorites.
    Former-commit-id: b6662e5478f96547e32dd9236135113968ad28e1

--- testmodel/webapp/selenium/test/templates-my-star-test.py
+++ testmodel/webapp/selenium/test/templates-my-star-test.py
@@ -12,28 +12,81 @@ class TemplatesMyStarTest(Super):
         Super.setUp(self)
         self.user = IMUser("zombie-testing-account-login@intermine.org");
 
-    def test_account_login(self):
+    def test_templates_my_star(self):
 
         browser = self.browser
-        browser.get(self.base_url + "/begin.do")
-        browser.find_element_by_link_text("Log in").click()
+        browser.get(self.base_url + "/login.do")
         browser.find_element_by_name("username").clear()
         browser.find_element_by_name("username").send_keys(self.user.name)
         browser.find_element_by_name("password").clear()
         browser.find_element_by_name("password").send_keys(self.user.password)
         browser.find_element_by_name("action").click()
 
-        # Long emails are truncated and appended with an ellipsis,
-        # so we can't assert by comparing user.name to what's on the DOM.
-        # Look for Log out link instead?
+        # Create a template
+        browser.get(self.base_url + "/import.do")
+
+        custom_template = """
+            <template name="My_Template_1" title="My Template 1" comment="">
+              <query name="CEO_Rivals" model="testmodel" view="CEO.name CEO.salary CEO.seniority CEO.company.name" longDescription="">
+                <constraint path="CEO.name" editable="true" op="!=" value="EmployeeB1"/>
+              </query>
+            </template>"""
+
+        browser.find_element_by_id("xml").send_keys(custom_template)
+        browser.find_element_by_css_selector("div > input[type=\"submit\"]").click()
+
+        browser.get(self.base_url + "/templates.do")
+
+        # Confirm that we have two visible non favorite templates
+        self.assert_visible_id("all_templates_template_item_line_Underwater_People")
+        self.assert_visible_id("all_templates_template_item_line_CEO_Rivals")
+
+        # Add a template to our favorites
+        browser.find_element_by_id("favourite_ManagerLookup").click()
+
+        # Show only our favorites
+        browser.find_element_by_id("filter_favourites_all_templates_template").click()
+
+        # Confirm that our non-favorite templates are gone
+        self.assert_invisible_id("all_templates_template_item_line_Underwater_People")
+        self.assert_invisible_id("all_templates_template_item_line_CEO_Rivals")
+
+        # Confirm that our favorite templates is visible
+        self.assert_visible_id("all_templates_template_item_line_ManagerLookup")
+
+        # Turn off the favorites filter
+        browser.find_element_by_id("filter_favourites_all_templates_template").click()
+
+        # Show only MY filters
+        browser.find_element_by_id("filter_scope_all_templates_template").click()
+
+        # Confirm that we see our template
+        self.assert_visible_id("all_templates_template_item_line_My_Template_1")
+
+        # Confirm that we don't see others
+        self.assert_invisible_id("all_templates_template_item_line_Underwater_People")
+        self.assert_invisible_id("all_templates_template_item_line_CEO_Rivals")
+        self.assert_invisible_id("all_templates_template_item_line_ManagerLookup")
+
+    def assert_visible_id(self, id):
+        browser = self.browser
         for i in range(60):
             try:
-                if self.is_element_present(By.LINK_TEXT, "Log out"): break
+                if browser.find_element_by_id(id).is_displayed(): break
             except: pass
             time.sleep(1)
         else: self.fail("time out")
+        return True
 
-        self.assertTrue(self.is_element_present(By.LINK_TEXT, "Log out"))
+    def assert_invisible_id(self, id):
+        browser = self.browser
+        for i in range(60):
+            try:
+                if not browser.find_element_by_id(id).is_displayed(): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        return True
 
     def is_element_present(self, how, what):
         try: self.browser.find_element(by=how, value=what)
