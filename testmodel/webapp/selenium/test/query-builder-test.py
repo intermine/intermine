3c299e06de646b61520bf06aed6859d4aeeb64ef
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-07-04 18:55:29 +0100

    Test for running

--- testmodel/webapp/selenium/test/query-builder-test.py
+++ testmodel/webapp/selenium/test/query-builder-test.py
@@ -70,6 +70,19 @@ class QueryBuilderTestCase(Super):
         """
         self.assertEquals(expected_query.strip(), self.elem('body').text)
 
+    def test_run_query(self):
+        query = """
+        <query model="testmodel" view="Bank.name Bank.debtors.debt" sortOrder="Bank.debtors.debt ASC" >
+          <constraint path="Bank.debtors.debt" op="&gt;" value="25000000" />
+        </query>
+        """
+        self.findLink("Import query from XML").click()
+        self.elem('#xml').send_keys(query)
+        self.elem('#importQueriesForm input[type="submit"]').click()
+        self.elem('#showResult').click()
+        summary = self.elem(".im-table-summary")
+        self.assertIn("22 rows", summary.text)
+
     def test_import_query(self):
         link = self.findLink("Import query from XML")
         self.assertIsNotNone(link)
