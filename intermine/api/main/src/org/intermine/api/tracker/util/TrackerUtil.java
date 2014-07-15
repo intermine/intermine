d2cf50c6e0c721a12bd1df35b265e343bb0418a1
Julie Sullivan <julie@flymine.org>
2014-07-15 10:48:56 +0100

    eclipse warnings
    Former-commit-id: 47bc3adc47fce2f14d5ec1978a51a03b58128470

--- intermine/api/main/src/org/intermine/api/tracker/util/TrackerUtil.java
+++ intermine/api/main/src/org/intermine/api/tracker/util/TrackerUtil.java
@@ -10,16 +10,30 @@ package org.intermine.api.tracker.util;
  *
  */
 
+/**
+ *
+ * @author Daniela
+ */
 public interface TrackerUtil
 {
+    /** name of class that tracks templates **/
     String TEMPLATE_TRACKER = "TemplateTracker";
+    /** name of table that tracks templates **/
     String TEMPLATE_TRACKER_TABLE = "templatetrack";
+    /** name of class that tracks lists **/
     String LIST_TRACKER = "ListTracker";
+    /** name of table that tracks lists **/
     String LIST_TRACKER_TABLE = "listtrack";
+    /** name of class that tracks lists **/
     String LOGIN_TRACKER = "LoginTracker";
+    /** name of table that tracks logins **/
     String LOGIN_TRACKER_TABLE = "logintrack";
+    /** name of class that tracks searches **/
     String SEARCH_TRACKER = "SearchTracker";
+    /** name of table that tracks searches **/
     String SEARCH_TRACKER_TABLE = "searchtrack";
+    /** name of class that tracks queries **/
     String QUERY_TRACKER = "QueryTracker";
+    /** name of table that tracks queries **/
     String QUERY_TRACKER_TABLE = "querytrack";
 }
