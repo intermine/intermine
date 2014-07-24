59006b3c20209659af8ae920a0cd25400f655160
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-07-24 14:16:21 +0100

    Allow ranges to specify the organism
    Ranges need the organism to be fully specified - this change allows
    ranges to have the format TAXONID:CHR:START..END, meaning we can
    stringify arbitrary lists of ranges safely.
    
    TODO: allow the RHS of a range constraint to be a list.

--- bio/core/test/src/org/intermine/bio/query/range/ChromosomeLocationHelperTest.java
+++ bio/core/test/src/org/intermine/bio/query/range/ChromosomeLocationHelperTest.java
@@ -10,7 +10,9 @@ package org.intermine.bio.query.range;
  *
  */
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -76,11 +78,105 @@ public class ChromosomeLocationHelperTest extends TestCase
         }
     }
 
+    public void testCreateConstraintWithTaxon() {
+        QueryClass qc = new QueryClass(Location.class);
+        PathConstraintRange pcr = new PathConstraintRange("Location",
+                ConstraintOp.OVERLAPS,
+                Arrays.asList("7227:X:123..456", "9606:X:123..456"));
+        ChromosomeLocationHelper clh = new ChromosomeLocationHelper();
+        Constraint rangeConstraint = clh.createConstraint(null, qc, pcr);
+        Set<Constraint> constraints = ((ConstraintSet) rangeConstraint).getConstraints();
+        assertEquals(2, constraints.size());
+        assertEquals(ConstraintOp.OR, ((ConstraintSet) rangeConstraint).getOp());
+        ConstraintSet firstRangeConstraint = (ConstraintSet) constraints.iterator().next();
+        assertEquals(ConstraintOp.AND, firstRangeConstraint.getOp());
+        Set<Constraint>  mainConstraints = firstRangeConstraint.getConstraints();
+        assertEquals(5, mainConstraints.size());
+
+
+        List<String> constraintStrings = new ArrayList<String>();
+        for (Constraint c : mainConstraints) {
+            constraintStrings.add(c.toString());
+        }
+        Collections.sort(constraintStrings);
+
+        List<String> expected = Arrays.asList(
+            "SimpleConstraint(QueryField(org.intermine.model.bio.Chromosome, primaryIdentifier) = java.lang.String: \"X\")",
+            "SimpleConstraint(QueryField(org.intermine.model.bio.Organism, taxonId) = java.lang.Integer: \"7227\")",
+            "org.intermine.model.bio.Chromosome.organism CONTAINS org.intermine.model.bio.Organism",
+            "org.intermine.model.bio.Location.locatedOn CONTAINS org.intermine.model.bio.Chromosome",
+            "start=QueryField(org.intermine.model.bio.Location, start), end=QueryField(org.intermine.model.bio.Location, end) OVERLAPS start=java.lang.Integer: \"123\", end=java.lang.Integer: \"456\""
+        );
+
+        for (int i = 0; i < 5; i++) {
+            assertEquals("Constraint " + i + " does not match", expected.get(i), constraintStrings.get(i));
+        }
+    }
+
     public void testGenomicInterval() {
         GenomicInterval g = new GenomicInterval("1:31222839..31224287");
         assertTrue(g.getStart() == 31222839);
         assertTrue(g.getEnd() == 31224287);
         assertEquals(g.getChr(), "1");
+    }
+
+    public void testGenomicIntervalThreeDots() {
+        GenomicInterval g = new GenomicInterval("1:31222839...31224287");
+        assertTrue(g.getStart() == 31222839);
+        assertTrue(g.getEnd() == 31224287);
+        assertEquals(g.getChr(), "1");
+    }
+
+    public void testGenomicIntervalDash() {
+        GenomicInterval g = new GenomicInterval("1:31222839-31224287");
+        assertTrue(g.getStart() == 31222839);
+        assertTrue(g.getEnd() == 31224287);
+        assertEquals(g.getChr(), "1");
+    }
+
+    public void testGenomicIntervalTabSeparated() {
+        GenomicInterval g = new GenomicInterval("1\t31222839\t31224287");
+        assertTrue(g.getStart() == 31222839);
+        assertTrue(g.getEnd() == 31224287);
+        assertEquals(g.getChr(), "1");
+    }
+
+    public void testGenomicIntervalTabSeparatedWithOtherFields() {
+        GenomicInterval g = new GenomicInterval("1\t31222839\t31224287\tFOO\tBAR");
+        assertTrue(g.getStart() == 31222839);
+        assertTrue(g.getEnd() == 31224287);
+        assertEquals(g.getChr(), "1");
+    }
+
+    public void testGff3Interval() {
+        GenomicInterval g = new GenomicInterval("2L\tGenbank\tSO:00001\t123\t456\tx\ty\tz");
+        assertEquals("GFF3", g.getParsedAs());
+        assertTrue(g.getStart() == 123);
+        assertTrue(g.getEnd() == 456);
+        assertEquals(g.getChr(), "2L");
+    }
+
+    public void testGenomicIntervalPoint() {
+        GenomicInterval g = new GenomicInterval("2L:123");
+        assertTrue(g.getStart() == 123);
+        assertEquals(g.getChr(), "2L");
+        assertEquals(g.getEnd(), Integer.valueOf(123));
+    }
+
+    public void testGenomicIntervalWithTaxon() {
+        GenomicInterval g = new GenomicInterval("7227:1:123..456");
+        assertEquals(g.getStart(), Integer.valueOf(123));
+        assertEquals(g.getEnd(), Integer.valueOf(456));
+        assertEquals(g.getChr(), "1");
+        assertEquals(g.getTaxonId(), "7227");
+    }
+
+    public void testGenomicIntervalWithTaxonDash() {
+        GenomicInterval g = new GenomicInterval("7227:1:123-456");
+        assertEquals(g.getStart(), Integer.valueOf(123));
+        assertEquals(g.getEnd(), Integer.valueOf(456));
+        assertEquals(g.getChr(), "1");
+        assertEquals(g.getTaxonId(), "7227");
     }
 }
 
