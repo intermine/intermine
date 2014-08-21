a1558e015303e61133901460b96e34a6e2857efa
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-08-21 13:21:36 +0100

    Fixed warning in AspectController. Added class to abstract common tag functionality.

---
+++ intermine/api/main/src/org/intermine/api/profile/TagMapper.java
@@ -0,0 +1,102 @@
+package org.intermine.api.profile;
+
+/*
+ * Copyright (C) 2002-2014 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.collections.Transformer;
+import org.intermine.model.userprofile.Tag;
+
+/**
+ * A class that handles transforming a list of tags into a list of strings of different types.
+ * @author Alex Kalderimis
+ *
+ */
+public class TagMapper implements Transformer
+{
+
+    public enum Field {Name, ID, Type, Owner}
+
+    private final Field field;
+
+    private TagMapper(Field field) {
+        this.field = field;
+    }
+
+    /**
+     * Get a tag mapper.
+     * @param field The kind of field this mapper reads.
+     * @return The mapper.
+     */
+    public static TagMapper getMapper(Field field) {
+        if (field == null) {
+            throw new NullPointerException("field must be one of Name, ID, Type or Owner.");
+        }
+        return new TagMapper(field);
+    }
+
+    @Override
+    public String transform(Object obj) {
+        if (obj == null || ! (obj instanceof Tag)) {
+            throw new IllegalArgumentException("tag must be a Tag, got " + obj);
+        }
+        return transform((Tag) obj);
+    }
+
+    /**
+     * Transform a tag into a string of some kind.
+     * @param tag The input tag. Must not be null.
+     * @return The output string. Might be null.
+     */
+    public String transform(Tag tag) {
+        if (tag == null) {
+            throw new NullPointerException("tag must not be null.");
+        }
+        switch (field) {
+        case Name:
+            return tag.getTagName();
+        case ID:
+            return tag.getObjectIdentifier();
+        case Type:
+            return tag.getType();
+        case Owner:
+            return tag.getUserProfile().getUsername();
+        default:
+            throw new IllegalStateException("Unknown field: " + field);
+        }
+    }
+
+    /**
+     * Safely transform a list of tags into a list of string, preserving type information.
+     *
+     * The lists produced by this method are intended to be read only. If you intend on modifying
+     * the list in any way, you must construct a new collection from the result.
+     *
+     * @param tags The input list of tags. Must not be null.
+     * @return The list of strings, never null.
+     */
+    public List<String> map(List<Tag> tags) {
+        if (tags == null) {
+            throw new NullPointerException("tags must not be null");
+        }
+        if (tags.isEmpty()) {
+            return Collections.emptyList();
+        }
+        List<String> retval = new ArrayList<String>(tags.size());
+        for (Tag tag: tags) {
+            retval.add(transform(tag));
+        }
+        return retval;
+    }
+
+}
---
+++ intermine/api/test/src/org/intermine/api/profile/TagMapperTest.java
@@ -0,0 +1,146 @@
+package org.intermine.api.profile;
+
+/*
+ * Copyright (C) 2002-2014 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Transformer;
+import org.intermine.api.profile.TagMapper.Field;
+import org.intermine.model.userprofile.Tag;
+import org.intermine.model.userprofile.UserProfile;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TagMapperTest {
+
+    private static final int N = 10;
+    private List<Tag> tags = null;
+
+    @Before
+    public void setup() {
+        UserProfile u = new UserProfile();
+        u.setUsername("user");
+        List<Tag> tags = new ArrayList<Tag>(N);
+        for (int i = 0; i < N; i++) {
+            Tag t = new Tag();
+            t.setTagName("tagName_" + i);
+            t.setObjectIdentifier("objectIdentifier_" + i);
+            t.setType("type_" + i);
+            t.setUserProfile(u);
+            tags.add(t);
+        }
+        this.tags = tags;
+    }
+
+    @Test
+    public void badField() {
+        try {
+            TagMapper.getMapper(null);
+            fail("Expected failure");
+        } catch (NullPointerException e) {
+            // expected behaviour.
+        }
+    }
+
+    @Test
+    public void apacheCollectionsIntegration() {
+        Transformer tm = TagMapper.getMapper(Field.Name);
+        @SuppressWarnings("unchecked")
+        Collection<String> names = CollectionUtils.collect(tags, tm);
+        List<String> expected = Arrays.asList(
+                "tagName_0",
+                "tagName_1",
+                "tagName_2",
+                "tagName_3",
+                "tagName_4",
+                "tagName_5",
+                "tagName_6",
+                "tagName_7",
+                "tagName_8",
+                "tagName_9"
+        );
+        assertEquals(expected, names);
+    }
+
+    @Test
+    public void testGetNames() {
+        List<String> names = TagMapper.getMapper(Field.Name).map(tags);
+        assertEquals(N, names.size());
+        List<String> expected = Arrays.asList(
+                "tagName_0",
+                "tagName_1",
+                "tagName_2",
+                "tagName_3",
+                "tagName_4",
+                "tagName_5",
+                "tagName_6",
+                "tagName_7",
+                "tagName_8",
+                "tagName_9"
+        );
+        assertEquals(expected, names);
+    }
+
+    @Test
+    public void testGetIds() {
+        List<String> names = TagMapper.getMapper(Field.ID).map(tags);
+        assertEquals(N, names.size());
+        List<String> expected = Arrays.asList(
+                "objectIdentifier_0",
+                "objectIdentifier_1",
+                "objectIdentifier_2",
+                "objectIdentifier_3",
+                "objectIdentifier_4",
+                "objectIdentifier_5",
+                "objectIdentifier_6",
+                "objectIdentifier_7",
+                "objectIdentifier_8",
+                "objectIdentifier_9"
+        );
+        assertEquals(expected, names);
+    }
+
+
+    @Test
+    public void testGetTypes() {
+        List<String> names = TagMapper.getMapper(Field.Type).map(tags);
+        assertEquals(N, names.size());
+        List<String> expected = Arrays.asList(
+                "type_0",
+                "type_1",
+                "type_2",
+                "type_3",
+                "type_4",
+                "type_5",
+                "type_6",
+                "type_7",
+                "type_8",
+                "type_9"
+        );
+        assertEquals(expected, names);
+    }
+
+    @Test
+    public void testGetOwner() {
+        Set<String> names = new HashSet<String>(TagMapper.getMapper(Field.Owner).map(tags));
+        assertEquals(1, names.size());
+        assertEquals("user", names.iterator().next());
+    }
+
+}
