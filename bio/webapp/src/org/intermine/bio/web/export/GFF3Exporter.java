intermine/intermine

commit c8a3fb29e0e51717825ba7e4aa760c196e303d5e

Better error reporting of distinct object store errors

bio/webapp/src/org/intermine/bio/web/logic/GenomicRegionSearchQueryRunner.java
@@ -27,6 +27,7 @@ import java.util.TreeMap;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
+import org.biojavax.ga.Organism;
 import org.intermine.api.InterMineAPI;
 import org.intermine.api.profile.Profile;
 import org.intermine.api.results.ExportResultsIterator;
@@ -384,25 +385,15 @@ public class GenomicRegionSearchQueryRunner implements Runnable
     public static Map<String, Integer> getTaxonInfo(InterMineAPI im, Profile profile) {
 
         Map<String, Integer> orgTaxonIdMap = new HashMap<String, Integer>();
+        Query q = new Query();
+        QueryClass organisms = new QueryClass(org.intermine.model.bio.Organism.class);
+        q.addFrom(organisms);
+        q.addToSelect(organisms);
 
-        PathQuery query = new PathQuery(im.getModel());
-
-        // Add views
-        query.addViews("Organism.shortName",
-                "Organism.taxonId");
-
-        // Add orderby
-        query.addOrderBy("Organism.shortName", OrderDirection.ASC);
-
-        ExportResultsIterator results = im.getPathQueryExecutor(profile).execute(query);
-
-        while (results.hasNext()) {
-            List<ResultElement> row = results.next();
-
-            String orgName = (String) row.get(0).getField();
-            Integer orgTaxonId = (Integer) row.get(1).getField();
-
-            orgTaxonIdMap.put(orgName, orgTaxonId);
+        List<?> orgs = im.getObjectStore().executeSingleton(q);
+        for (Object o: orgs) {
+            org.intermine.model.bio.Organism org = (org.intermine.model.bio.Organism) o;
+            orgTaxonIdMap.put(org.getName(), org.getTaxonId());
         }
         return orgTaxonIdMap;
     }

bio/webapp-test/.classpath
@@ -10,7 +10,6 @@
 	<classpathentry combineaccessrules="false" kind="src" path="/userprofile"/>
 	<classpathentry kind="lib" path="/testmodel-dbmodel/resources"/>
 	<classpathentry combineaccessrules="false" kind="src" path="/intermine-api-main"/>
-	<classpathentry kind="lib" path="/intermine-web/lib/commons-math-2.2.jar"/>
 	<classpathentry kind="lib" path="/intermine-api-main/lib/json20110106.jar"/>
 	<classpathentry combineaccessrules="false" kind="src" path="/bio-core-main"/>
 	<classpathentry kind="con" path="org.eclipse.jdt.junit.JUNIT_CONTAINER/4"/>

bio/webapp/.classpath
@@ -14,7 +14,6 @@
 	<classpathentry combineaccessrules="false" kind="src" path="/bio-testall-dbmodel"/>
 	<classpathentry kind="lib" path="/intermine-web/lib/jcommon-1.0.10.jar"/>
 	<classpathentry combineaccessrules="false" kind="src" path="/intermine-api-main"/>
-	<classpathentry kind="lib" path="/intermine-web/lib/commons-math-2.2.jar"/>
 	<classpathentry combineaccessrules="false" kind="src" path="/intermine-webservice-client"/>
 	<classpathentry kind="output" path="build/webapp/WEB-INF/classes"/>
 </classpath>

bio/webapp/src/org/intermine/bio/web/displayer/CuratedProteinsDisplayer.java
@@ -30,6 +30,7 @@ import org.intermine.api.results.ResultElement;
 import org.intermine.metadata.Model;
 import org.intermine.model.InterMineObject;
 import org.intermine.model.bio.Gene;
+import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.pathquery.Constraints;
 import org.intermine.pathquery.OrderDirection;
 import org.intermine.pathquery.PathQuery;
@@ -84,36 +85,41 @@ public class CuratedProteinsDisplayer extends ReportDisplayer
             // Execute the query.
             Profile profile = SessionMethods.getProfile(session);
             PathQueryExecutor executor = im.getPathQueryExecutor(profile);
-            ExportResultsIterator values = executor.execute(query);
+            ExportResultsIterator values;
+            try {
+                values = executor.execute(query);
+            } catch (ObjectStoreException e) {
+                throw new RuntimeException(e);
+            }
 
-            // Listize.
+             // Listize.
             Map<String, Map<String, Object>> results = new LinkedHashMap<String, Map<String, Object>>();
             while (values.hasNext()) {
-            	List<ResultElement> row = values.next();
-            	// Build the internal map.
-            	Map<String, Object> map = new HashMap<String, Object>();
-            	for (String column : columns) {
-            		map.put(column, row.get(columns.indexOf(column)).getField());
-            	}
-            	
-            	// Is this SwissProt curate?
-            	if (map.get("dataSetsName").equals("Swiss-Prot data set")) {
-            		map.put("isSwissProtCurate", true);
-            	} else {
-            		map.put("isSwissProtCurate", false);
-            	}            	
-            	
-            	// Find in map.
-            	String key = (String) map.get("primaryIdentifier");
-            	Map<String, Object> mapObj = results.get(key);
-            	
-            	if (mapObj != null) {
-            		if (!(Boolean) mapObj.get("isSwissProtCurate") && (Boolean) map.get("isSwissProtCurate")) {
-            			results.put(key, map);
-            		}
-            	} else {
-            		results.put(key, map);
-            	}
+               List<ResultElement> row = values.next();
+               // Build the internal map.
+               Map<String, Object> map = new HashMap<String, Object>();
+               for (String column : columns) {
+                  map.put(column, row.get(columns.indexOf(column)).getField());
+               }
+
+               // Is this SwissProt curate?
+               if (map.get("dataSetsName").equals("Swiss-Prot data set")) {
+                  map.put("isSwissProtCurate", true);
+               } else {
+                  map.put("isSwissProtCurate", false);
+               }
+
+               // Find in map.
+               String key = (String) map.get("primaryIdentifier");
+               Map<String, Object> mapObj = results.get(key);
+               
+               if (mapObj != null) {
+                  if (!(Boolean) mapObj.get("isSwissProtCurate") && (Boolean) map.get("isSwissProtCurate")) {
+                     results.put(key, map);
+                  }
+               } else {
+                  results.put(key, map);
+               }
             }
 
             // Set.
@@ -121,16 +127,16 @@ public class CuratedProteinsDisplayer extends ReportDisplayer
         }
     }
     
-	/**
-	 * Build PathQuery.    
-	 * @param genePrimaryID
-	 * @param query
-	 * @return
-	 */
+   /**
+    * Build PathQuery.    
+    * @param genePrimaryID
+    * @param query
+    * @return
+    */
     private PathQuery buildQuery(String genePrimaryID, PathQuery query) {
         // Select the output columns:
         query.addViews("Gene.proteins.primaryIdentifier",
-        		"Gene.proteins.id",
+              "Gene.proteins.id",
                 "Gene.proteins.primaryAccession",
                 "Gene.proteins.organism.name",
                 "Gene.proteins.isUniprotCanonical",

bio/webapp/src/org/intermine/bio/web/displayer/CytoscapeNetworkDisplayer.java
@@ -72,8 +72,12 @@ public class CytoscapeNetworkDisplayer extends ReportDisplayer
         String featureType = "";
 
         //=== Get Interaction information ===
-        Map<String, Set<String>> interactionInfoMap = CytoscapeNetworkUtil
-                .getInteractionInfo(model, executor);
+        Map<String, Set<String>> interactionInfoMap;
+        try {
+            interactionInfoMap = CytoscapeNetworkUtil.getInteractionInfo(model, executor);
+        } catch (ObjectStoreException e) {
+            throw new RuntimeException(e);
+        }
 
         if (interactionInfoMap == null) {
             request.setAttribute("dataNotIncludedMessage", DATA_NOT_INTEGRATED);
@@ -130,8 +134,13 @@ public class CytoscapeNetworkDisplayer extends ReportDisplayer
 
         //=== Query a full set of interacting genes ===
         CytoscapeNetworkDBQueryRunner queryRunner = new CytoscapeNetworkDBQueryRunner();
-        Set<Integer> fullInteractingGeneSet = queryRunner.getInteractingGenes(
-                featureType, startingFeatureSet, model, executor);
+        Set<Integer> fullInteractingGeneSet;
+        try {
+            fullInteractingGeneSet =
+                    queryRunner.getInteractingGenes(featureType, startingFeatureSet, model, executor);
+        } catch (ObjectStoreException e) {
+            throw new RuntimeException(e);
+        }
 
         // set fullInteractingGeneSet in request
         request.setAttribute("fullInteractingGeneSet",

bio/webapp/src/org/intermine/bio/web/displayer/DiseaseDisplayer.java
@@ -25,6 +25,7 @@ import org.intermine.api.query.PathQueryExecutor;
 import org.intermine.api.results.ExportResultsIterator;
 import org.intermine.api.results.ResultElement;
 import org.intermine.model.bio.Gene;
+import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.pathquery.Constraints;
 import org.intermine.pathquery.PathQuery;
 import org.intermine.util.StringUtil;
@@ -83,7 +84,12 @@ public class DiseaseDisplayer extends ReportDisplayer
         if (!q.isValid()) {
             return Collections.emptySet();
         }
-        ExportResultsIterator it = executor.execute(q);
+        ExportResultsIterator it;
+        try {
+            it = executor.execute(q);
+        } catch (ObjectStoreException e) {
+            throw new RuntimeException(e);
+        }
         while (it.hasNext()) {
             List<ResultElement> row = it.next();
             String identifier = (String) row.get(0).getField();

bio/webapp/src/org/intermine/bio/web/displayer/FriendlyMineLinkGenerator.java
@@ -35,6 +35,7 @@ import org.intermine.api.query.PathQueryExecutor;
 import org.intermine.api.results.ExportResultsIterator;
 import org.intermine.api.results.ResultElement;
 import org.intermine.metadata.Model;
+import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.pathquery.Constraints;
 import org.intermine.pathquery.OrderDirection;
 import org.intermine.pathquery.PathQuery;
@@ -241,11 +242,12 @@ public final class FriendlyMineLinkGenerator extends InterMineLinkGenerator
 
     /*****************************************************************************************
         HOMOLOGUES
+     * @throws ObjectStoreException 
      *****************************************************************************************/
 
     // query local mine for orthologues - results cache is handling
     private static Map<String, Set<String>> getLocalHomologues(FriendlyMineManager olm,
-            String constraintValue, String identifier) {
+            String constraintValue, String identifier) throws ObjectStoreException {
         Map<String, Set<String>> organismToHomologues = new HashMap<String, Set<String>>();
         InterMineAPI im = olm.getInterMineAPI();
         ProfileManager profileManager = im.getProfileManager();

bio/webapp/src/org/intermine/bio/web/displayer/GeneExpressionAtlasDiseasesDisplayer.java
@@ -26,6 +26,7 @@ import org.intermine.metadata.FieldDescriptor;
 import org.intermine.metadata.Model;
 import org.intermine.model.InterMineObject;
 import org.intermine.model.bio.Gene;
+import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.pathquery.Constraints;
 import org.intermine.pathquery.OrderDirection;
 import org.intermine.pathquery.PathQuery;
@@ -87,7 +88,12 @@ public class GeneExpressionAtlasDiseasesDisplayer extends ReportDisplayer
             // execute the query
             Profile profile = SessionMethods.getProfile(session);
             PathQueryExecutor executor = im.getPathQueryExecutor(profile);
-            ExportResultsIterator values = executor.execute(query);
+            ExportResultsIterator values;
+            try {
+                values = executor.execute(query);
+            } catch (ObjectStoreException e) {
+                throw new RuntimeException(e);
+            }
 
             // convert to a map
             GeneExpressionAtlasDiseasesExpressions geae = new GeneExpressionAtlasDiseasesExpressions(values);

commit 04d2c709324ad57a4d1ed93b8621a6bdb18cd802

Fixed GFF3 export in region search caused by c8a3fb29e0e51717825ba7e4aa760c196e303d5e. Refs #756

bio/webapp/src/org/intermine/bio/web/export/GFF3Exporter.java
@@ -146,7 +146,7 @@ public class GFF3Exporter implements Exporter
         StringBuffer header = new StringBuffer();
         Properties props = PropertiesUtil.getProperties();
 
-        if (organisms != null) {
+        if (organisms != null && !organisms.isEmpty()) {
             for (Integer taxId : organisms) {
                 if (taxId == 7227) {
                     String fV = props.getProperty("genomeVersion.fly");

bio/webapp/src/org/intermine/bio/web/logic/GenomicRegionSearchQueryRunner.java
@@ -393,7 +393,7 @@ public class GenomicRegionSearchQueryRunner implements Runnable
         List<?> orgs = im.getObjectStore().executeSingleton(q);
         for (Object o: orgs) {
             org.intermine.model.bio.Organism org = (org.intermine.model.bio.Organism) o;
-            orgTaxonIdMap.put(org.getName(), org.getTaxonId());
+            orgTaxonIdMap.put(org.getShortName(), org.getTaxonId());
         }
         return orgTaxonIdMap;
     }