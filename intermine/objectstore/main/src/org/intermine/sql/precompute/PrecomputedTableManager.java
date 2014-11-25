166439ea5090ebd968efd89c6e2dfdcc097dcad3
julie-sullivan <julie@flymine.org>
2014-11-25 16:53:01 +0000

    checkstyle fix

--- intermine/objectstore/main/src/org/intermine/sql/precompute/PrecomputedTableManager.java
+++ intermine/objectstore/main/src/org/intermine/sql/precompute/PrecomputedTableManager.java
@@ -470,7 +470,8 @@ public class PrecomputedTableManager
             Statement stmt = con.createStatement();
             stmt.execute(sql);
         } catch (SQLException e) {
-            LOG.warn("Error while creating index on " + table + ", table creation will proceed. " + sql);
+            LOG.warn("Error while creating index on " + table + ", table creation will proceed. "
+                    + sql);
         }
     }
 
