f7841133c3af442c3945f0f26834a3a9b87a8658
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-11-25 17:40:41 +0000

    Clean up tomcat script a bit

--- config/download_and_configure_tomcat.sh
+++ config/download_and_configure_tomcat.sh
@@ -32,19 +32,18 @@ if test -z $TOMCAT_VERSION; then
 fi
 
 echo "#--- Using tomcat $TOMCAT_VERSION"
-download http://mirror.ox.ac.uk/sites/rsync.apache.org/tomcat/tomcat-7/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.zip
-unzip apache-tomcat-${TOMCAT_VERSION}.zip
-cp config/tomcat-users.xml apache-tomcat-${TOMCAT_VERSION}/conf/tomcat-users.xml
+TOMCAT=apache-tomcat-${TOMCAT_VERSION}
+download http://mirror.ox.ac.uk/sites/rsync.apache.org/tomcat/tomcat-7/v${TOMCAT_VERSION}/bin/${TOMCAT}.zip
+unzip ${TOMCAT}.zip
+cp config/tomcat-users.xml ${TOMCAT}/conf/tomcat-users.xml
 echo 'JAVA_OPTS="$JAVA_OPTS -Dorg.apache.el.parser.SKIP_IDENTIFIER_CHECK=true"' >> prefixed
 echo 'export JAVA_OPTS' >> prefixed
-cat apache-tomcat-${TOMCAT_VERSION}/bin/startup.sh >> prefixed
-cp prefixed apache-tomcat-${TOMCAT_VERSION}/bin/startup.sh
-sed -i=bak -e 's!<Context>!<Context sessionCookiePath="/" useHttpOnly="false">!' apache-tomcat-${TOMCAT_VERSION}/conf/context.xml
-chmod +x apache-tomcat-${TOMCAT_VERSION}/bin/catalina.sh # startup.sh won't work unless catalina.sh is executable.
+cat ${TOMCAT}/bin/startup.sh >> prefixed
+cp prefixed ${TOMCAT}/bin/startup.sh
+sed -i=bak -e 's!<Context>!<Context sessionCookiePath="/" useHttpOnly="false">!' ${TOMCAT}/conf/context.xml
+chmod +x ${TOMCAT}/bin/*.sh # startup.sh won't work unless catalina.sh is executable.
 
 # Start tomcat on the default port (8080)
-touch nohup.out
-nohup bash -c "apache-tomcat-${TOMCAT_VERSION}/bin/startup.sh 2>&1" && sleep 3
-cat nohup.out
+./${TOMCAT}/bin/startup.sh 2>&1
 
 echo "#--- Started tomcat application container"
