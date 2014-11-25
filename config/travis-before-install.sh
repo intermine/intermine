6ed447df795bf6833f43f3330fa2ae4349562d48
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-11-25 19:05:24 +0000

    Only start X server for selenium tests

--- config/travis-before-install.sh
+++ config/travis-before-install.sh
@@ -2,7 +2,16 @@
 
 set -e
 
-sh -e /etc/init.d/xvfb start
-
-/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+if [ "$TEST_SUITE" = "selenium" ]; then
+    # Start X server so we can run 
+    sh -e /etc/init.d/xvfb start
+    # Configure the screen size.
+    /sbin/start-stop-daemon --start \
+                            --quiet \
+                            --pidfile /tmp/custom_xvfb_99.pid \
+                            --make-pidfile \
+                            --background \
+                            --exec /usr/bin/Xvfb -- \
+                            :99 -ac -screen 0 1280x1024x16
+fi
 
