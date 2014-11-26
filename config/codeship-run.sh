b04a7d585906f34cfcd048b2baa73ba94ee10eb1
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-11-26 19:04:16 +0000

    Also check the checkstyle output.

--- config/codeship-run.sh
+++ config/codeship-run.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-ant -f intermine/all/build.xml clean fulltest checkstyle
+PROJECT=intermine/all
 
-./config/lib/parse_test_report.py "intermine/all/build/test/results"
+ant -f ${PROJECT}/build.xml clean fulltest checkstyle
+
+./config/lib/parse_test_report.py "${PROJECT}/build/test/results"
+./config/lib/parse_checkstyle_report.py "${PROJECT}/build/checkstyle/checkstyle_report.xml"
 
 PSQL_USER=$PG_USER PSQL_PWD=$PG_PASSWORD sh testmodel/setup.sh
 sleep 10 # Wait for the webapp to come online
