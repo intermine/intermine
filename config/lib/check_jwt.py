edb5e0fba118c7fb7250c5520625f0cc70128bdd
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-11-27 18:10:08 +0000

    Added helper to check bearer tokens

---
+++ config/lib/check_jwt.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os
+import sys
+import requests
+
+base_url = os.getenv('TESTMODEL_URL', 'http://localhost:8080/intermine-demo')
+resource = base_url + '/service/whoami'
+
+token = sys.stdin.read()
+
+headers = {'Accept': 'application/json','Authorization': 'Bearer ' + token}
+
+print("Requesting resource: ", resource)
+resp = requests.get(resource, headers = headers)
+
+if resp.status_code != 200:
+    print(resp.text)
+    sys.exit(resp.status_code)
+
+data = resp.json()
+
+print('Authorized as', data['user'])
+
