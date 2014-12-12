7017f3e202755db6432afdbeac173330227d63b1
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-12-12 17:39:39 +0000

    Use flaky to help with flaky tests

--- testmodel/webapp/selenium/test/query-history-test.py
+++ testmodel/webapp/selenium/test/query-history-test.py
@@ -1,5 +1,8 @@
 import time
 
+# These test are unreliable on travis - allow them to be retried.
+from flaky import flaky
+
 from test.querybuildertestcase import QueryBuilderTestCase
 import test.conditions as conditions
 import test.actions as actions
@@ -8,38 +11,6 @@ from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support import expected_conditions as EC
 
 class QueryHistoryTest(QueryBuilderTestCase):
-
-    def test_query_history(self):
-        self.load_queries_into_history()
-        self.wait().until(conditions.in_title('query'))
-
-        self.assertIn('Custom query', self.browser.title)
-        self.assertEquals(2, len(self.elems('#modifyQueryForm tbody tr')))
-        self.assertEquals('query_2', self.elem('#modifyQueryForm tbody tr:nth-child(2) td:nth-child(2)').text)
-        root = self.elem('#modifyQueryForm tbody tr:nth-child(2) .historySummaryRoot').text
-        self.assertEquals('Bank', root)
-        showing = self.elems('#modifyQueryForm tbody tr:nth-child(2) .historySummaryShowing')
-        self.assertEquals(2, len(showing))
-        self.assertEquals(['Name', 'Debt'], [s.text for s in showing])
-
-    def test_delete_query_from_history(self):
-        self.load_queries_into_history()
-        row_selector = '#modifyQueryForm tbody tr'
-
-        def get_row_count():
-            return len(self.elems(row_selector))
-
-        there_are_two_rows = lambda d: get_row_count() == 2
-        there_is_one_row = lambda d: get_row_count() == 1
-
-        self.wait().until(there_are_two_rows, "Row count wrong")
-
-        self.assertEquals(2, get_row_count())
-        self.find_and_click('#selected_history_1')
-        self.find_and_click('#delete_button')
-        Alert(self.browser).accept()
-
-        self.wait().until(there_is_one_row, "Row count unchanged")
 
     def load_queries_into_history(self):
         query_1 = """<query model="testmodel" view="Bank.debtors.debt" sortOrder="Bank.debtors.debt asc"/>"""
@@ -61,12 +32,48 @@ class QueryHistoryTest(QueryBuilderTestCase):
             self.find_and_click('#showResult')
         self.browser.get(self.base_url + '/customQuery.do')
 
+    @flaky
+    def test_query_history(self):
+        self.load_queries_into_history()
+        self.wait().until(conditions.in_title('query'))
+
+        self.assertIn('Custom query', self.browser.title)
+        self.assertEquals(2, len(self.elems('#modifyQueryForm tbody tr')))
+        self.assertEquals('query_2', self.elem('#modifyQueryForm tbody tr:nth-child(2) td:nth-child(2)').text)
+        root = self.elem('#modifyQueryForm tbody tr:nth-child(2) .historySummaryRoot').text
+        self.assertEquals('Bank', root)
+        showing = self.elems('#modifyQueryForm tbody tr:nth-child(2) .historySummaryShowing')
+        self.assertEquals(2, len(showing))
+        self.assertEquals(['Name', 'Debt'], [s.text for s in showing])
+
+    @flaky
+    def test_delete_query_from_history(self):
+        self.load_queries_into_history()
+        row_selector = '#modifyQueryForm tbody tr'
+
+        def get_row_count():
+            return len(self.elems(row_selector))
+
+        there_are_two_rows = lambda d: get_row_count() == 2
+        there_is_one_row = lambda d: get_row_count() == 1
+
+        self.wait().until(there_are_two_rows, "Row count wrong")
+
+        self.assertEquals(2, get_row_count())
+        self.find_and_click('#selected_history_1')
+        self.find_and_click('#delete_button')
+        Alert(self.browser).accept()
+
+        self.wait().until(there_is_one_row, "Row count unchanged")
+
+    @flaky
     def test_run_query_in_query_history(self):
         self.load_queries_into_history()
 
         self.find_and_click('#modifyQueryForm tbody tr:nth-child(2) td:nth-child(7) span.fakelink:nth-child(1)')
         self.assertRowCountIs(16)
 
+    @flaky
     def test_edit_query_in_query_history(self):
         self.load_queries_into_history()
 
@@ -87,6 +94,7 @@ class QueryHistoryTest(QueryBuilderTestCase):
         self.find_and_click('#showResult')
         self.assertRowCountIs(15)
 
+    @flaky
     def test_export_query_in_query_history(self):
         self.load_queries_into_history()
         expected_query = '\n'.join([
