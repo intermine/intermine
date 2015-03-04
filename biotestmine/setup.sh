72342cb39b83e09bf0b750debf7ec20fc567592f
Alex Kalderimis <alex.kalderimis@gmail.com>
2015-03-04 15:57:16 +0000

    Allow the biotestmine to skip the build when releasing

--- biotestmine/setup.sh
+++ biotestmine/setup.sh
@@ -100,65 +100,72 @@ if test ! -f $IMDIR/$PROP_FILE; then
     echo "#--- Created $PROP_FILE"
 fi
 
-echo '#---> Checking databases...'
-for db in $USERPROFILE_DB $PROD_DB $ITEMS_DB; do
-    if psql --list | egrep -q '\s'$db'\s'; then
-        echo "#--- $db exists."
+echo '#---> Removing webapp...'
+ant -f $DIR/webapp/build.xml remove-webapp >> $DIR/log/remove-webapp.log
+
+# Allow callers to skip the DB build and go straight to the web-app
+if [ -z "$SKIP_BUILD" ]
+then
+    echo '#---> Checking databases...'
+    for db in $USERPROFILE_DB $PROD_DB $ITEMS_DB; do
+        if psql --list | egrep -q '\s'$db'\s'; then
+            echo "#--- $db exists."
+        else
+            echo "#---> Creating $db with encoding $DB_ENCODING ..."
+            createdb --template template0 \
+                    --username $PSQL_USER \
+                    --encoding $DB_ENCODING \
+                    $db
+        fi
+    done
+
+    if test -d $HOME/${MINENAME}-sample-data; then
+        echo '#--- Sample data already exists.'
     else
-        echo "#---> Creating $db with encoding $DB_ENCODING ..."
-        createdb --template template0 \
-                 --username $PSQL_USER \
-                 --encoding $DB_ENCODING \
-                 $db
+        cd $HOME
+        mkdir $DATA_DIR
+        cd $DATA_DIR
+        cp $DIR/../bio/tutorial/malaria-data.tar.gz .
+        echo '#---> Unpacking sample data...'
+        tar -zxvf malaria-data.tar.gz >> $DIR/log/extract.log
+        rm malaria-data.tar.gz
     fi
-done
 
-if test -d $HOME/${MINENAME}-sample-data; then
-    echo '#--- Sample data already exists.'
-else
-    cd $HOME
-    mkdir $DATA_DIR
-    cd $DATA_DIR
-    cp $DIR/../bio/tutorial/malaria-data.tar.gz .
-    echo '#---> Unpacking sample data...'
-    tar -zxvf malaria-data.tar.gz >> $DIR/log/extract.log
-    rm malaria-data.tar.gz
+    cd $DIR
+    if test ! -f project.xml; then
+        echo '#---> Copying over malariamine project.xml'
+        cp ../bio/tutorial/project.xml .
+    fi
+
+    echo '#---> Personalising project.xml'
+    sed -i "s!DATA_DIR!$DATA_DIR!g" project.xml
+    sed -i "s/malariamine/$MINENAME/g" project.xml
+
+    if egrep -q ProteinDomain.shortName $PRIORITIES; then
+        echo '#--- Integration key exists.'
+    else
+        echo '#---> Adjusting priorities.'
+        echo 'ProteinDomain.shortName = interpro, uniprot-malaria' >> $PRIORITIES
+    fi
+
+    cd $DIR/dbmodel
+    echo '#---> Building DB'
+    ant clean build-db >> $DIR/log/build-db.log
+
+    if test ! -f $PROJECT_BUILD; then
+        echo "ERROR: Cannot find project build script at $PROJECT_BUILD"
+        exit 1
+    fi
+
+    echo '#---> Loading data (this could take some time) ...'
+    cd $DIR
+    $PROJECT_BUILD -b -v $SERVER $HOME/${MINENAME}-dump 2>&1 \
+        | tee -a $LOAD_LOG \
+        | grep -E '(action.*took|failed)'
+
+    echo '#--- Finished loading data.'
+    cp pbuild.log $DIR/log/
 fi
-
-cd $DIR
-if test ! -f project.xml; then
-    echo '#---> Copying over malariamine project.xml'
-    cp ../bio/tutorial/project.xml .
-fi
-
-echo '#---> Personalising project.xml'
-sed -i "s!DATA_DIR!$DATA_DIR!g" project.xml
-sed -i "s/malariamine/$MINENAME/g" project.xml
-
-if egrep -q ProteinDomain.shortName $PRIORITIES; then
-    echo '#--- Integration key exists.'
-else
-    echo '#---> Adjusting priorities.'
-    echo 'ProteinDomain.shortName = interpro, uniprot-malaria' >> $PRIORITIES
-fi
-
-cd $DIR/dbmodel
-echo '#---> Building DB'
-ant clean build-db >> $DIR/log/build-db.log
-
-if test ! -f $PROJECT_BUILD; then
-    echo "ERROR: Cannot find project build script at $PROJECT_BUILD"
-    exit 1
-fi
-
-echo '#---> Loading data (this could take some time) ...'
-cd $DIR
-$PROJECT_BUILD -b -v $SERVER $HOME/${MINENAME}-dump 2>&1 \
-    | tee -a $LOAD_LOG \
-    | grep -E '(action.*took|failed)'
-
-echo '#--- Finished loading data.'
-cp pbuild.log $DIR/log/
 
 cd $DIR/webapp
 echo '#---> Building userprofile..'
@@ -166,7 +173,7 @@ ant build-db-userprofile >> $DIR/log/build-userprofile-db.log
 echo '#---> Building web-application'
 ant default >> $DIR/log/build-webapp.log
 echo '#---> Releasing web-application'
-ant remove-webapp release-webapp | tee -a $DIR/log/build-webapp.log | grep tomcat-deploy
+ant release-webapp | tee -a $DIR/log/build-webapp.log | grep tomcat-deploy
 
 echo BUILD COMPLETE: Logs available in $DIR/log
 
