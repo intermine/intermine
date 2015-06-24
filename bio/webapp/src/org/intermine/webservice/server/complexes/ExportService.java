36397a49418331abac1b4fe30ffc04541e99aed1
julie-sullivan <julie@flymine.org>
2015-06-24 15:36:48 +0100

    parse interaction

--- bio/webapp/src/org/intermine/webservice/server/complexes/ExportService.java
+++ bio/webapp/src/org/intermine/webservice/server/complexes/ExportService.java
@@ -15,6 +15,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.intermine.api.InterMineAPI;
 import org.intermine.api.query.MainHelper;
 import org.intermine.objectstore.ObjectStoreException;
@@ -33,10 +34,13 @@ import psidev.psi.mi.jami.factory.InteractionWriterFactory;
 import psidev.psi.mi.jami.json.InteractionViewerJson;
 import psidev.psi.mi.jami.json.MIJsonOptionFactory;
 import psidev.psi.mi.jami.json.MIJsonType;
+import psidev.psi.mi.jami.model.CvTerm;
 import psidev.psi.mi.jami.model.InteractionCategory;
 import psidev.psi.mi.jami.model.impl.DefaultComplex;
 import psidev.psi.mi.jami.model.impl.DefaultCvTerm;
+import psidev.psi.mi.jami.model.impl.DefaultInteractor;
 import psidev.psi.mi.jami.model.impl.DefaultModelledParticipant;
+import psidev.psi.mi.jami.model.impl.DefaultOrganism;
 
 /**
  * Web service that produces JSON required by the complex viewer.
@@ -48,6 +52,7 @@ public class ExportService extends JSONService
     private static final String FORMAT_PARAMETER = "format";
     private static final String DEFAULT_FORMAT = "JSON";
     private static final String EBI = "intact";
+    private static final Logger LOG = Logger.getLogger(ExportService.class);
 
     /**
      * Default constructor.
@@ -69,11 +74,15 @@ public class ExportService extends JSONService
         // EBI complex identifier
         String identifier = getComplexIdentifier();
 
+        LOG.error("parsing Complex " + identifier);
+
         // get complex from InterMine database, transform to JAMI complex
         DefaultComplex complex = getComplex(identifier);
 
         // initialise all existing json writers
         InteractionViewerJson.initialiseAllMIJsonWriters();
+
+        LOG.error("InteractionViewerJson.initialiseAllMIJsonWriters(); ");
 
         // get jsob option factory
         MIJsonOptionFactory optionFactory = MIJsonOptionFactory.getInstance();
@@ -114,7 +123,13 @@ public class ExportService extends JSONService
     }
 
     private DefaultComplex getComplex(String identifier) throws ObjectStoreException {
+
+        LOG.error("QUERYING");
+
         Query q = getQuery(identifier);
+
+        LOG.error("QUERYING " + q);
+
         Results results = im.getObjectStore().execute(q);
 
         DefaultComplex complex = new DefaultComplex(identifier);
@@ -122,20 +137,35 @@ public class ExportService extends JSONService
         for (Iterator<?> iter = results.iterator(); iter.hasNext();) {
             ResultsRow<?> row = (ResultsRow<?>) iter.next();
 
+            LOG.error(" *** loopdy");
+
             String name = (String) row.get(0);
+
+            LOG.error(" *** " + name);
             String systematicName = (String) row.get(1);
             String properties = (String) row.get(2);
             String function = (String) row.get(3);
-            String proteinAccession = (String) row.get(4);
+            String primaryIdentifier = (String) row.get(4);
             String stoichiometry = (String) row.get(5);
+            Integer taxonId = (Integer) row.get(6);
+            String biologicalRole = (String) row.get(7);
 
             complex.setFullName(name);
             complex.setSystematicName(systematicName);
             complex.setPhysicalProperties(properties);
 
+            CvTerm type = new DefaultCvTerm("protein");
+            DefaultOrganism organism = new DefaultOrganism(taxonId);
+
+            // java.lang.String name, CvTerm type, Organism organism, Xref uniqueId
+            DefaultInteractor interactor = new DefaultInteractor(primaryIdentifier, type, organism);
+
             DefaultCvTerm stoichTerm = new DefaultCvTerm(stoichiometry);
             DefaultModelledParticipant participant
-                = new DefaultModelledParticipant(complex, stoichTerm);
+                = new DefaultModelledParticipant(interactor, stoichTerm);
+
+            participant.setBiologicalRole(new DefaultCvTerm(biologicalRole));
+
             complex.addParticipant(participant);
         }
         return complex;
@@ -147,8 +177,10 @@ public class ExportService extends JSONService
                 "Complex.systematicName",
                 "Complex.properties",
                 "Complex.function",
-                "Complex.synComplex.allInteractors.participant.primaryIdentifier",
-                "Complex.allInteractors.stoichiometry");
+                "Complex.allInteractors.participant.primaryIdentifier",
+                "Complex.allInteractors.stoichiometry",
+                "Complex.allInteractors.participant.organism.taxonId",
+                "Complex.allInteractors.biologicalRole");
         query.addConstraint(Constraints.eq("Complex.identifier", identifier));
         return MainHelper.makeQuery(query, new HashMap(), new HashMap(), null, new HashMap());
     }
