b73eeddcf9492f777ae85db2c40ea9b9328b7918
julie-sullivan <julie@flymine.org>
2015-10-30 16:55:35 +0000

    checkstyle, fix prop file name, add to project XML

--- bio/sources/human/atlas-express/main/src/org/intermine/bio/dataconversion/AtlasExpressConverter.java
+++ bio/sources/human/atlas-express/main/src/org/intermine/bio/dataconversion/AtlasExpressConverter.java
@@ -15,7 +15,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-import org.apache.log4j.Logger;
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
 import org.intermine.objectstore.ObjectStoreException;
