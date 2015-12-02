96e2b6e0ca205a4d54b17aeec8c07cf105941461
rachellyne <rachel@flymine.org>
2015-12-02 13:15:16 +0000

    HumanGeneIdentifiers.pm

--- bio/scripts/DataDownloader/lib/DataDownloader/Source/HumanGeneIdentifiers.pm
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/HumanGeneIdentifiers.pm
@@ -10,15 +10,23 @@ use Ouch;
 use constant {
     TITLE       => 'Human Gene Identifiers',
     DESCRIPTION => 'Human Gene identifiers from HGNC biomart',
-    SOURCE_LINK => 'http://www.genenames.org/',
+    SOURCE_LINK => 'http://www.ensembl.org/',
     SOURCE_DIR  => 'human/identifiers',
-    BIOMART_SERVER => 'http://biomart.genenames.org/martform/',
+    BIOMART_SERVER => 'http://www.ensembl.org/biomart/martview',
     COMPARE     => 1,
 };
 
 use constant BIOMART_QUERY => q{<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE Query>
-<Query virtualSchemaName = "default" formatter = "TSV" header = "0" uniqueRows = "0" count = "" datasetConfigVersion = "0.7" ><Dataset name="hgnc_gene_mart" config="hgnc_gene_config"><Filter name="hgnc_gene__status_1010" value="Approved" filter_list=""/><Attribute name="hgnc_gene__approved_symbol_1010"/><Attribute name="hgnc_gene__hgnc_gene_id_1010"/><Attribute name="hgnc_gene__ncbi_gene__gene_id_1026"/><Attribute name="hgnc_gene__ensembl_gene__ensembl_gene_id_104"/><Attribute name="hgnc_gene__omim__omim_id_1027"/><Attribute name="hgnc_gene__approved_name_1010"/><Attribute name="hgnc_gene__hgnc_previous_symbol__previous_symbol_1012"/><Attribute name="hgnc_gene__hgnc_previous_name__previous_name_1011"/><Attribute name="hgnc_gene__hgnc_alias_name__alias_name_107"/><Attribute name="hgnc_gene__hgnc_alias_symbol__alias_symbol_108"/><Attribute name="hgnc_gene__chromosome_location_1010"/></Dataset></Query>
+<Query  virtualSchemaName = "default" formatter = "TSV" header = "0" uniqueRows = "0" count = "" datasetConfigVersion = "0.6" >
+			
+	<Dataset name = "hsapiens_gene_ensembl" interface = "default" >
+		<Attribute name = "ensembl_gene_id" />
+		<Attribute name = "description" />
+		<Attribute name = "source" />
+		<Attribute name = "hgnc_symbol" />
+	</Dataset>
+</Query>
 
 };
 
