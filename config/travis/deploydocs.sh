10e7bf520f43855dab42b9843cbe663e132d1e6e
Travis CI <travis@fakemail.com>
2016-04-06 14:37:21 +0000

    rebuild pages at 9398ef4

---
+++ config/travis/deploydocs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+### This script is used by travis to automatically deploy new
+### master branch pushes to gh-pages
+
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+git init
+git config user.name "Travis CI"
+git config user.email "travis@fakemail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+git fetch --depth=1 upstream
+git reset upstream/gh-pages
+
+touch .
+#stops gh-pages trying to build as jekyll
+touch .nojekyll
+
+#build db model for the javadoc
+cd bio/test-all/dbmodel
+ant clean-all
+ant build-db
+
+#build javadoc
+cd ../../../imbuild/javadoc
+ant clean
+ant
+
+cp -r build/javadoc ../../../
+
+cd ../../
+git add -A .
+
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
