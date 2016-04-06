4fe4fdd4f2d23664f484fa2dae57e527fe10c96a
Yo Yehudi <yochannah@gmail.com>
2016-04-06 15:01:53 +0100

    fix git checkout?

--- config/travis/deploydocs.sh
+++ config/travis/deploydocs.sh
@@ -13,19 +13,24 @@ git config user.name "Travis CI"
 git config user.email "travis@fakemail.com"
 
 git remote add upstream "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
-git fetch --depth=1 upstream dev
+git fetch --depth=1 upstream/dev
 git reset upstream/gh-pages
 
 touch .
+#stops gh-pages trying to build as jekyll
 touch .nojekyll
 
+#build db model for the javadoc
 cd bio/test-all/dbmodel
 ant clean-all
 ant build-db
 
+#build javadoc
 cd ../../../imbuild/javadoc
 ant clean
 ant
+
+cp build/javadoc ../../../
 
 git add -A .
 
