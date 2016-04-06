8bd2130184729edbea7a3a4df2fbc70032eae081
Yo Yehudi <yochannah@gmail.com>
2016-04-06 10:54:23 +0100

    travis auto deploy docs tweaks

---
+++ deploydocs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+### This script is used by travis to automatically deploy new
+### master branch pushes to gh-pages
+
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+git init
+git config user.name "Travis CI"
+git config user.email "travis@fakemail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+touch .nojekyll
+
+cd humanmine/webapp
+ant javadoc
+
+git add -A .
+
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
