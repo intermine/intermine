e70fa28cf77471b6d02af0f759e95cf2156b1199
julie-sullivan <julie@flymine.org>
2016-06-01 14:18:33 +0100

    add complexes to config

--- bio/scripts/DataDownloader/lib/DataDownloader/Source/InterPro.pm
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/InterPro.pm
@@ -8,12 +8,30 @@ use constant {
     DESCRIPTION => "Protein Family and Domain data from Interpro",
     SOURCE_LINK => 'http://www.ebi.ac.uk/interpro',
     SOURCE_DIR  => 'interpro',
-    SOURCES => [{
-        FILE => "interpro.xml.gz", 
-        HOST => "ftp.ebi.ac.uk",
-        REMOTE_DIR => "pub/databases/interpro",
-        EXTRACT => 1,
-    }],
+    SOURCES => [
+
+        {
+            SUBTITLE => 'Match complete',
+            HOST => 'ftp.ebi.ac.uk',
+            REMOTE_DIR => 'pub/databases/interpro',
+            FILE => 'match_complete.xml.gz',
+            EXTRACT => 1,
+        },
+        {
+            SUBTITLE => 'Proteins to domains',
+            HOST => 'ftp.ebi.ac.uk',
+            REMOTE_DIR => 'pub/databases/interpro',
+            FILE => 'protein2ipr.dat.gz',
+            EXTRACT => 1,
+        },
+        {
+            SUBTITLE => 'Domains',
+            HOST => 'ftp.ebi.ac.uk',
+            REMOTE_DIR => 'pub/databases/interpro',
+            FILE => 'interpro.xml.gz',
+            EXTRACT => 1,
+        },
+    ],
 };
 
 1;
