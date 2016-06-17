ba5ce7dbb96049296193598287f48ea69a63fe22
julie-sullivan <julie@flymine.org>
2016-06-17 13:58:20 +0100

    better comment

--- intermine/web/main/src/org/intermine/webservice/server/query/result/XMLValidator.java
+++ intermine/web/main/src/org/intermine/webservice/server/query/result/XMLValidator.java
@@ -62,8 +62,9 @@ public class XMLValidator
                 xml = xml.replaceAll(Pattern.quote("</query>"), "</xsq:query>");
             }
 
-            System.out.println("xmlSchemaUrl " + xmlSchemaUrl);
-            LOG.error("xmlSchemaUrl " + xmlSchemaUrl);
+            // changed to use baseURL instead of current URL because tomcat uses a different
+            // URL when in docker. There is no way this could not work.
+            LOG.info("Using the xmlSchemaUrl " + xmlSchemaUrl);
             SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
             URL schemaLocation = new URL(xmlSchemaUrl);
             Reader schemaReader = new InputStreamReader(schemaLocation.openStream());
