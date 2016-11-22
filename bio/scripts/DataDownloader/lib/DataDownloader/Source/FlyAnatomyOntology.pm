246e0d0d25328dc4c6bec625a25b87a59b01fd5f
julie-sullivan <julie@flymine.org>
2016-11-22 11:47:16 +0000

    update URL for Fly Anatomy

--- bio/scripts/DataDownloader/lib/DataDownloader/Source/FlyAnatomyOntology.pm
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/FlyAnatomyOntology.pm
@@ -10,7 +10,7 @@ use constant {
     SOURCE_DIR => 'ontologies/fly-anatomy',
     SOURCES => [{
         FILE   => 'fbbt.obo',
-        SERVER => 'https://raw.githubusercontent.com/FlyBase/drosophila-anatomy-developmental-ontology/master/fbbt/releases/fbbt.obo',
+        SERVER => 'https://raw.githubusercontent.com/FlyBase/drosophila-anatomy-developmental-ontology/master/fbbt/releases/',
     }],
 };
 
