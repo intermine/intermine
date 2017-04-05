778427c7e44e0fb45e036cf16d33606b914ea9a6
julie-sullivan <julie@flymine.org>
2017-04-05 10:02:46 +0100

    update filename

--- bio/scripts/DataDownloader/lib/DataDownloader/Source/NCBIGff.pm
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/NCBIGff.pm
@@ -10,7 +10,7 @@ use constant {
     SOURCE_LINK => "ftp.ncbi.nih.gov",
     SOURCE_DIR => "ncbi",
     SOURCES => [{
-        FILE => "ref_GRCh38.p2_top_level.gff3.gz", 
+        FILE => "ref_GRCh38.p7_top_level.gff3.gz", 
         HOST => "ftp.ncbi.nih.gov",
         REMOTE_DIR => "genomes/H_sapiens/GFF",
         EXTRACT => 1,
