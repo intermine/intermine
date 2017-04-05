4482864056466fed7d664bd5c5ab134189b8a68a
julie-sullivan <julie@flymine.org>
2017-04-05 10:33:59 +0100

    update location of MGI identifiers file

--- bio/scripts/DataDownloader/lib/DataDownloader/Source/MGIIdentifiers.pm
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/MGIIdentifiers.pm
@@ -1,10 +1,11 @@
 package DataDownloader::Source::MGIIdentifiers;
 
 use Moose;
-extends 'DataDownloader::Source::FtpBase';
+extends 'DataDownloader::Source::ABC';
 
 # ftp://ftp.informatics.jax.org/pub/reports/MGI_Coordinate.rpt
 # UPDATE: MGI_Coordinate.rpt was renamed to MGI_Coordinate_build37.rpt on 09/01/2013
+# removed FTP server? April 2017
 
 use constant {
     TITLE => "MGI Identifiers",
@@ -13,8 +14,7 @@ use constant {
     SOURCE_DIR => "mgi-identifiers",
     SOURCES => [{
         FILE => "MRK_List2.rpt", 
-        HOST => "ftp.informatics.jax.org",
-        REMOTE_DIR => "pub/reports",
+        URI => "http://www.informatics.jax.org/downloads/reports",
     }],
 };
 
