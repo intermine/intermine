a81a7a36108ef96dc22a05ab374089ee513ce4d6
julie-sullivan <julie@flymine.org>
2017-04-26 17:04:00 +0100

    update the name and location of the disease obo file

--- bio/scripts/DataDownloader/lib/DataDownloader/Source/DO.pm
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/DO.pm
@@ -6,12 +6,12 @@ extends 'DataDownloader::Source::ABC';
 use constant {
     TITLE => "Disease Ontology",
     DESCRIPTION => "Disease Ontology",
-    SOURCE_LINK => "http://www.geneontology.org",
+    SOURCE_LINK => "http://www.disease-ontology.org",
     SOURCE_DIR => "do",
     SOURCES => [
         {
-            URI => "https://github.com/DiseaseOntology/HumanDiseaseOntology/blob/master/src/ontology",
-            FILE => "HumanDO.obo",
+            URI => "http://purl.obolibrary.org/obo",
+            FILE => "doid.obo",
         },
     ],
 };
