d2223738ed2090eaa5d77360b59bb45e79686ab0
Sam Hokin <shokin@ncgr.org>
2017-05-03 08:47:19 -0600

    Update OboConverter.java

--- bio/core/main/src/org/intermine/bio/dataconversion/OboConverter.java
+++ bio/core/main/src/org/intermine/bio/dataconversion/OboConverter.java
@@ -207,7 +207,7 @@ public class OboConverter extends DataConverter
      */
     protected void configureItem(String termId, Item item, OboTerm term)
         throws ObjectStoreException {
-        if (term.getName()!=null && term.getName().trim().length()>0) {
+        if (term.getName() != null && term.getName().trim().length() > 0) {
             item.addAttribute(new Attribute("name", term.getName()));
         }
         item.addReference(new Reference("ontology", ontology.getIdentifier()));
