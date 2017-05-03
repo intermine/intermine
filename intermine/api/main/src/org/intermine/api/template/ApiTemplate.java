c8660d5e86dabbd58feb0690343e286c26a5a144
julie-sullivan <julie@flymine.org>
2017-05-03 16:27:29 +0100

    use header method instead of copypasting the whole toJSON method. Refs #1528

--- intermine/api/main/src/org/intermine/api/template/ApiTemplate.java
+++ intermine/api/main/src/org/intermine/api/template/ApiTemplate.java
@@ -13,10 +13,8 @@ package org.intermine.api.template;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import org.intermine.api.InterMineAPI;
@@ -28,9 +26,6 @@ import org.intermine.api.tag.TagTypes;
 import org.intermine.api.tracker.TrackerDelegate;
 import org.intermine.model.userprofile.SavedTemplateQuery;
 import org.intermine.model.userprofile.Tag;
-import org.intermine.pathquery.OrderElement;
-import org.intermine.pathquery.OuterJoinStatus;
-import org.intermine.pathquery.PathConstraint;
 import org.intermine.pathquery.PathQuery;
 import org.intermine.template.TemplateQuery;
 
@@ -187,7 +182,7 @@ public class ApiTemplate extends TemplateQuery implements WebSearchable
             }
             retVal.put("tags", tagNames);
         }
-
+        retVal.put("rank", getRank());
         return retVal;
     }
 
@@ -196,81 +191,4 @@ public class ApiTemplate extends TemplateQuery implements WebSearchable
         Integer templateRank = templateTracker.getRank(im.getTemplateManager(), name);
         return templateRank;
     }
-
-    /**
-     * Convert this PathQuery to a JSON serialisation.
-     * @param onlyRelevant  whether to only return relevant, active constraints.
-     * @return This query as json.
-     */
-    public synchronized String toJson(boolean onlyRelevant) {
-        StringBuffer sb = new StringBuffer("{");
-
-        sb.append(String.format("\"model\":{\"name\":\"%s\"}",
-                    im.getModel().getName()));
-
-        for (Entry<String, Object> attr: getHeadAttributes().entrySet()) {
-            addJsonProperty(sb, attr.getKey(), attr.getValue());
-        }
-
-        Integer rank = getRank();
-
-        if (rank != null) {
-            addJsonProperty(sb, "rank", rank.toString());
-        }
-
-        // SORT ORDER
-        List<OrderElement> order = getOrderBy();
-        if (!order.isEmpty()) {
-            sb.append(",\"orderBy\":[");
-            for (Iterator<OrderElement> it = order.iterator(); it.hasNext();) {
-                OrderElement oe = it.next();
-                sb.append(String.format("{\"%s\":\"%s\"}", oe.getOrderPath(), oe.getDirection()));
-                if (it.hasNext()) {
-                    sb.append(",");
-                }
-            }
-            sb.append("]");
-        }
-
-        // JOINS
-        Map<String, OuterJoinStatus> ojs = getOuterJoinStatus();
-        if (!ojs.isEmpty()) {
-            StringBuilder sb2 = new StringBuilder();
-            for (Iterator<Entry<String, OuterJoinStatus>> it = ojs.entrySet().iterator();
-                it.hasNext();) {
-                Entry<String, OuterJoinStatus> pair = it.next();
-                if (pair.getValue() == OuterJoinStatus.OUTER) {
-                    if (sb2.length() > 0) {
-                        sb2.append(",");
-                    }
-                    sb2.append("\"" + pair.getKey() + "\"");
-                }
-            }
-            if (sb2.length() != 0) {
-                sb.append(",\"joins\":[" + sb2.toString() + "]");
-            }
-        }
-
-        // CONSTRAINTS
-        Map<PathConstraint, String> cons =
-                onlyRelevant ? getRelevantConstraints() : getConstraints();
-        if (!cons.isEmpty()) {
-            sb.append(",\"where\":[");
-            Iterator<Entry<PathConstraint, String>> it = cons.entrySet().iterator();
-            while (it.hasNext()) {
-                Entry<PathConstraint, String> pair = it.next();
-
-                sb.append(constraintToJson(pair.getKey(), pair.getValue()));
-                if (it.hasNext()) {
-                    sb.append(",");
-                }
-            }
-            sb.append("]");
-        }
-        sb.append("}");
-
-        return sb.toString();
-    }
-
-
 }
