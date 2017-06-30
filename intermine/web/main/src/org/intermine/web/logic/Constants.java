b6cf7384f48077aa9409c0d4a2d73e7561ec21b5
julie-sullivan <julie@flymine.org>
2017-06-30 08:52:55 +0100

    InterMine 1.8.2

--- intermine/web/main/src/org/intermine/web/logic/Constants.java
+++ intermine/web/main/src/org/intermine/web/logic/Constants.java
@@ -214,7 +214,7 @@ public final class Constants
     /**
      * Current version of the InterMine code
      */
-    public static final String INTERMINE_VERSION = "1.8.1";
+    public static final String INTERMINE_VERSION = "1.8.2";
 
     /**
      * Key for a Map from class name to Boolean.TRUE for all classes in the model that do not have
