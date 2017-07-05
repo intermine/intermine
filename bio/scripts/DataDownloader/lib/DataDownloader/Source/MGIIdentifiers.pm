33c4cefa03dc8c5802f29e9a5cddd2163113ef0d
julie-sullivan <julie@flymine.org>
2017-07-05 10:53:53 +0100

    reformat. Refs #1612

--- bio/scripts/DataDownloader/lib/DataDownloader/Source/MGIIdentifiers.pm
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/MGIIdentifiers.pm
@@ -3,19 +3,17 @@ package DataDownloader::Source::MGIIdentifiers;
 use Moose;
 extends 'DataDownloader::Source::ABC';
 
-# ftp://ftp.informatics.jax.org/pub/reports/MGI_Coordinate.rpt
-# UPDATE: MGI_Coordinate.rpt was renamed to MGI_Coordinate_build37.rpt on 09/01/2013
-# removed FTP server? April 2017
-
 use constant {
     TITLE => 'MGI Identifiers',
     DESCRIPTION => 'Identifiers from MGI',
-    SOURCE_LINK => 'www.informatics.jax.org',
+    SOURCE_LINK => 'http://www.informatics.jax.org',
     SOURCE_DIR => 'mgi-identifiers',
-    SOURCES => [{
-        FILE   => 'MRK_List2.rpt',
-        SERVER => 'http://www.informatics.jax.org/downloads/reports',
-    }],
+    SOURCES => [
+        {
+            SERVER => 'http://www.informatics.jax.org/downloads/reports',
+            FILE => 'MRK_List2.rpt',
+        },
+    ],
 };
 
 1;
