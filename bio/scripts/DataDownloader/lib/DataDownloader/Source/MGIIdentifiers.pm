41db22b0082611876b844d942fb07181261fdf4d
julie-sullivan <julie@flymine.org>
2017-07-06 11:10:16 +0100

    remove whitespace. Refs #1612

--- bio/scripts/DataDownloader/lib/DataDownloader/Source/MGIIdentifiers.pm
+++ bio/scripts/DataDownloader/lib/DataDownloader/Source/MGIIdentifiers.pm
@@ -5,12 +5,12 @@ extends 'DataDownloader::Source::ABC';
 
 use constant {
     TITLE => 'MGI Identifiers',
-    DESCRIPTION => 'Identifiers from MGI',
+    DESCRIPTION => 'MGI Identifiers',
     SOURCE_LINK => 'http://www.informatics.jax.org',
     SOURCE_DIR => 'mgi-identifiers',
     SOURCES => [
         {
-            SERVER => 'http://www.informatics.jax.org/downloads/reports',
+            SERVER => 'http://www.informatics.jax.org/downloads/reports',            
             FILE => 'MRK_List2.rpt',
         },
     ],
