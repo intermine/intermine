d9dd0df45aab684438128b9041c7dc340df2b332
danielabutano <daniela@intermine.org>
2017-08-24 16:38:09 +0100

    bio-core migration started,. Ref #1483

--- bio/toGradle.sh
+++ bio/toGradle.sh
@@ -4,18 +4,19 @@ echo "Converting $1 project to gradle"
 cd $1
 echo "Creating src dir"
 mkdir -p src
-echo "Moving main and test dirs under src ..."
+echo "Moving main and test dirs under src ..."cd sr
 git mv main/ src/
-git mv test/src/
+git mv test/ src/
 echo "Done!"
 
 cd src/main
-mv src/ java/
+git mv src java
 cd ../test
-mv src/ java/
+git mv src java
 echo "Renamed main/src into main/java and test/src into test/java"
 
-cp ../skeleton-build.gradle .
+cd ../..
+cp ../skeleton-build.gradle build.gradle
 mv skeleton-build.gradle build.gradle
 echo "Created a build.gradle skeleton"  
 
