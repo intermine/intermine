837603cb0f4fb5d12ecd3bff21c99ab887ca8bef
danielabutano <daniela@intermine.org>
2017-09-08 15:53:39 +0100

    come fix in the script

--- bio/sources/migrateBioSources.sh
+++ bio/sources/migrateBioSources.sh
@@ -3,31 +3,32 @@ for dir in `ls -d */`
 do
 prj="${dir%%/}"
 
-echo "Converting $prj project to gradle"
-cd $prj
-echo "Creating src dir"
-mkdir -p src
-echo "Moving main and test dirs under src ..."
-git mv main/ src/
-git mv test/ src/
-echo "Done!"
+  if [ -d main ]
+    then
+    echo "Converting $prj project to gradle"
+    cd $prj
+    echo "Creating src dir"
+    mkdir -p src
+    echo "Moving main dir under src ..."
+    git mv main/ src/
+    cd src/main
+    git mv src java
+    echo "Done!"
+  fi
 
-cd src/main
-git mv src java
-rm .project
-rm .classpath
-rm .checkstyle
+  cd ../..
+  if [ -d test ]
+  then
+    echo "Moving test dir under src ..."
+    git mv test/ src/
+    cd src/test
+    git mv src java
+    echo "Done!"
+    cd ../..   
+  fi
 
-cd ../test
-git mv src java
-rm .project
-rm .classpath
-rm .checkstyle
-echo "Renamed main/src into main/java and test/src into test/java"
-
-cd ../..
 cp ../../skeleton-build.gradle build.gradle
 echo "Created a build.gradle skeleton"
-cd ..  
+cd ..
 
 done
