3b5faee8cf04b70e5b20d89b7085b4eecc43b5ff
danielabutano <daniela@intermine.org>
2017-11-23 11:50:06 +0000

    adapted the plugin+bio+biosurces+bitstmine to the renamed intermine properties file

--- plugin/src/main/groovy/org/intermine/plugin/IntegratePlugin.groovy
+++ plugin/src/main/groovy/org/intermine/plugin/IntegratePlugin.groovy
@@ -5,12 +5,22 @@ import org.gradle.api.Project
 
 class IntegratePlugin implements Plugin<Project> {
     void apply(Project project) {
-        project.task('load') {
+        project.task('loadSource') {
             //group "${taskGroup}"
             description "Load items into production database"
             doLast {
-                println 'Load from the org.intermine.plugin.IntegratePlugin'
-                project.ant {}
+                def ant = new AntBuilder()
+                ant.taskdef(name: "dataLoad", classname: "org.intermine.dataloader.ObjectStoreDataLoaderTask") {
+                    classpath {
+                        dirset(dir: project.getBuildDir().getAbsolutePath())
+                        pathelement(path: project.configurations.getByName("compile").asPath)
+                    }
+                }
+                ant.dataLoad(integrationWriter: "integration.production",
+                        source: "os.${common.os.prefix}-translated",
+                        sourceName: "", sourceType: "",
+                        ignoreDuplicates: "",
+                        allSources: "")
             }
         }
     }
