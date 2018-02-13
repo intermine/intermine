intermine/intermine

commit b4665885684cd4fca336cbfa59f78d7e50d4f0f8

I have to disable the generateKeys and gen onlyIf at the front of the addSourceDependencies task for now to get my biotestmine to work
Needs proper fixing

plugin/src/main/groovy/org/intermine/plugin/dbmodel/DBModelPlugin.groovy
@@ -56,7 +56,8 @@ class DBModelPlugin implements Plugin<Project> {
 
         project.task('addSourceDependencies') {
             description "Add associated datasource dependencies"
-            onlyIf {generateKeys && !new File(project.getBuildDir().getAbsolutePath() + File.separator + "gen").exists()}
+            //FIXME: I'm sure this bit is important, but it currently blocks getting my (justincc) biotestmine working
+            //onlyIf {generateKeys && !new File(project.getBuildDir().getAbsolutePath() + File.separator + "gen").exists()}
 
             doLast {
                 String projectXmlFilePath = project.getParent().getProjectDir().getAbsolutePath() + File.separator + "project.xml"

commit 4a0ff0707da7fd4d02fa877ca27340d3fbf40074

previous commit b466588 caused bio project build to fail, so let's just try disabling the gen/ check instead

plugin/src/main/groovy/org/intermine/plugin/dbmodel/DBModelPlugin.groovy
@@ -57,7 +57,7 @@ class DBModelPlugin implements Plugin<Project> {
         project.task('addSourceDependencies') {
             description "Add associated datasource dependencies"
             //FIXME: I'm sure this bit is important, but it currently blocks getting my (justincc) biotestmine working
-            //onlyIf {generateKeys && !new File(project.getBuildDir().getAbsolutePath() + File.separator + "gen").exists()}
+            onlyIf {generateKeys} // && !new File(project.getBuildDir().getAbsolutePath() + File.separator + "gen").exists()}
 
             doLast {
                 String projectXmlFilePath = project.getParent().getProjectDir().getAbsolutePath() + File.separator + "project.xml"