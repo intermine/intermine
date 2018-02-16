4952355cfc061d027017e147274139bc10c00b74
Justin Clark-Casey <justincc@justincc.org>
2018-02-16 13:12:22 +0000

    Fix breaks in IntegratePlugin.integrateMultipleSources that I introduced in d13831b

--- plugin/src/main/groovy/org/intermine/plugin/integrate/IntegratePlugin.groovy
+++ plugin/src/main/groovy/org/intermine/plugin/integrate/IntegratePlugin.groovy
@@ -91,19 +91,21 @@ class IntegratePlugin implements Plugin<Project> {
                     Properties props = bioSourceProperties.getBioSourceProperties(sourceName)
                     if (!props.containsKey("have.file.custom.direct")) {
                         println "Building tgt items database"
-                        dbUtils.createSchema("os." + COMMON_OS_PREFIX + "-tgt-items-std", "fulldata")
+                        dbUtils.createSchema("os." + COMMON_OS_PREFIX + "-tgt-items-std")
                         dbUtils.createTables("os." + COMMON_OS_PREFIX + "-tgt-items-std", "fulldata")
                         dbUtils.storeMetadata("os." + COMMON_OS_PREFIX + "-tgt-items-std", "fulldata")
                     }
+
                     integration.preRetrieveSingleSource(sourceName)
                     println "Retrieving " + sourceName + " in a tgt items database"
                     integration.retrieveSingleSource(sourceName)
 
                     if (!props.containsKey("have.file.custom.direct")) {
                         // need to do this after data is in items DB or loading is too slow
-                        dbUtils.createIndexes("os." + COMMON_OS_PREFIX + "-tgt-items-std", "fulldata", false)
+                        dbUtils.createIndexes("os." + COMMON_OS_PREFIX + "-tgt-items-std", false)
                         dbUtils.analyse("os." + COMMON_OS_PREFIX + "-tgt-items-std", "fulldata")
                     }
+
                     println "Loading " + sourceName + " tgt items into production database"
                     integration.loadSingleSource(intermineProject.sources.get(sourceName))
                 }
