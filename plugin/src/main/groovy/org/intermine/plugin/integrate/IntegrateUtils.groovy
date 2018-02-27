a9f4d337da811d8d8d7565275ee66ae9db168f8a
danielabutano <daniela@intermine.org>
2018-02-27 15:27:52 +0100

    Set osAlias as dynamic properties used by protein2ipr

--- plugin/src/main/groovy/org/intermine/plugin/integrate/IntegrateUtils.groovy
+++ plugin/src/main/groovy/org/intermine/plugin/integrate/IntegrateUtils.groovy
@@ -69,6 +69,11 @@ class IntegrateUtils {
                 ant.project.setProperty(prop.name, prop.value)
             }
         }
+        //protein2ipr source needs it
+        if (bioSourceProperties.containsKey("osAlias")) {
+            ant.project.setProperty("osAlias", bioSourceProperties.getProperty("osAlias"))
+        }
+
         ant.taskdef(name: "convertFile", classname: "org.intermine.task.FileConverterTask") {
             classpath {
                 dirset(dir: gradleProject.getBuildDir().getAbsolutePath())
