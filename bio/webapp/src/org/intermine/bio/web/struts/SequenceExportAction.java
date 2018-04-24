b00b493d427c02bf75d48197bdb1bf51612f69c4
sergio contrino <contrino.it@gmail.com>
2018-04-24 11:31:43 +0100

    removes import (cs) #1759

--- bio/webapp/src/org/intermine/bio/web/struts/SequenceExportAction.java
+++ bio/webapp/src/org/intermine/bio/web/struts/SequenceExportAction.java
@@ -21,7 +21,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
