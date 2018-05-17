f06dcd3476ed7088e1779d1df9852d204d43a5d5
julie-sullivan <julie@flymine.org>
2018-05-17 13:21:22 +0100

    enable travis tests for postprocessing whilst we debug

--- config/travis/run.sh
+++ config/travis/run.sh
@@ -44,12 +44,12 @@ elif [ "$TEST_SUITE" = "bio" ]; then
 
     (cd bio && ./gradlew build)
     (cd bio/sources && ./gradlew build)
-    #(cd bio/postprocess && ./gradlew build)
+    (cd bio/postprocess && ./gradlew build)
 
     echo CHECKING results
     ./config/lib/parse_test_report.py 'bio'
     ./config/lib/parse_test_report.py 'bio/sources'
-    #./config/lib/parse_test_report.py 'bio/postprocess'
+    ./config/lib/parse_test_report.py 'bio/postprocess'
 
     echo ALL TESTS PASSED
 elif [ "$TEST_SUITE" = "checkstyle" ]; then
