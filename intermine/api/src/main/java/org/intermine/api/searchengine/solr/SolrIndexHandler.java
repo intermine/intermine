f9657918565484363c1dc80ff6587deef263f08a
Arunan Sugunakumar <arunans23@gmail.com>
2018-05-18 18:52:48 +0530

    add solr implementation class of IndexHandler

---
+++ intermine/api/src/main/java/org/intermine/api/searchengine/solr/SolrIndexHandler.java
@@ -0,0 +1,12 @@
+package org.intermine.api.searchengine.solr;
+
+import org.intermine.api.searchengine.IndexHandler;
+import org.intermine.objectstore.ObjectStore;
+
+public class SolrIndexHandler implements IndexHandler
+{
+    @Override
+    public void createIndex(ObjectStore os) {
+
+    }
+}
