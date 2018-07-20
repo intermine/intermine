07c3a579c83e5462664512c684fe8ae6fe9b062a
julie-sullivan <julie@flymine.org>
2018-07-20 13:46:18 +0100

    only test the master branch for now

--- config/travis/init.sh
+++ config/travis/init.sh
@@ -61,7 +61,7 @@ else
             # We need the imjs code to exercise the webservices
             $GIT_GET https://github.com/intermine/imjs.git client
         elif [[ "$CLIENT" = "PY" ]]; then
-            $GIT_GET https://github.com/intermine/intermine-ws-client.py client
+            $GIT_GET -b master https://github.com/intermine/intermine-ws-python client
         fi
     fi
 fi
