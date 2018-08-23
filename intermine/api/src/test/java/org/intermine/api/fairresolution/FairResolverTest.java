545acbdb3e6f3dd41979cc628005b1c6e3bb1adc
Justin Clark-Casey <justincc@justincc.org>
2018-08-23 18:10:08 +0100

    add testAddMapping()

--- intermine/api/src/test/java/org/intermine/api/fairresolution/FairResolverTest.java
+++ intermine/api/src/test/java/org/intermine/api/fairresolution/FairResolverTest.java
@@ -6,6 +6,45 @@ import org.junit.Test;
 public class FairResolverTest {
 
     @Test
+    public void testAddMapping() {
+        FairResolver fr = new FairResolver();
+
+        Assert.assertNull(fr.resolve("ensembl", "ENSG00000092054"));
+
+        {
+            fr.addMapping("ensembl", "ENSG00000092054", 1);
+            int id = fr.resolve("ensembl", "ENSG00000092054");
+            Assert.assertEquals(id, 1);
+        }
+
+        // Overwriting the mapping simply replaces
+        {
+            fr.addMapping("ensembl", "ENSG00000092054", 3);
+            int id = fr.resolve("ensembl", "ENSG00000092054");
+            Assert.assertEquals(id, 3);
+        }
+
+        // Prefixes should be case insensitive
+        {
+            fr.addMapping("eNSEMBL", "ENSG1234", 11);
+            int id = fr.resolve("ensembl", "ENSG1234");
+            Assert.assertEquals(id, 11);
+        }
+
+        // But local IDs should not
+        {
+            fr.addMapping("ensembl", "ensg1234", 13);
+            int id = fr.resolve("ensembl", "ENSG1234");
+            Assert.assertEquals(id, 11);
+        }
+
+        {
+            int id = fr.resolve("ensembl", "ensg1234");
+            Assert.assertEquals(id, 13);
+        }
+    }
+
+    @Test
     public void testResolve() {
         FairResolver fr = new FairResolver();
         fr.addMapping("ensembl", "ENSG00000092054", 1);
