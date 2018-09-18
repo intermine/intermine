25d644f7a3c8c08d9c2ea970c714dae6b20acca3
Norbert Auer <norbert.auer@boku.ac.at>
2018-09-18 11:13:07 +0200

    Fix indentation

--- bio/sources/update-publications/main/src/org/intermine/bio/dataconversion/EntrezPublicationsRetriever.java
+++ bio/sources/update-publications/main/src/org/intermine/bio/dataconversion/EntrezPublicationsRetriever.java
@@ -359,34 +359,34 @@ public class EntrezPublicationsRetriever
      * @throws Exception if an error occurs
      */
     protected Reader getReader(Set<Integer> ids) throws Exception {
+        /**
+         * Fix - Use HTTP POST instead of HTTP GET method for uploading
+         * Pubmed Ids
+         * Author: Norbert Auer
+         * e-mail: norbert.auer@boku.ac.at
+         */
+         String url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi";
+         URL obj = new URL(url);
+         HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 
-    /**
-     * Fix - Use HTTP POST instead of HTTP GET method for uploading
-     * Pubmed Ids
-     * Author: Norbert Auer
-     * e-mail: norbert.auer@boku.ac.at
-     */
-     String url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi";
-     URL obj = new URL(url);
-     HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+         // add reuqest header to POST
+         con.setRequestMethod("POST");
 
-     // add reuqest header to POST
-     con.setRequestMethod("POST");
-     //con.setRequestProperty("User-Agent", USER_AGENT);
-     con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+         // con.setRequestProperty("User-Agent", USER_AGENT);
+         con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
-     String urlParameters = "tool=flymine&db=pubmed&rettype=abstract&retmode=xml&id=" + StringUtil.join(ids, ",");
+         String urlParameters = "tool=flymine&db=pubmed&rettype=abstract&retmode=xml&id=" + StringUtil.join(ids, ",");
 
-     // Send post request
-     con.setDoOutput(true);
-     DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-     wr.writeBytes(urlParameters);
-     wr.flush();
-     wr.close();
+         // Send post request
+         con.setDoOutput(true);
+         DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+         wr.writeBytes(urlParameters);
+         wr.flush();
+         wr.close();
 
-     int responseCode = con.getResponseCode();
+         int responseCode = con.getResponseCode();
 
-     return new BufferedReader(new InputStreamReader(con.getInputStream()));
+         return new BufferedReader(new InputStreamReader(con.getInputStream()));
     }
 
     private Set<Item> mapToItems(ItemFactory factory, Map map) {
