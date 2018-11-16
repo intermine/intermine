90d73b6477a3ab56e51af2df938604dc0ec1c0b4
danielabutano <daniela@intermine.org>
2018-11-16 12:50:02 +0000

    removed property not needed anymore

--- plugin/src/main/java/org/intermine/plugin/project/Project.java
+++ plugin/src/main/java/org/intermine/plugin/project/Project.java
@@ -248,15 +248,4 @@ public class Project
             }
         }
     }
-
-    public List<String> getSourceNames(String prefix) {
-        List<String> sourceNames = new ArrayList<>();
-        for (Source source : sources.values()) {
-            String sourcePrefix = source.getPrefix();
-            if (sourcePrefix != null && sourcePrefix.equals(prefix)) {
-                sourceNames.add(source.getName());
-            }
-        }
-        return sourceNames;
-    }
 }
