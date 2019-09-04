e5d2dfa0df6044b45a57e05fd3604c777a0e103e
julie-sullivan <julie@flymine.org>
2019-09-04 14:40:25 +0100

    test fix

--- bio/sources/ncbi-gene/src/test/java/org/intermine/bio/dataconversion/NcbiGeneConverterTest.java
+++ bio/sources/ncbi-gene/src/test/java/org/intermine/bio/dataconversion/NcbiGeneConverterTest.java
@@ -51,7 +51,7 @@ public class NcbiGeneConverterTest extends ItemsTestCase
      */
     public void testSimpleFiles() throws Exception {
         process("gene_info");
-        assertEquals(88, itemWriter.getItems().size());
+        assertEquals(84, itemWriter.getItems().size());
     }
 
 
