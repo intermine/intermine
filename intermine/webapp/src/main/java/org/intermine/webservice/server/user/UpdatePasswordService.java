1c9e5e419e52cd8fbd48e23e6303b0c6329284fd
danielabutano <daniela@intermine.org>
2019-11-04 16:46:52 +0000

    Added a new method to /user ws, to update the password. #2144

---
+++ intermine/webapp/src/main/java/org/intermine/webservice/server/user/UpdatePasswordService.java
@@ -0,0 +1,42 @@
+package org.intermine.webservice.server.user;
+
+/*
+ * Copyright (C) 2002-2019 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+import org.intermine.api.InterMineAPI;
+import org.intermine.api.profile.Profile;
+import org.intermine.api.profile.ProfileManager;
+import org.intermine.webservice.server.core.JSONService;
+import org.intermine.webservice.server.exceptions.UnauthorizedException;
+
+public class UpdatePasswordService extends JSONService
+{
+    /**
+     * Constructor
+     * @param im The InterMine API object.
+     */
+    public UpdatePasswordService(InterMineAPI im) {
+        super(im);
+    }
+
+    @Override
+    protected void execute() throws Exception {
+        String newPassword = getRequiredParameter("newPassword");
+        ProfileManager pm = im.getProfileManager();
+        Profile profile;
+        if (isAuthenticated()) {
+            profile = getPermission().getProfile();
+        } else {
+            throw new UnauthorizedException("The request " +
+                    "must be authenticated");
+        }
+
+        pm.setPassword(profile.getUsername(), newPassword);
+    }
+}
