e855a42603146612cb9df6e5675a784459eb2591
asherpasha <asher.pasha@utoronto.ca>
2020-10-01 11:08:11 -0400

    Update InterMine Build system to Travis Ubuntu Bionic 18.04 (Solr Updated)

--- config/travis/init-solr.sh
+++ config/travis/init-solr.sh
@@ -1,7 +1,7 @@
 # set up solr for testmine
 # testmine's setup script populates these empty indexes
 
-wget http://archive.apache.org/dist/lucene/solr/7.2.1/solr-7.2.1.tgz  
-tar xzf solr-7.2.1.tgz && ./solr-7.2.1/bin/solr start
-./solr-7.2.1/bin/solr create -c intermine-search
-./solr-7.2.1/bin/solr create -c intermine-autocomplete
+wget http://archive.apache.org/dist/lucene/solr/8.6.2/solr-8.6.2.tgz
+tar xzf solr-8.6.2.tgz && ./solr-8.6.2/bin/solr start
+./solr-8.6.2/bin/solr create -c intermine-search
+./solr-8.6.2/bin/solr create -c intermine-autocomplete
