0a39d0dbf17e18b7e61ec3d71dae40491e7d47f1
Daniela Butano <daniela@intermine.org>
2021-05-24 12:40:22 +0100

    release 5.0.2

--- intermine/webapp/src/main/java/org/intermine/web/logic/Constants.java
+++ intermine/webapp/src/main/java/org/intermine/web/logic/Constants.java
@@ -225,7 +225,7 @@ public final class Constants
     /**
      * Current version of the InterMine code
      */
-    public static final String INTERMINE_VERSION = "5.0.1";
+    public static final String INTERMINE_VERSION = "5.0.2";
 
     /**
      * Key for a Map from class name to Boolean.TRUE for all classes in the model that do not have
