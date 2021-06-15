54034e1088b4d7780e762fa1227842f8d5aed16d
Daniela Butano <daniela@intermine.org>
2021-06-15 15:17:00 +0100

    updated permanent-url WS to accept the type=rdf used by BG see #2380

--- intermine/webapp/src/main/java/org/intermine/webservice/server/fair/PermanentURLService.java
+++ intermine/webapp/src/main/java/org/intermine/webservice/server/fair/PermanentURLService.java
@@ -13,6 +13,8 @@ package org.intermine.webservice.server.fair;
 import org.apache.commons.lang.StringUtils;
 import org.intermine.api.InterMineAPI;
 import org.intermine.web.logic.PermanentURIHelper;
+import org.intermine.webservice.server.Format;
+import org.intermine.webservice.server.WebServiceRequestParser;
 import org.intermine.webservice.server.core.JSONService;
 
 /**
@@ -40,11 +42,15 @@ public class PermanentURLService extends JSONService
     @Override
     protected void execute() throws Exception {
         String id = getRequiredParameter("id");
+        String type = getOptionalParameter("type");
         String url = (new PermanentURIHelper(request)).getPermanentURL(
                 Integer.parseInt(id), getPermission().getProfile());
         if (url == null) {
             addOutputInfo("url", StringUtils.EMPTY);
         } else {
+            if (type != null && type.equals(WebServiceRequestParser.FORMAT_PARAMETER_RDF)) {
+                url += ".rdf";
+            }
             addOutputInfo("url", url);
         }
     }
