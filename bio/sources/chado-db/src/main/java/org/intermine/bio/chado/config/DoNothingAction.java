2de4a0def83b277b738caf6a04324a3fb91031e8
Sam Hokin <shokin@ncgr.org>
2023-11-20 11:15:20 -0700

    Don't need to yank actual code, just line in settings.gradle.

---
+++ bio/sources/chado-db/src/main/java/org/intermine/bio/chado/config/DoNothingAction.java
@@ -0,0 +1,20 @@
+package org.intermine.bio.chado.config;
+
+/*
+ * Copyright (C) 2002-2022 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+/**
+ * An action that does nothing.
+ * @author Kim Rutherford
+ */
+public class DoNothingAction extends ConfigAction
+{
+    // do nothing for this data
+}
