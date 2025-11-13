495b32b277dc3dfbcfff84625c24cf440587d8a4
Martin Burchell <mb2353@cam.ac.uk>
2025-11-13 16:52:41 +0000

    Make Gradle less noisy

--- config/ci/run.sh
+++ config/ci/run.sh
@@ -21,7 +21,8 @@ if [ "$TEST_SUITE" = "intermine" ]; then
     echo "RUNNING intermine unit tests"
     (cd "${WORKSPACE_DIR}"/plugin && ./gradlew install)
     # Add --rerun-tasks below to force rebuild
-    (cd "${WORKSPACE_DIR}"/intermine && ./gradlew build --info --stacktrace)
+    # Add --info --stacktrace for more useful information when debugging
+    (cd "${WORKSPACE_DIR}"/intermine && ./gradlew build)
 
     echo CHECKING results
     "${WORKSPACE_DIR}"/config/lib/parse_test_report.py "${WORKSPACE_DIR}/intermine"
