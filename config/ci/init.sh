b74a674a71e5f375d1655c56b7c39f4ef5e2cbc4
Martin Burchell <mb2353@cam.ac.uk>
2026-01-17 11:16:20 +0000

    Build Intermine plugin in checkstyle workflow

--- config/ci/init.sh
+++ config/ci/init.sh
@@ -30,9 +30,7 @@ GIT_GET="git clone --single-branch --depth 1"
 export KEYSTORE=${PWD}/keystore.jks
 
 if [ "$TEST_SUITE" = "checkstyle" ]; then
-    # TODO: It's pretty pointless to run this script with this suite
-    # Better to have custom scripts with shared common functions
-    # and fewer conditionals
+    (cd "${WORKSPACE_DIR}"/plugin && ./gradlew install --info)
     exit 0 # nothing to do
 fi
 
