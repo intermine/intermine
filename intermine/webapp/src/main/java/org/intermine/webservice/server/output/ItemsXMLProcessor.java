5bd33225e360664b172b1aa18091d780d64faaf9
Martin Burchell <mb2353@cam.ac.uk>
2026-06-24 12:57:39 +0100

    Handle case where child is processed before parent in ItemsXMLProcessor

--- intermine/webapp/src/main/java/org/intermine/webservice/server/output/ItemsXMLProcessor.java
+++ intermine/webapp/src/main/java/org/intermine/webservice/server/output/ItemsXMLProcessor.java
@@ -76,7 +76,6 @@ public class ItemsXMLProcessor extends ResultProcessor
 
                     FastPathObject obj = element.getObject();
 
-                    // putIfAbsent may not be necessary here
                     objMap.putIfAbsent(currentClassName, obj);
                     fieldsMap.putIfAbsent(currentClassName, new HashSet<>());
 
@@ -87,13 +86,11 @@ public class ItemsXMLProcessor extends ResultProcessor
                         Path partialPath = new Path(im.getModel(), stringPath);
                         if (partialPath.endIsReference() || partialPath.endIsCollection()) {
                             FieldDescriptor fd = partialPath.getEndFieldDescriptor();
-
-                            if (fd != null) {
-                                String parentClassName =
-                                    partialPath.getSecondLastClassDescriptor().getName();
-                                Set<String> parentFields = fieldsMap.get(parentClassName);
-                                parentFields.add(fd.getName());
-                            }
+                            String parentClassName =
+                                partialPath.getSecondLastClassDescriptor().getName();
+                            fieldsMap.putIfAbsent(parentClassName, new HashSet<>());
+                            Set<String> parentFields = fieldsMap.get(parentClassName);
+                            parentFields.add(fd.getName());
                         }
                     } catch (PathException pe) {
                         throw new BadRequestException(stringPath + " is not a valid path");
